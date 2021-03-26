88348882c4dbbbde697608f358951c06e407806f
Drew Powers <drew@pow.rs>
2021-03-26 11:44:05 -0600

    Add SSR Styles test

---
+++ test/fixtures/astro-styles-ssr/astro/components/ReactCSS.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './ReactCSS.css';
+
+function ReactCSS() {
+  return <h1 className="react-title">React CSS</h1>;
+}
+export default ReactCSS;
---
+++ test/fixtures/astro-styles-ssr/astro/components/VueCSS.vue
@@ -0,0 +1,9 @@
+<style>
+.vue-title {
+  font-family: cursive;
+}
+</style>
+
+<template>
+  <h1 class="vue-title">Vue CSS</h1>
+</template>
