5b49811e8203bf381f32a6a64811535d8774e1f4
Matthew Phillips <matthew@skypack.dev>
2021-03-31 16:36:59 -0400

    Disable empty fn rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     'no-shadow': 'warn',
     'prettier/prettier': 'error',
     'prefer-const': 'off',
