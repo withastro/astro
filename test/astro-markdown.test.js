ac22d94e1170bce5f1f50abfdf46052369a0edf8
Matt Mulder <muldmatt@gmail.com>
2021-04-13 13:04:01 -0400

    fix: bundle client-side code for components used in .md pages (#78)

--- test/astro-markdown.test.js
+++ test/astro-markdown.test.js
@@ -1,15 +1,22 @@
+import { existsSync, promises as fsPromises } from 'fs';
+import { join } from 'path';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { createRuntime } from '../lib/runtime.js';
+import { build } from '../lib/build.js';
 import { loadConfig } from '../lib/config.js';
 import { doc } from './test-utils.js';
 
+const { rmdir, readFile } = fsPromises;
+
 const Markdown = suite('Astro Markdown');
 
-let runtime, setupError;
+let runtime, setupError, fixturePath, astroConfig;
 
 Markdown.before(async () => {
-  const astroConfig = await loadConfig(new URL('./fixtures/astro-markdown', import.meta.url).pathname);
+  fixturePath = new URL('./fixtures/astro-markdown', import.meta.url).pathname;
+
+  astroConfig = await loadConfig(fixturePath);
 
   const logging = {
     level: 'error',
@@ -26,6 +33,7 @@ Markdown.before(async () => {
 
 Markdown.after(async () => {
   (await runtime) && runtime.shutdown();
+  rmdir(join(fixturePath, '_site'), { recursive: true });
 });
 
 Markdown('No errors creating a runtime', () => {
@@ -48,6 +56,15 @@ Markdown('Can load more complex jsxy stuff', async () => {
   const $ = doc(result.contents);
   const $el = $('#test');
   assert.equal($el.text(), 'Hello world');
+});
+
+Markdown('Bundles client-side JS for prod', async () => {
+  await build(astroConfig);
+
+  const complexHtml = await readFile(join(fixturePath, './_site/complex/index.html'), 'utf-8');
+
+  assert.match(complexHtml, `import("/_astro/components/Counter.js"`);
+  assert.ok(existsSync(join(fixturePath, `./_site/_astro/components/Counter.js`)), 'Counter.jsx is bundled for prod');
 });
 
 Markdown.run();
--- test/fixtures/astro-markdown/astro/pages/complex.md
+++ test/fixtures/astro-markdown/astro/pages/complex.md
@@ -4,8 +4,10 @@ title: My Blog Post
 description: This is a post about some stuff.
 import:
   Hello: '../components/Hello.jsx'
+  Counter: '../components/Counter.jsx'
 ---
 
 ## Interesting Topic
 
 <Hello name={`world`} />
+<Counter:load />
