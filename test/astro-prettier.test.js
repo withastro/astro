3ac56824759a0d3ef3c38749cae327af1b2e770a
Nate Moore <nate@skypack.dev>
2021-04-23 12:40:25 -0500

    chore: format

--- test/astro-prettier.test.js
+++ test/astro-prettier.test.js
@@ -7,15 +7,15 @@ const Prettier = suite('Prettier formatting');
 
 setup(Prettier, './fixtures/astro-prettier');
 
-/** 
-  * Utility to get `[src, out]` files 
-  * @param name {string}
-  * @param ctx {any}
-  */
+/**
+ * Utility to get `[src, out]` files
+ * @param name {string}
+ * @param ctx {any}
+ */
 const getFiles = async (name, { readFile }) => {
   const [src, out] = await Promise.all([readFile(`/in/${name}.astro`), readFile(`/out/${name}.astro`)]);
   return [src, out];
-}
+};
 
 Prettier('can format a basic Astro file', async (ctx) => {
   const [src, out] = await getFiles('basic', ctx);
@@ -28,7 +28,7 @@ Prettier('can format a basic Astro file', async (ctx) => {
 Prettier('can format an Astro file with frontmatter', async (ctx) => {
   const [src, out] = await getFiles('frontmatter', ctx);
   assert.not.equal(src, out);
-  
+
   const formatted = format(src);
   assert.equal(formatted, out);
 });
@@ -36,7 +36,7 @@ Prettier('can format an Astro file with frontmatter', async (ctx) => {
 Prettier('can format an Astro file with embedded JSX expressions', async (ctx) => {
   const [src, out] = await getFiles('embedded-expr', ctx);
   assert.not.equal(src, out);
-  
+
   const formatted = format(src);
   assert.equal(formatted, out);
 });
