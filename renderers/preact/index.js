12fe0db162c5e1bf35f827091211879ad682bc68
Nate Moore <nate@skypack.dev>
2021-04-30 09:27:30 -0500

    fix: preact plugin

--- renderers/preact/index.js
+++ renderers/preact/index.js
@@ -1,4 +1,4 @@
-export const preact = {
+export default {
     jsxFactory: 'h',
     jsxFragmentFactory: 'Fragment',
     jsxImportSource: 'preact',
@@ -22,74 +22,6 @@ export const preact = {
             return (Component, props, children) => `
                 const {h,hydrate} = ${jsx};
                 hydrate(h(${Component},${props},${children}),${el})
-            `
-        }
-    }
-}
-
-export const react = {
-    jsxFactory: 'createElement',
-    jsxFragmentFactory: 'Fragment',
-    jsxImportSource: 'react',
-    validExtensions: ['.jsx', '.tsx'],
-    transformChildrenTo: 'jsx',
-    
-    server: {
-        renderer: 'renderToStaticMarkup',
-        rendererSource: 'react-dom/server.js',
-        renderToStaticMarkup(jsx, renderToStaticMarkup) {
-            const { createElement } = jsx;
-            return async (Component, props, children) => {
-                const html = renderToStaticMarkup(createElement(Component, props, children));
-                return { html }
-            }
-        }
-    },
-
-    client: {
-        renderer: 'hydrate',
-        rendererSource: 'react-dom',
-        hydrateStaticMarkup(jsx, lib, el) {
-            return (Component, props, children) => `
-                const {createElement:h} = ${jsx};
-                const {hydrate} = ${lib};
-                hydrate(h(${Component},${props},${children}),${el})
-            `
-        }
-    }
-}
-
-export const vue = {
-    jsxFactory: 'h',
-    jsxImportSource: 'vue',
-    validExtensions: ['.vue'],
-    transformChildrenTo: 'string',
-    
-    server: {
-        renderer: 'renderToString',
-        rendererSource: '@vue/server-renderer',
-        renderToStaticMarkup(lib, renderToString) {
-            const { defineComponent, createSSRApp } = lib;
-            return async (Component, props, children) => {
-                const App = defineComponent({
-                    components: { Component },
-                    data: () => ({ props }),
-                    template: `<Component v-bind="props">${children.join('\n')}</Component>`,
-                });
-                const app = createSSRApp(App);
-                const html = await renderToString(app);
-                return { html };
-            }
-        }
-    },
-
-    client: {
-        rendererSource: 'vue',
-        hydrateStaticMarkup(_, lib, el) {
-            return (Component, props, children) => `
-                const {createApp,h} = ${lib};
-                const app = {render:()=>h(${Component},${props},{default:()=>${children}})};
-                createApp(app).mount(${el});
             `
         }
     }
