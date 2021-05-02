8a92293e161faba4c790973f45417637e349d42e
Kevin (Kun) Kassimo Qian <kevinkassimo@gmail.com>
2021-05-01 19:43:44 -0700

    Include outPath in the error logging for bad load status

--- packages/astro/src/build.ts
+++ packages/astro/src/build.ts
@@ -68,7 +68,7 @@ async function writeFilep(outPath: URL, bytes: string | Buffer, encoding: 'utf8'
 /** Utility for writing a build result to disk */
 async function writeResult(result: LoadResult, outPath: URL, encoding: null | 'utf8') {
   if (result.statusCode === 500 || result.statusCode === 404) {
-    error(logging, 'build', result.error || result.statusCode);
+    error(logging, 'build', result.error || result.statusCode, `when generating ${fileURLToPath(outPath)}`);
   } else if (result.statusCode !== 200) {
     error(logging, 'build', `Unexpected load result (${result.statusCode}) for ${fileURLToPath(outPath)}`);
   } else {
