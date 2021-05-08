058d6bfbd724f3567e477e476e0bf25049efaacf
Nate Moore <nate@skypack.dev>
2021-05-08 12:31:01 -0400

    chore: update remote-markdown examples

--- examples/remote-markdown/src/components/Yell.jsx
+++ examples/remote-markdown/src/components/Yell.jsx
@@ -1,8 +1,5 @@
 import { h, Fragment } from 'preact';
 
 export default function Yell({ children }) {
-  return children.map(child => {
-    if (typeof child === 'string') return <Fragment>{`${child.toUpperCase()}!`}</Fragment>;
-    return <Fragment>{child}</Fragment>
-  })
+  return children.filter(v => typeof v === 'string').join('').toUpperCase() + '!'
 }
