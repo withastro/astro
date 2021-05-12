9d6567c27493e1d6b031782354a109736f8118ec
Nate Moore <nate@skypack.dev>
2021-05-12 13:14:56 -0500

    feat: add svelte renderer

---
+++ packages/astro-renderer-svelte/SvelteWrapper.svelte
@@ -0,0 +1,7 @@
+<script>
+const { __astro_component: Component, __astro_children, ...props } = $$props;
+</script>
+
+<svelte:component this={Component} {...props}>
+    {@html __astro_children}
+</svelte:component>
---
+++ packages/astro-renderer-svelte/src/index.ts
@@ -0,0 +1,38 @@
+import type { AstroRenderer } from 'astro/renderer';
+import type { SvelteComponent } from 'svelte';
+
+interface SvelteDependencies {
+  shared: {},
+  server: {
+    './SvelteWrapper.svelte': { render: (props: any) => { html: string } }
+  },
+  client: {
+    './runtime.js': typeof import('./runtime')
+  }
+}
+
+const renderer: AstroRenderer<SvelteDependencies, SvelteComponent> = {
+  snowpackPlugin: ['@snowpack/plugin-svelte', { compilerOptions: { hydratable: true } }],
+
+  server: {
+    dependencies: ['./SvelteWrapper.svelte'],
+    renderToStaticMarkup({ ["./SvelteWrapper.svelte"]: SvelteWrapper }) {
+      return async (Component, props, children) => {
+          const { html: code } = SvelteWrapper.render({ __astro_component: Component, __astro_children: children.join('\n'), ...props });
+          return { '.html': { code } };
+      };
+    },
+  },
+
+  client: {
+    dependencies: ['./runtime.js'],
+    hydrateStaticMarkup({ ["./runtime.js"]: runtime }, el) {
+      return (Component, props, children) => `
+        const {default:render} = ${runtime};
+        render(${el}, ${Component}, ${props}, ${children});
+      `;
+    },
+  },
+};
+
+export default renderer;
