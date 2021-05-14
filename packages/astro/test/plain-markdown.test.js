c8631c86fb9346070b6701d91232c8bb72f59b41
Nate Moore <nate@skypack.dev>
2021-05-14 12:30:42 -0500

    test: add realworld markdown example

--- packages/astro/test/plain-markdown.test.js
+++ packages/astro/test/plain-markdown.test.js
@@ -1,46 +1,14 @@
-import { promises as fsPromises } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
-import { createRuntime } from '#astro/runtime';
-import { loadConfig } from '#astro/config';
 import { doc } from './test-utils.js';
+import { setup, setupBuild } from './helpers.js';
 
-const { rmdir, readFile } = fsPromises;
+const Markdown = suite('Plain Markdown tests');
 
-const Markdown = suite('Plain Markdown');
+setup(Markdown, './fixtures/plain-markdown');
+setupBuild(Markdown, './fixtures/plain-markdown');
 
-let runtime, setupError, fixturePath, astroConfig;
-
-Markdown.before(async () => {
-  fixturePath = fileURLToPath(new URL('./fixtures/plain-markdown', import.meta.url));
-
-  astroConfig = await loadConfig(fixturePath);
-
-  const logging = {
-    level: 'error',
-    dest: process.stderr,
-  };
-
-  try {
-    runtime = await createRuntime(astroConfig, { logging });
-  } catch (err) {
-    console.error(err);
-    setupError = err;
-  }
-});
-
-Markdown.after(async () => {
-  (await runtime) && runtime.shutdown();
-  rmdir(join(fixturePath, 'dist'), { recursive: true });
-});
-
-Markdown('No errors creating a runtime', () => {
-  assert.equal(setupError, undefined);
-});
-
-Markdown('Can load markdown pages with Astro', async () => {
+Markdown('Can load a simple markdown page with Astro', async ({ runtime }) => {
   const result = await runtime.load('/post');
 
   assert.equal(result.statusCode, 200);
@@ -51,5 +19,20 @@ Markdown('Can load markdown pages with Astro', async () => {
   assert.equal($('#first').text(), 'Some content');
   assert.equal($('#interesting-topic').text(), 'Interesting Topic');
 });
+
+Markdown('Can load a realworld markdown page with Astro', async ({ runtime }) => {
+  const result = await runtime.load('/realworld');
+  if (result.error) throw new Error(result.error);
+
+  assert.equal(result.statusCode, 200);
+  const $ = doc(result.contents);
+
+  assert.equal($('pre').length, 7);
+});
+
+Markdown('Builds markdown pages for prod', async (context) => {
+  await context.build();
+});
+
 
 Markdown.run();
