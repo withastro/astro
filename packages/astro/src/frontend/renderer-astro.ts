2f2a0f2471bed0834e992b86f079a2cf2b8d81f1
Nate Moore <nate@skypack.dev>
2021-05-25 12:38:10 -0500

    fix: Astro renderer should be async

--- packages/astro/src/frontend/renderer-astro.ts
+++ packages/astro/src/frontend/renderer-astro.ts
@@ -2,8 +2,8 @@ export default () => {
   return {
     name: 'astro',
     check: (Component: any) => Component.isAstroComponent,
-    renderToStaticMarkup: (Component: any, props: any, children: string) => {
-      const html = Component.__render(props, children);
+    renderToStaticMarkup: async (Component: any, props: any, children: string) => {
+      const html = await Component.__render(props, children);
       return { html }
     },
   }
