a2528a163baaa26651bfbad19eac7f7bf99ff567
Nate Moore <nate@skypack.dev>
2021-05-25 13:09:11 -0500

    chore: normalize config interface

--- packages/renderers/preact/index.js
+++ packages/renderers/preact/index.js
@@ -2,18 +2,16 @@ import { h } from 'preact';
 import { renderToString } from 'preact-render-to-string';
 import StaticHtml from './static-html';
 
-export default () => {
-  return {
-    name: '@astro-renderer/preact',
-    check: (Component, props) => {
-      try {
-        return Boolean(renderToString(h(Component, props)));
-      } catch (e) {}
-      return false;
-    },
-    renderToStaticMarkup: (Component, props, children) => {
-      const html = renderToString(h(Component, props, h(StaticHtml, { value: children })))
-      return { html }
-    },
-  }
-};
+export default {
+  name: '@astro-renderer/preact',
+  check: (Component, props) => {
+    try {
+      return Boolean(renderToString(h(Component, props)));
+    } catch (e) {}
+    return false;
+  },
+  renderToStaticMarkup: (Component, props, children) => {
+    const html = renderToString(h(Component, props, h(StaticHtml, { value: children })))
+    return { html }
+  },
+}
--- packages/renderers/react/index.js
+++ packages/renderers/react/index.js
@@ -2,18 +2,16 @@ import { createElement } from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import StaticHtml from './static-html';
 
-export default () => {
-  return {
-    name: '@astro-renderer/react',
-    check: (Component, props) => {
-      try {
-        return Boolean(renderToStaticMarkup(createElement(Component, props)));
-      } catch (e) {}
-      return false;
-    },
-    renderToStaticMarkup: (Component, props, children) => {
-      const html = renderToStaticMarkup(createElement(Component, props, createElement(StaticHtml, { value: children })))
-      return { html };
-    }
+export default {
+  name: '@astro-renderer/react',
+  check: (Component, props) => {
+    try {
+      return Boolean(renderToStaticMarkup(createElement(Component, props)));
+    } catch (e) {}
+    return false;
+  },
+  renderToStaticMarkup: (Component, props, children) => {
+    const html = renderToStaticMarkup(createElement(Component, props, createElement(StaticHtml, { value: children })))
+    return { html };
   }
-};
+}
