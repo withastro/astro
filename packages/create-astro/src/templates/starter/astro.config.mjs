5dc3aa007ade6d581474a70363f33422aee58367
duncanhealy <duncanhealy@users.noreply.github.com>
2021-05-26 19:56:19 +0100

    chore: update getting started config #245 (#250)
    * chore: update getting started config #245
    
    * chore: reorder renderers

--- packages/create-astro/src/templates/starter/astro.config.mjs
+++ packages/create-astro/src/templates/starter/astro.config.mjs
@@ -1,16 +1,36 @@
 export default {
+  /** Where to resolve all URLs relative to. Useful if you have a monorepo project. */
   projectRoot: '.',
+  /** Path to Astro components, pages, and data */
   astroRoot: './src',
+  /** When running `astro build`, path to final static output */
   dist: './dist',
+  /** A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing. */
   public: './public',
+  /** Extension-specific handlings */
   extensions: {
+    /** Set this to "preact" or "react" to determine what *.jsx files should load */
     '.jsx': 'react',
   },
-  snowpack: {
-    optimize: {
-      bundle: false,
-      minify: true,
-      target: 'es2018',
-    },
+  /** Options specific to `astro build` */
+  buildOptions: {
+    /** Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs. */
+    site: '',
+    /** Generate sitemap (set to "false" to disable) */
+    sitemap: true,
   },
+  /** Options for the development server run with `astro dev`. */
+  devOptions: {
+    /** The port to run the dev server on. */
+    port: 3000,
+    /** Path to tailwind.config.js if used, e.g. './tailwind.config.js' */
+    tailwindConfig: undefined,
+  },
+  /** default array of rendering packages inserted into runtime */
+  renderers: [
+    '@astro-renderer/preact',
+    '@astro-renderer/react',
+    '@astro-renderer/svelte',
+    '@astro-renderer/vue',
+  ],
 };
