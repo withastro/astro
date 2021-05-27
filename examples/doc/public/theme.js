a5782a37c8c72b0a926107552649b5adc30eb7aa
natemoo-re <natemoo-re@users.noreply.github.com>
2021-05-27 14:17:27 +0000

    [ci] yarn format

--- examples/doc/public/theme.js
+++ examples/doc/public/theme.js
@@ -1,8 +1,8 @@
 (() => {
   const root = document.documentElement;
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    root.classList.add('theme-dark')
+    root.classList.add('theme-dark');
   } else {
-    root.classList.remove('theme-dark')
+    root.classList.remove('theme-dark');
   }
-})()
+})();
--- examples/doc/src/components/ThemeToggle.tsx
+++ examples/doc/src/components/ThemeToggle.tsx
@@ -2,33 +2,37 @@ import type { FunctionalComponent } from 'preact';
 import { h, Fragment } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
 
-const themes = [
-  'system',
-  'light',
-  'dark'
-]
+const themes = ['system', 'light', 'dark'];
 
 const icons = [
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z" clip-rule="evenodd" />
-</svg>,
+    <path
+      fill-rule="evenodd"
+      d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z"
+      clip-rule="evenodd"
+    />
+  </svg>,
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
-    <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
+    <path
+      fillRule="evenodd"
+      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+      clipRule="evenodd"
+    />
   </svg>,
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
     <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-  </svg>
-]
+  </svg>,
+];
 
 const ThemeToggle: FunctionalComponent = () => {
   const [theme, setTheme] = useState(themes[0]);
 
   useEffect(() => {
-    const user = localStorage.getItem('theme')
+    const user = localStorage.getItem('theme');
     if (!user) return;
     setTheme(user);
-  }, [])
-  
+  }, []);
+
   useEffect(() => {
     const root = document.documentElement;
     if (theme === 'system') {
@@ -46,20 +50,22 @@ const ThemeToggle: FunctionalComponent = () => {
         root.classList.add('theme-dark');
       }
     }
-  }, [theme])
+  }, [theme]);
 
-  return <div id="theme-toggle">
-    {themes.map((t, i) => {
-      const icon = icons[i];
-      const checked = t === theme;
-      return (
-        <label className={checked ? 'checked' : ''}>
-          {icon}
-          <input type="radio" name="theme-toggle" checked={checked} value={t} title={`Use ${t} theme`} aria-label={`Use ${t} theme`} onChange={() => setTheme(t)} />
-        </label>
-      );
-    })}
-  </div>
-}
+  return (
+    <div id="theme-toggle">
+      {themes.map((t, i) => {
+        const icon = icons[i];
+        const checked = t === theme;
+        return (
+          <label className={checked ? 'checked' : ''}>
+            {icon}
+            <input type="radio" name="theme-toggle" checked={checked} value={t} title={`Use ${t} theme`} aria-label={`Use ${t} theme`} onChange={() => setTheme(t)} />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
 
 export default ThemeToggle;
