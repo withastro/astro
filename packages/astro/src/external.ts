b7df77a5d381f1fd1398d5d639be5b22816a0ded
Matthew Phillips <matthew@matthewphillips.info>
2021-05-28 18:51:50 -0400

    Improve dev perf by not esinstalling Node deps (#253)
    * Improve dev startup by making most dependencies externals
    
    This moves most dependencies to be an `external` for Snowpack, preventing backend deps from going through esinstall.
    
    * Update benchmark times
    
    * chore: add benchmark script for convenience
    
    * fix: update externals to allow renderers
    
    * chore: update benchmark times
    
    Co-authored-by: Nate Moore <nate@skypack.dev>

---
+++ packages/astro/src/external.ts
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const pkg = require('../package.json');
+
+/**
+ * This file allows us to automatically exclude
+ * particular packages from Snowpack's `esinstall`
+ * step.
+ */
+
+// These packages SHOULD be built by `esinstall`
+const allowList = new Set([
+  'astro-prism',
+  'prismjs',
+  'shorthash'
+]);
+
+const isAstroRenderer = (name: string) => {
+  return name.startsWith(`@astrojs/renderer-`);
+}
+
+// These packages should NOT be built by `esinstall`
+// But might not be explicit dependencies of `astro`
+const denyList = [
+  'prismjs/components/index.js',
+  '@vue/server-renderer'
+];
+
+
+export default Object.keys(pkg.dependencies)
+  // Filter out packages that should be loaded threw Snowpack
+  .filter(name => {
+    // Explicitly allowed packages should NOT be external
+    if (allowList.has(name)) return false;
+    // Astro renderers should NOT be external
+    if (isAstroRenderer(name)) return false;
+    // Everything else SHOULD be external
+    return true;
+  })
+  // Add extras
+  .concat(denyList)
+  .sort();
