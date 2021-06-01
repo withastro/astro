8ae2fc690c72aaa8f170e12de4a2a38a73d91bbf
Nate Moore <nate@skypack.dev>
2021-06-01 09:07:49 -0500

    fix: reenable vue renderer

--- packages/astro/src/runtime.ts
+++ packages/astro/src/runtime.ts
@@ -270,7 +270,7 @@ interface CreateSnowpackOptions {
 
 const DEFAULT_HMR_PORT = 12321;
 // '@astrojs/renderer-vue' disabled due to a bug
-const DEFAULT_RENDERERS = ['@astrojs/renderer-svelte', '@astrojs/renderer-react', '@astrojs/renderer-preact'];
+const DEFAULT_RENDERERS = ['@astrojs/renderer-vue', '@astrojs/renderer-svelte', '@astrojs/renderer-react', '@astrojs/renderer-preact'];
 
 /** Create a new Snowpack instance to power Astro */
 async function createSnowpack(astroConfig: AstroConfig, options: CreateSnowpackOptions) {
