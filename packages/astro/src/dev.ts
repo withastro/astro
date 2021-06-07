f43888f385e4958d98b63caa113d7b9132d1c847
Nate Moore <nate@skypack.dev>
2021-06-07 18:33:37 -0500

    fix: missing `.js` extension

--- packages/astro/src/dev.ts
+++ packages/astro/src/dev.ts
@@ -7,7 +7,7 @@ import path from 'path';
 import { performance } from 'perf_hooks';
 import { defaultLogDestination, defaultLogLevel, debug, error, info, parseError } from './logger.js';
 import { createRuntime } from './runtime.js';
-import { stopTimer } from './build/util';
+import { stopTimer } from './build/util.js';
 
 const hostname = '127.0.0.1';
 
