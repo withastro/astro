392cf52116b4494fff272c679021aa9be485e6a2
Matthew Phillips <matthew@skypack.dev>
2021-06-10 10:03:27 -0400

    Add it only if there are client side components

--- packages/astro/src/compiler/transform/head.ts
+++ packages/astro/src/compiler/transform/head.ts
@@ -55,7 +55,7 @@ export default function (opts: TransformOptions): Transformer {
         });
       }
 
-      if (isHmrEnabled) {
+      if (isHmrEnabled && hasComponents) {
         const { hmrPort } = opts.compileOptions;
         children.push(
           {
