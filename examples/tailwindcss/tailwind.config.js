2462ece7a6889461aca04dd3bcbe48b3b671aec1
Jeffrey Jose <jeffjosejeff@gmail.com>
2021-06-15 09:51:42 -0700

    Add .svelte to the tailwind config (#444)
    Add .svelte files to tailwind config so that svelte components can use tailwind classes.

--- examples/tailwindcss/tailwind.config.js
+++ examples/tailwindcss/tailwind.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}'],
 };
