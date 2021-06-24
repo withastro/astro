0ab0e1f8aa7564481fcf6276cc146b3e265d518a
Agustin Mulet <agustin.mulet@gmail.com>
2021-06-24 17:52:01 -0300

    Adding astro-nanostores example

---
+++ examples/astro-nanostores/src/components/AdminsReact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useStore } from 'nanostores/react'
+
+import { admins } from '../store/admins.js'
+import { counter, increaseCounter, decreaseCounter } from '../store/counter.js';
+
+const AdminsReact = () => {
+  const list = useStore(admins);
+  const count = useStore(counter);
+  return (
+    <>
+      <h1>React</h1>
+      <ul>
+        {list.map(user => <li key={user.name}>{JSON.stringify(user, null, 2)}</li>)}
+      </ul>
+      <div>
+        <h3>Counter</h3>
+        <p>{count}</p>
+        <button onClick={decreaseCounter}>-1</button>
+        <button onClick={increaseCounter}>+1</button>
+      </div>
+      <br />
+    </>
+  );
+}
+
+export default AdminsReact;
---
+++ examples/astro-nanostores/src/store/counter.js
@@ -0,0 +1,19 @@
+import { createStore, getValue } from 'nanostores'
+
+const counter = createStore(() => {
+  counter.set(0)
+})
+
+function increaseCounter() {
+  counter.set(getValue(counter) + 1)
+}
+
+function decreaseCounter() {
+  counter.set(getValue(counter) - 1)
+}
+
+export {
+  counter,
+  increaseCounter,
+  decreaseCounter
+}
