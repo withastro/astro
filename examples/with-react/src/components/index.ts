89092c188cdfdeb4e8aed627568681e16c286f44
Austin Crim <crim.austin@principal.com>
2021-06-26 11:59:46 -0500

    split out the rest of the examples

--- examples/with-react/src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as A } from './A.astro'
---
+++ examples/with-svelte/src/components/Counter.svelte
@@ -0,0 +1,20 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+<div class="counter">
+  <button on:click={subtract}>-</button>
+  <pre>{ count }</pre>
+  <button on:click={add}>+</button>
+</div>
+<div class="children">
+  <slot />
+</div>
