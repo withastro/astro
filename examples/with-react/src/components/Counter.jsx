3c4e6c5913f5697a04dcc0a8d6c9b9166aee0be9
natemoo-re <natemoo-re@users.noreply.github.com>
2021-06-28 12:04:07 +0000

    [ci] yarn format

--- examples/with-react/src/components/Counter.jsx
+++ examples/with-react/src/components/Counter.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Counter({ children }) {
-  const [count, setCount] = useState(0)
-  const add = () => setCount((i) => i + 1)
-  const subtract = () => setCount((i) => i - 1)
+  const [count, setCount] = useState(0);
+  const add = () => setCount((i) => i + 1);
+  const subtract = () => setCount((i) => i - 1);
 
   return (
     <>
-      <div className='counter'>
+      <div className="counter">
         <button onClick={subtract}>-</button>
         <pre>{count}</pre>
         <button onClick={add}>+</button>
       </div>
-      <div className='children'>{children}</div>
+      <div className="children">{children}</div>
     </>
-  )
+  );
 }
