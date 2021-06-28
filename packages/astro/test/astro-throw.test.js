4f4f45d985e81fc13bc62baac5f784098245132c
Nate Moore <nate@skypack.dev>
2021-06-28 15:29:54 -0400

    chore: add test for throwing inside of `.astro`

---
+++ packages/astro/test/astro-throw.test.js
@@ -0,0 +1,22 @@
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+import { doc } from './test-utils.js';
+import { setup, setupBuild } from './helpers.js';
+
+const Throwable = suite('Throw test');
+
+setup(Throwable, './fixtures/astro-throw', {
+  runtimeOptions: {
+    mode: 'development',
+  },
+});
+setupBuild(Throwable, './fixtures/astro-throw');
+
+Throwable('Can throw an error from an `.astro` file', async ({ runtime }) => {
+  const result = await runtime.load('/');
+  assert.equal(result.statusCode, 500);
+  assert.equal(result.error.message, 'Oops!');
+});
+
+
+Throwable.run();
