de77f56fb093c0b6096f88e38f9dfce405d397de
Nate Moore <nate@skypack.dev>
2021-06-29 10:00:00 -0400

    test(#494): add failing test

--- packages/astro/src/compiler/codegen/index.ts
+++ packages/astro/src/compiler/codegen/index.ts
@@ -627,7 +627,8 @@ async function compileHtml(enterNode: TemplateNode, state: CodegenState, compile
               if (componentName === 'Markdown') {
                 const { $scope } = attributes ?? {};
                 state.markers.insideMarkdown = typeof state.markers.insideMarkdown === 'object' ? { $scope, count: state.markers.insideMarkdown.count + 1 } : { $scope, count: 1 };
-                if (attributes.content) {
+                const keys = Object.keys(attributes).filter(attr => attr !== '$scope');
+                if (keys.length > 0) {
                   if (curr === 'markdown') {
                     await pushMarkdownToBuffer();
                   }
@@ -717,7 +718,7 @@ async function compileHtml(enterNode: TemplateNode, state: CodegenState, compile
           case 'Body':
           case 'Title':
           case 'Element': {
-            if (state.markers.insideMarkdown) {
+            if (curr === 'markdown') {
               await pushMarkdownToBuffer();
             }
             if (paren !== -1) {
