5b8d2e1167e873920fbe1076a3724db219e6628f
Nate Moore <nate@skypack.dev>
2021-07-07 13:49:21 -0500

    feat(create-astro): add framework-specific prompt and setup

--- packages/create-astro/src/templates.ts
+++ packages/create-astro/src/templates.ts
@@ -2,17 +2,21 @@ export const TEMPLATES = [
   {
     title: 'Starter Kit (Generic)',
     value: 'starter',
+    renderers: true,
   },
   {
     title: 'Blog',
     value: 'blog',
+    renderers: ['@astrojs/renderer-preact']
   },
   {
     title: 'Documentation',
     value: 'docs',
+    renderers: ['@astrojs/renderer-preact']
   },
   {
     title: 'Portfolio',
     value: 'portfolio',
+    renderers: ['@astrojs/renderer-preact']
   },
 ];
