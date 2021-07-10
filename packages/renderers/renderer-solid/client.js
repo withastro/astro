672d62e8f8cd3308e4bee1532b7b90d21c328776
eyelidlessness <gnosis@gmail.com>
2021-07-10 15:57:33 -0700

    Solid renderer: fix SSR of children, hydration (top level)
    Caveat: cannot hydrate children/descendants of hydrated parents

--- packages/renderers/renderer-solid/client.js
+++ packages/renderers/renderer-solid/client.js
@@ -1,5 +1,13 @@
-import { render, createComponent } from 'solid-js/web';
+import { createComponent } from 'solid-js/web';
 
-export default (element) => (Component, props, children) => {
-  return render(() => createComponent(Component, props), element);
+export default (element) => (Component, props) => {
+  // Solid `createComponent` just returns a DOM node with all reactivity
+  // already attached. There's no VDOM, so there's no real need to "mount".
+  // Likewise, `children` can just reuse the nearest `astro-fragment` node.
+  const comp = createComponent(Component, {
+    ...props,
+    children: element.querySelector('astro-fragment'),
+  });
+
+  element.replaceChildren(comp);
 }
