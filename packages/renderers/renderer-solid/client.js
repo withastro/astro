d6ba3c3d7009f86cc5f5fcf855a0113454a958ac
eyelidlessness <gnosis@gmail.com>
2021-07-10 16:01:37 -0700

    Fix hydration of fragments

--- packages/renderers/renderer-solid/client.js
+++ packages/renderers/renderer-solid/client.js
@@ -4,10 +4,14 @@ export default (element) => (Component, props) => {
   // Solid `createComponent` just returns a DOM node with all reactivity
   // already attached. There's no VDOM, so there's no real need to "mount".
   // Likewise, `children` can just reuse the nearest `astro-fragment` node.
-  const comp = createComponent(Component, {
+  const component = createComponent(Component, {
     ...props,
     children: element.querySelector('astro-fragment'),
   });
 
-  element.replaceChildren(comp);
+  const children = Array.isArray(component)
+    ? component
+    : [ component ];
+
+  element.replaceChildren(...children);
 }
