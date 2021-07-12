009c59ee2f2d070ac2fc4054a382d720a676302d
eyelidlessness <eyelidlessness@users.noreply.github.com>
2021-07-12 15:17:42 -0700

    [Renderer] Solid (#656)
    * feat: add support for `jsxImportSource`, new JSX transform
    
    * WIP: solid renderer
    
    * Solid renderer: fix SSR of children, hydration (top level)
    
    Caveat: cannot hydrate children/descendants of hydrated parents
    
    * Fix hydration of fragments
    
    * fix: SyntaxError in React/Preact renderers
    
    * fix: errors in React/Preact renderers
    
    * feat: update react external
    
    * chore: update examples
    
    * chore: delete old changelog
    
    * chore: update astro config
    
    Co-authored-by: Nate Moore <nate@skypack.dev>

--- packages/renderers/renderer-react/index.js
+++ packages/renderers/renderer-react/index.js
@@ -3,13 +3,14 @@ export default {
   client: './client',
   server: './server',
   knownEntrypoints: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/server'],
+  external: ['react-dom/server'],
   jsxImportSource: 'react',
   jsxTransformOptions: async () => {
-    const { default: jsx } = await import('@babel/plugin-transform-react-jsx');
+    const { default: { default: jsx }} = await import('@babel/plugin-transform-react-jsx');
     return {
       plugins: [
-        jsx({}, { runtime: 'automatic', importSource: 'preact' })
+        jsx({}, { runtime: 'automatic', importSource: 'react' })
       ]
     }
-  })
+  }
 };
--- packages/renderers/renderer-solid/client.js
+++ packages/renderers/renderer-solid/client.js
@@ -1,5 +1,17 @@
-import { render, createComponent } from 'solid-js/web';
+import { createComponent } from 'solid-js/web';
 
-export default (element) => (Component, props, children) => {
-  return render(() => createComponent(Component, props), element);
+export default (element) => (Component, props) => {
+  // Solid `createComponent` just returns a DOM node with all reactivity
+  // already attached. There's no VDOM, so there's no real need to "mount".
+  // Likewise, `children` can just reuse the nearest `astro-fragment` node.
+  const component = createComponent(Component, {
+    ...props,
+    children: element.querySelector('astro-fragment'),
+  });
+
+  const children = Array.isArray(component)
+    ? component
+    : [ component ];
+
+  element.replaceChildren(...children);
 }
