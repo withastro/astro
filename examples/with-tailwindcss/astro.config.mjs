02c31b6a9ebc73fc897611b4084b44afff6cf10d
Nixinova <nixinova@gmail.com>
2021-07-15 10:33:38 +1200

    Clarify `src` and `pages` options
    `src` is not given by default and `pages` must begin with the value of `src`.

--- examples/with-tailwindcss/astro.config.mjs
+++ examples/with-tailwindcss/astro.config.mjs
@@ -1,7 +1,8 @@
 export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
+  // src: './src',         // Path to website source files.
+  // pages: './src/pages', // Path to Astro pages and data. Must start with the value of `src` above.
+  // dist: './dist',       // When running `astro build`, path to final static output.
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
     // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
