40f8107280c12942f62016a4ea1cd46abda1e7f8
Nate Moore <nate@skypack.dev>
2021-07-14 15:53:21 -0500

    fix: ensure `astro/components` are evaluated lazily

--- packages/astro/components/index.js
+++ packages/astro/components/index.js
@@ -1,3 +1,17 @@
-export { default as Markdown } from './Markdown.astro';
-export { default as Prism } from './Prism.astro';
-export { default as Debug } from './Debug.astro';
+// `lazy` is a wrapper that creates async Astro components
+const lazy = (_import) => {
+  return {
+    isAstroComponent: true,
+    __render: async (...args) =>
+      _import().then(({ default: Component }) => {
+        return Component.__render(...args);
+      }),
+  };
+};
+
+// We use `lazy` to avoid the Astro Snowpack plugin from eagerly evaluating these components
+// because `__renderPage` is stateful and will inject any linked styles to `state`
+// for any consumers using `import { SingleComponent } from 'astro/component'`
+export const Markdown = lazy(() => import('./Markdown.astro'));
+export const Prism = lazy(() => import('./Prism.astro'));
+export const Debug = lazy(() => import('./Debug.astro'));
