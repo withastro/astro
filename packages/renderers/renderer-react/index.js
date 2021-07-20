33c3ffb6fb4e37fdd0275a2f90cc8f02c303334c
Nate Moore <nate@skypack.dev>
2021-07-20 14:56:47 -0500

    fix: move react/server.js to external

--- packages/renderers/renderer-react/index.js
+++ packages/renderers/renderer-react/index.js
@@ -3,7 +3,7 @@ export default {
   client: './client',
   server: './server',
   knownEntrypoints: ['react', 'react/jsx-runtime', 'react-dom', 'react-dom/server'],
-  external: ['react-dom/server'],
+  external: ['react-dom/server.js'],
   jsxImportSource: 'react',
   jsxTransformOptions: async () => {
     const { default: { default: jsx }} = await import('@babel/plugin-transform-react-jsx');
