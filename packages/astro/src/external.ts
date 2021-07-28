5572470bf11caac8e872a38ccb287a57dea3819e
Bartek Igielski <bartek.igielski@snow.dog>
2021-07-28 21:17:13 +0200

    Allow @vue/server-renderer to be processed

--- packages/astro/src/external.ts
+++ packages/astro/src/external.ts
@@ -18,7 +18,7 @@ const isAstroRenderer = (name: string) => {
 
 // These packages should NOT be built by `esinstall`
 // But might not be explicit dependencies of `astro`
-const denyList = ['prismjs/components/index.js', '@vue/server-renderer', '@astrojs/markdown-support', 'node:fs/promises', ...nodeBuiltinsMap.values()];
+const denyList = ['prismjs/components/index.js', '@astrojs/markdown-support', 'node:fs/promises', ...nodeBuiltinsMap.values()];
 
 export default Object.keys(pkg.dependencies)
   // Filter out packages that should be loaded threw Snowpack
