b4884733fa6fb07dc16aa61bb4a48c4c04a9d5eb
MarcusOtter <github@otterstrom.dev>
2021-07-30 20:06:07 +0200

    Undo mjs comment change

--- examples/docs/astro.config.mjs
+++ examples/docs/astro.config.mjs
@@ -2,7 +2,7 @@ export default {
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
   // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don't need processing.
+  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
     // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     // sitemap: true,       // Generate sitemap (set to "false" to disable)
