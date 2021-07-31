2467b89ede2d6d143624009971851a8a774b8b52
Fred K. Schott <fkschott@gmail.com>
2021-07-31 08:13:46 -0700

    full translation

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -1,56 +1,50 @@
-export const SIDEBAR = [
-  {
-    text: 'Setup',
-    link: '',
-    children: [
-      { text: 'Getting Started', link: 'getting-started' },
-      { text: 'Quickstart', link: 'quick-start' },
-      { text: 'Installation', link: 'installation' },
-      { text: 'Examples', link: 'examples' },
-      { text: 'Astro vs. X', link: 'comparing-astro-vs-other-tools' },
-    ],
-  },
-  {
-    text: 'Basics',
-    link: 'core-concepts',
-    children: [
-      { text: 'Project Structure', link: 'core-concepts/project-structure' },
-      { text: 'Components', link: 'core-concepts/astro-components' },
-      { text: 'Pages', link: 'core-concepts/astro-pages' },
-      { text: 'Layouts', link: 'core-concepts/layouts' },
-      { text: 'Collections', link: 'core-concepts/collections' },
-      { text: 'Partial Hydration', link: 'core-concepts/component-hydration' },
-    ],
-  },
-  {
-    text: 'Guides',
-    link: 'guides',
-    children: [
-      { text: 'Styling & CSS', link: 'guides/styling' },
-      { text: 'Data Fetching', link: 'guides/data-fetching' },
-      { text: 'Markdown', link: 'guides/markdown-content' },
-      { text: 'Supported Imports', link: 'guides/imports' },
-      // To be written when https://github.com/snowpackjs/astro/issues/501 is completed
-      // { text: 'Pagination', link: 'guides/pagination' },
-      { text: 'Deploy a Website', link: 'guides/deploy' },
-      { text: 'Publish a Component', link: 'guides/publish-to-npm' },
-    ],
-  },
-  {
-    text: 'Reference',
-    link: 'reference',
-    children: [
-      { text: 'Built-In Components', link: 'reference/builtin-components' },
-      { text: 'API Reference', link: 'reference/api-reference' },
-      { text: 'CLI Reference', link: 'reference/cli-reference' },
-      {
-        text: 'Configuration Reference',
-        link: 'reference/configuration-reference',
-      },
-      { text: 'Renderer Reference', link: 'reference/renderer-reference' },
-    ],
-  },
-];
+export const SIDEBAR = {
+  'en': [
+    { text: 'Setup', header: true },
+    { text: 'Getting Started', link: 'getting-started' },
+    { text: 'Quickstart', link: 'quick-start' },
+    { text: 'Installation', link: 'installation' },
+    { text: 'Examples', link: 'examples' },
+    { text: 'Astro vs. X', link: 'comparing-astro-vs-other-tools' },
+    
+    { text: 'Basics', header: true },
+    { text: 'Project Structure', link: 'core-concepts/project-structure' },
+    { text: 'Components', link: 'core-concepts/astro-components' },
+    { text: 'Pages', link: 'core-concepts/astro-pages' },
+    { text: 'Layouts', link: 'core-concepts/layouts' },
+    { text: 'Collections', link: 'core-concepts/collections' },
+    { text: 'Partial Hydration', link: 'core-concepts/component-hydration' },
+    
+    { text: 'Guides', header: true },
+    { text: 'Styling & CSS', link: 'guides/styling' },
+    { text: 'Data Fetching', link: 'guides/data-fetching' },
+    { text: 'Markdown', link: 'guides/markdown-content' },
+    { text: 'Supported Imports', link: 'guides/imports' },
+    { text: 'Deploy a Website', link: 'guides/deploy' },
+    { text: 'Publish a Component', link: 'guides/publish-to-npm' },
+    
+    { text: 'Reference', header: true },
+    { text: 'Built-In Components', link: 'reference/builtin-components' },
+    { text: 'API Reference', link: 'reference/api-reference' },
+    { text: 'CLI Reference', link: 'reference/cli-reference' },
+    {
+      text: 'Configuration Reference',
+      link: 'reference/configuration-reference',
+    },
+    { text: 'Renderer Reference', link: 'reference/renderer-reference' },
+  ],
+  'nl': [
+    { text: 'Welkom', header: true },
+    { text: 'Beginnen', link: 'nl/getting-started' },
+  ],
+  'fi': [
+    { text: 'Tervetuloa', header: true },
+    { text: 'Aloittaminen', link: 'fi/getting-started' },
+    { text: 'Pika-aloitus', link: 'fi/quick-start' },
+    { text: 'Asennus', link: 'fi/installation' },
+  ],
+}
+
 
 export const SITE = {
   title: 'Astro Documentation',
