905c089a18ab6991b865dcce51eb5d1b4c2042a7
Caleb Jasik <calebjasik@jasik.xyz>
2021-08-02 16:04:55 -0500

    Setup section tracking for the docs
    Currently it's a little buggy, I'm going to see if I can setup https://github.com/jake-low/remark-sectionize as a plugin so I can IntersectionObserver over the sections!

--- docs/src/components/RightSidebar/TableOfContents.tsx
+++ docs/src/components/RightSidebar/TableOfContents.tsx
@@ -1,29 +1,57 @@
 import type { FunctionalComponent } from 'preact';
 import { h, Fragment } from 'preact';
-import { useState, useEffect, useRef } from 'preact/hooks';
+import { useState, useEffect, useRef, StateUpdater } from 'preact/hooks';
+
+// provided by https://www.emgoto.com/react-table-of-contents/
+const useIntersectionObserver = (setActiveId: StateUpdater<string>) => {
+  const headingElementsRef = useRef({});
+  useEffect(() => {
+    const callback: IntersectionObserverCallback = (
+      headings: IntersectionObserverEntry[]
+    ) => {
+      headingElementsRef.current = headings.reduce((map, headingElement) => {
+        map[headingElement.target.id] = headingElement;
+        return map;
+      }, headingElementsRef.current);
+
+      const visibleHeadings = [];
+      Object.keys(headingElementsRef.current).forEach((key) => {
+        const headingElement = headingElementsRef.current[key];
+        if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
+      });
+
+      const getIndexFromId = (id) =>
+        headingElements.findIndex((heading) => heading.id === id);
+      console.log(visibleHeadings);
+      if (visibleHeadings.length === 1) {
+        setActiveId(visibleHeadings[0].target.id);
+      } else if (visibleHeadings.length > 1) {
+        const sortedVisibleHeadings = visibleHeadings.sort(
+          (a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id)
+        );
+        setActiveId(sortedVisibleHeadings[0].target.id);
+      }
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      rootMargin: '0px 0px -40% 0px',
+    });
+
+    const headingElements = Array.from(
+      document.querySelectorAll('article :is(h1, h2, h3)')
+    );
+
+    headingElements.forEach((element) => observer.observe(element));
+
+    return () => observer.disconnect();
+  }, [setActiveId]);
+};
 
 const TableOfContents: FunctionalComponent<{ headers: any[] }> = ({
   headers = [],
 }) => {
-  const itemOffsets = useRef([]);
   const [activeId, setActiveId] = useState<string>(undefined);
-
-  useEffect(() => {
-    const getItemOffsets = () => {
-      const titles = document.querySelectorAll('article :is(h1, h2, h3, h4)');
-      itemOffsets.current = Array.from(titles).map((title) => ({
-        id: title.id,
-        topOffset: title.getBoundingClientRect().top + window.scrollY,
-      }));
-    };
-
-    getItemOffsets();
-    window.addEventListener('resize', getItemOffsets);
-
-    return () => {
-      window.removeEventListener('resize', getItemOffsets);
-    };
-  }, []);
+  useIntersectionObserver(setActiveId);
 
   return (
     <>
