1350d57997c3f50ca1f20247a734962f6825b591
Fred K. Schott <fkschott@gmail.com>
2021-08-10 09:51:19 -0700

    Do not observe visible hydrate components more than once (#1015)
    * add support for truely unique astro root uids
    
    * update test to test uniqueness

--- packages/astro/test/vue-component.test.js
+++ packages/astro/test/vue-component.test.js
@@ -13,9 +13,12 @@ Vue('Can load Vue', async ({ runtime }) => {
   assert.ok(!result.error, `build error: ${result.error}`);
 
   const $ = doc(result.contents);
-  assert.equal($('h1').text(), 'Hello world!', 'Can use slots');
-  assert.equal($('button').length, 2, 'Can render components');
-  assert.equal($('pre').text(), '5', 'Can render nested components');
+  const allPreValues = $('pre').toArray().map((el) => $(el).text());
+  assert.equal(allPreValues, ['0', '1', '10', '100', '1000'], 'renders all components correctly');
+  assert.equal($('astro-root').length, 4, 'renders 3 astro-roots');
+  assert.equal($('astro-root[uid]').length, 4, 'all astro-roots have uid attributes');
+  const uniqueRootUIDs = $('astro-root').map((i, el) => $(el).attr('uid'));
+  assert.equal(new Set(uniqueRootUIDs).size, 4, 'all astro-roots have unique uid attributes');
 });
 
 Vue.run();
