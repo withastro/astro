de3af1f4b7edd9e8a747a9fa9d21b6e3b94a30cc
FredKSchott <FredKSchott@users.noreply.github.com>
2021-08-10 16:52:31 +0000

    [ci] yarn format

--- packages/astro/test/vue-component.test.js
+++ packages/astro/test/vue-component.test.js
@@ -13,7 +13,9 @@ Vue('Can load Vue', async ({ runtime }) => {
   assert.ok(!result.error, `build error: ${result.error}`);
 
   const $ = doc(result.contents);
-  const allPreValues = $('pre').toArray().map((el) => $(el).text());
+  const allPreValues = $('pre')
+    .toArray()
+    .map((el) => $(el).text());
   assert.equal(allPreValues, ['0', '1', '10', '100', '1000'], 'renders all components correctly');
   assert.equal($('astro-root').length, 4, 'renders 3 astro-roots');
   assert.equal($('astro-root[uid]').length, 4, 'all astro-roots have uid attributes');
