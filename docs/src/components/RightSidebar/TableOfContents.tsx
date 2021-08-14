1652d804e629614b7ab88c971fa516402f96b0b3
Caleb Jasik <calebjasik@jasik.xyz>
2021-08-13 21:04:28 -0500

    Refactor to display all titles that are onscreen

--- docs/src/components/RightSidebar/TableOfContents.tsx
+++ docs/src/components/RightSidebar/TableOfContents.tsx
@@ -3,39 +3,39 @@ import { h, Fragment } from 'preact';
 import { useState, useEffect, useRef, StateUpdater } from 'preact/hooks';
 
 // provided by https://www.emgoto.com/react-table-of-contents/
-const useIntersectionObserver = (setActiveId: StateUpdater<string>) => {
-  const headingElementsRef = useRef({});
+const useIntersectionObserver = () => {
+  const [activeIds, setActiveIds] = useState([]);
+  const headingElementsRef = useRef(
+    new Map<string, IntersectionObserverEntry>()
+  );
   useEffect(() => {
     const callback: IntersectionObserverCallback = (
       headings: IntersectionObserverEntry[]
     ) => {
       headingElementsRef.current = headings.reduce((map, headingElement) => {
-        map[headingElement.target.id] = headingElement;
+        map.set(headingElement.target.id, headingElement);
         return map;
       }, headingElementsRef.current);
 
-      const visibleHeadings = [];
-      Object.keys(headingElementsRef.current).forEach((key) => {
-        const headingElement = headingElementsRef.current[key];
-        if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
-      });
+      const visibleHeadings = Array.from(
+        headingElementsRef.current.values(),
+        (element) => {
+          if (element.isIntersecting) return element;
+          return undefined;
+        }
+      ).filter((element) => element != undefined);
 
       const getIndexFromId = (id) =>
         headingElements.findIndex((heading) => heading.id === id);
+      const getId = (element) => element.target.id;
 
-      if (visibleHeadings.length === 1) {
-        setActiveId(visibleHeadings[0].target.id);
-      } else if (visibleHeadings.length > 1) {
-        const sortedVisibleHeadings = visibleHeadings.sort(
-          (a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id)
-        );
-        setActiveId(sortedVisibleHeadings[0].target.id);
-      }
+      const sortedVisibleHeadings = visibleHeadings.sort(
+        (a, b) => getIndexFromId(a.target.id) - getIndexFromId(b.target.id)
+      );
+      setActiveIds(sortedVisibleHeadings.map(getId));
     };
 
-    const observer = new IntersectionObserver(callback, {
-      rootMargin: '0px 0px -40% 0px',
-    });
+    const observer = new IntersectionObserver(callback);
 
     const headingElements = Array.from(
       document.querySelectorAll('article :is(h1, h2, h3)')
@@ -44,22 +44,24 @@ const useIntersectionObserver = (setActiveId: StateUpdater<string>) => {
     headingElements.forEach((element) => observer.observe(element));
 
     return () => observer.disconnect();
-  }, [setActiveId]);
+  }, [setActiveIds]);
+  return activeIds;
 };
 
 const TableOfContents: FunctionalComponent<{ headers: any[] }> = ({
   headers = [],
 }) => {
-  const [activeId, setActiveId] = useState<string>(undefined);
-  useIntersectionObserver(setActiveId);
+  const activeIds = useIntersectionObserver();
 
   return (
     <>
       <h2 class="heading">On this page</h2>
+      <div>{JSON.stringify(activeIds)}</div>
       <ul>
         <li
+          key="overview"
           class={`header-link depth-2 ${
-            activeId === 'overview' ? 'active' : ''
+            activeIds.includes('overview') ? 'active' : ''
           }`.trim()}
         >
           <a href="#overview">Overview</a>
@@ -68,8 +70,9 @@ const TableOfContents: FunctionalComponent<{ headers: any[] }> = ({
           .filter(({ depth }) => depth > 1 && depth < 4)
           .map((header) => (
             <li
+              key={header.slug}
               class={`header-link depth-${header.depth} ${
-                activeId === header.slug ? 'active' : ''
+                activeIds.includes(header.slug) ? 'active' : ''
               }`.trim()}
             >
               <a href={`#${header.slug}`}>{header.text}</a>
