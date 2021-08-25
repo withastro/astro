affcd04f0f89de6268e7e359b2ddb8e07ef760d9
Fred K. Schott <fkschott@gmail.com>
2021-08-25 11:51:04 -0700

    stop building, bundling, and transforming public/ files (#1210)
    * stop bundling public/ files
    
    * update www and examples

--- packages/astro/src/build.ts
+++ packages/astro/src/build.ts
@@ -149,7 +149,7 @@ ${stack}
             astroRuntime.load(url).then((result) => {
               if (result.statusCode !== 200) {
                 if (result.statusCode === 404) {
-                  throw new Error(`${buildState[id].srcPath.href}: could not find "${path.basename(url)}"`);
+                  throw new Error(`${buildState[id].srcPath.href}: could not find "${url}"`);
                 }
                 // there shouldn’t be a build error here
                 throw (result as any).error || new Error(`unexpected status ${result.statusCode} when loading ${url}`);
