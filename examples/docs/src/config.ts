e0c39fb217537f89818006ca88731cd765df4fb5
Fred K. Schott <fkschott@gmail.com>
2021-08-26 15:18:45 -0700

    improve introduction page of example/docs

--- examples/docs/src/config.ts
+++ examples/docs/src/config.ts
@@ -16,6 +16,12 @@ export const KNOWN_LANGUAGES = {
   English: 'en',
 };
 
+// Uncomment this to add an "Edit this page" button to every page of documentation.
+// export const GITHUB_EDIT_URL = `https://github.com/snowpackjs/astro/blob/main/docs/`;
+
+// Uncomment this to add an "Join our Community" button to every page of documentation.
+// export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+
 // Uncomment this to enable site search.
 // See "Algolia" section of the README for more information.
 // export const ALGOLIA = {
