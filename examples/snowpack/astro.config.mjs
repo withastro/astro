b22755d59e1e1530dc6da0daca976b82fa381a4a
Peter Singh <drgaud@hotmail.com>
2021-09-01 02:09:16 +0100

    Checked the astro configs for the templates, (#1282)
    * Checked the astro configs for the templates,
    
    * Update astro.config.mjs
    
    * Update package.json
    
    Co-authored-by: Fred K. Schott <fkschott@gmail.com>

--- examples/snowpack/astro.config.mjs
+++ examples/snowpack/astro.config.mjs
@@ -1,11 +1,22 @@
-export default {
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
   projectRoot: '.',
   pages: './src/pages',
   dist: './dist',
   public: './public',
+  // Set "renderers" to "[]" to disable all default, builtin component support.
   renderers: [
     '@astrojs/renderer-vue',
     '@astrojs/renderer-svelte',
     '@astrojs/renderer-preact',
   ],
-};
+});
+
