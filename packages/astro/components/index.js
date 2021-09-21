353c7549754570145e8028bfffa3c816cb2a27f7
Nate Moore <nate@skypack.dev>
2021-09-21 17:04:25 +0000

    Throw Error for WIP Features

--- packages/astro/components/index.js
+++ packages/astro/components/index.js
@@ -1,4 +1,17 @@
-export { default as Code } from './Code.astro';
-export { default as Debug } from './Debug.astro';
-export { default as Markdown } from './Markdown.astro';
-export { default as Prism } from './Prism.astro';
+// export { default as Code } from './Code.astro';
+// export { default as Debug } from './Debug.astro';
+// export { default as Markdown } from './Markdown.astro';
+// export { default as Prism } from './Prism.astro';
+
+export const Code = () => {
+  throw new Error(`Cannot render <Code />. "astro/components" are still WIP!`)
+}
+export const Debug = () => {
+  throw new Error(`Cannot render <Debug />. "astro/components" are still WIP!`)
+}
+export const Markdown = () => {
+  throw new Error(`Cannot render <Markdown />. "astro/components" are still WIP!`)
+}
+export const Prism = () => {
+  throw new Error(`Cannot render <Prism />. "astro/components" are still WIP!`)
+}
