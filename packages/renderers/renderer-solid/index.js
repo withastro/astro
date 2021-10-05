f8d1a35ace6e1f0c0349ab26c7b9b97b2df7a2a8
Ryan Carniato <ryansolid@gmail.com>
2021-10-05 09:20:51 -0700

    Add hydration to Solid renderer (#1479)
    * feat: add hydration to Solid renderer
    
    * fix: intersection observer, move script to the end

--- packages/renderers/renderer-solid/index.js
+++ packages/renderers/renderer-solid/index.js
@@ -2,17 +2,18 @@ export default {
   name: '@astrojs/renderer-solid',
   client: './client',
   server: './server',
-  knownEntrypoints: ['solid-js', 'solid-js/web'],
-  external: ['solid-js/web/dist/server.js', 'solid-js/dist/server.js', 'babel-preset-solid'],
+  knownEntrypoints: ['solid-js', 'solid-js/web', 'solid-js/store'],
+  external: ['solid-js/web/dist/server.js', 'solid-js/store/dist/server.js', 'solid-js/dist/server.js', 'babel-preset-solid'],
   jsxImportSource: 'solid-js',
   jsxTransformOptions: async ({ isSSR }) => {
     const [{ default: solid }] = await Promise.all([import('babel-preset-solid')]);
     const options = {
-      presets: [solid({}, { generate: isSSR ? 'ssr' : 'dom' })],
+      presets: [solid({}, { generate: isSSR ? 'ssr' : 'dom', hydratable: true })],
     };
 
     if (isSSR) {
       options.alias = {
+        'solid-js/store': 'solid-js/store/dist/server.js',
         'solid-js/web': 'solid-js/web/dist/server.js',
         'solid-js': 'solid-js/dist/server.js',
       };
--- packages/renderers/renderer-solid/server.js
+++ packages/renderers/renderer-solid/server.js
@@ -1,24 +1,23 @@
-import { createComponent } from 'solid-js';
-import { renderToStringAsync, ssr } from 'solid-js/web/dist/server.js';
+import { renderToString, ssr, createComponent } from 'solid-js/web/dist/server.js';
 
-async function check(Component, props, children) {
+function check(Component, props, children) {
   if (typeof Component !== 'function') return false;
 
-  const { html } = await renderToStaticMarkup(Component, props, children);
+  const { html } = renderToStaticMarkup(Component, props, children);
   return typeof html === 'string';
 }
 
-async function renderToStaticMarkup(Component, props, children) {
-  const html = await renderToStringAsync(
-    () => () =>
+function renderToStaticMarkup(Component, props, children) {
+  const html = renderToString(
+    () =>
       createComponent(Component, {
         ...props,
         // In Solid SSR mode, `ssr` creates the expected structure for `children`.
         // In Solid client mode, `ssr` is just a stub.
-        children: ssr([`<astro-fragment>${children}</astro-fragment>`]),
+        children: ssr(`<astro-fragment>${children}</astro-fragment>`),
       })
   );
-  return { html };
+  return { html: html + `<script>window._$HYDRATION||(window._$HYDRATION={events:[],completed:new WeakSet})</script>` };
 }
 
 export default {
