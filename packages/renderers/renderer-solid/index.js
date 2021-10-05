1bd3cf8f4dd15fcd63ab650031b67f896ee9e0ee
Matthew Phillips <matthew@matthewphillips.info>
2021-10-05 16:57:51 -0400

    Add hydration to Solid renderer (#1479) (#1495)
    * feat: add hydration to Solid renderer
    
    * fix: intersection observer, move script to the end
    
    Co-authored-by: Ryan Carniato <ryansolid@gmail.com>

--- packages/renderers/renderer-solid/index.js
+++ packages/renderers/renderer-solid/index.js
@@ -4,6 +4,7 @@ export default {
   server: './server',
   external: ['solid-js/web/dist/server.js', 'solid-js/store/dist/server.js', 'solid-js/dist/server.js', 'babel-preset-solid'],
   knownEntrypoints: ['solid-js', 'solid-js/web', 'solid-js/store', 'solid-js/html', 'solid-js/h'],
+  external: ['solid-js/web/dist/server.cjs', 'solid-js/store/dist/server.cjs', 'solid-js/dist/server.cjs', 'babel-preset-solid'],
   jsxImportSource: 'solid-js',
   jsxTransformOptions: async ({ isSSR }) => {
     const [{ default: solid }] = await Promise.all([import('babel-preset-solid')]);
