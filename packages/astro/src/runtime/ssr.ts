22adb59e09275f5bc0a8813551ae22410b511dcd
Matthew Phillips <matthew@skypack.dev>
2021-10-11 16:19:51 -0400

    Fix eslint errors

--- packages/astro/src/runtime/ssr.ts
+++ packages/astro/src/runtime/ssr.ts
@@ -69,6 +69,7 @@ async function resolveRenderer(viteServer: ViteDevServer, renderer: string) {
 async function resolveRenderers(viteServer: ViteDevServer, ids: string[]): Promise<Renderer[]> {
   const renderers = await Promise.all(
     ids.map(renderer => {
+      // eslint-disable-next-line no-non-null-assertion
       if (cache.has(renderer)) return cache.get(renderer)!;
       let promise = resolveRenderer(viteServer, renderer);
       cache.set(renderer, promise);
@@ -141,8 +142,10 @@ export async function ssr({ astroConfig, filePath, logging, mode, origin, pathna
     let pageProps: Props = {};
     if (route && !route.pathname) {
       if (route.params.length) {
-        const paramsMatch = route.pattern.exec(pathname)!;
-        params = getParams(route.params)(paramsMatch);
+        const paramsMatch = route.pattern.exec(pathname);
+        if(paramsMatch) {
+          params = getParams(route.params)(paramsMatch);
+        }
       }
       validateGetStaticPathsModule(mod);
       routeCache[route.component] =
@@ -219,6 +222,7 @@ export async function ssr({ astroConfig, filePath, logging, mode, origin, pathna
             const loadedModule = await viteServer.ssrLoadModule(file);
             const astro = (loadedModule.metadata || {}) as FetchContentResultBase['astro'];
             const frontmatter = loadedModule.frontmatter || {};
+            //// eslint-disable-next-line no-shadow
             const result: FetchContentResult<T> = {
               ...frontmatter,
               astro,
