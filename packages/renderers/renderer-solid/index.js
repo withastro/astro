a38460ca62b27a1746180784fa26eeb469165a3f
Nate Moore <natemoo-re@users.noreply.github.com>
2021-10-14 14:07:54 -0500

    [next] Update renderers (#1509)
    * chore: update vite
    
    * fix(renderers): point renderers to resolved server/client entrypoints

--- packages/renderers/renderer-solid/index.js
+++ packages/renderers/renderer-solid/index.js
@@ -1,10 +1,9 @@
 export default {
   name: '@astrojs/renderer-solid',
-  client: './client',
-  server: './server',
+  client: './client.js',
+  server: './server.js',
   external: ['solid-js/web/dist/server.js', 'solid-js/store/dist/server.js', 'solid-js/dist/server.js', 'babel-preset-solid'],
   knownEntrypoints: ['solid-js', 'solid-js/web', 'solid-js/store', 'solid-js/html', 'solid-js/h'],
-  external: ['solid-js/web/dist/server.cjs', 'solid-js/store/dist/server.cjs', 'solid-js/dist/server.cjs', 'babel-preset-solid'],
   jsxImportSource: 'solid-js',
   jsxTransformOptions: async ({ isSSR }) => {
     const [{ default: solid }] = await Promise.all([import('babel-preset-solid')]);
