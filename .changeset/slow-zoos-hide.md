31d068801622e8a01fbedf7a59b0be8efe47bc3a
Levi <levi.ceccato@gmail.com>
2021-10-15 06:08:07 +1100

    Expose Vue component names for devtools (#1512)
    * Expose name on host component for Vue devtools
    
    * Add changeset
    
    * Update changeset to patch from minor
    
    * [ci] collect stats
    
    * [ci] collect stats
    
    Co-authored-by: leviceccato <leviceccato@users.noreply.github.com>
    Co-authored-by: Matthew Phillips <matthew@matthewphillips.info>

---
+++ .changeset/slow-zoos-hide.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/renderer-vue': patch
+---
+
+Expose name on host component for Vue devtools
--- packages/renderers/renderer-vue/client.js
+++ packages/renderers/renderer-vue/client.js
@@ -2,6 +2,8 @@ import { h, createSSRApp } from 'vue';
 import StaticHtml from './static-html.js';
 
 export default (element) => (Component, props, children) => {
-  const app = createSSRApp({ render: () => h(Component, props, { default: () => h(StaticHtml, { value: children }) }) });
+  // Expose name on host component for Vue devtools
+  const name = Component.name ? `${Component.name} Host` : undefined;
+  const app = createSSRApp({ name, render: () => h(Component, props, { default: () => h(StaticHtml, { value: children }) }) });
   app.mount(element, true);
 };
