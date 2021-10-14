83528966321116ee00d376852c62471bdff7d87c
Nate Moore <natemoo-re@users.noreply.github.com>
2021-10-14 14:07:54 -0500

    [next] Update renderers (#1509)
    * chore: update vite
    
    * fix(renderers): point renderers to resolved server/client entrypoints

--- packages/renderers/renderer-vue/index.js
+++ packages/renderers/renderer-vue/index.js
@@ -2,8 +2,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default {
   name: '@astrojs/renderer-vue',
-  client: './client',
-  server: './server',
+  client: './client.js',
+  server: './server.js',
   knownEntrypoints: ['vue', '@vue/server-renderer'],
   vitePlugins: [vue()],
 };
