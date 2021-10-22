400c3f13ce159dc9379d4573fe9520d56b8f1e31
Jonathan Neal <jonathantneal@hotmail.com>
2021-10-22 13:44:24 -0400

    Add tests for the absence of omitted classes

--- packages/astro/test/astro-class-list.test.js
+++ packages/astro/test/astro-class-list.test.js
@@ -20,6 +20,8 @@ describe('Class List', async () => {
     expect($('[class="test truthy"]')).to.have.lengthOf(1);
     expect($('[class="test set"]')).to.have.lengthOf(1);
     expect($('[class="hello goodbye world friend"]')).to.have.lengthOf(1);
+
+    expect($('.false, .noshow1, .noshow2, .noshow3, .noshow4')).to.have.lengthOf(0);
   });
 
   it('Passes class:list attributes as expected to components', async () => {
