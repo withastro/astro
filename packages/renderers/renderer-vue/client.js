1103cb1f9d2a2077f65a0d08aee67ee9bf0e9540
Matthew Phillips <matthew@skypack.dev>
2021-10-25 14:07:00 -0400

    Prevent passing class to Vue components

--- packages/renderers/renderer-vue/client.js
+++ packages/renderers/renderer-vue/client.js
@@ -2,6 +2,7 @@ import { h, createSSRApp } from 'vue';
 import StaticHtml from './static-html.js';
 
 export default (element) => (Component, props, children) => {
+  delete props['class'];
   // Expose name on host component for Vue devtools
   const name = Component.name ? `${Component.name} Host` : undefined;
   const app = createSSRApp({ name, render: () => h(Component, props, { default: () => h(StaticHtml, { value: children }) }) });
