c150576dfb11a2d12184292f6736c6cbf0550c4f
Nate Moore <nate@skypack.dev>
2021-10-26 22:32:05 -0500

    feat: add preact/compat entry for `@astrojs/renderer-preact`

---
+++ packages/renderers/renderer-preact/compat/index.js
@@ -0,0 +1,28 @@
+export default {
+  name: '@astrojs/renderer-preact/compat',
+  client: '../client.js',
+  server: '../server.js',
+  jsxImportSource: 'react',
+  jsxTransformOptions: async () => {
+    const {
+      default: { default: jsx },
+    } = await import('@babel/plugin-transform-react-jsx');
+    return {
+      plugins: [jsx({}, { runtime: 'automatic', importSource: 'preact/compat' })],
+    };
+  },
+  viteConfig() {
+    return {
+      alias: {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat'
+      },
+      optimizeDeps: {
+        include: ['@astrojs/renderer-preact/client.js', 'preact/compat', 'preact/compat/jsx-runtime', 'preact-render-to-string'],
+      },
+      ssr: {
+        external: ['preact-render-to-string'],
+      },
+    };
+  },
+};
