5ecc6033d631e4c1cd83cacb40938f5f8126b48b
Matthew Phillips <matthew@matthewphillips.info>
2021-11-02 09:52:40 -0400

    Small refactor to the server runtime (#1699)
    * Small refactor to the runtime
    
    * Update packages/astro/src/runtime/server/index.ts
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * Update packages/astro/src/runtime/server/index.ts
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * Update packages/astro/src/runtime/server/index.ts
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * Update packages/astro/src/runtime/server/index.ts
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * Update packages/astro/src/runtime/server/hydration.ts
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * Improve based on review comments
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

---
+++ packages/astro/src/runtime/server/util.ts
@@ -0,0 +1,30 @@
+
+export function serializeListValue(value: any) {
+  const hash: Record<string, any> = {};
+
+  push(value);
+
+  return Object.keys(hash).join(' ');
+
+  function push(item: any) {
+    // push individual iteratables
+    if (item && typeof item.forEach === 'function') item.forEach(push);
+    // otherwise, push object value keys by truthiness
+    else if (item === Object(item))
+      Object.keys(item).forEach((name) => {
+        if (item[name]) push(name);
+      });
+    // otherwise, push any other values as a string
+    else {
+      // get the item as a string
+      item = item == null ? '' : String(item).trim();
+
+      // add the item if it is filled
+      if (item) {
+        item.split(/\s+/).forEach((name: string) => {
+          hash[name] = true;
+        });
+      }
+    }
+  }
+}
