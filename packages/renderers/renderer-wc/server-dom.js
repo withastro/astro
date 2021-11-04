ecc5f428e9b82bed9096e4eea2ae9d94b70b5c28
Jonathan Neal <jonathantneal@hotmail.com>
2021-11-03 22:35:31 -0400

    add more code documentation

--- packages/renderers/renderer-wc/server-dom.js
+++ packages/renderers/renderer-wc/server-dom.js
@@ -15,25 +15,31 @@ export const getCustomElementByName = internal.CTR_BY_TAG.get.bind(internal.CTR_
 /** Returns a tag names by a given Custom Element. */
 export const getNameByCustomElement = internal.TAG_BY_CTR.get.bind(internal.TAG_BY_CTR);
 
+/** CustomElementRegistry used to register new custom elements and get information about previously registered custom elements. */
 export const customElements = {
+	/** Defines a new custom element using the given tag name and HTMLElement constructor. */
 	define(tag, ctr) {
 		internal.CTR_BY_TAG.set(tag, ctr)
 		internal.TAG_BY_CTR.set(ctr, tag)
 	},
+	/** Returns the constructor associated with the given tag name. */
 	get(tag) {
 		return internal.CTR_BY_TAG.get(tag)
 	},
 };
 
+/** HTMLElement interface representing any HTML element. */
 export const HTMLElement = class HTMLElement {
 	constructor() {
 		internal.INNER_HTML_BY_ELEMENT.set(this, '')
 	}
 
+	/** Attaches a Shadow DOM tree to the current element. */
 	attachShadow() {
-		this.shadowRoot = new HTMLElement()
+		return this.shadowRoot = new HTMLElement()
 	}
 
+	/** HTML markup contained within the current element. */
 	get innerHTML() {
 		return internal.INNER_HTML_BY_ELEMENT.get(this) || ''
 	}
@@ -42,6 +48,8 @@ export const HTMLElement = class HTMLElement {
 		internal.INNER_HTML_BY_ELEMENT.set(this, String(value))
 	}
 };
+
+// shim the global object
 
 globalThis.HTMLElement = HTMLElement;
 globalThis.customElements = customElements;
