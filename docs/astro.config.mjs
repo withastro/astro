cc61773fe5ba28d5f19ad14c40d58615feee55f5
Jonathan Neal <jonathantneal@hotmail.com>
2021-11-04 20:05:10 -0400

    fix resolutions

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,8 +12,8 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   vite: {
     resolve: {
       alias: {
-        '~': './src',
-        'components': './src/components',
+        '~': '/src',
+        'components': '/src/components',
       }
     }
   }
