31fbbd7eb05dff465805ae512669f98b78f069e0
Matthew Phillips <matthew@skypack.dev>
2021-11-11 14:13:02 -0500

    Get hoisted scripts to pass

--- packages/astro/test/astro-scripts.test.js
+++ packages/astro/test/astro-scripts.test.js
@@ -15,7 +15,7 @@ describe('Hoisted scripts', () => {
     const html = await fixture.readFile('/external/index.html');
     const $ = cheerio.load(html);
 
-    expect($('head script[type="module"][data-astro="hoist"]')).to.have.lengthOf(2);
+    expect($('head script[type="module"]:not([src="/regular_script.js"])')).to.have.lengthOf(1);
     expect($('body script')).to.have.lengthOf(0);
   });
 
@@ -23,7 +23,7 @@ describe('Hoisted scripts', () => {
     const html = await fixture.readFile('/inline/index.html');
     const $ = cheerio.load(html);
 
-    expect($('head script[type="module"][data-astro="hoist"]')).to.have.lengthOf(1);
+    expect($('head script[type="module"]')).to.have.lengthOf(1);
     expect($('body script')).to.have.lengthOf(0);
   });
 
@@ -33,7 +33,7 @@ describe('Hoisted scripts', () => {
     let $ = cheerio.load(inline);
 
     // test 1: Just one entry module
-    assert.equal($('script')).to.have.lengthOf(1);
+    expect($('script')).to.have.lengthOf(1);
 
     // test 2: attr removed
     expect($('script').attr('data-astro')).to.equal(undefined);
@@ -47,14 +47,14 @@ describe('Hoisted scripts', () => {
 
   it('External page builds the scripts to a single bundle', async () => {
     let external = await fixture.readFile('/external/index.html');
-    $ = cheerio.load(external);
+    let $ = cheerio.load(external);
 
     // test 1: there are two scripts
-    assert.equal($('script')).to.have.lengthOf(2);
+    expect($('script')).to.have.lengthOf(2);
 
     let el = $('script').get(1);
-    entryURL = path.join('external', $(el).attr('src'));
-    let externalEntryJS = await readFile(entryURL);
+    let entryURL = path.join('external', $(el).attr('src'));
+    let externalEntryJS = await fixture.readFile(entryURL);
 
     // test 2: the JS exists
     expect(externalEntryJS).to.be.ok;
