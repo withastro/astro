fad6bd09368d23d9dd2c80f71eea3814238f8c89
Matthew Phillips <matthew@matthewphillips.info>
2021-11-29 14:22:46 -0500

    Use of PUBLIC_ environment variables (#2044)
    * Enable client-side JS to read env variables via PUBLIC_
    
    * Fixes use of PUBLIC_ in client-JS

---
+++ packages/astro/test/fixtures/astro-envs/src/components/Client.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="client-component">
+    {{ PUBLIC_PLACE }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      PUBLIC_PLACE: import.meta.env.PUBLIC_PLACE,
+    };
+  },
+};
+</script>
