689d371903fcaad34245f43be9e534f1571f9d37
Matthew Phillips <matthew@skypack.dev>
2021-12-02 15:33:55 -0500

    Some more progress on the new build

--- packages/astro/src/core/build/index.ts
+++ packages/astro/src/core/build/index.ts
@@ -5,6 +5,7 @@ import type { RenderedChunk } from 'rollup';
 
 import { rollupPluginAstroBuildHTML } from '../../vite-plugin-build-html/index.js';
 import { rollupPluginAstroBuildCSS } from '../../vite-plugin-build-css/index.js';
+import { vitePluginNewBuild } from '../../vite-plugin-new-build/index.js';
 import fs from 'fs';
 import * as colors from 'kleur/colors';
 import { polyfill } from '@astropub/webapi';
@@ -192,6 +193,7 @@ class AstroBuilder {
         target: 'es2020', // must match an esbuild target
       },
       plugins: [
+        vitePluginNewBuild(),
         rollupPluginAstroBuildHTML({
           astroConfig: this.config,
           astroPageStyleMap,
