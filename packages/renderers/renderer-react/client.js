f2a249eaa397e6b5fe24c21ba1c5cb5b448da24a
GitHub Action <github-action@users.noreply.github.com>
2021-12-02 04:23:17 +0000

    [ci] Prettier fix

--- packages/renderers/renderer-react/client.js
+++ packages/renderers/renderer-react/client.js
@@ -4,6 +4,10 @@ import StaticHtml from './static-html.js';
 
 export default (element) => (Component, props, children) =>
   hydrate(
-    createElement(Component, { ...props, suppressHydrationWarning: true }, children != null ? createElement(StaticHtml, { value: children, suppressHydrationWarning: true }) : children),
+    createElement(
+      Component,
+      { ...props, suppressHydrationWarning: true },
+      children != null ? createElement(StaticHtml, { value: children, suppressHydrationWarning: true }) : children
+    ),
     element
   );
