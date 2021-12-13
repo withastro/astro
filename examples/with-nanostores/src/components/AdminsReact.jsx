23b7756d6b7b9cc094b97442123fffa08c6420e8
Agustin Mulet <31162600+agustinmulet@users.noreply.github.com>
2021-12-13 15:37:06 -0300

    Adding Solid to with-nanostores example (#2079)

--- examples/with-nanostores/src/components/AdminsReact.jsx
+++ examples/with-nanostores/src/components/AdminsReact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { useStore } from '@nanostores/react';
 
 import { admins } from '../store/admins.js';
@@ -7,17 +7,18 @@ import { counter, increaseCounter, decreaseCounter } from '../store/counter.js';
 const AdminsReact = () => {
   const list = useStore(admins);
   const count = useStore(counter);
+  
   return (
     <>
       <h1>React</h1>
       <ul>
-        {list.map((user) => (
-          <li key={user.name}>{JSON.stringify(user, null, 2)}</li>
+        {list.map((admin) => (
+          <li key={admin.id}>{JSON.stringify(admin, null, 2)}</li>
         ))}
       </ul>
       <div>
         <h3>Counter</h3>
-        <p>{count}</p>
+        <p>{count.value}</p>
         <button onClick={decreaseCounter}>-1</button>
         <button onClick={increaseCounter}>+1</button>
       </div>
