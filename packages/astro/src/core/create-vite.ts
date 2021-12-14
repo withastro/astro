50217148b2eb6e5094a995d7e5d98634edc360e5
Nate Moore <nate@skypack.dev>
2021-12-14 13:31:20 -0600

    fix: exclude babel/core

--- packages/astro/src/core/create-vite.ts
+++ packages/astro/src/core/create-vite.ts
@@ -18,6 +18,7 @@ const ALWAYS_EXTERNAL = new Set([
   '@sveltejs/vite-plugin-svelte',
   '@proload/core',
   '@proload/core/lib/esm/requireOrImport.mjs',
+  '@babel/core',
   'serialize-javascript',
   'micromark-util-events-to-acorn',
   'node-fetch',
