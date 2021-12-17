981c3e9e31b4c16f26e7af04de3bbfd31e792cc0
Nate Moore <nate@skypack.dev>
2021-12-17 10:13:18 -0600

    chore(examples): add AlpineJS example

---
+++ examples/framework-alpine/astro.config.mjs
@@ -0,0 +1,13 @@
+// Full Astro Configuration API Documentation:
+// https://docs.astro.build/reference/configuration-reference
+
+// @type-check enabled!
+// VSCode and other TypeScript-enabled text editors will provide auto-completion,
+// helpful tooltips, and warnings if your exported object is invalid.
+// You can disable this by removing "@ts-check" and `@type` comments below.
+
+// @ts-check
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  // No renderers are needed for AlpineJS support, just use Astro components!
+  renderers: [],
+});
