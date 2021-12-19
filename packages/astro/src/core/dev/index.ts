ac7f944d6a96bd95fcd190e9c526fa4a9d7ce4bf
Jonathan Neal <jonathantneal@hotmail.com>
2021-12-19 14:44:30 -0500

    Bump `@astropub/webapi` (#2218)
    * bump polyfill
    
    * chore(lint): Prettier fix
    
    * chore: force ci
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/astro/src/core/dev/index.ts
+++ packages/astro/src/core/dev/index.ts
@@ -37,7 +37,9 @@ export interface DevServer {
 /** `astro dev` */
 export default async function dev(config: AstroConfig, options: DevOptions = { logging: defaultLogOptions }): Promise<DevServer> {
   // polyfill WebAPIs to globalThis for Node v12, Node v14, and Node v16
-  polyfill(globalThis);
+  polyfill(globalThis, {
+    exclude: 'window document',
+  });
 
   // start dev server
   const server = new AstroDevServer(config, options);
