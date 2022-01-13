20eaddb2a723253c7fbde3e56955a549bdf3f342
Matthew Phillips <matthew@matthewphillips.info>
2022-01-13 13:28:29 -0500

    Fix for using React in the static build (#2369)
    * Fix react in the static build
    
    * Adds a changeset

--- packages/astro/test/static-build-frameworks.test.js
+++ packages/astro/test/static-build-frameworks.test.js
@@ -2,17 +2,16 @@ import { expect } from 'chai';
 import cheerio from 'cheerio';
 import { loadFixture } from './test-utils.js';
 
-function addLeadingSlash(path) {
-	return path.startsWith('/') ? path : '/' + path;
-}
-
 describe('Static build - frameworks', () => {
 	let fixture;
 
 	before(async () => {
 		fixture = await loadFixture({
 			projectRoot: './fixtures/static-build-frameworks/',
-			renderers: ['@astrojs/renderer-preact'],
+			renderers: [
+				'@astrojs/renderer-preact',
+				'@astrojs/renderer-react'
+			],
 			buildOptions: {
 				experimentalStaticBuild: true,
 			},
@@ -21,7 +20,12 @@ describe('Static build - frameworks', () => {
 	});
 
 	it('can build preact', async () => {
-		const html = await fixture.readFile('/index.html');
+		const html = await fixture.readFile('/preact/index.html');
+		expect(html).to.be.a('string');
+	});
+
+	it('can build react', async () => {
+		const html = await fixture.readFile('/react/index.html');
 		expect(html).to.be.a('string');
 	});
 });
