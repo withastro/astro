760bf8c8251523d9ce8f501842377faff922e6b5
Matthew Phillips <matthew@skypack.dev>
2022-01-14 13:50:57 -0500

    remove debugging code

--- packages/renderers/renderer-lit/server-shim.js
+++ packages/renderers/renderer-lit/server-shim.js
@@ -1,7 +1,5 @@
 import { installWindowOnGlobal } from "@lit-labs/ssr/lib/dom-shim.js";
-console.log('before', typeof window);
 installWindowOnGlobal();
-console.log('after', typeof window, typeof globalThis.window);
-window.global = window;
 
+window.global = window;
 document.getElementsByTagName = () => [];
