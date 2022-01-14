a0d9d5d5929cd239e6d38a231322ad64c26b7e8d
Matthew Phillips <matthew@skypack.dev>
2022-01-14 13:37:28 -0500

    Testing again

--- packages/renderers/renderer-lit/server-shim.js
+++ packages/renderers/renderer-lit/server-shim.js
@@ -1,5 +1,7 @@
-import '@lit-labs/ssr/lib/install-global-dom-shim.js';
-if(typeof window !== 'undefined') {
-	window.global = window;
-	document.getElementsByTagName = () => [];
-}
+import { installWindowOnGlobal } from "@lit-labs/ssr/lib/dom-shim.js";
+console.log('before', typeof window);
+installWindowOnGlobal();
+console.log('after', typeof window, typeof globalThis.window);
+window.global = window;
+
+document.getElementsByTagName = () => [];
