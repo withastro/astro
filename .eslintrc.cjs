140a7f0a028c840b03618e377c680419e2d021f5
Erika <3019731+Princesseuh@users.noreply.github.com>
2022-01-26 21:28:21 +0100

    Add EditorConfig, ESLint and Prettier config files from main repo (#160)

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "no-console": "warn",
+    "no-shadow": "error",
+    "prefer-const": "off",
+    // 'require-jsdoc': 'error', // re-enable this to enforce JSDoc for all functions
+  },
+};
