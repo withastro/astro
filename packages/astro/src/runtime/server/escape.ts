aadc9462f1c47afd63f4d0bce56bd3447f58a65b
Nate Moore <natemoo-re@users.noreply.github.com>
2022-01-28 18:15:27 -0600

    Update escape.ts

--- packages/astro/src/runtime/server/escape.ts
+++ packages/astro/src/runtime/server/escape.ts
@@ -9,9 +9,9 @@ export const escapeHTML = (string: any, { deprecated = false }: { deprecated?: b
 	console.warn(`Unescaped HTML content found inside expression!
 
 The next minor version of Astro will automatically escape all
-expression content. See https://err.astro.build/0001 for more info.
+expression content. Please use the `set:html` directive.
 
-Expression:
+Expression content:
 ${string}`);
 	warned.add(string);
 
