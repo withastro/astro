208344728b5a56144b018a2ef6735a6e74a6fa76
Jonathan Neal <jonathantneal@hotmail.com>
2022-01-31 10:48:45 -0500

    Minimize the minimal example

--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,11 +1,6 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
