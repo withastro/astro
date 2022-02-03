ed4969d1132c9e7d59382b3d40acf350106f3a38
Zade Viggers <74938858+zadeviggers@users.noreply.github.com>
2022-02-03 18:39:18 +1300

    Handles all http error code file names the same as 404 files. (#2525)
    * Fix #2195
    
    * Filter out error code files from sitemap

--- packages/astro/src/core/ssr/sitemap.ts
+++ packages/astro/src/core/ssr/sitemap.ts
@@ -1,3 +1,5 @@
+const ERROR_STATUS_CODE_REGEXES = [ /400\/?$/,/401\/?$/,/402\/?$/,/403\/?$/,/404\/?$/,/405\/?$/,/406\/?$/,/407\/?$/,/408\/?$/,/409\/?$/,/410\/?$/,/411\/?$/,/412\/?$/,/413\/?$/,/414\/?$/,/415\/?$/,/416\/?$/,/417\/?$/,/418\/?$/,/421\/?$/,/422\/?$/,/423\/?$/,/424\/?$/,/425\/?$/,/426\/?$/,/428\/?$/,/429\/?$/,/431\/?$/,/451\/?$/,/500\/?$/,/501\/?$/,/502\/?$/,/503\/?$/,/504\/?$/,/505\/?$/,/506\/?$/,/507\/?$/,/508\/?$/,/510\/?$/,/511\/?$/ ];
+
 /** Construct sitemap.xml given a set of URLs */
 export function generateSitemap(pages: string[]): string {
 	// TODO: find way to respect <link rel="canonical"> URLs here
@@ -6,7 +8,8 @@ export function generateSitemap(pages: string[]): string {
 
 	// copy just in case original copy is needed
 	// make sure that 404 page is excluded
-	const urls = [...pages].filter((url) => !/404\/?$/.test(url));
+	// also works for other error pages
+	const urls = [...pages].filter((url) => !ERROR_STATUS_CODE_REGEXES.find((code) => code.test(url)));
 	urls.sort((a, b) => a.localeCompare(b, 'en', { numeric: true })); // sort alphabetically so sitemap is same each time
 	let sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 	for (const url of urls) {
