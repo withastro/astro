a533e503fc4342a7ac9e2c3d484615616e699bae
Jonathan Neal <jonathantneal@hotmail.com>
2022-02-15 09:00:00 -0500

    chore: have smoketests use workspace

--- scripts/smoke/index.js
+++ scripts/smoke/index.js
@@ -1,4 +1,4 @@
-/** @todo migrate these to use the independent docs repository at https://github.com/withastro/docs */
+/** @file Runs all smoke tests and may add extra smoke-test dependencies to `yarn.lock`. */
 
 // @ts-check
 
@@ -10,46 +10,91 @@ import { promises as fs } from 'node:fs';
 
 polyfill(globalThis, { exclude: 'window document' });
 
-const currentDir = new URL('./', import.meta.url);
+/* Configuration
+/* ========================================================================== */
+
+/** URL directory containing this current script. */
+const scriptDir = new URL('./', import.meta.url);
+
+/** URL directory containing the entire project. */
+const rootDir = new URL('../../', import.meta.url);
+
+/** URL directory containing the example subdirectories. */
+const exampleDir = new URL('examples/', rootDir);
+
+/** URL directory containing the Astro package. */
+const astroDir = new URL('packages/astro/', rootDir);
+
+/** GitHub configuration for the external "docs" Astro project. */
+const docGithubConfig = { org: 'withastro', name: 'docs', branch: 'main' };
+
+/** GitHub configuration for the external "astro.build" Astro project. */
+const wwwGithubConfig = { org: 'withastro', name: 'astro.build', branch: 'main' };
+
+/* Functionality
+/* ========================================================================== */
 
 /** Returns the URL to the ZIP of the given GitHub project. */
-const getGitHubZipURL = (/** @type {GitHubZipOpts} */ opts) => `https://github.com/${opts.org}/${opts.name}/archive/refs/heads/${opts.branch}.zip`;
+const getGithubZipURL = (/** @type {GithubOpts} */ opts) => `https://github.com/${opts.org}/${opts.name}/archive/refs/heads/${opts.branch}.zip`;
 
 /** Returns the awaited ZIP Buffer from the given GitHub project. */
-const fetchGitHubZip = (/** @type {GitHubZipOpts} */ opts) =>
-	fetch(getGitHubZipURL(opts))
+const fetchGithubZip = (/** @type {GithubOpts} */ opts) =>
+	fetch(getGithubZipURL(opts))
 		.then((response) => response.arrayBuffer())
 		.then((arrayBuffer) => Buffer.from(arrayBuffer));
 
 /** Downloads a ZIP from the given GitHub project. */
-const downloadGitHubZip = async (/** @type {GitHubZipOpts} */ opts) => {
+const downloadGithubZip = async (/** @type {GithubOpts} */ opts) => {
 	/** Expected directory when the zip is downloaded. */
-	const expectedDir = new URL(`${opts.name}-${opts.branch}`, currentDir);
+	const githubDir = new URL(`${opts.name}-${opts.branch}`, scriptDir);
 
 	/** Whether the expected directory is already available */
-	const hasExpectedDir = await fs.stat(expectedDir).then(
+	const hasGithubDir = await fs.stat(githubDir).then(
 		(stats) => stats.isDirectory(),
 		() => false
 	);
 
-	if (!hasExpectedDir) {
+	if (!hasGithubDir) {
 		console.log('🤖', 'Downloading', `${opts.org}/${opts.name}#${opts.branch}`);
 
-		const buffer = await fetchGitHubZip(opts);
+		const buffer = await fetchGithubZip(opts);
 
 		console.log('🤖', 'Extracting', `${opts.org}/${opts.name}#${opts.branch}`);
 
-		new Zip(buffer).extractAllTo(fileURLToPath(currentDir), true);
+		new Zip(buffer).extractAllTo(fileURLToPath(scriptDir), true);
 
 		console.log('🤖', 'Preparing', `${opts.org}/${opts.name}#${opts.branch}`);
-		await execa('yarn', [], { cwd: fileURLToPath(expectedDir), stdout: 'inherit', stderr: 'inherit' });
+
+		const astroPackage = await readDirectoryPackage(astroDir);
+
+		const githubPackage = await readDirectoryPackage(githubDir);
+
+		if ('astro' in Object(githubPackage.dependencies)) {
+			githubPackage.dependencies['astro'] = astroPackage.version;
+		}
+
+		if ('astro' in Object(githubPackage.devDependencies)) {
+			githubPackage.devDependencies['astro'] = astroPackage.version;
+		}
+
+		if ('astro' in Object(githubPackage.peerDependencies)) {
+			githubPackage.peerDependencies['astro'] = astroPackage.version;
+		}
+
+		await writeDirectoryPackage(githubDir, githubPackage);
 	}
 
-	return expectedDir;
+	return githubDir;
 };
 
+/** Returns the parsed package.json of the given directory. */
+const readDirectoryPackage = async (/** @type {URL} */ dir) => JSON.parse(await fs.readFile(new URL('package.json', dir + '/'), 'utf-8'));
+
+/** Returns upon completion of writing a package.json to the given directory. */
+const writeDirectoryPackage = async (/** @type {URL} */ dir, /** @type {any} */ data) => await fs.writeFile(new URL('package.json', dir + '/'), JSON.stringify(data, null, '  ') + '\n');
+
 /** Returns all child directories of the given directory. */
-const getDirectories = async (/** @type {URL} */ dir) => {
+const getChildDirectories = async (/** @type {URL} */ dir) => {
 	/** @type {URL[]} */
 	const dirs = [];
 
@@ -62,13 +107,20 @@ const getDirectories = async (/** @type {URL} */ dir) => {
 	return dirs;
 };
 
+/* Application
+/* ========================================================================== */
+
 /** Runs all smoke tests. */
 export default async function run() {
 	console.log('');
 
-	const directories = await getDirectories(new URL('../../examples/', import.meta.url));
+	const directories = await getChildDirectories(exampleDir);
 
-	directories.push(await downloadGitHubZip({ org: 'withastro', name: 'docs', branch: 'main' }), await downloadGitHubZip({ org: 'withastro', name: 'astro.build', branch: 'main' }));
+	directories.push(await downloadGithubZip(docGithubConfig), await downloadGithubZip(wwwGithubConfig));
+
+	console.log('🤖', 'Preparing', 'yarn');
+
+	await execa('yarn', [], { cwd: fileURLToPath(rootDir), stdout: 'inherit', stderr: 'inherit' });
 
 	for (const directory of directories) {
 		console.log('🤖', 'Testing', directory.pathname.split('/').at(-1));
@@ -77,6 +129,7 @@ export default async function run() {
 			await execa('yarn', ['build'], { cwd: fileURLToPath(directory), stdout: 'inherit', stderr: 'inherit' });
 		} catch (err) {
 			console.log(err);
+
 			process.exit(1);
 		}
 
@@ -86,4 +139,4 @@ export default async function run() {
 
 run();
 
-/** @typedef {{ org: string, name: string, branch: string }} GitHubZipOpts */
+/** @typedef {{ org: string, name: string, branch: string }} GithubOpts */
