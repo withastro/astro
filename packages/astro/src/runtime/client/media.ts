b4dcc0f8d38a2b520154fb9b80f7f04e14aec248
matthewp <matthewp@users.noreply.github.com>
2022-02-16 15:13:06 +0000

    [ci] yarn format

--- packages/astro/src/runtime/client/media.ts
+++ packages/astro/src/runtime/client/media.ts
@@ -5,21 +5,21 @@ import type { GetHydrateCallback, HydrateOptions } from '../../@types/astro';
  */
 export default async function onMedia(astroId: string, options: HydrateOptions, getHydrateCallback: GetHydrateCallback) {
 	const roots = document.querySelectorAll(`astro-root[uid="${astroId}"]`);
-	if(roots.length === 0) {
+	if (roots.length === 0) {
 		throw new Error(`Unable to find the root for the component ${options.name}`);
 	}
 
 	let innerHTML: string | null = null;
 	let fragment = roots[0].querySelector(`astro-fragment`);
-	if(fragment == null && roots[0].hasAttribute('tmpl')) {
+	if (fragment == null && roots[0].hasAttribute('tmpl')) {
 		// If there is no child fragment, check to see if there is a template.
 		// This happens if children were passed but the client component did not render any.
 		let template = roots[0].querySelector(`template[data-astro-template]`);
-		if(template) {
+		if (template) {
 			innerHTML = template.innerHTML;
 			template.remove();
 		}
-	} else if(fragment) {
+	} else if (fragment) {
 		innerHTML = fragment.innerHTML;
 	}
 
