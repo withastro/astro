1eafc31b46bc9c64306ae3c239d813e6ddcefcc5
Nate Moore <nate@skypack.dev>
2022-02-17 17:45:13 -0600

    test: update astro-envs test

--- packages/astro/test/astro-envs.test.js
+++ packages/astro/test/astro-envs.test.js
@@ -14,10 +14,10 @@ describe('Environment Variables', () => {
 		expect(true).to.equal(true);
 	});
 
-	it('does render public env, does not render private env', async () => {
+	it('does render public env and private env', async () => {
 		let indexHtml = await fixture.readFile('/index.html');
 
-		expect(indexHtml).to.not.include('CLUB_33');
+		expect(indexHtml).to.include('CLUB_33');
 		expect(indexHtml).to.include('BLUE_BAYOU');
 	});
 
@@ -39,6 +39,27 @@ describe('Environment Variables', () => {
 			})
 		);
 
-		expect(found).to.equal(true, 'found the env variable in the JS build');
+		expect(found).to.equal(true, 'found the public env variable in the JS build');
+	});
+
+	it('does not include private env in client-side JS', async () => {
+		let dirs = await fixture.readdir('/assets');
+		let found = false;
+
+		// Look in all of the .js files to see if the public env is inlined.
+		// Testing this way prevents hardcoding expected js files.
+		// If we find it in any of them that's good enough to know its NOT working.
+		await Promise.all(
+			dirs.map(async (path) => {
+				if (path.endsWith('.js')) {
+					let js = await fixture.readFile(`/assets/${path}`);
+					if (js.includes('CLUB_33')) {
+						found = true;
+					}
+				}
+			})
+		);
+
+		expect(found).to.equal(false, 'found the private env variable in the JS build');
 	});
 });
