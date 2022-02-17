f26c7bfba0b13ba0ef4708b9e63f5648810c133d
Nate Moore <nate@skypack.dev>
2022-02-17 17:07:07 -0600

    chore(examples): add env-vars example

---
+++ examples/env-vars/src/env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly DB_PASSWORD: string;
+	readonly PUBLIC_SOME_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
---
+++ examples/env-vars/src/scripts/client.ts
@@ -0,0 +1,9 @@
+(() => {
+	const { SSR, DB_PASSWORD, PUBLIC_SOME_KEY } = import.meta.env;
+
+	// DB_PASSWORD is NOT available because we're running on the client
+	console.log({ SSR, DB_PASSWORD });
+
+	// PUBLIC_SOME_KEY is available everywhere
+	console.log({ SSR, PUBLIC_SOME_KEY });
+})()
