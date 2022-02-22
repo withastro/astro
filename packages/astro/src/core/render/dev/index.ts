f165b78ad2ba6cc25069849715cc821adb035a81
Joel Kuzmarski <leoj3n@gmail.com>
2022-02-22 03:53:21 -0600

    Make Non-HTML Pages work for static build (#2638)
    * Make Non-HTML Pages work for static build
    
    * Check hasOwnProperty instead of length
    
    * Fix typo "> 0"

--- packages/astro/src/core/render/dev/index.ts
+++ packages/astro/src/core/render/dev/index.ts
@@ -50,10 +50,10 @@ export async function render(renderers: Renderer[], mod: ComponentInstance, ssrO
 	const { astroConfig, filePath, logging, mode, origin, pathname, route, routeCache, viteServer } = ssrOpts;
 
 	// Add hoisted script tags
-	const scripts = createModuleScriptElementWithSrcSet(astroConfig.buildOptions.experimentalStaticBuild ? Array.from(mod.$$metadata.hoistedScriptPaths()) : []);
+	const scripts = createModuleScriptElementWithSrcSet(astroConfig.buildOptions.experimentalStaticBuild && mod.hasOwnProperty('$$metadata') ? Array.from(mod.$$metadata.hoistedScriptPaths()) : []);
 
 	// Inject HMR scripts
-	if (mode === 'development' && astroConfig.buildOptions.experimentalStaticBuild) {
+	if (mod.hasOwnProperty('$$metadata') && mode === 'development' && astroConfig.buildOptions.experimentalStaticBuild) {
 		scripts.add({
 			props: { type: 'module', src: '/@vite/client' },
 			children: '',
