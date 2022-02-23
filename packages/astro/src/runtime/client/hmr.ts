c8cfd62ee25bcf576c949c95e44760bfc287188c
Nate Moore <nate@skypack.dev>
2022-02-23 13:53:55 -0600

    chore: lint

--- packages/astro/src/runtime/client/hmr.ts
+++ packages/astro/src/runtime/client/hmr.ts
@@ -2,6 +2,7 @@ if (import.meta.hot) {
 	const parser = new DOMParser();
 	import.meta.hot.on('astro:update', async ({ file }) => {
 		const { default: diff } = await import('micromorph');
+		// eslint-disable-next-line no-console
 		console.log(`[vite] hot updated: ${file}`)
 		import.meta.hot?.accept();
 		const res = await fetch(`${window.location}`).then(res => res.text())
@@ -9,16 +10,11 @@ if (import.meta.hot) {
 		
 		// Match incoming islands to current state
 		const roots = Array.from(doc.querySelectorAll('astro-root'));
-		const uids = new Set(Array.from(document.querySelectorAll('astro-root')).map(el => el.getAttribute('uid')));
 		for (const root of roots) {
 			const uid = root.getAttribute('uid')
 			const current = document.querySelector(`astro-root[uid="${uid}"]`);
 			if (current) {
-				uids.delete(uid);
 				root.innerHTML = current?.innerHTML;
-			} else {
-				console.log(Array.from(uids))
-				console.log(`Could not find [${uid}]`)
 			}
 		}
 
