13fc066336f034af2ce2ca2d5e0822da614dd2a3
Nate Moore <nate@skypack.dev>
2022-02-23 16:53:49 -0600

    refactor: cleanup hmr script

--- packages/astro/src/runtime/client/hmr.ts
+++ packages/astro/src/runtime/client/hmr.ts
@@ -3,21 +3,18 @@ if (import.meta.hot) {
 	import.meta.hot.on('astro:update', async ({ file }) => {
 		const { default: diff } = await import('micromorph');
 		// eslint-disable-next-line no-console
-		console.log(`[vite] hot updated: ${file}`)
-		import.meta.hot?.accept();
-		const html = await fetch(`${window.location}`).then(res => res.text())
+		console.log(`[vite] hot updated: ${file}`);
+		const html = await fetch(`${window.location}`).then(res => res.text());
 		const doc = parser.parseFromString(html, 'text/html');
-		
+
 		// Match incoming islands to current state
-		const roots = Array.from(doc.querySelectorAll('astro-root'));
-		for (const root of roots) {
-			const uid = root.getAttribute('uid')
+		for (const root of doc.querySelectorAll('astro-root')) {
+			const uid = root.getAttribute('uid');
 			const current = document.querySelector(`astro-root[uid="${uid}"]`);
 			if (current) {
 				root.innerHTML = current?.innerHTML;
 			}
 		}
-
-		diff(document, doc)
+		diff(document, doc);
 	});
 }
