99b4ae888d56d09cca4c49ca7b1e543a56e5d5e6
JuanM04 <me@juanm04.com>
2022-02-24 20:40:26 -0300

    Updated with-markdown

--- examples/with-markdown/astro.config.mjs
+++ examples/with-markdown/astro.config.mjs
@@ -1,13 +1,10 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Run `astro add renderers` to add official renderers.
-	renderers: [],
+	renderers: ['@astrojs/renderer-react', '@astrojs/renderer-preact', '@astrojs/renderer-svelte', '@astrojs/renderer-vue'],
 });
