d0485a2f566513d6a3c167feb8789cfc7982a3fa
Erika <3019731+Princesseuh@users.noreply.github.com>
2022-02-28 15:18:14 -0500

    Apply relevant content transformations for TSX generation only in relevant parts (#162)
    * Apply relevant content transformations for JSX generation only in relevant parts
    
    * Fix frontmatter transformations applying to content by accident, add changeset

---
+++ .changeset/famous-apes-vanish.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/language-server': patch
+---
+
+Only apply content transformations for TSX generation in relevant places
--- packages/language-server/src/plugins/typescript/astro2tsx.ts
+++ packages/language-server/src/plugins/typescript/astro2tsx.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import { EOL } from 'os';
+import { parseAstro } from '../../core/documents/parseAstro';
 
 const ASTRO_DEFINITION_BYTES = readFileSync(require.resolve('../../../astro.d.ts'));
 const ASTRO_DEFINITION_STR = ASTRO_DEFINITION_BYTES.toString('utf-8');
@@ -22,63 +23,74 @@ interface Astro2TSXResult {
   code: string;
 }
 
-export default function(content: string): Astro2TSXResult {
-  let result: Astro2TSXResult = {
-    code: ''
-  };
+export default function (content: string): Astro2TSXResult {
+	let result: Astro2TSXResult = {
+		code: '',
+	};
 
-  // Replace frontmatter marks with comments
-  let raw = content
-    // Handle case where semicolons is not used in the frontmatter section
-    .replace(/((?!^)(?<!;)\n)(---)/g, (_whole, start, _dashes) => {
-      return start + ';' + '//';
-    })
-    .replace(/---/g, '///')
-    // Turn comments into JS comments
-    .replace(/<\s*!--([^-->]*)(.*?)-->/gs, (whole) => {
-      return `{/*${whole}*/}`;
-    })
-    // Turn styles into internal strings
-    .replace(/<\s*style([^>]*)>(.*?)<\s*\/\s*style>/gs, (_whole, attrs, children) => {
-      return `<style${attrs}>{\`${escapeTemplateLiteralContent(children)}\`}</style>`;
-    })
-    // Turn scripts into function calls
-    .replace(/<\s*script([^\/>]*)>(.*?)<\s*\/\s*script>/gs, (_whole, attrs, children, offset) => {
-      return `<script${attrs}>{()=>{${children}}}</script>`;
-    })
-    // Close void elements
-    .replace(/<(\s*(meta|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)([^>]*))>/g, (whole, inner) => {
-      if(whole.endsWith('/>')) return whole;
-      return `<${inner} />`;
-    })
-    // Replace `@` prefixed attributes with `_` prefix
-    .replace(/<([$A-Z_a-z][^\s\/>]*)([^\/>]*)>/g, (whole: string, tag: string, attrs: string) => {
-      if (attrs.includes('@')) {
-        return `<${tag}${attrs.replace(
-          // the following regular expression captures:
-          //   $1. any character that may appear before an attribute name (https://html.spec.whatwg.org/#before-attribute-name-state)
-          // then, one `@` at sign, then:
-          //   $2. any characters that may appear in an attribute name (https://html.spec.whatwg.org/#attribute-name-state)
-          // then, looking ahead any one character that may not appear in an attribute name, or the end
-          /([\f\n\r\t "'])@([^\f\n\r\t /=>"'<]+)(?=[\f\n\r\t /=>"'<]|$)/g,
-          '$1_$2'
-        )}>`
-      } else {
-        return whole;
-      }
-    })
-    // Fix doctypes
-    .replace(/<!(doctype html)>/gi, (_whole, main) => {
-      return `<${main.toLowerCase()}/>`;
-    });
+	const astroDocument = parseAstro(content);
 
-  result.code = (
-    raw + EOL +
+	// Frontmatter replacements
+	let frontMatterRaw = '';
+	if (astroDocument.frontmatter.state === 'closed') {
+		frontMatterRaw = content
+			.substring(astroDocument.frontmatter.startOffset ?? 0, (astroDocument.frontmatter.endOffset ?? 0) + 3)
+			// Handle case where semicolons is not used in the frontmatter section
+			.replace(/((?!^)(?<!;)\n)(---)/g, (_whole, start, _dashes) => {
+				return start + ';' + '//';
+			})
+			// Replace frontmatter marks with comments
+			.replace(/---/g, '///');
+	}
 
-    // Add TypeScript definitions
-    addProps(raw, ASTRO_DEFINITION_STR)
-  );
+	// Content replacement
+	let htmlRaw = content
+		.substring(astroDocument.content.firstNonWhitespaceOffset ?? 0)
+		// Turn comments into JS comments
+		.replace(/<\s*!--([^-->]*)(.*?)-->/gs, (whole) => {
+			return `{/*${whole}*/}`;
+		})
+		// Turn styles into internal strings
+		.replace(/<\s*style([^>]*)>(.*?)<\s*\/\s*style>/gs, (_whole, attrs, children) => {
+			return `<style${attrs}>{\`${escapeTemplateLiteralContent(children)}\`}</style>`;
+		})
+		// Turn scripts into function calls
+		.replace(/<\s*script([^\/>]*)>(.*?)<\s*\/\s*script>/gs, (_whole, attrs, children, offset) => {
+			return `<script${attrs}>{()=>{${children}}}</script>`;
+		})
+		// Close void elements
+		.replace(/<(\s*(meta|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)([^>]*))>/g, (whole, inner) => {
+			if (whole.endsWith('/>')) return whole;
+			return `<${inner} />`;
+		})
+		// Replace `@` prefixed attributes with `_` prefix
+		.replace(/<([$A-Z_a-z][^\s\/>]*)([^\/>]*)>/g, (whole: string, tag: string, attrs: string) => {
+			if (attrs.includes('@')) {
+				return `<${tag}${attrs.replace(
+					// the following regular expression captures:
+					//   $1. any character that may appear before an attribute name (https://html.spec.whatwg.org/#before-attribute-name-state)
+					// then, one `@` at sign, then:
+					//   $2. any characters that may appear in an attribute name (https://html.spec.whatwg.org/#attribute-name-state)
+					// then, looking ahead any one character that may not appear in an attribute name, or the end
+					/([\f\n\r\t "'])@([^\f\n\r\t /=>"'<]+)(?=[\f\n\r\t /=>"'<]|$)/g,
+					'$1_$2'
+				)}>`;
+			} else {
+				return whole;
+			}
+		})
+		// Fix doctypes
+		.replace(/<!(doctype html)>/gi, (_whole, main) => {
+			return `<${main.toLowerCase()}/>`;
+		});
 
+	result.code =
+		frontMatterRaw +
+		'\n' +
+		htmlRaw +
+		EOL +
+		// Add TypeScript definitions
+		addProps(frontMatterRaw, ASTRO_DEFINITION_STR);
 
-  return result;
+	return result;
 }
