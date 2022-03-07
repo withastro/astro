c34eb763342994f6ef123b26675c5b2ae52f92be
Nate Moore <nate@skypack.dev>
2022-03-07 13:00:01 -0600

    chore: update tests to use mocha

--- packages/webapi/run/test.setup.js
+++ packages/webapi/run/test.setup.js
@@ -1,36 +1,6 @@
-import { fork as nodeFork, spawn as nodeSpawn } from 'child_process'
 import { fileURLToPath } from 'url'
 
 export { strict as assert } from 'assert'
-
-export const args = () => process.argv.slice(2).reduce(
-	(argo, arg) => {
-		if (arg.startsWith('-')) {
-			argo.opts[argo.prop = arg] = []
-		} else if (argo.prop) {
-			argo.opts[argo.prop].push(arg)
-		} else {
-			argo.paths.push(arg)
-		}
-
-		return argo
-	},
-	{ opts: {}, paths: [] }
-)
-
-export const fork = (...args) => new Promise((resolve, reject) => {
-	const child = nodeFork(...args)
-
-	child.on('error', reject)
-	child.on('exit', resolve)
-})
-
-export const spawn = (...args) => new Promise((resolve, reject) => {
-	const child = nodeSpawn(...args)
-
-	child.on('error', reject)
-	child.on('exit', resolve)
-})
 
 export const pathFrom = (...args) => fileURLToPath(args.reduce((url, bit) => new URL(bit, url), new URL('file:')))
 
