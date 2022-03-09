fe8ac94108d6ea1198fea6671623dda2d55dd84a
Nate Moore <nate@skypack.dev>
2022-03-09 16:31:28 -0600

    feat: escape HTML by default

--- packages/astro/src/runtime/server/escape.ts
+++ packages/astro/src/runtime/server/escape.ts
@@ -1,23 +1,6 @@
 const entities = { '"': 'quot', '&': 'amp', "'": 'apos', '<': 'lt', '>': 'gt' } as const;
 
-const warned = new Set<string>();
-export const escapeHTML = (string: any, { deprecated = false }: { deprecated?: boolean } = {}) => {
-	const escaped = string.replace(/["'&<>]/g, (char: keyof typeof entities) => '&' + entities[char] + ';');
-	if (!deprecated) return escaped;
-	if (warned.has(string) || !string.match(/[&<>]/g)) return string;
-	// eslint-disable-next-line no-console
-	console.warn(`Unescaped HTML content found inside expression!
-
-The next minor version of Astro will automatically escape all
-expression content. Please use the \`set:html\` directive.
-
-Expression content:
-${string}`);
-	warned.add(string);
-
-	// Return unescaped content for now. To be removed.
-	return string;
-};
+export const escapeHTML = (string: any) => string.replace(/["'&<>]/g, (char: keyof typeof entities) => '&' + entities[char] + ';');
 
 /**
  * RawString is a "blessed" version of String
