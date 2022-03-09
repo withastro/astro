76dd486e3cf6622cb0216d4417bddd540710cb43
bholmesdev <hey@bholmes.dev>
2022-03-09 17:40:08 -0500

    feat: add getStaticPaths status code tests

--- packages/astro/test/astro-get-static-paths.test.js
+++ packages/astro/test/astro-get-static-paths.test.js
@@ -4,19 +4,57 @@ import { loadFixture } from './test-utils.js';
 describe('getStaticPaths()', () => {
 	let fixture;
 
-	before(async () => {
-		fixture = await loadFixture({
-			projectRoot: './fixtures/astro-get-static-paths/',
-			buildOptions: {
-				site: 'https://mysite.dev/blog/',
-				sitemap: false,
-			},
+	describe('number of build calls', () => {
+		before(async () => {
+			fixture = await loadFixture({
+				projectRoot: './fixtures/astro-get-static-paths/',
+				buildOptions: {
+					site: 'https://mysite.dev/blog/',
+					sitemap: false,
+				},
+			});
+			await fixture.build();
 		});
-		await fixture.build();
+		it('is only once during build', () => {
+			// useless expect; if build() throws in setup then this test fails
+			expect(true).to.equal(true);
+		});
 	});
 
-	it('is only called once during build', () => {
-		// useless expect; if build() throws in setup then this test fails
-		expect(true).to.equal(true);
+	describe('dynamic', () => {
+		let fixture;
+		let devServer;
+
+		before(async () => {
+			fixture = await loadFixture({ projectRoot: './fixtures/astro-get-static-paths/' });
+			devServer = await fixture.startDevServer();
+		});
+
+		after(async () => {
+			devServer && devServer.stop();
+		});
+		describe('[plain]', () => {
+			it('resolves 200 on matching static path', async () => {
+				const res = await fixture.fetch('/pizza/provolone-sausage');
+				expect(res.status).to.equal(200);
+			});
+
+			it('resolves 404 on bad static path', async () => {
+				const res = await fixture.fetch('/pizza/provolone-pineapple');
+				expect(res.status).to.equal(404);
+			});
+		});
+
+		describe('[...spread]', () => {
+			it('resolves 200 on matching static path', async () => {
+				const res = await fixture.fetch('/pizza/grimaldis/new-york');
+				expect(res.status).to.equal(200);
+			});
+
+			it('resolves 404 on bad static path', async () => {
+				const res = await fixture.fetch('/pizza/pizza-hut');
+				expect(res.status).to.equal(404);
+			});
+		});
 	});
 });
