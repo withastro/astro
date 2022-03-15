6d0d8acd5e5b6049b58f4b6be4b95c07d1b954a9
Nate Moore <nate@skypack.dev>
2022-03-15 14:42:59 -0500

    chore: prettier fix

--- examples/framework-vue/astro.config.mjs
+++ examples/framework-vue/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
--- examples/with-vite-plugin-pwa/astro.config.mjs
+++ examples/with-vite-plugin-pwa/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
