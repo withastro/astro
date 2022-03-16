379eb282d63286d7144181ec0ad4ba93bb1c1038
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-03-16 09:58:13 +0100

    fixing indent spacing

--- packages/astro/test/tailwindcss.test.js
+++ packages/astro/test/tailwindcss.test.js
@@ -7,7 +7,7 @@ let fixture;
 describe('Tailwind', () => {
 	let fixture;
 
-    before(async () => {
+	before(async () => {
 		fixture = await loadFixture({
 			projectRoot: './fixtures/tailwindcss/',
 			renderers: [],
@@ -19,12 +19,12 @@ describe('Tailwind', () => {
 		});
 	});
 
-    // test HTML and CSS contents for accuracy
-    describe('build', () => {
+	// test HTML and CSS contents for accuracy
+	describe('build', () => {
 		let $;
 		let bundledCSS;
 
-        before(async () => {
+		before(async () => {
 			await fixture.build();
 
 			// get bundled CSS (will be hashed, hence DOM query)
@@ -37,14 +37,14 @@ describe('Tailwind', () => {
 		it('resolves CSS in src/styles', async () => {
 			expect(bundledCSS, 'includes used component classes').to.match(/\.bg-purple-600{/);
 
-            // tests a random tailwind class that isn't used on the page
-            expect(bundledCSS, 'purges unused classes').not.to.match(/\.bg-blue-600{/);
+			// tests a random tailwind class that isn't used on the page
+			expect(bundledCSS, 'purges unused classes').not.to.match(/\.bg-blue-600{/);
 
-            // tailwind escapes colons, `lg:py-3` compiles to `lg\:py-3`
-            expect(bundledCSS, 'includes responsive classes').to.match(/\.lg\\:py-3{/);
+			// tailwind escapes colons, `lg:py-3` compiles to `lg\:py-3`
+			expect(bundledCSS, 'includes responsive classes').to.match(/\.lg\\:py-3{/);
 
-            // tailwind escapes brackets, `font-[900]` compiles to `font-\[900\]`
-            expect(bundledCSS, 'supports arbitrary value classes').to.match(/\.font-\\\[900\\\]{font-weight:900}/)
+			// tailwind escapes brackets, `font-[900]` compiles to `font-\[900\]`
+			expect(bundledCSS, 'supports arbitrary value classes').to.match(/\.font-\\\[900\\\]{font-weight:900}/);
 		});
 
 		it('maintains classes in HTML', async () => {
@@ -54,11 +54,11 @@ describe('Tailwind', () => {
 			expect(button.hasClass('lg:py-3'), 'responsive class').to.be.true;
 			expect(button.hasClass('font-[900]', 'arbitrary value')).to.be.true;
 		});
-    });
+	});
 
-    // with "build" handling CSS checking, the dev tests are mostly testing the paths resolve in dev
-    describe('dev', () => {
-        let devServer;
+	// with "build" handling CSS checking, the dev tests are mostly testing the paths resolve in dev
+	describe('dev', () => {
+		let devServer;
 		let $;
 
 		before(async () => {
@@ -73,21 +73,21 @@ describe('Tailwind', () => {
 
 		it('resolves CSS in src/styles', async () => {
 			const href = $(`link[href$="/src/styles/global.css"]`).attr('href');
-            const res = await fixture.fetch(href);
-            expect(res.status).to.equal(200);
+			const res = await fixture.fetch(href);
+			expect(res.status).to.equal(200);
 
-            const text = await res.text()
-            
-            expect(text, 'includes used component classes').to.match(/\.bg-purple-600/);
+			const text = await res.text();
+
+			expect(text, 'includes used component classes').to.match(/\.bg-purple-600/);
 
 			// tests a random tailwind class that isn't used on the page
-            expect(text, 'purges unused classes').not.to.match(/\.bg-blue-600/);
+			expect(text, 'purges unused classes').not.to.match(/\.bg-blue-600/);
 
 			// tailwind escapes colons, `lg:py-3` compiles to `lg\:py-3`
-            expect(text, 'includes responsive classes').to.match(/\.lg\\\\:py-3/);
+			expect(text, 'includes responsive classes').to.match(/\.lg\\\\:py-3/);
 
 			// tailwind escapes brackets, `font-[900]` compiles to `font-\[900\]`
-            expect(text, 'supports arbitrary value classes').to.match(/.font-\\[900\\]/)
+			expect(text, 'supports arbitrary value classes').to.match(/.font-\\[900\\]/);
 		});
 
 		it('maintains classes in HTML', async () => {
@@ -97,5 +97,5 @@ describe('Tailwind', () => {
 			expect(button.hasClass('lg:py-3'), 'responsive class').to.be.true;
 			expect(button.hasClass('font-[900]', 'arbitrary value')).to.be.true;
 		});
-    })
+	});
 });
