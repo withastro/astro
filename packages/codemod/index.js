1ac0f09d6d9e88e58b125dd36c560bdfdfa32094
Nate Moore <nate@skypack.dev>
2022-03-19 19:56:31 -0500

    fix: handle multiple integrations

--- packages/codemod/index.js
+++ packages/codemod/index.js
@@ -1,13 +1,20 @@
 import { addIntegrationToConfig } from './config/add.js';
+import { bgGreen, green, black, bgCyan, cyan } from 'kleur';
 
 async function run() {
-  const [add, name] = process.argv.splice(2);
-  if (add !== 'add') return;
-	if (!name) return;
-  await addIntegrationToConfig({ name, package: `@astrojs/${name}` });
-  console.log(
-    `\n  ${bgGreen(black(' success '))} Added ${cyan(name)} integration`
-  );
+  const [command, ...args] = process.argv.splice(2);
+  if (command !== 'add') return;
+	if (!args.length) return;
+	for (const name of args) {
+		await addIntegrationToConfig({ name, package: `@astrojs/${name}` });
+		// TODO: add integration to devDependencies and add peerDependencies as dependencies
+		console.log(
+			`\n  ${bgGreen(black(' success '))} Added ${cyan(name)} integration`
+		);
+	}
+	// console.log(
+	// 	`\n  ${bgCyan(black(' next '))} run ${green('npm install')}`
+	// );
 }
 
 run();
