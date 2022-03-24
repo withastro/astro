255394eec87b7490fd7dc059df2a1639c11eab65
Matthew Phillips <matthew@skypack.dev>
2022-03-24 17:08:36 -0400

    Build to a single file (#2873)
    * Build to a single file
    
    * Updates based on initial code review
    
    * Adds a changeset
    
    * Use the default export for cjs module
    
    * Await generatePages
    
    * Prevent timing from causing module to not import
    
    * Fix shared CSS
    
    * Properly handle windows ids
    
    * Dont shadow
    
    * Fix ts errors
    
    * Remove console.log

--- packages/integrations/node/src/server.ts
+++ packages/integrations/node/src/server.ts
@@ -1,5 +1,6 @@
 import type { SSRManifest } from 'astro';
 import type { IncomingMessage, ServerResponse } from 'http';
+import type { Readable } from 'stream';
 import { NodeApp } from 'astro/app/node';
 import { polyfill } from '@astrojs/webapi';
 
@@ -8,7 +9,7 @@ polyfill(globalThis, {
 });
 
 export function createExports(manifest: SSRManifest) {
-	const app = new NodeApp(manifest, new URL(import.meta.url));
+	const app = new NodeApp(manifest);
 	return {
 		async handler(req: IncomingMessage, res: ServerResponse, next?: (err?: unknown) => void) {
 			const route = app.match(req);
@@ -35,13 +36,8 @@ async function writeWebResponse(res: ServerResponse, webResponse: Response) {
 	const { status, headers, body } = webResponse;
 	res.writeHead(status, Object.fromEntries(headers.entries()));
 	if (body) {
-		const reader = body.getReader();
-		while (true) {
-			const { done, value } = await reader.read();
-			if (done) break;
-			if (value) {
-				res.write(value);
-			}
+		for await(const chunk of (body as unknown as Readable)) {
+			res.write(chunk);
 		}
 	}
 	res.end();
