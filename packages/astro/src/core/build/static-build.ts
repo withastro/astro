73df0ad36dae98ac2573d1b0dd5dbf7c25f51a90
bholmesdev <hey@bholmes.dev>
2022-03-24 13:22:29 -0400

    remove (includes setup + teardown)

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -103,7 +103,7 @@ export async function staticBuild(opts: StaticBuildOptions) {
 		info(opts.logging, null, `\n${bgMagenta(black(' finalizing server assets '))}\n`);
 		await ssrMoveAssets(opts);
 	}
-	info(opts.logging, null, dim(`Completed in ${getTimeStat(timer.generate, performance.now())} (includes setup + teardown)\n`));
+	info(opts.logging, null, dim(`Completed in ${getTimeStat(timer.generate, performance.now())}\n`));
 }
 
 async function ssrBuild(opts: StaticBuildOptions, internals: BuildInternals, input: Set<string>) {
