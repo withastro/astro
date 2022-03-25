f817eb1290056cbfcbd9aacbcf9c02526fc7ffe4
Matthew Phillips <matthew@skypack.dev>
2022-03-25 11:02:26 -0400

    Make it deployable to Netlify

--- packages/astro/src/core/request.ts
+++ packages/astro/src/core/request.ts
@@ -3,20 +3,23 @@ import type { LogOptions } from './logger';
 import { warn } from './logger.js';
 
 type HeaderType = Headers | Record<string, any> | IncomingHttpHeaders;
+type RequestBody = ArrayBuffer | Blob | ReadableStream | URLSearchParams | FormData;
 
 export interface CreateRequestOptions {
 	url: URL | string;
 	headers: HeaderType;
 	method?: string;
+	body?: RequestBody | undefined;
 	logging: LogOptions;
 }
 
-export function createRequest({ url, headers, method = 'GET', logging }: CreateRequestOptions): Request {
+export function createRequest({ url, headers, method = 'GET', body = undefined, logging }: CreateRequestOptions): Request {
 	let headersObj = headers instanceof Headers ? headers : new Headers(Object.entries(headers as Record<string, any>));
 
 	const request = new Request(url.toString(), {
 		method: method,
 		headers: headersObj,
+		body
 	});
 
 	Object.defineProperties(request, {
