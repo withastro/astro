51daacfabd3b3d87f2402a3afff5674aaf5b89aa
Nate Moore <nate@skypack.dev>
2022-03-26 05:19:52 -0500

    feat(spa): allow persistent option

--- packages/integrations/spa/client.js
+++ packages/integrations/spa/client.js
@@ -1,9 +1,25 @@
-import listen from 'micromorph/spa'
+import listen from 'micromorph/spa';
 
-export default () => {
+export default ({ persistent }) => {
 	listen({
+		beforeDiff(doc) {
+			if (!persistent) return;
+			for (const island of doc.querySelectorAll('astro-root')) {
+				const uid = island.getAttribute('uid');
+				const current = document.querySelector(`astro-root[uid="${uid}"]`);
+				if (current) {
+					current.dataset.persist = true;
+					island.replaceWith(current);
+				}
+			}
+		},
 		afterDiff() {
-			window.dispatchEvent(new CustomEvent('astro:locationchange'))
-		}
+			if (persistent) {
+				for (const island of doc.querySelectorAll('astro-root')) {
+					delete island.dataset.persist
+				}
+			}
+			window.dispatchEvent(new CustomEvent('astro:locationchange'));
+		},
 	});
-}
+};
