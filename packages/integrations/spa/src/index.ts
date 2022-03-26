1b4aa8f6a7309293007157f63d8cbfc12f4f1054
Nate Moore <nate@skypack.dev>
2022-03-26 05:19:52 -0500

    feat(spa): allow persistent option

--- packages/integrations/spa/src/index.ts
+++ packages/integrations/spa/src/index.ts
@@ -1,6 +1,10 @@
 import type { AstroIntegration } from 'astro';
 
-export default function createPlugin(): AstroIntegration {
+export interface SpaOptions {
+	persistent?: boolean;
+}
+
+export default function createPlugin({ persistent = true }: SpaOptions): AstroIntegration {
 	return {
 		name: '@astrojs/spa',
 		hooks: {
@@ -8,7 +12,7 @@ export default function createPlugin(): AstroIntegration {
 				// This gets injected into the user's page, so we need to re-export Turbolinks
 				// from our own package so that package managers like pnpm don't get mad and
 				// can follow the import correctly.
-				injectScript('page', `import listen from "@astrojs/spa/client.js"; listen();`);
+				injectScript('page', `import listen from "@astrojs/spa/client.js"; listen({ persistent: ${persistent} });`);
 			},
 		},
 	};
