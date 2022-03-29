f048cdff7011871c89b936e33c336690689e0fbb
Matthew Phillips <matthew@skypack.dev>
2022-03-29 16:59:06 -0400

    Fixes API route compat with Node 14 (#2936)
    * Fixes API route compat with Node 14
    
    * Changeset

--- packages/astro/test/fixtures/ssr-api-route/src/pages/food.json.js
+++ packages/astro/test/fixtures/ssr-api-route/src/pages/food.json.js
@@ -8,3 +8,13 @@ export function get() {
 		])
 	};
 }
+
+export async function post(params, request) {
+	const body = await request.text();
+	return new Response(body === `some data` ? `ok` : `not ok`, {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	});
+}
