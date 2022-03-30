ff1de0ae30132ff9bcb7a41f04d43a246a2e24bd
bholmesdev <hey@bholmes.dev>
2022-03-30 16:32:04 -0400

    fix: comment adapter to start

--- examples/astro-star-explorer/astro.config.mjs
+++ examples/astro-star-explorer/astro.config.mjs
@@ -5,5 +5,5 @@ import nodeAdapter from '@astrojs/node'
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
-  adapter: nodeAdapter(),
+  // adapter: nodeAdapter(),
 });
