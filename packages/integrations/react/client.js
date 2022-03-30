99513c3703992a22efdcca78a55cf2c45b3eb5ac
delucis <swithinbank@gmail.com>
2022-03-30 11:28:32 +0200

    First pass at supporting React 18 in @astrojs/react

--- packages/integrations/react/client.js
+++ packages/integrations/react/client.js
@@ -1,13 +1,28 @@
 import { createElement } from 'react';
-import { hydrate } from 'react-dom';
+import { version } from 'react-dom/package.json';
 import StaticHtml from './static-html.js';
 
-export default (element) => (Component, props, children) =>
-	hydrate(
+const usingReact18 = version.startsWith('18.');
+
+// Import the correct hydration method based on the version of React.
+const hydrateFn = (
+	async () => usingReact18
+		? (await import('react-dom/client')).hydrateRoot
+		: (await import('react-dom')).hydrate
+)();
+
+export default (element) => async (Component, props, children) => {
+	const args = [
 		createElement(
 			Component,
 			{ ...props, suppressHydrationWarning: true },
 			children != null ? createElement(StaticHtml, { value: children, suppressHydrationWarning: true }) : children
 		),
-		element
-	);
+		element,
+	];
+	
+	// `hydrateRoot` expects [container, component] instead of [component, container].
+	if (usingReact18) args.reverse();
+	
+	return (await hydrateFn)(...args);
+};
