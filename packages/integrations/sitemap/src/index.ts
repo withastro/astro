e3d30d790ffc268c2d6aa9154870c4a05beb4b23
bholmesdev <hey@bholmes.dev>
2022-04-02 11:12:24 -0400

    feat: add sitemap filter config option

--- packages/integrations/sitemap/src/index.ts
+++ packages/integrations/sitemap/src/index.ts
@@ -2,10 +2,26 @@ import fs from 'node:fs';
 import type { AstroConfig, AstroIntegration } from 'astro';
 const STATUS_CODE_PAGE_REGEXP = /\/[0-9]{3}\/?$/;
 
+type SitemapOptions =
+	| {
+			/**
+			 * All pages are included in your sitemap by default.
+			 * With this config option, you can filter included pages by URL.
+			 *
+			 * The `page` function parameter is the full URL of your rendered page, including your `site` domain.
+			 * Return `true` to include a page in your sitemap, and `false` to remove it.
+			 *
+			 * ```js
+			 * filter: (page) => page !== 'http://example.com/secret-page'
+			 * ```
+			 */
+			filter?(page: string): string;
+	  }
+	| undefined;
+
 /** Construct sitemap.xml given a set of URLs */
 function generateSitemap(pages: string[]) {
 	// TODO: find way to respect <link rel="canonical"> URLs here
-	// TODO: find way to exclude pages from sitemap
 	const urls = [...pages].filter((url) => !STATUS_CODE_PAGE_REGEXP.test(url));
 	urls.sort((a, b) => a.localeCompare(b, 'en', { numeric: true })); // sort alphabetically so sitemap is same each time
 	let sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
@@ -16,7 +32,7 @@ function generateSitemap(pages: string[]) {
 	return sitemap;
 }
 
-export default function createPlugin(): AstroIntegration {
+export default function createPlugin({ filter }: SitemapOptions = {}): AstroIntegration {
 	let config: AstroConfig;
 	return {
 		name: '@astrojs/sitemap',
@@ -29,7 +45,10 @@ export default function createPlugin(): AstroIntegration {
 				if (!finalSiteUrl) {
 					return;
 				}
-				const pageUrls = pages.map((p) => new URL(p.pathname, finalSiteUrl).href);
+				let pageUrls = pages.map((p) => new URL(p.pathname, finalSiteUrl).href);
+				if (filter) {
+					pageUrls.filter((page: string) => filter(page));
+				}
 				const sitemapContent = generateSitemap(pageUrls);
 				fs.writeFileSync(new URL('sitemap.xml', dir), sitemapContent);
 			},
