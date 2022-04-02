76f6643dddb9343218020cf7585be92a1fe45d3a
natemoo-re <natemoo-re@users.noreply.github.com>
2022-04-02 17:35:42 +0000

    [ci] format

--- packages/astro/src/vite-plugin-astro/index.ts
+++ packages/astro/src/vite-plugin-astro/index.ts
@@ -63,7 +63,7 @@ export default function astro({ config, logging }: AstroPluginOptions): vite.Plu
 				const { query: fromQuery, filename } = parseAstroRequest(from);
 				if (fromQuery.astro && isRelativePath(id) && fromQuery.type === 'script') {
 					const resolvedURL = new URL(id, `file://${filename}`);
-					const resolved = resolvedURL.pathname
+					const resolved = resolvedURL.pathname;
 					if (isBrowserPath(resolved)) {
 						return relativeToRoot(resolved + resolvedURL.search);
 					}
@@ -131,7 +131,9 @@ export default function astro({ config, logging }: AstroPluginOptions): vite.Plu
 						const src = hoistedScript.src!;
 						if (src.startsWith('/') && !isBrowserPath(src)) {
 							const publicDir = config.public.pathname.replace(/\/$/, '').split('/').pop() + '/';
-							throw new Error(`\n\n<script src="${src}"> references an asset in the "${publicDir}" directory. Please add the "is:inline" directive to keep this asset from being bundled.\n\nFile: ${filename}`)
+							throw new Error(
+								`\n\n<script src="${src}"> references an asset in the "${publicDir}" directory. Please add the "is:inline" directive to keep this asset from being bundled.\n\nFile: ${filename}`
+							);
 						}
 					}
 
