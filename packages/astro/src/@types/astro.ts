fd36a64da716aa2fe3b7579bf1d78cded2ec281f
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-03 11:24:35 +0200

    updating the default listed in config jsdocs

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -157,7 +157,7 @@ export interface AstroUserConfig {
 	 * @docs
 	 * @name publicDir
 	 * @type {string}
-	 * @default `"./publicDir"`
+	 * @default `"./public"`
 	 * @description
 	 * Set the directory for your static assets. Files in this directory are served at `/` during dev and copied to your build directory during build. These files are always served or copied as-is, without transform or bundling.
 	 *
