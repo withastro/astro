d69713a3c0c2b25bda7a6a4fdd0373b3dbe66c06
Fred K. Schott <fkschott@gmail.com>
2022-04-04 17:31:44 -0700

    fix markdown mode default

--- packages/astro/src/core/config.ts
+++ packages/astro/src/core/config.ts
@@ -146,7 +146,7 @@ export const AstroConfigSchema = z.object({
 			mode: z
 				.union([z.literal('md'), z.literal('mdx')])
 				.optional()
-				.default('md'),
+				.default('mdx'),
 			syntaxHighlight: z
 				.union([z.literal('shiki'), z.literal('prism'), z.literal(false)])
 				.optional()
