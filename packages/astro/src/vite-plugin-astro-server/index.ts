7ada6280f29bad3d755706301f0482e0f3cfcf37
matthewp <matthewp@users.noreply.github.com>
2022-04-07 19:18:48 +0000

    [ci] format

--- packages/astro/src/vite-plugin-astro-server/index.ts
+++ packages/astro/src/vite-plugin-astro-server/index.ts
@@ -49,15 +49,15 @@ async function writeWebResponse(res: http.ServerResponse, webResponse: Response)
 	const { status, headers, body } = webResponse;
 
 	let _headers = {};
-	if('raw' in headers) {
+	if ('raw' in headers) {
 		// Node fetch allows you to get the raw headers, which includes multiples of the same type.
 		// This is needed because Set-Cookie *must* be called for each cookie, and can't be
 		// concatenated together.
 		type HeadersWithRaw = Headers & {
-			raw: () => Record<string, string[]>
+			raw: () => Record<string, string[]>;
 		};
 
-		for(const [key, value] of Object.entries((headers as HeadersWithRaw).raw())) {
+		for (const [key, value] of Object.entries((headers as HeadersWithRaw).raw())) {
 			res.setHeader(key, value);
 		}
 	} else {
