f6ba53dcb107590943e31e8a05f72c47ad610115
Fred K. Schott <fkschott@gmail.com>
2022-04-10 20:32:49 -0700

    remove unused renderers

--- packages/renderers/renderer-preact/client.js
+++
@@ -1,8 +0,0 @@
-import { h, render } from 'preact';
-import StaticHtml from './static-html.js';
-
-export default (element) => (Component, props, children) =>
-	render(
-		h(Component, props, children != null ? h(StaticHtml, { value: children }) : children),
-		element
-	);
