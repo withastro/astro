b3a423849bc380ff1d21a7c1198f87a4a8d0d036
natemoo-re <natemoo-re@users.noreply.github.com>
2022-04-19 18:38:14 +0000

    [ci] format

--- packages/integrations/solid/client.js
+++ packages/integrations/solid/client.js
@@ -4,7 +4,7 @@ import { hydrate, createComponent } from 'solid-js/web';
 export default (element) => (Component, props, childHTML) => {
 	// Prepare global object expected by Solid's hydration logic
 	if (!window._$HY) {
-		window._$HY = { events: [], completed: new WeakSet, r: {} };
+		window._$HY = { events: [], completed: new WeakSet(), r: {} };
 	}
 	// Perform actual hydration
 	let children;
