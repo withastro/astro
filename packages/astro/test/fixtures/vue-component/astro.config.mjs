3a554737f353d2a36c5bd23ba124f4106ec4bfdb
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-19 10:52:56 +0200

    adding a tests for custom vue compiler options

--- packages/astro/test/fixtures/vue-component/astro.config.mjs
+++ packages/astro/test/fixtures/vue-component/astro.config.mjs
@@ -3,5 +3,11 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue()],
+	integrations: [vue({
+        template: {
+			compilerOptions: {
+				isCustomElement: tag => tag.includes('my-button')
+			}
+        }
+	})],
 });
