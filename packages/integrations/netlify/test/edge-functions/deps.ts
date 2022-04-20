ae97fef0199aa1cb867f7e7d6f9c6f7fa4504fc0
Matthew Phillips <matthew@skypack.dev>
2022-04-20 17:18:20 -0400

    Revert "Revert "Fixes using React.lazy and Suspense""
    This reverts commit e621c2f7d3844e950168f4198e4dd1c6f43031d0.

--- packages/integrations/netlify/test/edge-functions/deps.ts
+++ packages/integrations/netlify/test/edge-functions/deps.ts
@@ -1,3 +1,4 @@
 // @ts-nocheck
 export { fromFileUrl } from 'https://deno.land/std@0.110.0/path/mod.ts';
 export { assertEquals, assert } from 'https://deno.land/std@0.132.0/testing/asserts.ts';
+export * from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
