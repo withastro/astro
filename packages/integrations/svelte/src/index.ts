222eb9ca7ba7e8dbde1e24c69919169312d2ef4f
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-20 13:33:00 +0200

    updating svelte integration to allow custom user config

--- packages/integrations/svelte/src/index.ts
+++ packages/integrations/svelte/src/index.ts
@@ -1,6 +1,7 @@
 import type { AstroIntegration, AstroRenderer } from 'astro';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import preprocess from 'svelte-preprocess';
+import type { Options } from '@sveltejs/vite-plugin-svelte';
 
 function getRenderer(): AstroRenderer {
 	return {
@@ -10,38 +11,61 @@ function getRenderer(): AstroRenderer {
 	};
 }
 
-function getViteConfiguration(isDev: boolean) {
+function getViteConfiguration(isDev: boolean, options?: Options | OptionsCallback) {
+	const defaultOptions = {
+		emitCss: true,
+		compilerOptions: { dev: isDev, hydratable: true },
+		preprocess: [
+			preprocess({
+				less: true,
+				sass: { renderSync: true },
+				scss: { renderSync: true },
+				stylus: true,
+				typescript: true,
+			}),
+		],
+	};
+
+	let resolvedOptions: Partial<Options>;
+
+	if (!options) {
+		resolvedOptions = defaultOptions;
+	} else if (typeof options === 'function') {
+		resolvedOptions = options(defaultOptions);
+	} else {
+		resolvedOptions = {
+			...options,
+			...defaultOptions,
+			compilerOptions: {
+				...options.compilerOptions,
+				// Always use dev and hydratable from defaults
+				...defaultOptions.compilerOptions,
+			},
+			// Ignore default preprocessor if the user provided their own
+			preprocess: options.preprocess ?? defaultOptions.preprocess,
+		};
+	}
+
 	return {
 		optimizeDeps: {
 			include: ['@astrojs/svelte/client.js', 'svelte', 'svelte/internal'],
 			exclude: ['@astrojs/svelte/server.js'],
 		},
 		plugins: [
-			svelte({
-				emitCss: true,
-				compilerOptions: { dev: isDev, hydratable: true },
-				preprocess: [
-					preprocess({
-						less: true,
-						sass: { renderSync: true },
-						scss: { renderSync: true },
-						stylus: true,
-						typescript: true,
-					}),
-				],
-			}),
+			svelte(resolvedOptions),
 		],
 	};
 }
 
-export default function (): AstroIntegration {
+type OptionsCallback = (defaultOptions: Options) => Options;
+export default function (options?: Options | OptionsCallback): AstroIntegration {
 	return {
 		name: '@astrojs/svelte',
 		hooks: {
 			// Anything that gets returned here is merged into Astro Config
 			'astro:config:setup': ({ command, updateConfig, addRenderer }) => {
 				addRenderer(getRenderer());
-				updateConfig({ vite: getViteConfiguration(command === 'dev') });
+				updateConfig({ vite: getViteConfiguration(command === 'dev', options) });
 			},
 		},
 	};
