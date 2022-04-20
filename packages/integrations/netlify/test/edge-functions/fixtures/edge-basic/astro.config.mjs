62e93ecb1417812c50f4c89203cb040a186face3
Matthew Phillips <matthew@skypack.dev>
2022-04-20 17:17:36 -0400

    Revert "Fixes using React.lazy and Suspense"
    This reverts commit 0e045ee09f217c8931f7071ae0a78cd61d106433.

--- packages/integrations/netlify/test/edge-functions/fixtures/edge-basic/astro.config.mjs
+++ packages/integrations/netlify/test/edge-functions/fixtures/edge-basic/astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import { netlifyEdgeFunctions } from '@astrojs/netlify';
-import react from "@astrojs/react";
 
 export default defineConfig({
 	adapter: netlifyEdgeFunctions({
 		dist: new URL('./dist/', import.meta.url),
 	}),
-	integrations: [react()],
 	experimental: {
 		ssr: true
 	}
