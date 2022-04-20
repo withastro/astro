12f6b6099810f3f56e5ca956c84216725a6d777c
Matthew Phillips <matthew@skypack.dev>
2022-04-20 14:16:04 -0400

    Add test to verify Lit works in SSR (#3158)

---
+++ packages/astro/test/ssr-lit.test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { load as cheerioLoad } from 'cheerio';
+import { loadFixture } from './test-utils.js';
+import testAdapter from './test-adapter.js';
+
+describe('Lit integration in SSR', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/lit-element/',
+			experimental: {
+				ssr: true,
+			},
+			adapter: testAdapter(),
+		});
+		await fixture.build();
+	});
+
+	async function fetchHTML(path) {
+		const app = await fixture.loadTestAdapterApp();
+		const request = new Request('http://example.com' + path);
+		const response = await app.render(request);
+		const html = await response.text();
+		return html;
+	}
+
+	it('Is able to load', async () => {
+		const html = await fetchHTML('/');
+		const $ = cheerioLoad(html);
+		expect($('#win').text()).to.equal('function');
+	});
+});
