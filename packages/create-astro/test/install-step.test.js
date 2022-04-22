621a9f1476786710f4e43492a8e73bd3d04c96f9
bholmesdev <hey@bholmes.dev>
2022-04-22 17:54:57 -0400

    chore: add install step tests

---
+++ packages/create-astro/test/install-step.test.js
@@ -0,0 +1,60 @@
+import { setup, promiseWithTimeout, timeout, PROMPT_MESSAGES } from './utils.js';
+import { sep } from 'path';
+import fs from 'fs';
+import os from 'os';
+
+const FAKE_PACKAGE_MANAGER = 'banana';
+
+describe('[create-astro] install', function () {
+	this.timeout(timeout);
+	let tempDir = '';
+	beforeEach(async () => {
+		tempDir = await fs.promises.mkdtemp(`${os.tmpdir()}${sep}`);
+	});
+	process.env.npm_config_user_agent = FAKE_PACKAGE_MANAGER;
+	it('should respect package manager in prompt', function() {
+		const { stdout, stdin } = setup([tempDir]);
+		return promiseWithTimeout((resolve) => {
+			const seen = new Set();
+			stdout.on('data', (chunk) => {
+				if (!seen.has(PROMPT_MESSAGES.template) && chunk.includes(PROMPT_MESSAGES.template)) {
+					seen.add(PROMPT_MESSAGES.template);
+					stdin.write('\x0D');
+				}
+				if (!seen.has(PROMPT_MESSAGES.frameworks) && chunk.includes(PROMPT_MESSAGES.frameworks)) {
+					seen.add(PROMPT_MESSAGES.frameworks);
+					stdin.write('\x0D');
+				}
+				if (chunk.includes(PROMPT_MESSAGES.install(FAKE_PACKAGE_MANAGER))) {
+					resolve();
+				}
+			});
+		});
+	});
+
+	it('should respect package manager in next steps', function() {
+		const { stdout, stdin } = setup([tempDir]);
+		return promiseWithTimeout((resolve) => {
+			const seen = new Set();
+			const installPrompt = PROMPT_MESSAGES.install(FAKE_PACKAGE_MANAGER);
+			stdout.on('data', (chunk) => {
+				if (!seen.has(PROMPT_MESSAGES.template) && chunk.includes(PROMPT_MESSAGES.template)) {
+					seen.add(PROMPT_MESSAGES.template);
+					stdin.write('\x0D');
+				}
+				if (!seen.has(PROMPT_MESSAGES.frameworks) && chunk.includes(PROMPT_MESSAGES.frameworks)) {
+					seen.add(PROMPT_MESSAGES.frameworks);
+					stdin.write('\x0D');
+				}
+				
+				if (!seen.has(installPrompt) && chunk.includes(installPrompt)) {
+					seen.add(installPrompt)
+					stdin.write('n\x0D');
+				}
+				if (chunk.includes('banana dev')) {
+					resolve();
+				}
+			});
+		});
+	});
+})
