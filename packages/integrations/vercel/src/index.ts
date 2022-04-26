a7a966e2cfe3081973251a2470c6e74b9b519998
JuanM04 <me@juanm04.com>
2022-04-26 20:13:28 -0300

    Added static mode

--- packages/integrations/vercel/src/index.ts
+++ packages/integrations/vercel/src/index.ts
@@ -9,35 +9,56 @@ import { getTransformedRoutes, Redirect, Rewrite } from '@vercel/routing-utils';
 const writeJson = (path: PathLike, data: any) =>
 	fs.writeFile(path, JSON.stringify(data), { encoding: 'utf-8' });
 
-export function getAdapter({ edge }: { edge: boolean }): AstroAdapter {
-	return {
-		name: '@astrojs/vercel',
-		serverEntrypoint: `@astrojs/vercel/${edge ? 'edge' : 'serverless'}`,
-		exports: ['default'],
-	};
-}
-
 export interface Options {
-	edge?: boolean;
+	/**
+	 * - `static`: generates a static website following Vercel's output formats, redirects, etc.
+	 * - `serverless`: SSR inside a [Node.js 14 function](https://vercel.com/docs/concepts/functions/serverless-functions).
+	 * - `edge`: SSR inside a [Edge function](https://vercel.com/docs/concepts/functions/edge-functions).
+	 *
+	 * @type {'static' | 'serverless' | 'edge'}
+	 * @default {'serverless'}
+	 */
+	mode?: 'static' | 'serverless' | 'edge';
 }
 
-export default function vercel({ edge = false }: Options = {}): AstroIntegration {
+function getAdapter({ mode }: { mode: NonNullable<Options['mode']> }): AstroAdapter {
+	let adapter: AstroAdapter = {
+		name: '@astrojs/vercel',
+	};
+
+	if (mode !== 'static') {
+		adapter.serverEntrypoint = `@astrojs/vercel/${mode}`;
+		adapter.exports = ['default'];
+	}
+
+	return adapter;
+}
+
+export default function vercel({ mode = 'serverless' }: Options = {}): AstroIntegration {
 	let _config: AstroConfig;
-	let _serverOut: URL;
+	let _vercelOut: URL;
+	let _functionsFolder: URL;
 	let _serverEntry: string;
 
 	return {
 		name: '@astrojs/vercel',
 		hooks: {
 			'astro:config:setup': ({ config }) => {
-				config.outDir = new URL('./.vercel/output/', config.root);
+				_vercelOut = new URL('./.vercel/output/', config.root);
+
+				if (mode === 'static') {
+					config.outDir = new URL('./static/', _vercelOut);
+					config.build.format = 'directory';
+				} else {
+					config.outDir = _vercelOut;
+				}
 			},
 			'astro:config:done': ({ setAdapter, config }) => {
-				setAdapter(getAdapter({ edge }));
+				setAdapter(getAdapter({ mode }));
 				_config = config;
 			},
 			'astro:build:setup': ({ vite, target }) => {
-				if (!edge && target === 'server') {
+				if (mode === 'serverless' && target === 'server') {
 					vite.build = {
 						...(vite.build || {}),
 						rollupOptions: {
@@ -51,44 +72,55 @@ export default function vercel({ edge = false }: Options = {}): AstroIntegration
 				}
 			},
 			'astro:build:start': async ({ buildConfig }) => {
-				buildConfig.serverEntry = _serverEntry = 'entry.js';
-				buildConfig.client = new URL('./static/', _config.outDir);
-				buildConfig.server = _serverOut = new URL('./functions/render.func/', _config.outDir);
-
 				if (String(process.env.ENABLE_VC_BUILD) !== '1') {
 					throw new Error(
 						`The enviroment variable "ENABLE_VC_BUILD" was not found. Make sure you have it set to "1" in your Vercel project.\nLearn how to set enviroment variables here: https://vercel.com/docs/concepts/projects/environment-variables`
 					);
 				}
+
+				if (mode === 'static') {
+					buildConfig.serverEntry = _serverEntry = 'entry.mjs';
+					buildConfig.staticMode = true;
+				} else {
+					buildConfig.serverEntry = _serverEntry = 'entry.js';
+					buildConfig.client = new URL('./static/', _vercelOut);
+					buildConfig.server = _functionsFolder = new URL('./functions/render.func/', _vercelOut);
+				}
 			},
 			'astro:build:done': async ({ routes }) => {
-				const entryPath = fileURLToPath(new URL(_serverEntry, _serverOut));
+				// Build function
+				if (mode !== 'static') {
+					const entryPath = fileURLToPath(new URL(_serverEntry, _functionsFolder));
+					const edge = mode === 'edge';
 
-				// Bundle dependencies
-				await esbuild.build({
-					entryPoints: [entryPath],
-					outfile: entryPath,
-					bundle: true,
-					target: 'node14',
-					allowOverwrite: true,
-					...(edge ? { format: 'esm', platform: 'browser' } : { format: 'cjs', platform: 'node' }),
-				});
+					// Bundle dependencies
+					await esbuild.build({
+						entryPoints: [entryPath],
+						outfile: entryPath,
+						bundle: true,
+						target: 'node14',
+						allowOverwrite: true,
+						...(edge
+							? { format: 'esm', platform: 'browser' }
+							: { format: 'cjs', platform: 'node' }),
+					});
 
-				if (edge) {
-					// Edge function config
-					// https://vercel.com/docs/build-output-api/v3#vercel-primitives/edge-functions/configuration
-					await writeJson(new URL(`./.vc-config.json`, _serverOut), {
-						runtime: 'edge',
-						entrypoint: _serverEntry,
-					});
-				} else {
-					// Serverless function config
-					// https://vercel.com/docs/build-output-api/v3#vercel-primitives/serverless-functions/configuration
-					await writeJson(new URL(`./.vc-config.json`, _serverOut), {
-						runtime: 'nodejs14.x',
-						handler: _serverEntry,
-						launcherType: 'Nodejs',
-					});
+					if (edge) {
+						// Edge function config
+						// https://vercel.com/docs/build-output-api/v3#vercel-primitives/edge-functions/configuration
+						await writeJson(new URL(`./.vc-config.json`, _functionsFolder), {
+							runtime: 'edge',
+							entrypoint: _serverEntry,
+						});
+					} else {
+						// Serverless function config
+						// https://vercel.com/docs/build-output-api/v3#vercel-primitives/serverless-functions/configuration
+						await writeJson(new URL(`./.vc-config.json`, _functionsFolder), {
+							runtime: 'nodejs14.x',
+							handler: _serverEntry,
+							launcherType: 'Nodejs',
+						});
+					}
 				}
 
 				let rewrites: Rewrite[] = [];
@@ -111,21 +143,26 @@ export default function vercel({ edge = false }: Options = {}): AstroIntegration
 							)
 							.join('/');
 
-					rewrites.push({
-						source: path,
-						destination: '/render',
-					});
-
-					if (_config.trailingSlash === 'always') {
-						redirects.push({
+					if (mode !== 'static') {
+						rewrites.push({
 							source: path,
-							destination: path + '/',
+							destination: '/render',
 						});
-					} else if (_config.trailingSlash === 'never') {
-						redirects.push({
-							source: path + '/',
-							destination: path,
-						});
+					}
+
+					// trailingSlash for pages
+					if (route.type === 'page' && route.segments.length > 0) {
+						if (_config.trailingSlash === 'always') {
+							redirects.push({
+								source: path,
+								destination: path + '/',
+							});
+						} else if (_config.trailingSlash === 'never') {
+							redirects.push({
+								source: path + '/',
+								destination: path,
+							});
+						}
 					}
 				}
 
@@ -139,7 +176,7 @@ export default function vercel({ edge = false }: Options = {}): AstroIntegration
 
 				// Output configuration
 				// https://vercel.com/docs/build-output-api/v3#build-output-configuration
-				await writeJson(new URL(`./config.json`, _config.outDir), {
+				await writeJson(new URL(`./config.json`, _vercelOut), {
 					version: 3,
 					routes: transformedRoutes.routes,
 				});
