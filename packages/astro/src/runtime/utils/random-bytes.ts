cbd9e6222e0318f53df180a4eece747502921d7a
okikio <okikio@users.noreply.github.com>
2022-04-26 21:53:43 +0000

    [ci] format

--- packages/astro/src/runtime/utils/random-bytes.ts
+++ packages/astro/src/runtime/utils/random-bytes.ts
@@ -12,36 +12,40 @@ const MAX_UINT32 = 4294967295;
 const GlobalCrypto = globalThis.crypto as Crypto;
 
 export function randomBytes(size: number, cb?: (...args: any) => any) {
-    if (!(GlobalCrypto && GlobalCrypto.getRandomValues)) {
-        throw new Error('Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
-    }
+	if (!(GlobalCrypto && GlobalCrypto.getRandomValues)) {
+		throw new Error(
+			'Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11'
+		);
+	}
 
-    // phantomjs needs to throw
-    if (size > MAX_UINT32) throw new RangeError('requested too many random bytes');
+	// phantomjs needs to throw
+	if (size > MAX_UINT32) throw new RangeError('requested too many random bytes');
 
-    let bytes = new Uint32Array(size)
+	let bytes = new Uint32Array(size);
 
-    if (size > 0) {  // getRandomValues fails on IE if size == 0
-        if (size > MAX_BYTES) { // this is the max bytes crypto.getRandomValues
-            // can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
-            for (let generated = 0; generated < size; generated += MAX_BYTES) {
-                // buffer.slice automatically checks if the end is past the end of
-                // the buffer so we don't have to here
-                GlobalCrypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES));
-            }
-        } else {
-            GlobalCrypto.getRandomValues(bytes);
-        }
-    }
+	if (size > 0) {
+		// getRandomValues fails on IE if size == 0
+		if (size > MAX_BYTES) {
+			// this is the max bytes crypto.getRandomValues
+			// can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
+			for (let generated = 0; generated < size; generated += MAX_BYTES) {
+				// buffer.slice automatically checks if the end is past the end of
+				// the buffer so we don't have to here
+				GlobalCrypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES));
+			}
+		} else {
+			GlobalCrypto.getRandomValues(bytes);
+		}
+	}
 
-    if (typeof cb === 'function') {
-        Promise.resolve().then(() => {
-            return cb(null, bytes);
-        });
-        return;
-    }
+	if (typeof cb === 'function') {
+		Promise.resolve().then(() => {
+			return cb(null, bytes);
+		});
+		return;
+	}
 
-    return bytes;
+	return bytes;
 }
 
 export default randomBytes;
