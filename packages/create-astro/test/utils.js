e2f7ef8b44dafaef8f33815dcb1c3d54ea71f0c0
bholmesdev <bholmesdev@users.noreply.github.com>
2022-04-28 00:59:36 +0000

    [ci] format

--- packages/create-astro/test/utils.js
+++ packages/create-astro/test/utils.js
@@ -27,7 +27,8 @@ export const PROMPT_MESSAGES = {
 	directory: 'Where would you like to create your app?',
 	template: 'Which app template would you like to use?',
 	install: (pkgManager) => `Would you like us to run "${pkgManager} install?"`,
-	astroAdd: (astroAddCommand = 'npx astro@latest add --yes') => `Run "${astroAddCommand}?" This lets you optionally add component frameworks (ex. React), CSS frameworks (ex. Tailwind), and more.`,
+	astroAdd: (astroAddCommand = 'npx astro@latest add --yes') =>
+		`Run "${astroAddCommand}?" This lets you optionally add component frameworks (ex. React), CSS frameworks (ex. Tailwind), and more.`,
 };
 
 export function setup(args = []) {
