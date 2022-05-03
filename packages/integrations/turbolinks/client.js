7e7b4ee5898419746ed127b6ef01e96029eea8d6
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-05-03 16:50:13 -0500

    client hydration scripts should be removed before navigation

--- packages/integrations/turbolinks/client.js
+++ packages/integrations/turbolinks/client.js
@@ -1,2 +1,10 @@
 import Turbolinks from 'turbolinks';
 export { Turbolinks };
+
+// Before every page navigation, remove any previously added component hydration scripts
+document.addEventListener("turbolinks:before-render", function() {
+	const scripts = document.querySelectorAll("script[data-astro-component-hydration]");
+    for(const script of scripts) {
+        script.remove();
+    }
+})
