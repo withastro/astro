b39d03bc1432e1a7db06bdcce82d6faeb17d10bc
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-05-04 09:57:07 -0500

    reverting Astro global test to verify existing behavior

--- packages/astro/test/astro-global.test.js
+++ packages/astro/test/astro-global.test.js
@@ -26,10 +26,10 @@ describe('Astro.*', () => {
 		});
 
 		it('Astro.request.url', async () => {
-			expect($('#pathname').text()).to.equal('/');
+			expect($('#pathname').text()).to.equal('/blog/');
 			expect($('#searchparams').text()).to.equal('{}');
-			expect($('#child-pathname').text()).to.equal('/');
-			expect($('#nested-child-pathname').text()).to.equal('/');
+			expect($('#child-pathname').text()).to.equal('/blog/');
+			expect($('#nested-child-pathname').text()).to.equal('/blog/');
 		});
 	});
 
