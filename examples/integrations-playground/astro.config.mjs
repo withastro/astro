0df3de68a5c885493ee110c754cad0424fbe320b
Jonas Luebbers <jonaskluebbers@gmail.com>
2022-05-05 23:58:15 -0400

    Add solid to integrations-playground example

--- examples/integrations-playground/astro.config.mjs
+++ examples/integrations-playground/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import lit from '@astrojs/lit';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import turbolinks from '@astrojs/turbolinks';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
+import solid from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [lit(), react(), tailwind(), turbolinks(), partytown(), sitemap()],
+  integrations: [lit(), react(), tailwind(), turbolinks(), partytown(), sitemap(), solid()]
 });
