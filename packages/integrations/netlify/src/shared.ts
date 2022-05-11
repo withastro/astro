02cda86786834d068bdf6afa27ef8294fb28052b
thepassle <pascalschilp@gmail.com>
2022-05-11 10:32:56 +0200

    fix: create redirects file for netlify edge adapter

---
+++ packages/integrations/netlify/src/shared.ts
@@ -0,0 +1,29 @@
+import type { RouteData } from 'astro';
+import fs from 'fs';
+
+export async function createRedirects(
+  routes: RouteData[], 
+  dir: URL, 
+  entryFile: string
+  ) {
+  const _redirectsURL = new URL('./_redirects', dir);
+
+  // Create the redirects file that is used for routing.
+  let _redirects = '';
+  for (const route of routes) {
+    if (route.pathname) {
+      _redirects += `
+  ${route.pathname}    /.netlify/functions/${entryFile}    200`;
+    } else {
+      const pattern =
+        '/' + route.segments.map(([part]) => (part.dynamic ? '*' : part.content)).join('/');
+      _redirects += `
+  ${pattern}    /.netlify/functions/${entryFile}    200`;
+    }
+  }
+
+  // Always use appendFile() because the redirects file could already exist,
+  // e.g. due to a `/public/_redirects` file that got copied to the output dir.
+  // If the file does not exist yet, appendFile() automatically creates it.
+  await fs.promises.appendFile(_redirectsURL, _redirects, 'utf-8');
+}
