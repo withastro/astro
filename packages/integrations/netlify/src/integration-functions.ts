c00b2c750f77b888465949070e019d5e273308c9
Pascal Schilp <pascalschilp@gmail.com>
2022-05-11 17:13:23 +0200

    Fix/netlify edge redirects (#3342)
    * fix: create redirects file for netlify edge adapter
    
    * chore: changeset
    
    * chore: distinguish between edge/regular

--- packages/integrations/netlify/src/integration-functions.ts
+++ packages/integrations/netlify/src/integration-functions.ts
@@ -1,5 +1,5 @@
 import type { AstroAdapter, AstroIntegration, AstroConfig } from 'astro';
-import fs from 'fs';
+import { createRedirects } from './shared.js';
 
 export function getAdapter(): AstroAdapter {
 	return {
@@ -37,26 +37,7 @@ function netlifyFunctions({ dist }: NetlifyFunctionsOptions = {}): AstroIntegrat
 				buildConfig.server = new URL('./functions/', _config.outDir);
 			},
 			'astro:build:done': async ({ routes, dir }) => {
-				const _redirectsURL = new URL('./_redirects', dir);
-
-				// Create the redirects file that is used for routing.
-				let _redirects = '';
-				for (const route of routes) {
-					if (route.pathname) {
-						_redirects += `
-${route.pathname}    /.netlify/functions/${entryFile}    200`;
-					} else {
-						const pattern =
-							'/' + route.segments.map(([part]) => (part.dynamic ? '*' : part.content)).join('/');
-						_redirects += `
-${pattern}    /.netlify/functions/${entryFile}    200`;
-					}
-				}
-
-				// Always use appendFile() because the redirects file could already exist,
-				// e.g. due to a `/public/_redirects` file that got copied to the output dir.
-				// If the file does not exist yet, appendFile() automatically creates it.
-				await fs.promises.appendFile(_redirectsURL, _redirects, 'utf-8');
+				await createRedirects(routes, dir, entryFile, false);			
 			},
 		},
 	};
