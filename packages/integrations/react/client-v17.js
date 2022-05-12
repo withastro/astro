8666f22a0f845973de28cddd358861f65981f168
matthewp <matthewp@users.noreply.github.com>
2022-05-12 16:06:40 +0000

    [ci] format

--- packages/integrations/react/client-v17.js
+++ packages/integrations/react/client-v17.js
@@ -2,14 +2,12 @@ import { createElement } from 'react';
 import { render, hydrate } from 'react-dom';
 import StaticHtml from './static-html.js';
 
-export default (element) => (Component, props, children, { client }) =>
-	{
+export default (element) =>
+	(Component, props, children, { client }) => {
 		const componentEl = createElement(
 			Component,
 			props,
-			children != null
-				? createElement(StaticHtml, { value: children })
-				: children
+			children != null ? createElement(StaticHtml, { value: children }) : children
 		);
 		if (client === 'only') {
 			return render(componentEl, element);
--- packages/integrations/react/client.js
+++ packages/integrations/react/client.js
@@ -2,14 +2,12 @@ import { createElement } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
 import StaticHtml from './static-html.js';
 
-export default (element) => (Component, props, children, { client }) =>
-	{
+export default (element) =>
+	(Component, props, children, { client }) => {
 		const componentEl = createElement(
 			Component,
 			props,
-			children != null
-				? createElement(StaticHtml, { value: children })
-				: children
+			children != null ? createElement(StaticHtml, { value: children }) : children
 		);
 		if (client === 'only') {
 			return createRoot(element).render(componentEl);
