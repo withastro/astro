0259d7658be82a4a5e09fb703498571d958a0569
Matthew Phillips <matthew@skypack.dev>
2022-05-16 14:44:15 -0400

    Fix for loading markdown pages with spaces in the name (#3379)
    * Fix for loading markdown pages with spaces in the name
    
    * Adds a changeset

--- packages/astro/test/astro-pages.test.js
+++ packages/astro/test/astro-pages.test.js
@@ -1,19 +1,46 @@
 import { expect } from 'chai';
 import * as cheerio from 'cheerio';
-import { loadFixture } from './test-utils.js';
+import { loadFixture, isWindows} from './test-utils.js';
 
 describe('Pages', () => {
 	let fixture;
 
 	before(async () => {
-		fixture = await loadFixture({ root: './fixtures/astro-pages/' });
+		fixture = await loadFixture({ root: './fixtures/astro pages/' });
 		await fixture.build();
 	});
 
-	it('Can find page with "index" at the end file name', async () => {
-		const html = await fixture.readFile('/posts/name-with-index/index.html');
-		const $ = cheerio.load(html);
+	describe('Build', () => {
+		before(async () => {
+			await fixture.build();
+		});
 
-		expect($('h1').text()).to.equal('Name with index');
+		it('Can find page with "index" at the end file name', async () => {
+			const html = await fixture.readFile('/posts/name-with-index/index.html');
+			const $ = cheerio.load(html);
+
+			expect($('h1').text()).to.equal('Name with index');
+		});
+	});
+
+	if(isWindows) return;
+
+	describe('Development', () => {
+		let devServer;
+
+		before(async () => {
+			devServer = await fixture.startDevServer();
+		});
+
+		after(async () => {
+			await devServer.stop();
+		});
+
+		it('Is able to load md pages', async () => {
+			const html = await fixture.fetch('/').then((res) => res.text());
+			const $ = cheerio.load(html);
+
+			expect($('#testing').length).to.be.greaterThan(0);
+		});
 	});
 });
