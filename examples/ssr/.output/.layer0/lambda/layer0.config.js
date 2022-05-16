6177cc072ea67a4605cd85d554057633bfa9c76b
Tristan Lee <tristanlee85@gmail.com>
2022-05-16 17:10:25 -0400

    Ignore example output

--- examples/ssr/.output/.layer0/lambda/layer0.config.js
+++
@@ -1,2 +0,0 @@
-module.exports={connector:"./layer0",name:"astro-app",routes:"routes.js",backends:{},includeFiles:{"public/**/*":!0,"server/**/*":!0}};
-//# sourceMappingURL=layer0.config.js.map
--- examples/ssr/.output/layer0/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function prod(port) {
-	process.env.PORT = port.toString()
-  await import('../server/entry.mjs')
-}
