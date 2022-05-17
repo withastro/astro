10a0617601224cdaf406ed2c18d1277e67214de8
Nate Moore <nate@skypack.dev>
2022-05-17 15:25:15 -0500

    fix(#3309): use system default locale

---
+++ .changeset/thirty-drinks-shout.md
@@ -0,0 +1,5 @@
+---
+'astro': patch
+---
+
+Fix [#3309](https://github.com/withastro/astro/issues/3309) default logger locale behavior.
--- packages/astro/src/core/logger/core.ts
+++ packages/astro/src/core/logger/core.ts
@@ -14,18 +14,16 @@ export interface LogOptions {
 	level: LoggerLevel;
 }
 
-function getLoggerLocale(): string {
-	const defaultLocale = 'en-US';
-	if (process.env.LANG) {
-		const extractedLocale = process.env.LANG.split('.')[0].replace(/_/g, '-');
-		// Check if language code is atleast two characters long (ie. en, es).
-		// NOTE: if "c" locale is encountered, the default locale will be returned.
-		if (extractedLocale.length < 2) return defaultLocale;
-		else return extractedLocale.substring(0, 5);
-	} else return defaultLocale;
-}
-
-export const dateTimeFormat = new Intl.DateTimeFormat(getLoggerLocale(), {
+// Hey, locales are pretty complicated! Be careful modifying this logic...
+// If we throw at the top-level, international users can't use Astro.
+// 
+// Using `[]` sets the default locale properly from the system!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#parameters
+// 
+// Here be the dragons we've slain:
+// https://github.com/withastro/astro/issues/2625
+// https://github.com/withastro/astro/issues/3309
+export const dateTimeFormat = new Intl.DateTimeFormat([], {
 	hour: '2-digit',
 	minute: '2-digit',
 	second: '2-digit',
