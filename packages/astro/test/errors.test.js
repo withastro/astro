916888f2fb3ccdebe8f22bc91c8256071fdfd3f8
Matthew Phillips <matthew@skypack.dev>
2022-05-18 09:12:59 -0400

    await the change, and then a little bit

--- packages/astro/test/errors.test.js
+++ packages/astro/test/errors.test.js
@@ -54,8 +54,9 @@ describe('Error display', () => {
 			// 2. Edit the file, fixing the error
 			let changeOccured = fixture.onNextChange();
 			await fixture.editFile('./src/components/SvelteSyntaxError.svelte', `<h1>No mismatch</h1>`);
+			await changeOccured;
 			// Add a short wait so that Vite has a change to update its internals before we try to fetch a new page.
-			await new Promise(resolve => setTimeout(resolve, 5000));
+			await new Promise(resolve => setTimeout(resolve, 1000));
 
 			// 3. Verify that the file is fixed.
 			html = await fixture.fetch('/svelte-syntax-error').then((res) => res.text());
