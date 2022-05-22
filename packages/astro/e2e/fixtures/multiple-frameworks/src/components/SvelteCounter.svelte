a0c9cb5a6e86d3661ac82658297a66a1748b1af8
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-05-22 11:28:38 -0500

    adding a test suite for the framework-multiple example app

---
+++ packages/astro/e2e/fixtures/multiple-frameworks/src/components/SvelteCounter.svelte
@@ -0,0 +1,29 @@
+
+<script>
+	export let id;
+  let children;
+  let count = 0;
+
+  function add() {
+		count += 1;
+	}
+
+  function subtract() {
+		count -= 1;
+	}
+</script>
+
+<div {id} class="counter">
+  <button class="decrement" on:click={subtract}>-</button>
+  <pre>{ count }</pre>
+  <button class="increment" on:click={add}>+</button>
+</div>
+<div class="counter-message">
+  <slot />
+</div>
+
+<style>
+	.counter {
+		background: white;
+	}
+</style>
