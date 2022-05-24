4424b6183b417f5d33c64263d69d1d4be87e7c5b
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-05-24 19:55:47 +0200

    chore: prepare to use `distURL`

--- packages/integrations/pwa/src/index.ts
+++ packages/integrations/pwa/src/index.ts
@@ -1,20 +1,19 @@
 import { type VitePluginPWAAPI, VitePWA, type VitePWAOptions } from 'vite-plugin-pwa';
-import type { AstroIntegration, AstroConfig, RouteData } from 'astro';
+import type { AstroConfig, AstroIntegration, RouteData } from 'astro';
 import type { Plugin } from 'vite';
 import crypto from 'crypto';
 import fs from 'fs';
-import { resolve as resolveFs } from 'path';
 import type { ManifestEntry } from 'workbox-build';
-import { fileURLToPath, pathToFileURL } from 'url';
+import path from 'path';
+
 
 function buildManifestEntry(
-	publicDir: string,
 	url: string,
 	path: string,
 ): Promise<ManifestEntry> {
 	return new Promise((resolve, reject) => {
 		const cHash = crypto.createHash('MD5');
-		const stream = fs.createReadStream(resolveFs(publicDir, path));
+		const stream = fs.createReadStream(path);
 		stream.on('error', (err) => {
 			reject(err)
 		});
@@ -73,31 +72,38 @@ function patchWindowsImportPath(path: string) {
 		return path
 }
 
-async function regeneratePWA(dir: URL, routes: RouteData[], pwaPlugin: Plugin | undefined) {
+async function regeneratePWA(
+	dir: URL, 
+	routes: RouteData[],
+	pwaPlugin: Plugin | undefined
+) {
 	const api: VitePluginPWAAPI | undefined = pwaPlugin?.api
-	// todo@userquin: wait to fix it on pwa plugin
-	// if (routes && api && !api.isDisabled()) {
-	if (routes && api) {
-		// todo@userquin: this is an initial version, should change all the logic
-		// disambiguate the `<UNIT>:/` on windows: see nodejs/node#31710
-		let distFolder = patchWindowsImportPath(dir.href)
-		if (process.platform === 'win32' && distFolder.startsWith('/'))
-			distFolder = fileURLToPath(pathToFileURL(distFolder.slice(1)).href)
-		// eslint-disable-next-line no-console
-		console.log(distFolder)
-		const addRoutes = await Promise.all(routes.filter(r => r.type === 'page').map((r) => {
-			let path = r.component.slice(9, r.component.lastIndexOf('.'))
-			// eslint-disable-next-line no-console
-			console.log(path)
-			path = path === '/index' ? '/index.html' : (path === r.pathname ? `${path}/index.html` : `${path}/`)
-			// eslint-disable-next-line no-console
-			console.log(`${path} => ${r.pathname}`)
-			return buildManifestEntry(distFolder, r.pathname!, path.slice(1))
+	if (routes && api && !api.disabled) {
+		// todo@userquin: rn we only add the static pages
+		// // disambiguate the `<UNIT>:/` on windows: see nodejs/node#31710
+		// let distFolder = patchWindowsImportPath(dir.href)
+		// if (process.platform === 'win32' && distFolder.startsWith('/'))
+		// 	distFolder = fileURLToPath(pathToFileURL(distFolder.slice(1)).href)
+
+/*
+		const addRoutes = await Promise.all(routes.filter(r => r.type === 'page' && r.pathname && r.distURL).map((r) => {
+			let path = patchWindowsImportPath(r.distURL!.href)
+			if (process.platform === 'win32' && path.startsWith('/'))
+				path = fileURLToPath(pathToFileURL(path.slice(1)).href)
+			return buildManifestEntry(r.pathname!, path)
+			// let path = getOutputFilename(config, r.pathname!)
+			// if (path.startsWith('/'))
+			// 	path = path.slice(1)
+			//
+			// console.log(`${r.pathname!} => ${path} => ${resolveFs(distFolder, path)} => ${r.params}`)
+			// return buildManifestEntry(r.pathname!, resolveFs(distFolder, path))
 		}))
+		console.log(addRoutes)
 		api.extendManifestEntries((manifestEntries) => {
 			manifestEntries.push(...addRoutes)
 			return manifestEntries
 		})
+*/
 		// generate the manifest.webmanifest file
 		api.generateBundle()
 		// regenerate the sw
