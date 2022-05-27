95e560ec41359cfd2b10d8ea80bbd15993e5e7c9
Nate Moore <nate@skypack.dev>
2022-05-27 14:14:28 -0500

    fix(preact): ignore hydrate roots

--- packages/integrations/preact/client.js
+++ packages/integrations/preact/client.js
@@ -1,8 +1,10 @@
 import { h, render } from 'preact';
 import StaticHtml from './static-html.js';
 
-export default (element) => (Component, props, children) =>
+export default (element) => (Component, props, children) => {
+	if (!element.hasAttribute('ssr')) return;
 	render(
 		h(Component, props, children != null ? h(StaticHtml, { value: children }) : children),
 		element
 	);
+}
