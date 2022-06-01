cfd051d77d6400b96d07164b4979795d9627f04d
thepassle <pascalschilp@gmail.com>
2022-06-01 17:38:38 +0200

    feat: add route collision detection and error message

--- packages/astro/src/core/routing/manifest/create.ts
+++ packages/astro/src/core/routing/manifest/create.ts
@@ -267,41 +267,46 @@ export function createRouteManifest(
 
 	const pages = resolvePages(config);
 
-  config?._ctx?.injectedRoutes?.forEach(({pattern: name, entryPoint}) => {
-    const resolved = require.resolve(entryPoint);
-    const component = slash(path.relative(cwd || fileURLToPath(config.root), resolved));
-
-    const isDynamic = (str: string) => str?.[0] === '[';
-    const normalize = (str: string) => str?.substring(1, str?.length - 1);
-
-    const segments = name.split(path.sep)
-      .filter(Boolean)
-      .map((s: string) => {
-        validateSegment(s);
-
-        const dynamic = isDynamic(s);
-        const content = dynamic ? normalize(s) : s;
-        return [{
-          content,
-          dynamic,
-          spread: isSpread(s)
-        }]
-      });
-
-    const type = entryPoint.endsWith('.astro') ? 'page' : 'endpoint';
-    const isPage = type === 'page';
-    const trailingSlash = isPage ? config.trailingSlash : "never";
-
-    const pattern = getPattern(segments, trailingSlash);
-    const generate = getRouteGenerator(segments, trailingSlash);
-    const pathname = segments.every((segment) => segment.length === 1 && !segment[0].dynamic) ? `/${segments.map((segment) => segment[0].content).join("/")}` : null;
-    const params = segments.flat().filter((p) => p.dynamic).map((p) => p.content);
-
-    routes.push({type, pattern, segments, params, component, generate, pathname: pathname || void 0})
-  });
-
 	if (fs.existsSync(pages)) {
 		walk(fileURLToPath(pages), [], []);
+
+		config?._ctx?.injectedRoutes?.forEach(({pattern: name, entryPoint}) => {
+			const resolved = require.resolve(entryPoint);
+			const component = slash(path.relative(cwd || fileURLToPath(config.root), resolved));
+	
+			const isDynamic = (str: string) => str?.[0] === '[';
+			const normalize = (str: string) => str?.substring(1, str?.length - 1);
+	
+			const segments = name.split(path.sep)
+				.filter(Boolean)
+				.map((s: string) => {
+					validateSegment(s);
+	
+					const dynamic = isDynamic(s);
+					const content = dynamic ? normalize(s) : s;
+					return [{
+						content,
+						dynamic,
+						spread: isSpread(s)
+					}]
+				});
+	
+			const type = entryPoint.endsWith('.astro') ? 'page' : 'endpoint';
+			const isPage = type === 'page';
+			const trailingSlash = isPage ? config.trailingSlash : "never";
+	
+			const pattern = getPattern(segments, trailingSlash);
+			const generate = getRouteGenerator(segments, trailingSlash);
+			const pathname = segments.every((segment) => segment.length === 1 && !segment[0].dynamic) ? `/${segments.map((segment) => segment[0].content).join("/")}` : null;
+			const params = segments.flat().filter((p) => p.dynamic).map((p) => p.content);
+			
+			const collision = routes.find(route => route.pathname === pathname);
+      if(collision) {
+        throw new Error(`An integration attempted to inject a route that is already used in your project: "${pathname}" at "${component}". \nThis route collides with: "${collision.component}".`);
+      }
+
+			routes.push({type, pattern, segments, params, component, generate, pathname: pathname || void 0})
+		});
 	} else {
 		const pagesDirRootRelative = pages.href.slice(config.root.href.length);
 
