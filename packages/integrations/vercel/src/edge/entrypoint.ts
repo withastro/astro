baaedf3e2fe44c402fc17ac233f32a87d2cb36d0
Fred K. Schott <fkschott@gmail.com>
2022-06-01 16:18:21 -0700

    add import formatting

--- packages/integrations/vercel/src/edge/entrypoint.ts
+++ packages/integrations/vercel/src/edge/entrypoint.ts
@@ -1,5 +1,9 @@
+// NOTE(fks): Side-effect -- shim.js must run first. This isn't guaranteed by
+// the language, but it is a Node.js behavior that we rely on here. Keep this
+// separate from the other imports so that it doesn't get organized & reordered.
 import './shim.js';
 
+// Normal Imports
 import type { SSRManifest } from 'astro';
 import { App } from 'astro/app';
 
