9fee2fa559c804d680005680fe2bb091f7b00fef
Matthew Phillips <matthew@skypack.dev>
2022-06-03 13:38:05 -0400

    Fix use of dynamic imports / Astro.glob with Deno Deploy

--- packages/integrations/deno/test/deps.js
+++ packages/integrations/deno/test/deps.js
@@ -1,3 +1,4 @@
 export * from 'https://deno.land/std@0.110.0/path/mod.ts';
 export * from 'https://deno.land/std@0.132.0/testing/asserts.ts';
 export * from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
+export * from 'https://deno.land/std@0.142.0/streams/conversion.ts';
--- packages/integrations/deno/test/helpers.js
+++ packages/integrations/deno/test/helpers.js
@@ -1,4 +1,4 @@
-import { fromFileUrl } from './deps.js';
+import { readableStreamFromReader, fromFileUrl } from './deps.js';
 const dir = new URL('./', import.meta.url);
 
 export async function runBuild(fixturePath) {
@@ -10,14 +10,52 @@ export async function runBuild(fixturePath) {
 	return async () => await proc.close();
 }
 
-export async function runBuildAndStartApp(fixturePath, cb) {
-	const url = new URL(fixturePath, dir);
-	const close = await runBuild(fixturePath);
-	const mod = await import(new URL('./dist/server/entry.mjs', url));
+export async function startModFromImport(baseUrl) {
+	const entryUrl = new URL('./dist/server/entry.mjs', baseUrl);
+	const mod = await import(entryUrl);
+
 	if (!mod.running()) {
 		mod.start();
 	}
+
+	return () => mod.stop();
+}
+
+export async function startModFromSubprocess(baseUrl) {
+	const entryUrl = new URL('./dist/server/entry.mjs', baseUrl);
+	let proc = Deno.run({
+		cmd: ['deno', 'run', '--allow-env', '--allow-net', fromFileUrl(entryUrl)],
+		cwd: fromFileUrl(baseUrl),
+		stderr: 'piped'
+	});
+	const stderr = readableStreamFromReader(proc.stderr);
+	const dec = new TextDecoder();
+	for await(let bytes of stderr) {
+		let msg = dec.decode(bytes);
+		if(msg.includes(`Server running`)) {
+			break;
+		}
+	}
+	return () => proc.close();
+}
+
+export async function runBuildAndStartApp(fixturePath, cb) {
+	const url = new URL(fixturePath, dir);
+	const close = await runBuild(fixturePath);
+	const stop = await startModFromImport(url);
+
 	await cb();
-	await mod.stop();
+	await stop();
+	await close();
+}
+
+
+export async function runBuildAndStartAppFromSubprocess(fixturePath, cb) {
+	const url = new URL(fixturePath, dir);
+	const close = await runBuild(fixturePath);
+	const stop = await startModFromSubprocess(url);
+
+	await cb();
+	await stop();
 	await close();
 }
