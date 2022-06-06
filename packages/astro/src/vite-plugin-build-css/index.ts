c1d5108085696a231219c62e85795e807c28803a
unknown <matthew@skypack.dev>
2022-06-06 15:46:48 -0400

    we do need a pre and a post

--- packages/astro/src/vite-plugin-build-css/index.ts
+++ packages/astro/src/vite-plugin-build-css/index.ts
@@ -14,7 +14,7 @@ interface PluginOptions {
 	target: 'client' | 'server';
 }
 
-export function rollupPluginAstroBuildCSS(options: PluginOptions): VitePlugin {
+export function rollupPluginAstroBuildCSS(options: PluginOptions): VitePlugin[] {
 	const { internals } = options;
 
 	// This walks up the dependency graph and yields out each ModuleInfo object.
@@ -69,118 +69,129 @@ export function rollupPluginAstroBuildCSS(options: PluginOptions): VitePlugin {
 		}
 	}
 
-	return {
-		name: '@astrojs/rollup-plugin-build-css',
-		enforce: 'post',
+	const CSS_PLUGIN_NAME = '@astrojs/rollup-plugin-build-css';
+	const CSS_MINIFY_PLUGIN_NAME = '@astrojs/rollup-plugin-build-css-minify';
 
-		outputOptions(outputOptions) {
-			const manualChunks = outputOptions.manualChunks || Function.prototype;
-			outputOptions.manualChunks = function (id, ...args) {
-				// Defer to user-provided `manualChunks`, if it was provided.
-				if (typeof manualChunks == 'object') {
-					if (id in manualChunks) {
-						return manualChunks[id];
+	return [
+		{
+			name: CSS_PLUGIN_NAME,
+
+			outputOptions(outputOptions) {
+				const manualChunks = outputOptions.manualChunks || Function.prototype;
+				outputOptions.manualChunks = function (id, ...args) {
+					// Defer to user-provided `manualChunks`, if it was provided.
+					if (typeof manualChunks == 'object') {
+						if (id in manualChunks) {
+							return manualChunks[id];
+						}
+					} else if (typeof manualChunks === 'function') {
+						const outid = manualChunks.call(this, id, ...args);
+						if (outid) {
+							return outid;
+						}
 					}
-				} else if (typeof manualChunks === 'function') {
-					const outid = manualChunks.call(this, id, ...args);
-					if (outid) {
-						return outid;
+
+					// For CSS, create a hash of all of the pages that use it.
+					// This causes CSS to be built into shared chunks when used by multiple pages.
+					if (isCSSRequest(id)) {
+						return createHashOfPageParents(id, args[0]);
 					}
-				}
+				};
+			},
 
-				// For CSS, create a hash of all of the pages that use it.
-				// This causes CSS to be built into shared chunks when used by multiple pages.
-				if (isCSSRequest(id)) {
-					return createHashOfPageParents(id, args[0]);
-				}
-			};
-		},
+			async generateBundle(_outputOptions, bundle) {
+				type ViteMetadata = {
+					importedAssets: Set<string>;
+					importedCss: Set<string>;
+				};
 
-		async generateBundle(_outputOptions, bundle) {
-			type ViteMetadata = {
-				importedAssets: Set<string>;
-				importedCss: Set<string>;
-			};
+				for (const [_, chunk] of Object.entries(bundle)) {
+					if (chunk.type === 'chunk') {
+						const c = chunk;
+						if ('viteMetadata' in chunk) {
+							const meta = chunk['viteMetadata'] as ViteMetadata;
 
-			for (const [_, output] of Object.entries(bundle)) {
-				if (output.type === 'chunk') {
-					const c = output;
-					if ('viteMetadata' in output) {
-						const meta = output['viteMetadata'] as ViteMetadata;
+							// Chunks that have the viteMetadata.importedCss are CSS chunks
+							if (meta.importedCss.size) {
+								// For the client build, client:only styles need to be mapped
+								// over to their page. For this chunk, determine if it's a child of a
+								// client:only component and if so, add its CSS to the page it belongs to.
+								if (options.target === 'client') {
+									for (const [id] of Object.entries(c.modules)) {
+										for (const pageData of getParentClientOnlys(id, this)) {
+											for (const importedCssImport of meta.importedCss) {
+												pageData.css.add(importedCssImport);
+											}
+										}
+									}
+								}
 
-						// Chunks that have the viteMetadata.importedCss are CSS chunks
-						if (meta.importedCss.size) {
-							// For the client build, client:only styles need to be mapped
-							// over to their page. For this chunk, determine if it's a child of a
-							// client:only component and if so, add its CSS to the page it belongs to.
-							if (options.target === 'client') {
+								// For this CSS chunk, walk parents until you find a page. Add the CSS to that page.
 								for (const [id] of Object.entries(c.modules)) {
-									for (const pageData of getParentClientOnlys(id, this)) {
+									for (const pageViteID of getTopLevelPages(id, this)) {
+										const pageData = getPageDataByViteID(internals, pageViteID);
 										for (const importedCssImport of meta.importedCss) {
-											pageData.css.add(importedCssImport);
+											pageData?.css.add(importedCssImport);
 										}
 									}
 								}
 							}
-
-							// For this CSS chunk, walk parents until you find a page. Add the CSS to that page.
-							for (const [id] of Object.entries(c.modules)) {
-								for (const pageViteID of getTopLevelPages(id, this)) {
-									const pageData = getPageDataByViteID(internals, pageViteID);
-									for (const importedCssImport of meta.importedCss) {
-										pageData?.css.add(importedCssImport);
-									}
-								}
-							}
 						}
 					}
 				}
-
+			}
+		},
+		{
+			name: CSS_MINIFY_PLUGIN_NAME,
+			enforce: 'post',
+			async generateBundle(_outputOptions, bundle) {
 				// Minify CSS in each bundle ourselves, since server builds are not minified
 				// so that the JS is debuggable. Since you cannot configure vite:css-post to minify
 				// we need to do it ourselves.
 				if (options.target === 'server') {
-					if (output.type === 'asset') {
-						if (output.name?.endsWith('.css') && typeof output.source === 'string') {
-							const { code: minifiedCSS } = await esbuild.transform(output.source, {
-								loader: 'css',
-								minify: true,
-							});
-							output.source = minifiedCSS;
-						}
-					} else if (output.type === 'chunk') {
-						// vite:css-post removes "pure CSS" JavaScript chunks, that is chunks that only contain a comment
-						// about it being a CSS module. We need to keep these chunks around because Astro
-						// re-imports all modules as their namespace `import * as module1 from 'some/path';
-						// in order to determine if one of them is a side-effectual web component.
-						// If we ever get rid of that feature, the code below can be removed.
-						for(const [imp, bindings] of Object.entries(output.importedBindings)) {
-							if(imp.startsWith('chunks/') && !bundle[imp] && output.code.includes(imp)) {
-								// This just creates an empty chunk module so that the main entry module
-								// that is importing it doesn't break.
-								const depChunk: OutputChunk = {
-									type: 'chunk',
-									fileName: imp,
-									name: imp,
-									facadeModuleId: imp,
-									code: `/* Pure CSS chunk ${imp} */ ${bindings.map(b => `export const ${b} = {};`)}`,
-									dynamicImports: [],
-									implicitlyLoadedBefore: [],
-									importedBindings: {},
-									imports: [],
-									referencedFiles: [],
-									exports: Array.from(bindings),
-									isDynamicEntry: false,
-									isEntry: false,
-									isImplicitEntry: false,
-									modules: {},
-								};
-								bundle[imp] = depChunk;
+					for (const [, output] of Object.entries(bundle)) {
+						if (output.type === 'asset') {
+							if (output.name?.endsWith('.css') && typeof output.source === 'string') {
+								const { code: minifiedCSS } = await esbuild.transform(output.source, {
+									loader: 'css',
+									minify: true,
+								});
+								output.source = minifiedCSS;
+							}
+						} else if (output.type === 'chunk') {
+							// vite:css-post removes "pure CSS" JavaScript chunks, that is chunks that only contain a comment
+							// about it being a CSS module. We need to keep these chunks around because Astro
+							// re-imports all modules as their namespace `import * as module1 from 'some/path';
+							// in order to determine if one of them is a side-effectual web component.
+							// If we ever get rid of that feature, the code below can be removed.
+							for(const [imp, bindings] of Object.entries(output.importedBindings)) {
+								if(imp.startsWith('chunks/') && !bundle[imp] && output.code.includes(imp)) {
+									// This just creates an empty chunk module so that the main entry module
+									// that is importing it doesn't break.
+									const depChunk: OutputChunk = {
+										type: 'chunk',
+										fileName: imp,
+										name: imp,
+										facadeModuleId: imp,
+										code: `/* Pure CSS chunk ${imp} */ ${bindings.map(b => `export const ${b} = {};`)}`,
+										dynamicImports: [],
+										implicitlyLoadedBefore: [],
+										importedBindings: {},
+										imports: [],
+										referencedFiles: [],
+										exports: Array.from(bindings),
+										isDynamicEntry: false,
+										isEntry: false,
+										isImplicitEntry: false,
+										modules: {},
+									};
+									bundle[imp] = depChunk;
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-	};
+	];
 }
