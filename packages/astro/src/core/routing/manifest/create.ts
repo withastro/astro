aeb693e6504f441fe2d83f968ddd5cea10fa3996
Nate Moore <nate@skypack.dev>
2022-06-07 09:05:33 -0500

    fix: inject routes when no pages

--- packages/astro/src/core/routing/manifest/create.ts
+++ packages/astro/src/core/routing/manifest/create.ts
@@ -4,10 +4,12 @@ import type { LogOptions } from '../../logger/core';
 import fs from 'fs';
 import path from 'path';
 import slash from 'slash';
+import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { warn } from '../../logger/core.js';
 import { resolvePages } from '../../util.js';
 import { getRouteGenerator } from './generator.js';
+const require = createRequire(import.meta.url);
 
 interface Item {
 	basename: string;
@@ -271,50 +273,50 @@ export function createRouteManifest(
 
 	if (fs.existsSync(pages)) {
 		walk(fileURLToPath(pages), [], []);
-
-		config?._ctx?.injectedRoutes?.forEach(({pattern: name, entryPoint}) => {
-			const resolved = require.resolve(entryPoint);
-			const component = slash(path.relative(cwd || fileURLToPath(config.root), resolved));
-	
-			const isDynamic = (str: string) => str?.[0] === '[';
-			const normalize = (str: string) => str?.substring(1, str?.length - 1);
-	
-			const segments = name.split(path.sep)
-				.filter(Boolean)
-				.map((s: string) => {
-					validateSegment(s);
-	
-					const dynamic = isDynamic(s);
-					const content = dynamic ? normalize(s) : s;
-					return [{
-						content,
-						dynamic,
-						spread: isSpread(s)
-					}]
-				});
-	
-			const type = entryPoint.endsWith('.astro') ? 'page' : 'endpoint';
-			const isPage = type === 'page';
-			const trailingSlash = isPage ? config.trailingSlash : "never";
-	
-			const pattern = getPattern(segments, trailingSlash);
-			const generate = getRouteGenerator(segments, trailingSlash);
-			const pathname = segments.every((segment) => segment.length === 1 && !segment[0].dynamic) ? `/${segments.map((segment) => segment[0].content).join("/")}` : null;
-			const params = segments.flat().filter((p) => p.dynamic).map((p) => p.content);
-			const route = `/${segments.map(([{dynamic, content}]) => dynamic ? `[${content}]` : content).join('/')}`.toLowerCase();
-
-			const collision = routes.find(({route: r}) => r === route);
-      if(collision) {
-				throw new Error(`An integration attempted to inject a route that is already used in your project: "${route}" at "${component}". \nThis route collides with: "${collision.component}".`);
-      }
-
-			routes.push({type, route, pattern, segments, params, component, generate, pathname: pathname || void 0})
-		});
-	} else {
+	} else if (config?._ctx?.injectedRoutes?.length === 0) {
 		const pagesDirRootRelative = pages.href.slice(config.root.href.length);
 
 		warn(logging, 'astro', `Missing pages directory: ${pagesDirRootRelative}`);
 	}
+
+	config?._ctx?.injectedRoutes?.forEach(({pattern: name, entryPoint}) => {
+		const resolved = require.resolve(entryPoint, { paths: [cwd || fileURLToPath(config.root)] });
+		const component = slash(path.relative(cwd || fileURLToPath(config.root), resolved));
+
+		const isDynamic = (str: string) => str?.[0] === '[';
+		const normalize = (str: string) => str?.substring(1, str?.length - 1);
+
+		const segments = name.split(path.sep)
+			.filter(Boolean)
+			.map((s: string) => {
+				validateSegment(s);
+
+				const dynamic = isDynamic(s);
+				const content = dynamic ? normalize(s) : s;
+				return [{
+					content,
+					dynamic,
+					spread: isSpread(s)
+				}]
+			});
+
+		const type = resolved.endsWith('.astro') ? 'page' : 'endpoint';
+		const isPage = type === 'page';
+		const trailingSlash = isPage ? config.trailingSlash : "never";
+
+		const pattern = getPattern(segments, trailingSlash);
+		const generate = getRouteGenerator(segments, trailingSlash);
+		const pathname = segments.every((segment) => segment.length === 1 && !segment[0].dynamic) ? `/${segments.map((segment) => segment[0].content).join("/")}` : null;
+		const params = segments.flat().filter((p) => p.dynamic).map((p) => p.content);
+		const route = `/${segments.map(([{dynamic, content}]) => dynamic ? `[${content}]` : content).join('/')}`.toLowerCase();
+
+		const collision = routes.find(({route: r}) => r === route);
+		if(collision) {
+			throw new Error(`An integration attempted to inject a route that is already used in your project: "${route}" at "${component}". \nThis route collides with: "${collision.component}".`);
+		}
+
+		routes.push({type, route, pattern, segments, params, component, generate, pathname: pathname || void 0})
+	});
 
 	return {
 		routes,
