00eba8117c7f60432fdbfaf4b9ab31f13f7b2158
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-07 10:14:56 -0500

    test: verifying the dev server recovers

--- packages/astro/test/errors.test.js
+++ packages/astro/test/errors.test.js
@@ -25,21 +25,38 @@ describe('Error display', () => {
 			await devServer.stop();
 		});
 
-		it.skip('properly detect syntax errors in template', async () => {
+		it('properly detect syntax errors in template', async () => {
 			let html = await fixture.fetch('/astro-syntax-error').then((res) => res.text());
 
 			// 1. Verify an error message is being shown.
 			let $ = cheerio.load(html);
 			expect($('.statusMessage').text()).to.equal('Internal Error');
 			expect($('.error-message').text()).to.contain('Unexpected "}"');
+
+			// 2. Edit the file, fixing the error
+			await fixture.editFile('./src/pages/astro-syntax-error.astro', `<h1>No syntax error</h1>`);
+
+			// 3. Verify that the file is fixed.
+			html = await fixture.fetch('/astro-syntax-error').then((res) => res.text());
+			$ = cheerio.load(html);
+			expect($('h1').text()).to.equal('No syntax error');
 		});
 
 		it('shows useful error when frontmatter import is not found', async () => {
-			const html = await fixture.fetch('/import-not-found').then((res) => res.text());
+			let html = await fixture.fetch('/import-not-found').then((res) => res.text());
 
-			const $ = cheerio.load(html);
+			// 1. Verify an error message is being shown.
+			let $ = cheerio.load(html);
 			expect($('.statusMessage').text()).to.equal('Internal Error');
 			expect($('.error-message').text()).to.equal('failed to load module for ssr: ../abc.astro');
+
+			// 2. Edit the file, fixing the error
+			await fixture.editFile('./src/pages/import-not-found.astro', '<h1>No import error</h1>');
+
+			// 3. Verify that the file is fixed.
+			html = await fixture.fetch('/import-not-found').then((res) => res.text());
+			$ = cheerio.load(html);
+			expect($('h1').text()).to.equal('No import error');
 		});
 	});
 
