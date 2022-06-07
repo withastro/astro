f3ab822e328725c3905b0adad9889ad37653c24a
Matthew Phillips <matthew@skypack.dev>
2022-06-07 11:41:32 -0400

    Allow dynamic imports when using Netlify Edge Functions (#3535)
    * Allow dynamic imports when using Netlify Edge Functions
    
    * Update deno test script and changeset

---
+++ .changeset/spicy-planes-drum.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/netlify': patch
+---
+
+Fixes Netlify Edge Function and Astro.glob
--- packages/integrations/netlify/test/edge-functions/test-utils.ts
+++ packages/integrations/netlify/test/edge-functions/test-utils.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { fromFileUrl } from './deps.ts';
+import { fromFileUrl, readableStreamFromReader } from './deps.ts';
 const dir = new URL('./', import.meta.url);
 
 export async function runBuild(fixturePath: string) {
@@ -10,4 +10,22 @@ export async function runBuild(fixturePath: string) {
 	});
 	await proc.status();
 	return async () => await proc.close();
+}
+
+export async function runApp(entryPath: string) {
+	const entryUrl = new URL(entryPath, dir)
+	let proc = Deno.run({
+		cmd: ['deno', 'run', '--allow-env', '--allow-net', fromFileUrl(entryUrl)],
+		//cwd: fromFileUrl(entryUrl),
+		stderr: 'piped'
+	});
+	const stderr = readableStreamFromReader(proc.stderr);
+	const dec = new TextDecoder();
+	for await(let bytes of stderr) {
+		let msg = dec.decode(bytes);
+		if(msg.includes(`Server running`)) {
+			break;
+		}
+	}
+	return () => proc.close();
 }
