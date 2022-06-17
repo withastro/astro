52ec760557d149be6ab6e6e6aca802fc89ef6a08
Ben Holmes <hey@bholmes.dev>
2022-06-16 20:31:08 -0400

    Fix: Netlify edge deployment when using markdown (#3612)
    * wip: revert sitemap PR
    
    * fix: extract SSR-ready "slug" helper to separate module
    
    * Un-revert sitemap PR. Not to blame!
    
    This reverts commit 7cd2a8a5ea1e9fdee6837425881328928ceff852.
    
    * fix: use .netlify for edge deployment test
    
    * refactor: add md file to edge function fixture
    
    * fix: add netlify edge tests to ci
    
    * chore: remove stray console log
    
    * wip: undo "dist" change on edge tests
    
    * chore: changeset
    
    * Revert "wip: undo "dist" change on edge tests"
    
    This reverts commit 70e565ef06b228150547bca92bc12d1c236c0ed9.
    
    * temp: add ignore to breaking edge tests

--- packages/integrations/netlify/src/netlify-edge-functions.ts
+++ packages/integrations/netlify/src/netlify-edge-functions.ts
@@ -1,6 +1,6 @@
-import { SSRManifest } from 'astro';
-import { App } from 'astro/app';
 import './edge-shim.js';
+import type { SSRManifest } from 'astro';
+import { App } from 'astro/app';
 
 export function createExports(manifest: SSRManifest) {
 	const app = new App(manifest);
