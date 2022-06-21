18b209520a7e749ea304ba1c7d83590dbbb19481
dc7290 <dhkh.cba0927@gmail.com>
2022-06-21 10:34:20 +0900

    Eliminate testing of branches by os

--- packages/astro/test/static-build-dir.test.js
+++ packages/astro/test/static-build-dir.test.js
@@ -1,7 +1,7 @@
 import url from 'url';
 import path from 'path';
 import { expect } from 'chai';
-import { isWindows, loadFixture } from './test-utils.js';
+import { loadFixture } from './test-utils.js';
 
 describe('Static build: dir takes the URL path to the output directory', () => {
 	/** @type {URL} */
@@ -25,13 +25,6 @@ describe('Static build: dir takes the URL path to the output directory', () => {
 	it('dir takes the URL path to the output directory', async () => {
 		const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 		const relativeDirPath = checkDir.pathname.replace(__dirname, '');
-
-		// On Windows, it does not start with `/`, otherwise it starts with `/`.
-		if (isWindows) {
-			expect(checkDir.pathname.startsWith('/')).to.be.false;
-		} else {
-			expect(checkDir.pathname.startsWith('/')).to.be.true;
-		}
 
 		expect(relativeDirPath).to.be.equal('/fixtures/static-build-dir/dist/');
 	});
