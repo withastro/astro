ce04757696984f6fe8b751e1e1013c023ff527c6
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-21 09:43:39 -0500

    enabling for all packages

--- packages/integrations/sitemap/src/validate-options.ts
+++ packages/integrations/sitemap/src/validate-options.ts
@@ -10,7 +10,7 @@ export const validateOptions = (site: string | undefined, opts: SitemapOptions) 
 		site: z.string().optional(), // Astro takes care of `site`: how to validate, transform and refine
 		canonicalURL: z.string().optional(), // `canonicalURL` is already validated in prev step
 	})
-		.refine(({ site, canonicalURL }) => site || canonicalURL, {
+		.refine((options) => options.site || options.canonicalURL, {
 			message: 'Required `site` astro.config option or `canonicalURL` integration option',
 		})
 		.parse({
