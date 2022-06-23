54009d409f45190277ba2c9587714545f4fa15b8
Nate Moore <nate@astro.build>
2022-06-23 18:03:37 -0500

    test: add frameworks-in-jsx test

--- packages/astro/test/fixtures/jsx/astro.config.mjs
+++ packages/astro/test/fixtures/jsx/astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
 import renderer from 'astro/jsx/renderer.js';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+import svelte from '@astrojs/svelte';
+import vue from '@astrojs/vue';
+import solid from '@astrojs/solid-js';
 
 export default defineConfig({
 	integrations: [
@@ -10,6 +15,11 @@ export default defineConfig({
 					addRenderer(renderer);
 				}
 			}
-		}
+		},
+		preact(),
+		react(),
+		svelte(),
+		vue(),
+		solid(),
 	]
 })
--- packages/astro/test/jsx.test.js
+++ packages/astro/test/jsx.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import * as cheerio from 'cheerio';
 import { loadFixture } from './test-utils.js';
 
-describe('JSX', () => {
+describe('jsx-runtime', () => {
 	let fixture;
 
 	before(async () => {
@@ -18,5 +18,45 @@ describe('JSX', () => {
 
 		expect($('#basic').text()).to.equal('Basic');
 		expect($('#named').text()).to.equal('Named');
+	});
+
+	it('Can load Preact component inside Astro JSX', async () => {
+		const html = await fixture.readFile('/frameworks/index.html');
+		const $ = cheerio.load(html);
+
+		expect($('#has-preact #preact').length).to.equal(1);
+		expect($('#preact').text()).to.include('Preact');
+	});
+
+	it('Can load React component inside Astro JSX', async () => {
+		const html = await fixture.readFile('/frameworks/index.html');
+		const $ = cheerio.load(html);
+
+		expect($('#has-react #react').length).to.equal(1);
+		expect($('#react').text()).to.include('React');
+	});
+
+	it('Can load Solid component inside Astro JSX', async () => {
+		const html = await fixture.readFile('/frameworks/index.html');
+		const $ = cheerio.load(html);
+
+		expect($('#has-solid #solid').length).to.equal(1);
+		expect($('#solid').text()).to.include('Solid');
+	});
+
+	it('Can load Svelte component inside Astro JSX', async () => {
+		const html = await fixture.readFile('/frameworks/index.html');
+		const $ = cheerio.load(html);
+
+		expect($('#has-svelte #svelte').length).to.equal(1);
+		expect($('#svelte').text()).to.include('Svelte');
+	});
+
+	it('Can load Vue component inside Astro JSX', async () => {
+		const html = await fixture.readFile('/frameworks/index.html');
+		const $ = cheerio.load(html);
+
+		expect($('#has-vue #vue').length).to.equal(1);
+		expect($('#vue').text()).to.include('Vue');
 	});
 });
