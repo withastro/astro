16cdfeef81c7c0472c7fccc882fd3c39b0e88bcd
matthewp <matthewp@users.noreply.github.com>
2022-06-23 13:05:55 +0000

    [ci] format

--- packages/integrations/partytown/src/index.ts
+++ packages/integrations/partytown/src/index.ts
@@ -1,9 +1,9 @@
 import { partytownSnippet } from '@builder.io/partytown/integration';
 import { copyLibFiles, libDirPath } from '@builder.io/partytown/utils';
 import type { AstroConfig, AstroIntegration } from 'astro';
+import * as fs from 'fs';
 import { createRequire } from 'module';
 import path from 'path';
-import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import sirv from './sirv.js';
 const resolve = createRequire(import.meta.url).resolve;
@@ -54,11 +54,11 @@ export default function createPlugin(options: PartytownOptions): AstroIntegratio
 			'astro:build:ssr': async ({ manifest }) => {
 				const dirpath = libDirPath({ debugDir: false });
 				const files = await fs.promises.readdir(dirpath);
-				for(const file of files) {
-					if(file === 'debug') continue;
-					manifest.assets.push(`/~partytown/${file}`)
+				for (const file of files) {
+					if (file === 'debug') continue;
+					manifest.assets.push(`/~partytown/${file}`);
 				}
-			}
+			},
 		},
 	};
 }
