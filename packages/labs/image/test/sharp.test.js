e6dd6134c822c90d437f077ca497c0b2027d7c75
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-23 14:09:39 -0500

    updating the test for the refactored import

--- packages/labs/image/test/sharp.test.js
+++ packages/labs/image/test/sharp.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import createSharp from '../dist/sharp.js';
+import createSharp from '../dist/loaders/sharp.js';
 
 describe('Sharp service', async () => {
 	const sharp = createSharp({ routePattern: '/_image' });
