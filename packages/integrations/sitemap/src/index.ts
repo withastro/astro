1201ea31377cbf95829076a9889e07b5b7e674ca
bholmesdev <hey@bholmes.dev>
2022-06-23 10:06:14 -0400

    fix: offer suggestion for SSR sitemap users

--- packages/integrations/sitemap/src/index.ts
+++ packages/integrations/sitemap/src/index.ts
@@ -99,7 +99,12 @@ const createPlugin = (options?: SitemapOptions): AstroIntegration => {
 					}
 
 					if (pageUrls.length === 0) {
-						logger.warn(`No data for sitemap.\n\`${OUTFILE}\` is not created.`);
+						// offer suggestion for SSR users
+						if (typeof config.adapter !== 'undefined') {
+							logger.warn(`No pages found! We can only detect sitemap routes for "static" projects. Since you are using an SSR adapter, we recommend manually listing your sitemap routes using the "customPages" integration option.\n\nExample: \`sitemap({ customPages: ['https://example.com/route'] })\``);
+						} else {
+							logger.warn(`No pages found!\n\`${OUTFILE}\` not created.`);
+						}
 						return;
 					}
 
