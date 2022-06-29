54cd6b8dd184fb0acb2facaa9b6843be59f9c57f
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-06-29 14:46:23 +0000

    Fixes a bug in how `injectRoute` parses route patterns on Windows (#3763)
    * always remove a leading slash in the `injectRoute` pattern
    
    * chore: add changeset

--- packages/astro/src/core/routing/manifest/create.ts
+++ packages/astro/src/core/routing/manifest/create.ts
@@ -7,6 +7,7 @@ import path from 'path';
 import slash from 'slash';
 import { fileURLToPath } from 'url';
 import { warn } from '../../logger/core.js';
+import { removeLeadingForwardSlash } from '../../path.js';
 import { resolvePages } from '../../util.js';
 import { getRouteGenerator } from './generator.js';
 const require = createRequire(import.meta.url);
@@ -294,7 +295,7 @@ export function createRouteManifest(
 		const isDynamic = (str: string) => str?.[0] === '[';
 		const normalize = (str: string) => str?.substring(1, str?.length - 1);
 
-		const segments = name
+		const segments = removeLeadingForwardSlash(name)
 			.split(path.sep)
 			.filter(Boolean)
 			.map((s: string) => {
