ef4bcfd9f8c9c5c81cf1e0bce82bb63829dc2c4e
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-06-30 01:31:16 +0200

    Fix "Invalid hook call" warning

--- packages/integrations/preact/server.js
+++ packages/integrations/preact/server.js
@@ -4,6 +4,9 @@ import StaticHtml from './static-html.js';
 
 const slotName = (str) => str.trim().replace(/[-_]([a-z])/g, (_, w) => w.toUpperCase());
 
+let originalConsoleError;
+let runningChecks = 0;
+
 function check(Component, props, children) {
 	if (typeof Component !== 'function') return false;
 
@@ -11,19 +14,62 @@ function check(Component, props, children) {
 		return BaseComponent.isPrototypeOf(Component);
 	}
 
+	installConsoleErrorFilter();
+
+	runningChecks++;
 	try {
-		const { html } = renderToStaticMarkup(Component, props, children);
-		if (typeof html !== 'string') {
+		try {
+			const { html } = renderToStaticMarkup(Component, props, children);
+			if (typeof html !== 'string') {
+				return false;
+			}
+
+			// There are edge cases (SolidJS) where Preact *might* render a string,
+			// but components would be <undefined></undefined>
+
+			return !/\<undefined\>/.test(html);
+		} catch (err) {
 			return false;
 		}
-
-		// There are edge cases (SolidJS) where Preact *might* render a string,
-		// but components would be <undefined></undefined>
-
-		return !/\<undefined\>/.test(html);
-	} catch (err) {
-		return false;
+	} finally {
+		runningChecks--;
 	}
+}
+
+/**
+ * Attempts to filter `console.error` by passing all calls through
+ * the `filteredConsoleError` function.
+ */
+function installConsoleErrorFilter() {
+	// If we already tried to hook `console.error`,
+	// there is no need to do it again
+	if (originalConsoleError)
+		return;
+	originalConsoleError = console.error;
+
+	try {
+		console.error = filteredConsoleError;
+	} catch (error) {
+		// If we're unable to hook `console.error`, just accept it
+	}
+}
+
+/**
+ * Reduces console noise by filtering known non-problematic errors
+ * while any calls to the `check` function are running.
+ */
+function filteredConsoleError(msg, ...rest) {
+	if (runningChecks > 0 && typeof msg === 'string') {
+		// In `check`, we attempt to render JSX components through Preact.
+		// When attempting this on a React component, React may output
+		// the following error, which we can safely filter out:
+		const isKnownReactHookError =
+			msg.includes('Warning: Invalid hook call.') &&
+			msg.includes('https://reactjs.org/link/invalid-hook-call');
+		if (isKnownReactHookError)
+			return;
+	}
+	originalConsoleError(msg, ...rest);
 }
 
 function renderToStaticMarkup(Component, props, { default: children, ...slotted }) {
