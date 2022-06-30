aa56d34a094efec19ab18be3fae2fcaadcbaf94e
Nate Moore <nate@astro.build>
2022-06-30 12:18:31 -0400

    chore: add dev tests for MDX

--- packages/integrations/mdx/test/mdx-component.test.js
+++ packages/integrations/mdx/test/mdx-component.test.js
@@ -6,25 +6,58 @@ import { loadFixture } from '../../../astro/test/test-utils.js';
 
 describe('MDX Component', () => {
 	let fixture;
-
+	
 	before(async () => {
 		fixture = await loadFixture({ 
-			root: new URL('./fixtures/mdx-component', import.meta.url),
+			root: new URL('./fixtures/mdx-component/', import.meta.url),
 			integrations: [
 				mdx()
 			]
 		});
-		await fixture.build();
 	});
 
-	it('builds', async () => {
-		const html = await fixture.readFile('/index.html');
-		const { document } = parseHTML(html);
+	describe('build', () => {
+		before(async () => {
+			await fixture.build();
+		});
+
+
+		it('works', async () => {
+			const html = await fixture.readFile('/index.html');
+			const { document } = parseHTML(html);
+			
+			const h1 = document.querySelector('h1');
+			const foo = document.querySelector('#foo');
+
+			expect(h1.textContent).to.equal('Hello component!');
+			expect(foo.textContent).to.equal('bar');
+		});
+	})
+
+	describe('dev', () => {
+		let devServer;
 		
-		const h1 = document.querySelector('h1');
-		const foo = document.querySelector('#foo');
+		before(async () => {
+			devServer = await fixture.startDevServer();
+		});
 
-		expect(h1.textContent).to.equal('Hello component!');
-		expect(foo.textContent).to.equal('bar');
-	});
+		after(async () => {
+			await devServer.stop();
+		});
+
+		it('works', async () => {
+			const res = await fixture.fetch('/');
+
+			expect(res.status).to.equal(200);
+
+			const html = await res.text();
+			const { document } = parseHTML(html);
+		
+			const h1 = document.querySelector('h1');
+			const foo = document.querySelector('#foo');
+
+			expect(h1.textContent).to.equal('Hello component!');
+			expect(foo.textContent).to.equal('bar');
+		});
+	})
 })
