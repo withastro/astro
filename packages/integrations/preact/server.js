e47272972905eb4327c55cfbcbd2d2df7054bc71
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-06-30 13:20:36 +0200

    Apply code review suggestions

--- packages/integrations/preact/server.js
+++ packages/integrations/preact/server.js
@@ -5,7 +5,7 @@ import StaticHtml from './static-html.js';
 const slotName = (str) => str.trim().replace(/[-_]([a-z])/g, (_, w) => w.toUpperCase());
 
 let originalConsoleError;
-let runningChecks = 0;
+let consoleFilterRefs = 0;
 
 function check(Component, props, children) {
 	if (typeof Component !== 'function') return false;
@@ -14,9 +14,8 @@ function check(Component, props, children) {
 		return BaseComponent.isPrototypeOf(Component);
 	}
 
-	installConsoleErrorFilter();
+	useConsoleFilter();
 
-	runningChecks++;
 	try {
 		try {
 			const { html } = renderToStaticMarkup(Component, props, children);
@@ -32,46 +31,8 @@ function check(Component, props, children) {
 			return false;
 		}
 	} finally {
-		runningChecks--;
+		finishUsingConsoleFilter();
 	}
-}
-
-/**
- * Attempts to filter `console.error` by passing all calls through
- * the `filteredConsoleError` function.
- */
-function installConsoleErrorFilter() {
-	// If we already tried to hook `console.error`,
-	// there is no need to do it again
-	if (originalConsoleError)
-		return;
-	// eslint-disable-next-line no-console
-	originalConsoleError = console.error;
-
-	try {
-		// eslint-disable-next-line no-console
-		console.error = filteredConsoleError;
-	} catch (error) {
-		// If we're unable to hook `console.error`, just accept it
-	}
-}
-
-/**
- * Reduces console noise by filtering known non-problematic errors
- * while any calls to the `check` function are running.
- */
-function filteredConsoleError(msg, ...rest) {
-	if (runningChecks > 0 && typeof msg === 'string') {
-		// In `check`, we attempt to render JSX components through Preact.
-		// When attempting this on a React component, React may output
-		// the following error, which we can safely filter out:
-		const isKnownReactHookError =
-			msg.includes('Warning: Invalid hook call.') &&
-			msg.includes('https://reactjs.org/link/invalid-hook-call');
-		if (isKnownReactHookError)
-			return;
-	}
-	originalConsoleError(msg, ...rest);
 }
 
 function renderToStaticMarkup(Component, props, { default: children, ...slotted }) {
@@ -86,6 +47,62 @@ function renderToStaticMarkup(Component, props, { default: children, ...slotted 
 		h(Component, newProps, children != null ? h(StaticHtml, { value: children }) : children)
 	);
 	return { html };
+}
+
+/**
+ * Reduces console noise by filtering known non-problematic errors.
+ * 
+ * Performs reference counting to allow parallel usage from async code.
+ * 
+ * To stop filtering, please ensure that there always is a matching call
+ * to `finishUsingConsoleFilter` afterwards.
+ */
+function useConsoleFilter() {
+	consoleFilterRefs++;
+
+	if (!originalConsoleError) {
+		// eslint-disable-next-line no-console
+		originalConsoleError = console.error;
+
+		try {
+			// eslint-disable-next-line no-console
+			console.error = filteredConsoleError;
+		} catch (error) {
+			// If we're unable to hook `console.error`, just accept it
+		}
+	}
+}
+
+/**
+ * Indicates that the filter installed by `useConsoleFilter`
+ * is no longer needed by the calling code.
+ */
+function finishUsingConsoleFilter() {
+	consoleFilterRefs--;
+
+	// Note: Instead of reverting `console.error` back to the original
+	// when the reference counter reaches 0, we leave our hook installed
+	// to prevent potential race conditions once `check` is made async
+}
+
+/**
+ * Hook/wrapper function for the global `console.error` function.
+ * 
+ * Ignores known non-problematic errors while any code is using the console filter.
+ * Otherwise, simply forwards all arguments to the original function.
+ */
+function filteredConsoleError(msg, ...rest) {
+	if (consoleFilterRefs > 0 && typeof msg === 'string') {
+		// In `check`, we attempt to render JSX components through Preact.
+		// When attempting this on a React component, React may output
+		// the following error, which we can safely filter out:
+		const isKnownReactHookError =
+			msg.includes('Warning: Invalid hook call.') &&
+			msg.includes('https://reactjs.org/link/invalid-hook-call');
+		if (isKnownReactHookError)
+			return;
+	}
+	originalConsoleError(msg, ...rest);
 }
 
 export default {
