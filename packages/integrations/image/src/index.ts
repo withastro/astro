cefdc03852414d20e7b734245db638dbaeb771f0
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-07-06 18:50:10 -0500

    moving all normalization logic out of the Image component

--- packages/integrations/image/src/index.ts
+++ packages/integrations/image/src/index.ts
@@ -5,7 +5,9 @@ import slash from 'slash';
 import { fileURLToPath } from 'url';
 import type {
 	ImageAttributes,
+	ImageMetadata,
 	IntegrationOptions,
+	OutputFormat,
 	SSRImageService,
 	TransformOptions,
 } from './types';
@@ -14,6 +16,7 @@ import {
 	isRemoteImage,
 	loadLocalImage,
 	loadRemoteImage,
+	parseAspectRatio,
 	propsToFilename,
 } from './utils.js';
 import { createPlugin } from './vite-plugin-astro-image.js';
@@ -21,6 +24,90 @@ import { createPlugin } from './vite-plugin-astro-image.js';
 const PKG_NAME = '@astrojs/image';
 const ROUTE_PATTERN = '/_image';
 const OUTPUT_DIR = '/_image';
+
+export interface GetImageTransform extends Omit<TransformOptions, 'src'> {
+	src: string | ImageMetadata | Promise<{ default: ImageMetadata }>;
+}
+
+function resolveSize(transform: TransformOptions): TransformOptions {
+	// keep width & height as provided
+	if (transform.width && transform.height) {
+		return transform;
+	}
+
+	if (!transform.width && !transform.height) {
+		throw new Error(`"width" and "height" cannot both be undefined`);
+	}
+
+	if (!transform.aspectRatio) {
+		throw new Error(`"aspectRatio" must be included if only "${transform.width ? "width": "height"}" is provided`)
+	}
+
+	let aspectRatio: number;
+
+	// parse aspect ratio strings, if required (ex: "16:9")
+	if (typeof transform.aspectRatio === 'number') {
+		aspectRatio = transform.aspectRatio;
+	} else {
+		const [width, height] = transform.aspectRatio.split(':');
+		aspectRatio = parseInt(width) / parseInt(height);
+	}
+
+	if (transform.width) {
+	// only width was provided, calculate height
+		return {
+			...transform,
+			width: transform.width,
+			height: transform.width / aspectRatio
+		} as TransformOptions;
+	} else if (transform.height) {
+	// only height was provided, calculate width
+		return {
+			...transform,
+			width: transform.height * aspectRatio,
+			height: transform.height
+		};
+	}
+
+	return transform;
+}
+
+async function resolveTransform(input: GetImageTransform): Promise<TransformOptions> {
+	// for remote images, only validate the width and height props
+	if (typeof input.src === 'string') {
+		return resolveSize(input as TransformOptions);
+	}
+
+	// resolve the metadata promise, usually when the ESM import is inlined
+	const metadata = 'then' in input.src
+		? (await input.src).default
+		: input.src;
+
+	let { width, height, aspectRatio, format = metadata.format, ...rest } = input;
+
+	if (!width && !height) {
+		// neither dimension was provided, use the file metadata
+		width = metadata.width;
+		height = metadata.height;
+	} else if (width) {
+		// one dimension was provided, calculate the other
+		let ratio = parseAspectRatio(aspectRatio) || metadata.width / metadata.height;
+		height = height || width / ratio;
+	} else if (height) {
+		// one dimension was provided, calculate the other
+		let ratio = parseAspectRatio(aspectRatio) || metadata.width / metadata.height;
+		width = width || height * ratio;
+	}
+
+	return {
+		...rest,
+		src: metadata.src,
+		width,
+		height,
+		aspectRatio,
+		format: format as OutputFormat,
+	}
+}
 
 /**
  * Gets the HTML attributes required to build an `<img />` for the transformed image.
@@ -31,24 +118,26 @@ const OUTPUT_DIR = '/_image';
  */
 export async function getImage(
 	loader: SSRImageService,
-	transform: TransformOptions
+	transform: GetImageTransform
 ): Promise<ImageAttributes> {
 	(globalThis as any).loader = loader;
 
-	const attributes = await loader.getImageAttributes(transform);
+	const resolved = await resolveTransform(transform);
+
+	const attributes = await loader.getImageAttributes(resolved);
 
 	// For SSR services, build URLs for the injected route
 	if (typeof loader.transform === 'function') {
-		const { searchParams } = loader.serializeTransform(transform);
+		const { searchParams } = loader.serializeTransform(resolved);
 
 		// cache all images rendered to HTML
 		if (globalThis && (globalThis as any).addStaticImage) {
-			(globalThis as any)?.addStaticImage(transform);
+			(globalThis as any)?.addStaticImage(resolved);
 		}
 
 		const src =
 			globalThis && (globalThis as any).filenameFormat
-				? (globalThis as any).filenameFormat(transform, searchParams)
+				? (globalThis as any).filenameFormat(resolved, searchParams)
 				: `${ROUTE_PATTERN}?${searchParams.toString()}`;
 
 		return {
