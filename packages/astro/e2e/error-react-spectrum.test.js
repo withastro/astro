041f66e353f3d09183916296ee86dfe9b1d2cf3c
Nate Moore <nate@astro.build>
2022-07-08 17:18:21 -0500

    chore: move error-react-spectrum to e2e test

---
+++ packages/astro/e2e/error-react-spectrum.test.js
@@ -0,0 +1,27 @@
+import { expect } from '@playwright/test';
+import { testFactory } from './test-utils.js';
+
+const test = testFactory({ root: './fixtures/error-react-spectrum/' });
+
+let devServer;
+
+test.beforeEach(async ({ astro }) => {
+	devServer = await astro.startDevServer();
+});
+
+test.afterEach(async ({ astro }) => {
+	await devServer.stop();
+	astro.resetAllFiles();
+});
+
+test.describe('Error: React Spectrum', () => {
+	test('overlay', async ({ page, astro }) => {
+		await page.goto(astro.resolveUrl('/'));
+
+		const overlay = page.locator('vite-error-overlay');
+		
+		const text = await overlay.innerHTML();
+
+		expect(text, 'should have overlay').toMatch('@adobe/react-spectrum');
+	});
+});
