02ed1fbe35ef46cd862c101a0a5f204afff6e00b
MarcusOtter <github@otterstrom.dev>
2022-07-08 03:06:57 +0200

    Setup prettier plugin for astro files

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [require.resolve('prettier-plugin-astro')],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: true,
+  overrides: [
+    {
+      files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
