4ec41bbbe333b965be72b02c97ea8758249e28d5
Nate Moore <nate@astro.build>
2022-07-10 18:50:58 -0500

    test(e2e): update shared component tests

--- packages/astro/e2e/solid-component.test.js
+++ packages/astro/e2e/solid-component.test.js
@@ -2,26 +2,33 @@ import { prepareTestFactory } from './shared-component-tests.js';
 
 const { test, createTests } = prepareTestFactory({ root: './fixtures/solid-component/' });
 
+const config = {
+	componentFilePath: './src/components/SolidComponent.jsx',
+	counterComponentFilePath: './src/components/Counter.jsx',
+	errorReplace: '{ createSignal } from ',
+	errorMessage: 'createSignal is not defined',
+}
+
 test.describe('Solid components in Astro files', () => {
 	createTests({
+		...config,
 		pageUrl: '/',
 		pageSourceFilePath: './src/pages/index.astro',
-		componentFilePath: './src/components/SolidComponent.jsx',
 	});
 });
 
 test.describe('Solid components in Markdown files', () => {
 	createTests({
+		...config,
 		pageUrl: '/markdown/',
 		pageSourceFilePath: './src/pages/markdown.md',
-		componentFilePath: './src/components/SolidComponent.jsx',
 	});
 });
 
 test.describe('Solid components in MDX files', () => {
 	createTests({
+		...config,
 		pageUrl: '/mdx/',
 		pageSourceFilePath: './src/pages/mdx.mdx',
-		componentFilePath: './src/components/SolidComponent.jsx',
 	});
 });
