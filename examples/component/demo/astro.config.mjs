1aa8d292b1927fdb0bc1bc47797933804de5d27a
bholmesdev <hey@bholmes.dev>
2022-07-12 13:02:17 -0400

    fix: add ssr.noExternal to components ex

--- examples/component/demo/astro.config.mjs
+++ examples/component/demo/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		ssr: {
+			noExternal: ['@example/my-component']
+		},
+	},
+});
