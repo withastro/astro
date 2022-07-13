ba0fab10aad34cbe61cd4e34bd93a16e8b64ab17
Princesseuh <princssdev@gmail.com>
2022-07-13 10:10:09 -0400

    Revert "Revert "Load language integrations from the user project (#332)""
    This reverts commit c8c29eeb03db8b6958cc3cb7de71e4bf3883bc49.

---
+++ .changeset/sour-ravens-juggle.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/language-server': minor
+---
+
+Load language integrations from the user's project instead of bundling them in the language server
--- packages/language-server/src/plugins/typescript/snapshots/utils.ts
+++ packages/language-server/src/plugins/typescript/snapshots/utils.ts
@@ -4,9 +4,8 @@ import astro2tsx from '../astro2tsx';
 import { URI, Utils } from 'vscode-uri';
 import { FrameworkExt, getFrameworkFromFilePath, isAstroFilePath, isFrameworkFilePath } from '../utils';
 import { AstroSnapshot, TypeScriptDocumentSnapshot } from './DocumentSnapshot';
-import { toTSX as svelte2tsx } from '@astrojs/svelte-language-integration';
-import { toTSX as vue2tsx } from '@astrojs/vue-language-integration';
 import { toPascalCase } from '../../../utils';
+import { importSvelteIntegration, importVueIntegration } from '../../../importPackage';
 
 // Utilities to create Snapshots from different contexts
 export function createFromDocument(document: AstroDocument) {
@@ -76,9 +75,15 @@ export function createFromFrameworkFilePath(filePath: string, framework: Framewo
 	let code = '';
 
 	if (framework === 'svelte') {
-		code = svelte2tsx(originalText, className);
+		const svelteIntegration = importSvelteIntegration(filePath);
+		if (svelteIntegration) {
+			code = svelteIntegration.toTSX(originalText, className);
+		}
 	} else if (framework === 'vue') {
-		code = vue2tsx(originalText, className);
+		const vueIntegration = importVueIntegration(filePath);
+		if (vueIntegration) {
+			code = vueIntegration.toTSX(originalText, className);
+		}
 	}
 
 	return new TypeScriptDocumentSnapshot(0, filePath, code, ts.ScriptKind.TSX);
