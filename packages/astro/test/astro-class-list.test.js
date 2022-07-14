606de4e657a215546a36e63197552f586d753835
Nate Moore <nate@astro.build>
2022-07-14 11:18:30 -0500

    fix(#3920): properly handle false in `class:list`

--- packages/astro/test/astro-class-list.test.js
+++ packages/astro/test/astro-class-list.test.js
@@ -20,6 +20,7 @@ describe('Class List', async () => {
 		expect($('[class="test truthy"]')).to.have.lengthOf(1);
 		expect($('[class="test set"]')).to.have.lengthOf(1);
 		expect($('[class="hello goodbye world friend"]')).to.have.lengthOf(1);
+		expect($('[class="foo baz"]')).to.have.lengthOf(1);
 
 		expect($('.false, .noshow1, .noshow2, .noshow3, .noshow4')).to.have.lengthOf(0);
 	});
@@ -34,5 +35,6 @@ describe('Class List', async () => {
 		expect($('[class="test truthy"]')).to.have.lengthOf(1);
 		expect($('[class="test set"]')).to.have.lengthOf(1);
 		expect($('[class="hello goodbye world friend"]')).to.have.lengthOf(1);
+		expect($('[class="foo baz"]')).to.have.lengthOf(1);
 	});
 });
