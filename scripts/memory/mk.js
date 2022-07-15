147c9d7c4f19eac976cb4d761884ca84a4d2ea53
bholmesdev <hey@bholmes.dev>
2022-07-15 17:34:42 -0400

    chore: revert memory test changes

--- scripts/memory/mk.js
+++ scripts/memory/mk.js
@@ -1,16 +1,6 @@
 import fs from 'fs';
 
-const base = new URL('./project/', import.meta.url);
-const pages = new URL('./src/pages/', base);
-
-await fs.promises.writeFile(new URL('./package.json', base), `{
-  "name": "@test/smoke",
-  "version": "0.0.0",
-  "private": true,
-  "dependencies": {
-    "astro": "workspace:*"
-  }
-}`);
+const pages = new URL('./project/src/pages/', import.meta.url);
 
 for (let i = 0; i < 100; i++) {
 	let content = `---
