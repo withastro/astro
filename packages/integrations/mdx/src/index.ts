39f48dd031a240d1f6f50f1cf5f8a5557d310ec5
bholmesdev <hey@bholmes.dev>
2022-07-20 18:46:21 -0400

    wip: apply rehype plugins depending on config

--- packages/integrations/mdx/src/index.ts
+++ packages/integrations/mdx/src/index.ts
@@ -3,6 +3,8 @@ import type { AstroIntegration } from 'astro';
 import { parse as parseESM } from 'es-module-lexer';
 import remarkGfm from 'remark-gfm';
 import remarkSmartypants from 'remark-smartypants';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypePrism from '@mapbox/rehype-prism';
 import { getFileInfo } from './utils.js';
 
 type WithExtends<T> = T | { extends: T };
@@ -16,8 +18,8 @@ const DEFAULT_REMARK_PLUGINS = [remarkGfm, remarkSmartypants];
 
 function handleExtends<T>(
 	config: WithExtends<T[] | undefined>,
-	defaults: T[] = []
-): T[] | undefined {
+	defaults: T[] = [],
+): T[] {
 	if (Array.isArray(config)) return config;
 
 	return [...defaults, ...(config?.extends ?? [])];
@@ -27,16 +29,27 @@ export default function mdx(mdxOptions: MdxOptions = {}): AstroIntegration {
 	return {
 		name: '@astrojs/mdx',
 		hooks: {
-			'astro:config:setup': ({ updateConfig, config, addPageExtension, command }: any) => {
+			'astro:config:setup': async ({ updateConfig, config, addPageExtension, command }: any) => {
 				addPageExtension('.mdx');
+				let remarkPlugins = handleExtends(mdxOptions.remarkPlugins, DEFAULT_REMARK_PLUGINS);
+				let rehypePlugins = handleExtends(mdxOptions.rehypePlugins);
+
+				if (config.markdown.syntaxHighlight === 'shiki') {
+					rehypePlugins.push(rehypePrettyCode);
+				}
+
+				if (config.markdown.syntaxHighlight === 'prism') {
+					rehypePlugins.push(rehypePrism);
+				}
+
 				updateConfig({
 					vite: {
 						plugins: [
 							{
 								enforce: 'pre',
 								...mdxPlugin({
-									remarkPlugins: handleExtends(mdxOptions.remarkPlugins, DEFAULT_REMARK_PLUGINS),
-									rehypePlugins: handleExtends(mdxOptions.rehypePlugins),
+									remarkPlugins,
+									rehypePlugins,
 									// place these after so the user can't override
 									jsx: true,
 									jsxImportSource: 'astro',
