b48e011d508d7e81aeeadcaa5d03ff9f6fb11bef
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-07-22 17:22:35 -0500

    e2e tests were missing the legacy flag

--- packages/astro/e2e/fixtures/preact-compat-component/astro.config.mjs
+++ packages/astro/e2e/fixtures/preact-compat-component/astro.config.mjs
@@ -3,5 +3,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+	legacy: {
+		astroFlavoredMarkdown: true,
+	},
 	integrations: [preact({ compat: true })],
 });
