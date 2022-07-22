02a6237dab096c81dafda28cec1a00d2d4e848e9
Matthew Phillips <matthew@skypack.dev>
2022-07-22 14:38:25 -0400

    Fixes Node adapter receiving a request body

--- packages/astro/src/core/app/node.ts
+++ packages/astro/src/core/app/node.ts
@@ -7,15 +7,16 @@ import { App } from './index.js';
 
 const clientAddressSymbol = Symbol.for('astro.clientAddress');
 
-function createRequestFromNodeRequest(req: IncomingMessage): Request {
+function createRequestFromNodeRequest(req: IncomingMessage, body?: string): Request {
 	let url = `http://${req.headers.host}${req.url}`;
 	let rawHeaders = req.headers as Record<string, any>;
 	const entries = Object.entries(rawHeaders);
 	let request = new Request(url, {
 		method: req.method || 'GET',
 		headers: new Headers(entries),
+		body
 	});
-	if (req.socket.remoteAddress) {
+	if (req.socket?.remoteAddress) {
 		Reflect.set(request, clientAddressSymbol, req.socket.remoteAddress);
 	}
 	return request;
@@ -26,6 +27,31 @@ export class NodeApp extends App {
 		return super.match(req instanceof Request ? req : createRequestFromNodeRequest(req));
 	}
 	render(req: IncomingMessage | Request) {
+		if('on' in req) {
+			let body: string | undefined = undefined;
+			let reqBodyComplete = new Promise((resolve, reject) => {
+				req.on('data', d => {
+					if(body === undefined) {
+						body = '';
+					}
+					if(d instanceof Buffer) {
+						body += d.toString('utf-8');
+					} else if(typeof d === 'string') {
+						body += d;
+					}
+				});
+				req.on('end', () => {
+					resolve(body);
+				});
+				req.on('error', err => {
+					reject(err);
+				});
+			});
+
+			return reqBodyComplete.then(() => {
+				return super.render(req instanceof Request ? req : createRequestFromNodeRequest(req, body));
+			});
+		}
 		return super.render(req instanceof Request ? req : createRequestFromNodeRequest(req));
 	}
 }
---
+++ packages/integrations/node/test/api-route.test.js
@@ -0,0 +1,37 @@
+import nodejs from '../dist/index.js';
+import { loadFixture, createRequestAndResponse, toPromise } from './test-utils.js';
+import { expect } from 'chai';
+
+
+describe('API routes', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/api-route/',
+			experimental: {
+				ssr: true,
+			},
+			adapter: nodejs(),
+		});
+		await fixture.build();
+	});
+
+	it('Can get the request body', async () => {
+		const { handler } = await import('./fixtures/api-route/dist/server/entry.mjs');
+
+		let { req, res, done } = createRequestAndResponse({
+			method: 'POST',
+			url: '/recipes'
+		});
+
+		handler(req, res);
+		req.send(JSON.stringify({ id: 2 }));
+
+		let [ buffer ] = await done;
+		let json = JSON.parse(buffer.toString('utf-8'));
+		expect(json.length).to.equal(1);
+		expect(json[0].name).to.equal('Broccoli Soup');
+	});
+});
