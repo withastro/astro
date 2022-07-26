55c8aced44a60a664f28de9dabf03ca51897599e
matthewp <matthewp@users.noreply.github.com>
2022-07-26 21:33:54 +0000

    [ci] format

--- packages/astro/test/markdown.test.js
+++ packages/astro/test/markdown.test.js
@@ -16,26 +16,26 @@ describe('Markdown tests', () => {
 		before(async () => {
 			await fixture.build();
 		});
-	
+
 		it('Can load a simple markdown page with Astro', async () => {
 			const html = await fixture.readFile('/post/index.html');
 			const $ = cheerio.load(html);
-	
+
 			expect($('p').first().text()).to.equal('Hello world!');
 			expect($('#first').text()).to.equal('Some content');
 			expect($('#interesting-topic').text()).to.equal('Interesting Topic');
 		});
-	
+
 		it('Can load a realworld markdown page with Astro', async () => {
 			const html = await fixture.readFile('/realworld/index.html');
 			const $ = cheerio.load(html);
-	
+
 			expect($('pre')).to.have.lengthOf(7);
 		});
 
 		it('Does not unescape entities', async () => {
 			const html = await fixture.readFile('/entities/index.html');
-			expect(html).to.match(new RegExp("&#x3C;i>This should NOT be italic&#x3C;/i>"));
+			expect(html).to.match(new RegExp('&#x3C;i>This should NOT be italic&#x3C;/i>'));
 		});
 	});
 });
