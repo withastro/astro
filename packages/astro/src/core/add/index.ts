be27dd002b47cfb3c8d0e098aa2da8fcc77840d3
Viktor Persson <viktor.persson@arcsin.se>
2022-07-27 10:20:25 +0200

    Add mdx extension to tailwind content glob.

--- packages/astro/src/core/add/index.ts
+++ packages/astro/src/core/add/index.ts
@@ -41,7 +41,7 @@ const ALIASES = new Map([
 const ASTRO_CONFIG_STUB = `import { defineConfig } from 'astro/config';\n\nexport default defineConfig({});`;
 const TAILWIND_CONFIG_STUB = `/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
