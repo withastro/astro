fd3dfba5e192c98f8a61130a5b4f3e746e8d1d40
Matthew Phillips <matthew@skypack.dev>
2022-07-27 16:15:19 -0400

    Fixes cloudflare throwing over process (#4072)
    * Fixes cloudflare throwing over process
    
    * Up the timeout for slower CI servers
    
    * Fix linting
    
    * Up the timeout a bit

---
+++ packages/integrations/cloudflare/test/basics.test.js
@@ -0,0 +1,32 @@
+import { loadFixture, runCLI } from './test-utils.js';
+import { expect } from 'chai';
+import * as cheerio from 'cheerio';
+
+describe('Basic app', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/basics/',
+		});
+		await fixture.build();
+	});
+
+	it('can render', async () => {
+		const { ready, stop } = runCLI('./fixtures/basics/', { silent: true });
+
+		try {
+			await ready;
+
+			let res = await fetch(`http://localhost:8787/`);
+			expect(res.status).to.equal(200);
+			let html = await res.text();
+			let $ = cheerio.load(html);
+			expect($('h1').text()).to.equal('Testing');
+		} finally {
+			await stop();
+		}
+	});
+});
+
--- packages/integrations/cloudflare/test/test-utils.js
+++ packages/integrations/cloudflare/test/test-utils.js
@@ -1,4 +1,6 @@
 import { loadFixture as baseLoadFixture } from '../../../astro/test/test-utils.js';
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
 
 export { fixLineEndings } from '../../../astro/test/test-utils.js';
 
@@ -7,4 +9,54 @@ export function loadFixture(config) {
 		config.root = new URL(config.root, import.meta.url);
 	}
 	return baseLoadFixture(config);
+}
+
+const wranglerPath = fileURLToPath(new URL('../node_modules/wrangler/bin/wrangler.js', import.meta.url));
+
+export function runCLI(basePath, { silent }) {
+	const script = fileURLToPath(new URL(`${basePath}/dist/_worker.js`, import.meta.url));
+	const p = spawn('node', [wranglerPath, 'dev', '-l', script]);
+
+	p.stderr.setEncoding('utf-8');
+	p.stdout.setEncoding('utf-8');
+
+	const timeout = 10000;
+
+	const ready = new Promise(async (resolve, reject) => {
+		const failed = setTimeout(() => reject(new Error(`Timed out starting the wrangler CLI`)), timeout);
+
+		(async function () {
+			for(const msg of p.stderr) {
+				if(!silent) {
+					// eslint-disable-next-line
+					console.error(msg);
+				}
+			}
+		})();
+
+		for await(const msg of p.stdout) {
+			if(!silent) {
+				// eslint-disable-next-line
+				console.log(msg);
+			}
+			if(msg.includes(`Listening on`)) {
+				break;
+			}
+		}
+
+		clearTimeout(failed);
+		resolve();
+	});
+
+	return {
+		ready,
+		stop() {
+			p.kill();
+			return new Promise(resolve => {
+				p.addListener('exit', () => {
+					resolve();
+				});
+			})
+		}
+	}
 }
