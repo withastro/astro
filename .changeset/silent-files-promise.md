6120a71e5425ad55a17ddac800d64a3f50273bce
Matthew Phillips <matthew@skypack.dev>
2022-07-28 10:58:44 -0400

    Ensure file and url are always present in MDX for Astro.glob (#4076)

---
+++ .changeset/silent-files-promise.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/mdx': patch
+---
+
+Ensure file and url are always present in MDX for Astro.glob
---
+++ packages/integrations/mdx/test/mdx-get-static-paths.test.js
@@ -0,0 +1,29 @@
+import mdx from '@astrojs/mdx';
+
+import { expect } from 'chai';
+import { loadFixture } from '../../../astro/test/test-utils.js';
+import * as cheerio from 'cheerio';
+
+const FIXTURE_ROOT = new URL('./fixtures/mdx-get-static-paths', import.meta.url);
+
+describe('getStaticPaths', () => {
+	/** @type {import('astro/test/test-utils').Fixture} */
+	let fixture;
+	before(async () => {
+		fixture = await loadFixture({
+			root: FIXTURE_ROOT,
+			integrations: [mdx()],
+		});
+		await fixture.build();
+	});
+
+	it('Provides file and url', async () => {
+		const html = await fixture.readFile('/one/index.html');
+
+		const $ = cheerio.load(html);
+		expect($('p').text()).to.equal('First mdx file');
+		expect($('#one').text()).to.equal('hello', 'Frontmatter included');
+		expect($('#url').text()).to.equal('/src/content/1.mdx', 'url is included');
+		expect($('#file').text()).to.contain('fixtures/mdx-get-static-paths/src/content/1.mdx', 'file is included');
+	});
+});
