fffdebbc39a9e99fbc4033b669f8163472d1de4b
Nate Moore <nate@astro.build>
2022-08-01 15:29:03 -0500

    refactor: revert changes to vite-plugin-scripts

--- packages/astro/src/vite-plugin-scripts/index.ts
+++ packages/astro/src/vite-plugin-scripts/index.ts
@@ -11,54 +11,52 @@ export const BEFORE_HYDRATION_SCRIPT_ID = `${SCRIPT_ID_PREFIX}${
 export const PAGE_SCRIPT_ID = `${SCRIPT_ID_PREFIX}${'page' as InjectedScriptStage}.js`;
 export const PAGE_SSR_SCRIPT_ID = `${SCRIPT_ID_PREFIX}${'page-ssr' as InjectedScriptStage}.js`;
 
-export default function astroScriptsPlugin({ config }: { config: AstroConfig }): VitePlugin[] {
+export default function astroScriptsPlugin({ config }: { config: AstroConfig }): VitePlugin {
 	let env: ConfigEnv | undefined = undefined;
-	return [
-		{
-			name: 'astro:scripts',
+	return {
+		name: 'astro:scripts',
 
-			config(_config, _env) {
-				env = _env;
-			},
-
-			async resolveId(id) {
-				if (id.startsWith(SCRIPT_ID_PREFIX)) {
-					return id;
-				}
-				return undefined;
-			},
-
-			async load(id) {
-				if (id === BEFORE_HYDRATION_SCRIPT_ID) {
-					return config._ctx.scripts
-						.filter((s) => s.stage === 'before-hydration')
-						.map((s) => s.content)
-						.join('\n');
-				}
-				if (id === PAGE_SCRIPT_ID) {
-					return config._ctx.scripts
-						.filter((s) => s.stage === 'page')
-						.map((s) => s.content)
-						.join('\n');
-				}
-				if (id === PAGE_SSR_SCRIPT_ID) {
-					return config._ctx.scripts
-						.filter((s) => s.stage === 'page-ssr')
-						.map((s) => s.content)
-						.join('\n');
-				}
-				return null;
-			},
-			buildStart(options) {
-				const hasHydrationScripts = config._ctx.scripts.some((s) => s.stage === 'before-hydration');
-				if (hasHydrationScripts && env?.command === 'build' && !env?.ssrBuild) {
-					this.emitFile({
-						type: 'chunk',
-						id: BEFORE_HYDRATION_SCRIPT_ID,
-						name: BEFORE_HYDRATION_SCRIPT_ID,
-					});
-				}
-			},
+		config(_config, _env) {
+			env = _env;
 		},
-	];
+
+		async resolveId(id) {
+			if (id.startsWith(SCRIPT_ID_PREFIX)) {
+				return id;
+			}
+			return undefined;
+		},
+
+		async load(id) {
+			if (id === BEFORE_HYDRATION_SCRIPT_ID) {
+				return config._ctx.scripts
+					.filter((s) => s.stage === 'before-hydration')
+					.map((s) => s.content)
+					.join('\n');
+			}
+			if (id === PAGE_SCRIPT_ID) {
+				return config._ctx.scripts
+					.filter((s) => s.stage === 'page')
+					.map((s) => s.content)
+					.join('\n');
+			}
+			if (id === PAGE_SSR_SCRIPT_ID) {
+				return config._ctx.scripts
+					.filter((s) => s.stage === 'page-ssr')
+					.map((s) => s.content)
+					.join('\n');
+			}
+			return null;
+		},
+		buildStart(options) {
+			const hasHydrationScripts = config._ctx.scripts.some((s) => s.stage === 'before-hydration');
+			if (hasHydrationScripts && env?.command === 'build' && !env?.ssrBuild) {
+				this.emitFile({
+					type: 'chunk',
+					id: BEFORE_HYDRATION_SCRIPT_ID,
+					name: BEFORE_HYDRATION_SCRIPT_ID,
+				});
+			}
+		},
+	};
 }
