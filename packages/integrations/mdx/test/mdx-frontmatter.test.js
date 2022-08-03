086f8061ad39742d17299eadefdc353649dab1b5
bholmesdev <hey@bholmes.dev>
2022-08-03 11:57:47 -0400

    test: headings in layouts

--- packages/integrations/mdx/test/mdx-frontmatter.test.js
+++ packages/integrations/mdx/test/mdx-frontmatter.test.js
@@ -7,33 +7,24 @@ import { loadFixture } from '../../../astro/test/test-utils.js';
 const FIXTURE_ROOT = new URL('./fixtures/mdx-frontmatter/', import.meta.url);
 
 describe('MDX frontmatter', () => {
-	it('builds when "frontmatter.property" is in JSX expression', async () => {
-		const fixture = await loadFixture({
+	let fixture;
+	before(async () => {
+		fixture = await loadFixture({
 			root: FIXTURE_ROOT,
 			integrations: [mdx()],
 		});
 		await fixture.build();
+	})
+	it('builds when "frontmatter.property" is in JSX expression', async () => {
 		expect(true).to.equal(true);
 	});
 
 	it('extracts frontmatter to "frontmatter" export', async () => {
-		const fixture = await loadFixture({
-			root: FIXTURE_ROOT,
-			integrations: [mdx()],
-		});
-		await fixture.build();
-
 		const { titles } = JSON.parse(await fixture.readFile('/glob.json'));
 		expect(titles).to.include('Using YAML frontmatter');
 	});
 
 	it('renders layout from "layout" frontmatter property', async () => {
-		const fixture = await loadFixture({
-			root: FIXTURE_ROOT,
-			integrations: [mdx()],
-		});
-		await fixture.build();
-
 		const html = await fixture.readFile('/index.html');
 		const { document } = parseHTML(html);
 
@@ -43,12 +34,6 @@ describe('MDX frontmatter', () => {
 	});
 
 	it('passes frontmatter to layout via "content" and "frontmatter" props', async () => {
-		const fixture = await loadFixture({
-			root: FIXTURE_ROOT,
-			integrations: [mdx()],
-		});
-		await fixture.build();
-
 		const html = await fixture.readFile('/index.html');
 		const { document } = parseHTML(html);
 
@@ -59,8 +44,20 @@ describe('MDX frontmatter', () => {
 		expect(frontmatterTitle.textContent).to.equal('Using YAML frontmatter');
 	});
 
+	it('passes headings to layout via "headings" prop', async () => {
+		const html = await fixture.readFile('/with-headings/index.html');
+		const { document } = parseHTML(html);
+
+		const headingSlugs = [...document.querySelectorAll('[data-headings] > li')]
+			.map(el => el.textContent);
+
+		expect(headingSlugs.length).to.be.greaterThan(0);
+		expect(headingSlugs).to.contain('section-1');
+		expect(headingSlugs).to.contain('section-2');
+	});
+
 	it('extracts frontmatter to "customFrontmatter" export when configured', async () => {
-		const fixture = await loadFixture({
+		const customFixture = await loadFixture({
 			root: new URL('./fixtures/mdx-custom-frontmatter-name/', import.meta.url),
 			integrations: [
 				mdx({
@@ -70,9 +67,9 @@ describe('MDX frontmatter', () => {
 				}),
 			],
 		});
-		await fixture.build();
+		await customFixture.build();
 
-		const { titles } = JSON.parse(await fixture.readFile('/glob.json'));
+		const { titles } = JSON.parse(await customFixture.readFile('/glob.json'));
 		expect(titles).to.include('Using YAML frontmatter');
 	});
 });
