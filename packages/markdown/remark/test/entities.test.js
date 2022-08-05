16034f0dd5b3683e9e022dbd413e85bd18d2b031
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-08-05 16:23:16 +0200

    Fix double-escaping of non-highlighted code blocks in Astro-flavored markdown (#4169)

--- packages/markdown/remark/test/entities.test.js
+++ packages/markdown/remark/test/entities.test.js
@@ -2,11 +2,25 @@ import { renderMarkdown } from '../dist/index.js';
 import { expect } from 'chai';
 
 describe('entities', () => {
-	const renderAstroMd = (text) => renderMarkdown(text, { isAstroFlavoredMd: false });
-
-	it('should not unescape entities', async () => {
-		const { code } = await renderAstroMd(`&lt;i&gt;This should NOT be italic&lt;/i&gt;`);
+	it('should not unescape entities in regular Markdown', async () => {
+		const { code } = await renderMarkdown(`&lt;i&gt;This should NOT be italic&lt;/i&gt;`, {
+			isAstroFlavoredMd: false,
+		});
 
 		expect(code).to.equal(`<p>&#x3C;i>This should NOT be italic&#x3C;/i></p>`);
+	});
+
+	it('should not escape entities in code blocks twice in Astro-flavored markdown', async () => {
+		const { code } = await renderMarkdown(
+			`\`\`\`astro\n<h1>{x && x.name || ''}!</h1>\n\`\`\``,
+			{
+				isAstroFlavoredMd: true,
+				syntaxHighlight: false,
+			}
+		);
+
+		expect(code).to.equal(
+			`<pre is:raw><code class="language-astro">&lt;h1&gt;{x &amp;&amp; x.name || ''}!&lt;/h1&gt;\n</code></pre>`
+		);
 	});
 });
