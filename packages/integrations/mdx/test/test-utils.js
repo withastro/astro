3c7a530924d5b02abd8c54624926a73d939f21ac
bholmesdev <hey@bholmes.dev>
2022-08-04 21:43:54 -0400

    test: remark vdata

--- packages/integrations/mdx/test/test-utils.js
+++ packages/integrations/mdx/test/test-utils.js
@@ -1,5 +1,6 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
+import { visit } from 'unist-util-visit';
 import { jsToTreeNode } from '../dist/utils.js';
 
 export function rehypeReadingTime() {
@@ -9,5 +10,15 @@ export function rehypeReadingTime() {
 			jsToTreeNode(`export const readingTime = ${JSON.stringify(readingTime)}`)
 		);
 		data.astro.frontmatter.injectedReadingTime = readingTime;
+	};
+}
+
+export function remarkTitle() {
+	return function (tree, { data }) {
+		visit(tree, ['heading'], (node) => {
+			if (node.depth === 1) {
+				data.astro.frontmatter.title = toString(node.children);
+			}
+		});
 	};
 }
