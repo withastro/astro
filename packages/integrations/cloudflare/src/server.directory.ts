fcc43086aa7bcf3f63c9a1d49a53944ee54bac85
natemoo-re <natemoo-re@users.noreply.github.com>
2022-08-08 17:12:48 +0000

    [ci] format

--- packages/integrations/cloudflare/src/server.directory.ts
+++ packages/integrations/cloudflare/src/server.directory.ts
@@ -4,7 +4,7 @@ import type { SSRManifest } from 'astro';
 import { App } from 'astro/app';
 
 export function createExports(manifest: SSRManifest) {
-	const app = new App(manifest, false)
+	const app = new App(manifest, false);
 
 	const onRequest = async ({
 		request,
