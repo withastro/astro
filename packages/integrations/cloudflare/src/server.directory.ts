f4c571bdb0bbcd0dfed68a484dfbfe274f8a5f45
Richard Cooke <hello@richardcooke.io>
2022-08-08 18:10:48 +0100

    feat: add support to `@astrojs/cloudflare` directory deploy mode (#3806)
    Co-authored-by: Matthew Phillips <matthew@skypack.dev>
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

---
+++ packages/integrations/cloudflare/src/server.directory.ts
@@ -0,0 +1,41 @@
+import './shim.js';
+
+import type { SSRManifest } from 'astro';
+import { App } from 'astro/app';
+
+export function createExports(manifest: SSRManifest) {
+	const app = new App(manifest, false)
+
+	const onRequest = async ({
+		request,
+		next,
+	}: {
+		request: Request;
+		next: (request: Request) => void;
+	}) => {
+		const { origin, pathname } = new URL(request.url);
+
+		// static assets
+		if (manifest.assets.has(pathname)) {
+			const assetRequest = new Request(`${origin}/static${pathname}`, request);
+			return next(assetRequest);
+		}
+
+		let routeData = app.match(request, { matchNotFound: true });
+		if (routeData) {
+			Reflect.set(
+				request,
+				Symbol.for('astro.clientAddress'),
+				request.headers.get('cf-connecting-ip')
+			);
+			return app.render(request, routeData);
+		}
+
+		return new Response(null, {
+			status: 404,
+			statusText: 'Not found',
+		});
+	};
+
+	return { onRequest };
+}
