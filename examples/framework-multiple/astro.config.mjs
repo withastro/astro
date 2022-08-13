aa842fb4b23aeb10137e487ece2e57f0ef98d2e4
Peter Singh <afuzzybear@outlook.com>
2022-08-13 16:46:03 +0000

    Reove: references to Lit

--- examples/framework-multiple/astro.config.mjs
+++ examples/framework-multiple/astro.config.mjs
@@ -4,10 +4,10 @@ import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
 import solid from '@astrojs/solid-js';
-import lit from '@astrojs/lit';
+
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable many frameworks to support all different kinds of components.
-	integrations: [preact(), react(), svelte(), vue(), solid(), lit()],
+	integrations: [preact(), react(), svelte(), vue(), solid()],
 });
