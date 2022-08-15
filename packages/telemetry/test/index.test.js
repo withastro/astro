bd9248ce86f3fbb235db12e18a412cff30209f4b
Belar <belar@belardesign.com>
2022-08-15 16:44:27 +0000

    test: add case for disabled recording

--- packages/telemetry/test/index.test.js
+++ packages/telemetry/test/index.test.js
@@ -1,9 +1,28 @@
 import { expect } from 'chai';
-import { AstroTelemetry } from '../dist/index.js';
+import * as td from 'testdouble';
 
 describe('AstroTelemetry', () => {
+	let postModuleMock;
+	let AstroTelemetry;
+
+	beforeEach(async () => {
+		postModuleMock = await td.replaceEsm('../dist/post.js');
+
+		AstroTelemetry = (await import('../dist/index.js')).AstroTelemetry;
+	});
+
 	it('initializes when expected arguments are given', () => {
 		const telemetry = new AstroTelemetry({ version: '0.0.0-test.1' });
 		expect(telemetry).to.be.instanceOf(AstroTelemetry);
+	});
+
+	it('drops recording request if disabled', () => {
+		td.when(postModuleMock.post(td.matchers.anything())).thenResolve();
+
+		const telemetry = new AstroTelemetry({ version: '0.0.0-test.1' });
+		telemetry.setEnabled(false);
+		telemetry.record([{}]);
+
+		td.verify(postModuleMock.post(), { times: 0, ignoreExtraArgs: true });
 	});
 });
