faa49db39b265373c7b0232b0eabab4350fbf126
FredKSchott <FredKSchott@users.noreply.github.com>
2022-08-15 04:28:38 +0000

    [ci] format

--- examples/blog/astro.config.mjs
+++ examples/blog/astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [ mdx(), sitemap()],
+	integrations: [mdx(), sitemap()],
 });
