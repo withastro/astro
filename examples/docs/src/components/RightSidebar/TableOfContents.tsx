545ef8a6b24543d369dcff55b0af021e9aa096a4
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-08-17 00:15:47 +0200

    chore: prettier

--- examples/docs/src/components/RightSidebar/TableOfContents.tsx
+++ examples/docs/src/components/RightSidebar/TableOfContents.tsx
@@ -1,60 +1,56 @@
-import type { FunctionalComponent } from "preact";
-import { useState, useEffect, useRef } from "preact/hooks";
-import type { MarkdownHeading } from "astro";
+import type { FunctionalComponent } from 'preact';
+import { useState, useEffect, useRef } from 'preact/hooks';
+import type { MarkdownHeading } from 'astro';
 
 type ItemOffsets = {
-  id: string;
-  topOffset: number;
+	id: string;
+	topOffset: number;
 };
 
 const TableOfContents: FunctionalComponent<{ headings: MarkdownHeading[] }> = ({
-  headings = [],
+	headings = [],
 }) => {
-  const itemOffsets = useRef<ItemOffsets[]>([]);
-  // FIXME: Not sure what this state is doing. It was never set to anything truthy.
-  const [activeId] = useState<string>("");
-  useEffect(() => {
-    const getItemOffsets = () => {
-      const titles = document.querySelectorAll("article :is(h1, h2, h3, h4)");
-      itemOffsets.current = Array.from(titles).map((title) => ({
-        id: title.id,
-        topOffset: title.getBoundingClientRect().top + window.scrollY,
-      }));
-    };
+	const itemOffsets = useRef<ItemOffsets[]>([]);
+	// FIXME: Not sure what this state is doing. It was never set to anything truthy.
+	const [activeId] = useState<string>('');
+	useEffect(() => {
+		const getItemOffsets = () => {
+			const titles = document.querySelectorAll('article :is(h1, h2, h3, h4)');
+			itemOffsets.current = Array.from(titles).map((title) => ({
+				id: title.id,
+				topOffset: title.getBoundingClientRect().top + window.scrollY,
+			}));
+		};
 
-    getItemOffsets();
-    window.addEventListener("resize", getItemOffsets);
+		getItemOffsets();
+		window.addEventListener('resize', getItemOffsets);
 
-    return () => {
-      window.removeEventListener("resize", getItemOffsets);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('resize', getItemOffsets);
+		};
+	}, []);
 
-  return (
-    <>
-      <h2 className="heading">On this page</h2>
-      <ul>
-        <li
-          className={`heading-link depth-2 ${
-            activeId === "overview" ? "active" : ""
-          }`.trim()}
-        >
-          <a href="#overview">Overview</a>
-        </li>
-        {headings
-          .filter(({ depth }) => depth > 1 && depth < 4)
-          .map((heading) => (
-            <li
-              className={`heading-link depth-${heading.depth} ${
-                activeId === heading.slug ? "active" : ""
-              }`.trim()}
-            >
-              <a href={`#${heading.slug}`}>{heading.text}</a>
-            </li>
-          ))}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<h2 className="heading">On this page</h2>
+			<ul>
+				<li className={`heading-link depth-2 ${activeId === 'overview' ? 'active' : ''}`.trim()}>
+					<a href="#overview">Overview</a>
+				</li>
+				{headings
+					.filter(({ depth }) => depth > 1 && depth < 4)
+					.map((heading) => (
+						<li
+							className={`heading-link depth-${heading.depth} ${
+								activeId === heading.slug ? 'active' : ''
+							}`.trim()}
+						>
+							<a href={`#${heading.slug}`}>{heading.text}</a>
+						</li>
+					))}
+			</ul>
+		</>
+	);
 };
 
 export default TableOfContents;
--- examples/docs/src/config.ts
+++ examples/docs/src/config.ts
@@ -1,32 +1,32 @@
 export const SITE = {
-  title: "Documentation",
-  description: "Your website description.",
-  defaultLanguage: "en_US",
+	title: 'Documentation',
+	description: 'Your website description.',
+	defaultLanguage: 'en_US',
 };
 
 export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
-  },
-  twitter: "astrodotbuild",
+	image: {
+		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true',
+		alt:
+			'astro logo on a starry expanse of space,' +
+			' with a purple saturn-like planet floating in the right foreground',
+	},
+	twitter: 'astrodotbuild',
 };
 
 // This is the type of the frontmatter you put in the docs markdown files.
 export type Frontmatter = {
-  title: string;
-  description: string;
-  layout: string;
-  image?: { src: string; alt: string };
-  dir?: "ltr" | "rtl";
-  ogLocale?: string;
-  lang?: string;
+	title: string;
+	description: string;
+	layout: string;
+	image?: { src: string; alt: string };
+	dir?: 'ltr' | 'rtl';
+	ogLocale?: string;
+	lang?: string;
 };
 
 export const KNOWN_LANGUAGES = {
-  English: "en",
+	English: 'en',
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
@@ -36,22 +36,22 @@ export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-  indexName: "XXXXXXXXXX",
-  appId: "XXXXXXXXXX",
-  apiKey: "XXXXXXXXXX",
+	indexName: 'XXXXXXXXXX',
+	appId: 'XXXXXXXXXX',
+	apiKey: 'XXXXXXXXXX',
 };
 
 export type Sidebar = Record<
-  typeof KNOWN_LANGUAGE_CODES[number],
-  Record<string, { text: string; link: string }[]>
+	typeof KNOWN_LANGUAGE_CODES[number],
+	Record<string, { text: string; link: string }[]>
 >;
 export const SIDEBAR: Sidebar = {
-  en: {
-    "Section Header": [
-      { text: "Introduction", link: "en/introduction" },
-      { text: "Page 2", link: "en/page-2" },
-      { text: "Page 3", link: "en/page-3" },
-    ],
-    "Another Section": [{ text: "Page 4", link: "en/page-4" }],
-  },
+	en: {
+		'Section Header': [
+			{ text: 'Introduction', link: 'en/introduction' },
+			{ text: 'Page 2', link: 'en/page-2' },
+			{ text: 'Page 3', link: 'en/page-3' },
+		],
+		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
+	},
 };
