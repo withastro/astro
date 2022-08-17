1c6895884c4e0729bd7fc3c501914420166c6ed9
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-08-17 19:37:10 +0000

    Testing perf improvements for e2e tests (#4354)
    * WIP: testing fixes to share the dev server in e2e test suites
    
    * temp: testing multiple workers
    
    * Revert "temp: testing multiple workers"
    
    This reverts commit 9c7bc9d93c9c3f6dd62e3732f878f2d86016b213.

--- packages/astro/e2e/nested-in-vue.test.js
+++ packages/astro/e2e/nested-in-vue.test.js
@@ -5,11 +5,11 @@ const test = testFactory({ root: './fixtures/nested-in-vue/' });
 
 let devServer;
 
-test.beforeEach(async ({ astro }) => {
+test.beforeAll(async ({ astro }) => {
 	devServer = await astro.startDevServer();
 });
 
-test.afterEach(async () => {
+test.afterAll(async () => {
 	await devServer.stop();
 });
 
