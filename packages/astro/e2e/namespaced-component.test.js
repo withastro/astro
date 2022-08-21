ae9f82f0e5a182dc8895c60d05d7f3966064c30d
John Daly <john.daly2@gmail.com>
2022-08-20 18:36:26 -0700

    Adding tests for MDX

--- packages/astro/e2e/namespaced-component.test.js
+++ packages/astro/e2e/namespaced-component.test.js
@@ -20,7 +20,6 @@ test.describe('Hydrating namespaced components', () => {
 		await page.goto('/');
 
 		// Counter declared with: <ns.components.PreactCounter id="preact-counter-namespace" client:load>
-
 		const namespacedCounter = await page.locator('#preact-counter-namespace');
 		await expect(namespacedCounter, 'component is visible').toBeVisible();
 
@@ -35,8 +34,41 @@ test.describe('Hydrating namespaced components', () => {
 
 		await expect(namespacedCount, 'count incremented by 1').toHaveText('1');
 
-		// Counter declared with: <components.PreactCounter id="preact-counter-named" client:load>
+		// Counter declared with: <components.PreactCounterTwo id="preact-counter-named" client:load>
+		const namedCounter = await page.locator('#preact-counter-named');
+		await expect(namedCounter, 'component is visible').toBeVisible();
 
+		const namedCount = await namedCounter.locator('pre');
+		await expect(namedCount, 'initial count is 0').toHaveText('0');
+
+		const namedChildren = await namedCounter.locator('.children');
+		await expect(namedChildren, 'children exist').toHaveText('preact (named import)');
+
+		const namedIncrement = await namedCounter.locator('.increment');
+		await namedIncrement.click();
+
+		await expect(namedCount, 'count incremented by 1').toHaveText('1');
+	});
+
+	test('MDX', async ({ page }) => {
+		await page.goto('/mdx');
+
+		// Counter declared with: <ns.components.PreactCounter id="preact-counter-namespace" client:load>
+		const namespacedCounter = await page.locator('#preact-counter-namespace');
+		await expect(namespacedCounter, 'component is visible').toBeVisible();
+
+		const namespacedCount = await namespacedCounter.locator('pre');
+		await expect(namespacedCount, 'initial count is 0').toHaveText('0');
+
+		const namespacedChildren = await namespacedCounter.locator('.children');
+		await expect(namespacedChildren, 'children exist').toHaveText('preact (namespace import)');
+
+		const namespacedIncrement = await namespacedCounter.locator('.increment');
+		await namespacedIncrement.click();
+
+		await expect(namespacedCount, 'count incremented by 1').toHaveText('1');
+
+		// Counter declared with: <components.PreactCounterTwo id="preact-counter-named" client:load>
 		const namedCounter = await page.locator('#preact-counter-named');
 		await expect(namedCounter, 'component is visible').toBeVisible();
 
