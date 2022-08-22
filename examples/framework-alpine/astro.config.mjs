7310e8a1780dff2ffb57f8a6cfd3d021d019f6b8
Fred K. Schott <fkschott@gmail.com>
2022-08-22 11:25:57 -0700

    add an alpine integration and update example (#4406)

--- examples/framework-alpine/astro.config.mjs
+++ examples/framework-alpine/astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-	// No integrations are needed for AlpineJS support, just use Astro components!
-	integrations: [],
+	integrations: [alpine()],
 });
