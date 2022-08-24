2f6f9032f6771af66ba8fb37979c791d3870c3b8
Princesseuh <princssdev@gmail.com>
2022-08-24 11:56:00 -0300

    Add more details

--- packages/astro/src/core/render/result.ts
+++ packages/astro/src/core/render/result.ts
@@ -100,7 +100,7 @@ class Slots {
 				warn(
 					this.#loggingOpts,
 					'Astro.slots.render',
-					`Expected second parameter to be an array, received a ${typeof args}`
+					`Expected second parameter to be an array, received a ${typeof args}. If you're trying to pass an array as a single argument and getting unexpected results, make sure you're passing your array as a item of an array. Ex: Astro.slots.render('default', [["Hello", "World"]])`
 				);
 			} else {
 				if (expression) {
