04d6c84b11cbaa6f733b42daba552bc12279c78c
bluwy <bjornlu.dev@gmail.com>
2022-08-24 21:07:52 +0800

    Add css hmr test

---
+++ packages/astro/e2e/css.test.js
@@ -0,0 +1,29 @@
+import { expect } from '@playwright/test';
+import { testFactory } from './test-utils.js';
+
+const test = testFactory({
+	root: './fixtures/css/',
+});
+
+let devServer;
+
+test.beforeAll(async ({ astro }) => {
+	devServer = await astro.startDevServer();
+});
+
+test.afterAll(async () => {
+	await devServer.stop();
+});
+
+test.describe('HMR', () => {
+	test('edit CSS from @import', async ({ page, astro }) => {
+		await page.goto(astro.resolveUrl('/'));
+
+		const h = page.locator('h1');
+		await expect(h, 'original text set').toHaveText('hello world');
+
+		await astro.editFile('./src/pages/style.css', (original) => original.replace('world', 'astro'));
+
+		await expect(h, 'text changed').toHaveText('hello astro');
+	});
+});
