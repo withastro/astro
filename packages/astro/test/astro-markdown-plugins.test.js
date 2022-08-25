13ff88fb3fe138458e7a25bb418fba3189fc037d
bholmesdev <hey@bholmes.dev>
2022-08-25 09:44:06 -0400

    test: remark plugins with extends

--- packages/astro/test/astro-markdown-plugins.test.js
+++ packages/astro/test/astro-markdown-plugins.test.js
@@ -3,12 +3,24 @@ import * as cheerio from 'cheerio';
 import { loadFixture } from './test-utils.js';
 import addClasses from './fixtures/astro-markdown-plugins/add-classes.mjs';
 
-describe('Astro Markdown plugins', () => {
-	let fixture;
+async function buildFixture(config) {
+	const fixture = await loadFixture({
+		root: './fixtures/astro-markdown-plugins/',
+		...config,
+	});
+	await fixture.build();
+	return fixture;
+}
 
-	before(async () => {
-		fixture = await loadFixture({
-			root: './fixtures/astro-markdown-plugins/',
+function remarkExamplePlugin() {
+	return (tree) => {
+		tree.children.push({ type: 'paragraph', children: [{ type: 'text', value: 'Remark plugin applied!' }] })
+	}
+}
+
+describe('Astro Markdown plugins', () => {
+	it('Can render markdown with plugins', async () => {
+		const fixture = await buildFixture({
 			markdown: {
 				remarkPlugins: [
 					'remark-code-titles',
@@ -21,17 +33,57 @@ describe('Astro Markdown plugins', () => {
 				],
 			},
 		});
-		await fixture.build();
-	});
-
-	it('Can render markdown with plugins', async () => {
 		const html = await fixture.readFile('/index.html');
 		const $ = cheerio.load(html);
 
 		// test 1: Added a TOC
 		expect($('.toc')).to.have.lengthOf(1);
 
-		// teste 2: Added .title to h1
+		// test 2: Added .title to h1
 		expect($('#hello-world').hasClass('title')).to.equal(true);
 	});
+
+	it('Preserves default plugins with "extends"', async () => {
+		const fixture = await buildFixture({
+			markdown: {
+				remarkPlugins: {
+					extends: [remarkExamplePlugin],
+				},
+				rehypePlugins: [
+					[addClasses, { 'h1,h2,h3': 'title' }],
+				],
+			},
+		});
+		const html = await fixture.readFile('/with-gfm/index.html');
+		const $ = cheerio.load(html);
+
+		// test 1: GFM autolink applied
+		expect($('a[href="https://example.com"]')).to.have.lengthOf(1);
+
+		// test 2: Code title applied
+		expect(html).to.include('Remark plugin applied!');
+
+		// test 3: (sanity check) rehype plugins still applied
+		expect($('#github-flavored-markdown-test')).to.have.lengthOf(1);
+		expect($('#github-flavored-markdown-test').hasClass('title')).to.equal(true);
+	})
+
+	it('Removes default plugins without "extends"', async () => {
+		const fixture = await buildFixture({
+			markdown: {
+				remarkPlugins: [remarkExamplePlugin],
+				rehypePlugins: [
+					[addClasses, { 'h1,h2,h3': 'title' }],
+				],
+			},
+		});
+		const html = await fixture.readFile('/with-gfm/index.html');
+		const $ = cheerio.load(html);
+
+		// test 1: GFM autolink no longer applied
+		expect($('a[href="https://example.com"]')).to.have.lengthOf(0);
+
+		// test 2: Code title still applied
+		expect(html).to.include('Remark plugin applied!');
+	})
 });
--- packages/astro/test/fixtures/astro-markdown-plugins/astro.config.mjs
+++ packages/astro/test/fixtures/astro-markdown-plugins/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+	integrations: [],
 });
