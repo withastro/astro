b5503588f52b4b10d40b2ee87db37696b6a12886
Ben Holmes <hey@bholmes.dev>
2022-08-25 17:02:21 -0400

    nit: Note -> REAL note
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -669,7 +669,9 @@ export interface AstroUserConfig {
 		 * @description
 		 * Pass [rehype plugins](https://github.com/remarkjs/remark-rehype) to customize how your Markdown's output HTML is processed. You can import and apply the plugin function (recommended), or pass the plugin name as a string.
 		 *
-		 * Note: Providing a list of plugins will **remove** our default plugins. To preserve these defaults, see the `extendDefaultPlugins` flag.
+		 * :::note 
+		 * Providing a list of plugins will **remove** our default plugins. To preserve these defaults, see the `extendDefaultPlugins` flag.
+		 * :::
 		 *
 		 * ```js
 		 * import rehypeMinifyHtml from 'rehype-minify';
