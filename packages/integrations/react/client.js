d48de5a06f06ef26d1e2c7352b8cf1221f9a1f88
matthewp <matthewp@users.noreply.github.com>
2022-08-25 18:53:12 +0000

    [ci] format

--- packages/integrations/react/client.js
+++ packages/integrations/react/client.js
@@ -29,9 +29,9 @@ export default (element) =>
 		if (client === 'only') {
 			return startTransition(() => {
 				createRoot(element).render(componentEl);
-			})
+			});
 		}
 		return startTransition(() => {
 			hydrateRoot(element, componentEl);
-		})
+		});
 	};
