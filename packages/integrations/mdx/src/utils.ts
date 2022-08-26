a75ae27283a2ef51530cbb68fa8bbe7b6726e48f
bholmesdev <hey@bholmes.dev>
2022-08-26 17:08:12 -0400

    feat: add warning log for string plugins

--- packages/integrations/mdx/src/utils.ts
+++ packages/integrations/mdx/src/utils.ts
@@ -3,6 +3,7 @@ import type { AstroConfig, SSRError } from 'astro';
 import type { MdxjsEsm } from 'mdast-util-mdx';
 import type { PluggableList } from '@mdx-js/mdx/lib/core.js';
 import type { Options as MdxRollupPluginOptions } from '@mdx-js/rollup';
+import { bold, yellow } from 'kleur/colors';
 import { nodeTypes } from '@mdx-js/mdx';
 import { parse } from 'acorn';
 import rehypeRaw from 'rehype-raw';
@@ -172,9 +173,25 @@ export function getRehypePlugins(
 }
 
 function ignoreStringPlugins(plugins: PluggableList) {
-	return plugins.filter((plugin) =>
-		Array.isArray(plugin) ? typeof plugin[0] !== 'string' : typeof plugin !== 'string'
-	);
+	let validPlugins: PluggableList = [];
+	let hasInvalidPlugin = false;
+	for (const plugin of plugins) {
+		if (typeof plugin === 'string') {
+			console.warn(yellow(`[MDX] ${bold(plugin)} not applied.`));
+			hasInvalidPlugin = true;
+		} else if (Array.isArray(plugin) && typeof plugin[0] === 'string') {
+			console.warn(yellow(`[MDX] ${bold(plugin[0])} not applied.`));
+			hasInvalidPlugin = true;
+		} else {
+			validPlugins.push(plugin);
+		}
+	}
+	if (hasInvalidPlugin) {
+		console.warn(
+			`To inherit Markdown plugins in MDX, please use explicit imports in your config instead of "strings." See Markdown docs: https://docs.astro.build/en/guides/markdown-content/#markdown-plugins`
+		);
+	}
+	return validPlugins;
 }
 
 // TODO: remove for 1.0
