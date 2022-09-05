6aa889d4ea11b770d623376cf4c447d275b62611
beeb <vbersier@gmail.com>
2022-09-05 23:10:57 +0200

    Typo

--- packages/integrations/image/test/background-color-image-ssg.test.js
+++ packages/integrations/image/test/background-color-image-ssg.test.js
@@ -57,7 +57,7 @@ describe('SSG image with background - dev', function () {
 	});
 });
 
-describes.skip('SSG image with background - build', function () {
+describe.skip('SSG image with background - build', function () {
 	let fixture;
 	let $;
 	let html;
