7aedf39f639a5ffec70c493c5475b848b565d8cc
Matthew Phillips <matthew@skypack.dev>
2022-09-12 08:55:58 -0400

    Support signals in Preact islands

--- packages/integrations/preact/src/server.ts
+++ packages/integrations/preact/src/server.ts
@@ -1,13 +1,15 @@
+import type { RendererContext, SignalLike } from './types';
 import { h, Component as BaseComponent } from 'preact';
 import render from 'preact-render-to-string';
 import StaticHtml from './static-html.js';
+import { getContext, incrementId } from './context.js';
 
-const slotName = (str) => str.trim().replace(/[-_]([a-z])/g, (_, w) => w.toUpperCase());
+const slotName = (str: string) => str.trim().replace(/[-_]([a-z])/g, (_, w) => w.toUpperCase());
 
-let originalConsoleError;
+let originalConsoleError: typeof console.error;
 let consoleFilterRefs = 0;
 
-function check(Component, props, children) {
+function check(this: RendererContext, Component: any, props: Record<string, any>, children: any) {
 	if (typeof Component !== 'function') return false;
 
 	if (Component.prototype != null && typeof Component.prototype.render === 'function') {
@@ -18,7 +20,7 @@ function check(Component, props, children) {
 
 	try {
 		try {
-			const { html } = renderToStaticMarkup(Component, props, children);
+			const { html } = renderToStaticMarkup.call(this, Component, props, children);
 			if (typeof html !== 'string') {
 				return false;
 			}
@@ -35,18 +37,48 @@ function check(Component, props, children) {
 	}
 }
 
-function renderToStaticMarkup(Component, props, { default: children, ...slotted }) {
-	const slots = {};
+function isSignal(x: any): x is SignalLike {
+	return x != null && typeof x === 'object' && typeof x.peek === 'function' && 'value' in x;
+}
+
+function renderToStaticMarkup(this: RendererContext, Component: any, props: Record<string, any>, { default: children, ...slotted }: Record<string, any>) {
+	const ctx = getContext(this.result);
+
+	const slots: Record<string, ReturnType<typeof h>> = {};
 	for (const [key, value] of Object.entries(slotted)) {
 		const name = slotName(key);
 		slots[name] = h(StaticHtml, { value, name });
 	}
 	// Note: create newProps to avoid mutating `props` before they are serialized
 	const newProps = { ...props, ...slots };
+
+	// Check for signals
+	const signals: Record<string, string> = {};
+	for(const [key, value] of Object.entries(props)) {
+		if(isSignal(value)) {
+			// Set the value to the current signal value
+			newProps[key] = value.peek();
+
+			let id: string;
+			if(ctx.signals.has(value)) {
+				id = ctx.signals.get(value)!;
+			} else {
+				id = incrementId(ctx);
+				ctx.signals.set(value, id);
+			}
+			signals[key] = id;
+		}
+	}
+
 	const html = render(
 		h(Component, newProps, children != null ? h(StaticHtml, { value: children }) : children)
 	);
-	return { html };
+	return {
+		attrs: {
+			'data-preact-signals': JSON.stringify(signals),
+		},
+		html
+	};
 }
 
 /**
@@ -91,7 +123,7 @@ function finishUsingConsoleFilter() {
  * Ignores known non-problematic errors while any code is using the console filter.
  * Otherwise, simply forwards all arguments to the original function.
  */
-function filteredConsoleError(msg, ...rest) {
+function filteredConsoleError(msg: string, ...rest: any[]) {
 	if (consoleFilterRefs > 0 && typeof msg === 'string') {
 		// In `check`, we attempt to render JSX components through Preact.
 		// When attempting this on a React component, React may output
