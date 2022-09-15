13a4b0d48813a4aa42f3a4ccd54ff7d37c8008a6
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-15 21:24:23 +0000

    Revert "Adds a new "astro:build:generated" hook for SSG builds (#4772)" (#4774)
    This reverts commit 03b18e8d1b9b88d7e9b22e15809a78cc222cb55b.

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1159,7 +1159,6 @@ export interface AstroIntegration {
 			target: 'client' | 'server';
 			updateConfig: (newConfig: ViteConfigWithSSR) => void;
 		}) => void | Promise<void>;
-		'astro:build:generated'?: (options: { dir: URL }) => void | Promise<void>;
 		'astro:build:done'?: (options: {
 			pages: { pathname: string }[];
 			dir: URL;
