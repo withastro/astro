ff2947db6450265e184d5b658aeea516ee0544bb
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-15 14:56:17 -0500

    test: using dev for the squoosh tests

--- packages/integrations/image/test/squoosh-service.test.js
+++ packages/integrations/image/test/squoosh-service.test.js
@@ -1,57 +1,61 @@
 import { expect } from 'chai';
 import * as cheerio from 'cheerio';
-import sizeOf from 'image-size';
-import { fileURLToPath } from 'url';
 import { loadFixture } from './test-utils.js';
 
 describe('Squoosh service', function () {
 	let fixture;
+	let devServer;
 	let $;
-	let html;
 
 	before(async () => {
-		fixture = await loadFixture({ root: './fixtures/squoosh-service/' });
-		await fixture.build();
-
-		html = await fixture.readFile('/index.html');
+		fixture = await loadFixture({ root: './fixtures/basic-image/' });
+		devServer = await fixture.startDevServer();
+		const html = await fixture.fetch('/').then((res) => res.text());
 		$ = cheerio.load(html);
 	});
 
-	function verifyImage(pathname, expected) {
-		const url = new URL('./fixtures/squoosh-service/dist/' + pathname, import.meta.url);
-		const dist = fileURLToPath(url);
-		const result = sizeOf(dist);
-		expect(result).to.deep.equal(expected);
-	}
+	after(async () => {
+		await devServer.stop();
+	});
 
 	[
 		{
 			title: 'Local images',
 			id: '#social-jpg',
-			regex: /^\/assets\/social.\w{8}_\w{4,10}.jpg/,
-			size: { width: 506, height: 253, type: 'jpg' },
+			url: '/@astroimage/assets/social.jpg',
+			query: { f: 'jpg', w: '506', h: '253' },
 		},
 		{
 			title: 'Remote images',
 			id: '#google',
-			regex: /^\/assets\/googlelogo_color_272x92dp_\w{4,10}.webp/,
-			size: { width: 544, height: 184, type: 'webp' },
+			url: '/_image',
+			query: {
+				f: 'webp',
+				w: '544',
+				h: '184',
+				href: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+			},
 		},
 		{
 			title: 'Public images',
 			id: '#hero',
-			regex: /^\/assets\/hero_\w{4,10}.webp/,
-			size: { width: 768, height: 414, type: 'webp' },
+			url: '/_image',
+			query: { f: 'webp', w: '768', h: '414', href: '/hero.jpg' },
 		},
-	].forEach(({ title, id, regex, size }) => {
+	].forEach(({ title, id, url, query }) => {
 		it(title, () => {
 			const image = $(id);
 
-			expect(image.attr('src')).to.match(regex);
-			expect(image.attr('width')).to.equal(size.width.toString());
-			expect(image.attr('height')).to.equal(size.height.toString());
+			const src = image.attr('src');
+			const [route, params] = src.split('?');
 
-			verifyImage(image.attr('src'), size);
+			expect(route).to.equal(url);
+
+			const searchParams = new URLSearchParams(params);
+
+			for (const [key, value] of Object.entries(query)) {
+				expect(searchParams.get(key)).to.equal(value);
+			}
 		});
 	});
 });
