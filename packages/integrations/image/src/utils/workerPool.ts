39e5b845a52ed489ea6e2a618a179694de06b3b5
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-09-15 12:53:11 -0500

    adding @astrojs/webapi for the TransformStream polyfill

--- packages/integrations/image/src/utils/workerPool.ts
+++ packages/integrations/image/src/utils/workerPool.ts
@@ -1,5 +1,5 @@
 import { Worker, parentPort } from 'worker_threads';
-import { TransformStream } from 'web-streams-polyfill';
+import { TransformStream } from '@astrojs/webapi';
 
 function uuid() {
   return Array.from({ length: 16 }, () =>
