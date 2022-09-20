0987f402393bef9fe9fa33ed6c4e63df7d3d8025
Matthew Phillips <matthew@skypack.dev>
2022-09-20 09:00:43 -0400

    Prevent errors in React components from crashing the dev server

---
+++ packages/astro/test/fixtures/react-component/src/components/ThrowsAnError.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export default function() {
+	let player = undefined;
+	const [] = useState(player.currentTime || null);
+
+	return (
+		<div>Should have thrown</div>
+	)
+}
