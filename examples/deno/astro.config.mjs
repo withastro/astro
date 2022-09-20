4d1b1b583bb9be37f5fafe23baa6b700cb205702
matthewp <matthewp@users.noreply.github.com>
2022-09-20 12:32:27 +0000

    [ci] format

--- examples/deno/astro.config.mjs
+++ examples/deno/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import deno from "@astrojs/deno";
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: deno()
+	output: 'server',
+	adapter: deno(),
 });
