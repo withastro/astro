26ff183f1006914d0dc51427676bc20544b3ac17
bluwy <bjornlu.dev@gmail.com>
2022-09-22 20:51:58 +0800

    Cleanup test

--- packages/astro/test/api-routes.test.js
+++ packages/astro/test/api-routes.test.js
@@ -1,8 +1,5 @@
 import { expect } from 'chai';
-import * as cheerio from 'cheerio';
 import { loadFixture } from './test-utils.js';
-import * as fs from 'fs';
-import { FormData, File } from 'node-fetch';
 
 describe('API routes', () => {
 	/** @type {import('./test-utils').Fixture} */
