5868c1ed0af9568e166c9f56123d3bdfd92689ea
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-09-25 13:45:18 +0530

    Update hydration.ts

--- packages/astro/src/runtime/server/hydration.ts
+++ packages/astro/src/runtime/server/hydration.ts
@@ -92,8 +92,10 @@ export function extractDirectives(inputProps: Record<string | number, any>): Ext
 				}
 			}
 		} else if (key === 'class:list') {
-			// support "class" from an expression passed into a component (#782)
-			extracted.props[key.slice(0, -5)] = serializeListValue(value);
+			if (value) {
+				// support "class" from an expression passed into a component (#782)
+				extracted.props[key.slice(0, -5)] = serializeListValue(value);
+			}
 		} else {
 			extracted.props[key] = value;
 		}
