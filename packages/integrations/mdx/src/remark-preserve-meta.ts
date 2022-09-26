6c147df059012a1c99298897708d3d286be19492
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-09-27 03:36:56 +0530

    Update remark-preserve-meta.ts

--- packages/integrations/mdx/src/remark-preserve-meta.ts
+++ packages/integrations/mdx/src/remark-preserve-meta.ts
@@ -6,9 +6,8 @@ export default function remarkPreserveMeta() {
 		visit(tree, 'code', (node: any) => {
 			let { lang, value, meta } = node;
 			node.type = 'html';
-			let codeClasses = lang && lang.length > 0 ? `class=language-${lang}` : ''
 			let metaAttribute = meta && meta.length > 0 ? `meta=${meta}` : ''
-			node.value = `<pre><code ${codeClasses} ${metaAttribute}>${value}</code></pre>`;
+			node.value = `<pre><code ${metaAttribute}>${value}</code></pre>`;
 			return node;
 		});
 }
