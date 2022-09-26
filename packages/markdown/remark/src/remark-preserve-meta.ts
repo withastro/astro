680bc4eb7b790247a50b66199f34b07d19c5e108
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-27 03:27:43 +0530

    Create preserveMeta in markdown

---
+++ packages/markdown/remark/src/remark-preserve-meta.ts
@@ -0,0 +1,25 @@
+import { visit } from 'unist-util-visit';
+
+type MaybeString = string | null | undefined;
+
+/**  */
+function transformer(className: MaybeString) {
+	return function (tree: any) {
+		const visitor = (node: any) => {
+			const { lang, value, meta } = node;
+			node.type = "html";
+			let preClasses = className?.length ? `class=${className}` : ''
+			let codeClasses = lang && lang.length > 0 ? `class=language-${lang}` : ''
+			let metaAttribute = meta && meta.length > 0 ? `meta=${meta}` : ''
+			node.value = `<pre ${preClasses}><code is:raw ${codeClasses} ${metaAttribute}>${value}</code></pre>`;
+			return node;
+		};
+		return visit(tree, 'code', visitor);
+	};
+}
+
+function plugin(className: MaybeString) {
+	return transformer.bind(null, className);
+}
+
+export default plugin;
