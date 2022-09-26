f8343eb6248f1c581d56723769ef42e89a056b8e
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-27 03:28:14 +0530

    Preserve meta in markdown prism integration

--- packages/markdown/remark/src/remark-prism.ts
+++ packages/markdown/remark/src/remark-prism.ts
@@ -8,7 +8,7 @@ type MaybeString = string | null | undefined;
 function transformer(className: MaybeString) {
 	return function (tree: any) {
 		const visitor = (node: any) => {
-			let { lang, value } = node;
+			let { lang, value, meta } = node;
 			node.type = 'html';
 
 			let { html, classLanguage } = runHighlighterWithAstro(lang, value);
@@ -16,9 +16,10 @@ function transformer(className: MaybeString) {
 			if (className) {
 				classes.push(className);
 			}
+			let metaAttribute = meta && meta.length > 0 ? `meta=${meta}` : ''
 			node.value = `<pre class="${classes.join(
 				' '
-			)}"><code is:raw class="${classLanguage}">${html}</code></pre>`;
+			)}"><code is:raw class="${classLanguage}" ${metaAttribute}>${html}</code></pre>`;
 			return node;
 		};
 		return visit(tree, 'code', visitor);
