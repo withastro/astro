8f89fdf887b88360762eb1262aa157ff3594248a
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-27 03:26:48 +0530

    Preserve meta in mdx prism integration

--- packages/integrations/mdx/src/remark-prism.ts
+++ packages/integrations/mdx/src/remark-prism.ts
@@ -5,14 +5,15 @@ import { visit } from 'unist-util-visit';
 export default function remarkPrism() {
 	return (tree: any) =>
 		visit(tree, 'code', (node: any) => {
-			let { lang, value } = node;
+			let { lang, value, meta } = node;
 			node.type = 'html';
 
 			let { html, classLanguage } = runHighlighterWithAstro(lang, value);
 			let classes = [classLanguage];
+			let metaAttribute = meta && meta.length > 0 ? `meta=${meta}` : ''
 			node.value = `<pre class="${classes.join(
 				' '
-			)}"><code class="${classLanguage}">${html}</code></pre>`;
+			)}"><code class="${classLanguage}" ${metaAttribute}>${html}</code></pre>`;
 			return node;
 		});
 }
