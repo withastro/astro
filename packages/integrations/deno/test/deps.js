18d6c193a75c74b72139838d70df78916edd4e0d
Robin Lindner <robin@deeprobin.de>
2022-10-06 21:43:24 +0200

    Improve test infrastructure for integrations/deno

--- packages/integrations/deno/test/deps.js
+++
@@ -1,4 +0,0 @@
-export * from 'https://deno.land/std@0.110.0/path/mod.ts';
-export * from 'https://deno.land/std@0.132.0/testing/asserts.ts';
-export * from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
-export * from 'https://deno.land/std@0.142.0/streams/conversion.ts';
--- packages/integrations/deno/test/dynamic-import.test.ts
+++ packages/integrations/deno/test/dynamic-import.test.ts
@@ -1,7 +1,8 @@
-import { runBuildAndStartAppFromSubprocess } from './helpers.js';
-import { assertEquals, assert, DOMParser } from './deps.js';
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.35-alpha/deno-dom-wasm.ts";
+import { assert, assertEquals } from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import { runBuildAndStartAppFromSubprocess } from "./helpers.ts";
 
-async function startApp(cb) {
+async function startApp(cb: () => Promise<void>) {
 	await runBuildAndStartAppFromSubprocess('./fixtures/dynimport/', cb);
 }
 
@@ -14,7 +15,7 @@ Deno.test({
 			const html = await resp.text();
 			assert(html);
 			const doc = new DOMParser().parseFromString(html, `text/html`);
-			const div = doc.querySelector('#thing');
+			const div = doc!.querySelector('#thing');
 			assert(div, 'div exists');
 		});
 	},
