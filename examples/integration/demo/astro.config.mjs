d4d67eb5dea2e51eb8da1ac5f87827eef82b83bf
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-10-07 09:36:56 -0500

    removing the monorepo and demo project, adding links to docs and examples

--- examples/integration/demo/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import myIntegration from '@example/my-integration';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [myIntegration()],
-});
