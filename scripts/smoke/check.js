ad01760e88b46dfac3553c24363526d524261cac
Princesseuh <princssdev@gmail.com>
2022-10-07 17:59:21 -0300

    Run astro check on all examples in CI

---
+++ scripts/smoke/check.js
@@ -0,0 +1,63 @@
+// @ts-check
+
+import { exec, execSync, spawn } from 'child_process';
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
+import * as path from 'path';
+import { tsconfigResolverSync } from 'tsconfig-resolver';
+
+function checkExamples() {
+	let examples = readdirSync('./examples', { withFileTypes: true });
+	examples = examples.filter((dirent) => dirent.isDirectory());
+
+	console.log(`Running astro check on ${examples.length} examples...`);
+
+	Promise.all(
+		examples.map(
+			(example) =>
+				new Promise((resolve) => {
+					prepareExample(example.name);
+					let data = '';
+					const child = spawn('node', ['../../packages/astro/astro.js', 'check'], {
+						cwd: path.join('./examples', example.name),
+					});
+
+					child.stdout.on('data', function (buffer) {
+						data += buffer.toString();
+					});
+
+					child.on('exit', (code) => {
+						if (code !== 0) {
+							console.error(data);
+						}
+						resolve(code);
+					});
+				})
+		)
+	).then((codes) => {
+		if (codes.some((code) => code !== 0)) {
+			process.exit(1);
+		}
+	});
+}
+
+/**
+ * @param {string} examplePath
+ */
+function prepareExample(examplePath) {
+	const tsconfigPath = path.join('./examples/', examplePath, 'tsconfig.json');
+	const tsconfig = tsconfigResolverSync({ filePath: tsconfigPath, cache: false });
+
+	if (tsconfig.exists) {
+		tsconfig.config.extends = 'astro/tsconfigs/strictest';
+
+		if (tsconfig.config.compilerOptions) {
+			tsconfig.config.compilerOptions = Object.assign(tsconfig.config.compilerOptions, {
+				types: tsconfig.config.compilerOptions.types ?? [], // Speeds up tests
+			});
+		}
+	}
+
+	writeFileSync(tsconfigPath, JSON.stringify(tsconfig.config, null, 2));
+}
+
+checkExamples();
