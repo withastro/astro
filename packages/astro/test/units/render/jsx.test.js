a376a04b9103bff359810e6cf7f025736707376c
Matthew Phillips <matthew@skypack.dev>
2022-10-13 15:20:28 -0400

    Fixes JSX usage of slots with dashes in their name

--- packages/astro/test/units/render/jsx.test.js
+++ packages/astro/test/units/render/jsx.test.js
@@ -44,5 +44,44 @@ describe('core/render', () => {
 			const html = await response.text();
 			expect(html).to.include('<div><p class="n">works</p></div>');
 		});
+
+		it('Can render slots with a dash in the name', async () => {
+			const Wrapper = createComponent((result, _props, slots = {}) => {
+				return render`<div>${renderSlot(result, slots['my-slot'])}</div>`;
+			});
+	
+			const Page = createAstroJSXComponent(() => {
+				return jsx('main', {
+					children: [
+						jsx(Wrapper, {
+							// Children as an array
+							children: [
+								jsx('p', {
+									slot: 'my-slot',
+									className: 'n',
+									children: 'works'
+								})
+							]
+						}),
+						jsx(Wrapper, {
+							// Children as a VNode
+							children: jsx('p', {
+								slot: 'my-slot',
+								className: 'p',
+								children: 'works'
+							})
+						})
+					]
+				})
+			});
+
+			const ctx = createRenderContext({ request: new Request('http://example.com/' )});
+			const response = await renderPage(createAstroModule(Page), ctx, env);
+	
+			expect(response.status).to.equal(200);
+	
+			const html = await response.text();
+			expect(html).to.include('<main><div><p class="n">works</p></div><div><p class="p">works</p></div></main>');
+		});
 	});
 });
