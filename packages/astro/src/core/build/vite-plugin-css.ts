76867cb37cfc5c015ce5308a355ed17ab3ead592
bluwy <bjornlu.dev@gmail.com>
2022-10-20 23:31:31 +0800

    Update compiler

--- packages/astro/src/core/build/vite-plugin-css.ts
+++ packages/astro/src/core/build/vite-plugin-css.ts
@@ -223,38 +223,6 @@ export function rollupPluginAstroBuildCSS(options: PluginOptions): VitePlugin[] 
 								});
 								output.source = minifiedCSS;
 							}
-						} else if (output.type === 'chunk') {
-							// vite:css-post removes "pure CSS" JavaScript chunks, that is chunks that only contain a comment
-							// about it being a CSS module. We need to keep these chunks around because Astro
-							// re-imports all modules as their namespace `import * as module1 from 'some/path';
-							// in order to determine if one of them is a side-effectual web component.
-							// If we ever get rid of that feature, the code below can be removed.
-							for (const [imp, bindings] of Object.entries(output.importedBindings)) {
-								if (imp.startsWith('chunks/') && !bundle[imp] && output.code.includes(imp)) {
-									// This just creates an empty chunk module so that the main entry module
-									// that is importing it doesn't break.
-									const depChunk: OutputChunk = {
-										type: 'chunk',
-										fileName: imp,
-										name: imp,
-										facadeModuleId: imp,
-										code: `/* Pure CSS chunk ${imp} */ ${bindings
-											.map((b) => `export const ${b} = {};`)
-											.join('')}`,
-										dynamicImports: [],
-										implicitlyLoadedBefore: [],
-										importedBindings: {},
-										imports: [],
-										referencedFiles: [],
-										exports: Array.from(bindings),
-										isDynamicEntry: false,
-										isEntry: false,
-										isImplicitEntry: false,
-										modules: {},
-									};
-									bundle[imp] = depChunk;
-								}
-							}
 						}
 					}
 				}
