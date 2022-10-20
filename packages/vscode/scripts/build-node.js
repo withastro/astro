c61d1d73d28445cd584a1edb38c06755038a3492
Princesseuh <princssdev@gmail.com>
2022-10-20 13:08:03 -0300

    Attempt to fix types directory path

--- packages/vscode/scripts/build-node.js
+++ packages/vscode/scripts/build-node.js
@@ -40,8 +40,9 @@ require('esbuild')
 			require('esbuild-plugin-copy').copy({
 				resolveFrom: 'cwd',
 				assets: {
-					from: ['../language-server/types/*.d.ts'],
-					to: ['./dist/types/*'],
+					from: ['../language-server/types/**/*.d.ts'],
+					to: ['./dist/types'],
+					keepStructure: true,
 				},
 			}),
 		],
