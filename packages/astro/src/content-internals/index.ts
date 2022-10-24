47645d8dfdb486b001b3ce774218d25f7cb7b68d
bholmesdev <hey@bholmes.dev>
2022-10-24 11:13:30 -0400

    refactor: move more logic to content-internals

--- packages/astro/src/content-internals/index.ts
+++ packages/astro/src/content-internals/index.ts
@@ -1,8 +1,38 @@
 import { z } from 'zod';
 
+export async function parseEntryData(
+	collection: string,
+	entryKey: string,
+	unparsedEntry: { data: any; rawData: string },
+	{ schemaMap }: { schemaMap: Record<string, any> }
+) {
+	const schemaImport = (await schemaMap[collection]) ?? {};
+	if (!('schema' in schemaImport)) throw getErrorMsg.schemaNamedExp(collection);
+	const { schema } = schemaImport;
+
+	try {
+		return schema.parse(unparsedEntry.data, { errorMap });
+	} catch (e) {
+		if (e instanceof z.ZodError) {
+			const formattedError = new Error(
+				[
+					`Could not parse frontmatter in ${String(collection)} → ${String(entryKey)}`,
+					...e.errors.map((e) => e.message),
+				].join('\n')
+			);
+			(formattedError as any).loc = {
+				file: 'TODO',
+				line: getFrontmatterErrorLine(unparsedEntry.rawData, String(e.errors[0].path[0])),
+				column: 1,
+			};
+			throw formattedError;
+		}
+	}
+}
+
 const flattenPath = (path: (string | number)[]) => path.join('.');
 
-export const errorMap: z.ZodErrorMap = (error, ctx) => {
+const errorMap: z.ZodErrorMap = (error, ctx) => {
 	if (error.code === 'invalid_type') {
 		const badKeyPath = JSON.stringify(flattenPath(error.path));
 		if (error.received === 'undefined') {
@@ -15,7 +45,7 @@ export const errorMap: z.ZodErrorMap = (error, ctx) => {
 };
 
 // WARNING: MAXIMUM JANK AHEAD
-export function getFrontmatterErrorLine(rawFrontmatter: string, frontmatterKey: string) {
+function getFrontmatterErrorLine(rawFrontmatter: string, frontmatterKey: string) {
 	console.log({ rawFrontmatter, frontmatterKey });
 	const indexOfFrontmatterKey = rawFrontmatter.indexOf(`\n${frontmatterKey}`);
 	if (indexOfFrontmatterKey === -1) return 0;
@@ -24,3 +54,10 @@ export function getFrontmatterErrorLine(rawFrontmatter: string, frontmatterKey: 
 	const numNewlinesBeforeKey = frontmatterBeforeKey.split('\n').length;
 	return numNewlinesBeforeKey;
 }
+
+export const getErrorMsg = {
+	schemaMissing: (collection: string) =>
+		`${collection} does not have a ~schema file. We suggest adding one for type safety!`,
+	schemaNamedExp: (collection: string) =>
+		new Error(`${collection}/~schema needs a named \`schema\` export.`),
+};
