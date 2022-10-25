e2aee287d44ecae811933e2433866686c9353af9
bholmesdev <hey@bholmes.dev>
2022-10-25 13:44:34 -0400

    feat: new-and-improved render-content

--- examples/with-content/src/render-content.ts
+++ examples/with-content/src/render-content.ts
@@ -1,6 +1,6 @@
 import { renderContentMap } from '../.astro/render-content-generated';
 
-export async function renderContent(entry: { id: string } | string, $$result?: any) {
+export async function renderContent(this: any, entry: { id: string } | string) {
 	// TODO: respect srcDir
 	const contentKey = typeof entry === 'object' ? entry.id : entry;
 	const modImport = renderContentMap[contentKey];
@@ -9,9 +9,9 @@ export async function renderContent(entry: { id: string } | string, $$result?: a
 
 	const mod = await modImport();
 
-	if (import.meta.env.PROD && 'collectedCss' in mod && $$result) {
+	if (import.meta.env.PROD && 'collectedCss' in mod && 'links' in (this ?? {})) {
 		for (const cssAsset of mod.collectedCss) {
-			$$result.links.add({ props: { rel: 'stylesheet', href: cssAsset }, children: '' });
+			this.links.add({ props: { rel: 'stylesheet', href: cssAsset }, children: '' });
 		}
 	}
 	return mod;
