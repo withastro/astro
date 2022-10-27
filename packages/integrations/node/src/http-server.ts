9d9aeb3f9e2773a3ca952e49873b13bfdba4afa3
BeanWei <bean0wei@dingtalk.com>
2022-10-27 17:17:38 +0800

    Fix node standalone for windows system (#5207)

--- packages/integrations/node/src/http-server.ts
+++ packages/integrations/node/src/http-server.ts
@@ -16,10 +16,9 @@ export function createServer(
 ) {
 	const listener: http.RequestListener = (req, res) => {
 		if (req.url) {
-			const fileURL = new URL('.' + req.url, client);
-
-			const stream = send(req, fileURLToPath(fileURL), {
-				dotfiles: 'deny',
+			const stream = send(req, encodeURI(req.url), {
+				root: fileURLToPath(client),
+				dotfiles: "deny"
 			});
 
 			let forwardError = false;
