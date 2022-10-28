cad294f5b64bea6a247f587abe212474793a769e
bholmesdev <hey@bholmes.dev>
2022-10-28 09:44:50 -0400

    feat: clean up example

--- examples/with-content/src/content/blog/~schema.ts
+++ examples/with-content/src/content/blog/~schema.ts
@@ -2,4 +2,11 @@ import { z } from 'zod';
 
 export const schema = z.object({
 	title: z.string(),
+	description: z.string().max(60, 'For SEO purposes, keep descriptions short!'),
+	// mark optional properties with `.optional()`
+	image: z.string().optional(),
+	tags: z.array(z.string()).default([]),
+	// transform to another data type with `transform`
+	// ex. convert date strings to Date objects
+	publishedDate: z.string().transform((str) => new Date(str)),
 });
