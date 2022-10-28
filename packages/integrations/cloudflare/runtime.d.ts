c6b149bc0a223e404333240a66e42ea933d9b7a4
matthewp <matthewp@users.noreply.github.com>
2022-10-28 14:21:57 +0000

    [ci] format

--- packages/integrations/cloudflare/runtime.d.ts
+++ packages/integrations/cloudflare/runtime.d.ts
@@ -1,8 +1,3 @@
-export type {
-  WorkerRuntime,
-  PagesRuntime
-} from './dist/runtime';
+export type { WorkerRuntime, PagesRuntime } from './dist/runtime';
 
-export {
-  getRuntime
-} from './dist/runtime';
+export { getRuntime } from './dist/runtime';
