eb2535996e602d5501ad14114bea2120e8e47191
Princesseuh <princssdev@gmail.com>
2022-11-08 13:25:27 -0400

    Remove unnecessary console.log

--- packages/astro/e2e/errors.test.js
+++ packages/astro/e2e/errors.test.js
@@ -38,7 +38,6 @@ test.describe('Error display', () => {
 		await page.goto(astro.resolveUrl('/import-not-found'));
 
 		const message = await getErrorOverlayMessage(page);
-		console.log(message);
 		expect(message).toMatch(
 			`Could not import ../abc.astro\n\nThis is often caused by a typo in the import path, please make sure the file exists`
 		);
