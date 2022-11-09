3957c21c11a1407580e13a0bde02c2d2ded097fe
Princesseuh <princssdev@gmail.com>
2022-11-09 09:31:16 -0400

    Misc punctuations fixes and additions to README

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1,6 +1,6 @@
 // BEFORE ADDING AN ERROR: Please look at the README.md in this folder for general guidelines on writing error messages
-// Additionally, this code, much like `@types/astro.ts` will be used to generate documentation in the future,
-// so make sure to pass your changes by our wonderful docs team before merging!
+// Additionally, this code, much like `@types/astro.ts`, is used to generate documentation, so make sure to pass
+// your changes by our wonderful docs team before merging!
 
 interface ErrorData {
 	code: number;
@@ -19,7 +19,7 @@ export const AstroErrorData = defineErrors({
 		code: 3001,
 		message:
 			"Redirects are only available when using output: 'server'. Update your Astro config if you need SSR features.",
-		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#enabling-ssr-in-your-project for more information on how to enable SSR',
+		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#enabling-ssr-in-your-project for more information on how to enable SSR.',
 	},
 	SSRClientAddressNotAvailableInAdapter: {
 		code: 3002,
@@ -30,14 +30,14 @@ export const AstroErrorData = defineErrors({
 		code: 3003,
 		message:
 			"Astro.clientAddress is only available when using output: 'server'. Update your Astro config if you need SSR features.",
-		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#enabling-ssr-in-your-project for more information on how to enable SSR',
+		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#enabling-ssr-in-your-project for more information on how to enable SSR.',
 	},
 	NoMatchingStaticPathFound: {
 		code: 3004,
 		message: (pathName: string) =>
 			`A getStaticPaths route pattern was matched, but no matching static path was found for requested path ${pathName}.`,
 		hint: (possibleRoutes: string[]) =>
-			`Possible dynamic routes being matched: ${possibleRoutes.join(', ')}`,
+			`Possible dynamic routes being matched: ${possibleRoutes.join(', ')}.`,
 	},
 	OnlyResponseCanBeReturned: {
 		code: 3005,
@@ -45,7 +45,7 @@ export const AstroErrorData = defineErrors({
 			`Route ${
 				route ? route : ''
 			} returned a ${returnedValue}. Only a Response can be returned from Astro files.`,
-		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#response for more information',
+		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/#response for more information.',
 	},
 	MissingMediaQueryDirective: {
 		code: 3006,
@@ -81,7 +81,7 @@ but ${plural ? 'none were' : 'it was not'} able to server-side render ${componen
 		code: 3008,
 		message: (componentName: string, clientDirective: string, rendererName: string) =>
 			`${componentName} component has a \`client:${clientDirective}\` directive, but no client entrypoint was provided by ${rendererName}!`,
-		hint: 'See https://docs.astro.build/en/reference/integrations-reference/#addrenderer-option for more information on how to configure your renderer',
+		hint: 'See https://docs.astro.build/en/reference/integrations-reference/#addrenderer-option for more information on how to configure your renderer.',
 	},
 	NoClientOnlyHint: {
 		code: 3009,
@@ -94,36 +94,36 @@ but ${plural ? 'none were' : 'it was not'} able to server-side render ${componen
 		code: 3010,
 		message: (paramType) =>
 			`Invalid params given to getStaticPaths path. Expected an object, got ${paramType}`,
-		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths',
+		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths.',
 	},
 	InvalidGetStaticPathsReturn: {
 		code: 3011,
 		message: (returnType) =>
 			`Invalid type returned by getStaticPaths. Expected an array, got ${returnType}`,
-		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths',
+		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths.',
 	},
 	GetStaticPathsDeprecatedRSS: {
 		code: 3012,
 		message:
 			'The RSS helper has been removed from getStaticPaths! Try the new @astrojs/rss package instead.',
-		hint: 'See https://docs.astro.build/en/guides/rss/ for more information',
+		hint: 'See https://docs.astro.build/en/guides/rss/ for more information.',
 	},
 	GetStaticPathsExpectedParams: {
 		code: 3013,
 		message: 'Missing or empty required params property on getStaticPaths route',
-		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths',
+		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths.',
 	},
 	GetStaticPathsInvalidRouteParam: {
 		code: 3014,
 		message: (key: string, value: any) =>
 			`Invalid getStaticPaths route parameter for \`${key}\`. Expected a string or number, received \`${typeof value}\` ("${value}")`,
-		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths',
+		hint: 'See https://docs.astro.build/en/reference/api-reference/#getstaticpaths for more information on getStaticPaths.',
 	},
 	GetStaticPathsRequired: {
 		code: 3015,
 		message:
 			'getStaticPaths() function is required for dynamic routes. Make sure that you `export` a `getStaticPaths` function from your dynamic route.',
-		hint: `See https://docs.astro.build/en/core-concepts/routing/#dynamic-routes for more information on dynamic routes
+		hint: `See https://docs.astro.build/en/core-concepts/routing/#dynamic-routes for more information on dynamic routes.
 
 Alternatively, set \`output: "server"\` in your Astro config file to switch to a non-static server build.
 See https://docs.astro.build/en/guides/server-side-rendering/ for more information on non-static rendering.`,
@@ -131,18 +131,18 @@ See https://docs.astro.build/en/guides/server-side-rendering/ for more informati
 	ReservedSlotName: {
 		code: 3016,
 		message: (slotName: string) =>
-			`Unable to create a slot named "${slotName}". ${slotName}" is a reserved slot name!\nPlease update the name of this slot.`,
+			`Unable to create a slot named "${slotName}". ${slotName}" is a reserved slot name! Please update the name of this slot.`,
 	},
 	NoAdapterInstalled: {
 		code: 3017,
 		message: `Cannot use \`output: 'server'\` without an adapter. Please install and configure the appropriate server adapter for your final deployment.`,
-		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/ for more information',
+		hint: 'See https://docs.astro.build/en/guides/server-side-rendering/ for more information.',
 	},
 	NoMatchingImport: {
 		code: 3018,
 		message: (componentName: string) =>
-			`Could not render ${componentName}. No matching import has been found for ${componentName}`,
-		hint: 'Please make sure the component is properly imported',
+			`Could not render ${componentName}. No matching import has been found for ${componentName}.`,
+		hint: 'Please make sure the component is properly imported.',
 	},
 	// CSS Errors - 4xxx
 	UnknownCSSError: {
@@ -157,14 +157,14 @@ See https://docs.astro.build/en/guides/server-side-rendering/ for more informati
 	},
 	FailedToLoadModuleSSR: {
 		code: 5001,
-		message: (importName: string) => `Could not import ${importName}`,
+		message: (importName: string) => `Could not import "${importName}".`,
 		hint: 'This is often caused by a typo in the import path. Please make sure the file exists.',
 	},
 	InvalidGlob: {
 		code: 5002,
 		message: (globPattern: string) =>
-			`Invalid glob pattern: "${globPattern}". Glob patterns must start with './', '../' or '/'`,
-		hint: 'See https://docs.astro.build/en/guides/imports/#glob-patterns for more information on supported glob patterns',
+			`Invalid glob pattern: "${globPattern}". Glob patterns must start with './', '../' or '/'.`,
+		hint: 'See https://docs.astro.build/en/guides/imports/#glob-patterns for more information on supported glob patterns.',
 	},
 	// Markdown Errors - 6xxx
 	UnknownMarkdownError: {
