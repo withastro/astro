39fc314288ef9bef16fa3b89c1cae7dd1cee9c18
Matthew Phillips <matthew@skypack.dev>
2022-11-10 15:51:00 -0800

    Remove unused code

--- packages/integrations/image/src/index.ts
+++ packages/integrations/image/src/index.ts
@@ -100,12 +100,6 @@ export default function integration(options: IntegrationOptions = {}): AstroInte
 				_buildConfig = config.build;
 			},
 			'astro:build:start': ({ buildConfig }) => {
-				const adapterName = _config.adapter?.name;
-				if (adapterName && UNSUPPORTED_ADAPTERS.has(adapterName)) {
-					throw new Error(
-						`@astrojs/image is not supported with the ${adapterName} adapter. Please choose a Node.js compatible adapter.`
-					);
-				}
 				// Backwards compat
 				if (needsBuildConfig) {
 					_buildConfig = buildConfig;
