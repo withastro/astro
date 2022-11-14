0f2033f6973a39ca5147a2a0ca9b652c2bf0e129
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-11-14 08:37:06 -0800

    test: will add extra test coverage after an RFC check

--- packages/integrations/image/test/fixtures/custom-loaders/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-
-// https://astro.build/config
-export default defineConfig({
-	site: 'http://localhost:3000',
-	integrations: [image({ serviceEntryPoint: '@test/image-lib/ssr' })]
-});
