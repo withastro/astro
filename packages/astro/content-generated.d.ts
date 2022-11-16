41df912c5cf00fee40003d3741acc7906aa1ceff
bholmesdev <hey@bholmes.dev>
2022-11-16 10:01:15 -0500

    feat: streamline type alias!

--- packages/astro/content-generated.d.ts
+++ packages/astro/content-generated.d.ts
@@ -1,23 +1,24 @@
-import { z } from 'zod';
-
-export declare const entryMap: {
+declare const entryMap: {
 	// @@ENTRY_MAP@@
 };
-export declare const schemaMap: {
+declare const schemaMap: {
 	// @@SCHEMA_MAP@@
 };
-export declare function getEntry<
-	C extends keyof typeof entryMap,
-	E extends keyof typeof entryMap[C]
->(collection: C, entryKey: E): Promise<typeof entryMap[C][E]>;
-export declare function getCollection<
-	C extends keyof typeof entryMap,
-	E extends keyof typeof entryMap[C]
->(
-	collection: C,
-	filter?: (data: typeof entryMap[C][E]) => boolean
-): Promise<typeof entryMap[C][keyof typeof entryMap[C]][]>;
-export declare function renderEntry<
-	C extends keyof typeof entryMap,
-	E extends keyof typeof entryMap[C]
->(entry: { collection: C; id: E }): Promise<{ Content: any }>;
+
+declare module 'astro:content' {
+	export function getEntry<C extends keyof typeof entryMap, E extends keyof typeof entryMap[C]>(
+		collection: C,
+		entryKey: E
+	): Promise<typeof entryMap[C][E]>;
+	export function getCollection<
+		C extends keyof typeof entryMap,
+		E extends keyof typeof entryMap[C]
+	>(
+		collection: C,
+		filter?: (data: typeof entryMap[C][E]) => boolean
+	): Promise<typeof entryMap[C][keyof typeof entryMap[C]][]>;
+	export function renderEntry<
+		C extends keyof typeof entryMap,
+		E extends keyof typeof entryMap[C]
+	>(entry: { collection: C; id: E }): Promise<{ Content: any }>;
+}
