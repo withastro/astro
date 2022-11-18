805517421e4be4644e56a5ea9a73a0746a2dabc7
bholmesdev <hey@bholmes.dev>
2022-11-18 17:59:38 -0500

    refactor: union -> enum

--- examples/docs/src/content/docs/index.ts
+++ examples/docs/src/content/docs/index.ts
@@ -6,7 +6,7 @@ export default defineCollection({
 		title: z.string().default(SITE.title),
 		description: z.string().default(SITE.description),
 		lang: z.literal('en-us').default(SITE.defaultLanguage),
-		dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
+		dir: z.enum(['ltr', 'rtl']).default('ltr'),
 		image: z
 			.object({
 				src: z.string(),
