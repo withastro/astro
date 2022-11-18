d981a5cb9c2aa899c9473c0b3d529997b3e8b79d
bholmesdev <hey@bholmes.dev>
2022-11-18 16:56:03 -0500

    feat: migrate examples/docs to content!

---
+++ examples/docs/src/content/docs/index.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from 'astro:content';
+import { SITE } from '../../config';
+
+export default defineCollection({
+	schema: z.object({
+		title: z.string().default(SITE.title),
+		description: z.string().default(SITE.description),
+		lang: z.literal('en-us').default(SITE.defaultLanguage),
+		dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
+		image: z
+			.object({
+				src: z.string(),
+				alt: z.string(),
+			})
+			.optional(),
+		ogLocale: z.string().optional(),
+	}),
+});
