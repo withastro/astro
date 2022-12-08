1aeabe417077505bc0cdb8d2e47366ddbc616072
Matthew Phillips <matthew@skypack.dev>
2022-12-08 08:08:27 -0500

    Fix Astro.params not having values when using base in SSR (#5553)
    * Fix Astro.params not having values when using base in SSR
    
    * Adding a changeseet

---
+++ packages/astro/test/ssr-params.test.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import * as cheerio from 'cheerio';
+import { loadFixture } from './test-utils.js';
+import testAdapter from './test-adapter.js';
+
+describe('Astro.params in SSR', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/ssr-params/',
+			adapter: testAdapter(),
+			output: 'server',
+			base: '/users/houston/',
+		});
+		await fixture.build();
+	});
+
+	it('Params are passed to component', async () => {
+		const app = await fixture.loadTestAdapterApp();
+		const request = new Request('http://example.com/users/houston/food');
+		const response = await app.render(request);
+		expect(response.status).to.equal(200);
+		const html = await response.text();
+		const $ = cheerio.load(html);
+		expect($('.category').text()).to.equal('food');
+	});
+
+});
