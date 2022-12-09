8dbd664b4b2f322d45ce886e3e82d69034b60771
Nate Moore <nate@astro.build>
2022-12-09 16:14:30 -0600

    feat: switch to `experimental_prerender`

--- packages/astro/src/vite-plugin-scanner/index.ts
+++ packages/astro/src/vite-plugin-scanner/index.ts
@@ -5,17 +5,16 @@ import { isPage, isEndpoint } from '../core/util.js';
 import * as eslexer from 'es-module-lexer';
 import { PageOptions } from '../vite-plugin-astro/types.js';
 
-const BOOLEAN_EXPORTS = new Set(['prerender']);
+const BOOLEAN_EXPORTS = new Set(['experimental_prerender']);
 
-// TODO: still needed?
 // Quick scan to determine if code includes recognized export
 // False positives are not a problem, so be forgiving!
-// function includesExport(code: string) {
-// 	for (const name of BOOLEAN_EXPORTS) {
-// 		if (code.includes(name)) return true;
-// 	}
-// 	return false;
-// }
+function includesExport(code: string) {
+	for (const name of BOOLEAN_EXPORTS) {
+		if (code.includes(name)) return true;
+	}
+	return false;
+}
 
 export default function astroScannerPlugin({ settings }: { settings: AstroSettings }): VitePlugin {
 	function normalizeFilename(filename: string) {
@@ -45,13 +44,15 @@ export default function astroScannerPlugin({ settings }: { settings: AstroSettin
 			const fileIsPage = isPage(fileURL, settings);
 			const fileIsEndpoint = isEndpoint(fileURL, settings);
 			if (!(fileIsPage || fileIsEndpoint)) return;
+			if (!includesExport(code)) return;
 
 			await eslexer.init;
 			const [_, exports] = eslexer.parse(code, id);
 			let pageOptions: PageOptions = {};
 			for (const e of exports) {
 				if (BOOLEAN_EXPORTS.has(e.n)) {
-					pageOptions[e.n as keyof PageOptions] = true;
+					const name = e.n.replace(/^experimental_/, '') as keyof PageOptions;
+					pageOptions[name] = true;
 					let expr = code.slice(e.le).trim().replace(/\=/, '').trim().split(/[;\n]/)[0];
 					if (expr !== 'true') {
 						// TODO: warn
