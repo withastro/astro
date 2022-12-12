4d9ef23b6745b28a92ca985a6a1d86b45c894f3c
Juan Martín Seery <me@juanm04.com>
2022-12-12 16:45:57 -0300

    fix(vercel): support node-fetch and Node 18 fetch (#5587)

--- packages/integrations/vercel/src/serverless/entrypoint.ts
+++ packages/integrations/vercel/src/serverless/entrypoint.ts
@@ -3,11 +3,20 @@ import type { SSRManifest } from 'astro';
 import { App } from 'astro/app';
 import type { IncomingMessage, ServerResponse } from 'node:http';
 
-import { getRequest, setResponse } from './request-transform.js';
+import * as requestTransformLegacy from './request-transform/legacy.js';
+import * as requestTransformNode18 from './request-transform/node18.js';
 
 polyfill(globalThis, {
 	exclude: 'window document',
 });
+
+// Node 18+ has a new API for request/response, while older versions use node-fetch
+// When we drop support for Node 14, we can remove the legacy code by switching to undici
+
+const nodeVersion = parseInt(process.version.split('.')[0].slice(1)); // 'v14.17.0' -> 14
+
+const { getRequest, setResponse } =
+	nodeVersion >= 18 ? requestTransformNode18 : requestTransformLegacy;
 
 export const createExports = (manifest: SSRManifest) => {
 	const app = new App(manifest);
