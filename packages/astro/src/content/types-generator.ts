43b684d2432ba67a6ce0106d1bd4bab6977fa75c
bholmesdev <hey@bholmes.dev>
2022-12-29 13:24:05 -0500

    fix: no config found when configPath is undefined

--- packages/astro/src/content/types-generator.ts
+++ packages/astro/src/content/types-generator.ts
@@ -14,6 +14,7 @@ import {
 	ContentPaths,
 	getContentPaths,
 	loadContentConfig,
+	NotFoundError,
 } from './utils.js';
 
 type ChokidarEvent = 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir';
@@ -68,6 +69,8 @@ export async function createContentTypesGenerator({
 	async function init() {
 		if (contentPaths.config) {
 			await handleEvent({ name: 'add', entry: contentPaths.config }, { logLevel: 'warn' });
+		} else {
+			contentConfigObserver.set({ status: 'error', error: new NotFoundError() });
 		}
 		const globResult = await glob('**', {
 			cwd: fileURLToPath(contentPaths.contentDir),
