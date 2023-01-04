95ccbc201829125666a2caf150e1bfcd7e0cd88e
bluwy <bjornlu.dev@gmail.com>
2023-01-04 22:46:04 +0800

    Use localhost as preview default host

--- packages/astro/src/core/preview/util.ts
+++ packages/astro/src/core/preview/util.ts
@@ -1,7 +1,7 @@
 export function getResolvedHostForHttpServer(host: string | boolean) {
 	if (host === false) {
 		// Use a secure default
-		return '127.0.0.1';
+		return 'localhost';
 	} else if (host === true) {
 		// If passed --host in the CLI without arguments
 		return undefined; // undefined typically means 0.0.0.0 or :: (listen on all IPs)
