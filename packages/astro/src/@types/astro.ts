d3b18ae5af16bfc5d3d2659d7d0084654772af6a
Sarah Rainsberger <sarah@rainsberger.ca>
2023-01-04 16:07:22 -0400

    tightened up wording

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -444,7 +444,7 @@ export interface AstroUserConfig {
 	 * @name base
 	 * @type {string}
 	 * @description
-	 * The base path you're deploying to. Astro will match this pathname during development so that your development experience matches your build environment as closely as possible. You can access this value in your app via `import.meta.env.BASE_URL`.
+	 * The base path you're deploying to. Astro will match this pathname during development so that your development experience matches your build environment. 
 	 *
 	 * In the example below, `astro dev` will start your server at `/docs`.
 	 *
