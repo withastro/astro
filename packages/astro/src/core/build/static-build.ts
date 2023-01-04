57596824268b2432cc60fe7daca66ca35952762e
wulinsheng123 <409187100@qq.com>
2023-01-04 02:02:21 +0000

    fix: change entryFileNames into assets folder

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -212,7 +212,7 @@ async function clientBuild(
 				input: Array.from(input),
 				output: {
 					format: 'esm',
-					entryFileNames: '[name].[hash].js',
+					entryFileNames: 'assets/[name].[hash].js',
 					chunkFileNames: 'assets/chunks/[name].[hash].js',
 					assetFileNames: 'assets/[name].[hash][extname]',
 					...viteConfig.build?.rollupOptions?.output,
