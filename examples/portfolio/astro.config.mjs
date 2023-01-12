779f3d46292ad533bd24d905bdf00ba300ea3aa4
Chris Swithinbank <swithinbank@gmail.com>
2023-01-12 15:41:42 +0100

    Remove `experimental.contentCollections` flag

--- examples/portfolio/astro.config.mjs
+++ examples/portfolio/astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	experimental: {
-		contentCollections: true,
-	},
-});
+export default defineConfig({});
