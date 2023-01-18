b2967e7734d3bac3507a0abf97fe503c2df8e767
Happydev <81974850+MoustaphaDev@users.noreply.github.com>
2023-01-18 02:49:15 +0000

    add `aria-hidden` to svgs
    Co-authored-by: Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>

--- packages/astro/src/core/errors/overlay.ts
+++ packages/astro/src/core/errors/overlay.ts
@@ -470,12 +470,12 @@ ${style.trim()}
       <div>
         <input type="checkbox" class="theme-toggle-checkbox" id="chk" />
         <label id="theme-toggle-label" for="chk">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon-tabler icon-tabler-sun" width="15px" height="15px" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="icon-tabler icon-tabler-sun" width="15px" height="15px" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <circle cx="12" cy="12" r="4" />
             <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
           </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon-tabler icon-tabler-moon"  width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="icon-tabler icon-tabler-moon"  width="15" height="15" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
           </svg>
