16dc36a870df47a4151a8ed2d91d0bd1bb812458
Nate Moore <natemoo-re@users.noreply.github.com>
2023-01-18 12:57:31 -0600

    Remove legacy compiler error handling (#5855)
    * refactor: remove legacy compiler handling
    
    * docs: add hint to unknown compiler error
    
    * feat: add link detection to error overlay
    
    * chore: update compiler
    
    * chore: update compiler issue link
    
    * Update packages/astro/src/core/errors/overlay.ts
    
    Co-authored-by: Erika <3019731+Princesseuh@users.noreply.github.com>
    
    Co-authored-by: Nate Moore <nate@astro.build>
    Co-authored-by: Erika <3019731+Princesseuh@users.noreply.github.com>

--- packages/astro/src/core/errors/overlay.ts
+++ packages/astro/src/core/errors/overlay.ts
@@ -522,6 +522,15 @@ class ErrorOverlay extends HTMLElement {
 
 		const el = this.root.querySelector(selector);
 
+		if (html) {
+			// Automatically detect links
+			text = text.split(' ').map(v => {
+				if (!v.startsWith('https://')) return v;
+				if (v.endsWith('.')) return `<a target="_blank" href="${v.slice(0, -1)}">${v.slice(0, -1)}</a>.`
+				return `<a target="_blank" href="${v}">${v}</a>`
+			}).join(' ');
+		}
+
 		if (el) {
 			if (!html) {
 				el.textContent = text.trim();
