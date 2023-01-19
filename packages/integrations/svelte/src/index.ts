d2cec972cec03d0318c438c06391645dcef36e08
bluwy <bjornlu.dev@gmail.com>
2023-01-19 17:17:02 +0800

    Make fallback

--- packages/integrations/svelte/src/index.ts
+++ packages/integrations/svelte/src/index.ts
@@ -1,7 +1,8 @@
 import type { Options } from '@sveltejs/vite-plugin-svelte';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import type { AstroIntegration, AstroRenderer } from 'astro';
 import type { UserConfig } from 'vite';
+import { fileURLToPath } from 'url';
 
 function getRenderer(): AstroRenderer {
 	return {
@@ -11,12 +12,24 @@ function getRenderer(): AstroRenderer {
 	};
 }
 
+async function svelteConfigHasPreprocess(root: URL) {
+	const svelteConfigFiles = ['./svelte.config.js', './svelte.config.cjs', './svelte.config.mjs'];
+	for (const file of svelteConfigFiles) {
+		const filePath = fileURLToPath(new URL(file, root));
+		try {
+			const config = (await import(filePath)).default;
+			return !!config.preprocess;
+		} catch {}
+	}
+}
+
 type ViteConfigurationArgs = {
 	isDev: boolean;
 	options?: Options | OptionsCallback;
+	root: URL;
 };
 
-function getViteConfiguration({ options, isDev }: ViteConfigurationArgs): UserConfig {
+async function getViteConfiguration({ options, isDev, root }: ViteConfigurationArgs): UserConfig {
 	const defaultOptions: Partial<Options> = {
 		emitCss: true,
 		compilerOptions: { dev: isDev, hydratable: true },
@@ -45,6 +58,10 @@ function getViteConfiguration({ options, isDev }: ViteConfigurationArgs): UserCo
 		};
 	}
 
+	if (!resolvedOptions.preprocess && !(await svelteConfigHasPreprocess(root))) {
+		resolvedOptions.preprocess = vitePreprocess();
+	}
+
 	return {
 		optimizeDeps: {
 			include: ['@astrojs/svelte/client.js'],
@@ -60,12 +77,13 @@ export default function (options?: Options | OptionsCallback): AstroIntegration 
 		name: '@astrojs/svelte',
 		hooks: {
 			// Anything that gets returned here is merged into Astro Config
-			'astro:config:setup': ({ command, updateConfig, addRenderer }) => {
+			'astro:config:setup': ({ command, updateConfig, addRenderer, config }) => {
 				addRenderer(getRenderer());
 				updateConfig({
 					vite: getViteConfiguration({
 						options,
 						isDev: command === 'dev',
+						root: config.root,
 					}),
 				});
 			},
@@ -73,4 +91,4 @@ export default function (options?: Options | OptionsCallback): AstroIntegration 
 	};
 }
 
-export { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+export { vitePreprocess };
