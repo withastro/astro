8d0e5694e45c33f63c0055478f8232c51ee0aff3
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-01-24 18:26:58 +0000

    add test

--- packages/astro/test/content-collections.test.js
+++ packages/astro/test/content-collections.test.js
@@ -187,6 +187,17 @@ describe('Content Collections', () => {
 		});
 	});
 
+	describe('With spaces in path', () => {
+		it('Does not throw', async () => {
+			const fixture = await loadFixture({ root: './fixtures/content with spaces in folder name/' });
+			try {
+				await fixture.build();
+			} catch (e) {
+				expect(e.message).to.not.contain('Failed to load url');
+			}
+		});
+	});
+
 	describe('SSR integration', () => {
 		let app;
 
