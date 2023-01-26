299ad4906078257881f241a80dc6c68c4c324241
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-01-26 17:17:55 +0100

    Fix `Astro.url.protocol` when using the @astrojs/node SSR adapter with HTTPS

---
+++ packages/integrations/node/test/url-protocol.test.js
@@ -0,0 +1,73 @@
+import { TLSSocket } from 'tls';
+import nodejs from '../dist/index.js';
+import { loadFixture, createRequestAndResponse } from './test-utils.js';
+import { expect } from 'chai';
+
+describe('URL protocol', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/url-protocol/',
+			output: 'server',
+			adapter: nodejs({ mode: 'standalone' }),
+		});
+		await fixture.build();
+	});
+
+	it('return http when non-secure', async () => {
+		const { handler } = await import('./fixtures/url-protocol/dist/server/entry.mjs');
+		let { req, res, text } = createRequestAndResponse({
+			url: '/',
+		});
+
+		handler(req, res);
+		req.send();
+
+		const html = await text();
+		expect(html).to.include('http:');
+	});
+
+	it('return https when secure', async () => {
+		const { handler } = await import('./fixtures/url-protocol/dist/server/entry.mjs');
+		let { req, res, text } = createRequestAndResponse({
+			socket: new TLSSocket(),
+			url: '/',
+		});
+
+		handler(req, res);
+		req.send();
+
+		const html = await text();
+		expect(html).to.include('https:');
+	});
+
+	it('return http when the X-Forwarded-Proto header is set to http', async () => {
+		const { handler } = await import('./fixtures/url-protocol/dist/server/entry.mjs');
+		let { req, res, text } = createRequestAndResponse({
+			headers: { 'X-Forwarded-Proto': 'http' },
+			url: '/',
+		});
+
+		handler(req, res);
+		req.send();
+
+		const html = await text();
+		expect(html).to.include('http:');
+	});
+
+	it('return https when the X-Forwarded-Proto header is set to https', async () => {
+		const { handler } = await import('./fixtures/url-protocol/dist/server/entry.mjs');
+		let { req, res, text } = createRequestAndResponse({
+			headers: { 'X-Forwarded-Proto': 'https' },
+			url: '/',
+		});
+
+		handler(req, res);
+		req.send();
+
+		const html = await text();
+		expect(html).to.include('https:');
+	});
+});
