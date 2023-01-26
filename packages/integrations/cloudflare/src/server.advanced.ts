f0b1840b28e77496efadee102edaf3b693cc76fe
Matthew Phillips <matthew@skypack.dev>
2023-01-26 12:23:46 -0500

    Prevent process proxy from running during prerender phase

--- packages/integrations/cloudflare/src/server.advanced.ts
+++ packages/integrations/cloudflare/src/server.advanced.ts
@@ -1,8 +1,10 @@
 import type { SSRManifest } from 'astro';
 import { App } from 'astro/app';
-import { getProcessEnvProxy } from './util.js';
+import { getProcessEnvProxy, isNode } from './util.js';
 
-process.env = getProcessEnvProxy();
+if(!isNode) {
+	process.env = getProcessEnvProxy();
+}
 
 type Env = {
 	ASSETS: { fetch: (req: Request) => Promise<Response> };
