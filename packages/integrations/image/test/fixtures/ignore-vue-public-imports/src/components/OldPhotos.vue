6490dab286743684b4d8c5e09a18b5760e77d572
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-02-01 15:00:22 +0000

    perf(test): replace heavy image with pixel

--- packages/integrations/image/test/fixtures/ignore-vue-public-imports/src/components/OldPhotos.vue
+++ packages/integrations/image/test/fixtures/ignore-vue-public-imports/src/components/OldPhotos.vue
@@ -2,7 +2,7 @@
   <div>
     <h2 class="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">Old Photos from Ute Valley Park</h2>
     <div class="grid md:grid-cols-3 gap-3">
-      <img class="image" src="/images/old-ute-valley-park-photos/old-ute-1.jpg" />      
+      <img class="image" src="/images/old-ute-valley-park-photos/old-ute-1.png" />      
     </div>
     <div v-if="!viewMore">
       <button class="text-base my-2 ml-1 text-green-900 rounded px-2 py-1 underline" @click="viewMore = true">
@@ -11,7 +11,7 @@
     </div>
     <div v-else>
       <div class="grid md:grid-cols-3 gap-3 mt-3">
-        <img class="image" src="/images/old-ute-valley-park-photos/old-ute-1.jpg" />
+        <img class="image" src="/images/old-ute-valley-park-photos/old-ute-1.png" />
       </div>
     </div>
   </div>
