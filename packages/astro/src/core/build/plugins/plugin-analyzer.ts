4f82f418f0d7b551632453b10a1b025cb95ce882
Matthew Phillips <matthew@skypack.dev>
2023-02-03 12:48:43 -0500

    fix

--- packages/astro/src/core/build/plugins/plugin-analyzer.ts
+++ packages/astro/src/core/build/plugins/plugin-analyzer.ts
@@ -8,9 +8,10 @@ import { prependForwardSlash } from '../../../core/path.js';
 import { getTopLevelPages, moduleIsTopLevelPage, walkParentInfos } from '../graph.js';
 import { getPageDataByViteID, trackClientOnlyPageDatas } from '../internal.js';
 import { PROPAGATED_ASSET_FLAG } from '../../../content/consts.js';
+import { pathToFileURL } from 'url';
 
 function isPropagatedAsset(id: string) {
-	return new URL('file://' + id).searchParams.has(PROPAGATED_ASSET_FLAG);
+	return pathToFileURL(id).searchParams.has(PROPAGATED_ASSET_FLAG);
 }
 
 export function vitePluginAnalyzer(internals: BuildInternals): VitePlugin {
