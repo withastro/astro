ceae51445ec9bd6fc154e402dd49bbe3ef3a4a30
Isaac <isaac.alexander.smith@gmail.com>
2023-02-06 13:24:47 -0400

    Update packages/astro/src/core/build/static-build.ts
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -61,7 +61,7 @@ export async function staticBuild(opts: StaticBuildOptions) {
 	// Empty out the dist folder, if needed. Vite has a config for doing this
 	// but because we are running 2 vite builds in parallel, that would cause a race
 	// condition, so we are doing it ourselves
-	if(!settings.config?.vite?.build?.emptyOutDir) {
+	if (settings.config?.vite?.build?.emptyOutDir !== false) {
 		emptyDir(settings.config.outDir, new Set('.git'));
 	}
 
