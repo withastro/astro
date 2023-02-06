7b81280923272fa16de7644e04209cc381432391
Isaac <isaac.alexander.smith@gmail.com>
2023-02-06 11:09:39 -0400

    Check vite.emptyOutDir before emptying dir

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -61,7 +61,9 @@ export async function staticBuild(opts: StaticBuildOptions) {
 	// Empty out the dist folder, if needed. Vite has a config for doing this
 	// but because we are running 2 vite builds in parallel, that would cause a race
 	// condition, so we are doing it ourselves
-	emptyDir(settings.config.outDir, new Set('.git'));
+	if(!settings.config?.vite?.emptyOutDir) {
+		emptyDir(settings.config.outDir, new Set('.git'));
+	}
 
 	// Register plugins
 	const container = createPluginContainer(opts, internals);
