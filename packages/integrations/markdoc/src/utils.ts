9f589a9c4b703575736b8f38c22f34148c80a1ae
bholmesdev <hey@bholmes.dev>
2023-02-07 16:01:34 -0500

    feat: addContentEntryType integration hook

---
+++ packages/integrations/markdoc/src/utils.ts
@@ -0,0 +1,25 @@
+import matter from 'gray-matter';
+import type { ErrorPayload as ViteErrorPayload } from 'vite';
+
+/**
+ * Match YAML exception handling from Astro core errors
+ * @see 'astro/src/core/errors.ts'
+ */
+export function parseFrontmatter(fileContents: string, filePath: string) {
+	try {
+		// `matter` is empty string on cache results
+		// clear cache to prevent this
+		(matter as any).clearCache();
+		return matter(fileContents);
+	} catch (e: any) {
+		if (e.name === 'YAMLException') {
+			const err: Error & ViteErrorPayload['err'] = e;
+			err.id = filePath;
+			err.loc = { file: e.id, line: e.mark.line + 1, column: e.mark.column };
+			err.message = e.reason;
+			throw err;
+		} else {
+			throw e;
+		}
+	}
+}
