2816b4f887d845a68d477031986ceb086270ff25
bholmesdev <hey@bholmes.dev>
2023-02-08 15:50:52 -0500

    deps: mdx

--- examples/with-markdoc/astro.config.mjs
+++ examples/with-markdoc/astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import markdoc from '@astrojs/markdoc';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [markdoc()],
+	integrations: [markdoc(), mdx()],
 });
