2b072101db5850f7360c0332a433c5fbeed425ff
bholmesdev <hey@bholmes.dev>
2023-02-13 15:10:39 -0500

    chore: remove unused deps

--- examples/with-markdoc/astro.config.mjs
+++ examples/with-markdoc/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import markdoc from '@astrojs/markdoc';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [markdoc(), mdx()],
+	integrations: [markdoc()],
 });
