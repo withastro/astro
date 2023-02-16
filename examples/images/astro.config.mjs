092c3485c94d62467cb8dcaf9eabe629ec405487
Princesseuh <princssdev@gmail.com>
2023-02-16 15:01:58 +0100

    config(vercel): Remove test image service

--- examples/images/astro.config.mjs
+++ examples/images/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	image: {
-		service: 'astro/image/services/sharp',
+		service: 'astro/image/services/squoosh',
 	},
 });
