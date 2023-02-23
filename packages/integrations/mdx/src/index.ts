075b87e8b72a69a608cd2ff1196dc0989e2cf1e1
Raphael Bossek <raphael.bossek@gmail.com>
2023-02-23 16:20:06 +0100

    Update MDX integration to es-module-lexer@1.1.1 (#6296)
    * Update MDX integration to es-module-lexer@1.1.1
    
    * Added missing pnpm-lock.yaml file
    
    * Create lucky-flowers-wonder.md
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- packages/integrations/mdx/src/index.ts
+++ packages/integrations/mdx/src/index.ts
@@ -112,13 +112,13 @@ export default function mdx(partialMdxOptions: Partial<MdxOptions> = {}): AstroI
 									}
 
 									const { fileUrl, fileId } = getFileInfo(id, config);
-									if (!moduleExports.includes('url')) {
+									if (!moduleExports.find(({n}) => n === 'url')) {
 										code += `\nexport const url = ${JSON.stringify(fileUrl)};`;
 									}
-									if (!moduleExports.includes('file')) {
+									if (!moduleExports.find(({n}) => n === 'file')) {
 										code += `\nexport const file = ${JSON.stringify(fileId)};`;
 									}
-									if (!moduleExports.includes('Content')) {
+									if (!moduleExports.find(({n}) => n === 'Content')) {
 										// Make `Content` the default export so we can wrap `MDXContent` and pass in `Fragment`
 										code = code.replace('export default MDXContent;', '');
 										code += `\nexport const Content = (props = {}) => MDXContent({
