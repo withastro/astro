3b2e624cf2d562ee0d8495c5d06dccf750ad3cf7
bluwy <bluwy@users.noreply.github.com>
2023-02-26 01:53:18 +0000

    [ci] format

--- packages/create-astro/src/actions/template.ts
+++ packages/create-astro/src/actions/template.ts
@@ -7,9 +7,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { error, info, spinner, title } from '../messages.js';
 
-export async function template(
-	ctx: Pick<Context, 'template' | 'prompt' | 'dryRun' | 'exit'>
-) {
+export async function template(ctx: Pick<Context, 'template' | 'prompt' | 'dryRun' | 'exit'>) {
 	if (!ctx.template) {
 		const { template: tmpl } = await ctx.prompt({
 			name: 'template',
