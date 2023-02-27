754c5ca9aa93d4e8674059ce79f6b694c147db83
Nate Moore <natemoo-re@users.noreply.github.com>
2023-02-27 10:09:54 -0600

    Respect original `package.json` indentation (#6375)
    * fix(#6338): respect original indentation
    
    * chore: add changeset

--- packages/create-astro/src/actions/template.ts
+++ packages/create-astro/src/actions/template.ts
@@ -50,17 +50,19 @@ const FILES_TO_UPDATE = {
 	'package.json': (file: string, overrides: { name: string }) =>
 		fs.promises
 			.readFile(file, 'utf-8')
-			.then((value) =>
+			.then((value) => {
+				// Match first indent in the file or fallback to `\t`
+				const indent = /(^\s+)/m.exec(value)?.[1] ?? '\t';
 				fs.promises.writeFile(
 					file,
 					JSON.stringify(
 						Object.assign(JSON.parse(value), Object.assign(overrides, { private: undefined })),
 						null,
-						'\t'
+						indent
 					),
 					'utf-8'
 				)
-			),
+			}),
 };
 
 export default async function copyTemplate(tmpl: string, ctx: Context) {
