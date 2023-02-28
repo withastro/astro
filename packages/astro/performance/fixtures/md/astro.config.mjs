b6f752a3ee99edff54685d82f7416d815f54cb71
bholmesdev <hey@bholmes.dev>
2023-02-28 12:34:37 -0500

    feat: add react-component render benchmark

--- packages/astro/performance/fixtures/md/astro.config.mjs
+++ packages/astro/performance/fixtures/md/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc(), react()],
+  integrations: [react()]
 });
