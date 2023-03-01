4186664db0bc33617b725de5826942dd130a1581
bholmesdev <hey@bholmes.dev>
2023-03-01 14:21:08 -0500

    test: update fixture to match new config

--- packages/integrations/markdoc/test/fixtures/content-collections/astro.config.mjs
+++ packages/integrations/markdoc/test/fixtures/content-collections/astro.config.mjs
@@ -3,5 +3,22 @@ import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [markdoc()],
+	integrations: [markdoc({
+		variables: {
+			showMarquee: true,
+		},
+		tags: {
+			mq: {
+				render: 'marquee',
+				attributes: {
+					direction: {
+						type: String,
+						default: 'left',
+						matches: ['left', 'right', 'up', 'down'],
+						errorLevel: 'critical',
+					},
+				},
+			},
+		}
+	})],
 });
