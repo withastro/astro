5e755158d537db08f36cc6291d58d738a877e10d
bholmesdev <hey@bholmes.dev>
2023-03-02 13:53:55 -0500

    test: update Markdoc node config test

--- packages/integrations/markdoc/test/fixtures/content-collections/astro.config.mjs
+++ packages/integrations/markdoc/test/fixtures/content-collections/astro.config.mjs
@@ -3,22 +3,5 @@ import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [markdoc({
-		variables: {
-			showMarquee: true,
-		},
-		tags: {
-			mq: {
-				render: 'marquee',
-				attributes: {
-					direction: {
-						type: String,
-						default: 'left',
-						matches: ['left', 'right', 'up', 'down'],
-						errorLevel: 'critical',
-					},
-				},
-			},
-		}
-	})],
+	integrations: [markdoc()],
 });
