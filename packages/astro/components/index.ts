3ba14abe4e13872c6d9cb594b6adc8ceee96f2c3
Princesseuh <princssdev@gmail.com>
2023-03-03 14:44:41 +0100

    fix(images): Remove export from astro/components

--- packages/astro/components/index.ts
+++ packages/astro/components/index.ts
@@ -1,4 +1,2 @@
 export { default as Code } from './Code.astro';
 export { default as Debug } from './Debug.astro';
-// TODO: Uncomment this once enhanced images are not experimental anymore
-// export { default as Image } from './Image.astro';
