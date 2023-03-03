a487022a3759bf7b314f73fde8aac50cce1a7bd4
userquin <userquin@gmail.com>
2023-03-03 23:18:57 +0100

    chore: update deps and example

--- examples/with-vite-plugin-pwa/astro.config.mjs
+++ examples/with-vite-plugin-pwa/astro.config.mjs
@@ -5,29 +5,38 @@ import AstroPWA from '@vite-pwa/astro';
 export default defineConfig({
 	integrations: [
 		AstroPWA({
-			mode: 'development',
+			// mode: 'development',
 			base: '/',
 			scope: '/',
 			registerType: 'autoUpdate',
 			manifest: {
+				id: '/',
+				start_url: '/',
+				display: 'standalone',
 				name: 'Astro PWA',
 				short_name: 'Astro PWA',
 				theme_color: '#ffffff',
+				background_color: '#ffffff',
 				icons: [
 					{
-						src: 'favicon.svg',
-						sizes: 'any',
-						type: 'image/svg+xml',
-						purpose: 'any maskable',
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
 					},
 				],
 			},
 			workbox: {
-				globPatterns: ['**/*.{css,js,html,svg}'],
+				navigateFallback: '/404',
+				globPatterns: ['**/*.{css,js,html,svg,png,ico}'],
 			},
 			devOptions: {
 				enabled: true,
-				navigateFallback: '/',
+				navigateFallbackAllowlist: [/^\/404$/],
 			},
 		}),
 	],
