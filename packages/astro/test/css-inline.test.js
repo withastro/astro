f4025eba89aac594fdff48b17251c4bca4e2a8db
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-05 15:30:42 +0000

    test: add another expectation

--- packages/astro/test/css-inline.test.js
+++ packages/astro/test/css-inline.test.js
@@ -18,6 +18,8 @@ describe('Importing raw/inlined CSS', () => {
 			const $ = cheerio.load(html);
 
 			expect($('#inline').text()).to.contain('tomato');
+			expect($('link[rel=stylesheet]')).to.have.lengthOf(1);
+			expect($('style')).to.have.lengthOf(0);
 		});
 
 		it('?raw is imported as a string', async () => {
@@ -25,6 +27,8 @@ describe('Importing raw/inlined CSS', () => {
 			const $ = cheerio.load(html);
 
 			expect($('#raw').text()).to.contain('plum');
+			expect($('link[rel=stylesheet]')).to.have.lengthOf(1);
+			expect($('style')).to.have.lengthOf(0);
 		});
 	});
 
@@ -43,7 +47,6 @@ describe('Importing raw/inlined CSS', () => {
 		it("?inline is imported as string and doesn't make css bundled ", async () => {
 			const response = await fixture.fetch('/');
 			const html = await response.text();
-			console.log(html);
 			const $ = cheerio.load(html);
 
 			expect($('#inline').text()).to.contain('tomato');
