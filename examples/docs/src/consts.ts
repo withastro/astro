8ebf4b7481ad783243badec6866aeb4cdd4005c3
Nate Moore <natemoo-re@users.noreply.github.com>
2023-03-06 14:03:33 -0600

    chore: update branding assets (#6436)

--- examples/docs/src/consts.ts
+++ examples/docs/src/consts.ts
@@ -6,7 +6,7 @@ export const SITE = {
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
+		src: 'https://github.com/withastro/astro/blob/main/.github/assets/banner.png?raw=true',
 		alt:
 			'astro logo on a starry expanse of space,' +
 			' with a purple saturn-like planet floating in the right foreground',
