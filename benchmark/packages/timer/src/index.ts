00a0af7ed44f5bb275f570ca38bbd0b798ce84ad
Bjorn Lu <bjornlu.dev@gmail.com>
2023-03-07 10:52:47 +0800

    Move benchmark package and update changeset config (#6433)

--- benchmark/packages/timer/src/index.ts
+++ benchmark/packages/timer/src/index.ts
@@ -2,22 +2,22 @@ import type { AstroAdapter, AstroIntegration } from 'astro';
 
 export function getAdapter(): AstroAdapter {
 	return {
-		name: '@astrojs/timer',
-		serverEntrypoint: '@astrojs/timer/server.js',
-		previewEntrypoint: '@astrojs/timer/preview.js',
+		name: '@benchmark/timer',
+		serverEntrypoint: '@benchmark/timer/server.js',
+		previewEntrypoint: '@benchmark/timer/preview.js',
 		exports: ['handler'],
 	};
 }
 
 export default function createIntegration(): AstroIntegration {
 	return {
-		name: '@astrojs/timer',
+		name: '@benchmark/timer',
 		hooks: {
 			'astro:config:setup': ({ updateConfig }) => {
 				updateConfig({
 					vite: {
 						ssr: {
-							noExternal: ['@astrojs/timer'],
+							noExternal: ['@benchmark/timer'],
 						},
 					},
 				});
@@ -26,7 +26,8 @@ export default function createIntegration(): AstroIntegration {
 				setAdapter(getAdapter());
 
 				if (config.output === 'static') {
-					console.warn(`[@astrojs/timer] \`output: "server"\` is required to use this adapter.`);
+					// eslint-disable-next-line no-console
+					console.warn(`[@benchmark/timer] \`output: "server"\` is required to use this adapter.`);
 				}
 			},
 		},
