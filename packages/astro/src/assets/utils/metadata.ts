d3ccc9cc8b13089c4b03e7ef66f94e6b2496f84b
Matthew Phillips <matthew@skypack.dev>
2023-03-09 13:07:29 -0500

    Try using an import

--- packages/astro/src/assets/utils/metadata.ts
+++ packages/astro/src/assets/utils/metadata.ts
@@ -1,9 +1,7 @@
-import { createRequire } from 'module';
 import fs from 'node:fs/promises';
-import { fileURLToPath, pathToFileURL } from 'node:url';
+import { fileURLToPath } from 'node:url';
 import { ImageMetadata, InputFormat } from '../types.js';
-const require = createRequire(getModuleURL(import.meta.url));
-const sizeOf = require('image-size');
+import sizeOf from 'image-size';
 
 export interface Metadata extends ImageMetadata {
 	orientation?: number;
@@ -36,17 +34,4 @@ export async function imageMetadata(
 		format: type as InputFormat,
 		orientation,
 	};
-}
-
-/**
-* On certain serverless hosts, our ESM bundle is transpiled to CJS before being run, which means
-* import.meta.url is undefined, so we'll fall back to __dirname in those cases
-* We should be able to remove this once https://github.com/netlify/zip-it-and-ship-it/issues/750 is fixed
-*/
-export function getModuleURL(url: string | undefined): string {
-	if (!url) {
-		return pathToFileURL(__dirname).toString();
-	}
-
- return url
 }
