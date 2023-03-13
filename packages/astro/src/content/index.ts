87d5e96da4463b712dcb087c50fa7587de48203f
bholmesdev <bholmesdev@users.noreply.github.com>
2023-03-13 18:19:00 +0000

    [ci] format

--- packages/astro/src/content/index.ts
+++ packages/astro/src/content/index.ts
@@ -1,7 +1,7 @@
+export { errorMap } from './error-map.js';
 export { attachContentServerListeners } from './server-listeners.js';
 export { createContentTypesGenerator } from './types-generator.js';
 export { contentObservable, getContentPaths, getDotAstroTypeReference } from './utils.js';
 export { astroContentAssetPropagationPlugin } from './vite-plugin-content-assets.js';
 export { astroContentImportPlugin } from './vite-plugin-content-imports.js';
 export { astroContentVirtualModPlugin } from './vite-plugin-content-virtual-mod.js';
-export { errorMap } from './error-map.js';
