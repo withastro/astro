68988425119255382f94c983796574050006f003
Chloe A <solely@riseup.net>
2023-03-20 23:44:58 +0500

    fix: Add missing --watch flag when running astro check --help (#6586)
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/astro/src/cli/check/index.ts
+++ packages/astro/src/cli/check/index.ts
@@ -89,7 +89,10 @@ export async function check(
 			commandName: 'astro check',
 			usage: '[...flags]',
 			tables: {
-				Flags: [['--help (-h)', 'See all available flags.']],
+				Flags: [
+					['--watch', 'Watch Astro files for changes and re-run checks.'],
+					['--help (-h)', 'See all available flags.']
+				],
 			},
 			description: `Runs diagnostics against your project and reports errors to the console.`,
 		});
