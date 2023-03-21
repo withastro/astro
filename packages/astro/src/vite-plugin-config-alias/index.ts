9099d1059f66b8c3d6264021f9cb0f7108fdb3f8
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-03-21 17:51:42 +0000

    use existing resolvers when possible

--- packages/astro/src/vite-plugin-config-alias/index.ts
+++ packages/astro/src/vite-plugin-config-alias/index.ts
@@ -72,16 +72,19 @@ export default function configAliasVitePlugin({
 				};
 			}
 		},
-		resolveId(id) {
+		async resolveId(id, importer, options) {
 			if (id.startsWith('.') || id.startsWith('/')) return;
 
 			// Handle baseUrl mapping for non-relative and non-root imports.
 			// Since TypeScript only applies `baseUrl` autocompletions for files that exist
 			// in the filesystem only, we can use this heuristic to skip resolve if needed.
 			const resolved = path.posix.join(resolvedBaseUrl, id);
-			if (fs.existsSync(resolved)) {
-				return resolved;
-			}
+
+			const resolvedAliasedId = await this.resolve(resolved, importer, {
+				skipSelf: true,
+				...options,
+			});
+			if (resolvedAliasedId) return resolvedAliasedId;
 		},
 	};
 }
