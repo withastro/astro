8726aa3ad0678559cb717035042e35b1fb38f458
bholmesdev <hey@bholmes.dev>
2023-03-23 08:48:56 -0400

    Revert "Fix tsconfig alias regression (#6617)"
    This reverts commit 38e6ec21e266ad8765d8ca2293034123b34e839a.

--- packages/astro/src/vite-plugin-config-alias/index.ts
+++ packages/astro/src/vite-plugin-config-alias/index.ts
@@ -71,18 +71,16 @@ export default function configAliasVitePlugin({
 				};
 			}
 		},
-		async resolveId(id, importer, options) {
-			if (id.startsWith('.') || path.isAbsolute(id)) return;
+		resolveId(id) {
+			if (id.startsWith('.') || id.startsWith('/')) return;
 
 			// Handle baseUrl mapping for non-relative and non-root imports.
 			// Since TypeScript only applies `baseUrl` autocompletions for files that exist
 			// in the filesystem only, we can use this heuristic to skip resolve if needed.
 			const resolved = path.posix.join(resolvedBaseUrl, id);
-
-			return await this.resolve(resolved, importer, {
-				skipSelf: true,
-				...options,
-			});
+			if (fs.existsSync(resolved)) {
+				return resolved;
+			}
 		},
 	};
 }
