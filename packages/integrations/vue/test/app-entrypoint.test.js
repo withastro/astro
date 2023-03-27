c5ab942b434a6858cd74436252e88f325ee6eb07
wuls <linsheng.wu@beantechs.com>
2023-03-27 18:41:19 +0800

    add fix

--- packages/integrations/vue/test/app-entrypoint.test.js
+++ packages/integrations/vue/test/app-entrypoint.test.js
@@ -1,7 +1,10 @@
 import { loadFixture } from './test-utils.js';
 import { expect } from 'chai';
+import * as cheerio from 'cheerio';
 import { parseHTML } from 'linkedom';
-
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 describe('App Entrypoint', () => {
 	/** @type {import('./test-utils').Fixture} */
 	let fixture;
@@ -31,4 +34,22 @@ describe('App Entrypoint', () => {
 		const js = await fixture.readFile(client);
 		expect(js).to.match(/\w+\.component\(\"Bar\"/gm);
 	});
+	it('testing async vues setup when it has a await in the setup', async () => {
+		const data = await fixture.fetch('/suspense/index.html');
+		const $ = cheerio.load(data);
+		expect($('#test').html()).to.equal(2);
+		// const client = dom.innerHTML
+		// expect(client).to.eq('2');
+	});
 });
+
+describe('dev', async () => {
+	let devServer;
+
+	before(async () => {
+		devServer = await fixture.startDevServer();
+	});
+
+	after(async () => {
+		await devServer.stop();
+	});
---
+++ packages/integrations/vue/test/fixtures/app-entrypoint/src/components/Test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+/** The top-level await causes event handling to not work (button click) */
+await new Promise((resolve) => setTimeout(resolve, 10));
+
+const a = ref(1);
+
+onMounted(()=>{
+  a.value = 2
+})
+
+</script>
+
+<template>
+  <div id="test">
+   {{ a }}
+  </div>
+</template>
