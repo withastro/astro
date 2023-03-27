14962b31ec22b52c3b87b0d83bff7cabe66e1af0
Princesseuh <princssdev@gmail.com>
2023-03-27 15:31:29 +0200

    fix(images): Fix schema preprocessing logic returning undefined when the file doesn't exist

--- packages/astro/src/content/utils.ts
+++ packages/astro/src/content/utils.ts
@@ -130,10 +130,12 @@ export async function getEntryData(
 					object[schemaName] = z.preprocess(
 						async (value: unknown) => {
 							if (!value || typeof value !== 'string') return value;
-							return (await resolver(value))?.id;
+							return (
+								(await resolver(value))?.id ?? path.join(path.dirname(entry._internal.filePath))
+							);
 						},
 						schema,
-						{ description: undefined }
+						{ description: '__image' }
 					);
 				} else if ('shape' in schema) {
 					await preprocessAssetPaths(schema.shape);
