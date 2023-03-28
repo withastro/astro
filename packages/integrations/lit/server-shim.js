b6df0232cfd3dbaf33af2657fd2b13417971bebc
Elliott Marquez <5981958+e111077@users.noreply.github.com>
2023-03-27 17:00:41 -0700

    delete unnecessary lit shim checks

--- packages/integrations/lit/server-shim.js
+++ packages/integrations/lit/server-shim.js
@@ -1,16 +1,4 @@
-import { installWindowOnGlobal } from '@lit-labs/ssr/lib/dom-shim.js';
-
-if (typeof fetch === 'function') {
-	const _fetch = fetch;
-	installWindowOnGlobal();
-	globalThis.fetch = _fetch;
-} else {
-	installWindowOnGlobal();
-}
-
 document.getElementsByTagName = () => [];
-// See https://github.com/lit/lit/issues/2393
-document.currentScript = null;
 
 const ceDefine = customElements.define;
 customElements.define = function (tagName, Ctr) {
