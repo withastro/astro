d59e511d16482cfc7067555dd0a456098ec69e30
wulinsheng123 <409187100@qq.com>
2023-04-01 01:42:03 +0800

    supporting top of await  (#6671)
    * add fix
    
    * fix
    
    * revert verison
    
    * fix fn is undefined g
    
    * add e2e test
    
    * fix unit test
    
    * delete redundant code
    
    * add changeset
    
    ---------
    
    Co-authored-by: wuls <linsheng.wu@beantechs.com>

---
+++ .changeset/spotty-keys-change.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/vue': patch
+---
+
+Supporting the top of the await syntax sugar for Vue in the template's setup
--- packages/integrations/vue/client.js
+++ packages/integrations/vue/client.js
@@ -1,4 +1,4 @@
-import { h, createSSRApp, createApp } from 'vue';
+import { h, createSSRApp, createApp, Suspense } from 'vue';
 import { setup } from 'virtual:@astrojs/vue/app';
 import StaticHtml from './static-html.js';
 
@@ -13,13 +13,26 @@ export default (element) =>
 		for (const [key, value] of Object.entries(slotted)) {
 			slots[key] = () => h(StaticHtml, { value, name: key === 'default' ? undefined : key });
 		}
+
+		let content = h(Component, props, slots);
+		// related to https://github.com/withastro/astro/issues/6549
+		// if the component is async, wrap it in a Suspense component
+		if (isAsync(Component.setup)) {
+			content = h(Suspense, null, content)
+		}
+		
 		if (client === 'only') {
-			const app = createApp({ name, render: () => h(Component, props, slots) });
+			const app = createApp({ name, render: () => content });
 			await setup(app);
 			app.mount(element, false);
 		} else {
-			const app = createSSRApp({ name, render: () => h(Component, props, slots) });
+			const app = createSSRApp({ name, render: () => content });
 			await setup(app);
 			app.mount(element, true);
 		}
 	};
+
+function isAsync (fn) {
+	const constructor = fn?.constructor
+	return constructor && constructor.name === 'AsyncFunction';
+}
