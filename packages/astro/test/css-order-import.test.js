3376aa96b70494c6dac41ae5fb4a8fb7129637d2
bluwy <bjornlu.dev@gmail.com>
2023-04-05 21:38:28 +0800

    Add more tests

--- packages/astro/test/css-order-import.test.js
+++ packages/astro/test/css-order-import.test.js
@@ -117,6 +117,14 @@ describe('CSS ordering - import order', () => {
 				'CSS from Client2.jsx leaked into index.astro when chunking'
 			);
 		});
+
+		it('dedupe css between astro and framework components', async () => {
+			let html = await fixture.readFile('/dedupe/index.html');
+
+			const content = await Promise.all(getLinks(html).map((href) => getLinkContent(href)));
+			const css = content.map((c) => c.css).join('');
+			expect(css.match(/\.astro-jsx/)?.length).to.eq(1, '.astro-jsx class is duplicated');
+		});
 	});
 
 	describe('Dynamic import', () => {
