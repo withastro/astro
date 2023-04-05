c5cf65614e096b71144987d06094bb240d91e114
bholmesdev <hey@bholmes.dev>
2023-04-05 09:45:41 -0400

    wip: playing with api ideas

--- packages/astro/test/fixtures/content/src/content/config.ts
+++ packages/astro/test/fixtures/content/src/content/config.ts
@@ -1,10 +1,38 @@
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
 
 const blog = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string().max(60, 'For SEO purposes, keep descriptions short!'),
+		authors: reference('authors', { relation: 'many' }),
 	}),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+	type: 'data',
+	getId({ fileId }) {
+		return fileId.replace(/\.json$/, '');
+	},
+	schema: z.object({
+		name: z.string(),
+		website: z.string().url(),
+	}),
+});
+
+export const collections = { blog, authors };
+
+// i18n ideas??
+
+const i18n = defineCollection({
+	type: 'data',
+	schema: z.record(z.string()),
+});
+
+function withI18n<T extends z.ZodObject<any>>(schema: T) {
+	return ({ id }: { id: string }) =>
+		z
+			.object({
+				i18n: reference('i18n', { default: id.split('/')[0] }),
+			})
+			.extend(schema);
+}
