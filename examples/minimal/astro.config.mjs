b60ca13ff3d2e8e0966db9716c4eee56d7eb8a33
bholmesdev <hey@bholmes.dev>
2023-04-07 10:53:40 -0400

    chore: remove minimal example change

--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,19 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	integrations: [
-		{
-			name: 'server-middlewares',
-			hooks: {
-				'astro:server:setup'({ server }) {
-					console.log('Server setup');
-					server.middlewares.use((req, res, next) => {
-						console.log('Server middleware');
-						next();
-					});
-				},
-			},
-		},
-	],
-});
+export default defineConfig({});
