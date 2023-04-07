5d09d416b76fae20bd31598192563aa12c60b8c0
bholmesdev <hey@bholmes.dev>
2023-04-07 10:45:33 -0400

    test: middleware from astro:server:setup

--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [
+		{
+			name: 'server-middlewares',
+			hooks: {
+				'astro:server:setup'({ server }) {
+					console.log('Server setup');
+					server.middlewares.use((req, res, next) => {
+						console.log('Server middleware');
+						next();
+					});
+				},
+			},
+		},
+	],
+});
