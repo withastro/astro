0b118727a41692631de5be0c789f9fd0e9a6a263
bluwy <bjornlu.dev@gmail.com>
2023-04-11 21:11:08 +0800

    Support tsconfig aliases in CSS `@import`

--- packages/astro/test/alias-tsconfig.test.js
+++ packages/astro/test/alias-tsconfig.test.js
@@ -1,17 +1,37 @@
 import { expect } from 'chai';
 import * as cheerio from 'cheerio';
-import { isWindows, loadFixture } from './test-utils.js';
+import { loadFixture } from './test-utils.js';
 
 describe('Aliases with tsconfig.json', () => {
 	let fixture;
+
+	/**
+	 * @param {string} html
+	 * @returns {string[]}
+	 */
+	function getLinks(html) {
+		let $ = cheerio.load(html);
+		let out = [];
+		$('link[rel=stylesheet]').each((i, el) => {
+			out.push($(el).attr('href'));
+		});
+		return out;
+	}
+
+	/**
+	 * @param {string} href
+	 * @returns {Promise<{ href: string; css: string; }>}
+	 */
+	async function getLinkContent(href, f = fixture) {
+		const css = await f.readFile(href);
+		return { href, css };
+	}
 
 	before(async () => {
 		fixture = await loadFixture({
 			root: './fixtures/alias-tsconfig/',
 		});
 	});
-
-	if (isWindows) return;
 
 	describe('dev', () => {
 		let devServer;
@@ -50,13 +70,66 @@ describe('Aliases with tsconfig.json', () => {
 			expect($('#namespace').text()).to.equal('namespace');
 		});
 
-		// TODO: fix this https://github.com/withastro/astro/issues/6551
-		it.skip('works in css @import', async () => {
+		it('works in css @import', async () => {
 			const html = await fixture.fetch('/').then((res) => res.text());
-			console.log(html);
 			// imported css should be bundled
 			expect(html).to.include('#style-red');
 			expect(html).to.include('#style-blue');
+		});
+
+		it('works in components', async () => {
+			const html = await fixture.fetch('/').then((res) => res.text());
+			const $ = cheerio.load(html);
+
+			expect($('#alias').text()).to.equal('foo');
+		});
+	});
+
+	describe('build', () => {
+		before(async () => {
+			await fixture.build();
+		});
+
+		it('can load client components', async () => {
+			const html = await fixture.readFile('/index.html');
+			const $ = cheerio.load(html);
+
+			// Should render aliased element
+			expect($('#client').text()).to.equal('test');
+
+			const scripts = $('script').toArray();
+			expect(scripts.length).to.be.greaterThan(0);
+		});
+
+		it('can load via baseUrl', async () => {
+			const html = await fixture.readFile('/index.html');
+			const $ = cheerio.load(html);
+
+			expect($('#foo').text()).to.equal('foo');
+			expect($('#constants-foo').text()).to.equal('foo');
+		});
+
+		it('can load namespace packages with @* paths', async () => {
+			const html = await fixture.readFile('/index.html');
+			const $ = cheerio.load(html);
+
+			expect($('#namespace').text()).to.equal('namespace');
+		});
+
+		it('works in css @import', async () => {
+			const html = await fixture.readFile('/index.html');
+			const content = await Promise.all(getLinks(html).map((href) => getLinkContent(href)));
+			const [{ css }] = content;
+			// imported css should be bundled
+			expect(css).to.include('#style-red');
+			expect(css).to.include('#style-blue');
+		});
+
+		it('works in components', async () => {
+			const html = await fixture.readFile('/index.html');
+			const $ = cheerio.load(html);
+
+			expect($('#alias').text()).to.equal('foo');
 		});
 	});
 });
