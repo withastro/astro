14cff4ce19f342f42ecdbc23ea6a7577d7e5c185
bluwy <bjornlu.dev@gmail.com>
2023-04-12 19:46:33 +0800

    Add more logs

--- packages/astro/src/vite-plugin-load-fallback/index.ts
+++ packages/astro/src/vite-plugin-load-fallback/index.ts
@@ -14,6 +14,9 @@ export default function loadFallbackPlugin({
 	fs,
 	root,
 }: LoadFallbackPluginParams): vite.Plugin[] | false {
+	// @ts-expect-error check default
+	console.log('test123', !fs, fs === nodeFs, fs.default === nodeFs);
+
 	// Only add this plugin if a custom fs implementation is provided.
 	// Also check for `fs.default` because `import * as fs from 'fs'` will
 	// export as so, which only it's `.default` would === `nodeFs`.
@@ -22,7 +25,7 @@ export default function loadFallbackPlugin({
 		return false;
 	}
 
-	console.log(fs)
+	console.log('test456', fs);
 
 	const tryLoadModule = async (id: string) => {
 		try {
@@ -47,6 +50,7 @@ export default function loadFallbackPlugin({
 			name: 'astro:load-fallback',
 			enforce: 'post',
 			async resolveId(id, parent) {
+				console.log('test789', id, fs);
 				// See if this can be loaded from our fs
 				if (parent) {
 					const candidateId = npath.posix.join(npath.posix.dirname(slashify(parent)), id);
