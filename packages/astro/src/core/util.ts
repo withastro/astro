c086fd1054734ed22056bbd033d3e2be9bd7299d
bluwy <bjornlu.dev@gmail.com>
2023-04-19 15:54:57 +0200

    Fix CI fail

--- packages/astro/src/core/util.ts
+++ packages/astro/src/core/util.ts
@@ -160,9 +160,9 @@ export function rootRelativePath(root: URL, idOrUrl: URL | string) {
 	}
 	const normalizedRoot = normalizePath(fileURLToPath(root));
 	if (id.startsWith(normalizedRoot)) {
-		return id.slice(normalizedRoot.length);
+		id = id.slice(normalizedRoot.length);
 	}
-	return id;
+	return prependForwardSlash(id);
 }
 
 export function emoji(char: string, fallback: string) {
