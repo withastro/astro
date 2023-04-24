b24607069ed1b305b47ca25171e682f7c71603eb
chlorine <nicksonlvqq@gmail.com>
2023-04-24 13:25:03 +0800

    chore: cleanup code (#6897)

--- packages/astro/src/core/build/generate.ts
+++ packages/astro/src/core/build/generate.ts
@@ -89,7 +89,7 @@ export async function generatePages(opts: StaticBuildOptions, internals: BuildIn
 	const serverEntry = opts.buildConfig.serverEntry;
 	const outFolder = ssr ? opts.buildConfig.server : getOutDirWithinCwd(opts.settings.config.outDir);
 
-	if (opts.settings.config.output === 'server' && !hasPrerenderedPages(internals)) return;
+	if (ssr && !hasPrerenderedPages(internals)) return;
 
 	const verb = ssr ? 'prerendering' : 'generating';
 	info(opts.logging, null, `\n${bgGreen(black(` ${verb} static routes `))}`);
@@ -98,7 +98,7 @@ export async function generatePages(opts: StaticBuildOptions, internals: BuildIn
 	const ssrEntry = await import(ssrEntryURL.toString());
 	const builtPaths = new Set<string>();
 
-	if (opts.settings.config.output === 'server') {
+	if (ssr) {
 		for (const pageData of eachPrerenderedPageData(internals)) {
 			await generatePage(opts, internals, pageData, ssrEntry, builtPaths);
 		}
