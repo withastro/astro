ebae1eaf87f49399036033c673b513338f7d9c42
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-04-27 18:16:40 +0200

    feat(image): Update image service config to allow passing settings to the service (#6848)

--- packages/astro/test/core-image.test.js
+++ packages/astro/test/core-image.test.js
@@ -620,7 +620,7 @@ describe('astro:image', () => {
 					assets: true,
 				},
 				image: {
-					service: fileURLToPath(new URL('./fixtures/core-image/service.mjs', import.meta.url)),
+					service: { entrypoint: fileURLToPath(new URL('./fixtures/core-image/service.mjs', import.meta.url)), config: {foo: 'bar'} }
 				},
 			});
 			devServer = await fixture.startDevServer();
@@ -640,6 +640,14 @@ describe('astro:image', () => {
 
 			const $ = cheerio.load(html);
 			expect($('img').attr('data-service')).to.equal('my-custom-service');
+		});
+
+		it('gets service config', async () => {
+			const response = await fixture.fetch('/');
+			const html = await response.text();
+
+			const $ = cheerio.load(html);
+			expect($('#local img').attr('data-service-config')).to.equal('bar');
 		});
 	});
 });
