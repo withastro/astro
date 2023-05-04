71332cf9697755884e5e2e63d6d2499cc2c5edd1
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-05-04 21:57:23 +0200

    fix: move service functions from astro/assets to astro/config so it can be imported (#6995)
    * fix: move service functions from astro/assets to astro/config so people can import it
    
    * chore: changeset

--- packages/astro/src/assets/index.ts
+++ packages/astro/src/assets/index.ts
@@ -1,21 +1,5 @@
-import type { ImageServiceConfig } from '../@types/astro.js';
-
 export { getConfiguredImageService, getImage } from './internal.js';
 export { baseService, isLocalService } from './services/service.js';
 export { type LocalImageProps, type RemoteImageProps } from './types.js';
 export { emitESMImage } from './utils/emitAsset.js';
 export { imageMetadata } from './utils/metadata.js';
-
-export function sharpImageService(): ImageServiceConfig {
-	return {
-		entrypoint: 'astro/assets/services/sharp',
-		config: {},
-	};
-}
-
-export function squooshImageService(): ImageServiceConfig {
-	return {
-		entrypoint: 'astro/assets/services/squoosh',
-		config: {},
-	};
-}
