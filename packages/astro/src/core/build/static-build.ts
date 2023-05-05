55771c143063bc65bb6de1c5e35a02bf16289afe
Emanuele Stoppa <my.burning@gmail.com>
2023-05-05 16:08:21 +0100

    chore: restore code, ooops!

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -115,14 +115,14 @@ export async function staticBuild(opts: StaticBuildOptions, internals: BuildInte
 		case 'static': {
 			settings.timer.start('Static generate');
 			await generatePages(opts, internals);
-			// await cleanServerOutput(opts);
+			await cleanServerOutput(opts);
 			settings.timer.end('Static generate');
 			return;
 		}
 		case 'server': {
 			settings.timer.start('Server generate');
 			await generatePages(opts, internals);
-			// await cleanStaticOutput(opts, internals);
+			await cleanStaticOutput(opts, internals);
 			info(opts.logging, null, `\n${bgMagenta(black(' finalizing server assets '))}\n`);
 			await ssrMoveAssets(opts);
 			settings.timer.end('Server generate');
