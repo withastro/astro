cf44f7d60b5e12c3caf7e9930fde17113a1a7f49
Louis Baligand <louis.baligand@gmail.com>
2023-05-07 09:57:02 +0000

    Update consts.ts

--- examples/blog/src/consts.ts
+++ examples/blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
+export const SITE_TITLE = 'Cabinet de psychologie et psychothérapie Amandine Beyer & Almudena Sanahuja';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
