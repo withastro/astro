5ea9316e747bbf0cefc692a5bb6ca5dbc014c184
bholmesdev <hey@bholmes.dev>
2023-05-09 17:05:50 -0400

    fix: bubble `ws.send` errors from astro sync

--- packages/astro/src/core/errors/index.ts
+++ packages/astro/src/core/errors/index.ts
@@ -1,5 +1,12 @@
 export type { ErrorLocation, ErrorWithMetadata } from './errors';
 export { AstroErrorData, type AstroErrorCodes } from './errors-data.js';
-export { AggregateError, AstroError, CompilerError, CSSError, MarkdownError } from './errors.js';
+export {
+	AggregateError,
+	AstroError,
+	CompilerError,
+	CSSError,
+	MarkdownError,
+	isAstroError,
+} from './errors.js';
 export { codeFrame } from './printer.js';
 export { createSafeError, positionAt } from './utils.js';
