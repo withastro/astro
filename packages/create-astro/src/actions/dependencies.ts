607c3a04cc2d6391fab565072347231429d29a9a
ematipico <ematipico@users.noreply.github.com>
2023-05-12 15:02:23 +0000

    [ci] format

--- packages/create-astro/src/actions/dependencies.ts
+++ packages/create-astro/src/actions/dependencies.ts
@@ -1,7 +1,7 @@
-import type { Context } from './context';
 import { color } from '@astrojs/cli-kit';
 import { execa } from 'execa';
 import { error, info, spinner, title } from '../messages.js';
+import type { Context } from './context';
 
 export async function dependencies(
 	ctx: Pick<Context, 'install' | 'yes' | 'prompt' | 'pkgManager' | 'cwd' | 'dryRun'>
