8a64125a4fcb77c76e8f11161b21ab1cd4675adf
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-05-13 20:22:10 +0000

    test: update cloudflare integration tests

--- packages/integrations/cloudflare/test/prerender.test.js
+++ packages/integrations/cloudflare/test/prerender.test.js
@@ -6,14 +6,47 @@ describe('Prerendering', () => {
 	let fixture;
 
 	before(async () => {
+		process.env.PRERENDER = true;
 		fixture = await loadFixture({
 			root: './fixtures/prerender/',
 		});
 		await fixture.build();
 	});
 
+	after(() => {
+		delete process.env.PRERENDER;
+		fixture.clean();
+	});
+
 	it('includes prerendered routes in the routes.json config', async () => {
 		const routes = JSON.parse(await fixture.readFile('/_routes.json'));
-		expect(routes.exclude).to.include('/one/');
+		expect(routes.exclude).to.deep.equal(['/_worker.js', '/one/index.html', '/one/']);
+	});
+});
+
+describe('Hybrid rendering', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		process.env.PRERENDER = false;
+		fixture = await loadFixture({
+			root: './fixtures/prerender/',
+			output: 'hybrid',
+			experimental: {
+				hybridOutput: true,
+			},
+		});
+		await fixture.build();
+	});
+
+	after(() => {
+		delete process.env.PRERENDER;
+	});
+
+	it('includes prerendered routes in the routes.json config', async () => {
+		const routes = JSON.parse(await fixture.readFile('/_routes.json'));
+		expect(routes.exclude).to.deep.equal(['/_worker.js', '/index.html', '/']);
+
 	});
 });
