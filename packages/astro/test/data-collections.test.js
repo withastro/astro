b83fe29d5c943998010629d38ef376bb4735d9e9
bholmesdev <hey@bholmes.dev>
2023-05-15 18:55:55 -0400

    test: translations data collection

--- packages/astro/test/data-collections.test.js
+++ packages/astro/test/data-collections.test.js
@@ -24,7 +24,7 @@ describe('Data Collections', () => {
 		});
 
 		it('Generates correct ids', async () => {
-			const ids = json.map((item) => item.id);
+			const ids = json.map((item) => item.id).sort();
 			expect(ids).to.deep.equal(['Ben Holmes', 'Fred K Schott', 'Nate Moore']);
 		});
 
@@ -71,6 +71,78 @@ describe('Data Collections', () => {
 					case 'Nate Moore':
 						expect(json.data.name).to.equal('Nate Something Moore');
 						expect(json.data.twitter).to.equal('https://twitter.com/n_moore');
+						break;
+				}
+			});
+		}
+	});
+
+	describe('Translations Collection', () => {
+		let json;
+		before(async () => {
+			const rawJson = await fixture.readFile('/translations/all.json');
+			json = JSON.parse(rawJson);
+		});
+
+		it('Returns', async () => {
+			expect(Array.isArray(json)).to.be.true;
+			expect(json.length).to.equal(3);
+		});
+
+		it('Generates correct ids', async () => {
+			const ids = json.map((item) => item.id).sort();
+			expect(ids).to.deep.equal(translationIds);
+		});
+
+		it('Generates correct data', async () => {
+			const sorted = json.sort((a, b) => a.id.localeCompare(b.id));
+			const homepageGreetings = sorted.map((item) => item.data.homepage?.greeting);
+			expect(homepageGreetings).to.deep.equal([
+				'Hello World!',
+				'¡Hola Mundo!',
+				'Bonjour le monde!',
+			]);
+
+			const homepagePreambles = sorted.map((item) => item.data.homepage?.preamble);
+			expect(homepagePreambles).to.deep.equal([
+				'Welcome to the future of content.',
+				'Bienvenido al futuro del contenido.',
+				'Bienvenue dans le futur du contenu.',
+			]);
+		});
+	});
+
+	describe('Translations Entry', () => {
+		for (const translationId of translationIds) {
+			let json;
+			before(async () => {
+				const rawJson = await fixture.readFile(`/translations/${translationId}.json`);
+				json = JSON.parse(rawJson);
+			});
+
+			it(`Returns ${translationId}`, async () => {
+				expect(json).to.haveOwnProperty('id');
+				expect(json.id).to.equal(translationId);
+			});
+
+			it(`Generates correct data for ${translationId}`, async () => {
+				expect(json).to.haveOwnProperty('data');
+				expect(json.data).to.haveOwnProperty('homepage');
+				expect(json.data.homepage).to.haveOwnProperty('greeting');
+				expect(json.data.homepage).to.haveOwnProperty('preamble');
+
+				switch (translationId) {
+					case 'en':
+						expect(json.data.homepage.greeting).to.equal('Hello World!');
+						expect(json.data.homepage.preamble).to.equal('Welcome to the future of content.');
+						break;
+					case 'es':
+						expect(json.data.homepage.greeting).to.equal('¡Hola Mundo!');
+						expect(json.data.homepage.preamble).to.equal('Bienvenido al futuro del contenido.');
+						break;
+					case 'fr':
+						expect(json.data.homepage.greeting).to.equal('Bonjour le monde!');
+						expect(json.data.homepage.preamble).to.equal('Bienvenue dans le futur du contenu.');
 						break;
 				}
 			});
