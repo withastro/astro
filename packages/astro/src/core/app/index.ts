08c1b222625688c1ac686b745f4aa63d6cfec647
Moustapha HappyDev <moustapha.happydev@gmail.com>
2023-05-15 12:21:27 +0000

    `'/' + base`   ------> `prependForwardSlash(base)`

--- packages/astro/src/core/app/index.ts
+++ packages/astro/src/core/app/index.ts
@@ -178,7 +178,7 @@ export class App {
 		status = 200
 	): Promise<Response> {
 		const url = new URL(request.url);
-		const pathname = '/' + this.removeBase(url.pathname);
+		const pathname = prependForwardSlash(this.removeBase(url.pathname));
 		const info = this.#routeDataToRouteInfo.get(routeData!)!;
 		// may be used in the future for handling rel=modulepreload, rel=icon, rel=manifest etc.
 		const links = new Set<never>();
