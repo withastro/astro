30c825350d62fe2e1723adb0fb173b930eb65b01
bholmesdev <hey@bholmes.dev>
2023-05-23 16:13:30 -0400

    test: style bleed (it fails...)

--- packages/integrations/markdoc/test/propagated-assets.test.js
+++ packages/integrations/markdoc/test/propagated-assets.test.js
@@ -15,11 +15,22 @@ describe('Markdoc - propagated assets', () => {
 
 	for (const mode of modes) {
 		describe(mode, () => {
+			/** @type {Document} */
+			let stylesDocument;
+			/** @type {Document} */
+			let scriptsDocument;
+
 			before(async () => {
 				if (mode === 'prod') {
 					await fixture.build();
+					stylesDocument = parseHTML(await fixture.readFile('/styles/index.html')).document;
+					scriptsDocument = parseHTML(await fixture.readFile('/scripts/index.html')).document;
 				} else if (mode === 'dev') {
 					devServer = await fixture.startDevServer();
+					const styleRes = await fixture.fetch('/styles');
+					const scriptRes = await fixture.fetch('/scripts');
+					stylesDocument = parseHTML(await styleRes.text()).document;
+					scriptsDocument = parseHTML(await scriptRes.text()).document;
 				}
 			});
 
@@ -27,36 +38,44 @@ describe('Markdoc - propagated assets', () => {
 				if (mode === 'dev') devServer?.stop();
 			});
 
-			describe('Styles', () => {
-				/** @type {Document} */
-				let document;
+			it('Bundles styles', async () => {
+				let styleContents;
+				if (mode === 'dev') {
+					const styles = stylesDocument.querySelectorAll('style');
+					expect(styles).to.have.lengthOf(1);
+					styleContents = styles[0].textContent;
+				} else {
+					const links = stylesDocument.querySelectorAll('link[rel="stylesheet"]');
+					expect(links).to.have.lengthOf(1);
+					styleContents = await fixture.readFile(links[0].href);
+				}
+				expect(styleContents).to.include('--color-base-purple: 269, 79%;');
+			});
 
-				before(async () => {
-					if (mode === 'prod') {
-						const html = await fixture.readFile('/styles/index.html');
-						document = parseHTML(html).document;
-					} else if (mode === 'dev') {
-						const res = await fixture.fetch('/styles');
-						const html = await res.text();
-						document = parseHTML(html).document;
-					}
-				});
-
-				it('Bundles styles', async () => {
-					let styleContents;
-					if (mode === 'dev') {
-						const styles = document.querySelectorAll('style');
-						expect(styles).to.have.lengthOf(1);
-						styleContents = styles[0].textContent;
-					} else {
-						console.log(document.head.innerHTML);
-						const links = document.querySelectorAll('link[rel="stylesheet"]');
-						expect(links).to.have.lengthOf(1);
-						styleContents = await fixture.readFile(links[0].href);
-					}
-					expect(styleContents).to.include('--color-base-purple: 269, 79%;');
-				});
+			it('[fails] Does not bleed styles to other page', async () => {
+				if (mode === 'dev') {
+					const styles = scriptsDocument.querySelectorAll('style');
+					expect(styles).to.have.lengthOf(0);
+				} else {
+					const links = scriptsDocument.querySelectorAll('link[rel="stylesheet"]');
+					expect(links).to.have.lengthOf(0);
+				}
 			});
 		});
 	}
 });
+
+/**
+ * @param {string} filePath
+ * @param {'dev' | 'prod'} mode
+ * @returns {Document} */
+async function parseDocument(filePath, mode) {
+	if (mode === 'prod') {
+		const html = await fixture.readFile(filePath + 'index.html');
+		document = parseHTML(html).document;
+	} else if (mode === 'dev') {
+		const res = await fixture.fetch('/styles');
+		const html = await res.text();
+		document = parseHTML(html).document;
+	}
+}
