c2af343113558ddf573c8d1833c5d65a3f96a6d1
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-05-29 22:04:44 +0530

    Update inlineStylesheets version in the configuration reference
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -807,7 +807,7 @@ export interface AstroUserConfig {
 		 * @name build.inlineStylesheets
 		 * @type {('always' | 'auto' | 'never')}
 		 * @default `never`
-		 * @version 2.4.0
+		 * @version 2.6.0
 		 * @description
 		 * Control whether styles are sent to the browser in a separate css file or inlined into `<style>` tags. Choose from the following options:
 		 *  - `'always'` - all styles are inlined into `<style>` tags
