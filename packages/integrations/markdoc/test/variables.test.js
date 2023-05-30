85fb722748e6f31672a3e823ebfa04346333c884
bholmesdev <hey@bholmes.dev>
2023-05-30 13:36:06 -0400

    chore: missed a spot

--- packages/integrations/markdoc/test/variables.test.js
+++ packages/integrations/markdoc/test/variables.test.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { loadFixture } from '../../../astro/test/test-utils.js';
 import markdoc from '../dist/index.js';
 
-const root = new URL('./fixtures/entry-prop/', import.meta.url);
+const root = new URL('./fixtures/variables/', import.meta.url);
 
 describe('Markdoc - Variables', () => {
 	let baseFixture;
