416db2f2699d67946b9e6406322492c0a84d35ab
Matthew Phillips <matthew@skypack.dev>
2023-06-05 09:03:20 -0400

    Redirects (#7067)
    * Redirects spike
    
    * Allow redirects in static mode
    
    * Support in Netlify as well
    
    * Adding a changeset
    
    * Rename file
    
    * Fix build problem
    
    * Refactor to be more modular
    
    * Fix location ref
    
    * Late test should only run in SSR
    
    * Support redirects in Netlify SSR configuration (#7167)
    
    * Implement support for dynamic routes in redirects (#7173)
    
    * Implement support for dynamic routes in redirects
    
    * Remove the .only
    
    * No need to special-case redirects in static build
    
    * Implement support for redirects config in the Vercel adapter (#7182)
    
    * Implement support for redirects config in the Vercel adapter
    
    * Remove unused condition
    
    * Move to a internal helper package
    
    * Add support for the object notation in redirects
    
    * Use status 308 for non-GET redirects (#7186)
    
    * Implement redirects in Cloudflare (#7198)
    
    * Implement redirects in Cloudflare
    
    * Fix build
    
    * Update tests b/c of new ordering
    
    * Debug issue
    
    * Use posix.join
    
    * Update packages/underscore-redirects/package.json
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Update based on review comments
    
    * Update broken test
    
    ---------
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Test that redirects can come from middleware (#7213)
    
    * Test that redirects can come from middleware
    
    * Allow non-promise returns for middleware
    
    * Implement priority (#7210)
    
    * Refactor
    
    * Fix netlify test ordering
    
    * Fix ordering again
    
    * Redirects: Allow preventing the output of the static HTML file (#7245)
    
    * Do a simple push for priority
    
    * Adding changesets
    
    * Put the implementation behind a flag.
    
    * Self review
    
    * Update .changeset/chatty-actors-stare.md
    
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    
    * Update docs on dynamic restrictions.
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Code review changes
    
    * Document netlify static adapter
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Slight reword
    
    * Update .changeset/twenty-suns-vanish.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Add a note about public/_redirects file
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    ---------
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- packages/integrations/netlify/src/index.ts
+++ packages/integrations/netlify/src/index.ts
@@ -1,2 +1,3 @@
 export { netlifyEdgeFunctions } from './integration-edge-functions.js';
 export { netlifyFunctions as default, netlifyFunctions } from './integration-functions.js';
+export { netlifyStatic } from './integration-static.js';
