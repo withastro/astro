4929332c3210d1634b8607c7736d9049860a2079
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2023-06-06 11:09:16 -0400

    #7226 - fixes NodeJS adapter for multiple set-cookie headers (and other header issues) (#7227)
    * Utilizes the new standard WebAPI Fetch Headers.getSetCookie() function
    to safely handle multiple set-cookie headers when converting from a
    WebAPI Response to a NodeJS ServerResponse
    
    Modifies the existing nodeMiddleware logic which first set AstroCookies
    on ServerResponse.setHeader(...) and then called
    ServerResponse.writeHead(status, Response.headers) which means any that
    if the WebAPI Response had any set-cookie headers on it, they would
    replace anything from AstroCookies.
    
    The new logic delegates appending AstroCookie values onto the WebAPI
    Response Headers object, so that a single unified function safely
    converts the WebAPI Response Headers into a NodeJS compatible
    OutgoingHttpHeaders object utilizing the new standard
    Headers.getSetCookie() function provided by the undici WebAPI polyfills.
    
    Plus extensive test coverage.
    
    * #7226 - changeset for NodeJS adapter set-cookie fix
    
    * fixing all double quotes to single quotes
    
    ---------
    
    Co-authored-by: Alex Sherwin <alex.sherwin@acadia.inc>
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

---
+++ .changeset/chilled-rockets-flash.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/node': minor
+---
+
+Fixes NodeJS adapter for multiple set-cookie headers and combining AstroCookies and Response.headers cookies
--- packages/integrations/node/src/nodeMiddleware.ts
+++ packages/integrations/node/src/nodeMiddleware.ts
@@ -3,6 +3,7 @@ import type { IncomingMessage, ServerResponse } from 'http';
 import type { Readable } from 'stream';
 import { responseIterator } from './response-iterator';
 import type { Options } from './types';
+import { createOutgoingHttpHeaders } from './createOutgoingHttpHeaders';
 
 export default function (app: NodeApp, mode: Options['mode']) {
 	return async function (
@@ -44,12 +45,16 @@ async function writeWebResponse(app: NodeApp, res: ServerResponse, webResponse: 
 
 	if (app.setCookieHeaders) {
 		const setCookieHeaders: Array<string> = Array.from(app.setCookieHeaders(webResponse));
+    
 		if (setCookieHeaders.length) {
-			res.setHeader('Set-Cookie', setCookieHeaders);
+      for (const setCookieHeader of setCookieHeaders) {
+        webResponse.headers.append('set-cookie', setCookieHeader);
+      }
 		}
 	}
 
-	res.writeHead(status, Object.fromEntries(headers.entries()));
+  const nodeHeaders = createOutgoingHttpHeaders(headers);
+  res.writeHead(status, nodeHeaders);
 	if (webResponse.body) {
 		try {
 			for await (const chunk of responseIterator(webResponse) as unknown as Readable) {
