a11b62ee1f5d524b0ba942818525b623a6d6eb99
Ben Holmes <hey@bholmes.dev>
2023-06-06 14:48:54 -0400

    Fix: Markdoc type errors (#7311)
    * fix: config, prism, shiki exports
    
    * fix: type error for `render` property
    
    * chore: use `.ts` files in select tests for type checks
    
    * fix: type error on shiki() promise
    
    * chore: changeset

--- packages/integrations/markdoc/src/config.ts
+++ packages/integrations/markdoc/src/config.ts
@@ -1,12 +1,26 @@
-import type { ConfigType as MarkdocConfig } from '@markdoc/markdoc';
+import type { AstroInstance } from 'astro';
+import type {
+	ConfigType as MarkdocConfig,
+	Config,
+	NodeType,
+	Schema,
+	MaybePromise,
+} from '@markdoc/markdoc';
 import _Markdoc from '@markdoc/markdoc';
 import { heading } from './heading-ids.js';
 
-export type AstroMarkdocConfig<C extends Record<string, any> = Record<string, any>> =
-	MarkdocConfig & {
-		ctx?: C;
-		extends?: ResolvedAstroMarkdocConfig[];
-	};
+type Render = AstroInstance['default'] | string;
+
+export type AstroMarkdocConfig<C extends Record<string, any> = Record<string, any>> = Omit<
+	MarkdocConfig,
+	'tags' | 'nodes'
+> &
+	Partial<{
+		tags: Record<string, Schema<Config, Render>>;
+		nodes: Partial<Record<NodeType, Schema<Config, Render>>>;
+		ctx: C;
+		extends: MaybePromise<ResolvedAstroMarkdocConfig>[];
+	}>;
 
 export type ResolvedAstroMarkdocConfig = Omit<AstroMarkdocConfig, 'extends'>;
 
