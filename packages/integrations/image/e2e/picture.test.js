e6b293c9d4a8d4473036645d4ee3bda046ebad1e
Wooseop Kim <wooseop@protonmail.com>
2023-06-09 01:48:28 +0900

    test: use `currentSrc` to ensure correct images are selected

--- packages/integrations/image/e2e/picture.test.js
+++ packages/integrations/image/e2e/picture.test.js
@@ -17,71 +17,16 @@ test.describe('Basic picture', () => {
 		});
 
 		test('requests chosen src', async ({ page, astro }) => {
-			/** @type {string[]} */
-			const requests = [];
-
-			page.on('request', (request) => requests.push(request.url()));
-
 			await page.goto(astro.resolveUrl('/'));
 
 			await page.waitForLoadState('networkidle');
 
-			await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-
-			const pictureElements = await page.$$('picture');
-			const imgAttributes = await Promise.all(pictureElements.map(getAttributes));
-			const expectedExtensions = await Promise.all(
-				imgAttributes
-					.filter(distinctBySources)
-					.map(({ id }) => (id === 'webp-only' ? 'webp' : 'avif'))
-			);
-			const imageRequests = requests.filter((url) => url !== astro.resolveUrl('/'));
-			const requestedExtensions = imageRequests.map((url) => url.slice(url.lastIndexOf('.') + 1));
-			expect(requestedExtensions).toEqual(expectedExtensions);
+			const imgs = await page.locator('picture img').all();
+			for (const img of imgs) {
+				const currentSrc = await img.evaluate((elem) => elem.currentSrc);
+				const src = await img.evaluate((elem) => elem.src);
+				expect(currentSrc).not.toEqual(src);
+			}
 		});
 	});
 });
-
-/** @typedef {{ id: string, src: string, types: string[] }} Attributes */
-
-/**
- * @param {Attributes} attributes
- * @param {number} index
- * @param {Attributes[]} array
- * @returns {boolean}
- */
-function distinctBySources({ src, types }, index, array) {
-	return array.findIndex((attrs) => attrs.src === src && setsEqual(attrs.types, types)) === index;
-}
-
-/**
- * @param {import('@playwright/test').ElementHandle} picture
- * @returns {Attributes}
- */
-async function getAttributes(picture) {
-	const img = await picture.$('img');
-	const sources = await picture.$$('source');
-	const types = sources.map((source) => source.getAttribute('type'));
-	return {
-		id: await img.getAttribute('id'),
-		src: await img.getAttribute('src'),
-		types: new Set(await Promise.all(types)),
-	};
-}
-
-/**
- * @param {Set} a
- * @param {Set} b
- * @returns {boolean}
- */
-function setsEqual(a, b) {
-	if (a.size !== b.size) {
-		return false;
-	}
-	for (const elem of a) {
-		if (!b.has(elem)) {
-			return false;
-		}
-	}
-	return true;
-}
