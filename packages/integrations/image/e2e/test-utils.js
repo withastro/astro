981373b83fee950ef0e1d9a050bd081537c4d4e6
Wooseop Kim <wooseop@protonmail.com>
2023-06-09 01:47:22 +0900

    fix: add missing `loadFixture`

--- packages/integrations/image/e2e/test-utils.js
+++ packages/integrations/image/e2e/test-utils.js
@@ -1,4 +1,17 @@
 import { test as testBase } from '@playwright/test';
+import { loadFixture as baseLoadFixture } from '../../../astro/test/test-utils.js';
+
+export function loadFixture(inlineConfig) {
+	if (!inlineConfig || !inlineConfig.root)
+		throw new Error("Must provide { root: './fixtures/...' }");
+
+	// resolve the relative root (i.e. "./fixtures/tailwindcss") to a full filepath
+	// without this, the main `loadFixture` helper will resolve relative to `packages/astro/test`
+	return baseLoadFixture({
+		...inlineConfig,
+		root: new URL(inlineConfig.root, import.meta.url).toString(),
+	});
+}
 
 export function testFactory(inlineConfig) {
 	let fixture;
