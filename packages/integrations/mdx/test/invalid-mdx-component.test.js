1ac016225da74a6d3fe3b03c6851dc4cf3b2c05d
MoustaphaDev <MoustaphaDev@users.noreply.github.com>
2023-06-09 20:23:53 +0000

    [ci] format

--- packages/integrations/mdx/test/invalid-mdx-component.test.js
+++ packages/integrations/mdx/test/invalid-mdx-component.test.js
@@ -1,40 +1,37 @@
-import { expect } from "chai";
-import { loadFixture } from "../../../astro/test/test-utils.js";
-import mdx from "../dist/index.js";
+import { expect } from 'chai';
+import { loadFixture } from '../../../astro/test/test-utils.js';
+import mdx from '../dist/index.js';
 
-const FIXTURE_ROOT = new URL(
-  "./fixtures/invalid-mdx-component/",
-  import.meta.url,
-);
+const FIXTURE_ROOT = new URL('./fixtures/invalid-mdx-component/', import.meta.url);
 
-describe("MDX component with runtime error", () => {
-  let fixture;
+describe('MDX component with runtime error', () => {
+	let fixture;
 
-  before(async () => {
-    fixture = await loadFixture({
-      root: FIXTURE_ROOT,
-      integrations: [mdx()],
-    });
-  });
+	before(async () => {
+		fixture = await loadFixture({
+			root: FIXTURE_ROOT,
+			integrations: [mdx()],
+		});
+	});
 
-  describe("build", () => {
-    /** @type {Error | null} */
-    let error;
+	describe('build', () => {
+		/** @type {Error | null} */
+		let error;
 
-    before(async () => {
-      error = null;
-      try {
-        await fixture.build();
-      } catch (e) {
-        error = e;
-      }
-    });
+		before(async () => {
+			error = null;
+			try {
+				await fixture.build();
+			} catch (e) {
+				error = e;
+			}
+		});
 
-    it("Throws the right error", async () => {
-      expect(error).to.exist;
-      expect(error?.hint).to.match(
-        /This issue often occurs when your MDX component encounters runtime errors/,
-      );
-    });
-  });
+		it('Throws the right error', async () => {
+			expect(error).to.exist;
+			expect(error?.hint).to.match(
+				/This issue often occurs when your MDX component encounters runtime errors/
+			);
+		});
+	});
 });
