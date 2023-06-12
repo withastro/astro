752bde402017c1db3d72e5450713426eacd2f6e9
bluwy <bjornlu.dev@gmail.com>
2023-06-12 22:17:10 +0800

    Use faster image service for test

--- packages/integrations/mdx/test/fixtures/mdx-images/astro.config.ts
+++ packages/integrations/mdx/test/fixtures/mdx-images/astro.config.ts
@@ -1,8 +1,12 @@
 import mdx from '@astrojs/mdx';
+import { testImageService } from '../../../../../astro/test/test-image-service.js';
 
 export default {
 	integrations: [mdx()],
 	experimental: {
-		assets: true
-	}
+		assets: true,
+	},
+	image: {
+		service: testImageService(),
+	},
 }
