684094ff1024b8138ab98288a9324dfbfcecac20
bluwy <bjornlu.dev@gmail.com>
2023-06-12 16:27:53 +0800

    Update example versions with a script

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -9,14 +9,6 @@ module.exports = {
   plugins: ['./node_modules/prettier-plugin-astro'],
   overrides: [
     {
-      // Changesets run Prettier using our configuration, however it uses a very old version of Prettier that does
-      // not support our plugin and it ends up doing more harm than good. As such, we'll disable our plugin for changelogs
-      files: ['CHANGELOG.md'],
-      options: {
-        plugins: [],
-      },
-    },
-    {
       files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
       options: {
         useTabs: false,
