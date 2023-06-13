16f434034e20ea2b5e76eb2ad5450a6526abf7ec
bluwy <bjornlu.dev@gmail.com>
2023-06-13 13:55:11 +0800

    Fix typo

--- packages/integrations/vercel/test/fixtures/redirects/astro.config.mjs
+++ packages/integrations/vercel/test/fixtures/redirects/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
 	adapter: vercel(),
