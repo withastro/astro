fc441349e40aa433ed7d73f0fa05dd392e2fe223
Bjorn Lu <bjornlu.dev@gmail.com>
2023-06-13 22:47:24 +0800

    Use stub image service for tests (#7376)

--- packages/integrations/vercel/test/fixtures/image/astro.config.mjs
+++ packages/integrations/vercel/test/fixtures/image/astro.config.mjs
@@ -1,9 +1,13 @@
 import vercel from '@astrojs/vercel/static';
 import { defineConfig } from 'astro/config';
+import { testImageService } from '../../../../../astro/test/test-image-service.js';
 
 export default defineConfig({
 	adapter: vercel({imageService: true}),
 	experimental: {
-		assets: true
-	}
+		assets: true,
+	},
+	image: {
+		service: testImageService(),
+	},
 });
