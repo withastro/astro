0da176d57d25f917e5745f8e4630fff2bffba493
Sarah Rainsberger <sarah@rainsberger.ca>
2023-06-15 11:43:00 -0300

    still say it's reserved!

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1044,7 +1044,7 @@ See https://docs.astro.build/en/guides/server-side-rendering/ for more informati
 	 * @see
 	 * - [The reserved entry `slug` field](https://docs.astro.build/en/guides/content-collections/#defining-custom-slugs)
 	 * @description
-	 * A content collection schema should not contain the `slug` field. Remove `slug` from your schema. You can still use custom slugs in your frontmatter.
+	 * A content collection schema should not contain the `slug` field. This is reserved by Astro for generating entry slugs. Remove `slug` from your schema. You can still use custom slugs in your frontmatter.
 	 */
 	ContentSchemaContainsSlugError: {
 		title: 'Content Schema should not contain `slug`.',
