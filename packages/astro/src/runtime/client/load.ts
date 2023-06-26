dfa9290d469599d0934879a792d5a4e0c2940c25
Felix Eckhardt <felix.e@gmx.de>
2023-06-26 13:22:05 +0200

    add check if dom is already completed, before loadDirective is called. This happens sometimes in Firefox

--- packages/astro/src/runtime/client/load.ts
+++ packages/astro/src/runtime/client/load.ts
@@ -1,14 +1,14 @@
 import type {ClientDirective} from '../../@types/astro';
 
 const loadDirective: ClientDirective = async (load, options) => {
-	if (options.value === "dom") {
+	if (options.value !== "dom" || document.readyState === 'complete' || document.readyState === 'interactive') {
+		const hydrate = await load();
+		await hydrate();
+	} else {
 		window.addEventListener('DOMContentLoaded', async () => {
 			const hydrate = await load();
 			await hydrate();
 		})
-	} else {
-		const hydrate = await load();
-		await hydrate();
 	}
 };
 
