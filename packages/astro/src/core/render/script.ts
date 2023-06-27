b44d46889e3692b26aec0ad3deda3d2d76b1d6e6
bluwy <bjornlu.dev@gmail.com>
2023-06-27 21:38:03 +0800

    Remove unused rendering code

--- packages/astro/src/core/render/script.ts
+++
@@ -1,8 +0,0 @@
-export const SCRIPT_EXTENSIONS = new Set(['.js', '.ts']);
-
-const scriptRe = new RegExp(
-	`\\.(${Array.from(SCRIPT_EXTENSIONS)
-		.map((s) => s.slice(1))
-		.join('|')})($|\\?)`
-);
-export const isScriptRequest = (request: string): boolean => scriptRe.test(request);
