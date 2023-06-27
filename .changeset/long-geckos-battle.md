d7a1603133cd09ebc585ada5bf112243942dc600
Emanuele Stoppa <my.burning@gmail.com>
2023-06-27 15:14:20 +0100

    feat: export `createAPIContext` to user-land

---
+++ .changeset/long-geckos-battle.md
@@ -0,0 +1,7 @@
+---
+'astro': minor
+---
+
+The module `astro/middleware` exports a new API called `createAPIContext`.
+
+This a low-level API that integrations can use to create a context that can be consumed by middleware functions.
--- packages/astro/src/core/middleware/index.ts
+++ packages/astro/src/core/middleware/index.ts
@@ -1,9 +1,10 @@
 import type { MiddlewareResponseHandler } from '../../@types/astro';
 import { sequence } from './sequence.js';
+import { createAPIContext } from '../endpoint/index.js';
 
 function defineMiddleware(fn: MiddlewareResponseHandler) {
 	return fn;
 }
 
 // NOTE: this export must export only the functions that will be exposed to user-land as officials APIs
-export { sequence, defineMiddleware };
+export { sequence, defineMiddleware, createAPIContext };
