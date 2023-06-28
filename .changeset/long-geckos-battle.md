feee6af24a0635d88d6ca65918d656e4a51bb403
Emanuele Stoppa <my.burning@gmail.com>
2023-06-28 13:46:46 +0100

    chore: apply code suggestions

--- .changeset/long-geckos-battle.md
+++ .changeset/long-geckos-battle.md
@@ -2,6 +2,6 @@
 'astro': minor
 ---
 
-The module `astro/middleware` exports a new API called `createAPIContext`.
+The module `astro/middleware` exports a new API called `createContext`.
 
-This a low-level API that integrations can use to create a context that can be consumed by middleware functions.
+This a low-level API that adapters can use to create a context that can be consumed by middleware functions.
--- packages/astro/src/core/middleware/index.ts
+++ packages/astro/src/core/middleware/index.ts
@@ -1,4 +1,4 @@
-import type { MiddlewareResponseHandler } from '../../@types/astro';
+import type { MiddlewareResponseHandler, Params } from '../../@types/astro';
 import { sequence } from './sequence.js';
 import { createAPIContext } from '../endpoint/index.js';
 
@@ -6,5 +6,31 @@ function defineMiddleware(fn: MiddlewareResponseHandler) {
 	return fn;
 }
 
+/**
+ * Payload for creating a context to be passed to Astro middleware
+ */
+export type CreateContext = {
+	/**
+	 * The incoming request
+	 */
+	request: Request;
+	/**
+	 * Optional parameters
+	 */
+	params?: Params;
+};
+
+/**
+ * Creates a context to be passed to Astro middleware `onRequest` function.
+ */
+function createContext({ request, params }: CreateContext) {
+	return createAPIContext({
+		request,
+		params: params ?? {},
+		props: {},
+		site: undefined,
+	});
+}
+
 // NOTE: this export must export only the functions that will be exposed to user-land as officials APIs
-export { sequence, defineMiddleware, createAPIContext };
+export { sequence, defineMiddleware, createContext };
