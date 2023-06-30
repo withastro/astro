ff9783e984244be357aea5e5e7e0bcf97452a699
Ken Powers <ken@kenpowers.net>
2023-06-30 10:38:27 -0400

    Revert "Use non-conflicting naming"
    This reverts commit c71c4e65658c9cc6711ab94a407008f29b8b49dd.

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1772,8 +1772,8 @@ export interface EndpointOutput {
 	encoding?: BufferEncoding;
 }
 
-export type APIRoute<RouteProps extends Props = Props> = (
-	context: APIContext<RouteProps>
+export type APIRoute<Props extends Record<string, any> = Record<string, any>> = (
+	context: APIContext<Props>
 ) => EndpointOutput | Response | Promise<EndpointOutput | Response>;
 
 export interface EndpointHandler {
