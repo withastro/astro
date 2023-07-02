c2a00d7b769070601e3cab88fb69c0ccbc432127
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-07-02 08:15:13 +0300

    Update astro.config.mjs

--- examples/framework-react/astro.config.mjs
+++ examples/framework-react/astro.config.mjs
@@ -3,6 +3,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
+	// Enable Astro to support React JSX components.
 	integrations: [react()],
 });
