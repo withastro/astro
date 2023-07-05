6dc251334f612bfe59d845ca4ce95a1fb958cccf
bluwy <bluwy@users.noreply.github.com>
2023-07-05 06:17:56 +0000

    [ci] format

--- packages/astro/src/core/module-loader/loader.ts
+++ packages/astro/src/core/module-loader/loader.ts
@@ -48,7 +48,7 @@ export interface ModuleNode {
 	} | null;
 	ssrError: Error | null;
 	importedModules: Set<ModuleNode>;
-	importers: Set<ModuleNode>
+	importers: Set<ModuleNode>;
 }
 
 export interface ModuleInfo {
