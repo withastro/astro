e0ba3889b345a7562f5c8b54b4b1e3cdf1da2438
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2023-07-05 15:47:13 -0400

    7576 - initial support for HTML inside Markdoc.
    This uses htmlparser2 to perform a pure token transform/mutation
    on the markdown-it tokens, replacing the original raw HTML string
    tokens with a richer set of tokens per HTML node, and in the process
    Markdoc tags are interleaved in the resulting token graph at the
    appropriate locations
    
    This removes the legacy config of the @astrojs/markdoc integration
    entirely (suggested by @bholmesdev) and introduces a new type for
    options to be specified in the astro config, initially, with just the new
    "enableHTML" option
    
    When "enableHTML" is *not* enabled (the default), the behavior
    of the entire @astrojs/markdoc integration should remain functionally
    equivalent to before this change

---
+++ packages/integrations/markdoc/src/html/tagdefs/html.tag.ts
@@ -0,0 +1,37 @@
+// third party
+import type { Config, Schema } from "@markdoc/markdoc";
+import Markdoc from "@markdoc/markdoc";
+
+// local
+import { parseInlineCSSToReactLikeObject } from "../css/parse-inline-css-to-react.js";
+import { mutateRawHtmlAttributesToReactifiedVariants } from "../htmlattributes/mutate-raw-html-attributes-to-react-compatible-variants.js";
+
+// a Markdoc tag that will render a given HTML element and its attributes, as produced by the htmlTokenTransform function
+export const html: Schema<Config, never> = {
+
+  attributes: {
+    name: { type: String, required: true },
+    attrs: { type: Object },
+  },
+
+  transform(node, config) {
+
+    const { name, attrs: unsafeAttributes } = node.attributes;
+    const children = node.transformChildren(config);
+
+    // pull out any "unsafe" attributes which need additional processing
+    const { style, ...safeAttributes } = unsafeAttributes as Record<string, unknown>;
+
+    // if the inline "style" attribute is present we need to parse the HTML into a react-like React.CSSProperties object
+    if (typeof style === "string") {
+      const styleObject = parseInlineCSSToReactLikeObject(style);
+      safeAttributes.style = styleObject;
+    }
+
+    // fix (rename/mutate) various raw HTML attributes that need to be camelCase'd for React
+    mutateRawHtmlAttributesToReactifiedVariants(safeAttributes);
+
+    // create a Markdoc Tag for the given HTML node with the HTML attributes and children
+    return new Markdoc.Tag(name, safeAttributes, children);
+  },
+};
