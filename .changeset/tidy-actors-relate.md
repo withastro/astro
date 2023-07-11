dd3fe5ec3542d922dee4c1bc5487051d758c1f8e
Emanuele Stoppa <my.burning@gmail.com>
2023-07-11 15:09:13 +0100

    fix(@astrojs/vercel): improve file detection

---
+++ .changeset/tidy-actors-relate.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/vercel': patch
+---
+
+Improve file detection of the middleware file handler
--- packages/integrations/vercel/test/edge-middleware.test.js
+++ packages/integrations/vercel/test/edge-middleware.test.js
@@ -1,30 +1,33 @@
 import { loadFixture } from './test-utils.js';
-import { expect, use } from 'chai';
+import { expect } from 'chai';
 import chaiJestSnapshot from 'chai-jest-snapshot';
 
-use(chaiJestSnapshot);
-
-describe('Serverless prerender', () => {
-	/** @type {import('./test-utils').Fixture} */
-	let fixture;
-
-	beforeEach(function () {
-		chaiJestSnapshot.configureUsingMochaContext(this);
-	});
-
-	before(async () => {
-		chaiJestSnapshot.resetSnapshotRegistry();
-		fixture = await loadFixture({
-			root: './fixtures/middleware/',
+describe('Vercel edge middleware', () => {
+	it('with edge handle file, should successfully build the middleware', async () => {
+		const fixture = await loadFixture({
+			root: './fixtures/middleware-with-edge-file/',
 		});
-	});
-
-	it('build successfully the middleware edge file', async () => {
 		await fixture.build();
 		const contents = await fixture.readFile(
 			// this is abysmal...
-			'../.vercel/output/functions/render.func/packages/integrations/vercel/test/fixtures/middleware/dist/middleware.mjs'
+			'../.vercel/output/functions/render.func/packages/integrations/vercel/test/fixtures/middleware-with-edge-file/dist/middleware.mjs'
 		);
-		expect(contents).to.matchSnapshot();
+		expect(contents.includes('title:')).to.be.true;
+		chaiJestSnapshot.setTestName('Middleware with handler file');
+		expect(contents).to.matchSnapshot(true);
+	});
+
+	it('with edge handle file, should successfully build the middleware', async () => {
+		const fixture = await loadFixture({
+			root: './fixtures/middleware-without-edge-file/',
+		});
+		await fixture.build();
+		const contents = await fixture.readFile(
+			// this is abysmal...
+			'../.vercel/output/functions/render.func/packages/integrations/vercel/test/fixtures/middleware-without-edge-file/dist/middleware.mjs'
+		);
+		expect(contents.includes('title:')).to.be.false;
+		chaiJestSnapshot.setTestName('Middleware without handler file');
+		expect(contents).to.matchSnapshot(true);
 	});
 });
