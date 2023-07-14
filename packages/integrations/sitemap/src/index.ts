922061c4e748d731e6df4bfa6eb325d6b077b594
Nate Moore <nate@astro.build>
2023-07-14 12:58:59 -0500

    fix(#7080): sitemap should only add trailing slash to pages

--- packages/integrations/sitemap/src/index.ts
+++ packages/integrations/sitemap/src/index.ts
@@ -1,4 +1,4 @@
-import type { AstroConfig, AstroIntegration } from 'astro';
+import type { AstroConfig, AstroIntegration, RouteType } from 'astro';
 import {
 	EnumChangefreq,
 	simpleSitemapAndIndex,
@@ -111,7 +111,7 @@ const createPlugin = (options?: SitemapOptions): AstroIntegration => {
 
 							if (config.trailingSlash === 'never') {
 								urls.push(newUrl);
-							} else if (config.build.format === 'directory' && !newUrl.endsWith('/')) {
+							} else if (config.build.format === 'directory' && !newUrl.endsWith('/') && r.type === 'page') {
 								urls.push(newUrl + '/');
 							} else {
 								urls.push(newUrl);
