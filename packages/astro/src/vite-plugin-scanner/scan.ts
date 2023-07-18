d79d1417ec9c671ee5236878c566b09a3520a5ad
Nate Moore <nate@astro.build>
2023-07-18 16:11:29 -0500

    refactor: make settings optional

--- packages/astro/src/vite-plugin-scanner/scan.ts
+++ packages/astro/src/vite-plugin-scanner/scan.ts
@@ -3,6 +3,7 @@ import type { AstroSettings } from '../@types/astro.js';
 
 import * as eslexer from 'es-module-lexer';
 import { AstroError, AstroErrorData } from '../core/errors/index.js';
+import { isServerLikeOutput } from '../prerender/utils.js';
 
 const BOOLEAN_EXPORTS = new Set(['prerender']);
 
@@ -36,7 +37,7 @@ function isFalsy(value: string) {
 
 let didInit = false;
 
-export async function scan(code: string, id: string, settings: AstroSettings): Promise<PageOptions> {
+export async function scan(code: string, id: string, settings?: AstroSettings): Promise<PageOptions> {
 	if (!includesExport(code)) return {};
 	if (!didInit) {
 		await eslexer.init;
@@ -65,7 +66,7 @@ export async function scan(code: string, id: string, settings: AstroSettings): P
 			if (prefix !== 'const' || !(isTruthy(suffix) || isFalsy(suffix))) {
 				throw new AstroError({
 					...AstroErrorData.InvalidPrerenderExport,
-					message: AstroErrorData.InvalidPrerenderExport.message(prefix, suffix, settings.config.output === 'hybrid'),
+					message: AstroErrorData.InvalidPrerenderExport.message(prefix, suffix, settings?.config.output === 'hybrid' ?? false),
 					location: { file: id },
 				});
 			} else {
