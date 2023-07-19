d6e96be10e91349f24049635c3b7d87248fafb10
Matthew Phillips <matthew@skypack.dev>
2023-07-19 14:55:45 -0400

    Remove usage from examples

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	experimental: {
-		viewTransitions: true,
-	},
-});
+export default defineConfig({});
--- examples/with-tailwindcss/astro.config.mjs
+++ examples/with-tailwindcss/astro.config.mjs
@@ -5,7 +5,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), tailwind()],
-	experimental: {
-		viewTransitions: true,
-	}
 });
