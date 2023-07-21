bdef69672683b8198ae301adb201e62fae7f9b18
Ottomated <ottomated.57@gmail.com>
2023-07-21 19:48:17 -0400

    add styles to test

--- packages/astro/test/hoisted-imports.test.js
+++ packages/astro/test/hoisted-imports.test.js
@@ -11,9 +11,10 @@ describe('Hoisted Imports', () => {
 		});
 	});
 
-	async function getAllScriptText(page) {
+	async function getPageBundleText(page) {
 		const html = await fixture.readFile(page);
 		const $ = cheerio.load(html);
+		// Scripts
 		const scriptText = [];
 
 		const importRegex = /import\s*?['"]([^'"]+?)['"]/g;
@@ -39,7 +40,22 @@ describe('Hoisted Imports', () => {
 			}
 			scriptText.push(await resolveImports(text));
 		}
-		return scriptText.join('\n');
+
+		// Styles
+		const styleText = [];
+
+		const styleLinks = $('link[rel="stylesheet"]');
+		for (let i = 0; i < styleLinks.length; i++) {
+			const href = styleLinks.eq(i).attr('href');
+			styleText.push(await fixture.readFile(href));
+		}
+		const styleBlocks = $('link[rel="stylesheet"]');
+		for (let i = 0; i < styleBlocks.length; i++) {
+			const text = styleBlocks.eq(i).text();
+			styleText.push(text);
+		}
+
+		return [scriptText.join('\n'), styleText.join('\n')];
 	}
 
 	describe('build', () => {
@@ -47,46 +63,50 @@ describe('Hoisted Imports', () => {
 			await fixture.build();
 		});
 
-		function expectScript(scripts, letter) {
+		function expectComponent([scripts, styles], letter) {
 			const regex = new RegExp(`console.log\\(['"]${letter}['"]\\)`);
-			expect(scripts, 'missing component ' + letter).to.match(regex);
+			expect(scripts, 'missing component script ' + letter).to.match(regex);
+			const styleRegex = new RegExp(`\\.${letter}`);
+			expect(styles, 'missing component style ' + letter).to.match(styleRegex);
 		}
-		function expectNotScript(scripts, letter) {
+		function expectNotComponent([scripts, styles], letter) {
 			const regex = new RegExp(`console.log\\(['"]${letter}['"]\\)`);
-			expect(scripts, "shouldn't include component " + letter).to.not.match(regex);
+			expect(scripts, "shouldn't include component script " + letter).to.not.match(regex);
+			const styleRegex = new RegExp(`\\.${letter}`);
+			expect(styles, "shouldn't include component style " + letter).to.not.match(styleRegex);
 		}
 
 		it('includes all imported scripts', async () => {
-			const scripts = await getAllScriptText('/all/index.html');
-			expectScript(scripts, 'A');
-			expectScript(scripts, 'B');
-			expectScript(scripts, 'C');
-			expectScript(scripts, 'D');
-			expectScript(scripts, 'E');
+			const bundle = await getPageBundleText('/all/index.html');
+			expectComponent(bundle, 'A');
+			expectComponent(bundle, 'B');
+			expectComponent(bundle, 'C');
+			expectComponent(bundle, 'D');
+			expectComponent(bundle, 'E');
 		});
 		it('includes all imported scripts when dynamically imported', async () => {
-			const scripts = await getAllScriptText('/dynamic/index.html');
-			expectScript(scripts, 'A');
-			expectScript(scripts, 'B');
-			expectScript(scripts, 'C');
-			expectScript(scripts, 'D');
-			expectScript(scripts, 'E');
+			const bundle = await getPageBundleText('/dynamic/index.html');
+			expectComponent(bundle, 'A');
+			expectComponent(bundle, 'B');
+			expectComponent(bundle, 'C');
+			expectComponent(bundle, 'D');
+			expectComponent(bundle, 'E');
 		});
 		it('includes no scripts when none imported', async () => {
-			const scripts = await getAllScriptText('/none/index.html');
-			expectNotScript(scripts, 'A');
-			expectNotScript(scripts, 'B');
-			expectNotScript(scripts, 'C');
-			expectNotScript(scripts, 'D');
-			expectNotScript(scripts, 'E');
+			const bundle = await getPageBundleText('/none/index.html');
+			expectNotComponent(bundle, 'A');
+			expectNotComponent(bundle, 'B');
+			expectNotComponent(bundle, 'C');
+			expectNotComponent(bundle, 'D');
+			expectNotComponent(bundle, 'E');
 		});
 		it('includes some scripts', async () => {
-			const scripts = await getAllScriptText('/some/index.html');
-			expectScript(scripts, 'A');
-			expectNotScript(scripts, 'B');
-			expectScript(scripts, 'C');
-			expectNotScript(scripts, 'D');
-			expectNotScript(scripts, 'E');
+			const bundle = await getPageBundleText('/some/index.html');
+			expectComponent(bundle, 'A');
+			expectNotComponent(bundle, 'B');
+			expectComponent(bundle, 'C');
+			expectNotComponent(bundle, 'D');
+			expectNotComponent(bundle, 'E');
 		});
 	});
 });
