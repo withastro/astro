54bb6d8546d248ed2b171f8adb68893e505e1417
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-07-21 21:24:36 +0200

    test: add setup for Vercel / Netlify tests (#7716)
    Co-authored-by: Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>

---
+++ packages/integrations/vercel/test/hosted/hosted-astro-project/astro.config.mjs
@@ -0,0 +1,11 @@
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	output: 'server',
+	adapter: vercel(),
+	experimental: {
+		assets: true,
+	},
+});
