af8454b13c040628f044ea3e163c8c1d5abbd2fe
doodlemarks <peck.mark@gmail.com>
2023-07-22 18:48:00 -0500

    whole lotta updates and including some new fonts

--- examples/blog/astro.config.mjs
+++ examples/blog/astro.config.mjs
@@ -7,4 +7,5 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	experimental: { viewTransitions: true },
 });
