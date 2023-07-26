477f2af527d0e514a8bab111d818038b70010805
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-07-26 23:58:47 +0200

    test: add test in the Node adapter for astro:assets (#7734)

---
+++ packages/integrations/node/test/image.test.js
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import nodejs from '../dist/index.js';
+import { loadFixture } from './test-utils.js';
+
+describe('Image endpoint', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+	let devPreview;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/image/',
+			output: 'server',
+			adapter: nodejs({ mode: 'standalone' }),
+			experimental: {
+				assets: true,
+			},
+		});
+		await fixture.build();
+		devPreview = await fixture.preview();
+	});
+
+	after(async () => {
+		await devPreview.stop();
+	});
+
+	it('it returns images', async () => {
+		const res = await fixture.fetch('/');
+		expect(res.status).to.equal(200);
+
+		const resImage = await fixture.fetch(
+			'/_image?href=/_astro/some_penguin.97ef5f92.png&w=50&f=webp'
+		);
+
+		console.log(resImage);
+		const content = resImage.text();
+		console.log(content);
+		expect(resImage.status).to.equal(200);
+	});
+});
