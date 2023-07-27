3793f3f7673d9b243695110110c0947ea5d73a44
Marco Schumacher <marco@schumacher.dev>
2023-07-27 23:29:00 +0200

    cleaned up test

--- packages/integrations/cloudflare/test/routes.test.js
+++ packages/integrations/cloudflare/test/routes.test.js
@@ -12,19 +12,14 @@ describe('generate _routes.json"', () => {
 		await fixture.build();
 	});
 
-	it('a', async () => {
-		try {
-			const _routesJson = await fixture.readFile('/_routes.json');
-			const routes = JSON.parse(_routesJson);
+	it('creates includes for functions and excludes for static files where needed', async () => {
+		const _routesJson = await fixture.readFile('/_routes.json');
+		const routes = JSON.parse(_routesJson);
 
-			expect(routes).to.deep.equal({
-				version: 1,
-				include: ['/a/endpoint', '/a/*'],
-				exclude: ['/a/prerendered1/index.html', '/a/prerendered1/'],
-			});
-		} catch (e) {
-			console.log(e);
-			expect(false).to.equal(true);
-		}
+		expect(routes).to.deep.equal({
+			version: 1,
+			include: ['/a/endpoint', '/a/*'],
+			exclude: ['/a/prerendered1/index.html', '/a/prerendered1/'],
+		});
 	});
 });
