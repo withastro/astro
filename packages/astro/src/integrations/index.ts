2a84125db717760796855db9496bcabb1432b0ba
Emanuele Stoppa <my.burning@gmail.com>
2023-07-28 10:23:16 +0100

    fix: rebase

--- packages/astro/src/integrations/index.ts
+++ packages/astro/src/integrations/index.ts
@@ -18,7 +18,7 @@ import type { SerializedSSRManifest } from '../core/app/types';
 import type { PageBuildData } from '../core/build/types';
 import { buildClientDirectiveEntrypoint } from '../core/client-directive/index.js';
 import { mergeConfig } from '../core/config/index.js';
-import { info, type LogOptions, AstroIntegrationLogger } from '../core/logger/core.js';
+import { info, warn, error, type LogOptions, AstroIntegrationLogger } from '../core/logger/core.js';
 import { isServerLikeOutput } from '../prerender/utils.js';
 import { validateSupportedFeatures } from './astroFeaturesValidation.js';
 
