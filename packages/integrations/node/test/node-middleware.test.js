7e934478ee1be522d7ea600ad5ec4d89b714a14f
Nate Moore <natemoo-re@users.noreply.github.com>
2023-08-01 09:52:16 -0500

    Refactor 404 and 500 approach (#7754)
    * fix(app): refactor 404 and 500 approach
    
    * chore: refactor logic
    
    * fix: always treat error as page
    
    * test: migrate ssr-prerender-404 to node adapter
    
    * feat: merge original response metadata with error response
    
    * chore: update lockfile
    
    * chore: trigger ci
    
    * chore(lint): fix lint issue
    
    * fix: ensure merged request has proper status
    
    * fix(node): prerender test
    
    * chore: update test label
    
    * fix(node): improve 404 behavior in middleware mode
    
    * fix(vercel): improve 404 behavior
    
    * fix(netlify): improve 404 behavior
    
    * chore: update test labels
    
    * chore: force ci
    
    * chore: fix lint
    
    * fix: avoid infinite loops
    
    * test: fix failing test in Node 18
    
    * chore: remove volta

--- packages/integrations/node/test/node-middleware.test.js
+++ packages/integrations/node/test/node-middleware.test.js
@@ -3,35 +3,51 @@ import { loadFixture } from './test-utils.js';
 import { expect } from 'chai';
 import * as cheerio from 'cheerio';
 
-describe('test 404 cant load', () => {
+/**
+ * @typedef {import('../../../astro/test/test-utils').Fixture} Fixture
+ */
+
+async function load() {
+	const mod = await import(`./fixtures/node-middleware/dist/server/entry.mjs?dropcache=${Date.now()}`);
+	return mod;
+}
+
+describe('behavior from middleware', () => {
+	/** @type {import('./test-utils').Fixture} */
 	let fixture;
+	let server;
+
 	before(async () => {
+		process.env.ASTRO_NODE_AUTOSTART = 'disabled';
+		process.env.PRERENDER = false;
 		fixture = await loadFixture({
 			root: './fixtures/node-middleware/',
 			output: 'server',
 			adapter: nodejs({ mode: 'standalone' }),
 		});
 		await fixture.build();
+		const { startServer } = await load();
+		let res = startServer();
+		server = res.server;
 	});
-	describe('test 404', async () => {
-		let devPreview;
 
-		before(async () => {
-			devPreview = await fixture.preview();
-		});
-		after(async () => {
-			await devPreview.stop();
-		});
+	after(async () => {
+		await server.stop();
+		await fixture.clean();
+		delete process.env.PRERENDER;
+	})
+
+	describe('404', async () => {
 		it('when mode is standalone', async () => {
-			const res = await fixture.fetch('/error-page');
+			const res = await fetch(`http://${server.host}:${server.port}/error-page`);
 
 			expect(res.status).to.equal(404);
 
 			const html = await res.text();
 			const $ = cheerio.load(html);
 
-			const h1 = $('h1');
-			expect(h1.text()).to.equal('404!!!!!!!!!!');
+			const body = $('body');
+			expect(body.text()).to.equal('Page does not exist');
 		});
 	});
 });
