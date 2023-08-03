3ed56526bc0547e279a20e5ca35ceb1899ead303
Colin McDonnell <colinmcd94@gmail.com>
2023-08-03 13:15:23 -0700

    Support Bun in create-astro

--- packages/create-astro/src/actions/next-steps.ts
+++ packages/create-astro/src/actions/next-steps.ts
@@ -5,7 +5,12 @@ import { nextSteps, say } from '../messages.js';
 
 export async function next(ctx: Pick<Context, 'cwd' | 'pkgManager' | 'skipHouston'>) {
 	let projectDir = path.relative(process.cwd(), ctx.cwd);
-	const devCmd = ctx.pkgManager === 'npm' ? 'npm run dev' : `${ctx.pkgManager} dev`;
+	const devCmd =
+		ctx.pkgManager === 'npm'
+			? 'npm run dev'
+			: ctx.pkgManager === 'bun'
+			? 'bun run dev'
+			: `${ctx.pkgManager} dev`;
 	await nextSteps({ projectDir, devCmd });
 
 	if (!ctx.skipHouston) {
