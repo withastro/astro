f32a6be8a075a5c5b650f56695b7f606210b7130
bluwy <bjornlu.dev@gmail.com>
2023-08-04 15:43:45 +0800

    Remove markdown component

--- packages/markdown/component/test/fixtures/astro-markdown-plugins/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [preact()],
-});
--- packages/markdown/component/test/fixtures/astro-markdown/src/components/Example.jsx
+++
@@ -1,5 +0,0 @@
-import { h } from 'preact';
-
-export default function () {
-  return <div id="test">Testing</div>;
-}
