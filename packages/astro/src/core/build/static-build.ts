e54645553c950f2dac0cc670b2e2d9abd1cfedb6
Nate Moore <nate@astro.build>
2023-08-07 10:58:53 -0400

    fix(#7561): refactor astro page filename logic

--- packages/astro/src/core/build/static-build.ts
+++ packages/astro/src/core/build/static-build.ts
@@ -445,22 +445,10 @@ export function makeAstroPageEntryPointFileName(
 	facadeModuleId: string,
 	routes: RouteData[]
 ) {
-	const pageModuleId = facadeModuleId
-		.replace(prefix, '')
-		.replace(ASTRO_PAGE_EXTENSION_POST_PATTERN, '.');
-	let route = routes.find((routeData) => {
-		return routeData.route === pageModuleId;
-	});
-	let name = pageModuleId;
-	if (route) {
-		name = route.route;
-	}
-	if (name.endsWith('/')) name += 'index';
-	const fileName = `${name.replaceAll('[', '_').replaceAll(']', '_').replaceAll('...', '---')}.mjs`;
-	if (name.startsWith('..')) {
-		return `pages${fileName}`;
-	}
-	return fileName;
+	const pageModuleId = facadeModuleId.replace(prefix, '').replace(ASTRO_PAGE_EXTENSION_POST_PATTERN, '.');
+	const route = routes.find((routeData) => routeData.component === pageModuleId);
+	const name = route?.route ?? pageModuleId;
+	return `pages${name.replace(/\/$/, '/index').replaceAll(/[\[\]]/g, '_').replaceAll('...', '---')}.astro.mjs`;
 }
 
 /**
