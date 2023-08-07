d828b7bcab1683efd7631370abe9203f8d1520b8
Nate Moore <nate@astro.build>
2023-08-07 11:46:06 -0400

    refactor: cleanup injectedRoute resolution logic

--- packages/astro/src/vite-plugin-integrations-container/index.ts
+++ packages/astro/src/vite-plugin-integrations-container/index.ts
@@ -1,6 +1,9 @@
-import { normalizePath, type Plugin as VitePlugin } from 'vite';
-import type { AstroSettings } from '../@types/astro.js';
+import type { Plugin as VitePlugin } from 'vite';
+import type { PluginContext } from 'rollup';
+import type { AstroSettings, InjectedRoute, ResolvedInjectedRoute } from '../@types/astro.js';
 import type { LogOptions } from '../core/logger/core.js';
+
+import { normalizePath } from 'vite';
 import { runHookServerSetup } from '../integrations/index.js';
 
 /** Connect Astro integrations into Vite, as needed. */
@@ -11,28 +14,24 @@ export default function astroIntegrationsContainerPlugin({
 	settings: AstroSettings;
 	logging: LogOptions;
 }): VitePlugin {
-	let hasResolvedInjectedRoutes = false;
 	return {
 		name: 'astro:integration-container',
 		configureServer(server) {
 			runHookServerSetup({ config: settings.config, server, logging });
 		},
-		// Run `load` hook once just to get access to `PluginContext.resolve`
-		// which allows us to resolve each injectedRoute entryPoint to a real file URL on disk
-		async load() {
-			if (hasResolvedInjectedRoutes) return;
+		async buildStart() {
 			// Ensure the injectedRoutes are all resolved to their final paths through Rollup
-			settings.resolvedInjectedRoutes = await Promise.all(settings.injectedRoutes.map(async injectedRoute => {
-				let resolvedEntryPoint: URL | undefined;
-				try {
-					const resolvedId = await this.resolve(injectedRoute.entryPoint).then(res => res?.id ?? injectedRoute.entryPoint)
-					const filename = normalizePath(resolvedId);
-					resolvedEntryPoint = new URL(`file://${filename}`);
-				} catch {}
-				return { ...injectedRoute, resolvedEntryPoint }
-			}))
-			hasResolvedInjectedRoutes = true;
-			return;
+			settings.resolvedInjectedRoutes = await Promise.all(settings.injectedRoutes.map(route => resolveEntryPoint.call(this, route)))
 		},
 	};
+}
+
+async function resolveEntryPoint(this: PluginContext, route: InjectedRoute): Promise<ResolvedInjectedRoute> {
+	const resolvedId = await this.resolve(route.entryPoint)
+		.then(res => res?.id)
+		.catch(() => undefined);
+	if (!resolvedId) return route;
+
+	const resolvedEntryPoint = new URL(`file://${normalizePath(resolvedId)}`);
+	return { ...route, resolvedEntryPoint };
 }
