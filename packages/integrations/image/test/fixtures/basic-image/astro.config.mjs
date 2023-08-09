08c3afb8606f7e0cde30db66c07782c6c058f182
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-08-09 21:03:59 +0200

    feat: remove @astrojs/image completely (#7922)

--- packages/integrations/image/test/fixtures/basic-image/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-
-// https://astro.build/config
-export default defineConfig({
-	site: 'http://localhost:4321',
-	integrations: [image({ logLevel: 'silent', serviceEntryPoint: '@astrojs/image/sharp' })]
-});
