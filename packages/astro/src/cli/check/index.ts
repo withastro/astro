65c354969e6fe0ef6d622e8f4c545e2f717ce8c6
Bjorn Lu <bjornlu.dev@gmail.com>
2023-08-09 17:06:29 +0800

    Fix examples check hang (#7998)

--- packages/astro/src/cli/check/index.ts
+++ packages/astro/src/cli/check/index.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import type { Arguments } from 'yargs-parser';
 import { error, info } from '../../core/logger/core.js';
-import { createLoggingFromFlags } from '../flags.js';
+import { createLoggingFromFlags, flagsToAstroInlineConfig } from '../flags.js';
 import { getPackage } from '../install-package.js';
 
 export async function check(flags: Arguments) {
@@ -22,6 +22,16 @@ export async function check(flags: Arguments) {
 			'The `@astrojs/check` and `typescript` packages are required for this command to work. Please manually install them into your project and try again.'
 		);
 		return;
+	}
+
+	// Run sync before check to make sure types are generated.
+	// NOTE: In the future, `@astrojs/check` can expose a `before lint` hook so that this works during `astro check --watch` too.
+	// For now, we run this once as usually `astro check --watch` is ran alongside `astro dev` which also calls `astro sync`.
+	const { sync } = await import('../../core/sync/index.js');
+	const inlineConfig = flagsToAstroInlineConfig(flags);
+	const exitCode = await sync(inlineConfig);
+	if (exitCode !== 0) {
+		process.exit(exitCode);
 	}
 
 	const { check: checker, parseArgsAsCheckConfig } = checkPackage;
