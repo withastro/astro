6b57628d128779290db3344bbb6de7282196fb97
Matthew Phillips <matthew@skypack.dev>
2023-08-11 09:58:32 -0400

    Prevent Vercel NFT from scanning /dev (#8039)
    * Prevent Vercel NFT from scanning /dev
    
    * Add a comment

--- packages/integrations/vercel/src/lib/nft.ts
+++ packages/integrations/vercel/src/lib/nft.ts
@@ -28,6 +28,9 @@ export async function copyDependenciesToFunction({
 	const { nodeFileTrace } = await import('@vercel/nft');
 	const result = await nodeFileTrace([entryPath], {
 		base: fileURLToPath(base),
+		// If you have a route of /dev this appears in source and NFT will try to
+		// scan your local /dev :8
+		ignore: ['/dev/**']
 	});
 
 	for (const error of result.warnings) {
