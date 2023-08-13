10a2fff07613111deb182bd3965fd53b2054a43a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-08-13 14:15:25 +0530

    Add Test for Sharp

---
+++ packages/astro/test/astro-sharp.test.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import * as cheerio from 'cheerio';
+import { loadFixture } from './test-utils.js';
+
+describe('Sharp Default Setup', () => {
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/astro-sharp',
+		});
+		await fixture.build();
+	});
+
+	it('Shows static content', async () => {
+		const html = await fixture.readFile('/index.html');
+		const $ = cheerio.load(html);
+		expect($('#opt-image').attr('src')).to.include('_astro');
+	});
+});
