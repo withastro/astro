13d7e21dc5e3acdbcdd70863e6ace1b9ddd69211
Sergey Shishkin <sergey@shishkin.org>
2023-08-13 20:05:38 +0200

    fix: export AstroConfigSchema as type

--- packages/astro/src/core/config/index.ts
+++ packages/astro/src/core/config/index.ts
@@ -1,6 +1,6 @@
 export { resolveConfig, resolveConfigPath, resolveFlags, resolveRoot } from './config.js';
 export { createNodeLogging } from './logging.js';
 export { mergeConfig } from './merge.js';
-export type { AstroConfigSchema } from './schema';
+export type { AstroConfigType } from './schema';
 export { createSettings } from './settings.js';
 export { loadTSConfig, updateTSConfigForFramework } from './tsconfig.js';
