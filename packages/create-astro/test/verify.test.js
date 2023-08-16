dc911cc6b603ce611b8e2080b26c502e7bb4de35
Nate Moore <nate@astro.build>
2023-08-16 10:39:42 -0500

    feat(create-astro): verify that --template exists

---
+++ packages/create-astro/test/verify.test.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+
+import { verify } from '../dist/index.js';
+import { setup } from './utils.js';
+
+describe('verify', () => {
+	const fixture = setup();
+	const exit = (code) => {
+		throw code;
+	}
+
+	it('basics', async () => {
+		const context = { template: 'basics', exit };
+		await verify(context);
+		expect(fixture.messages().length).to.equal(0, 'Did not expect `verify` to log any messages')
+	});
+
+	it('missing', async () => {
+		const context = { template: 'missing', exit };
+		let err = null;
+		try {
+			await verify(context);
+		} catch (e) {
+			err = e;
+		}
+		expect(err).to.eq(1);
+		expect(fixture.hasMessage('Template missing does not exist!'));
+	});
+
+	it('starlight', async () => {
+		const context = { template: 'starlight', exit };
+		await verify(context);
+		expect(fixture.messages().length).to.equal(0, 'Did not expect `verify` to log any messages')
+	});
+
+	it('starlight/tailwind', async () => {
+		const context = { template: 'starlight/tailwind', exit };
+		await verify(context);
+		expect(fixture.messages().length).to.equal(0, 'Did not expect `verify` to log any messages')
+	});
+});
