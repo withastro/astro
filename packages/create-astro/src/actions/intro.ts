2bdce4c4a0f2720b9c06d2025f98cddec5e22599
Nate Moore <natemoo-re@users.noreply.github.com>
2023-08-25 18:09:30 -0500

    feat(create-astro): --fancy (#8235)

--- packages/create-astro/src/actions/intro.ts
+++ packages/create-astro/src/actions/intro.ts
@@ -4,8 +4,9 @@ import { color, label } from '@astrojs/cli-kit';
 import { random } from '@astrojs/cli-kit/utils';
 import { banner, say, welcome } from '../messages.js';
 
-export async function intro(ctx: Pick<Context, 'skipHouston' | 'version' | 'username'>) {
+export async function intro(ctx: Pick<Context, 'skipHouston' | 'version' | 'username' | 'fancy'>) {
 	if (!ctx.skipHouston) {
+		const hat = ctx.fancy ? random(['🎩', '🎩', '👑', '🧢', '🍦']) : undefined;
 		await say([
 			[
 				'Welcome',
@@ -15,7 +16,7 @@ export async function intro(ctx: Pick<Context, 'skipHouston' | 'version' | 'user
 				`${ctx.username}!`,
 			],
 			random(welcome),
-		]);
+		], { hat });
 		await banner(ctx.version);
 	} else {
 		await banner(ctx.version);
