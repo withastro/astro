476cb73c94f75764653a8c70e15ba70d7a190000
Emanuele Stoppa <my.burning@gmail.com>
2023-08-28 10:19:12 +0100

    chore: correctly export `pageModule` when using `functionPerRoute`

--- packages/integrations/vercel/test/fixtures/serverless-prerender/astro.config.mjs
+++ packages/integrations/vercel/test/fixtures/serverless-prerender/astro.config.mjs
@@ -5,7 +5,6 @@ export default defineConfig({
 	adapter: vercel({
 		// Pass some value to make sure it doesn't error out
 		includeFiles: ['included.js'],
-		functionPerRoute: false,
 	}),
 	output: 'server'
 });
