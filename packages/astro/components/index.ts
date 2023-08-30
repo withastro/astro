feb009405b81c150db3d3ee3e04c1055d883690b
Princesseuh <princssdev@gmail.com>
2023-08-30 17:30:07 +0200

    fix: tsc errors in components.ts

--- packages/astro/components/index.ts
+++ packages/astro/components/index.ts
@@ -1,2 +1,6 @@
+// The `ts-expect-error` comments here are necessary because we're importing this file inside the `astro:components`
+// virtual module's types, which means that `tsc` will try to resolve these imports. Don't mind the editor errors.
+// @ts-expect-error
 export { default as Code } from './Code.astro';
+// @ts-expect-error
 export { default as Debug } from './Debug.astro';
