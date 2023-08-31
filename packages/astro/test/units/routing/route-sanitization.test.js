c211e9c6553cdf0506b453782e06167eba7abd93
Nate Moore <nate@astro.build>
2023-08-31 12:40:42 -0500

    chore: fix lint

--- packages/astro/test/units/routing/route-sanitization.test.js
+++ packages/astro/test/units/routing/route-sanitization.test.js
@@ -4,16 +4,11 @@ import {
 	createRequestAndResponse,
 	defaultLogger,
 } from '../test-utils.js';
-import { createRouteManifest } from '../../../dist/core/routing/index.js';
 import { fileURLToPath } from 'node:url';
-import { createViteLoader } from '../../../dist/core/module-loader/vite.js';
 import { expect } from 'chai';
 import { createContainer } from '../../../dist/core/dev/container.js';
 import * as cheerio from 'cheerio';
 import testAdapter from '../../test-adapter.js';
-import { getSortedPreloadedMatches } from '../../../dist/prerender/routing.js';
-import { createDevelopmentManifest } from '../../../dist/vite-plugin-astro-server/plugin.js';
-import DevPipeline from '../../../dist/vite-plugin-astro-server/devPipeline.js';
 
 const root = new URL('../../fixtures/alias/', import.meta.url);
 const fileSystem = {
@@ -34,8 +29,6 @@ const fileSystem = {
 };
 
 describe('Route sanitization', () => {
-	let pipeline;
-	let manifestData;
 	let container;
 	let settings;
 
@@ -52,18 +45,6 @@ describe('Route sanitization', () => {
 			settings,
 			logger: defaultLogger,
 		});
-
-		const loader = createViteLoader(container.viteServer);
-		const manifest = createDevelopmentManifest(container.settings);
-		pipeline = new DevPipeline({ manifest, logger: defaultLogger, settings, loader });
-		manifestData = createRouteManifest(
-			{
-				cwd: fileURLToPath(root),
-				settings,
-				fsMod: fs,
-			},
-			defaultLogger
-		);
 	});
 
 	after(async () => {
