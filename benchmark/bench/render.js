535a4589998041cf2e3dfcac462a51479c1d13a3
giovanni <854413241@qq.com>
2023-09-04 18:42:46 +0800

    fix: remove duplicate import (#8393)
    Co-authored-by: jesse <qfei.shao@gmail.com>

--- benchmark/bench/render.js
+++ benchmark/bench/render.js
@@ -5,9 +5,8 @@ import http from 'node:http';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { waitUntilBusy } from 'port-authority';
-import { calculateStat } from './_util.js';
+import { calculateStat, astroBin } from './_util.js';
 import { renderFiles } from '../make-project/render-default.js';
-import { astroBin } from './_util.js';
 
 const port = 4322;
 
