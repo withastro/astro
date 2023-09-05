7d95bd9baaf755239fd7d35e4813861b2dbccf42
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-09-05 23:31:15 +0530

    Allow vite to refer to inlined CSS (#8351)
    * fix(client build): keep inlined stylesheets
    
    * add changesets
    
    * appease linter
    
    * eslint: allow variables beginning with an underscore to be unused
    
    * remove eslint-ignore that's no longer needed
    
    * ready for review

---
+++ packages/astro/test/css-dangling-references.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import { loadFixture } from './test-utils.js';
+
+const cssAssetReferenceRegExp = /_astro\/[A-Za-z0-9\-]+\.[a0-9a-f]{8}\.css/g
+
+describe("When Vite's preloadModule polyfill is used", async () => {
+
+    let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+            root: './fixtures/css-dangling-references/'
+        });
+		await fixture.build();
+	});
+
+    it('there are no references to deleted CSS chunks', async () => {
+        
+        const fileNames = await fixture.readdir('/_astro/')
+        const filePaths = fileNames.map(filename => '_astro/' + filename)
+        
+        const expectations = 
+            filePaths
+            .filter(filePath => filePath.endsWith('js'))
+            .map(async filePath => {
+                const contents = await fixture.readFile(filePath)
+                const cssReferences = contents.match(cssAssetReferenceRegExp)
+                
+                if (cssReferences === null) return
+                
+                expect(filePaths).to.contain.members(cssReferences, filePath + ' contains a reference to a deleted css asset: ' + cssReferences)
+            })
+        
+        await Promise.all(expectations)
+    })
+})
---
+++ packages/astro/test/fixtures/css-dangling-references/src/components/Wrapper.svelte
@@ -0,0 +1,15 @@
+<script>
+
+  export let path
+  
+  const allAppModules = import.meta.glob('./*.svelte')  
+  
+  const AppModule = Object.entries(allAppModules).find(
+    ([key]) => key.includes(path)
+  )[1]
+
+</script>
+
+{#await AppModule() then Mod}
+    <Mod.default />
+{/await}
