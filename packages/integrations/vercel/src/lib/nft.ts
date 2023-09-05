966bc2ca0ef890d9fa7e1a9cd1a82d49fa0a9973
Emanuele Stoppa <my.burning@gmail.com>
2023-09-05 10:45:17 +0100

    chore: fix build

--- packages/integrations/vercel/src/lib/nft.ts
+++ packages/integrations/vercel/src/lib/nft.ts
@@ -21,7 +21,7 @@ export async function copyDependenciesToFunction(
 	cache: object
 ): Promise<{ handler: string }> {
 	const entryPath = fileURLToPath(entry);
-	logger.info(`Bundling function ${relative(fileURLToPath(outDir), entryPath)}`);
+	logger.info(`Bundling function ${relativePath(fileURLToPath(outDir), entryPath)}`);
 
 	// Get root of folder of the system (like C:\ on Windows or / on Linux)
 	let base = entry;
