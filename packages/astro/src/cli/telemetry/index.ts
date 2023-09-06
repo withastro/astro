d2f2a11cdb42b0de79be21c798eda8e7e7b2a277
Jacob Lamb <jacobtlamb@hey.com>
2023-09-06 02:15:10 -0700

    Improve Package Detection (#8306)

--- packages/astro/src/cli/telemetry/index.ts
+++ packages/astro/src/cli/telemetry/index.ts
@@ -2,14 +2,17 @@
 import type yargs from 'yargs-parser';
 import * as msg from '../../core/messages.js';
 import { telemetry } from '../../events/index.js';
+import whichPm from 'which-pm';
 
 interface TelemetryOptions {
 	flags: yargs.Arguments;
 }
 
+
 export async function notify() {
+	const packageManager = (await whichPm(process.cwd())).name ?? 'npm';
 	await telemetry.notify(() => {
-		console.log(msg.telemetryNotice() + '\n');
+		console.log(msg.telemetryNotice(packageManager) + '\n');
 		return true;
 	});
 }
