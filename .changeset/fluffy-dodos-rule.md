49dc0abf34d4e98f6029fae681e0723507b7929a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-09-16 15:00:01 +0530

    log only once in serverless adapter

---
+++ .changeset/fluffy-dodos-rule.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/vercel': minor
+---
+
+log only once in serverless adapter
--- packages/integrations/vercel/src/serverless/adapter.ts
+++ packages/integrations/vercel/src/serverless/adapter.ts
@@ -245,6 +245,8 @@ You can set functionPerRoute: false to prevent surpassing the limit.`
 				const filesToInclude = includeFiles?.map((file) => new URL(file, _config.root)) || [];
 				filesToInclude.push(...extraFilesToInclude);
 
+				getRuntime(true);
+
 				// Multiple entrypoint support
 				if (_entryPoints.size) {
 					const getRouteFuncName = (route: RouteData) => route.component.replace('src/pages/', '');
@@ -312,29 +314,31 @@ You can set functionPerRoute: false to prevent surpassing the limit.`
 	};
 }
 
-function getRuntime() {
+function getRuntime(verbose?: boolean) {
 	const version = process.version.slice(1); // 'v16.5.0' --> '16.5.0'
 	const major = version.split('.')[0]; // '16.5.0' --> '16'
-	const support = SUPPORTED_NODE_VERSIONS[major];
-	if (support === undefined) {
-		console.warn(
-			`[${PACKAGE_NAME}] The local Node.js version (${major}) is not supported by Vercel Serverless Functions.`
-		);
-		console.warn(`[${PACKAGE_NAME}] Your project will use Node.js 18 as the runtime instead.`);
-		console.warn(`[${PACKAGE_NAME}] Consider switching your local version to 18.`);
-		return 'nodejs18.x';
-	}
-	if (support.status === 'deprecated') {
-		console.warn(
-			`[${PACKAGE_NAME}] Your project is being built for Node.js ${major} as the runtime.`
-		);
-		console.warn(
-			`[${PACKAGE_NAME}] This version is deprecated by Vercel Serverless Functions, and scheduled to be disabled on ${new Intl.DateTimeFormat(
-				undefined,
-				{ dateStyle: 'long' }
-			).format(support.removal)}.`
-		);
-		console.warn(`[${PACKAGE_NAME}] Consider upgrading your local version to 18.`);
+	if (verbose) {
+		const support = SUPPORTED_NODE_VERSIONS[major];
+		if (support === undefined) {
+			console.warn(
+				`[${PACKAGE_NAME}] The local Node.js version (${major}) is not supported by Vercel Serverless Functions.`
+			);
+			console.warn(`[${PACKAGE_NAME}] Your project will use Node.js 18 as the runtime instead.`);
+			console.warn(`[${PACKAGE_NAME}] Consider switching your local version to 18.`);
+			return 'nodejs18.x';
+		}
+		if (support.status === 'deprecated') {
+			console.warn(
+				`[${PACKAGE_NAME}] Your project is being built for Node.js ${major} as the runtime.`
+			);
+			console.warn(
+				`[${PACKAGE_NAME}] This version is deprecated by Vercel Serverless Functions, and scheduled to be disabled on ${new Intl.DateTimeFormat(
+					undefined,
+					{ dateStyle: 'long' }
+				).format(support.removal)}.`
+			);
+			console.warn(`[${PACKAGE_NAME}] Consider upgrading your local version to 18.`);
+		}
 	}
 	return `nodejs${major}.x`;
 }
