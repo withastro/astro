37462a063304ff0d756ddd207f4090d6c7bb4964
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-09-19 15:11:10 +0000

    fix(node): report render error to the terminal

--- packages/integrations/node/src/nodeMiddleware.ts
+++ packages/integrations/node/src/nodeMiddleware.ts
@@ -51,6 +51,7 @@ export default function (app: NodeApp, mode: Options['mode']) {
 				await writeWebResponse(app, res, response);
 			}
 		} catch (err: unknown) {
+			console.error(new Error(`Could not render ${req.url}`, { cause: err }))
 			if (!res.headersSent) {
 				res.writeHead(500, `Server error`);
 				res.end();
