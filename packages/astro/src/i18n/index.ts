5839e1419fda63d75699d52baf5a00d0cfa497d7
Emanuele Stoppa <my.burning@gmail.com>
2023-09-21 09:47:38 +0200

    Update packages/astro/src/i18n/index.ts
    Co-authored-by: Florian Lefebvre <contact@florian-lefebvre.dev>

--- packages/astro/src/i18n/index.ts
+++ packages/astro/src/i18n/index.ts
@@ -5,11 +5,12 @@ import type { Logger } from '../core/logger/core.js';
  * The base URL
  */
 export function getI18nBaseUrl(locale: string, config: AstroConfig, logger: Logger) {
-	if (!config.experimental.i18n) {
-		logger.debug('i18n', "The project isn't using i18n features, no need to use this function.");
-		return;
-	}
 	const base = config.base;
+
+	if (!config.experimental.i18n) {
+		logger.error('i18n', "The project isn't using i18n features, no need to use this function.");
+		return base ? `/${base}/` : '/';
+	}
 
 	if (base) {
 		logger.debug('i18n', 'The project has a base directory, using it.');
