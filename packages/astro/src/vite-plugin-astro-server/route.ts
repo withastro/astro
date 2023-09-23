30861f8aceb0ecf98696efceeab0ab437acd1a6a
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-09-23 04:22:26 +0000

    fix(rerouting): check that the new route is different

--- packages/astro/src/vite-plugin-astro-server/route.ts
+++ packages/astro/src/vite-plugin-astro-server/route.ts
@@ -220,6 +220,7 @@ export async function handleRoute({
 	let response = await pipeline.renderRoute(renderContext, mod);
 	if (response.status === 404 && has404Route(manifestData)) {
 		const fourOhFourRoute = await matchRoute('/404', manifestData, pipeline);
+		if (fourOhFourRoute?.route !== options.route)
 		return handleRoute({
 			...options,
 			matchedRoute: fourOhFourRoute,
@@ -342,6 +343,6 @@ function getStatus(matchedRoute?: MatchedRoute): 404 | 500 | undefined {
 	if (matchedRoute.route.route === '/500') return 500;
 }
 
-function has404Route(manifest: ManifestData): RouteData | undefined {
-	return manifest.routes.find((route) => route.route === '/404');
+function has404Route(manifest: ManifestData): boolean {
+	return manifest.routes.some((route) => route.route === '/404');
 }
