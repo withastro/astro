85b92eb7aa7400e82b887ded844581242a6196d6
Nate Moore <nate@astro.build>
2023-10-02 15:24:20 -0500

    update examples

--- examples/blog/astro.config.mjs
+++ examples/blog/astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+// https://astro.build/config!
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// https://astro.build/config!
 export default defineConfig({});
