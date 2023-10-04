6bba4efb3f545790431a9008dc941534f5c5050a
Chris <mail@prass.tech>
2023-10-04 12:23:58 +0200

    Fixes: Shiki syntax highlighting adds is:raw attribute to the HTML output (#8715)
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

---
+++ packages/markdown/remark/test/shiki.js
@@ -0,0 +1,16 @@
+import { createMarkdownProcessor } from '../dist/index.js';
+import chai from 'chai';
+
+describe('shiki syntax highlighting', async () => {
+	const processor = await createMarkdownProcessor();
+
+	it('does not add is:raw to the output', async () => {
+		const {
+			code,
+		} = await processor.render('```\ntest\n```');
+
+		chai
+			.expect(code)
+			.not.to.contain("is:raw");
+	});
+});
