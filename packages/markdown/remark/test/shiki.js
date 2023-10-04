05833ab44e9088764de2ac04da794ee990d36a4e
bluwy <bluwy@users.noreply.github.com>
2023-10-04 10:26:21 +0000

    [ci] format

--- packages/markdown/remark/test/shiki.js
+++ packages/markdown/remark/test/shiki.js
@@ -5,12 +5,8 @@ describe('shiki syntax highlighting', async () => {
 	const processor = await createMarkdownProcessor();
 
 	it('does not add is:raw to the output', async () => {
-		const {
-			code,
-		} = await processor.render('```\ntest\n```');
+		const { code } = await processor.render('```\ntest\n```');
 
-		chai
-			.expect(code)
-			.not.to.contain("is:raw");
+		chai.expect(code).not.to.contain('is:raw');
 	});
 });
