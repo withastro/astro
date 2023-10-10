7669231c1587c9fb5b16a039e0c9a5e824ed5bd2
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-10-10 14:37:42 +0000

    switch supportsViewTransitions to import.meta.env

--- packages/astro/src/transitions/router.ts
+++ packages/astro/src/transitions/router.ts
@@ -387,7 +387,7 @@ let navigateOnSeverWarned = false;
 
 export function navigate(href: string, options?: Options) {
 	
-	if (!supportsViewTransitions) {
+	if (import.meta.env.SSR) {
 		if (!navigateOnSeverWarned) {
 			// instantiate an error for the stacktrace to show to user.
 			const warning = new Error("The view transtions client API was called during a server side render. This may be unintentional as the navigate() function is expected to be called in response to user interactions. Please make sure that your usage is correct.");
