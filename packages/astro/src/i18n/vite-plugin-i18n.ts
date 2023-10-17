81abf24e52d8dc2900995367d6984aac880ccca9
Emanuele Stoppa <my.burning@gmail.com>
2023-10-17 19:43:43 +0100

    feat(i18n): fallback system in SSR (#8850)
    * feat(i18n): fallback system in SSG
    
    * feat(i18n): fallback system in SSR

--- packages/astro/src/i18n/vite-plugin-i18n.ts
+++ packages/astro/src/i18n/vite-plugin-i18n.ts
@@ -1,18 +1,17 @@
 import * as vite from 'vite';
 import type { AstroSettings } from '../@types/astro.js';
-import type { Logger } from '../core/logger/core.js';
 
 const virtualModuleId = 'astro:i18n';
 const resolvedVirtualModuleId = '\0' + virtualModuleId;
 
 type AstroInternalization = {
 	settings: AstroSettings;
-	logger: Logger;
 };
 
 export default function astroInternalization({ settings }: AstroInternalization): vite.Plugin {
 	return {
 		name: 'astro:i18n',
+		enforce: 'pre',
 		async resolveId(id) {
 			if (id === virtualModuleId) {
 				return resolvedVirtualModuleId;
