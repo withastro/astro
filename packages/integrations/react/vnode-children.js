65cd3cabc5d685aeb31077f5b78988fa97028ab9
Matthew Phillips <matthew@skypack.dev>
2023-10-23 15:53:51 -0400

    Use recursion instead of walking

--- packages/integrations/react/vnode-children.js
+++ packages/integrations/react/vnode-children.js
@@ -1,35 +1,30 @@
-import { parse, walkSync, DOCUMENT_NODE, ELEMENT_NODE, TEXT_NODE } from 'ultrahtml';
+import { parse, DOCUMENT_NODE, ELEMENT_NODE, TEXT_NODE } from 'ultrahtml';
 import { createElement, Fragment } from 'react';
 
 let ids = 0;
 export default function convert(children) {
-	const nodeMap = new WeakMap();
 	let doc = parse(children.toString().trim());
 	let id = ids++;
 	let key = 0;
-	let root = createElement(Fragment, { children: [] });
 
-	walkSync(doc, (node, parent, index) => {
-		let newNode = {};
-		if (node.type === DOCUMENT_NODE) {
-			nodeMap.set(node, root);
-		} else if (node.type === ELEMENT_NODE) {
+	function createReactElementFromNode(node) {
+		const childVnodes = Array.isArray(node.children) ? node.children.map(child => {
+			if(child.type === ELEMENT_NODE) {
+				return createReactElementFromNode(child);
+			} else if(child.type === TEXT_NODE) {
+				// 0-length text gets omitted in JSX
+				return child.value.trim() ? child.value : undefined;
+			}
+		}).filter(n => !!n) : undefined;
+	
+		if(node.type === DOCUMENT_NODE) {
+			return createElement(Fragment, {}, childVnodes);
+		} else if(node.type === ELEMENT_NODE) {
 			const { class: className, ...props } = node.attributes;
-			// NOTE: do not manually pass `children`, React handles this internally
-			newNode = createElement(node.name, { ...props, className, key: `${id}-${key++}` }, []);
-			nodeMap.set(node, newNode);
-			if (parent) {
-				const newParent = nodeMap.get(parent);
-				newParent.props.children[index] = newNode;
-			}
-		} else if (node.type === TEXT_NODE) {
-			newNode = node.value;
-			if (newNode.trim() && parent) {
-				const newParent = nodeMap.get(parent);
-				newParent.props.children[index] = newNode;
-			}
+			return createElement(node.name, { ...props, className, key: `${id}-${key++}` }, childVnodes);
 		}
-	});
+	}
 
+	const root = createReactElementFromNode(doc);
 	return root.props.children;
 }
