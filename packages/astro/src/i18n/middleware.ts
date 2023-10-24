b14eb7ffc5507d3e5119803e2c866e5e56183ad1
Emanuele Stoppa <my.burning@gmail.com>
2023-10-24 11:07:27 +0100

    feat(i18n): fallback logic of  defaultLocale for i18n routing (#8899)

--- packages/astro/src/i18n/middleware.ts
+++ packages/astro/src/i18n/middleware.ts
@@ -1,5 +1,6 @@
 import type { MiddlewareEndpointHandler } from '../@types/astro.js';
 import type { SSRManifest } from '../@types/astro.js';
+import type { Environment } from '../core/render/index.js';
 
 export function createI18nMiddleware(
 	i18n: SSRManifest['i18n']
@@ -10,23 +11,42 @@ export function createI18nMiddleware(
 	const locales = i18n.locales;
 
 	return async (context, next) => {
-		if (!i18n.fallback) {
-			return next();
+		if (!i18n.fallback || !i18n.fallback) {
+			return await next();
 		}
-
 		const response = await next();
-		if (i18n.fallbackControl === 'redirect' && response instanceof Response) {
-			const fallbackKeys = i18n.fallback ? Object.keys(i18n.fallback) : [];
-			const url = context.url;
+
+		const url = context.url;
+		if (response instanceof Response) {
 			const separators = url.pathname.split('/');
+			const fallbackWithRedirect = i18n.fallbackControl === 'redirect';
+			if (fallbackWithRedirect && url.pathname.includes(`/${i18n.defaultLocale}`)) {
+				const content = await response.text();
+				const newLocation = url.pathname.replace(`/${i18n.defaultLocale}`, '');
+				response.headers.set('Location', newLocation);
+				return new Response(content, {
+					status: 302,
+					headers: response.headers,
+				});
+			}
+			if (fallbackWithRedirect && response.status >= 300) {
+				const fallbackKeys = i18n.fallback ? Object.keys(i18n.fallback) : [];
 
-			const urlLocale = separators.find((s) => locales.includes(s));
+				const urlLocale = separators.find((s) => locales.includes(s));
 
-			if (urlLocale && fallbackKeys.includes(urlLocale)) {
-				// TODO: correctly handle chain of fallback
-				const fallbackLocale = i18n.fallback[urlLocale][0];
-				const newPathname = url.pathname.replace(`/${urlLocale}`, `/${fallbackLocale}`);
-				return context.redirect(newPathname);
+				if (urlLocale && fallbackKeys.includes(urlLocale)) {
+					const fallbackLocale = i18n.fallback[urlLocale];
+					let newPathname: string;
+					// If a locale falls back to the default locale, we want to **remove** the locale because
+					// the default locale doesn't have a prefix
+					if (fallbackLocale === i18n.defaultLocale) {
+						newPathname = url.pathname.replace(`/${urlLocale}`, ``);
+					} else {
+						newPathname = url.pathname.replace(`/${urlLocale}`, `/${fallbackLocale}`);
+					}
+
+					return context.redirect(newPathname);
+				}
 			}
 		}
 
