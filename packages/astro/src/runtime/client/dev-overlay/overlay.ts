3cb49bfdd473c4c8cdaed6edfe5e83abb3c6e64f
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2023-11-07 16:47:39 +0100

    fix: remove duplicate property

--- packages/astro/src/runtime/client/dev-overlay/overlay.ts
+++ packages/astro/src/runtime/client/dev-overlay/overlay.ts
@@ -72,9 +72,7 @@ export class AstroDevOverlay extends HTMLElement {
 				height: 56px;
 				overflow: hidden;
 				pointer-events: auto;
-
 				background: linear-gradient(180deg, #13151A 0%, rgba(19, 21, 26, 0.88) 100%);
-				box-shadow: 0px 0px 0px 0px #13151A4D;
 				border: 1px solid #343841;
 				border-radius: 9999px;
 				box-shadow: 0px 0px 0px 0px rgba(19, 21, 26, 0.30), 0px 1px 2px 0px rgba(19, 21, 26, 0.29), 0px 4px 4px 0px rgba(19, 21, 26, 0.26), 0px 10px 6px 0px rgba(19, 21, 26, 0.15), 0px 17px 7px 0px rgba(19, 21, 26, 0.04), 0px 26px 7px 0px rgba(19, 21, 26, 0.01);
