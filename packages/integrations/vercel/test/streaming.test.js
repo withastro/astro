503f60fe8b0004fb449900f73fcbf560cb979794
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-11-07 14:01:04 +0000

    feat(vercel): streaming (#8879)

---
+++ packages/integrations/vercel/test/streaming.test.js
@@ -0,0 +1,21 @@
+import { loadFixture } from './test-utils.js';
+import { expect } from 'chai';
+
+describe('maxDuration', () => {
+	/** @type {import('./test-utils.js').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/streaming/',
+		});
+		await fixture.build();
+	});
+
+	it('makes it to vercel function configuration', async () => {
+		const vcConfig = JSON.parse(
+			await fixture.readFile('../.vercel/output/functions/render.func/.vc-config.json')
+		);
+		expect(vcConfig).to.deep.include({ supportsResponseStreaming: true });
+	});
+});
