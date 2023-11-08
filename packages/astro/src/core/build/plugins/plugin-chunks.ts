240fbfcb55bf22e113306331d15e985716e5e702
Nate Moore <nate@astro.build>
2023-11-08 16:42:20 -0600

    chore: add comment

--- packages/astro/src/core/build/plugins/plugin-chunks.ts
+++ packages/astro/src/core/build/plugins/plugin-chunks.ts
@@ -8,6 +8,7 @@ export function vitePluginChunks(): VitePlugin {
         outputOptions(outputOptions) {
             extendManualChunks(outputOptions, {
                 after(id) {
+                    // Place Astro's server runtime in a single `astro/server.mjs` file
                     if (id.includes('astro/dist/runtime/server/')) {
                         return 'astro/server'
                     }
@@ -17,6 +18,7 @@ export function vitePluginChunks(): VitePlugin {
     }
 }
 
+// Build plugin that configures specific chunking behavior
 export function pluginChunks(): AstroBuildPlugin {
 	return {
 		targets: ['server'],
