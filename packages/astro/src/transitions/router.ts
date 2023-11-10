8c71d7a5a886168b72109219d160612bf1a63eae
Matthew Phillips <matthew@skypack.dev>
2023-11-10 11:03:47 -0500

    Add another comment

--- packages/astro/src/transitions/router.ts
+++ packages/astro/src/transitions/router.ts
@@ -463,6 +463,7 @@ export function navigate(href: string, options?: Options) {
 	}
 	const toLocation = new URL(href, location.href);
 	// We do not have page transitions on navigations to the same page (intra-page navigation)
+	// *unless* they are form posts which have side-effects and so need to happen
 	// but we want to handle prevent reload on navigation to the same page
 	// Same page means same origin, path and query params (but maybe different hash)
 	if (location.origin === toLocation.origin && samePage(toLocation) && !options?.formData) {
