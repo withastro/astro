3b0d7d0b7d48f7a2838fb98b913c570a57578065
bluwy <bjornlu.dev@gmail.com>
2023-11-13 22:22:52 +0800

    Fix markdoc shiki

--- packages/integrations/markdoc/src/extensions/shiki.ts
+++ packages/integrations/markdoc/src/extensions/shiki.ts
@@ -1,20 +1,17 @@
 import Markdoc from '@markdoc/markdoc';
-import { createShikiHighlighter, type ShikiHighlighter } from '@astrojs/markdown-remark';
+import { createShikiHighlighter } from '@astrojs/markdown-remark';
 import type { ShikiConfig } from 'astro';
 import { unescapeHTML } from 'astro/runtime/server/index.js';
 import type { AstroMarkdocConfig } from '../config.js';
 
 export default async function shiki(config?: ShikiConfig): Promise<AstroMarkdocConfig> {
-	let highlighterAsync: Promise<ShikiHighlighter> | undefined;
+	const highlighter = await createShikiHighlighter(config);
 
 	return {
 		nodes: {
 			fence: {
 				attributes: Markdoc.nodes.fence.attributes!,
-				async transform({ attributes }) {
-					highlighterAsync ??= createShikiHighlighter(config);
-					const highlighter = await highlighterAsync;
-
+				transform({ attributes }) {
 					const lang = typeof attributes.language === 'string' ? attributes.language : 'plaintext';
 					const html = highlighter.highlight(attributes.content, lang);
 
