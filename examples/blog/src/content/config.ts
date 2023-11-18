ce5539c3f0713aba645e081525da54f9a9de9db2
Thom van den Akker <thodor20@gmail.com>
2023-11-18 20:25:01 +0100

    Update config.ts

--- examples/blog/src/content/config.ts
+++ examples/blog/src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
+	// Type must either be 'content' for showing page content or 'data' for data files (json, yaml, etc)
+	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
