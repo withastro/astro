190b8c14afa17c639490d81be777ca487207101e
Louis Gariépy <louis.gariepy.42@gmail.com>
2023-11-18 14:47:09 -0500

    Make content collection type explicit in examples.

--- examples/blog/src/content/config.ts
+++ examples/blog/src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
+	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
--- examples/portfolio/src/content/config.ts
+++ examples/portfolio/src/content/config.ts
@@ -2,6 +2,7 @@ import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	work: defineCollection({
+		type: 'content',
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
