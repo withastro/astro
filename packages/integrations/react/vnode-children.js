28911b789a3cd670c7d172c03a0ee669ddbc5e7e
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-11-20 16:36:26 +0000

    make children `undefined` with self-closing tags

--- packages/integrations/react/vnode-children.js
+++ packages/integrations/react/vnode-children.js
@@ -1,6 +1,9 @@
 import { parse, DOCUMENT_NODE, ELEMENT_NODE, TEXT_NODE } from 'ultrahtml';
 import { createElement, Fragment } from 'react';
 
+// https://github.com/facebook/react/blob/bbb9cb116dbf7b6247721aa0c4bcb6ec249aa8af/packages/react-dom-bindings/src/server/ReactFizzConfigDOM.js#L3593-L3617
+const selfClosingTags = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'keygen', 'meta', 'param', 'source', 'track', 'wbr']
+
 let ids = 0;
 export default function convert(children) {
 	let doc = parse(children.toString().trim());
@@ -8,6 +11,10 @@ export default function convert(children) {
 	let key = 0;
 
 	function createReactElementFromNode(node) {
+		if (node.isSelfClosingTag === true && Array.isArray(node.children) && node.children.length === 0) {
+			node.children = undefined;
+		}
+			
 		const childVnodes = Array.isArray(node.children)
 			? node.children
 					.map((child) => {
