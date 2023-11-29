e0aa82fc1b2f11bad2cbd4fcd997e8e852c4afea
Fred K. Schott <fkschott@gmail.com>
2023-11-29 13:50:40 -0800

    dev overlay polish

--- packages/astro/src/runtime/client/dev-overlay/plugins/audit.ts
+++ packages/astro/src/runtime/client/dev-overlay/plugins/audit.ts
@@ -1,7 +1,7 @@
 import type { DevOverlayMetadata, DevOverlayPlugin } from '../../../../@types/astro.js';
 import type { DevOverlayHighlight } from '../ui-library/highlight.js';
-import { getIconElement } from '../ui-library/icons.js';
 import { attachTooltipToHighlight, createHighlight, positionHighlight } from './utils/highlight.js';
+import { createWindowElement } from './utils/window.js';
 
 const icon =
 	'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 16"><path fill="#fff" d="M.6 2A1.1 1.1 0 0 1 1.7.9h16.6a1.1 1.1 0 1 1 0 2.2H1.6A1.1 1.1 0 0 1 .8 2Zm1.1 7.1h6a1.1 1.1 0 0 0 0-2.2h-6a1.1 1.1 0 0 0 0 2.2ZM9.3 13H1.8a1.1 1.1 0 1 0 0 2.2h7.5a1.1 1.1 0 1 0 0-2.2Zm11.3 1.9a1.1 1.1 0 0 1-1.5 0l-1.7-1.7a4.1 4.1 0 1 1 1.6-1.6l1.6 1.7a1.1 1.1 0 0 1 0 1.6Zm-5.3-3.4a1.9 1.9 0 1 0 0-3.8 1.9 1.9 0 0 0 0 3.8Z"/></svg>';
@@ -32,8 +32,6 @@ export default {
 		document.addEventListener('astro:page-load', async () => refreshLintPositions);
 
 		async function lint() {
-			initStyle();
-
 			audits.forEach(({ highlightElement }) => {
 				highlightElement.remove();
 			});
@@ -65,20 +63,38 @@ export default {
 					})
 				);
 
-				const noAuditBlock = document.createElement('div');
-				noAuditBlock.id = 'no-audit';
+				const window = createWindowElement(
+					`<style>
+						header {
+							display: flex;
+						}
+	
+						h1 {
+							display: flex;
+							align-items: center;
+							gap: 8px;
+							font-weight: 600;
+							color: #fff;
+							margin: 0;
+							font-size: 22px;
+						}
+	
+						astro-dev-overlay-icon {
+							width: 1em;
+						   height: 1em;
+						   padding: 8px;
+							display: block;
+							background: green;
+							border-radius: 9999px;
+						}
+					</style>
+					<header>
+						<h1><astro-dev-overlay-icon icon="check-circle"></astro-dev-overlay-icon>No issues detected.</h1>
+					</header>
+					`
+				);
 
-				const noAuditIcon = getIconElement('check-circle');
-				const text = document.createElement('div');
-				text.textContent = 'No issues found!';
-
-				if (noAuditIcon) {
-					noAuditIcon.style.width = '24px';
-					noAuditBlock.append(noAuditIcon);
-				}
-				noAuditBlock.append(text);
-
-				canvas.append(noAuditBlock);
+				canvas.append(window);
 			}
 
 			(['scroll', 'resize'] as const).forEach((event) => {
@@ -163,51 +179,5 @@ export default {
 
 			return tooltip;
 		}
-
-		function initStyle() {
-			const devOverlayRect = document
-				.querySelector('astro-dev-overlay')
-				?.shadowRoot.querySelector('#dev-overlay')
-				?.getBoundingClientRect();
-
-			const style = document.createElement('style');
-			style.textContent = `
-			:host {
-				opacity: 0;
-				transition: opacity 0.1s ease-in-out;
-			}
-
-			:host([data-active]) {
-				opacity: 1;
-			}
-
-			#no-audit {
-				border: 1px solid rgba(113, 24, 226, 1);
-				background-color: #310A65;
-				box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.30), 0px 1px 2px 0px rgba(0, 0, 0, 0.29), 0px 4px 4px 0px rgba(0, 0, 0, 0.26), 0px 10px 6px 0px rgba(0, 0, 0, 0.15), 0px 17px 7px 0px rgba(0, 0, 0, 0.04), 0px 26px 7px 0px rgba(0, 0, 0, 0.01);
-				color: white;
-				text-align: center;
-				border-radius: 4px;
-				padding: 8px;
-				font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-				position: fixed;
-				transform: translate(-50%, 0);
-				top: ${(devOverlayRect?.top ?? 0) - (devOverlayRect?.height ?? 0) - 16}px;
-				left: calc(50% + 12px);
-				width: 200px;
-			}
-		`;
-
-			canvas.append(style);
-		}
-	},
-	async beforeTogglingOff(canvas) {
-		canvas.host?.removeAttribute('data-active');
-
-		await new Promise((resolve) => {
-			canvas.host.addEventListener('transitionend', resolve);
-		});
-
-		return true;
 	},
 } satisfies DevOverlayPlugin;
--- packages/astro/src/runtime/client/dev-overlay/plugins/utils/window.ts
+++ packages/astro/src/runtime/client/dev-overlay/plugins/utils/window.ts
@@ -1,42 +1,5 @@
-export function createWindowWithTransition(windowContent: string, addedNodes: Node[] = []) {
+export function createWindowElement(content: string) {
 	const windowElement = document.createElement('astro-dev-overlay-window');
-	windowElement.innerHTML = `
-				<style>
-					:host {
-						opacity: 0;
-						transition: opacity 0.15s ease-in-out;
-					}
-
-					:host([data-active]) {
-						opacity: 1;
-					}
-
-					@media screen and (prefers-reduced-motion: no-preference) {
-						:host astro-dev-overlay-window {
-							transform: translateY(55px) translate(-50%, -50%);
-							transition: transform 0.15s ease-in-out;
-							transform-origin: center bottom;
-						}
-
-						:host([data-active]) astro-dev-overlay-window {
-							transform: translateY(0) translate(-50%, -50%);
-						}
-					}
-				</style>
-				${windowContent}
-		`;
-
-	windowElement.append(...addedNodes);
-
+	windowElement.innerHTML = content;
 	return windowElement;
-}
-
-export async function waitForTransition(canvas: ShadowRoot): Promise<boolean> {
-	canvas.host?.removeAttribute('data-active');
-
-	await new Promise((resolve) => {
-		canvas.host.addEventListener('transitionend', resolve);
-	});
-
-	return true;
 }
