deeb1b88098e8ba7a81a3735fdfbfbf3de09596e
Matthew Phillips <matthew@skypack.dev>
2023-11-30 15:00:05 -0500

    Improve highlight/tooltip positioning

--- packages/astro/src/runtime/client/dev-overlay/plugins/utils/highlight.ts
+++ packages/astro/src/runtime/client/dev-overlay/plugins/utils/highlight.ts
@@ -15,23 +15,34 @@ export function createHighlight(rect: DOMRect, icon?: Icon) {
 	return highlight;
 }
 
-export function getHighlightZIndex(el: Element) {
+// Figures out the element's z-index and position, based on it's parents.
+export function getElementsPositionInDocument(el: Element) {
 	let highestZIndex = 0;
+	let fixed = false;
 	let current: Element | ParentNode | null = el;
 	while (current instanceof Element) {
-		let zIndex = Number(getComputedStyle(current).zIndex);
+		let style = getComputedStyle(current);
+		let zIndex = Number(style.zIndex);
 		if (!Number.isNaN(zIndex) && zIndex > highestZIndex) {
 			highestZIndex = zIndex;
 		}
+		if(style.position === 'fixed') {
+			fixed = true;
+		}
 		current = current.parentNode;
 	}
-	return highestZIndex + 1;
+	return {
+		zIndex: highestZIndex + 1,
+		fixed,
+	};
 }
 
 export function positionHighlight(highlight: DevOverlayHighlight, rect: DOMRect) {
 	highlight.style.display = 'block';
+	// If the highlight is fixed, don't position based on scroll
+	const scrollY = highlight.style.position === 'fixed' ? 0 : window.scrollY;
 	// Make an highlight that is 10px bigger than the element on all sides
-	highlight.style.top = `${Math.max(rect.top + window.scrollY - 10, 0)}px`;
+	highlight.style.top = `${Math.max(rect.top + scrollY - 10, 0)}px`;
 	highlight.style.left = `${Math.max(rect.left + window.scrollX - 10, 0)}px`;
 	highlight.style.width = `${rect.width + 15}px`;
 	highlight.style.height = `${rect.height + 15}px`;
