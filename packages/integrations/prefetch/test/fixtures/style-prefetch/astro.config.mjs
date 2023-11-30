8da1a10b85f661d8648d74ff58e953a8e389cd81
bluwy <bjornlu.dev@gmail.com>
2023-11-30 17:21:32 +0800

    Remove @astrojs/prefetch integration

--- packages/integrations/prefetch/test/fixtures/style-prefetch/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import prefetch from '@astrojs/prefetch';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [prefetch()],
-});
