99a696b11f18529d46184d4683b21593f5791241
Fred K. Schott <fkschott@gmail.com>
2023-12-01 15:48:30 -0800

    Update a11y.ts

--- packages/astro/src/runtime/client/dev-overlay/plugins/audit/a11y.ts
+++ packages/astro/src/runtime/client/dev-overlay/plugins/audit/a11y.ts
@@ -1,8 +1,28 @@
-// This file is adapted from the [Svelte](https://github.com/sveltejs/svelte) repository
-// Specifically https://github.com/sveltejs/svelte/blob/d57eff76ed24ae2330f11f3d3938761ae4e14b4b/packages/svelte/src/compiler/phases/2-analyze/a11y.js
+/**
+ * https://github.com/sveltejs/svelte/blob/61e5e53eee82e895c1a5b4fd36efb87eafa1fc2d/LICENSE.md
+ * @license MIT
+ *
+ * Copyright (c) 2016-23 [these people](https://github.com/sveltejs/svelte/graphs/contributors)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-// This code is licensed under the MIT License per https://github.com/sveltejs/svelte/blob/d57eff76ed24ae2330f11f3d3938761ae4e14b4b/LICENSE.md
-// See [Astro's LICENSE](https://github.com/withastro/astro/blob/main/LICENSE) for more information.
 import type { AuditRuleWithSelector } from './index.js';
 import type { ARIARoleDefinitionKey } from 'aria-query';
 import { roles, aria } from 'aria-query';
