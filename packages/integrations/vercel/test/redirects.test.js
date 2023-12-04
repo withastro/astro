1aa6de73f6cacd2be637fb8e7be30ac151586f5c
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-12-04 21:55:59 +0000

    align tests with intended behavior

--- packages/integrations/vercel/test/redirects.test.js
+++ packages/integrations/vercel/test/redirects.test.js
@@ -32,15 +32,15 @@ describe('Redirects', () => {
 	it('define static routes', async () => {
 		const config = await getConfig();
 
-		const oneRoute = config.routes.find((r) => r.src === '/\\/one');
+		const oneRoute = config.routes.find((r) => r.src === '/one');
 		expect(oneRoute.headers.Location).to.equal('/');
 		expect(oneRoute.status).to.equal(301);
 
-		const twoRoute = config.routes.find((r) => r.src === '/\\/two');
+		const twoRoute = config.routes.find((r) => r.src === '/two');
 		expect(twoRoute.headers.Location).to.equal('/');
 		expect(twoRoute.status).to.equal(301);
 
-		const threeRoute = config.routes.find((r) => r.src === '/\\/three');
+		const threeRoute = config.routes.find((r) => r.src === '/three');
 		expect(threeRoute.headers.Location).to.equal('/');
 		expect(threeRoute.status).to.equal(302);
 	});
@@ -48,7 +48,7 @@ describe('Redirects', () => {
 	it('defines dynamic routes', async () => {
 		const config = await getConfig();
 
-		const blogRoute = config.routes.find((r) => r.src.startsWith('/\\/blog'));
+		const blogRoute = config.routes.find((r) => r.src.startsWith('/blog'));
 		expect(blogRoute).to.not.be.undefined;
 		expect(blogRoute.headers.Location.startsWith('/team/articles')).to.equal(true);
 		expect(blogRoute.status).to.equal(301);
@@ -57,7 +57,7 @@ describe('Redirects', () => {
 	it('define trailingSlash redirect for sub pages', async () => {
 		const config = await getConfig();
 
-		const subpathRoute = config.routes.find((r) => r.src === '/\\/subpage');
+		const subpathRoute = config.routes.find((r) => r.src === '/subpage');
 		expect(subpathRoute).to.not.be.undefined;
 		expect(subpathRoute.headers.Location).to.equal('/subpage/');
 	});
