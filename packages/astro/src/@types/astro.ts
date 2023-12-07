54883c411dbdda6cf6a6e4fe11386d9b9fa8f7e4
Mads Erik Forberg <forberg@gmail.com>
2023-12-07 10:16:40 +0100

    chore: update type for client:visible

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -70,7 +70,7 @@ export interface AstroBuiltinProps {
 	'client:load'?: boolean;
 	'client:idle'?: boolean;
 	'client:media'?: string;
-	'client:visible'?: boolean;
+	'client:visible'?: string;
 	'client:only'?: boolean | string;
 }
 
