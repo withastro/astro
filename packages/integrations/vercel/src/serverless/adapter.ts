bfd0770085c69c0b88935f6ee62d1f748e901af4
Jacob Lamb <jacobtlamb@hey.com>
2023-12-11 22:30:10 -0800

    Add warning for beta

--- packages/integrations/vercel/src/serverless/adapter.ts
+++ packages/integrations/vercel/src/serverless/adapter.ts
@@ -377,6 +377,13 @@ function validateRuntime() {
 	const version = process.version.slice(1); // 'v16.5.0' --> '16.5.0'
 	const major = version.split('.')[0]; // '16.5.0' --> '16'
 	const support = SUPPORTED_NODE_VERSIONS[major];
+	if (support.status === 'beta') {
+		console.warn(
+			`[${PACKAGE_NAME}] The local Node.js version (${major}) is currently in beta by Vercel Serverless Functions.`
+		);
+		console.warn(`[${PACKAGE_NAME}] Make sure to update your Vercel settings to use ${major}.`);
+		return;
+	}
 	if (support === undefined) {
 		console.warn(
 			`[${PACKAGE_NAME}] The local Node.js version (${major}) is not supported by Vercel Serverless Functions.`
