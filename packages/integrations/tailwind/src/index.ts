9328ce2cbed17dc64b387d8819c858dc7019e6c0
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-12-17 13:18:48 +0100

    fix: enforce type

--- packages/integrations/tailwind/src/index.ts
+++ packages/integrations/tailwind/src/index.ts
@@ -1,4 +1,4 @@
-import type { AstroIntegration } from 'astro';
+import type { AstroIntegration, ViteUserConfig } from 'astro';
 import autoprefixerPlugin from 'autoprefixer';
 import tailwindPlugin from 'tailwindcss';
 import type { CSSOptions, UserConfig } from 'vite';
@@ -24,7 +24,7 @@ async function getPostCssConfig(
 async function getViteConfiguration(
 	tailwindConfigPath: string | undefined,
 	viteConfig: UserConfig
-) {
+): Promise<Partial<ViteUserConfig>> {
 	// We need to manually load postcss config files because when inlining the tailwind and autoprefixer plugins,
 	// that causes vite to ignore postcss config files
 	const postcssConfigResult = await getPostCssConfig(viteConfig.root, viteConfig.css?.postcss);
