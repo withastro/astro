b16fcbb3878d7c4e5000a83da138c9dfdd5aa934
Erika <3019731+Princesseuh@users.noreply.github.com>
2023-12-21 15:49:03 -0500

    fix: temporarily disable checking `.js(x)` files (#737)
    * fix: temporarily disable checking `.js(x)` files
    
    * fix: only output once
    
    * chore: changeset

---
+++ .changeset/tricky-wasps-check.md
@@ -0,0 +1,6 @@
+---
+'@astrojs/language-server': patch
+'@astrojs/check': patch
+---
+
+Temporarily disable checking `.jsx` and `.tsx` files using `astro check` until https://github.com/withastro/language-tools/issues/727 is fixed
--- packages/language-server/src/check.ts
+++ packages/language-server/src/check.ts
@@ -33,6 +33,9 @@ export class AstroCheck {
 	public project!: ReturnType<typeof kit.createProject>;
 	private linter!: ReturnType<typeof kit.createLinter>;
 
+	// TODO: Remove this when the JSX typing issue is fixed
+	private skipJSX = false;
+
 	constructor(
 		private readonly workspacePath: string,
 		private readonly typescriptPath: string | undefined,
@@ -60,8 +63,12 @@ export class AstroCheck {
 			  }
 			| undefined;
 	}): Promise<CheckResult> {
-		const files =
+		let files =
 			fileNames !== undefined ? fileNames : this.project.languageHost.getScriptFileNames();
+
+		if (this.skipJSX) {
+			files = files.filter((file) => !file.endsWith('.jsx') && !file.endsWith('.tsx'));
+		}
 
 		const result: CheckResult = {
 			status: undefined,
@@ -160,6 +167,14 @@ export class AstroCheck {
 					absolute: true,
 				});
 			});
+		}
+
+		const files = this.project.languageHost.getScriptFileNames();
+		if (files.some((file) => file.endsWith('.jsx') || file.endsWith('.tsx'))) {
+			console.warn(
+				'\x1b[33;1mWARNING:\x1b[0m Checking `.jsx` and `.tsx` files is temporarily disabled due to an issue in the Astro language server and TypeScript. See https://github.com/withastro/language-tools/issues/727 for more details. In the meantime, such files can be checked using `tsc --noEmit`.'
+			);
+			this.skipJSX = true;
 		}
 
 		this.linter = kit.createLinter(config, this.project.languageHost);
