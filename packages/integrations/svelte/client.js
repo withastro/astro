218617473bbf9ad6e1f4f91fc3125d658d1af8ea
Nikhil Kothari <nikhilk@users.noreply.github.com>
2023-12-23 20:58:48 -0800

    Filter out Svelte's unknown data prop warnings
    Fixes https://github.com/withastro/astro/issues/9508
    
    The existing console filter looked for class attributes. This change adds a check for warnings related to data attributes as well.

--- packages/integrations/svelte/client.js
+++ packages/integrations/svelte/client.js
@@ -104,9 +104,11 @@ function finishUsingConsoleFilter() {
  */
 function filteredConsoleWarning(msg, ...rest) {
 	if (consoleFilterRefs > 0 && typeof msg === 'string') {
-		// Astro passes a `class` prop to the Svelte component, which
+		// Astro passes `class` and `data-astro-cid` props to the Svelte component, which
 		// outputs the following warning, which we can safely filter out.
-		const isKnownSvelteError = msg.endsWith("was created with unknown prop 'class'");
+		const isKnownSvelteError =
+			msg.endsWith("was created with unknown prop 'class'") ||
+			msg.indexOf("was created with unknown prop 'data-astro-cid") > 0;
 		if (isKnownSvelteError) return;
 	}
 	originalConsoleWarning(msg, ...rest);
