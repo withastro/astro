84a7703d6b45e09b3954941cc4c594a8504de64c
bluwy <bjornlu.dev@gmail.com>
2023-12-27 18:23:06 +0800

    Better Vue workaround

--- packages/astro/test/fixtures/vue-component/astro.config.mjs
+++ packages/astro/test/fixtures/vue-component/astro.config.mjs
@@ -3,11 +3,17 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [vue({
-        template: {
-			compilerOptions: {
-				isCustomElement: tag => tag.includes('my-button')
-			}
-        }
-	})],
+	integrations: [
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => tag.includes('my-button'),
+				},
+				// Don't transform img src to imports
+				transformAssetUrls: {
+					includeAbsolute: false,
+				},
+			},
+		}),
+	],
 });
