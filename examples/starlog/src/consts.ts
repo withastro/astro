d25813f2cc0baabcda2f504f3da3c040caf37d9a
Mark Peck <peck.mark@gmail.com>
2023-12-28 15:08:51 -0600

    feat(examples): add starlog release notes theme
    Co-authored-by: Florian Lefebvre <69633530+florian-lefebvre@users.noreply.github.com>
    Co-authored-by: Emanuele Stoppa <602478+ematipico@users.noreply.github.com>
    Co-authored-by: Nate Moore <7118177+natemoo-re@users.noreply.github.com>
    Co-authored-by:  Matthew Phillips <361671+matthewp@users.noreply.github.com>

---
+++ examples/starlog/src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SiteTitle = 'Starlog';
+export const SiteDescription = 'Welcome to my website!';
+
