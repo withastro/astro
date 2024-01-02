d112bded60ea2faa75134ebdd4cc463cc190abb9
bluwy <bjornlu.dev@gmail.com>
2024-01-02 22:32:50 +0800

    Check against UnhandledRejection

--- packages/astro/src/vite-plugin-astro-server/error.ts
+++ packages/astro/src/vite-plugin-astro-server/error.ts
@@ -3,8 +3,9 @@ import type { AstroConfig } from '../@types/astro.js';
 import type DevPipeline from './devPipeline.js';
 
 import { collectErrorMetadata } from '../core/errors/dev/index.js';
-import { createSafeError } from '../core/errors/index.js';
+import { createSafeError, AstroErrorData } from '../core/errors/index.js';
 import { formatErrorMessage } from '../core/messages.js';
+import { eventError, telemetry } from '../events/index.js';
 
 export function recordServerError(
 	loader: ModuleLoader,
@@ -22,6 +23,11 @@ export function recordServerError(
 	// This is our last line of defense regarding errors where we still might have some information about the request
 	// Our error should already be complete, but let's try to add a bit more through some guesswork
 	const errorWithMetadata = collectErrorMetadata(err, config.root);
+
+	// Ignore unhandled rejection errors as they appear A LOT and we cannot record the amount to telemetry
+	if (errorWithMetadata.name !== AstroErrorData.UnhandledRejection.name) {
+		telemetry.record(eventError({ cmd: 'dev', err: errorWithMetadata, isFatal: false }));
+	}
 
 	pipeline.logger.error(
 		null,
