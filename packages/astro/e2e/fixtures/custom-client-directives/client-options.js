7723c01541f3a5609dc33c178406f62004341ecf
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-01-03 15:38:30 -0500

    nit: comment

--- packages/astro/e2e/fixtures/custom-client-directives/client-options.js
+++ packages/astro/e2e/fixtures/custom-client-directives/client-options.js
@@ -1,4 +1,4 @@
-// Hydrate on first click on the window
+// Hydrate directly and write the passed options to the DOM
 export default async (load, options) => {
 		const hydrate = await load()
 		document.write(`<div id="options">Passed options are: ${JSON.stringify(options.value)}</div>`)
