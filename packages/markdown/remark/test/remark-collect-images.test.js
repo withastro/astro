89bfd8404aafcd56886c08aa78fba4a78d2b9aa3
Oliver Speir <oliverspeir9@gmail.com>
2024-01-02 20:42:20 -0700

    update test to reflect new expected result

--- packages/markdown/remark/test/remark-collect-images.test.js
+++ packages/markdown/remark/test/remark-collect-images.test.js
@@ -2,32 +2,32 @@ import { createMarkdownProcessor } from '../dist/index.js';
 import chai from 'chai';
 
 describe('collect images', async () => {
-	const processor = await createMarkdownProcessor();
+    const processor = await createMarkdownProcessor();
 
-	it('should collect inline image paths', async () => {
-		const {
-			code,
-			metadata: { imagePaths },
-		} = await processor.render(`Hello ![inline image url](./img.png)`, {
-			fileURL: 'file.md',
-		});
+    it('should collect inline image paths', async () => {
+        const {
+            code,
+            metadata: { imagePaths },
+        } = await processor.render(`Hello ![inline image url](./img.png)`, {
+            fileURL: 'file.md',
+        });
 
-		chai
-			.expect(code)
-			.to.equal('<p>Hello <img alt="inline image url" __ASTRO_IMAGE_="./img.png"></p>');
+        chai
+            .expect(code)
+            .to.equal('<p>Hello <img alt="inline image url" __ASTRO_IMAGE_="{&#x22;src&#x22;:&#x22;./img.png&#x22;}"></p>');
 
-		chai.expect(Array.from(imagePaths)).to.deep.equal(['./img.png']);
-	});
+        chai.expect(Array.from(imagePaths)).to.deep.equal(['./img.png']);
+    });
 
-	it('should add image paths from definition', async () => {
-		const {
-			code,
-			metadata: { imagePaths },
-		} = await processor.render(`Hello ![image ref][img-ref]\n\n[img-ref]: ./img.webp`, {
-			fileURL: 'file.md',
-		});
+    it('should add image paths from definition', async () => {
+        const {
+            code,
+            metadata: { imagePaths },
+        } = await processor.render(`Hello ![image ref][img-ref]\n\n[img-ref]: ./img.webp`, {
+            fileURL: 'file.md',
+        });
 
-		chai.expect(code).to.equal('<p>Hello <img alt="image ref" __ASTRO_IMAGE_="./img.webp"></p>');
-		chai.expect(Array.from(imagePaths)).to.deep.equal(['./img.webp']);
-	});
+        chai.expect(code).to.equal('<p>Hello <img alt="image ref" __ASTRO_IMAGE_="{&#x22;src&#x22;:&#x22;./img.webp&#x22;}"></p>');
+        chai.expect(Array.from(imagePaths)).to.deep.equal(['./img.webp']);
+    });
 });
