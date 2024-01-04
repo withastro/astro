c3c3fa2110dd4c2d162bb48e60b79a0bee22f4f3
Arsh <ematipico@users.noreply.github.com>
2024-01-04 16:38:10 +0000

    [ci] format

--- packages/integrations/vercel/src/static/adapter.ts
+++ packages/integrations/vercel/src/static/adapter.ts
@@ -44,9 +44,9 @@ export interface VercelStaticConfig {
 	webAnalytics?: VercelWebAnalyticsConfig;
 	/**
 	 * @deprecated This option lets you configure the legacy speed insights API which is now deprecated by Vercel.
-	 * 
+	 *
 	 * See [Vercel Speed Insights Quickstart](https://vercel.com/docs/speed-insights/quickstart) for instructions on how to use the library instead.
-	 * 
+	 *
 	 * https://vercel.com/docs/speed-insights/quickstart
 	 */
 	speedInsights?: VercelSpeedInsightsConfig;
