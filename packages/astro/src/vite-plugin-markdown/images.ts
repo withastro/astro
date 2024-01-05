f03fca578bfbb744e59509582243753b5d43fd5a
Oliver Speir <115520730+OliverSpeir@users.noreply.github.com>
2024-01-05 11:22:51 -0700

    add comment back in
    Co-authored-by: Erika <3019731+Princesseuh@users.noreply.github.com>

--- packages/astro/src/vite-plugin-markdown/images.ts
+++ packages/astro/src/vite-plugin-markdown/images.ts
@@ -37,6 +37,9 @@ export function getMarkdownCodeForImages(imagePaths: MarkdownImagePath[], html: 
       });
     }
     
+	  // NOTE: This causes a top-level await to appear in the user's code, which can break very easily due to a Rollup
+	  // bug and certain adapters not supporting it correctly. See: https://github.com/rollup/rollup/issues/4708
+	  // Tread carefully!
     const html = await updateImageReferences(${JSON.stringify(html)});
   `;
 }
