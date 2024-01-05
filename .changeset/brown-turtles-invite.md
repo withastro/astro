22a5405b4a4b7948458ad170b0a7bde6954058c1
Arsh <69170106+lilnasy@users.noreply.github.com>
2024-01-05 18:27:48 +0000

    fix(vercel): include 404.html as fallback in the static adapter (#9591)
    * include 404.html as fallback
    
    * add changeset
    
    * include only when 404.astro exists
    
    * add test
    
    * route.component -> route.pathname

---
+++ .changeset/brown-turtles-invite.md
@@ -0,0 +1,5 @@
+---
+"@astrojs/vercel": patch
+---
+
+Fixes an issue where 404.astro was not used in static mode.
--- packages/integrations/vercel/src/static/adapter.ts
+++ packages/integrations/vercel/src/static/adapter.ts
@@ -125,6 +125,11 @@ export default function vercelStatic({
 							continue: true,
 						},
 						{ handle: 'filesystem' },
+						...routes.find(route => route.pathname === "/404") ? [{
+							src: `/.*`,
+							dest: `/404.html`,
+							status: 404,
+						}] : [],
 					],
 					...(imageService || imagesConfig
 						? {
