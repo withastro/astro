58ab1b28e4a162de8dfb034bdd20aa786304f5c0
Oliver Speir <oliverspeir9@gmail.com>
2024-01-06 22:05:12 -0700

    add srcset

--- packages/astro/src/vite-plugin-markdown/images.ts
+++ packages/astro/src/vite-plugin-markdown/images.ts
@@ -1,27 +1,32 @@
 export type MarkdownImagePath = { raw: string; resolved: string; safeName: string };
 
 export function getMarkdownCodeForImages(imagePaths: MarkdownImagePath[], html: string) {
-  return `
+	return `
     import { getImage } from "astro:assets";
     ${imagePaths
-      .map((entry) => `import Astro__${entry.safeName} from ${JSON.stringify(entry.raw)};`)
-      .join('\n')}
+			.map((entry) => `import Astro__${entry.safeName} from ${JSON.stringify(entry.raw)};`)
+			.join('\n')}
 
     const images = async function(html) {
       const imageSources = {};
       ${imagePaths
-        .map((entry) => {
-          return `{
-            const regex = new RegExp('__ASTRO_IMAGE_="([^"]*' + ${JSON.stringify(entry.raw)} + '[^"]*)"', 'g');
+				.map((entry) => {
+					return `{
+            const regex = new RegExp('__ASTRO_IMAGE_="([^"]*' + ${JSON.stringify(
+							entry.raw
+						)} + '[^"]*)"', 'g');
             const match = regex.exec(html);
             if (match) {
               const imageProps = JSON.parse(match[1].replace(/&#x22;/g, '"'));
               const { src, ...props } = imageProps;
-              imageSources[${JSON.stringify(entry.raw)}] = await getImage({src: Astro__${entry.safeName}, ...props});
+							console.log(props)
+              imageSources[${JSON.stringify(entry.raw)}] = await getImage({src: Astro__${
+								entry.safeName
+							}, ...props});
             }
           }`;
-        })
-        .join('\n')}
+				})
+				.join('\n')}
       return imageSources;
     }
 
@@ -29,6 +34,9 @@ export function getMarkdownCodeForImages(imagePaths: MarkdownImagePath[], html: 
       return images(html).then((imageSources) => {
         return html.replaceAll(/__ASTRO_IMAGE_="([^"]+)"/gm, (full, imagePath) => {
           const decodedImagePath = JSON.parse(imagePath.replace(/&#x22;/g, '"'));
+					if (imageSources[decodedImagePath.src].srcSet.values.length > 0) {
+						imageSources[decodedImagePath.src].attributes.srcset = imageSources[decodedImagePath.src].srcSet.attribute;
+					}
           return spreadAttributes({
             src: imageSources[decodedImagePath.src].src,
             ...imageSources[decodedImagePath.src].attributes,
@@ -43,4 +51,3 @@ export function getMarkdownCodeForImages(imagePaths: MarkdownImagePath[], html: 
     const html = await updateImageReferences(${JSON.stringify(html)});
   `;
 }
-
