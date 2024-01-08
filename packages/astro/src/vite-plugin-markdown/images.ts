1d414c929580defa85db281191ce7881b115590e
Oliver Speir <oliverspeir9@gmail.com>
2024-01-07 17:58:39 -0700

    works on multiple images

--- packages/astro/src/vite-plugin-markdown/images.ts
+++ packages/astro/src/vite-plugin-markdown/images.ts
@@ -1,53 +1,61 @@
-export type MarkdownImagePath = { raw: string; resolved: string; safeName: string };
+export type MarkdownImagePath = { raw: string; resolved: string; safeName: string; count: string };
 
 export function getMarkdownCodeForImages(imagePaths: MarkdownImagePath[], html: string) {
 	return `
-    import { getImage } from "astro:assets";
-    ${imagePaths
-			.map((entry) => `import Astro__${entry.safeName} from ${JSON.stringify(entry.raw)};`)
-			.join('\n')}
-
-    const images = async function(html) {
-      const imageSources = {};
-      ${imagePaths
-				.map((entry) => {
-					return `{
-            const regex = new RegExp('__ASTRO_IMAGE_="([^"]*' + ${JSON.stringify(
-							entry.raw
-						)} + '[^"]*)"', 'g');
-            const match = regex.exec(html);
-            if (match) {
-              const imageProps = JSON.parse(match[1].replace(/&#x22;/g, '"'));
-              const { src, ...props } = imageProps;
-							console.log(props)
-              imageSources[${JSON.stringify(entry.raw)}] = await getImage({src: Astro__${
-								entry.safeName
-							}, ...props});
-            }
-          }`;
-				})
+			import { getImage } from "astro:assets";
+			${imagePaths
+				.map((entry) => `import Astro__${entry.safeName} from ${JSON.stringify(entry.raw)};`)
 				.join('\n')}
-      return imageSources;
-    }
 
-    async function updateImageReferences(html) {
-      return images(html).then((imageSources) => {
-        return html.replaceAll(/__ASTRO_IMAGE_="([^"]+)"/gm, (full, imagePath) => {
-          const decodedImagePath = JSON.parse(imagePath.replace(/&#x22;/g, '"'));
-					if (imageSources[decodedImagePath.src].srcSet.values.length > 0) {
-						imageSources[decodedImagePath.src].attributes.srcset = imageSources[decodedImagePath.src].srcSet.attribute;
-					}
-          return spreadAttributes({
-            src: imageSources[decodedImagePath.src].src,
-            ...imageSources[decodedImagePath.src].attributes,
-          });
-        });
-      });
-    }
-    
-	  // NOTE: This causes a top-level await to appear in the user's code, which can break very easily due to a Rollup
-	  // bug and certain adapters not supporting it correctly. See: https://github.com/rollup/rollup/issues/4708
-	  // Tread carefully!
-    const html = await updateImageReferences(${JSON.stringify(html)});
-  `;
+			const images = async function(html) {
+					const imageSources = {};
+					${imagePaths
+						.map((entry) => {
+							const rawUrl = JSON.stringify(entry.raw);
+							return `{
+											const regex = new RegExp('__ASTRO_IMAGE_="([^"]*' + ${rawUrl} + '[^"]*)"', 'g');
+											let match;
+											let occurrenceCounter = 0;
+											while ((match = regex.exec(html)) !== null) {
+													const matchKey = ${rawUrl} + '_' + occurrenceCounter;
+													const imageProps = JSON.parse(match[1].replace(/&#x22;/g, '"'));
+													const { src, ...props } = imageProps;
+
+													imageSources[matchKey] = await getImage({src: Astro__${entry.safeName}, ...props});
+													occurrenceCounter++;
+											}
+									}`;
+						})
+						.join('\n')}
+					return imageSources;
+			};
+
+			async function updateImageReferences(html) {
+				return images(html).then((imageSources) => {
+						return html.replaceAll(/__ASTRO_IMAGE_="([^"]+)"/gm, (full, imagePath) => {
+								const decodedImagePath = JSON.parse(imagePath.replace(/&#x22;/g, '"'));
+		
+								// Use the 'index' property for each image occurrence
+								const srcKey = decodedImagePath.src + '_' + decodedImagePath.index.index;
+		
+								if (imageSources[srcKey].srcSet && imageSources[srcKey].srcSet.values.length > 0) {
+										console.log("srcset found", imageSources[srcKey].srcSet.attribute);
+										imageSources[srcKey].attributes.srcset = imageSources[srcKey].srcSet.attribute;
+										console.log("attributes", imageSources[srcKey].attributes);
+								}
+		
+								const { index, ...attributesWithoutIndex } = imageSources[srcKey].attributes;
+		
+								return spreadAttributes({
+										src: imageSources[srcKey].src,
+										...attributesWithoutIndex,
+								});
+						});
+				});
+		}
+		
+
+			// Top-level await warning
+			const html = await updateImageReferences(${JSON.stringify(html)});
+	`;
 }
--- packages/markdown/remark/src/rehype-images.ts
+++ packages/markdown/remark/src/rehype-images.ts
@@ -3,20 +3,30 @@ import type { MarkdownVFile } from './types.js';
 
 export function rehypeImages() {
 	return () =>
-		function (tree: any, file: MarkdownVFile) {
-			visit(tree, (node) => {
-				if (node.type !== 'element') return;
-				if (node.tagName !== 'img') return;
+			function (tree: any, file: MarkdownVFile) {
+					const imageOccurrenceMap = new Map();
 
-				if (node.properties?.src) {
-					if (file.data.imagePaths?.has(node.properties.src)) {
-						const { alt, ...otherProps } = node.properties;
-						node.properties['__ASTRO_IMAGE_'] = JSON.stringify({ ...otherProps });
-						Object.keys(otherProps).forEach((prop) => {
-							delete node.properties[prop];
-						});
-					}
-				}
-			});
-		};
+					visit(tree, (node) => {
+							if (node.type !== 'element') return;
+							if (node.tagName !== 'img') return;
+
+							if (node.properties?.src) {
+									if (file.data.imagePaths?.has(node.properties.src)) {
+											const { alt, ...otherProps } = node.properties;
+
+											// Initialize or increment occurrence count for this image
+											const currentCount = imageOccurrenceMap.get(node.properties.src) || 0;
+											imageOccurrenceMap.set(node.properties.src, currentCount + 1);
+
+											const index = { index: currentCount };
+											console.log("creating __ASTRO_IMAGE__ img ", "with props:", otherProps, "at index:", index)
+											node.properties['__ASTRO_IMAGE_'] = JSON.stringify({ ...otherProps, index });
+											
+											Object.keys(otherProps).forEach((prop) => {
+													delete node.properties[prop];
+											});
+									}
+							}
+					});
+			};
 }
