23c01eb647200de87ce2a6f27086986e8f1a3091
bholmesdev <hey@bholmes.dev>
2024-01-09 16:01:48 -0500

    fix: load seed file separately for build

--- packages/db/src/integration.ts
+++ packages/db/src/integration.ts
@@ -3,15 +3,20 @@ import { vitePluginDb } from './vite-plugin-db.js';
 import { vitePluginInjectEnvTs } from './vite-plugin-inject-env-ts.js';
 import { typegen } from './typegen.js';
 import { collectionsSchema } from './types.js';
+import { seed } from './seed.js';
 
 export function integration(): AstroIntegration {
 	return {
 		name: 'astro:db',
 		hooks: {
-			async 'astro:config:setup'({ updateConfig, config }) {
+			async 'astro:config:setup'({ updateConfig, config, command }) {
 				// TODO: refine where we load collections
 				// @matthewp: may want to load collections by path at runtime
 				const collections = collectionsSchema.parse(config.db?.collections ?? {});
+				const isDev = command === 'dev';
+				if (!isDev) {
+					await seed({ collections, root: config.root });
+				}
 				updateConfig({
 					vite: {
 						plugins: [
@@ -20,6 +25,7 @@ export function integration(): AstroIntegration {
 							vitePluginDb({
 								collections,
 								root: config.root,
+								isDev,
 							}),
 							// @ts-ignore
 							vitePluginInjectEnvTs(config),
