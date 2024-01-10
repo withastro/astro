e5a748627bd1be5740cf13fb53c46a95c41b247b
Arsh <lilnasy@users.noreply.github.com>
2024-01-10 14:53:36 +0000

    [ci] format

--- packages/integrations/vercel/src/serverless/adapter.ts
+++ packages/integrations/vercel/src/serverless/adapter.ts
@@ -288,11 +288,11 @@ You can set functionPerRoute: false to prevent surpassing the limit.`
 						maxDuration,
 					});
 					for (const route of routes) {
-						if (route.prerender) continue
+						if (route.prerender) continue;
 						routeDefinitions.push({
 							src: route.pattern.source,
 							dest: 'render',
-						})
+						});
 					}
 				}
 				const fourOhFourRoute = routes.find((route) => route.pathname === '/404');
@@ -309,11 +309,15 @@ You can set functionPerRoute: false to prevent surpassing the limit.`
 						},
 						{ handle: 'filesystem' },
 						...routeDefinitions,
-						...fourOhFourRoute ? [{
-								src: '/.*',
-								dest: fourOhFourRoute.prerender ? '/404.html' : 'render',
-								status: 404,
-							}] : [],
+						...(fourOhFourRoute
+							? [
+									{
+										src: '/.*',
+										dest: fourOhFourRoute.prerender ? '/404.html' : 'render',
+										status: 404,
+									},
+								]
+							: []),
 					],
 					...(imageService || imagesConfig
 						? {
