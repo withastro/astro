0d41a7e8a2cf0ff4cd0c11e0df0a7eaa5af09cfe
bholmesdev <hey@bholmes.dev>
2024-01-11 15:40:41 -0500

    chore: remove seed file logic

--- packages/db/src/consts.ts
+++ packages/db/src/consts.ts
@@ -7,8 +7,6 @@ export const PACKAGE_NAME = JSON.parse(
 export const INTERNAL_MOD_IMPORT = JSON.stringify(`${PACKAGE_NAME}/internal`);
 export const DRIZZLE_MOD_IMPORT = JSON.stringify(`${PACKAGE_NAME}/internal-drizzle`);
 
-export const SUPPORTED_SEED_FILES = ['db.seed.js', 'db.seed.mjs', 'db.seed.mts', 'db.seed.ts'];
-
 export const DB_TYPES_FILE = 'db-types.d.ts';
 
 export const VIRTUAL_MODULE_ID = 'astro:db';
