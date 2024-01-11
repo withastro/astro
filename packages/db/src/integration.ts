4e941af5fbc0ef17737df2b2348d8b5e529f0ae1
bholmesdev <hey@bholmes.dev>
2024-01-11 15:40:36 -0500

    refactor: move dev to use filesystem to run seed at right time

--- packages/db/src/integration.ts
+++ packages/db/src/integration.ts
@@ -3,29 +3,29 @@ import { vitePluginDb } from './vite-plugin-db.js';
 import { vitePluginInjectEnvTs } from './vite-plugin-inject-env-ts.js';
 import { typegen } from './typegen.js';
 import { collectionsSchema } from './types.js';
-import { seed } from './seed.js';
 import { existsSync } from 'fs';
 import { rm } from 'fs/promises';
 import { getDbUrl } from './consts.js';
+import { createDb, setupDbTables } from './internal.js';
 
 export function integration(): AstroIntegration {
 	return {
 		name: 'astro:db',
 		hooks: {
-			async 'astro:config:setup'({ updateConfig, config, command }) {
+			async 'astro:config:setup'({ logger, updateConfig, config, command }) {
 				if (command === 'preview') return;
 
 				// TODO: refine where we load collections
 				// @matthewp: may want to load collections by path at runtime
 				const collections = collectionsSchema.parse(config.db?.collections ?? {});
-				const isDev = command === 'dev';
-				if (command === 'build') {
-					const dbUrl = getDbUrl(config.root);
-					if (existsSync(dbUrl)) {
-						await rm(dbUrl);
-					}
-					await seed({ collections, root: config.root });
+				const dbUrl = getDbUrl(config.root);
+				if (existsSync(dbUrl)) {
+					await rm(dbUrl);
 				}
+				const db = await createDb({ dbUrl: dbUrl.href });
+				await setupDbTables({ db, collections, logger });
+				logger.info('Collections set up 🚀');
+
 				updateConfig({
 					vite: {
 						plugins: [
@@ -34,7 +34,6 @@ export function integration(): AstroIntegration {
 							vitePluginDb({
 								collections,
 								root: config.root,
-								isDev,
 							}),
 							// @ts-ignore
 							vitePluginInjectEnvTs(config),
