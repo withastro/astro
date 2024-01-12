fd9109bcffebfd531b6fd8b5125465b5874b2517
Matthew Phillips <matthew@skypack.dev>
2024-01-12 15:47:17 -0500

    CLI sync command

---
+++ packages/db/src/cli/index.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { red } from 'kleur/colors';
+import { astroConfigWithDBValidator } from '../config.js';
+import { errorMap } from '../error-map.js';
+import { loadAstroConfig } from '../load-astro-config.js';
+
+async function main() {
+	const cmd = process.argv[2];
+
+	switch (cmd) {
+		case 'sync': {
+			const { sync } = await import('./sync/index.js');
+			const astroConfig = await getAstroConfigOrExit();
+
+			const collections = astroConfig.db?.collections ?? {};
+
+			return await sync({ collections });
+		}
+		default: {
+			// eslint-disable-next-line no-console
+			console.error(`Unknown command: ${cmd}`);
+			return;
+		}
+	}
+}
+
+async function getAstroConfigOrExit(root: string = process.cwd()) {
+	const astroConfig = await loadAstroConfig(root);
+	const parsed = astroConfigWithDBValidator.safeParse(astroConfig, { errorMap });
+	if (parsed.success) {
+		return parsed.data;
+	}
+	// eslint-disable-next-line no-console
+	console.error(
+		red('⚠️ Invalid studio config. Check your astro config file\n') +
+			parsed.error.issues.map((i) => i.message).join('\n'),
+	);
+	process.exit(0);
+}
+
+await main();
