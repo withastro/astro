2fb04285d2bf6e2b79ceb26594408f0cd19a3e27
bluwy <bjornlu.dev@gmail.com>
2024-01-17 23:31:38 +0800

    [skip ci] improve error message

--- packages/astro/src/vite-plugin-astro/index.ts
+++ packages/astro/src/vite-plugin-astro/index.ts
@@ -91,7 +91,10 @@ export default function astro({ settings, logger }: AstroPluginOptions): vite.Pl
 			const filename = normalizePath(normalizeFilename(parsedId.filename, config.root));
 			const compileMetadata = astroFileToCompileMetadata.get(filename);
 			if (!compileMetadata) {
-				throw new Error('No cached compile metadata found for ' + id);
+				throw new Error(
+					`No cached compile metadata found for "${id}". The main Astro module "${filename}" should have ` +
+						`compiled and filled the metadata first, before its virtual modules can be requested.`
+				);
 			}
 
 			switch (query.type) {
