f61248895b8b7007c1fd5dccbd550f2d123f2da2
Emanuele Stoppa <my.burning@gmail.com>
2024-01-17 13:10:43 +0000

    Adapter enhancements (#9661)
    * quality of life updates for `App` (#9579)
    
    * feat(app): writeResponse for node-based adapters
    
    * add changeset
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * add examples for NodeApp static methods
    
    * unexpose createOutgoingHttpHeaders from public api
    
    * move headers test to core
    
    * clientAddress test
    
    * cookies test
    
    * destructure renderOptions right at the start
    
    ---------
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Fallback node standalone to localhost (#9545)
    
    * Fallback node standalone to localhost
    
    * Update .changeset/tame-squids-film.md
    
    * quality of life updates for the node adapter (#9582)
    
    * descriptive names for files and functions
    
    * update tests
    
    * add changeset
    
    * appease linter
    
    * Apply suggestions from code review
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * `server-entrypoint.js` -> `server.js`
    
    * prevent crash on stream error (from PR 9533)
    
    * Apply suggestions from code review
    
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    
    * `127.0.0.1` -> `localhost`
    
    * add changeset for fryuni's fix
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * chore(vercel): delete request response conversion logic (#9583)
    
    * refactor
    
    * add changeset
    
    * bump peer dependencies
    
    * unexpose symbols (#9683)
    
    * Update .changeset/tame-squids-film.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    ---------
    
    Co-authored-by: Arsh <69170106+lilnasy@users.noreply.github.com>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

---
+++ packages/integrations/node/src/serve-static.ts
@@ -0,0 +1,86 @@
+import path from "node:path";
+import url from "node:url";
+import send from "send";
+import type { IncomingMessage, ServerResponse } from "node:http";
+import type { Options } from "./types.js";
+import type { NodeApp } from "astro/app/node";
+
+/**
+ * Creates a Node.js http listener for static files and prerendered pages.
+ * In standalone mode, the static handler is queried first for the static files.
+ * If one matching the request path is not found, it relegates to the SSR handler.
+ * Intended to be used only in the standalone mode.
+ */
+export function createStaticHandler(app: NodeApp, options: Options) {
+	const client = resolveClientDir(options);
+	/**
+	 * @param ssr The SSR handler to be called if the static handler does not find a matching file.
+	 */
+    return (req: IncomingMessage, res: ServerResponse, ssr: () => unknown) => {
+		if (req.url) {
+			let pathname = app.removeBase(req.url);
+			pathname = decodeURI(new URL(pathname, 'http://host').pathname);
+
+			const stream = send(req, pathname, {
+				root: client,
+				dotfiles: pathname.startsWith('/.well-known/') ? 'allow' : 'deny',
+			});
+
+			let forwardError = false;
+
+			stream.on('error', (err) => {
+				if (forwardError) {
+					console.error(err.toString());
+					res.writeHead(500);
+					res.end('Internal server error');
+					return;
+				}
+				// File not found, forward to the SSR handler
+				ssr();
+			});
+			stream.on('headers', (_res: ServerResponse) => {
+                // assets in dist/_astro are hashed and should get the immutable header
+				if (pathname.startsWith(`/${options.assets}/`)) {
+					// This is the "far future" cache header, used for static files whose name includes their digest hash.
+					// 1 year (31,536,000 seconds) is convention.
+					// Taken from https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control#immutable
+					_res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+				}
+			});
+			stream.on('directory', () => {
+				// On directory find, redirect to the trailing slash
+				let location: string;
+				if (req.url!.includes('?')) {
+					const [url1 = '', search] = req.url!.split('?');
+					location = `${url1}/?${search}`;
+				} else {
+					location = appendForwardSlash(req.url!);
+				}
+
+				res.statusCode = 301;
+				res.setHeader('Location', location);
+				res.end(location);
+			});
+			stream.on('file', () => {
+				forwardError = true;
+			});
+			stream.pipe(res);
+		} else {
+			ssr();
+		}
+	};
+}
+
+function resolveClientDir(options: Options) {
+	const clientURLRaw = new URL(options.client);
+	const serverURLRaw = new URL(options.server);
+	const rel = path.relative(url.fileURLToPath(serverURLRaw), url.fileURLToPath(clientURLRaw));
+	const serverEntryURL = new URL(import.meta.url);
+	const clientURL = new URL(appendForwardSlash(rel), serverEntryURL);
+	const client = url.fileURLToPath(clientURL);
+	return client;
+}
+
+function appendForwardSlash(pth: string) {
+	return pth.endsWith('/') ? pth : pth + '/';
+}
