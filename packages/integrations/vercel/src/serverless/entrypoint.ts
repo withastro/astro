efefcf294f75e48c1373f41c2b9ebad91335cda3
Emanuele Stoppa <my.burning@gmail.com>
2024-01-17 13:10:43 +0000

    Adapter enhancements (#9661)
    * quality of life updates for `App` (#9579)
    
    * feat(app): writeResponse for node-based adapters
    
    * add changeset
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * add examples for NodeApp static methods
    
    * unexpose createOutgoingHttpHeaders from public api
    
    * move headers test to core
    
    * clientAddress test
    
    * cookies test
    
    * destructure renderOptions right at the start
    
    ---------
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * Fallback node standalone to localhost (#9545)
    
    * Fallback node standalone to localhost
    
    * Update .changeset/tame-squids-film.md
    
    * quality of life updates for the node adapter (#9582)
    
    * descriptive names for files and functions
    
    * update tests
    
    * add changeset
    
    * appease linter
    
    * Apply suggestions from code review
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    
    * `server-entrypoint.js` -> `server.js`
    
    * prevent crash on stream error (from PR 9533)
    
    * Apply suggestions from code review
    
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    
    * `127.0.0.1` -> `localhost`
    
    * add changeset for fryuni's fix
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    
    * chore(vercel): delete request response conversion logic (#9583)
    
    * refactor
    
    * add changeset
    
    * bump peer dependencies
    
    * unexpose symbols (#9683)
    
    * Update .changeset/tame-squids-film.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    ---------
    
    Co-authored-by: Arsh <69170106+lilnasy@users.noreply.github.com>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- packages/integrations/vercel/src/serverless/entrypoint.ts
+++ packages/integrations/vercel/src/serverless/entrypoint.ts
@@ -1,35 +1,21 @@
 import type { SSRManifest } from 'astro';
-import { App } from 'astro/app';
-import { applyPolyfills } from 'astro/app/node';
+import { applyPolyfills, NodeApp } from 'astro/app/node';
 import type { IncomingMessage, ServerResponse } from 'node:http';
-
 import { ASTRO_LOCALS_HEADER } from './adapter.js';
-import { getRequest, setResponse } from './request-transform.js';
 
 applyPolyfills();
 
 export const createExports = (manifest: SSRManifest) => {
-	const app = new App(manifest);
-
+	const app = new NodeApp(manifest);
 	const handler = async (req: IncomingMessage, res: ServerResponse) => {
-		let request: Request;
-
-		try {
-			request = await getRequest(`https://${req.headers.host}`, req);
-		} catch (err: any) {
-			res.statusCode = err.status || 400;
-			return res.end(err.reason || 'Invalid request body');
-		}
-
-		let routeData = app.match(request);
-		let locals = {};
-		if (request.headers.has(ASTRO_LOCALS_HEADER)) {
-			let localsAsString = request.headers.get(ASTRO_LOCALS_HEADER);
-			if (localsAsString) {
-				locals = JSON.parse(localsAsString);
-			}
-		}
-		await setResponse(app, res, await app.render(request, { routeData, locals }));
+		const clientAddress = req.headers['x-forwarded-for'] as string | undefined;
+		const localsHeader = req.headers[ASTRO_LOCALS_HEADER]
+		const locals =
+			typeof localsHeader === "string" ? JSON.parse(localsHeader)
+			: Array.isArray(localsHeader) ? JSON.parse(localsHeader[0])
+			: {};
+		const webResponse  = await app.render(req, { locals, clientAddress })
+		await NodeApp.writeResponse(webResponse, res);
 	};
 
 	return { default: handler };
