a5f1682347e602330246129d4666a9227374c832
Ross Robino <ross@robino.dev>
2024-01-17 08:11:58 -0500

    feat: add experimental client prerender (#9644)
    * feat: add experimental client prerender
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * docs: add more details about effects of the feature
    
    * add changeset
    
    * add tests
    
    * edit jsdoc and changeset with suggestions
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    
    * Update packages/astro/src/prefetch/index.ts
    
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    
    * Update .changeset/sixty-dogs-sneeze.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Update .changeset/sixty-dogs-sneeze.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Update .changeset/sixty-dogs-sneeze.md
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    * Update packages/astro/src/@types/astro.ts
    
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    
    ---------
    
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1566,6 +1566,42 @@ export interface AstroUserConfig {
 		 * ```
 		 */
 		contentCollectionCache?: boolean;
+
+		/**
+		 * @docs
+		 * @name experimental.clientPrerender
+		 * @type {boolean}
+		 * @default `false`
+		 * @version: 4.2.0
+		 * @description
+		 * Enables pre-rendering your prefetched pages on the client in supported browsers.
+		 *
+		 * This feature uses the experimental [Speculation Rules Web API](https://developer.mozilla.org/en-US/docs/Web/API/Speculation_Rules_API) and overrides the default `prefetch` behavior globally to prerender links on the client.
+		 * You may wish to review the [possible risks when prerendering on the client](https://developer.mozilla.org/en-US/docs/Web/API/Speculation_Rules_API#unsafe_prefetching) before enabling this feature.
+		 *
+		 * Enable client side prerendering in your `astro.config.mjs` along with any desired `prefetch` configuration options:
+		 *
+		 * ```js
+		 * // astro.config.mjs
+		 * {
+		 *   prefetch: {
+		 *     prefetchAll: true,
+		 *     defaultStrategy: 'viewport',
+		 *   },
+		 * 	experimental: {
+		 * 		clientPrerender: true,
+		 * 	},
+		 * }
+		 * ```
+		 *
+		 * Continue to use the `data-astro-prefetch` attribute on any `<a />` link on your site to opt in to prefetching.
+		 * Instead of appending a `<link>` tag to the head of the document or fetching the page with JavaScript, a `<script>` tag will be appended with the corresponding speculation rules.
+		 *
+		 * Client side prerendering requires browser support. If the Speculation Rules API is not supported, `prefetch` will fallback to the supported strategy.
+		 *
+		 * See the [Prefetch Guide](https://docs.astro.build/en/guides/prefetch/) for more `prefetch` options and usage.
+		 */
+		clientPrerender?: boolean;
 	};
 }
 
