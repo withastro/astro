962c425ceb013f21dc69717ce8459eeae369dfbc
Fred K. Schott <fkschott@gmail.com>
2024-01-18 22:04:23 -0800

    cleanup migration commands

---
+++ packages/db/src/migrations.ts
@@ -0,0 +1,38 @@
+import deepDiff from 'deep-diff';
+import { mkdir, readFile, readdir, writeFile } from 'fs/promises';
+const { diff, applyChange } = deepDiff;
+
+export async function getMigrations(): Promise<string[]> {
+	const migrationFiles = await readdir('./migrations').catch((err) => {
+		if (err.code === 'ENOENT') {
+			return [];
+		}
+		throw err;
+	});
+	return migrationFiles;
+}
+
+export async function loadMigration(migration: string): Promise<{ diff: any[]; db: any[] }> {
+	return JSON.parse(await readFile(`./migrations/${migration}`, 'utf-8'));
+}
+
+export async function loadInitialSnapshot(): Promise<any> {
+	return JSON.parse(await readFile('./migrations/0000_snapshot.json', 'utf-8'));
+}
+
+export async function initializeMigrationsDirectory(currentSnapshot: unknown) {
+	await mkdir('./migrations');
+	await writeFile('./migrations/0000_snapshot.json', JSON.stringify(currentSnapshot, undefined, 2));
+}
+
+export async function initializeFromMigrations(allMigrationFiles: string[]) {
+	const prevSnapshot = await loadInitialSnapshot();
+	for (const migration of allMigrationFiles) {
+		if (migration === '0000_snapshot.json') continue;
+		const migrationContent = await loadMigration(migration);
+		migrationContent.diff.forEach((change: any) => {
+			applyChange(prevSnapshot, {}, change);
+		});
+	}
+	return prevSnapshot;
+}
