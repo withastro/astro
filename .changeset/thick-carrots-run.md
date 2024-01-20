a505190933365268d48139a5f197a3cfb5570870
Bjorn Lu <bjornlu.dev@gmail.com>
2024-01-20 18:59:40 +0800

    Make `@astrojs/markdown-remark` browser-safe (#9738)

---
+++ .changeset/thick-carrots-run.md
@@ -0,0 +1,5 @@
+---
+"@astrojs/markdown-remark": minor
+---
+
+Fixes usage in browser environments by using subpath imports
--- packages/markdown/remark/src/load-plugins.ts
+++ packages/markdown/remark/src/load-plugins.ts
@@ -1,26 +1,12 @@
-import { resolve as importMetaResolve } from 'import-meta-resolve';
-import path from 'node:path';
-import { pathToFileURL } from 'node:url';
 import type * as unified from 'unified';
+import { importPlugin as _importPlugin } from '#import-plugin';
 
-let cwdUrlStr: string | undefined;
-
-async function importPlugin(p: string | unified.Plugin): Promise<unified.Plugin> {
+async function importPlugin(p: string | unified.Plugin<any[], any>) {
 	if (typeof p === 'string') {
-		// Try import from this package first
-		try {
-			const importResult = await import(p);
-			return importResult.default;
-		} catch {}
-
-		// Try import from user project
-		cwdUrlStr ??= pathToFileURL(path.join(process.cwd(), 'package.json')).toString();
-		const resolved = importMetaResolve(p, cwdUrlStr);
-		const importResult = await import(resolved);
-		return importResult.default;
+		return await _importPlugin(p);
+	} else {
+		return p;
 	}
-
-	return p;
 }
 
 export function loadPlugins(
