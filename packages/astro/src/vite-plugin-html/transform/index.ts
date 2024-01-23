2831cf687da91ebadeca4afa43bbcb56616b1a98
Nate Moore <natemoo-re@users.noreply.github.com>
2024-01-23 12:21:02 -0600

    Update packages/astro/src/vite-plugin-html/transform/index.ts

--- packages/astro/src/vite-plugin-html/transform/index.ts
+++ packages/astro/src/vite-plugin-html/transform/index.ts
@@ -5,7 +5,7 @@ import escape from './escape.js';
 import slots, { SLOT_PREFIX } from './slots.js';
 
 export async function transform(code: string, id: string) {
-	let s = new MagicString(code, { filename: id });
+	const s = new MagicString(code, { filename: id });
 	const parser = rehype().data('settings', { fragment: true }).use(escape, { s }).use(slots, { s });
 
 	const vfile = new VFile({ value: code, path: id });
