b0c74c94b6025c8b1492ec56cd9005786aa96d6d
Florian Lefebvre <ematipico@users.noreply.github.com>
2024-01-24 14:44:08 +0000

    [ci] format

--- packages/integrations/alpinejs/src/index.ts
+++ packages/integrations/alpinejs/src/index.ts
@@ -4,32 +4,32 @@ import { resolve } from 'node:path';
 
 interface Options {
 	/**
- 	*	You can extend Alpine by setting this option to a root-relative import specifier (for example, `entrypoint: "/src/entrypoint"`).
-	*
-	* The default export of this file should be a function that accepts an Alpine instance prior to starting, allowing the use of custom directives, plugins and other customizations for advanced use cases.
-	*
-	* ```js
-	* // astro.config.mjs
-	* import { defineConfig } from 'astro/config';
-	* import alpine from '@astrojs/alpinejs';
-	* 
-	* export default defineConfig({
-	*   // ...
-	*   integrations: [alpine({ entrypoint: '/src/entrypoint' })],
-	* });
-	* ```
-	*
-	* ```js
-	* // src/entrypoint.ts
-	* import type { Alpine } from 'alpinejs'
-	*
-	* export default (Alpine: Alpine) => {
-	*     Alpine.directive('foo', el => {
-	*         el.textContent = 'bar';
-	*     })
-	* }
-	* ```
-	*/
+	 *	You can extend Alpine by setting this option to a root-relative import specifier (for example, `entrypoint: "/src/entrypoint"`).
+	 *
+	 * The default export of this file should be a function that accepts an Alpine instance prior to starting, allowing the use of custom directives, plugins and other customizations for advanced use cases.
+	 *
+	 * ```js
+	 * // astro.config.mjs
+	 * import { defineConfig } from 'astro/config';
+	 * import alpine from '@astrojs/alpinejs';
+	 *
+	 * export default defineConfig({
+	 *   // ...
+	 *   integrations: [alpine({ entrypoint: '/src/entrypoint' })],
+	 * });
+	 * ```
+	 *
+	 * ```js
+	 * // src/entrypoint.ts
+	 * import type { Alpine } from 'alpinejs'
+	 *
+	 * export default (Alpine: Alpine) => {
+	 *     Alpine.directive('foo', el => {
+	 *         el.textContent = 'bar';
+	 *     })
+	 * }
+	 * ```
+	 */
 	entrypoint?: string;
 }
 
