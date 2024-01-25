ff0dca3b6fee2cba787997602e36ffa98a771596
Nate Moore <nate@astro.build>
2024-01-25 16:41:11 -0600

    wip: db action

--- packages/db/src/cli/commands/verify/index.ts
+++ packages/db/src/cli/commands/verify/index.ts
@@ -5,9 +5,13 @@ import { getMigrations, initializeFromMigrations } from '../../../migrations.js'
 const { diff } = deepDiff;
 
 export async function cmd({ config }: { config: AstroConfig; flags: Arguments }) {
+	const core = await isGithubActions();
 	const currentSnapshot = JSON.parse(JSON.stringify(config.db?.collections ?? {}));
 	const allMigrationFiles = await getMigrations();
 	if (allMigrationFiles.length === 0) {
+		if (core) {
+			core.setFailed(`Project not yet initialized!`);
+		}
 		console.log('Project not yet initialized!');
 		process.exit(1);
 	}
@@ -15,9 +19,25 @@ export async function cmd({ config }: { config: AstroConfig; flags: Arguments })
 	const prevSnapshot = await initializeFromMigrations(allMigrationFiles);
 	const calculatedDiff = diff(prevSnapshot, currentSnapshot);
 	if (calculatedDiff) {
+		if (core) {
+			  core.setFailed(`Schema changes detected!`);
+			  process.exit(1);
+		}
 		console.log('Changes detected!');
 		process.exit(1);
 	}
+	if (core) {
+		  core.info('No changes detected!');
+	}
 	console.log('No changes detected.');
 	return;
+}
+
+
+async function isGithubActions(): Promise<typeof import('@actions/core') | undefined> {
+	if (process.env['GITHUB_ACTIONS'] === 'true') {
+		return import('@actions/core').then(res => res.default)
+	}
+
+	return undefined;
 }
