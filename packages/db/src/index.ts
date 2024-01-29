46904a1dcb0aa9bb5945767c21b3ad1e59deb817
Fred K. Schott <fkschott@gmail.com>
2024-01-29 00:29:39 -0800

    expose relevant types

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,17 @@
 export { defineCollection, defineWritableCollection, field } from './config.js';
-
-export { type ResolvedCollectionConfig, type DBDataContext } from './config.js';
+export type { ResolvedCollectionConfig, DBDataContext } from './config.js';
+export type {
+	DBCollection,
+	DBCollections,
+	DBSnapshot,
+	DBField,
+	BooleanField,
+	NumberField,
+	TextField,
+	DateField,
+	DateFieldInput,
+	JsonField,
+	FieldType,
+} from './types.js';
 export { cli } from './cli/index.js';
 export { integration as default } from './integration.js';
