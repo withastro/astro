24eac1c53a7bca890c9b24b46c253696bc067f78
Emanuele Stoppa <my.burning@gmail.com>
2024-01-30 16:50:00 +0000

    Update packages/astro/src/@types/astro.ts
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1541,26 +1541,36 @@ export interface AstroUserConfig {
 			 * @version 4.3.0
 			 * @description
 			 *
-			 * Configures the URL pattern of one or more supported languages to use a domain (or sub-domain). When a locale is mapped to a domain, a `/[locale]/` path prefix will not be used.
+			 * Configures the URL pattern of one or more supported languages to use a custom domain (or sub-domain). When a locale is mapped to a domain, a `/[locale]/` path prefix will not be used. However, localized folders within `src/pages/` are still required, including for your configured `defaultLocale`.
 			 *
-			 *  For example, you can configure the `fr` URLs to be of the form `https://fr.example.com/blog`. If not configured in `domains`, the `defaultLocale` will default to `https://example.com/blog` and any other locale not configured will default to `https://example.com/[locale]/blog`.
+			 *  Any other locale not configured will default to a localized path-based URL according to your `prefixDefaultLocale` strategy (e.g. `https://example.com/[locale]/blog`).
+			 *
 			 *
 			 * ```js
+			 * //astro.config.mjs
 			 * export default defineConfig({
-			 * 	output: "server",
 			 * 	site: "https://example.com",
+			 * 	output: "server", // required, with no prerendered pages
+			 *   adapter: node({
+			 *     mode: 'standalone',
+			 *   }),
 			 * 	i18n: {
 			 * 		defaultLocale: "en",
 			 * 		locales: ["en", "fr", "pt-br", "es"],
-			 *		domains: {
-			 *			fr: "https://fr.example.com",
+			 *     prefixDefaultLocale: false,
+			 *		  domains: {
+			 *			  fr: "https://fr.example.com",
+			 *       es: "https://example.es"
 			 *    },
-			 * 		routing: {
-			 * 			strategy: "domains"
-			 *		}
+			 *  experimental: {
+			 *     i18nDomains: true
 			 *  }
 			 * })
 			 * ```
+			 *  
+		   * Both page routes built and URLs returned by the i18n helper functions `getAbsoluteLocaleUrl()` and `getAbsoluteLocaleUrlList()` will use the options set in `i18n.domains`.
+		   *  
+		   * See the [Internationalization Guide](https://docs.astro.build/en/guides/internationalization/#domains-experimental) for more details, including limitations of this experimental feature.
 			 */
 			domains?: Record<string, string>;
 		};
