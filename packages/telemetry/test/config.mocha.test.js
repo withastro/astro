909553991515ce0d1b8defe4ba0a818c8b3f7030
voxel!() <voxelmc@hotmail.com>
2024-01-31 08:21:23 -0800

    Remove mocha tests again

--- packages/telemetry/test/config.mocha.test.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { GlobalConfig } from '../dist/config.js';
-
-describe('GlobalConfig', () => {
-	it('initializes when expected arguments are given', () => {
-		const config = new GlobalConfig({ name: 'TEST_NAME' });
-		expect(config).to.be.instanceOf(GlobalConfig);
-	});
-});
