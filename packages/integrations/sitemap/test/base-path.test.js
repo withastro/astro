8d38e567f97c33358e15ec3bb7f394fa9fa099b6
Mohamed <10786768+xMohamd@users.noreply.github.com>
2024-01-31 01:13:57 +0000

    chore(@astrojs/integrations/sitemap): use Node.js for testing

--- packages/integrations/sitemap/test/base-path.test.js
+++ packages/integrations/sitemap/test/base-path.test.js
@@ -1,39 +1,40 @@
 import { loadFixture, readXML } from './test-utils.js';
-import { expect } from 'chai';
+import * as assert from 'node:assert/strict';
+import { describe, it, before } from 'node:test';
 
 describe('URLs with base path', () => {
-	/** @type {import('./test-utils').Fixture} */
-	let fixture;
+ /** @type {import('./test-utils').Fixture} */
+ let fixture;
 
-	describe('using node adapter', () => {
-		before(async () => {
-			fixture = await loadFixture({
-				root: './fixtures/ssr/',
-				base: '/base',
-			});
-			await fixture.build();
-		});
+ describe('using node adapter', () => {
+   before(async () => {
+     fixture = await loadFixture({
+       root: './fixtures/ssr/',
+       base: '/base',
+     });
+     await fixture.build();
+   });
 
-		it('Base path is concatenated correctly', async () => {
-			const data = await readXML(fixture.readFile('/client/sitemap-0.xml'));
-			const urls = data.urlset.url;
-			expect(urls[0].loc[0]).to.equal('http://example.com/base/one/');
-		});
-	});
+   it('Base path is concatenated correctly', async () => {
+     const data = await readXML(fixture.readFile('/client/sitemap-0.xml'));
+     const urls = data.urlset.url;
+     assert.equal(urls[0].loc[0], 'http://example.com/base/one/');
+   });
+ });
 
-	describe('static', () => {
-		before(async () => {
-			fixture = await loadFixture({
-				root: './fixtures/static/',
-				base: '/base',
-			});
-			await fixture.build();
-		});
+ describe('static', () => {
+   before(async () => {
+     fixture = await loadFixture({
+       root: './fixtures/static/',
+       base: '/base',
+     });
+     await fixture.build();
+   });
 
-		it('Base path is concatenated correctly', async () => {
-			const data = await readXML(fixture.readFile('/sitemap-0.xml'));
-			const urls = data.urlset.url;
-			expect(urls[0].loc[0]).to.equal('http://example.com/base/123/');
-		});
-	});
+   it('Base path is concatenated correctly', async () => {
+     const data = await readXML(fixture.readFile('/sitemap-0.xml'));
+     const urls = data.urlset.url;
+     assert.equal(urls[0].loc[0], 'http://example.com/base/123/');
+   });
+ });
 });
