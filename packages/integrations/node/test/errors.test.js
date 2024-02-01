a4030275160d81bc87fdf533e6aa8a3de6542a16
Arsh <69170106+lilnasy@users.noreply.github.com>
2024-02-01 07:02:40 +0000

    fix(NodeApp): end with "Internal server error" on mid-stream error (#9908)
    * fix(NodeApp): end with "Internal server error" on mid-stream error
    
    * add changeset
    
    * add test
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- packages/integrations/node/test/errors.test.js
+++ packages/integrations/node/test/errors.test.js
@@ -1,4 +1,4 @@
-import * as assert from 'node:assert/strict';
+import assert from 'node:assert/strict';
 import { describe, it, before, after } from 'node:test';
 import nodejs from '../dist/index.js';
 import { loadFixture } from './test-utils.js';
@@ -22,11 +22,26 @@ describe('Errors', () => {
 	after(async () => {
 		await devPreview.stop();
 	});
-	it('when mode is standalone', async () => {
+
+	it('rejected promise in template', async () => {
 		const res = await fixture.fetch('/in-stream');
 		const html = await res.text();
 		const $ = cheerio.load(html);
 
 		assert.equal($('p').text().trim(), 'Internal server error');
+	});
+	
+	it('generator that throws called in template', async () => {
+		/** @type {Response} */
+		const res = await fixture.fetch('/generator');
+		const reader = res.body.getReader();
+		const decoder = new TextDecoder();
+		const expect = async ({ done, value }) => {
+			const result = await reader.read();
+			assert.equal(result.done, done);
+			if (!done) assert.equal(decoder.decode(result.value), value);
+		}
+		await expect({ done: false, value: "<!DOCTYPE html><h1>Astro</h1> 1Internal server error" });
+		await expect({ done: true });
 	});
 });
