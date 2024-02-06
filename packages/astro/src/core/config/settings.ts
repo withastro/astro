a05857f7a9100eb92b17f19ac50270535e178b14
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-02-06 17:25:33 +0100

    feat: move code to right place

--- packages/astro/src/core/config/settings.ts
+++ packages/astro/src/core/config/settings.ts
@@ -1,5 +1,5 @@
 import yaml from 'js-yaml';
-import path from 'node:path';
+import { dirname, relative } from 'node:path';
 import { fileURLToPath, pathToFileURL } from 'node:url';
 import type { AstroConfig, AstroSettings } from '../../@types/astro.js';
 import { getContentPaths } from '../../content/index.js';
@@ -11,6 +11,9 @@ import { formatYAMLException, isYAMLException } from '../errors/utils.js';
 import { SUPPORTED_MARKDOWN_FILE_EXTENSIONS } from './../constants.js';
 import { AstroTimer } from './timer.js';
 import { loadTSConfig } from './tsconfig.js';
+import type { Logger } from '../logger/core.js';
+import { mkdirSync, writeFileSync } from 'node:fs';
+import type { TSConfckParseResult } from 'tsconfck';
 
 export function createBaseSettings(config: AstroConfig): AstroSettings {
 	const { contentDir } = getContentPaths(config);
@@ -31,10 +34,7 @@ export function createBaseSettings(config: AstroConfig): AstroSettings {
 				getEntryInfo({ contents, fileUrl }) {
 					if (contents === undefined || contents === '') return { data: {} };
 
-					const pathRelToContentDir = path.relative(
-						fileURLToPath(contentDir),
-						fileURLToPath(fileUrl)
-					);
+					const pathRelToContentDir = relative(fileURLToPath(contentDir), fileURLToPath(fileUrl));
 					let data;
 					try {
 						data = JSON.parse(contents);
@@ -73,10 +73,7 @@ export function createBaseSettings(config: AstroConfig): AstroSettings {
 
 						return { data, rawData };
 					} catch (e) {
-						const pathRelToContentDir = path.relative(
-							fileURLToPath(contentDir),
-							fileURLToPath(fileUrl)
-						);
+						const pathRelToContentDir = relative(fileURLToPath(contentDir), fileURLToPath(fileUrl));
 						const formattedError = isYAMLException(e)
 							? formatYAMLException(e)
 							: new Error('contains invalid YAML.');
@@ -107,6 +104,80 @@ export function createBaseSettings(config: AstroConfig): AstroSettings {
 	};
 }
 
+async function handleTypescriptConfig(
+	astroConfig: AstroConfig,
+	tsconfig: TSConfckParseResult,
+	logger: Logger
+) {
+	const invalidFields: Array<string> = [];
+
+	if (tsconfig.tsconfig?.include?.length > 0) {
+		invalidFields.push('include');
+	}
+	if (tsconfig.tsconfig?.exclude?.length > 0) {
+		invalidFields.push('exclude');
+	}
+	if (tsconfig.tsconfig?.files?.length > 0) {
+		invalidFields.push('files');
+	}
+
+	if (invalidFields.length > 0) {
+		logger.warn(
+			null,
+			`The following fields of your tsconfig.json will conflict with Astro: ${invalidFields.join(
+				', '
+			)}`
+		);
+	}
+
+	function getRelativePathToCacheDir(url: URL) {
+		const path = fileURLToPath(url);
+		return relative(fileURLToPath(new URL('.astro', astroConfig.root)), path).replaceAll('\\', '/');
+	}
+
+	function getField(_tsconfig: any, name: 'include' | 'exclude' | 'files') {
+		return [
+			...(astroConfig.typescript?.[name] ?? []),
+			...(invalidFields.includes(name) ? (_tsconfig[name] as Array<string>) : []),
+		];
+	}
+
+	function deduplicate<T extends Array<unknown>>(array: T) {
+		return [...new Set([...array])];
+	}
+
+	const newTsconfig = {
+		include: deduplicate(['astro/client', ...getField(tsconfig.tsconfig, 'include')]),
+		exclude: deduplicate([
+			...getField(tsconfig.tsconfig, 'exclude'),
+			...(astroConfig.typescript?.excludeDefaults
+				? [
+						getRelativePathToCacheDir(astroConfig.outDir),
+						getRelativePathToCacheDir(astroConfig.publicDir),
+					]
+				: []),
+		]),
+		files: deduplicate(getField(tsconfig.tsconfig, 'files')),
+	};
+
+	const rawTsConfigPath = './.astro/tsconfig.json';
+	const tsconfigPath = fileURLToPath(new URL(rawTsConfigPath, astroConfig.root));
+	mkdirSync(dirname(tsconfigPath), { recursive: true });
+	writeFileSync(tsconfigPath, JSON.stringify(newTsconfig, null, 2), 'utf-8');
+
+	const outputTsconfig = tsconfig.tsconfig;
+	if (typeof tsconfig.tsconfig.extends === 'string') {
+		outputTsconfig.extends = [tsconfig.tsconfig.extends, rawTsConfigPath];
+		writeFileSync(tsconfig.tsconfigFile, JSON.stringify(outputTsconfig, null, 2), 'utf-8');
+	} else if (
+		Array.isArray(tsconfig.tsconfig.extends) &&
+		!tsconfig.tsconfig.extends.includes(rawTsConfigPath)
+	) {
+		outputTsconfig.extends = [...tsconfig.tsconfig.extends, rawTsConfigPath];
+		writeFileSync(tsconfig.tsconfigFile, JSON.stringify(outputTsconfig, null, 2), 'utf-8');
+	}
+}
+
 export async function createSettings(config: AstroConfig, cwd?: string): Promise<AstroSettings> {
 	const tsconfig = await loadTSConfig(cwd);
 	const settings = createBaseSettings(config);
@@ -117,6 +188,14 @@ export async function createSettings(config: AstroConfig, cwd?: string): Promise
 	}
 
 	if (typeof tsconfig !== 'string') {
+		await handleTypescriptConfig(
+			config,
+			tsconfig.rawConfig,
+			// TODO: find how to pass a real logger
+			{
+				warn: (_: any, msg: string) => console.log(msg),
+			} as any
+		);
 		watchFiles.push(
 			...[tsconfig.tsconfigFile, ...(tsconfig.extended ?? []).map((e) => e.tsconfigFile)]
 		);
