981e1e92a23a3a224a10465abf343540fa68951d
bholmesdev <hey@bholmes.dev>
2024-02-06 17:27:43 -0500

    feat: add pragma when pushing migrations

--- packages/db/src/core/cli/commands/push/index.ts
+++ packages/db/src/core/cli/commands/push/index.ts
@@ -191,7 +191,7 @@ async function pushData({
 }
 
 async function runMigrateQuery({
-	queries,
+	queries: baseQueries,
 	migrations,
 	snapshot,
 	appToken,
@@ -203,6 +203,8 @@ async function runMigrateQuery({
 	appToken: string;
 	isDryRun?: boolean;
 }) {
+	const queries = ['pragma defer_foreign_keys=true;', ...baseQueries];
+
 	const requestBody = {
 		snapshot,
 		migrations,
