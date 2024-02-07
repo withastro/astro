0e2e51bf036187fb423e95dc073cfdb48570472b
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:14:01 -0800

    Delete examples/with-tailwindcss directory

--- examples/with-tailwindcss/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [tailwind()],
-});
