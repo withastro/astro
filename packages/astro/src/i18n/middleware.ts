0511c93652b425bc0ee3c528c7691bcaf7373843
Emanuele Stoppa <my.burning@gmail.com>
2024-02-07 13:55:12 +0000

    fix(i18n): don't consider URLs that start with the name of the default locale

--- packages/astro/src/i18n/middleware.ts
+++ packages/astro/src/i18n/middleware.ts
@@ -65,7 +65,13 @@ export function createI18nMiddleware(
 	};
 
 	const prefixOtherLocales = (url: URL, response: Response): Response | undefined => {
-		const pathnameContainsDefaultLocale = url.pathname.includes(`/${i18n.defaultLocale}`);
+		let pathnameContainsDefaultLocale = false;
+		for (const segment of url.pathname.split('/')) {
+			if (normalizeTheLocale(segment) === normalizeTheLocale(i18n.defaultLocale)) {
+				pathnameContainsDefaultLocale = true;
+				break;
+			}
+		}
 		if (pathnameContainsDefaultLocale) {
 			const newLocation = url.pathname.replace(`/${i18n.defaultLocale}`, '');
 			response.headers.set('Location', newLocation);
