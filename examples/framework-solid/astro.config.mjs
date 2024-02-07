91d03c326669a5adaa6fe6687e42554c5e23bd43
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:12:25 -0800

    Delete examples/framework-solid directory

--- examples/framework-solid/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-
-// https://astro.build/config
-export default defineConfig({
-	// Enable Solid to support Solid JSX components.
-	integrations: [solid()],
-});
