439e456e4cf16fc346b4c2931b33dda35845b501
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:11:16 -0800

    Delete examples/framework-alpine directory

--- examples/framework-alpine/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import alpine from '@astrojs/alpinejs';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [alpine()],
-});
