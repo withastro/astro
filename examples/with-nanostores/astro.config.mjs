785f4863f99fc861c67a0ba9239832e2cd9235c5
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:14:00 -0800

    Delete examples/with-nanostores directory

--- examples/with-nanostores/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-
-// https://astro.build/config
-export default defineConfig({
-	// Enable many frameworks to support all different kinds of components.
-	integrations: [preact()],
-});
