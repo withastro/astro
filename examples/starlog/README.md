54c7ebc148bbcedcb3649157afb6d9222f894c84
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:13:19 -0800

    Delete examples/starlog directory

--- examples/starlog/README.md
+++
@@ -1,7 +0,0 @@
-# Starlog
-
-## Release notes theme for Astro
-
-![starlog-gh](https://github.com/doodlemarks/starlog/assets/2244813/9c5c2e46-665a-437e-a971-053db4dbff63)
-
-Built with Astro and Sass. Supports both dark and light modes.
--- examples/starlog/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SiteTitle = 'Starlog';
-export const SiteDescription = 'Welcome to my website!';
