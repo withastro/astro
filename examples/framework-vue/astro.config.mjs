e3711e83da1cb94f20e6986b6be87a16615c461e
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:12:20 -0800

    Delete examples/framework-vue directory

--- examples/framework-vue/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-
-// https://astro.build/config
-export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue()],
-});
