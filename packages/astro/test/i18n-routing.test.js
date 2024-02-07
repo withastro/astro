037e4f12dd2f460d66f72c9f2d992b95e74d2da9
Emanuele Stoppa <my.burning@gmail.com>
2024-02-07 14:42:51 +0000

    fix(i18n): don't consider URLs that start with the name of the defaut locale (#10016)

--- packages/astro/test/i18n-routing.test.js
+++ packages/astro/test/i18n-routing.test.js
@@ -59,6 +59,30 @@ describe('astro:i18n virtual module', () => {
 	});
 });
 describe('[DEV] i18n routing', () => {
+	describe('should render a page that stars with a locale but it is a page', () => {
+		/** @type {import('./test-utils').Fixture} */
+		let fixture;
+		/** @type {import('./test-utils').DevServer} */
+		let devServer;
+
+		before(async () => {
+			fixture = await loadFixture({
+				root: './fixtures/i18n-routing/',
+			});
+			devServer = await fixture.startDevServer();
+		});
+
+		after(async () => {
+			await devServer.stop();
+		});
+
+		it('renders the page', async () => {
+			const response = await fixture.fetch('/endurance');
+			expect(response.status).to.equal(200);
+			expect(await response.text()).includes('Endurance');
+		});
+	});
+
 	describe('i18n routing', () => {
 		/** @type {import('./test-utils').Fixture} */
 		let fixture;
@@ -1005,6 +1029,23 @@ describe('[SSG] i18n routing', () => {
 		});
 	});
 
+	describe('should render a page that stars with a locale but it is a page', () => {
+		/** @type {import('./test-utils').Fixture} */
+		let fixture;
+
+		before(async () => {
+			fixture = await loadFixture({
+				root: './fixtures/i18n-routing/',
+			});
+			await fixture.build();
+		});
+
+		it('renders the page', async () => {
+			const html = await fixture.readFile('/endurance/index.html');
+			expect(html).includes('Endurance');
+		});
+	});
+
 	describe('current locale', () => {
 		describe('with [prefix-other-locales]', () => {
 			/** @type {import('./test-utils').Fixture} */
@@ -1068,6 +1109,29 @@ describe('[SSG] i18n routing', () => {
 });
 describe('[SSR] i18n routing', () => {
 	let app;
+
+	describe('should render a page that stars with a locale but it is a page', () => {
+		/** @type {import('./test-utils').Fixture} */
+		let fixture;
+
+		before(async () => {
+			fixture = await loadFixture({
+				root: './fixtures/i18n-routing/',
+				output: 'server',
+				adapter: testAdapter(),
+			});
+			await fixture.build();
+			app = await fixture.loadTestAdapterApp();
+		});
+
+		it('renders the page', async () => {
+			let request = new Request('http://example.com/endurance');
+			let response = await app.render(request);
+			expect(response.status).to.equal(200);
+			expect(await response.text()).includes('Endurance');
+		});
+	});
+
 	describe('default', () => {
 		/** @type {import('./test-utils').Fixture} */
 		let fixture;
