5e6c87caa75f23dc5117f3a42cf030776f274adc
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:11:13 -0800

    Delete examples/blog directory

--- examples/blog/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
