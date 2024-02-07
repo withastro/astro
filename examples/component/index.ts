12e7298663c15a58ca016eb5810fd46d02b835d1
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:11:15 -0800

    Delete examples/component directory

--- examples/component/index.ts
+++
@@ -1,6 +0,0 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
-import MyComponent from './src/MyComponent.astro';
-
-export default MyComponent;
