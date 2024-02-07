20a77c26eac84e55bef27451a32686894e02c345
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:11:18 -0800

    Delete examples/framework-lit directory

--- examples/framework-lit/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import lit from '@astrojs/lit';
-
-// https://astro.build/config
-export default defineConfig({
-	// Enable Lit to support LitHTML components and templates.
-	integrations: [lit()],
-});
