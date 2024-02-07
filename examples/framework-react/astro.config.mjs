492746eee539fc5a6910ca68e96ae57ab6545a23
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-02-07 14:12:28 -0800

    Delete examples/framework-react directory

--- examples/framework-react/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-// https://astro.build/config
-export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
-});
