0687adaab17551907b227bec1280d5485fcb8754
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-02-08 18:12:58 +0100

    feat: update astro-scripts check

--- scripts/smoke/check.js
+++ scripts/smoke/check.js
@@ -4,9 +4,9 @@ import { spawn } from 'node:child_process';
 import { existsSync, readdirSync, readFileSync, writeFileSync } from 'node:fs';
 import * as path from 'node:path';
 import pLimit from 'p-limit';
-import { tsconfigResolverSync } from 'tsconfig-resolver';
+import { parse, TSConfckParseError } from 'tsconfck'
 
-function checkExamples() {
+async function checkExamples() {
 	let examples = readdirSync('./examples', { withFileTypes: true });
 	examples = examples.filter((dirent) => dirent.isDirectory());
 
@@ -17,10 +17,21 @@ function checkExamples() {
 	const limit = pLimit(5);
 
 	for (const example of examples) {
+		// Ensure codegen has run to generate tsconfigs and prevent tsconfck from failing
+		await new Promise((resolve) => {
+			console.log(`Running \`astro sync\` for ${example.name}...`)
+			spawn('node', ['../../packages/astro/astro.js', 'sync'], {
+				cwd: path.join('./examples', example.name),
+				env: { ...process.env, FORCE_COLOR: 'true' },
+			}).on('close', () => resolve(undefined))
+		})
+	}
+
+	for (const example of examples) {
 		checkPromises.push(
 			limit(
 				() =>
-					new Promise((resolve) => {
+					new Promise(async (resolve) => {
 						// Sometimes some examples may get deleted, but after a `git pull` the directory still exists.
 						// This can stall the process time as it'll typecheck the entire monorepo, so do a quick exist
 						// check here before typechecking this directory.
@@ -29,7 +40,7 @@ function checkExamples() {
 							return;
 						}
 
-						const originalConfig = prepareExample(example.name);
+						const originalConfig = await prepareExample(example.name);
 						let data = '';
 						const child = spawn('node', ['../../packages/astro/astro.js', 'check'], {
 							cwd: path.join('./examples', example.name),
@@ -64,29 +75,61 @@ function checkExamples() {
 }
 
 /**
- * @param {string} examplePath
+ * 
+ * @param {string} tsconfigPath 
+ * @param {import('tsconfck').TSConfckParseOptions} options 
+ * @returns {Promise<import('tsconfck').TSConfckParseResult | 'invalid-config' | 'missing-config' | 'unknown-error'>}
  */
-function prepareExample(examplePath) {
-	const tsconfigPath = path.join('./examples/', examplePath, 'tsconfig.json');
-	const tsconfig = tsconfigResolverSync({ filePath: tsconfigPath, cache: false });
-	let originalConfig = undefined;
+async function safeParse(tsconfigPath, options = {}) {
+	try {
+		const parseResult = await parse(tsconfigPath, options);
 
-	if (tsconfig.exists) {
-		tsconfig.config.extends = 'astro/tsconfigs/strictest';
-		originalConfig = readFileSync(tsconfigPath).toString();
-
-		if (!tsconfig.config.compilerOptions) {
-			tsconfig.config.compilerOptions = {};
+		if (parseResult.tsconfig == null) {
+			return 'missing-config';
 		}
 
-		tsconfig.config.compilerOptions = Object.assign(tsconfig.config.compilerOptions, {
-			types: tsconfig.config.compilerOptions.types ?? [], // Speeds up tests
-		});
+		return parseResult;
+	} catch (e) {
+		// TODO: remove
+		console.error(e)
+		if (e instanceof TSConfckParseError) {
+			return 'invalid-config';
+		}
+
+		return 'unknown-error';
+	}
+}
+
+/**
+ * @param {string} examplePath
+ */
+async function prepareExample(examplePath) {
+	const tsconfigPath = path.join('./examples/', examplePath, 'tsconfig.json');
+	const tsconfig = await safeParse(tsconfigPath)
+	let originalConfig = undefined;
+
+	if (tsconfig === 'missing-config') {
+		return originalConfig
 	}
 
-	if (tsconfig.config) {
-		writeFileSync(tsconfigPath, JSON.stringify(tsconfig.config));
+	if (typeof tsconfig === 'string') {
+		throw new Error(`Couldn't load tsconfig at "${tsconfigPath}". Reason: "${tsconfig}"`)
 	}
+
+	const rawTsConfig = (tsconfig.extended ?? [])[0]
+	rawTsConfig.tsconfig.extends
+		.replace('astro/tsconfig/base', 'astro/tsconfigs/strictest')
+		.replace('astro/tsconfigs/strict', 'astro/tsconfigs/strictest')
+
+	originalConfig = readFileSync(tsconfigPath).toString();
+	
+		if (!rawTsConfig.tsconfig.compilerOptions) {
+			rawTsConfig.tsconfig.compilerOptions = {};
+		}
+	rawTsConfig.tsconfig.compilerOptions = Object.assign(rawTsConfig.tsconfig.compilerOptions, {
+			types: rawTsConfig.tsconfig.compilerOptions.types ?? [], // Speeds up tests
+	});
+	writeFileSync(tsconfigPath, JSON.stringify(rawTsConfig.tsconfig));
 
 	return originalConfig;
 }
