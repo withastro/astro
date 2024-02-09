10110ca9c8f9f061df6b1eb33b65518ae4ffd4a6
Emanuele Stoppa <my.burning@gmail.com>
2024-02-09 13:06:24 +0000

    test: move some unit tests to use node:test

--- packages/astro/test/units/config/config-resolve.nodetest.js
+++ packages/astro/test/units/config/config-resolve.nodetest.js
@@ -1,7 +1,8 @@
-import { expect } from 'chai';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { resolveConfig } from '../../../dist/core/config/index.js';
+import { describe, it } from 'node:test';
+import * as assert from 'node:assert/strict';
 
 describe('resolveConfig', () => {
 	it('resolves relative inline root correctly', async () => {
@@ -13,6 +14,6 @@ describe('resolveConfig', () => {
 			'dev'
 		);
 		const expectedRoot = path.join(process.cwd(), 'relative/path/');
-		expect(fileURLToPath(astroConfig.root)).to.equal(expectedRoot);
+		assert.equal(fileURLToPath(astroConfig.root), expectedRoot);
 	});
 });
--- packages/astro/test/units/config/config-validate.nodetest.js
+++ packages/astro/test/units/config/config-validate.nodetest.js
@@ -1,24 +1,26 @@
-import { expect } from 'chai';
 import { z } from 'zod';
 import stripAnsi from 'strip-ansi';
 import { formatConfigErrorMessage } from '../../../dist/core/messages.js';
 import { validateConfig } from '../../../dist/core/config/config.js';
+import { describe, it } from 'node:test';
+import * as assert from 'node:assert/strict';
 
 describe('Config Validation', () => {
 	it('empty user config is valid', async () => {
-		expect(() => validateConfig({}, process.cwd()).catch((err) => err)).not.to.throw();
+		assert.doesNotThrow(() => validateConfig({}, process.cwd()).catch((err) => err));
 	});
 
 	it('Zod errors are returned when invalid config is used', async () => {
 		const configError = await validateConfig({ site: 42 }, process.cwd()).catch((err) => err);
-		expect(configError instanceof z.ZodError).to.equal(true);
+		assert.equal(configError instanceof z.ZodError, true);
 	});
 
 	it('A validation error can be formatted correctly', async () => {
 		const configError = await validateConfig({ site: 42 }, process.cwd()).catch((err) => err);
-		expect(configError instanceof z.ZodError).to.equal(true);
+		assert.equal(configError instanceof z.ZodError, true);
 		const formattedError = stripAnsi(formatConfigErrorMessage(configError));
-		expect(formattedError).to.equal(
+		assert.equal(
+			formattedError,
 			`[config] Astro found issue(s) with your configuration:
   ! site  Expected string, received number.`
 		);
@@ -30,9 +32,10 @@ describe('Config Validation', () => {
 			build: { format: 'invalid' },
 		};
 		const configError = await validateConfig(veryBadConfig, process.cwd()).catch((err) => err);
-		expect(configError instanceof z.ZodError).to.equal(true);
+		assert.equal(configError instanceof z.ZodError, true);
 		const formattedError = stripAnsi(formatConfigErrorMessage(configError));
-		expect(formattedError).to.equal(
+		assert.equal(
+			formattedError,
 			`[config] Astro found issue(s) with your configuration:
   ! integrations.0  Expected object, received number.
   ! build.format  Invalid input.`
@@ -44,18 +47,18 @@ describe('Config Validation', () => {
 			{ integrations: [0, false, null, undefined] },
 			process.cwd()
 		);
-		expect(result.integrations).to.deep.equal([]);
+		assert.deepEqual(result.integrations, []);
 	});
 	it('normalizes "integration" values', async () => {
 		const result = await validateConfig({ integrations: [{ name: '@astrojs/a' }] }, process.cwd());
-		expect(result.integrations).to.deep.equal([{ name: '@astrojs/a', hooks: {} }]);
+		assert.deepEqual(result.integrations, [{ name: '@astrojs/a', hooks: {} }]);
 	});
 	it('flattens array "integration" values', async () => {
 		const result = await validateConfig(
 			{ integrations: [{ name: '@astrojs/a' }, [{ name: '@astrojs/b' }, { name: '@astrojs/c' }]] },
 			process.cwd()
 		);
-		expect(result.integrations).to.deep.equal([
+		assert.deepEqual(result.integrations, [
 			{ name: '@astrojs/a', hooks: {} },
 			{ name: '@astrojs/b', hooks: {} },
 			{ name: '@astrojs/c', hooks: {} },
@@ -66,14 +69,15 @@ describe('Config Validation', () => {
 			{ integrations: [null, undefined, false, '', ``] },
 			process.cwd()
 		).catch((err) => err);
-		expect(configError).to.be.not.instanceOf(Error);
+		assert.equal(configError instanceof Error, false);
 	});
 	it('Error when outDir is placed within publicDir', async () => {
 		const configError = await validateConfig({ outDir: './public/dist' }, process.cwd()).catch(
 			(err) => err
 		);
-		expect(configError instanceof z.ZodError).to.equal(true);
-		expect(configError.errors[0].message).to.equal(
+		assert.equal(configError instanceof z.ZodError, true);
+		assert.equal(
+			configError.errors[0].message,
 			'The value of `outDir` must not point to a path within the folder set as `publicDir`, this will cause an infinite loop'
 		);
 	});
@@ -89,8 +93,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				'The default locale `en` is not present in the `i18n.locales` array.'
 			);
 		});
@@ -111,8 +116,8 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal('Array must contain at least 1 element(s)');
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(configError.errors[0].message, 'Array must contain at least 1 element(s)');
 		});
 
 		it('errors if the default locale is not in path', async () => {
@@ -131,8 +136,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				'The default locale `uk` is not present in the `i18n.locales` array.'
 			);
 		});
@@ -150,8 +156,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The locale `it` value in the `i18n.fallback` record doesn't exist in the `i18n.locales` array."
 			);
 		});
@@ -169,8 +176,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The locale `it` key in the `i18n.fallback` record doesn't exist in the `i18n.locales` array."
 			);
 		});
@@ -188,8 +196,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"You can't use the default locale as a key. The default locale can only be used as value."
 			);
 		});
@@ -208,8 +217,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				'The option `i18n.redirectToDefaultLocale` is only useful when the `i18n.prefixDefaultLocale` is set to `true`. Remove the option `i18n.redirectToDefaultLocale`, or change its value to `true`.'
 			);
 		});
@@ -228,8 +238,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The locale `lorem` key in the `i18n.domains` record doesn't exist in the `i18n.locales` array."
 			);
 		});
@@ -248,8 +259,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The domain value must be a valid URL, and it has to start with 'https' or 'http'."
 			);
 		});
@@ -268,8 +280,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The domain value must be a valid URL, and it has to start with 'https' or 'http'."
 			);
 		});
@@ -288,8 +301,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The URL `https://www.example.com/blog/page/` must contain only the origin. A subsequent pathname isn't allowed here. Remove `/blog/page/`."
 			);
 		});
@@ -311,8 +325,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				"The option `site` isn't set. When using the 'domains' strategy for `i18n`, `site` is required to create absolute URLs for locales that aren't mapped to a domain."
 			);
 		});
@@ -335,8 +350,9 @@ describe('Config Validation', () => {
 				},
 				process.cwd()
 			).catch((err) => err);
-			expect(configError instanceof z.ZodError).to.equal(true);
-			expect(configError.errors[0].message).to.equal(
+			assert.equal(configError instanceof z.ZodError, true);
+			assert.equal(
+				configError.errors[0].message,
 				'Domain support is only available when `output` is `"server"`.'
 			);
 		});
