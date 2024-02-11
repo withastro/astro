f34627145e8aa109cb397ef6bb5ac481c0cb40b1
Mohamed <10786768+xMohamd@users.noreply.github.com>
2024-02-11 14:10:22 +0000

    revert static-build-frameworks tests to original version

--- packages/astro/test/static-build-frameworks.test.js
+++ packages/astro/test/static-build-frameworks.test.js
@@ -1,6 +1,5 @@
+import { expect } from 'chai';
 import * as cheerio from 'cheerio';
-import assert from 'node:assert/strict';
-import { before, describe, it } from 'node:test';
 import { loadFixture, isWindows } from './test-utils.js';
 
 describe('Static build - frameworks', () => {
@@ -19,25 +18,25 @@ describe('Static build - frameworks', () => {
 
 	it('can build preact', async () => {
 		const html = await fixture.readFile('/preact/index.html');
-		assert.equal(typeof html, 'string');
+		expect(html).to.be.a('string');
 	});
 
 	it('can build react', async () => {
 		const html = await fixture.readFile('/react/index.html');
-		assert.equal(typeof html, 'string');
+		expect(html).to.be.a('string');
 	});
 
 	// SKIP: Lit polyfills the server in a way that breaks `sass` require/import
 	// Leads to CI bugs like: "Cannot read properties of undefined (reading 'length')"
 	it.skip('can build lit', async () => {
 		const html = await fixture.readFile('/lit/index.html');
-		assert.equal(typeof html, 'string');
+		expect(html).to.be.a('string');
 	});
 
 	it('can build nested framework usage', async () => {
 		const html = await fixture.readFile('/nested/index.html');
 		const $ = cheerio.load(html);
 		const counter = $('.nested-counter .counter');
-		assert.equal(counter.length, 1, 'Found the counter');
+		expect(counter.length).to.equal(1, 'Found the counter');
 	});
 });
