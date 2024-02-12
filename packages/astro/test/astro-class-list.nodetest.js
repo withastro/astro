613a6c526cd45c80ce77577f4825bdc9e616a621
Mohamed <10786768+xMohamd@users.noreply.github.com>
2024-02-12 17:15:45 +0000

    replace strictEqual with equal in astro-class-list

--- packages/astro/test/astro-class-list.nodetest.js
+++ packages/astro/test/astro-class-list.nodetest.js
@@ -15,49 +15,49 @@ describe('Class List', async () => {
 		const html = await fixture.readFile('/index.html');
 		const $ = cheerio.load(html);
 
-		assert.strictEqual($('[class="test control"]').length, 1, '[class="test control"]');
-		assert.strictEqual($('[class="test expression"]').length, 1, '[class="test expression"]');
-		assert.strictEqual($('[class="test true"]').length, 1, '[class="test true"]');
-		assert.strictEqual($('[class="test truthy"]').length, 1, '[class="test truthy"]');
-		assert.strictEqual($('[class="test set"]').length, 1, '[class="test set"]');
-		assert.strictEqual(
+		assert.equal($('[class="test control"]').length, 1, '[class="test control"]');
+		assert.equal($('[class="test expression"]').length, 1, '[class="test expression"]');
+		assert.equal($('[class="test true"]').length, 1, '[class="test true"]');
+		assert.equal($('[class="test truthy"]').length, 1, '[class="test truthy"]');
+		assert.equal($('[class="test set"]').length, 1, '[class="test set"]');
+		assert.equal(
 			$('[class="hello goodbye hello world hello friend"]').length,
 			1,
 			'[class="hello goodbye hello world hello friend"]'
 		);
-		assert.strictEqual($('[class="foo baz"]').length, 1, '[class="foo baz"]');
-		assert.strictEqual($('span:not([class])').length, 1, 'span:not([class])');
+		assert.equal($('[class="foo baz"]').length, 1, '[class="foo baz"]');
+		assert.equal($('span:not([class])').length, 1, 'span:not([class])');
 
-		assert.strictEqual($('.false, .noshow1, .noshow2, .noshow3, .noshow4').length, 0);
+		assert.equal($('.false, .noshow1, .noshow2, .noshow3, .noshow4').length, 0);
 	});
 
 	it('Passes class:list attributes as expected to components', async () => {
 		const html = await fixture.readFile('/component/index.html');
 		const $ = cheerio.load(html);
 
-		assert.strictEqual($('[class="test control"]').length, 1, '[class="test control"]');
-		assert.strictEqual($('[class="test expression"]').length, 1, '[class="test expression"]');
-		assert.strictEqual($('[class="test true"]').length, 1, '[class="test true"]');
-		assert.strictEqual($('[class="test truthy"]').length, 1, '[class="test truthy"]');
-		assert.strictEqual($('[class="test set"]').length, 1, '[class="test set"]');
-		assert.strictEqual(
+		assert.equal($('[class="test control"]').length, 1, '[class="test control"]');
+		assert.equal($('[class="test expression"]').length, 1, '[class="test expression"]');
+		assert.equal($('[class="test true"]').length, 1, '[class="test true"]');
+		assert.equal($('[class="test truthy"]').length, 1, '[class="test truthy"]');
+		assert.equal($('[class="test set"]').length, 1, '[class="test set"]');
+		assert.equal(
 			$('[class="hello goodbye hello world hello friend"]').length,
 			1,
 			'[class="hello goodbye hello world hello friend"]'
 		);
-		assert.strictEqual($('[class="foo baz"]').length, 1, '[class="foo baz"]');
-		assert.strictEqual($('span:not([class])').length, 1, 'span:not([class])');
+		assert.equal($('[class="foo baz"]').length, 1, '[class="foo baz"]');
+		assert.equal($('span:not([class])').length, 1, 'span:not([class])');
 
-		assert.strictEqual($('[class="test control"]').text(), 'test control');
-		assert.strictEqual($('[class="test expression"]').text(), 'test expression');
-		assert.strictEqual($('[class="test true"]').text(), 'test true');
-		assert.strictEqual($('[class="test truthy"]').text(), 'test truthy');
-		assert.strictEqual($('[class="test set"]').text(), 'test set');
-		assert.strictEqual(
+		assert.equal($('[class="test control"]').text(), 'test control');
+		assert.equal($('[class="test expression"]').text(), 'test expression');
+		assert.equal($('[class="test true"]').text(), 'test true');
+		assert.equal($('[class="test truthy"]').text(), 'test truthy');
+		assert.equal($('[class="test set"]').text(), 'test set');
+		assert.equal(
 			$('[class="hello goodbye hello world hello friend"]').text(),
 			'hello goodbye hello world hello friend'
 		);
-		assert.strictEqual($('[class="foo baz"]').text(), 'foo baz');
-		assert.strictEqual($('span:not([class])').text(), '');
+		assert.equal($('[class="foo baz"]').text(), 'foo baz');
+		assert.equal($('span:not([class])').text(), '');
 	});
 });
