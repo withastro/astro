3888b172308de1644551e58773e0d0a0a86c8f6e
Mohamed <mohamedbenhamzaa@gmail.com>
2024-02-14 00:42:57 +0100

    Update the 'crypto' import to 'node:crypto'

--- packages/integrations/vercel/src/serverless/adapter.ts
+++ packages/integrations/vercel/src/serverless/adapter.ts
@@ -5,7 +5,7 @@ import type {
 	AstroIntegrationLogger,
 	RouteData,
 } from 'astro';
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 import { AstroError } from 'astro/errors';
 import glob from 'fast-glob';
 import { basename } from 'node:path';
