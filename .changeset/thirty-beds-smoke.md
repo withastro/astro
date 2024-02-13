e6846b625ea7475fc7da4ec7f4e4961a46263b08
Remco Haszing <remcohaszing@gmail.com>
2024-02-13 17:52:05 +0100

    fix(markdown): don’t generate mdast html nodes
    `html` nodes from mdast are converted to `raw` hast nodes. These nodes
    are then not processed by proper rehype plugins. Typically if a remark
    plugin generates `html` nodes, this indicates it should have actually
    been a rehype plugin.
    
    This changes the remark plugins that generate `html` nodes into rehype
    nodes. These were `remarkPrism` and `remarkShiki`.
    
    Closes #9909

---
+++ .changeset/thirty-beds-smoke.md
@@ -0,0 +1,6 @@
+---
+"@astrojs/mdx": patch
+"@astrojs/markdown-remark": patch
+---
+
+Handle syntax highlighting using rehype plugins instead of remark plugins.
---
+++ packages/markdown/remark/src/highlight.ts
@@ -0,0 +1,65 @@
+import type { Element, Root } from 'hast';
+import { fromHtml } from 'hast-util-from-html';
+import { toText } from 'hast-util-to-text';
+import { removePosition } from 'unist-util-remove-position';
+import { visitParents } from 'unist-util-visit-parents';
+
+type Highlighter = (code: string, language: string) => string;
+
+const languagePattern = /\blanguage-(\S+)\b/;
+
+/**
+ * A hast utility to syntax highlight code blocks with a given syntax highlighter.
+ *
+ * @param tree
+ *   The hast tree in which to syntax highlight code blocks.
+ * @param highlighter
+ *   A fnction which receives the code and language, and returns the HTML of a syntax
+ *   highlighted `<pre>` element.
+ */
+export function highlightCodeBlocks(tree: Root, highlighter: Highlighter) {
+	// We’re looking for `<code>` elements
+	visitParents(tree, { type: 'element', tagName: 'code' }, (node, ancestors) => {
+		const parent = ancestors.at(-1);
+
+		// Whose parent is a `<pre>`.
+		if (parent?.type !== 'element' || parent.tagName !== 'pre') {
+			return;
+		}
+
+		// Where the `<code>` is the only child.
+		if (parent.children.length !== 1) {
+			return;
+		}
+
+		// And the `<code>` has a class name that starts with `language-`.
+		let languageMatch: RegExpMatchArray | null | undefined;
+		let { className } = node.properties;
+		if (typeof className === 'string') {
+			languageMatch = className.match(languagePattern);
+		} else if (Array.isArray(className)) {
+			for (const cls of className) {
+				if (typeof cls !== 'string') {
+					continue;
+				}
+
+				languageMatch = cls.match(languagePattern);
+				if (languageMatch) {
+					break;
+				}
+			}
+		}
+
+		const code = toText(node, { whitespace: 'pre' });
+		const html = highlighter(code, languageMatch?.[1] || 'plaintext');
+		// The replacement returns a root node with 1 child, the `<pr>` element replacement.
+		const replacement = fromHtml(html, { fragment: true }).children[0] as Element;
+		// We just generated this node, so any positional information is invalid.
+		removePosition(replacement);
+
+		// We replace the parent in its parent with the new `<pre>` element.
+		const grandParent = ancestors.at(-2)!;
+		const index = grandParent.children.indexOf(parent);
+		grandParent.children[index] = replacement;
+	});
+}
