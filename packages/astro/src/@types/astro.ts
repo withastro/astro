e6f7026d1884d15d3617a0c13fc2c8ce5295c164
bluwy <bjornlu.dev@gmail.com>
2024-02-15 17:52:57 +0800

    Add enable by default note

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1608,9 +1608,11 @@ export interface AstroUserConfig {
 		 * @description
 		 * Directly render scripts as declared in Astro files (with features like TypeScript and importing `node_modules` still working).
 		 * This should result in scripts running in the correct pages compared to the previous static analysis approach.
-		 * 
+		 *
 		 * However, as scripts are now directly rendered, they are no longer hoisted to the `<head>` and multiple scripts on a page are no
 		 * longer bundled together. If you enable this option, you should check if it affects your site's behaviour.
+		 *
+		 * This option will be enabled by default in Astro 5.0.
 		 *
 		 * ```js
 		 * {
