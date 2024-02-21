f87b0cbc6b94c1477a4b27338ccc5b53e33e6edc
Emanuele Stoppa <my.burning@gmail.com>
2024-02-21 14:08:19 +0000

    chore: use biome to sort imports - only test files (#10180)
    * chore: use biome to sort imports
    
    * do the sorting
    
    * Update package.json
    
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    
    ---------
    
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/integrations/vercel/test/isr.test.js
+++ packages/integrations/vercel/test/isr.test.js
@@ -1,6 +1,6 @@
-import { loadFixture } from './test-utils.js';
 import assert from 'node:assert/strict';
 import { before, describe, it } from 'node:test';
+import { loadFixture } from './test-utils.js';
 
 describe('ISR', () => {
 	/** @type {import('./test-utils.js').Fixture} */
