d8feaa03dd68efede254bd2c95093e238dc4ca6c
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-02-22 16:50:44 +0000

    add test

--- packages/astro/test/units/vite-plugin-astro-server/request.test.js
+++ packages/astro/test/units/vite-plugin-astro-server/request.test.js
@@ -1,5 +1,8 @@
 import * as assert from 'node:assert/strict';
-import { describe, it } from 'node:test';
+import { describe, it, before, after } from 'node:test';
+import { fileURLToPath } from 'node:url';
+import { createContainer } from '../../../dist/core/dev/container.js';
+import testAdapter from '../../test-adapter.js';
 import { createLoader } from '../../../dist/core/module-loader/index.js';
 import { createRouteManifest } from '../../../dist/core/routing/index.js';
 import { createComponent, render } from '../../../dist/runtime/server/index.js';
@@ -71,6 +74,57 @@ describe('vite-plugin-astro-server', () => {
 			const html = await text();
 			assert.equal(res.statusCode, 200);
 			assert.equal(html.includes('<div id="test">'), true);
+		});
+	});
+
+	describe('url', () => {
+		let container;
+		let settings;
+		
+		before(async () => {
+			const root = new URL('../../fixtures/api-routes/', import.meta.url);
+			const fileSystem = {
+				'/src/pages/url.astro': `{Astro.request.url}`,
+				'/src/pages/prerendered.astro': `---
+			export const prerender = true;
+			---
+			{Astro.request.url}`,
+			};
+			const fs = createFs(fileSystem, root);
+			settings = await createBasicSettings({
+				root: fileURLToPath(root),
+				output: 'server',
+				adapter: testAdapter(),
+			});
+			container = await createContainer({
+				fs,
+				settings,
+				logger: defaultLogger,
+			});
+		});
+
+		after(async () => {
+			await container.close();
+		});
+
+		it('params are included', async () => {
+			const { req, res, text } = createRequestAndResponse({
+				method: 'GET',
+				url: '/url?xyz=123',
+			});
+			container.handle(req, res);
+			const html = await text();
+			assert.deepEqual(html, '<!DOCTYPE html>http://undefined/url?xyz=123');
+		});
+
+		it('params are excluded on prerendered routes', async () => {
+			const { req, res, text } = createRequestAndResponse({
+				method: 'GET',
+				url: '/prerendered?xyz=123',
+			});
+			container.handle(req, res);
+			const html = await text();
+			assert.deepEqual(html, '<!DOCTYPE html>http://undefined/prerendered');
 		});
 	});
 });
