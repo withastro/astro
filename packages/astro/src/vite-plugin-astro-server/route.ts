4a9ba42de9b53e3d0765b1a30617c555ea1a320a
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-02-29 14:45:41 +0000

    fix(dev): cosider `base` when special-casing `/_image`

--- packages/astro/src/vite-plugin-astro-server/route.ts
+++ packages/astro/src/vite-plugin-astro-server/route.ts
@@ -10,7 +10,6 @@ import { createRequest } from '../core/request.js';
 import { matchAllRoutes } from '../core/routing/index.js';
 import { normalizeTheLocale } from '../i18n/index.js';
 import { getSortedPreloadedMatches } from '../prerender/routing.js';
-import { isServerLikeOutput } from '../prerender/utils.js';
 import type { DevPipeline } from './pipeline.js';
 import { handle404Response, writeSSRResult, writeWebResponse } from './response.js';
 
@@ -146,8 +145,6 @@ export async function handleRoute({
 		return handle404Response(origin, incomingRequest, incomingResponse);
 	}
 
-	const buildingToSSR = isServerLikeOutput(config);
-
 	let request: Request;
 	let renderContext: RenderContext;
 	let mod: ComponentInstance | undefined = undefined;
@@ -183,10 +180,12 @@ export async function handleRoute({
 				return handle404Response(origin, incomingRequest, incomingResponse);
 			}
 			request = createRequest({
+				base: config.base,
 				url,
-				headers: buildingToSSR ? incomingRequest.headers : new Headers(),
+				headers: incomingRequest.headers,
 				logger,
-				ssr: buildingToSSR,
+				// no route found, so we assume the default for rendering the 404 page
+				staticLike: config.output === "static" || config.output === "hybrid",
 			});
 			route = {
 				component: '',
@@ -221,15 +220,14 @@ export async function handleRoute({
 		// Allows adapters to pass in locals in dev mode.
 		const locals = Reflect.get(incomingRequest, clientLocalsSymbol);
 		request = createRequest({
+			base: config.base,
 			url,
-			// Headers are only available when using SSR.
-			headers: buildingToSSR ? incomingRequest.headers : new Headers(),
+			headers: incomingRequest.headers,
 			method: incomingRequest.method,
 			body,
 			logger,
-			ssr: buildingToSSR,
-			clientAddress: buildingToSSR ? incomingRequest.socket.remoteAddress : undefined,
-			removeParams: buildingToSSR === false || route.prerender,
+			clientAddress: incomingRequest.socket.remoteAddress,
+			staticLike: config.output === "static" ? true : route.prerender,
 		});
 
 		// Set user specified headers to response object.
