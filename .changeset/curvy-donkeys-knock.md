e556151603a2f0173059d0f98fdcbec0610b48ff
Arsh <69170106+lilnasy@users.noreply.github.com>
2024-03-01 00:08:01 +0530

    fix(dev): cosider `base` when special-casing `/_image` (#10274)
    * fix(dev): cosider `base` when special-casing `/_image`
    
    * add changeset
    
    * adjust tests
    
    * Apply suggestions from code review
    
    * add test

---
+++ .changeset/curvy-donkeys-knock.md
@@ -0,0 +1,5 @@
+---
+"astro": patch
+---
+
+Fixes a regression introduced in v4.4.5 where image optimization did not work in dev mode when a base was configured.
--- packages/astro/src/core/request.ts
+++ packages/astro/src/core/request.ts
@@ -1,54 +1,67 @@
 import type { IncomingHttpHeaders } from 'node:http';
 import type { Logger } from './logger/core.js';
+import { appendForwardSlash, prependForwardSlash } from './path.js';
 
 type HeaderType = Headers | Record<string, any> | IncomingHttpHeaders;
 type RequestBody = ArrayBuffer | Blob | ReadableStream | URLSearchParams | FormData;
 
 export interface CreateRequestOptions {
+	base: string;
 	url: URL | string;
 	clientAddress?: string | undefined;
 	headers: HeaderType;
 	method?: string;
 	body?: RequestBody | undefined;
 	logger: Logger;
-	ssr: boolean;
 	locals?: object | undefined;
-	removeParams?: boolean;
+	/**
+	 * Whether the request is being created for a static build or for a prerendered page within a hybrid/SSR build, or for emulating one of those in dev mode.
+	 * 
+	 * When `true`, the request will not include search parameters or body, and warn when headers are accessed.
+	 * 
+	 * @default false
+	 */
+	staticLike?: boolean;
 }
 
 const clientAddressSymbol = Symbol.for('astro.clientAddress');
 const clientLocalsSymbol = Symbol.for('astro.locals');
 
 export function createRequest({
+	base,
 	url,
 	headers,
 	clientAddress,
 	method = 'GET',
 	body = undefined,
 	logger,
-	ssr,
 	locals,
-	removeParams = false,
+	staticLike = false
 }: CreateRequestOptions): Request {
-	let headersObj =
+	// headers are made available on the created request only if the request is for a page that will be on-demand rendered
+	const headersObj =
+		staticLike ? undefined :
 		headers instanceof Headers
 			? headers
 			: new Headers(Object.entries(headers as Record<string, any>));
 
 	if (typeof url === 'string') url = new URL(url);
 
+	const imageEndpoint = prependForwardSlash(appendForwardSlash(base)) + '_image';
+	
 	// HACK! astro:assets uses query params for the injected route in `dev`
-	if (removeParams && url.pathname !== '/_image') {
+	if (staticLike && url.pathname !== imageEndpoint) {
 		url.search = '';
 	}
 
 	const request = new Request(url, {
 		method: method,
 		headers: headersObj,
-		body,
+		// body is made available only if the request is for a page that will be on-demand rendered
+		body: staticLike ? null : body,
 	});
 
-	if (!ssr) {
+	if (staticLike) {
 		// Warn when accessing headers in SSG mode
 		const _headers = request.headers;
 		const headersDesc = Object.getOwnPropertyDescriptor(request, 'headers') || {};
@@ -63,6 +76,7 @@ export function createRequest({
 			},
 		});
 	} else if (clientAddress) {
+		// clientAddress is stored to be read by RenderContext, only if the request is for a page that will be on-demand rendered
 		Reflect.set(request, clientAddressSymbol, clientAddress);
 	}
 
