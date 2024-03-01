b51765da628e338a4e14af5929b6071e685e2338
Luiz Ferraz <luiz@lferraz.com>
2024-03-01 02:10:27 -0300

    Reproduce issues

---
+++ packages/astro/test/dynamic-route-collision.test.js
@@ -0,0 +1,53 @@
+import assert from 'node:assert/strict';
+import { before, describe, it } from 'node:test';
+import * as cheerio from 'cheerio';
+import { loadFixture } from './test-utils.js';
+
+describe('Dynamic route collision', () => {
+	/** @type {import('./test-utils').Fixture} */
+	let fixture;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/dynamic-route-collision',
+		});
+
+		await fixture.build();
+	});
+
+	it('Builds a static route when in conflict with a dynamic route', async () => {
+		const html = await fixture.readFile('/about/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Static About');
+	});
+
+	it('Builds a static route when in conflict with a rest route', async () => {
+		const html = await fixture.readFile('/who/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Static Who We Are');
+	});
+
+	it('Builds a static nested index when in conflict with a rest route', async () => {
+		const html = await fixture.readFile('/tags/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Static Tags Index');
+	});
+
+	it('Builds a static root index when in conflict with a rest route', async () => {
+		const html = await fixture.readFile('/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Static Index');
+	});
+
+	it('Builds a dynamic route when in conflict with a rest route', async () => {
+		const html = await fixture.readFile('/blog/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Dynamic Blog');
+	});
+
+	it('Builds the highest priority route out of two conflicting dynamic routes', async () => {
+		const html = await fixture.readFile('/order/index.html');
+		const $ = cheerio.load(html);
+		assert.equal($('h1').text(), 'Order from A');
+	});
+});
