366acb16d1d9c404b006dbb44e165b60035f64cc
bluwy <bjornlu.dev@gmail.com>
2024-03-04 16:46:33 +0800

    Typo

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1612,7 +1612,7 @@ export interface AstroUserConfig {
 		 * and deduplicating scripts). You can also now conditionally render scripts in your Astro file.
 
 		 * However, this means scripts are no longer hoisted to the `<head>` and multiple scripts on a page are no longer bundled together.
-		If you enable this option, you should check that all your `<script>` tags behave as expected.
+		 * If you enable this option, you should check that all your `<script>` tags behave as expected.
 		 *
 		 * This option will be enabled by default in Astro 5.0.
 		 *
