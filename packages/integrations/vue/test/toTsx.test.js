439155c36e4484e9eced1e51b7b376d2d714939d
Phk <ematipico@users.noreply.github.com>
2024-03-05 15:22:07 +0000

    [ci] format

--- packages/integrations/vue/test/toTsx.test.js
+++ packages/integrations/vue/test/toTsx.test.js
@@ -1,10 +1,10 @@
-import { toTSX } from '../dist/editor.cjs';
-import {  describe, it } from 'node:test';
 import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+import { toTSX } from '../dist/editor.cjs';
 
 describe('toTSX function', () => {
 	it('should correctly transform Vue code to TSX with comments', () => {
-    const vueCode = `
+		const vueCode = `
       <template>
         <div>{{ msg }}</div>
       </template>
@@ -16,14 +16,17 @@ describe('toTSX function', () => {
       </script>
     `;
 
-    const className = 'MyComponent';
-    const result = toTSX(vueCode, className);
+		const className = 'MyComponent';
+		const result = toTSX(vueCode, className);
 
-    // Replace the expectations below with the expected result based on your logic
-		assert.strictEqual(result, `export default function ${className}__AstroComponent_(_props: Record<string, any>): any {}`)
-  });
+		// Replace the expectations below with the expected result based on your logic
+		assert.strictEqual(
+			result,
+			`export default function ${className}__AstroComponent_(_props: Record<string, any>): any {}`
+		);
+	});
 	it('should correctly transform Vue code to TSX', () => {
-    const vueCode = `
+		const vueCode = `
       <template>
         <div @click="handleClick">{{ msg }}</div>
       </template>
@@ -38,8 +41,11 @@ describe('toTSX function', () => {
       </script>
     `;
 
-    const className = 'MyComponent';
-    const result = toTSX(vueCode, className);
-		assert.strictEqual(result.replace(/\s/g, ''), `import{defineProps}from'vue';constProps=defineProps({msg:String})exportdefaultfunction${className}__AstroComponent_(_props:typeofProps):any{<div></div>}`)
-  });
+		const className = 'MyComponent';
+		const result = toTSX(vueCode, className);
+		assert.strictEqual(
+			result.replace(/\s/g, ''),
+			`import{defineProps}from'vue';constProps=defineProps({msg:String})exportdefaultfunction${className}__AstroComponent_(_props:typeofProps):any{<div></div>}`
+		);
+	});
 });
