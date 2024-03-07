7e773a908ba1053ee2749b4e1776441af5925f11
Emanuele Stoppa <my.burning@gmail.com>
2024-03-07 13:58:02 +0000

    chore: address Bjorn's comments

--- packages/astro/src/assets/utils/getAssetsPrefix.ts
+++ packages/astro/src/assets/utils/getAssetsPrefix.ts
@@ -1,13 +1,12 @@
 import type { AssetsPrefix } from '../../core/app/types.js';
 
-export function getAssetsPrefix(fileType: string, assetsPrefix?: AssetsPrefix): string {
+export function getAssetsPrefix(fileExtension: string, assetsPrefix?: AssetsPrefix): string {
 	if (!assetsPrefix) return '';
 	if (typeof assetsPrefix === 'string') return assetsPrefix;
-	fileType = fileType[0] === '.' ? fileType.slice(1) : fileType;
-	if (assetsPrefix[fileType]) {
-		return assetsPrefix[fileType];
-	} else if (assetsPrefix.fallback) {
-		return assetsPrefix.fallback;
+	// we assume the file extension has a leading '.' and we remove it
+	const dotLessFileExtension = fileExtension.slice(1);
+	if (assetsPrefix[dotLessFileExtension]) {
+		return assetsPrefix[dotLessFileExtension];
 	}
-	return '';
+	return assetsPrefix.fallback;
 }
