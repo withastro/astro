e584c0674049f600c37f97f5b30fccac16f5b9b1
bholmesdev <hey@bholmes.dev>
2024-03-07 13:25:29 -0500

    refactor: use db.batch in test fixture

--- packages/db/test/fixtures/basics/db/seed.ts
+++ packages/db/test/fixtures/basics/db/seed.ts
@@ -2,20 +2,21 @@ import { asDrizzleTable } from '@astrojs/db/utils';
 import { Themes as ThemesConfig } from './theme';
 import { Author, db } from 'astro:db';
 
+const Themes = asDrizzleTable('Themes', ThemesConfig);
 export default async function () {
-	const Themes = asDrizzleTable('Themes', ThemesConfig);
-
-	await db
-		.insert(Themes)
-		.values([{ name: 'dracula' }, { name: 'monokai', added: new Date() }])
-		.returning({ name: Themes.name });
-	await db
-		.insert(Author)
-		.values([
-			{ name: 'Ben' },
-			{ name: 'Nate' },
-			{ name: 'Erika' },
-			{ name: 'Bjorn' },
-			{ name: 'Sarah' },
-		]);
+	await db.batch([
+		db
+			.insert(Themes)
+			.values([{ name: 'dracula' }, { name: 'monokai', added: new Date() }])
+			.returning({ name: Themes.name }),
+		db
+			.insert(Author)
+			.values([
+				{ name: 'Ben' },
+				{ name: 'Nate' },
+				{ name: 'Erika' },
+				{ name: 'Bjorn' },
+				{ name: 'Sarah' },
+			]),
+	]);
 }
