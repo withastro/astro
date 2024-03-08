ac766647b0e6156b7c4a0bb9a11981fe168852d7
Nicolas Mattia <nicolas@nmattia.com>
2024-03-08 05:22:58 +0100

    fix(config): prevent vite from loading config (#10360)

--- packages/astro/src/core/config/vite-load.ts
+++ packages/astro/src/core/config/vite-load.ts
@@ -6,6 +6,7 @@ import { debug } from '../logger/core.js';
 
 async function createViteServer(root: string, fs: typeof fsType): Promise<ViteDevServer> {
 	const viteServer = await createServer({
+		configFile: false,
 		server: { middlewareMode: true, hmr: false, watch: null },
 		optimizeDeps: { noDiscovery: true },
 		clearScreen: false,
