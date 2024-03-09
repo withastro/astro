18cb0dcd78a50f004d5bea294fad09411947cf8e
eveeify <eveeg1971@gmail.com>
2024-03-09 19:51:01 +1100

    feat: changeset, formating and linting

---
+++ .changeset/eight-mirrors-laugh.md
@@ -0,0 +1,5 @@
+---
+"@astrojs/tailwind": minor
+---
+
+Adds support for tailwind v4.
--- packages/integrations/tailwind/src/v4.ts
+++ packages/integrations/tailwind/src/v4.ts
@@ -1,37 +1,37 @@
+import tailwindcss from '@tailwindcss/vite';
 import type { AstroIntegration } from 'astro';
-import tailwindcss from '@tailwindcss/vite'
 
 type TailwindOptions = {
-    /**
-     * Apply Tailwind's base styles
-     * Disabling this is useful when further customization of Tailwind styles
-     * and directives is required. See {@link https://tailwindcss.com/docs/functions-and-directives#tailwind Tailwind's docs}
-     * for more details on directives and customization.
-     * @default true
-     */
-    applyBaseStyles?: boolean;
+	/**
+	 * Apply Tailwind's base styles
+	 * Disabling this is useful when further customization of Tailwind styles
+	 * and directives is required. See {@link https://tailwindcss.com/docs/functions-and-directives#tailwind Tailwind's docs}
+	 * for more details on directives and customization.
+	 * @default true
+	 */
+	applyBaseStyles?: boolean;
 };
 
 export default function tailwindIntegration(options?: TailwindOptions): AstroIntegration {
-    const applyBaseStyles = options?.applyBaseStyles ?? true;
+	const applyBaseStyles = options?.applyBaseStyles ?? true;
 
-    return {
-        name: '@astrojs/tailwind',
-        hooks: {
-            'astro:config:setup': async ({ updateConfig, injectScript }) => {
-                // Inject the Tailwind vite plugin
-                updateConfig({
-                    vite: {
-                      plugins: [tailwindcss()],
-                      css: { transformer: 'lightningcss' },
-                    }
-                });
+	return {
+		name: '@astrojs/tailwind',
+		hooks: {
+			'astro:config:setup': async ({ updateConfig, injectScript }) => {
+				// Inject the Tailwind vite plugin
+				updateConfig({
+					vite: {
+						plugins: [tailwindcss()],
+						css: { transformer: 'lightningcss' },
+					},
+				});
 
-                if (applyBaseStyles) {
-                    // Inject the Tailwind base import
-                    injectScript('page-ssr', `import '@astrojs/tailwind/base-v4.css';`);
-                }
-            },
-        },
-    };
+				if (applyBaseStyles) {
+					// Inject the Tailwind base import
+					injectScript('page-ssr', `import '@astrojs/tailwind/base-v4.css';`);
+				}
+			},
+		},
+	};
 }
