83e80fbb9051fc694441c5a4f7f65311549724c3
eveeify <eveeg1971@gmail.com>
2024-03-09 19:01:05 +1100

    chore: removed other package foud better way

--- packages/integrations/tailwind-v4/test/fixtures/basic/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-import path from 'node:path';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: [path.join(__dirname, 'src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}')],
-};
