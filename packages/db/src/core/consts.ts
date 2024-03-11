401e9107ddffc3cf48e072cbbd7fde1152c93f75
bholmesdev <hey@bholmes.dev>
2024-03-11 14:38:01 -0400

    Revert "chore: remove runtime/config from package json"
    This reverts commit c1b46789983384ca66172b404a74440784888514.

--- packages/db/src/core/consts.ts
+++ packages/db/src/core/consts.ts
@@ -5,8 +5,7 @@ export const PACKAGE_NAME = JSON.parse(
 ).name;
 
 export const RUNTIME_IMPORT = JSON.stringify(`${PACKAGE_NAME}/runtime`);
-// Use `dist` path. Not exposed as a public API.
-export const RUNTIME_CONFIG_IMPORT = JSON.stringify(`${PACKAGE_NAME}/dist/runtime/config.js`);
+export const RUNTIME_CONFIG_IMPORT = JSON.stringify(`${PACKAGE_NAME}/runtime/config`);
 
 export const DB_TYPES_FILE = 'db-types.d.ts';
 
