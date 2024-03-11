c9a11112757f8184781639ac0db129a42fa22859
Fred K. Schott <FredKSchott@users.noreply.github.com>
2024-03-11 19:33:26 +0000

    [ci] format

--- packages/db/src/core/cli/commands/push/index.ts
+++ packages/db/src/core/cli/commands/push/index.ts
@@ -94,9 +94,7 @@ async function pushSchema({
 		console.error(await response.text());
 		throw new Error(`/db/push fetch failed: ${response.status} ${response.statusText}`);
 	}
-	const result = (await response.json()) as
-		| { success: false; }
-		| { success: true; };
+	const result = (await response.json()) as { success: false } | { success: true };
 	if (!result.success) {
 		console.error(`${url.toString()} unsuccessful`);
 		console.error(await response.text());
