3d078b262edabbd1b5fd3ed68cf3ec4f12c49e4e
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-03-28 14:01:39 +0100

    fix(types): Omit data types from polymorphic attributes

--- packages/astro/types.d.ts
+++ packages/astro/types.d.ts
@@ -1,13 +1,13 @@
 import './astro-jsx';
 import type { AstroBuiltinAttributes } from './dist/@types/astro.js';
-import type { Simplify } from './dist/type-utils.js';
+import type { OmitIndexSignature, Simplify } from './dist/type-utils.js';
 
 /** Any supported HTML or SVG element name, as defined by the HTML specification */
 export type HTMLTag = keyof astroHTML.JSX.DefinedIntrinsicElements;
 
 /** The built-in attributes for any known HTML or SVG element name */
 export type HTMLAttributes<Tag extends HTMLTag> = Omit<
-	astroHTML.JSX.IntrinsicElements[Tag],
+	astroHTML.JSX.DefinedIntrinsicElements[Tag],
 	keyof Omit<AstroBuiltinAttributes, 'class:list'>
 >;
 
@@ -18,7 +18,12 @@ export type CSSProperty = keyof astroHTML.JSX.KebabCSSDOMProperties;
 
 type PolymorphicAttributes<P extends { as: HTMLTag }> = Omit<P, 'as'> & {
 	as?: P['as'];
-} & HTMLAttributes<P['as']>;
+} & Omit<
+		// This is the same as HTMLAttributes<P['as']>, except we're using OmitIndexSignature to remove the index signature,
+		// used for data attribute, because it seems like it get too complex for TypeScript with it, not sure why.
+		OmitIndexSignature<astroHTML.JSX.DefinedIntrinsicElements[P['as']]>,
+		keyof Omit<AstroBuiltinAttributes, 'class:list'>
+	>;
 
 export type Polymorphic<P extends { as: HTMLTag }> = PolymorphicAttributes<
 	Omit<P, 'as'> & { as: NonNullable<P['as']> }
