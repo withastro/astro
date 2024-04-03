90cfade88c2b9a34d8a5fe711ce329732d690409
Ben Holmes <hey@bholmes.dev>
2024-04-03 16:48:53 -0400

    feat: automatic Markdoc partial resolution (#10649)
    * wip: react counter example
    
    * feat: resolve markdoc partials by file path
    
    * test: components within partials
    
    * test: html within partial
    
    * chore: changeset
    
    * fix: respect user configured partials
    
    * test: basic partials
    
    * chore: lock
    
    * chore: fix lock
    
    * chore: minor -> patch
    
    * fix: use --parallel for dev server timeout error
    
    * refactor: move component tests to separate file
    
    * fix: build indent fixture
    
    * fix: check before addWatchFile
    
    * refactor: rootRelative -> relativePartial
    
    * deps: use workspace react integration
    
    * refactor: split test files by fixture
    
    * refactor: switch to preact to avoid react prod build error
    
    * feat: use vite pluginContext
    
    * fix: handle missing ./
    
    * chore: bump timeout

--- packages/integrations/markdoc/test/fixtures/render-partials/astro.config.mjs
+++ packages/integrations/markdoc/test/fixtures/render-partials/astro.config.mjs
@@ -1,8 +1,7 @@
 import markdoc from '@astrojs/markdoc';
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [markdoc(), preact()],
+	integrations: [markdoc()],
 });
