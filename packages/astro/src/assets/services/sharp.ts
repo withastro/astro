ab0ef29726236d57d2283d1d50e24d0fc50b61b6
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-03 12:58:33 +0300

    Add issue to comment in sharp.ts

--- packages/astro/src/assets/services/sharp.ts
+++ packages/astro/src/assets/services/sharp.ts
@@ -32,7 +32,7 @@ async function loadSharp() {
 		throw new AstroError(AstroErrorData.MissingSharp);
 	}
 
-	// Disable the `sharp` `libvips` cache as it errors when the file is too small and operations are happening too fast (runs into a race condition)
+	// Disable the `sharp` `libvips` cache as it errors when the file is too small and operations are happening too fast (runs into a race condition) https://github.com/lovell/sharp/issues/3935#issuecomment-1881866341
 	sharpImport.cache(false);
 
 	return sharpImport;
