60e5b344c817a018dc9652f0486857be4f4fea9a
bluwy <bjornlu.dev@gmail.com>
2024-04-05 14:05:56 +0800

    Refactor MDX transformJSX handling

--- packages/astro/src/vite-plugin-mdx/index.ts
+++ packages/astro/src/vite-plugin-mdx/index.ts
@@ -1,35 +1,19 @@
-import { type Plugin, type ResolvedConfig, transformWithEsbuild } from 'vite';
-import type { AstroRenderer, AstroSettings } from '../@types/astro.js';
-import type { Logger } from '../core/logger/core.js';
-
+import { type Plugin, transformWithEsbuild } from 'vite';
 import { CONTENT_FLAG, PROPAGATED_ASSET_FLAG } from '../content/index.js';
 import { astroEntryPrefix } from '../core/build/plugins/plugin-component-entry.js';
 import { removeQueryString } from '../core/path.js';
 import { transformJSX } from './transform-jsx.js';
-
-interface AstroPluginJSXOptions {
-	settings: AstroSettings;
-	logger: Logger;
-}
 
 // Format inspired by https://github.com/vitejs/vite/blob/main/packages/vite/src/node/constants.ts#L54
 const SPECIAL_QUERY_REGEX = new RegExp(
 	`[?&](?:worker|sharedworker|raw|url|${CONTENT_FLAG}|${PROPAGATED_ASSET_FLAG})\\b`
 );
 
-/** Use Astro config to allow for alternate or multiple JSX renderers (by default Vite will assume React) */
-export default function mdxVitePlugin({ settings }: AstroPluginJSXOptions): Plugin {
-	let viteConfig: ResolvedConfig;
-	// A reference to Astro's internal JSX renderer.
-	let astroJSXRenderer: AstroRenderer;
-
+// TODO: Move this Vite plugin into `@astrojs/mdx` in Astro 5
+export default function mdxVitePlugin(): Plugin {
 	return {
 		name: 'astro:jsx',
 		enforce: 'pre', // run transforms before other plugins
-		async configResolved(resolvedConfig) {
-			viteConfig = resolvedConfig;
-			astroJSXRenderer = settings.renderers.find((r) => r.jsxImportSource === 'astro')!;
-		},
 		async transform(code, id, opts) {
 			// Skip special queries and astro entries. We skip astro entries here as we know it doesn't contain
 			// JSX code, and also because we can't detect the import source to apply JSX transforms.
@@ -53,14 +37,7 @@ export default function mdxVitePlugin({ settings }: AstroPluginJSXOptions): Plug
 					},
 				},
 			});
-			return transformJSX({
-				code: jsxCode,
-				id,
-				renderer: astroJSXRenderer,
-				mode: viteConfig.mode,
-				ssr: Boolean(opts?.ssr),
-				root: settings.config.root,
-			});
+			return await transformJSX(jsxCode, id, opts?.ssr);
 		},
 	};
 }
--- packages/astro/src/vite-plugin-mdx/transform-jsx.ts
+++ packages/astro/src/vite-plugin-mdx/transform-jsx.ts
@@ -1,33 +1,18 @@
-import type { TransformResult } from 'rollup';
-import type { AstroRenderer } from '../@types/astro.js';
-import type { PluginMetadata } from '../vite-plugin-astro/types.js';
-
 import babel from '@babel/core';
-import tagExportsPlugin from './tag.js';
+import type { TransformResult } from 'rollup';
+import type { JSXTransformConfig } from '../@types/astro.js';
+import { jsxTransformOptions } from '../jsx/transform-options.js';
+import type { PluginMetadata } from '../vite-plugin-astro/types.js';
+import { tagExportsPlugin } from './tag.js';
 
-interface TransformJSXOptions {
-	code: string;
-	id: string;
-	mode: string;
-	renderer: AstroRenderer;
-	ssr: boolean;
-	root: URL;
-}
+export async function transformJSX(
+	code: string,
+	id: string,
+	ssr?: boolean
+): Promise<TransformResult> {
+	const options = await getJsxTransformOptions();
+	const plugins = ssr ? [...(options.plugins ?? []), tagExportsPlugin] : options.plugins;
 
-export async function transformJSX({
-	code,
-	mode,
-	id,
-	ssr,
-	renderer,
-	root,
-}: TransformJSXOptions): Promise<TransformResult> {
-	const { jsxTransformOptions } = renderer;
-	const options = await jsxTransformOptions!({ mode, ssr });
-	const plugins = [...(options.plugins || [])];
-	if (ssr) {
-		plugins.push(await tagExportsPlugin({ rendererName: renderer.name, root }));
-	}
 	const result = await babel.transformAsync(code, {
 		presets: options.presets,
 		plugins,
@@ -38,30 +23,47 @@ export async function transformJSX({
 		sourceMaps: true,
 		configFile: false,
 		babelrc: false,
+		browserslistConfigFile: false,
 		inputSourceMap: options.inputSourceMap,
 	});
+
 	// TODO: Be more strict about bad return values here.
 	// Should we throw an error instead? Should we never return `{code: ""}`?
 	if (!result) return null;
 
-	if (renderer.name === 'astro:jsx') {
-		const { astro } = result.metadata as unknown as PluginMetadata;
-		return {
-			code: result.code || '',
-			map: result.map,
-			meta: {
-				astro,
-				vite: {
-					// Setting this vite metadata to `ts` causes Vite to resolve .js
-					// extensions to .ts files.
-					lang: 'ts',
-				},
-			},
-		};
-	}
-
+	const { astro } = result.metadata as unknown as PluginMetadata;
 	return {
 		code: result.code || '',
 		map: result.map,
+		meta: {
+			astro,
+			vite: {
+				// Setting this vite metadata to `ts` causes Vite to resolve .js
+				// extensions to .ts files.
+				lang: 'ts',
+			},
+		},
 	};
+}
+
+let cachedJsxTransformOptions: Promise<JSXTransformConfig> | JSXTransformConfig | undefined;
+
+/**
+ * Get the `jsxTransformOptions` with caching
+ */
+async function getJsxTransformOptions(): Promise<JSXTransformConfig> {
+	if (cachedJsxTransformOptions) {
+		return cachedJsxTransformOptions;
+	}
+
+	const options = jsxTransformOptions();
+
+	// Cache the promise
+	cachedJsxTransformOptions = options;
+	// After the promise is resolved, cache the final resolved options
+	options.then((resolvedOptions) => {
+		cachedJsxTransformOptions = resolvedOptions;
+	});
+
+	return options;
 }
