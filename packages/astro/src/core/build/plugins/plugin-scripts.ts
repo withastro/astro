094eb6e684af0871919da3440a2e45f82210834f
Bjorn Lu <bjornlu.dev@gmail.com>
2024-04-08 18:53:50 +0800

    Fix script inline with directRenderScript (#10686)

--- packages/astro/src/core/build/plugins/plugin-scripts.ts
+++ packages/astro/src/core/build/plugins/plugin-scripts.ts
@@ -17,18 +17,31 @@ export function vitePluginScripts(internals: BuildInternals): VitePlugin {
 		},
 
 		async generateBundle(_options, bundle) {
-			for (const [id, output] of Object.entries(bundle)) {
+			const outputs = Object.values(bundle);
+
+			// Track ids that are imported by chunks so we don't inline scripts that are imported
+			const importedIds = new Set<string>();
+			for (const output of outputs) {
+				if (output.type === 'chunk') {
+					for (const id of output.imports) {
+						importedIds.add(id);
+					}
+				}
+			}
+
+			for (const output of outputs) {
 				// Try to inline scripts that don't import anything as is within the inline limit
 				if (
 					output.type === 'chunk' &&
 					output.facadeModuleId &&
 					internals.discoveredScripts.has(output.facadeModuleId) &&
+					!importedIds.has(output.fileName) &&
 					output.imports.length === 0 &&
 					output.dynamicImports.length === 0 &&
 					shouldInlineAsset(output.code, output.fileName, assetInlineLimit)
 				) {
 					internals.inlinedScripts.set(output.facadeModuleId, output.code.trim());
-					delete bundle[id];
+					delete bundle[output.fileName];
 				}
 			}
 		},
