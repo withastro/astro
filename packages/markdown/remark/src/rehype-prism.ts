c091465f6f2074065fae98294b5bd68604ef10a1
Bjorn Lu <bjornlu.dev@gmail.com>
2024-04-10 11:29:25 +0800

    Update packages/markdown/remark/src/rehype-prism.ts

--- packages/markdown/remark/src/rehype-prism.ts
+++ packages/markdown/remark/src/rehype-prism.ts
@@ -9,7 +9,7 @@ export const rehypePrism: Plugin<[], Root> = () => {
 			let { html, classLanguage } = runHighlighterWithAstro(language, code);
 
 			return Promise.resolve(
-				`<pre data-language="${language}" class="${classLanguage}"><code is:raw class="${classLanguage}">${html}</code></pre>`
+				`<pre class="${classLanguage}" data-language="${language}"><code is:raw class="${classLanguage}">${html}</code></pre>`
 			);
 		});
 	};
