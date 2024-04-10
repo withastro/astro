96bf2527a90cd9e38027fabf9d6683ba307509d7
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-04-10 18:15:33 +0200

    fix: setup file

---
+++ packages/astro/src/integrations/index.ts
@@ -0,0 +1,10 @@
+import type { AstroIntegration } from "../@types/astro.js";
+
+type IntegrationDefinition = {
+	name: string;
+	setup: (hooks: AstroIntegration['hooks']) => AstroIntegration;
+}
+
+export function defineIntegration(integration: IntegrationDefinition) {
+	return integration;
+}
