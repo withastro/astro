5e9b61b032946c4e237e409ec902a3668a95d6e1
Arsh <lilnasy@users.noreply.github.com>
2024-04-11 02:38:29 +0000

    [ci] format

--- packages/astro/test/astro-slots.test.js
+++ packages/astro/test/astro-slots.test.js
@@ -171,14 +171,14 @@ describe('Slots', () => {
 		}
 	});
 
-	it("Arguments can be passed to named slots with Astro.slots.render()", async () => {
+	it('Arguments can be passed to named slots with Astro.slots.render()', async () => {
 		const html = await fixture.readFile('/slotted-named-functions/index.html');
 		const $ = cheerio.load(html);
-		const befor = $("div#before");
-		const [ beforeDiv ] = befor.children("div")
-		assert.deepEqual(beforeDiv.firstChild.data, "Test Content BEFORE")
-		const after = $("div#after");
-		const [ afterDiv ] = after.children("div");
-		assert.deepEqual(afterDiv.firstChild.data, "Test Content AFTER")
+		const befor = $('div#before');
+		const [beforeDiv] = befor.children('div');
+		assert.deepEqual(beforeDiv.firstChild.data, 'Test Content BEFORE');
+		const after = $('div#after');
+		const [afterDiv] = after.children('div');
+		assert.deepEqual(afterDiv.firstChild.data, 'Test Content AFTER');
 	});
 });
