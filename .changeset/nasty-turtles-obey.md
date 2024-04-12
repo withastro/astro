1a569f8c8c5cf4856871b1776bb90537892315c5
Adrian Lyjak <adrian@chatmeter.com>
2024-04-12 14:35:53 -0400

    Make status code check more strict

---
+++ .changeset/nasty-turtles-obey.md
@@ -0,0 +1,5 @@
+---
+"@astrojs/sitemap": patch
+---
+
+Site map status code exclusion now only excludes pages like /[error-code] or /[locale]/[error-code]
--- packages/integrations/sitemap/test/fixtures/static/astro.config.mjs
+++ packages/integrations/sitemap/test/fixtures/static/astro.config.mjs
@@ -2,7 +2,15 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap({
+      i18n: {
+          defaultLocale: 'it',
+          locales: {
+              it: 'it-IT',
+              de: 'de-DE',
+          }
+      }
+  })],
 	site: 'http://example.com',
       redirects: {
         '/redirect': '/'
