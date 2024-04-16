2a75e771f94214567888eb429244414e19e1c727
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-04-16 18:08:52 +0200

    nit: small changes

--- examples/toolbar-app/app.ts
+++ examples/toolbar-app/app.ts
@@ -1,5 +1,6 @@
 import { defineToolbarApp } from 'astro:toolbar';
 
+// https://docs.astro.build/en/reference/dev-toolbar-app-reference/
 export default defineToolbarApp({
 	init(canvas) {
 		const astroWindow = document.createElement('astro-dev-toolbar-window');
@@ -8,6 +9,7 @@ export default defineToolbarApp({
 		text.textContent = 'Hello, Astro!';
 
 		astroWindow.appendChild(text);
+
 		canvas.appendChild(astroWindow);
 	},
 });
--- examples/toolbar-app/integration.ts
+++ examples/toolbar-app/integration.ts
@@ -1,5 +1,6 @@
 import type { AstroIntegration } from 'astro';
 
+// https://docs.astro.build/en/reference/integrations-reference/
 export default {
 	name: 'my-astro-integration',
 	hooks: {
