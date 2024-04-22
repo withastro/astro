ae92463153150915cfc8790e330e05a566c6a4c8
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-22 18:20:50 +0200

    Discard changes to examples/basics/astro.config.mjs

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,12 +1,4 @@
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    experimental: {
-        env: {
-            schema: {
-                PUBLIC_TEST: envField.string({ context: "client", access: "public", optional: true, default: "TEST" })
-            }
-        }
-    }
-});
+export default defineConfig({});
