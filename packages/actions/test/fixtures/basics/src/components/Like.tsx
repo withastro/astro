ae69456c2f325e42112b73496da712b49c113c04
bholmesdev <hey@bholmes.dev>
2024-04-24 16:46:22 -0400

    feat(test): add useActionState to actions example

--- packages/actions/test/fixtures/basics/src/components/Like.tsx
+++ packages/actions/test/fixtures/basics/src/components/Like.tsx
@@ -1,30 +1,22 @@
-// @ts-expect-error useFormStatus and useFormState are untyped
-import { useFormStatus, useFormState } from "react-dom";
+// @ts-expect-error useFormStatus and useActionState are untyped
+import { useActionState } from "react";
 import { actions } from "astro:actions";
 
 export function Like({ postId, initial }: { postId: string; initial: number }) {
-  const [state, actionName] = useFormState(
+  const [state, actionName, pending] = useActionState(
     async () => {
       const { likes } = await actions.blog.like({ postId });
       return { likes };
     },
     {
       likes: initial,
-    }
+    },
   );
   return (
     <form action={actionName}>
-      <Button likes={state.likes} />
+			<button disabled={pending} type="submit">
+				{state.likes} ❤️
+			</button>
     </form>
-  );
-}
-
-function Button({ likes }: { likes: number }) {
-  const { pending } = useFormStatus();
-
-  return (
-    <button disabled={pending} type="submit">
-      {likes} ❤️
-    </button>
   );
 }
