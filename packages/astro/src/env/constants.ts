aa0312d7cbbdcb17624cc9d36e92aba318cfc7ce
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-25 13:34:35 +0200

    feat: harcode constants

--- packages/astro/src/env/constants.ts
+++ packages/astro/src/env/constants.ts
@@ -1,11 +1,7 @@
-function resolveVirtualModuleId<T extends string>(id: T): `\0${T}` {
-	return `\0${id}`;
-}
-
 export const VIRTUAL_CLIENT_MODULE_ID = 'astro:env/client';
-export const RESOLVED_VIRTUAL_CLIENT_MODULE_ID = resolveVirtualModuleId(VIRTUAL_CLIENT_MODULE_ID);
+export const RESOLVED_VIRTUAL_CLIENT_MODULE_ID = '\0astro:env/client';
 export const VIRTUAL_SERVER_MODULE_ID = 'astro:env/server';
-export const RESOLVED_VIRTUAL_SERVER_MODULE_ID = resolveVirtualModuleId(VIRTUAL_SERVER_MODULE_ID);
+export const RESOLVED_VIRTUAL_SERVER_MODULE_ID = '\0astro:env/server';
 
 export const PUBLIC_PREFIX = 'PUBLIC_';
 export const ENV_TYPES_FILE = 'astro-env.d.ts';
