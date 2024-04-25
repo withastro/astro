e5b723be96aca935ecad8e78ba15de7de5d7daf5
bluwy <bjornlu.dev@gmail.com>
2024-04-25 22:33:05 +0800

    Make it stop

--- packages/astro/test/units/dev/collections-renderentry.test.js
+++ packages/astro/test/units/dev/collections-renderentry.test.js
@@ -84,36 +84,6 @@ _describe('Content Collections - render()', () => {
 	it('can be used in a layout component', async () => {
 		const fs = createFsWithFallback(
 			{
-				// Loading the content config with `astro:content` oddly
-				// causes this test to fail. Spoof a different src/content entry
-				// to ensure `existsSync` checks pass.
-				// TODO: revisit after addressing this issue
-				// https://github.com/withastro/astro/issues/6121
-				'/src/content/blog/promo/launch-week.mdx': `---
-title: Launch Week
-description: Astro is launching this week!
----
-# Launch Week
-- [x] Launch Astro
-- [ ] Celebrate`,
-				'/src/components/Layout.astro': `
-					---
-					import { getCollection } from 'astro:content';
-					const blog = await getCollection('blog');
-					const launchWeekEntry = blog.find(post => post.id === 'promo/launch-week.mdx');
-					const { Content } = await launchWeekEntry.render();
-					---
-					<html>
-						<head></head>
-						<body>
-							<slot name="title"></slot>
-							<article>
-								<Content />
-							</article>
-						</body>
-					</html>
-
-				`,
 				'/src/pages/index.astro': `
 					---
 					import Layout from '../components/Layout.astro';
