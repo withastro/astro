8ce23d266c205b7775a9466a680619f1a78c6554
Chris Swithinbank <swithinbank@gmail.com>
2024-04-26 12:35:53 +0200

    Peace of mind

--- packages/integrations/web-vitals/test/basics.test.js
+++ packages/integrations/web-vitals/test/basics.test.js
@@ -13,7 +13,7 @@ describe('Web Vitals integration basics', () => {
 
 	before(async () => {
 		fixture = await loadFixture({ root: './fixtures/basics/' });
-		devServer = await fixture.startDevServer();
+		devServer = await fixture.startDevServer({});
 	});
 
 	after(async () => {
@@ -21,7 +21,7 @@ describe('Web Vitals integration basics', () => {
 	});
 
 	it('adds a meta tag to the page', async () => {
-		const html = await fixture.fetch('/').then((res) => res.text());
+		const html = await fixture.fetch('/', {}).then((res) => res.text());
 		const { document } = parseHTML(html);
 		const meta = document.querySelector('head > meta[name="x-astro-vitals-route"]');
 		assert.ok(meta);
@@ -29,7 +29,7 @@ describe('Web Vitals integration basics', () => {
 	});
 	
 	it('adds a meta tag using the route pattern to the page', async () => {
-		const html = await fixture.fetch('/test').then((res) => res.text());
+		const html = await fixture.fetch('/test', {}).then((res) => res.text());
 		const { document } = parseHTML(html);
 		const meta = document.querySelector('head > meta[name="x-astro-vitals-route"]');
 		assert.ok(meta);
@@ -38,7 +38,6 @@ describe('Web Vitals integration basics', () => {
 
 	it.todo('accepts data to the injected endpoint', async () => {
 		const res = await fixture.fetch('/_/astro-vitals', { method: 'POST', body: '[]' });
-		// console.log(res)
 		assert.equal(res.status, 200);
 	})
 });
