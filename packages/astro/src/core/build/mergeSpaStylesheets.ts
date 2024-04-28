653b0b82e882fbdd8a93136e3ebd9b6a8a6fad9d
Lorenzo Fontoura <lorenzo@nioel.com>
2024-04-28 21:00:48 +1000

    feat: merge styles

--- packages/astro/src/core/build/mergeSpaStylesheets.ts
+++ packages/astro/src/core/build/mergeSpaStylesheets.ts
@@ -1,20 +1,45 @@
 import type {SerializedSSRManifest} from "../app/types.js";
+import type {StylesheetAsset} from "./types.js";
 
 export function mergeSpaStylesheets(manifest: SerializedSSRManifest) {
-  // TODO: optimise this, as a lot of duplicate CSS may be sent.
-  const indexedStyles = manifest.routes.map((r, i) => ({
-    styles: r.styles,
-    i,
-  }));
-  const routes = Object.values(manifest.routes);
-  for (let i = 0; i < routes.length; i++) {
-    const route = routes[i];
-    for (const styles of indexedStyles) {
-      if (i == styles.i) {
-        // Skip duplicating styles from origin route.
-        continue;
-      }
-      route.styles.push(...styles.styles);
-    }
+  const routes = manifest.routes.map(r => r);
+  const styles = routes.flatMap(r => r.styles);
+  const inlineStyleContents = styles
+    .map(s => s.type === "inline" ? s.content : null)
+    .filter(s => typeof s === "string") as string[];
+  const externalStyleSources = styles
+    .map(s => s.type === "external" ? s.src : null)
+    .filter(s => typeof s === "string") as string[];
+  const inlineStyle: StylesheetAsset = {
+    type: "inline",
+    content: mergeInlineStylesheets(inlineStyleContents)
+  };
+  const externalStyles = mergeExternalStyleSheets(externalStyleSources)
+    .map(src => ({
+      type: "external",
+      src
+    } satisfies StylesheetAsset));
+  for (const route of routes) {
+    route.styles = [inlineStyle, ...externalStyles];
   }
+}
+
+function mergeExternalStyleSheets(
+  sources: string[]
+): string[] {
+  const set = new Set<string>();
+  for (const source of sources) {
+    set.add(source);
+  }
+  return Array.from(set);
+}
+
+function mergeInlineStylesheets(values: string[]) : string {
+  const merged = values.join("");
+  const ruleSet = new Set();
+  const rules = merged.split(";").map(s => s.trim());
+  for (const rule of rules) {
+    ruleSet.add(rule);
+  }
+  return Array.from(ruleSet).join(";");
 }
