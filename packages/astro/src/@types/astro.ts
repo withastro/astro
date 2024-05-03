542af7ba91e00ccfeede768ceca660927dda174f
bholmesdev <hey@bholmes.dev>
2024-05-03 16:05:56 -0400

    docs: beef up actions experimental docs

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1712,9 +1712,8 @@ export interface AstroUserConfig {
 		 * @default `false`
 		 * @version 4.7.0
 		 * @description
-		 * Enables the new Astro Actions API, which lets you declare type-safe endpoints to call from your components.
 		 *
-		 * This option will be enabled by default in Astro 5.0.
+		 * Actions help you write type-safe backend functions you can call from anywhere.
 		 *
 		 * ```js
 		 * {
@@ -1723,6 +1722,85 @@ export interface AstroUserConfig {
 		 *   },
 		 * }
 		 * ```
+		 *
+		 * All actions are declared in a global actions handler: the `src/actions/index.ts` file.
+		 *
+		 * You can define an action using the `defineAction()` utility from the `astro:actions` module. This accepts the `handler` property to define your server-side request handler. If your action accepts arguments, apply the `schema` property to validate parameters with Zod.
+		 *
+		 * This example defines two actions: `like` and `comment`. The `like` action accepts a JSON object with a `postId` string, while the `comment` action accepts [form data](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest_API/Using_FormData_Objects) with `postId`, `author`, and `body` strings.
+		 *
+		 * ```ts
+		 * import { defineAction, z } from "astro:actions";
+		 *
+		 * export const server = {
+		 *   like: defineAction({
+		 *     // accept json
+		 *     input: z.object({ postId: z.string() }),
+		 *     handler: async ({ postId }, context) => {
+		 *       // update likes in db
+		 *
+		 *       return likes;
+		 *     },
+		 *   }),
+		 *   comment: defineAction({
+		 *     // accept form requests
+		 *     accept: "form",
+		 *     input: z.object({
+		 *       postId: z.string(),
+		 *       author: z.string(),
+		 *       body: z.string(),
+		 *     }),
+		 *     handler: async ({ postId }, context) => {
+		 *       // insert comments in db
+		 *
+		 *       return comment;
+		 *     },
+		 *   }),
+		 * };
+		 * ```
+		 *
+		 * Then, call an action from your client components using the `actions` object from `astro:actions`. You can pass a type-safe object when using JSON, or a `FormData` object when using `accept: 'form'` in your action definition:
+		 *
+		 * ```tsx
+		 * // src/components/blog.tsx
+		 * import { actions } from "astro:actions";
+		 * import { useState } from "preact/hooks";
+		 *
+		 * export function Like({ postId }: { postId: string }) {
+		 *   const [likes, setLikes] = useState(0);
+		 *   return (
+		 *     <button
+		 *       onClick={async () => {
+		 *         const newLikes = await actions.like({ postId });
+		 *         setLikes(newLikes);
+		 *       }}
+		 *     >
+		 *       {likes} likes
+		 *     </button>
+		 *   );
+		 * }
+		 *
+		 * export function Comment({ postId }: { postId: string }) {
+		 *   return (
+		 *     <form
+		 *       onSubmit={async (e) => {
+		 *         e.preventDefault();
+		 *         const formData = new FormData(e.target);
+		 *         const result = await actions.blog.comment(formData);
+		 *         // handle result
+		 *       }}
+		 *     >
+		 *       <input type="hidden" name="postId" value={postId} />
+		 *       <label for="author">Author</label>
+		 *       <input id="author" type="text" name="author" />
+		 *       <textarea rows={10} name="body"></textarea>
+		 *       <button type="submit">Post</button>
+		 *     </form>
+		 *   );
+		 * }
+		 * ```
+		 *
+		 * For a complete overview, and to give feedback on this experimental API, see the [Actions RFC](https://github.com/withastro/roadmap/blob/actions/proposals/0046-actions.md).
 		 */
 		actions?: boolean;
 
