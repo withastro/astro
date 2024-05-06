347ba4105f7381ff4099c107030ebed88eae818d
bholmesdev <hey@bholmes.dev>
2024-05-06 13:04:58 -0400

    feat: block action calls from the server with RFC link

--- packages/astro/actions-module.template.mjs
+++ packages/astro/actions-module.template.mjs
@@ -1,9 +1,6 @@
 import { ActionError, callSafely } from 'astro:actions';
 
-function toActionProxy(
-	actionCallback = {},
-	aggregatedPath = '/_actions/'
-) {
+function toActionProxy(actionCallback = {}, aggregatedPath = '/_actions/') {
 	return new Proxy(actionCallback, {
 		get(target, objKey) {
 			const path = aggregatedPath + objKey.toString();
@@ -11,6 +8,13 @@ function toActionProxy(
 				return target[objKey];
 			}
 			async function action(param) {
+				if (import.meta.env.SSR) {
+					throw new ActionError({
+						code: 'BAD_REQUEST',
+						message:
+							'Action unexpectedly called on the server. If this error is unexpected, share your feedback on our RFC discussion: https://github.com/withastro/roadmap/pull/912',
+					});
+				}
 				const headers = new Headers();
 				headers.set('Accept', 'application/json');
 				let body = param;
@@ -32,7 +36,7 @@ function toActionProxy(
 			action.toString = () => path;
 			action.safe = (input) => {
 				return callSafely(() => action(input));
-			}
+			};
 			action.safe[Symbol.for('astro:action:safe')] = true;
 			action.safe.toString = () => path;
 			// recurse to construct queries for nested object paths
