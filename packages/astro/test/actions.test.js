b34d28696d69069afe4ae2db78b1d6c6d0cafdbf
bholmesdev <hey@bholmes.dev>
2024-05-06 18:28:52 -0400

    test: build preview

--- packages/astro/test/actions.test.js
+++ packages/astro/test/actions.test.js
@@ -1,11 +1,15 @@
 import assert from 'node:assert/strict';
 import { before, after, describe, it } from 'node:test';
 import { loadFixture } from './test-utils.js';
+import testAdapter from './test-adapter.js';
 
 describe('Astro Actions', () => {
 	let fixture;
 	before(async () => {
-		fixture = await loadFixture({ root: './fixtures/actions/' });
+		fixture = await loadFixture({
+			root: './fixtures/actions/',
+			adapter: testAdapter(),
+		});
 	});
 
 	describe('dev', () => {
@@ -77,6 +81,86 @@ describe('Astro Actions', () => {
 				method: 'POST',
 				body: formData,
 			});
+
+			assert.equal(res.ok, true);
+			assert.equal(res.headers.get('Content-Type'), 'application/json');
+
+			const json = await res.json();
+			assert.equal(json.success, true);
+			assert.equal(json.isFormData, true, 'Should receive plain FormData');
+		});
+	});
+
+	describe('build', () => {
+		let app;
+
+		before(async () => {
+			await fixture.build();
+			app = await fixture.loadTestAdapterApp();
+		});
+
+		it('Exposes subscribe action', async () => {
+			const req = new Request('http://example.com/_actions/subscribe', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ channel: 'bholmesdev' }),
+			});
+			const res = await app.render(req);
+
+			assert.equal(res.ok, true);
+			assert.equal(res.headers.get('Content-Type'), 'application/json');
+
+			const json = await res.json();
+			assert.equal(json.channel, 'bholmesdev');
+			assert.equal(json.subscribeButtonState, 'smashed');
+		});
+
+		it('Exposes comment action', async () => {
+			const formData = new FormData();
+			formData.append('channel', 'bholmesdev');
+			formData.append('comment', 'Hello, World!');
+			const req = new Request('http://example.com/_actions/comment', {
+				method: 'POST',
+				body: formData,
+			});
+			const res = await app.render(req);
+
+			assert.equal(res.ok, true);
+			assert.equal(res.headers.get('Content-Type'), 'application/json');
+
+			const json = await res.json();
+			assert.equal(json.channel, 'bholmesdev');
+			assert.equal(json.comment, 'Hello, World!');
+		});
+
+		it('Raises validation error on bad form data', async () => {
+			const formData = new FormData();
+			formData.append('channel', 'bholmesdev');
+			const req = new Request('http://example.com/_actions/comment', {
+				method: 'POST',
+				body: formData,
+			});
+			const res = await app.render(req);
+
+			assert.equal(res.ok, false);
+			assert.equal(res.status, 400);
+			assert.equal(res.headers.get('Content-Type'), 'application/json');
+
+			const json = await res.json();
+			assert.equal(json.type, 'AstroActionInputError');
+		});
+
+		it('Exposes plain formData action', async () => {
+			const formData = new FormData();
+			formData.append('channel', 'bholmesdev');
+			formData.append('comment', 'Hello, World!');
+			const req = new Request('http://example.com/_actions/commentPlainFormData', {
+				method: 'POST',
+				body: formData,
+			});
+			const res = await app.render(req);
 
 			assert.equal(res.ok, true);
 			assert.equal(res.headers.get('Content-Type'), 'application/json');
