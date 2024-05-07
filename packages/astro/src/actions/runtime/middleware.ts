2290788b40c86a77e1710248e1fe6cb42e9f08cc
bholmesdev <hey@bholmes.dev>
2024-05-07 12:51:32 -0400

    refactor: defineProperty with write false

--- packages/astro/src/actions/runtime/middleware.ts
+++ packages/astro/src/actions/runtime/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from '../../core/middleware/index.js';
 import { ApiContextStorage } from './store.js';
 import { formContentTypes, getAction, hasContentType } from './utils.js';
 import { callSafely } from './virtual/shared.js';
-import type { APIContext } from '../../@types/astro.js';
+import type { APIContext, MiddlewareNext } from '../../@types/astro.js';
 
 export type Locals = {
 	_actionsInternal: {
@@ -14,21 +14,19 @@ export const onRequest = defineMiddleware(async (context, next) => {
 	const locals = context.locals as Locals;
 	const { request } = context;
 	const contentType = request.headers.get('Content-Type');
-	locals._actionsInternal = {
-		getActionResult: () => undefined,
-	};
 
-	if (!contentType || !hasContentType(contentType, formContentTypes)) return next();
+	if (!contentType || !hasContentType(contentType, formContentTypes))
+		return nextWithLocalsStub(next, locals);
 
 	const formData = await request.clone().formData();
 	const actionPath = formData.get('_astroAction');
-	if (typeof actionPath !== 'string') return next();
+	if (typeof actionPath !== 'string') return nextWithLocalsStub(next, locals);
 
 	const actionPathKeys = actionPath.replace('/_actions/', '').split('.');
 	const action = await getAction(actionPathKeys);
 	const result = await ApiContextStorage.run(context, () => callSafely(() => action(formData)));
 
-	locals._actionsInternal = {
+	const actionsInternal: Locals['_actionsInternal'] = {
 		getActionResult: (actionFn) => {
 			if (actionFn.toString() !== actionPath) return Promise.resolve(undefined);
 			// The `action` uses type `unknown` since we can't infer the user's action type.
@@ -36,5 +34,16 @@ export const onRequest = defineMiddleware(async (context, next) => {
 			return result as any;
 		},
 	};
+	Object.defineProperty(locals, '_actionsInternal', { writable: false, value: actionsInternal });
 	return next();
 });
+
+function nextWithLocalsStub(next: MiddlewareNext, locals: Locals) {
+	Object.defineProperty(locals, '_actionsInternal', {
+		writable: false,
+		value: {
+			getActionResult: () => undefined,
+		},
+	});
+	return next();
+}
