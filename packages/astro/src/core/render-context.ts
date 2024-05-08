2ed52d2e435a076b4cc950f5f07438ceb6f153b5
Bryce Russell <brycetrussell@gmail.com>
2024-05-08 13:12:46 -0500

    Throw when accessing clientAddess on prerendered request

--- packages/astro/src/core/render-context.ts
+++ packages/astro/src/core/render-context.ts
@@ -455,11 +455,16 @@ export class RenderContext {
 		if (clientAddressSymbol in request) {
 			return Reflect.get(request, clientAddressSymbol) as string;
 		}
-		if (pipeline.adapterName) {
-			throw new AstroError({
-				...AstroErrorData.ClientAddressNotAvailable,
-				message: AstroErrorData.ClientAddressNotAvailable.message(pipeline.adapterName),
-			});
+		if (pipeline.serverLike) {
+			if (request.body === null) {
+				throw new AstroError(AstroErrorData.PrerenderClientAddressNotAvailable);
+			}
+			if (pipeline.adapterName) {
+				throw new AstroError({
+					...AstroErrorData.ClientAddressNotAvailable,
+					message: AstroErrorData.ClientAddressNotAvailable.message(pipeline.adapterName),
+				});
+			}
 		} else {
 			throw new AstroError(AstroErrorData.StaticClientAddressNotAvailable);
 		}
