d35e47bb10ff63311025a0de479801323fc755cd
bholmesdev <hey@bholmes.dev>
2024-05-10 18:45:05 -0400

    fix: use LibsqlError for remote db errors

--- packages/db/src/runtime/db-client.ts
+++ packages/db/src/runtime/db-client.ts
@@ -4,7 +4,7 @@ import type { LibSQLDatabase } from 'drizzle-orm/libsql';
 import { drizzle as drizzleLibsql } from 'drizzle-orm/libsql';
 import { type SqliteRemoteDatabase, drizzle as drizzleProxy } from 'drizzle-orm/sqlite-proxy';
 import { z } from 'zod';
-import { AstroDbError, safeFetch } from './utils.js';
+import { DetailedLibsqlError, safeFetch } from './utils.js';
 
 const isWebContainer = !!process.versions?.webcontainer;
 
@@ -65,7 +65,10 @@ export function createRemoteDatabaseClient(appToken: string, remoteDbURL: string
 				const json = await res.json();
 				remoteResult = remoteResultSchema.parse(json);
 			} catch (e) {
-				throw new AstroDbError(await getUnexpectedResponseMessage(res));
+				throw new DetailedLibsqlError({
+					message: await getUnexpectedResponseMessage(res),
+					code: KNOWN_ERROR_CODES.SQL_QUERY_FAILED,
+				});
 			}
 
 			if (method === 'run') return remoteResult;
@@ -107,7 +110,10 @@ export function createRemoteDatabaseClient(appToken: string, remoteDbURL: string
 				const json = await res.json();
 				remoteResults = z.array(remoteResultSchema).parse(json);
 			} catch (e) {
-				throw new AstroDbError(await getUnexpectedResponseMessage(res));
+				throw new DetailedLibsqlError({
+					message: await getUnexpectedResponseMessage(res),
+					code: KNOWN_ERROR_CODES.SQL_QUERY_FAILED,
+				});
 			}
 			let results: any[] = [];
 			for (const [idx, rawResult] of remoteResults.entries()) {
@@ -153,20 +159,21 @@ const KNOWN_ERROR_CODES = {
 const getUnexpectedResponseMessage = async (response: Response) =>
 	`Unexpected response from remote database:\n(Status ${response.status}) ${await response.text()}`;
 
-async function parseRemoteError(response: Response): Promise<AstroDbError> {
+async function parseRemoteError(response: Response): Promise<DetailedLibsqlError> {
 	let error;
 	try {
 		error = errorSchema.parse(await response.json()).error;
 	} catch (e) {
-		return new AstroDbError(await getUnexpectedResponseMessage(response));
+		return new DetailedLibsqlError({
+			message: await getUnexpectedResponseMessage(response),
+			code: KNOWN_ERROR_CODES.SQL_QUERY_FAILED,
+		});
 	}
 	// Strip LibSQL error prefixes
-	let details =
-		error.details?.replace(/.*SQLite error: /, '') ??
-		`(Code ${error.code}) \nError querying remote database.`;
+	let details = error.details?.replace(/.*SQLite error: /, '') ?? 'Error querying remote database.';
 	let hint = `See the Astro DB guide for query and push instructions: https://docs.astro.build/en/guides/astro-db/#query-your-database`;
 	if (error.code === KNOWN_ERROR_CODES.SQL_QUERY_FAILED && details.includes('no such table')) {
 		hint = `Did you run \`astro db push\` to push your latest table schemas?`;
 	}
-	return new AstroDbError(details, hint);
+	return new DetailedLibsqlError({ message: details, code: error.code, hint });
 }
