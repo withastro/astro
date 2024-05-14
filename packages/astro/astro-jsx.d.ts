10739fc3deffeeca69071251b98fa37aeb9f7025
Argana Phangquestian <Princesseuh@users.noreply.github.com>
2024-05-14 08:39:06 +0000

    [ci] format

--- packages/astro/astro-jsx.d.ts
+++ packages/astro/astro-jsx.d.ts
@@ -649,7 +649,7 @@ declare namespace astroHTML.JSX {
 		type?: 'submit' | 'reset' | 'button' | undefined | null;
 		value?: string | string[] | number | undefined | null;
 		popovertarget?: string | undefined | null;
-		popovertargetaction?: "hide" | "show" | "toggle" | undefined | null;
+		popovertargetaction?: 'hide' | 'show' | 'toggle' | undefined | null;
 	}
 
 	interface CanvasHTMLAttributes extends HTMLAttributes {
@@ -816,7 +816,7 @@ declare namespace astroHTML.JSX {
 		value?: string | string[] | number | undefined | null;
 		width?: number | string | undefined | null;
 		popovertarget?: string | undefined | null;
-		popovertargetaction?: "hide" | "show" | "toggle" | undefined | null;
+		popovertargetaction?: 'hide' | 'show' | 'toggle' | undefined | null;
 	}
 
 	interface KeygenHTMLAttributes extends HTMLAttributes {
