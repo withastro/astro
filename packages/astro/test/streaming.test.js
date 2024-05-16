f4628b4df66fc7b669688d838cf8d1bb8a88b2eb
Matthew Phillips <matthew@skypack.dev>
2024-05-16 09:23:47 -0400

    Add new assertion

--- packages/astro/test/streaming.test.js
+++ packages/astro/test/streaming.test.js
@@ -38,6 +38,7 @@ describe('Streaming', () => {
 				chunks.push(chunk);
 			}
 			assert.equal(chunks.length > 5, true);
+			assert.ok(!chunks[0].includes('id="promise"'), 'the first chunk should not include the initial promise creation');
 		});
 
 		it('Body of slots is chunked', async () => {
