19939db7d976b017a22848a14e065b7aa248d390
bholmesdev <hey@bholmes.dev>
2024-05-16 13:19:27 -0400

    feat(test): Like with use action state test

--- packages/astro/e2e/fixtures/actions-react-19/src/components/Like.tsx
+++ packages/astro/e2e/fixtures/actions-react-19/src/components/Like.tsx
@@ -1,9 +1,26 @@
 import { actions } from 'astro:actions';
+import { useActionState } from 'react';
 import { useFormStatus } from 'react-dom';
+import { withState } from '@astrojs/react/actions';
 
 export function Like({ postId, label, likes }: { postId: string; label: string; likes: number }) {
 	return (
 		<form action={actions.blog.like}>
+			<input type="hidden" name="postId" value={postId} />
+			<Button likes={likes} label={label} />
+		</form>
+	);
+}
+
+
+export function LikeWithActionState({ postId, label, likes: initial }: { postId: string; label: string; likes: number }) {
+	const [likes, action] = useActionState(
+		withState(actions.blog.likeWithActionState.safe),
+		10,
+	);
+
+	return (
+		<form action={action}>
 			<input type="hidden" name="postId" value={postId} />
 			<Button likes={likes} label={label} />
 		</form>
