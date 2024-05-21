b0185d006fb977f212cfd2bf990b0b8db48373d2
Matthew Phillips <matthew@skypack.dev>
2024-05-21 09:32:38 -0400

    Update packages/db/src/runtime/db-client.ts
    Co-authored-by: Ben Holmes <hey@bholmes.dev>

--- packages/db/src/runtime/db-client.ts
+++ packages/db/src/runtime/db-client.ts
@@ -73,7 +73,7 @@ export function createRemoteDatabaseClient(appToken: string, remoteDbURL: string
 
 			if (method === 'run') {
 				const rawRows = Array.from(remoteResult.rows);
-				// in order to serialize properly much implement toJSON
+				// Implement basic `toJSON()` for Drizzle to serialize properly
 				(remoteResult as any).rows.toJSON = () => rawRows;
 				// Using `db.run()` drizzle massages the rows into an object.
 				// So in order to make dev/prod consistent, we need to do the same here.
