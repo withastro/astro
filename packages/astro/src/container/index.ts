ad264918d4c1a47176b59d25192c9275ebadbd6b
Matthew Phillips <matthew@skypack.dev>
2024-05-24 13:21:41 -0400

    Pass manifest too

--- packages/astro/src/container/index.ts
+++ packages/astro/src/container/index.ts
@@ -260,7 +260,7 @@ export class experimental_AstroContainer {
 	): Promise<experimental_AstroContainer> {
 		const { streaming = false, manifest, renderers = [], resolve } = containerOptions;
 		const astroConfig = await validateConfig(ASTRO_CONFIG_DEFAULTS, process.cwd(), 'container');
-		return new experimental_AstroContainer({ streaming, renderers, astroConfig, resolve });
+		return new experimental_AstroContainer({ streaming, manifest, renderers, astroConfig, resolve });
 	}
 
 	// NOTE: we keep this private via TS instead via `#` so it's still available on the surface, so we can play with it.
