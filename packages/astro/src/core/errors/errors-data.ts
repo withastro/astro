06488c7ec94de0d6c9d3b334a677e2bbf4897dc8
Emanuele Stoppa <my.burning@gmail.com>
2024-06-10 11:36:08 +0100

    ci: fix regression in errors data

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1199,7 +1199,7 @@ export const i18nNotEnabled = {
 /**
  * @docs
  * @description
- * An i18n utility tried to use the locale from a URL path that does not contain one. You can prevent this error by using pathHasLocale to check URLs for a `locale` first before using i18n utilities.
+ * An i18n utility tried to use the locale from a URL path that does not contain one. You can prevent this error by using pathHasLocale to check URLs for a locale first before using i18n utilities.
  *
  */
 export const i18nNoLocaleFoundInPath = {
