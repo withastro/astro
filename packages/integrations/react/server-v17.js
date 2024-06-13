f5d08a793e610265adc099e0db26deae15536417
Emanuele Stoppa <my.burning@gmail.com>
2024-06-13 11:46:44 +0100

    revert changes to react integration

--- packages/integrations/react/server-v17.js
+++ packages/integrations/react/server-v17.js
@@ -86,11 +86,3 @@ export default {
 	renderToStaticMarkup,
 	supportsAstroStaticSlot: true,
 };
-
-export const renderer = {
-	ssr: {
-		check,
-		renderToStaticMarkup,
-		supportsAstroStaticSlot: true,
-	}
-}
