21b53a5206cedcfb0b4bcb87ef493e2d4d529f58
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-06-13 20:59:00 -0400

    add missing leading slash in tests

--- packages/astro/test/astro-pagination-root-spread.test.js
+++ packages/astro/test/astro-pagination-root-spread.test.js
@@ -17,9 +17,9 @@ describe('Pagination root', () => {
 
 	it('correct prev url in root spread', async () => {
 		const prevMap = {
-			'/4/': 'blog/3',
-			'/3/': 'blog/2',
-			'/2/': 'blog/',
+			'/4/': '/blog/3',
+			'/3/': '/blog/2',
+			'/2/': '/blog',
 			'/': undefined,
 		};
 
