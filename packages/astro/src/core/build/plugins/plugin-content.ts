888ca0543e3a526def336a3e91089be5d7e76989
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-06-22 00:29:24 +0800

    chore: update

--- packages/astro/src/core/build/plugins/plugin-content.ts
+++ packages/astro/src/core/build/plugins/plugin-content.ts
@@ -95,7 +95,7 @@ function vitePluginContent(
 		try {
 			const data = fsMod.readFileSync(contentManifestFile, { encoding: 'utf8' });
 			oldManifest = JSON.parse(data);
-		} catch { }
+		} catch {}
 	}
 
 	return {
