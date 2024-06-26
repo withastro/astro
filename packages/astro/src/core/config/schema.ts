1051270836075d16285cea9dae220377900d65de
madcampos <me@madcampos.dev>
2024-06-26 02:18:40 -0400

    feat: propagate shiki option for default color to astro config

--- packages/astro/src/core/config/schema.ts
+++ packages/astro/src/core/config/schema.ts
@@ -318,6 +318,7 @@ export const AstroConfigSchema = z.object({
 								.or(z.custom<ShikiTheme>())
 						)
 						.default(ASTRO_CONFIG_DEFAULTS.markdown.shikiConfig.themes!),
+					defaultColor: z.union([z.string(), z.literal(false)]).optional(),
 					wrap: z.boolean().or(z.null()).default(ASTRO_CONFIG_DEFAULTS.markdown.shikiConfig.wrap!),
 					transformers: z
 						.custom<ShikiTransformer>()
