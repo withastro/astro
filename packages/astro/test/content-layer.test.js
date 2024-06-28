0bf85c6791c7d3d45c19403b5e8cf55221ffbfac
Matt Kane <m@mk.gg>
2024-06-28 14:31:14 +0100

    Add test

--- packages/astro/test/content-layer.test.js
+++ packages/astro/test/content-layer.test.js
@@ -1,83 +1,192 @@
 import assert from 'node:assert/strict';
-import { before, describe, it } from 'node:test';
+import { after, before, describe, it } from 'node:test';
 import { loadFixture } from './test-utils.js';
 
 describe('Content Layer', () => {
+	/** @type {import("./test-utils.js").Fixture} */
 	let fixture;
-	let json;
 
 	before(async () => {
 		fixture = await loadFixture({ root: './fixtures/content-layer/' });
-		await fixture.build();
-		const rawJson = await fixture.readFile('/collections.json');
-		json = JSON.parse(rawJson);
 	});
 
-	it('Returns custom loader collection', async () => {
-		assert.ok(json.hasOwnProperty('customLoader'));
-		assert.ok(Array.isArray(json.customLoader));
+	describe('Build', () => {
+		let json;
+		before(async () => {
+			fixture = await loadFixture({ root: './fixtures/content-layer/' });
+			await fixture.build();
+			const rawJson = await fixture.readFile('/collections.json');
+			json = JSON.parse(rawJson);
+		});
 
-		const item = json.customLoader[0];
-		assert.deepEqual(item, {
-			id: '1',
-			collection: 'blog',
-			data: {
-				userId: 1,
-				id: 1,
-				title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-				body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-			},
-			type: 'experimental_data',
+		it('Returns custom loader collection', async () => {
+			assert.ok(json.hasOwnProperty('customLoader'));
+			assert.ok(Array.isArray(json.customLoader));
+
+			const item = json.customLoader[0];
+			assert.deepEqual(item, {
+				id: '1',
+				collection: 'blog',
+				data: {
+					userId: 1,
+					id: 1,
+					title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+					body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+				},
+				type: 'experimental_data',
+			});
+		});
+
+		it('Returns `file()` loader collection', async () => {
+			assert.ok(json.hasOwnProperty('fileLoader'));
+			assert.ok(Array.isArray(json.fileLoader));
+
+			const ids = json.fileLoader.map((item) => item.data.id);
+			assert.deepEqual(ids, [
+				'labrador-retriever',
+				'german-shepherd',
+				'golden-retriever',
+				'french-bulldog',
+				'bulldog',
+				'beagle',
+				'poodle',
+				'rottweiler',
+				'german-shorthaired-pointer',
+				'yorkshire-terrier',
+				'boxer',
+				'dachshund',
+				'siberian-husky',
+				'great-dane',
+				'doberman-pinscher',
+				'australian-shepherd',
+				'miniature-schnauzer',
+				'cavalier-king-charles-spaniel',
+				'shih-tzu',
+				'boston-terrier',
+				'bernese-mountain-dog',
+				'pomeranian',
+				'havanese',
+				'english-springer-spaniel',
+				'shetland-sheepdog',
+			]);
+		});
+
+		it('Returns data entry by id', async () => {
+			assert.ok(json.hasOwnProperty('dataEntryById'));
+			assert.deepEqual(json.dataEntryById, {
+				id: 'beagle',
+				collection: 'dogs',
+				data: {
+					breed: 'Beagle',
+					id: 'beagle',
+					size: 'Small to Medium',
+					origin: 'England',
+					lifespan: '12-15 years',
+					temperament: ['Friendly', 'Curious', 'Merry'],
+				},
+			});
 		});
 	});
 
-	it('Returns `file()` loader collection', async () => {
-		assert.ok(json.hasOwnProperty('fileLoader'));
-		assert.ok(Array.isArray(json.fileLoader));
+	describe('Dev', () => {
+		let devServer;
+		let json;
+		before(async () => {
+			devServer = await fixture.startDevServer();
+			const rawJsonResponse = await fixture.fetch('/collections.json');
+			const rawJson = await rawJsonResponse.text();
+			json = JSON.parse(rawJson);
+		});
 
-		const ids = json.fileLoader.map((item) => item.data.id);
-		assert.deepEqual(ids, [
-			'labrador-retriever',
-			'german-shepherd',
-			'golden-retriever',
-			'french-bulldog',
-			'bulldog',
-			'beagle',
-			'poodle',
-			'rottweiler',
-			'german-shorthaired-pointer',
-			'yorkshire-terrier',
-			'boxer',
-			'dachshund',
-			'siberian-husky',
-			'great-dane',
-			'doberman-pinscher',
-			'australian-shepherd',
-			'miniature-schnauzer',
-			'cavalier-king-charles-spaniel',
-			'shih-tzu',
-			'boston-terrier',
-			'bernese-mountain-dog',
-			'pomeranian',
-			'havanese',
-			'english-springer-spaniel',
-			'shetland-sheepdog',
-		]);
-	});
+		after(async () => {
+			devServer?.stop();
+		});
 
-	it('Returns data entry by id', async () => {
-		assert.ok(json.hasOwnProperty('dataEntryById'));
-		assert.deepEqual(json.dataEntryById, {
-			id: 'beagle',
-			collection: 'dogs',
-			data: {
-				breed: 'Beagle',
+		it('Returns custom loader collection', async () => {
+			assert.ok(json.hasOwnProperty('customLoader'));
+			assert.ok(Array.isArray(json.customLoader));
+
+			const item = json.customLoader[0];
+			assert.deepEqual(item, {
+				id: '1',
+				collection: 'blog',
+				data: {
+					userId: 1,
+					id: 1,
+					title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+					body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+				},
+				type: 'experimental_data',
+			});
+		});
+
+		it('Returns `file()` loader collection', async () => {
+			assert.ok(json.hasOwnProperty('fileLoader'));
+			assert.ok(Array.isArray(json.fileLoader));
+
+			const ids = json.fileLoader.map((item) => item.data.id);
+			assert.deepEqual(ids, [
+				'labrador-retriever',
+				'german-shepherd',
+				'golden-retriever',
+				'french-bulldog',
+				'bulldog',
+				'beagle',
+				'poodle',
+				'rottweiler',
+				'german-shorthaired-pointer',
+				'yorkshire-terrier',
+				'boxer',
+				'dachshund',
+				'siberian-husky',
+				'great-dane',
+				'doberman-pinscher',
+				'australian-shepherd',
+				'miniature-schnauzer',
+				'cavalier-king-charles-spaniel',
+				'shih-tzu',
+				'boston-terrier',
+				'bernese-mountain-dog',
+				'pomeranian',
+				'havanese',
+				'english-springer-spaniel',
+				'shetland-sheepdog',
+			]);
+		});
+
+		it('Returns data entry by id', async () => {
+			assert.ok(json.hasOwnProperty('dataEntryById'));
+			assert.deepEqual(json.dataEntryById, {
 				id: 'beagle',
-				size: 'Small to Medium',
-				origin: 'England',
-				lifespan: '12-15 years',
-				temperament: ['Friendly', 'Curious', 'Merry'],
-			},
+				collection: 'dogs',
+				data: {
+					breed: 'Beagle',
+					id: 'beagle',
+					size: 'Small to Medium',
+					origin: 'England',
+					lifespan: '12-15 years',
+					temperament: ['Friendly', 'Curious', 'Merry'],
+				},
+			});
+		});
+
+		it('updates collection when data file is changed', async () => {
+			const rawJsonResponse = await fixture.fetch('/collections.json');
+			const initialJson = await rawJsonResponse.json();
+			assert.equal(initialJson.fileLoader[0].data.temperament.includes('Bouncy'), false);
+
+			await fixture.editFile('/src/content/_data/dogs.json', (prev) => {
+				const data = JSON.parse(prev);
+				data[0].temperament.push('Bouncy');
+				return JSON.stringify(data, null, 2);
+			});
+
+			// Writes are debounced to 500ms
+			await new Promise((r) => setTimeout(r, 700));
+
+			const updatedJsonResponse = await fixture.fetch('/collections.json');
+			const updated = await updatedJsonResponse.json();
+			assert.ok(updated.fileLoader[0].data.temperament.includes('Bouncy'));
 		});
 	});
 });
