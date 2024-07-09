b2f1dddb14949ffadb903e77168e06736f2d877a
bholmesdev <hey@bholmes.dev>
2024-07-09 10:42:10 -0400

    feat: allow type `unknown` on `isInputError`

--- packages/astro/src/actions/runtime/virtual/shared.ts
+++ packages/astro/src/actions/runtime/virtual/shared.ts
@@ -40,6 +40,15 @@ const statusToCodeMap: Record<number, ActionErrorCode> = Object.entries(codeToSt
 	{}
 );
 
+/**
+ * Used to preserve the input schema type in the error object.
+ * This allows for type inference on the `fields` property
+ * when type narrowed to an `ActionInputError`.
+ *
+ * Example: Action has an input schema of `{ name: z.string() }`.
+ * When calling the action and checking `isInputError(result.error)`,
+ * `result.error.fields` will be typed with the `name` field.
+ */
 export type ErrorInferenceObject = Record<string, any>;
 
 export class ActionError<T extends ErrorInferenceObject = ErrorInferenceObject> extends Error {
@@ -85,6 +94,10 @@ export class ActionError<T extends ErrorInferenceObject = ErrorInferenceObject> 
 
 export function isInputError<T extends ErrorInferenceObject>(
 	error?: ActionError<T>
+): error is ActionInputError<T>;
+export function isInputError(error?: unknown): error is ActionInputError<ErrorInferenceObject>;
+export function isInputError<T extends ErrorInferenceObject>(
+	error?: unknown | ActionError<T>
 ): error is ActionInputError<T> {
 	return error instanceof ActionInputError;
 }
