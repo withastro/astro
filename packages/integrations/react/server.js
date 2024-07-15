1a1a8e46b9e658e9944c480a543216380e9215ed
Arturo Silva <arturo.silva@washpost.com>
2024-07-14 21:37:11 -0400

    feat: add experimentalDisableStreaming to react integration

--- packages/integrations/react/server.js
+++ packages/integrations/react/server.js
@@ -31,17 +31,17 @@ async function check(Component, props, children) {
 
 	if (Component.prototype != null && typeof Component.prototype.render === 'function') {
 		return React.Component.isPrototypeOf(Component) || React.PureComponent.isPrototypeOf(Component);
-	}
+  }
 
 	let error = null;
 	let isReactComponent = false;
 	function Tester(...args) {
 		try {
-			const vnode = Component(...args);
-			if (vnode && vnode['$$typeof'] === reactTypeof) {
+      const vnode = Component(...args);
+      if (vnode && vnode['$$typeof'] === reactTypeof) {
 				isReactComponent = true;
 			}
-		} catch (err) {
+    } catch (err) {
 			if (!errorIsComingFromPreactComponent(err)) {
 				error = err;
 			}
@@ -114,6 +114,9 @@ async function renderToStaticMarkup(Component, props, { default: children, ...sl
 		formState,
 	};
 	let html;
+	if (opts.experimentalDisableStreaming) {
+		html = ReactDOM.renderToString(vnode);
+	}
 	if ('renderToReadableStream' in ReactDOM) {
 		html = await renderToReadableStreamAsync(vnode, renderOptions);
 	} else {
--- packages/integrations/react/src/index.ts
+++ packages/integrations/react/src/index.ts
@@ -14,6 +14,7 @@ export type ReactIntegrationOptions = Pick<
 	'include' | 'exclude' | 'babel'
 > & {
 	experimentalReactChildren?: boolean;
+	experimentalDisableStreaming?: boolean;
 };
 
 const FAST_REFRESH_PREAMBLE = react.preambleCode;
@@ -26,7 +27,7 @@ function getRenderer(reactConfig: ReactVersionConfig) {
 	};
 }
 
-function optionsPlugin(experimentalReactChildren: boolean): vite.Plugin {
+function optionsPlugin(experimentalReactChildren: boolean, experimentalDisableStreaming: boolean): vite.Plugin {
 	const virtualModule = 'astro:react:opts';
 	const virtualModuleId = '\0' + virtualModule;
 	return {
@@ -40,7 +41,8 @@ function optionsPlugin(experimentalReactChildren: boolean): vite.Plugin {
 			if (id === virtualModuleId) {
 				return {
 					code: `export default {
-						experimentalReactChildren: ${JSON.stringify(experimentalReactChildren)}
+						experimentalReactChildren: ${JSON.stringify(experimentalReactChildren)},
+						experimentalDisableStreaming: ${JSON.stringify(experimentalDisableStreaming)}
 					}`,
 				};
 			}
@@ -49,7 +51,7 @@ function optionsPlugin(experimentalReactChildren: boolean): vite.Plugin {
 }
 
 function getViteConfiguration(
-	{ include, exclude, babel, experimentalReactChildren }: ReactIntegrationOptions = {},
+	{ include, exclude, babel, experimentalReactChildren, experimentalDisableStreaming }: ReactIntegrationOptions = {},
 	reactConfig: ReactVersionConfig
 ) {
 	return {
@@ -63,7 +65,7 @@ function getViteConfiguration(
 			],
 			exclude: [reactConfig.server],
 		},
-		plugins: [react({ include, exclude, babel }), optionsPlugin(!!experimentalReactChildren)],
+		plugins: [react({ include, exclude, babel }), optionsPlugin(!!experimentalReactChildren, !!experimentalDisableStreaming)],
 		resolve: {
 			dedupe: ['react', 'react-dom', 'react-dom/server'],
 		},
@@ -86,6 +88,7 @@ export default function ({
 	exclude,
 	babel,
 	experimentalReactChildren,
+	experimentalDisableStreaming,
 }: ReactIntegrationOptions = {}): AstroIntegration {
 	const majorVersion = getReactMajorVersion();
 	if (isUnsupportedVersion(majorVersion)) {
@@ -100,7 +103,7 @@ export default function ({
 				addRenderer(getRenderer(versionConfig));
 				updateConfig({
 					vite: getViteConfiguration(
-						{ include, exclude, babel, experimentalReactChildren },
+						{ include, exclude, babel, experimentalReactChildren, experimentalDisableStreaming },
 						versionConfig
 					),
 				});
