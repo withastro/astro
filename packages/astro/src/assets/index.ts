aff19f0f92a183911c6b56a8c08040355710708b
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-07-15 17:22:10 +0200

    fix: dont export from astro/assets

--- packages/astro/src/assets/index.ts
+++ packages/astro/src/assets/index.ts
@@ -1,4 +1,3 @@
 export { getConfiguredImageService, getImage } from './internal.js';
 export { baseService, isLocalService } from './services/service.js';
-export { inferRemoteSize } from './utils/remoteProbe.js';
 export { type LocalImageProps, type RemoteImageProps } from './types.js';
