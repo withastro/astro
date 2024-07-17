6975aca738bd485aa230df5d3eff960979358d84
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-07-17 12:36:00 +0200

    feat: better astro:env errors (#11455)

--- packages/astro/src/env/validators.ts
+++ packages/astro/src/env/validators.ts
@@ -1,16 +1,16 @@
 import type { EnumSchema, EnvFieldType, NumberSchema, StringSchema } from './schema.js';
 
 export type ValidationResultValue = EnvFieldType['default'];
+export type ValidationResultErrors = ['missing'] | ['type'] | Array<string>;
 
 type ValidationResult =
 	| {
 			ok: true;
-			type: string;
 			value: ValidationResultValue;
 	  }
 	| {
 			ok: false;
-			type: string;
+			errors: ValidationResultErrors;
 	  };
 
 export function getEnvFieldType(options: EnvFieldType) {
@@ -26,45 +26,50 @@ export function getEnvFieldType(options: EnvFieldType) {
 	return `${type}${optional ? ' | undefined' : ''}`;
 }
 
-type ValueValidator = (input: string | undefined) => {
-	valid: boolean;
-	parsed: ValidationResultValue;
-};
+type ValueValidator = (input: string | undefined) => ValidationResult;
 
 const stringValidator =
 	({ max, min, length, url, includes, startsWith, endsWith }: StringSchema): ValueValidator =>
 	(input) => {
-		let valid = typeof input === 'string';
+		if (typeof input !== 'string') {
+			return {
+				ok: false,
+				errors: ['type'],
+			};
+		}
+		const errors: Array<string> = [];
 
-		if (valid && max !== undefined) {
-			valid = input!.length <= max;
+		if (max !== undefined && !(input.length <= max)) {
+			errors.push('max');
 		}
-		if (valid && min !== undefined) {
-			valid = input!.length >= min;
+		if (min !== undefined && !(input.length >= min)) {
+			errors.push('min');
 		}
-		if (valid && length !== undefined) {
-			valid = input!.length === length;
+		if (length !== undefined && !(input.length === length)) {
+			errors.push('length');
 		}
-		if (valid && url !== undefined) {
-			try {
-				new URL(input!);
-			} catch (_) {
-				valid = false;
-			}
+		if (url !== undefined && !URL.canParse(input)) {
+			errors.push('url');
 		}
-		if (valid && includes !== undefined) {
-			valid = input!.includes(includes);
+		if (includes !== undefined && !input.includes(includes)) {
+			errors.push('includes');
 		}
-		if (valid && startsWith !== undefined) {
-			valid = input!.startsWith(startsWith);
+		if (startsWith !== undefined && !input.startsWith(startsWith)) {
+			errors.push('startsWith');
 		}
-		if (valid && endsWith !== undefined) {
-			valid = input!.endsWith(endsWith);
+		if (endsWith !== undefined && !input.endsWith(endsWith)) {
+			errors.push('endsWith');
 		}
 
+		if (errors.length > 0) {
+			return {
+				ok: false,
+				errors,
+			};
+		}
 		return {
-			valid,
-			parsed: input,
+			ok: true,
+			value: input,
 		};
 	};
 
@@ -72,45 +77,71 @@ const numberValidator =
 	({ gt, min, lt, max, int }: NumberSchema): ValueValidator =>
 	(input) => {
 		const num = parseFloat(input ?? '');
-		let valid = !isNaN(num);
+		if (isNaN(num)) {
+			return {
+				ok: false,
+				errors: ['type'],
+			};
+		}
+		const errors: Array<string> = [];
 
-		if (valid && gt !== undefined) {
-			valid = num > gt;
+		if (gt !== undefined && !(num > gt)) {
+			errors.push('gt');
 		}
-		if (valid && min !== undefined) {
-			valid = num >= min;
+		if (min !== undefined && !(num >= min)) {
+			errors.push('min');
 		}
-		if (valid && lt !== undefined) {
-			valid = num < lt;
+		if (lt !== undefined && !(num < lt)) {
+			errors.push('lt');
 		}
-		if (valid && max !== undefined) {
-			valid = num <= max;
+		if (max !== undefined && !(num <= max)) {
+			errors.push('max');
 		}
-		if (valid && int !== undefined) {
+		if (int !== undefined) {
 			const isInt = Number.isInteger(num);
-			valid = int ? isInt : !isInt;
+			if (!(int ? isInt : !isInt)) {
+				errors.push('int');
+			}
 		}
 
+		if (errors.length > 0) {
+			return {
+				ok: false,
+				errors,
+			};
+		}
 		return {
-			valid,
-			parsed: num,
+			ok: true,
+			value: num,
 		};
 	};
 
 const booleanValidator: ValueValidator = (input) => {
 	const bool = input === 'true' ? true : input === 'false' ? false : undefined;
+	if (typeof bool !== 'boolean') {
+		return {
+			ok: false,
+			errors: ['type'],
+		};
+	}
 	return {
-		valid: typeof bool === 'boolean',
-		parsed: bool,
+		ok: true,
+		value: bool,
 	};
 };
 
 const enumValidator =
 	({ values }: EnumSchema): ValueValidator =>
 	(input) => {
+		if (!(typeof input === 'string' ? values.includes(input) : false)) {
+			return {
+				ok: false,
+				errors: ['type'],
+			};
+		}
 		return {
-			valid: typeof input === 'string' ? values.includes(input) : false,
-			parsed: input,
+			ok: true,
+			value: input,
 		};
 	};
 
@@ -131,29 +162,19 @@ export function validateEnvVariable(
 	value: string | undefined,
 	options: EnvFieldType
 ): ValidationResult {
-	const validator = selectValidator(options);
-
-	const type = getEnvFieldType(options);
-
-	if (options.optional || options.default !== undefined) {
-		if (value === undefined) {
-			return {
-				ok: true,
-				value: options.default,
-				type,
-			};
-		}
-	}
-	const { valid, parsed } = validator(value);
-	if (valid) {
+	const isOptional = options.optional || options.default !== undefined;
+	if (isOptional && value === undefined) {
 		return {
 			ok: true,
-			value: parsed,
-			type,
+			value: options.default,
 		};
 	}
-	return {
-		ok: false,
-		type,
-	};
+	if (!isOptional && value === undefined) {
+		return {
+			ok: false,
+			errors: ['missing'],
+		};
+	}
+
+	return selectValidator(options)(value);
 }
