6814439d1a9caf035f9098527f7e59aac054479f
Neville Mehta <nmehta@Nevilles-MacBook-Pro.local>
2024-07-18 01:04:47 -0700

    remove await from startServer()

--- packages/integrations/node/test/node-middleware.test.js
+++ packages/integrations/node/test/node-middleware.test.js
@@ -23,7 +23,7 @@ describe('behavior from middleware, standalone', () => {
 		});
 		await fixture.build();
 		const { startServer } = await fixture.loadAdapterEntryModule();
-		let res = await startServer();
+		let res = startServer();
 		server = res.server;
 		await waitServerListen(server.server);
 	});
