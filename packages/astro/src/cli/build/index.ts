acdb567f60e2ffd4b762bc56eaa44cee4d5c7fe2
Matt Kane <m@mk.gg>
2024-07-24 11:05:45 +0100

    Document --force flag

--- packages/astro/src/cli/build/index.ts
+++ packages/astro/src/cli/build/index.ts
@@ -15,6 +15,10 @@ export async function build({ flags }: BuildOptions) {
 			tables: {
 				Flags: [
 					['--outDir <directory>', `Specify the output directory for the build.`],
+					[
+						'--force',
+						'Clear the content layer and content collection cache, forcing a full rebuild.',
+					],
 					['--help (-h)', 'See all available flags.'],
 				],
 			},
