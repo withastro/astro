8d8a32e930083d0b051405220843ba61d2f37dea
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-08-05 16:20:31 +0200

    Discard changes to examples/basics/astro.config.mjs

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,30 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
 
 // https://astro.build/config
-export default defineConfig({
-    output: "server",
-    integrations: [
-        node({
-            mode: "standalone"
-        }),
-        {
-            name: "test",
-            hooks: {
-                "astro:config:done": ({ injectTypes }) => {
-                    injectTypes({
-                        filename: "types.d.ts",
-                        content: "declare const FOO: string;"
-                    })
-                }
-            }
-        }
-    ],
-    experimental: {
-        env: {
-            schema: {}
-        },
-        actions: true
-    }
-});
+export default defineConfig({});
