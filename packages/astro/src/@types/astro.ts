d93e5a95d2c4b62f7376c55dc686bf25389a12d8
Matt Kane <m@mk.gg>
2024-08-08 16:36:03 +0100

    Lint

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -124,7 +124,7 @@ export type TransitionAnimationValue =
 	| TransitionDirectionalAnimations;
 
 // Allow users to extend this for astro-jsx.d.ts
- 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface AstroClientDirectives {}
 
 export interface AstroBuiltinAttributes {
