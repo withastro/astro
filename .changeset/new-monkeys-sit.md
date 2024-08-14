8fe75604df27b6e4758e0aa02cf6076e14933f6c
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2024-08-14 15:23:33 +0200

    extend virtual module astro:transitions/client to exports swapFunctions

---
+++ .changeset/new-monkeys-sit.md
@@ -0,0 +1,5 @@
+---
+'astro': patch
+---
+
+Extends `astro:transitions/client` with `swapFunctions`, which provides building blocks for custom swap functions.
--- packages/astro/src/transitions/swap-functions.ts
+++ packages/astro/src/transitions/swap-functions.ts
@@ -133,6 +133,15 @@ const shouldCopyProps = (el: HTMLElement): boolean => {
 	return persistProps == null || persistProps === 'false';
 };
 
+export const swapFunctions = {
+	deselectScripts,
+	swapRootAttributes,
+	swapHeadElements,
+	swapBodyElement,
+	saveFocus,
+	restoreFocus,
+};
+
 export const swap = (doc: Document) => {
 	deselectScripts(doc);
 	swapRootAttributes(doc);
