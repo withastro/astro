e5732fff84fb6968d374e0a31944018392ab6fbe
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-08-14 13:43:28 +0200

    chore: update compiler

--- packages/language-server/test/typescript/organize-imports.test.ts
+++ packages/language-server/test/typescript/organize-imports.test.ts
@@ -83,4 +83,79 @@ describe('TypeScript - Organize & Sort Imports', () => {
 			},
 		]);
 	});
+
+	it('Can organize imports in files using CRLF', async () => {
+		const document = await languageServer.openFakeDocument(
+			`---\r\n\r\nimport os from "node:os";\r\n\r\nimport fs from "node:fs";\r\n\r\n---\r\n\r\n`,
+			'astro',
+		);
+		const organizeEdits = await languageServer.handle.sendCodeActionsRequest(
+			document.uri,
+			Range.create(6, 0, 6, 0),
+			{
+				diagnostics: [],
+				only: ['source.organizeImports'],
+				triggerKind: 1,
+			},
+		);
+
+		expect(organizeEdits).to.deep.equal([
+			{
+				data: {
+					original: {
+						edit: {
+							documentChanges: [
+								{
+									edits: [
+										{
+											newText: '',
+											range: Range.create(4, 0, 5, 0),
+										},
+										{
+											newText: '',
+											range: Range.create(6, 0, 7, 0),
+										},
+									],
+									textDocument: {
+										uri: URI.from({
+											scheme: 'volar-embedded-content',
+											authority: 'tsx',
+											path: '/' + encodeURIComponent(document.uri),
+										}).toString(),
+										version: null,
+									},
+								},
+							],
+						},
+					},
+					pluginIndex: 3,
+					uri: document.uri,
+					version: (organizeEdits?.[0] as any).data.version,
+				},
+				diagnostics: [],
+				edit: {
+					documentChanges: [
+						{
+							edits: [
+								{
+									newText: '',
+									range: Range.create(2, 0, 3, 0),
+								},
+								{
+									newText: '',
+									range: Range.create(4, 0, 5, 0),
+								},
+							],
+							textDocument: {
+								uri: document.uri,
+								version: null,
+							},
+						},
+					],
+				},
+				kind: 'source.organizeImports',
+				title: 'Organize Imports',
+			},
+		]);
+	});
 });
