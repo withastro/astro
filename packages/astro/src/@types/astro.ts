6f5971d03bc1fd440a68117397f2a21243acb23b
Sarah Rainsberger <sarah@rainsberger.ca>
2024-08-15 08:40:57 -0300

    Apply suggestions from code review
    Co-authored-by: Matt Kane <m@mk.gg>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -2351,7 +2351,7 @@ export interface AstroUserConfig {
 		 *     ```ts ins={3,8} del={7}
 		 *     // src/content/config.ts
 		 *     import { defineCollection, z } from 'astro:content';
-		 *      import { glob } from 'astro/loaders';
+		 *     import { glob } from 'astro/loaders';
 		 *
 		 *     const blog = defineCollection({
 		 *       // For content layer you no longer define a `type`
@@ -2374,8 +2374,8 @@ export interface AstroUserConfig {
 		 *     export async function getStaticPaths() {
 		 *       const posts = await getCollection('blog');
 		 *       return posts.map((post) => ({
-		 *        params: { slug: post.slug },
-		 *        params: { slug: post.id },
+		 *         params: { slug: post.slug },
+		 *         params: { slug: post.id },
 		 *         props: post,
 		 *       }));
 		 *     }
