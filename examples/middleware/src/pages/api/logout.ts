fbc40e111efb6be640b5af526896c6dde432d4d4
@hephaistos_DE <raphael.bossek@gmail.com>
2024-08-16 16:33:14 +0200

    Update examples/middleware/src/pages/api/logout.ts
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- examples/middleware/src/pages/api/logout.ts
+++ examples/middleware/src/pages/api/logout.ts
@@ -1,5 +1,5 @@
-import { type APIRoute } from "astro";
+import type { APIRoute, APIContext } from "astro";
 
-export const GET: APIRoute = async (context) => {
+export const GET: APIRoute = async (context: APIContext) => {
   return new Response(null, { status: 200 });
 };
