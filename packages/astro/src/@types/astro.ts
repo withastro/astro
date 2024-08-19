8129ba8639b48a75afdc21289799ce8330b729fb
Emanuele Stoppa <my.burning@gmail.com>
2024-08-19 15:04:25 +0100

    Update packages/astro/src/@types/astro.ts

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -2276,7 +2276,7 @@ export interface AstroUserConfig {
 		 * ```
 		 *
 	   * :::note
-		 * Loaders will not automatically [exclude files prefaced with an `_`](/en/guides/routing/#excluding-pages). Use a regular expression such as `pattern: '**/[^_]*.md'` in your loader to to ignore these files.
+		 * Loaders will not automatically [exclude files prefaced with an `_`](/en/guides/routing/#excluding-pages). Use a regular expression such as `pattern: '**/[^_]*.md' in your loader to to ignore these files.
 		 * :::
 		 *
 		 * #### Querying and rendering with the Content Layer API
