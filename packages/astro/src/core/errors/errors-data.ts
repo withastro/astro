d3f9ccb3202e8cf526f3ed3ccc76a19785e8f5d7
Emanuele Stoppa <my.burning@gmail.com>
2024-08-21 10:12:40 +0100

    change deprecation version

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1670,7 +1670,7 @@ export const ActionsWithoutServerOutputError = {
  * - [Actions RFC](https://github.com/withastro/roadmap/blob/actions/proposals/0046-actions.md)
  * @description
  * Action was called from a form using a GET request, but only POST requests are supported. This often occurs if `method="POST"` is missing on the form.
- * @deprecated Deprecated since version 4.15.
+ * @deprecated Deprecated since version 4.14.4.
  */
 export const ActionsUsedWithForGetError = {
 	name: 'ActionsUsedWithForGetError',
