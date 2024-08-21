1724ef045d92ede3a546370c72869290742f4278
bholmesdev <hey@bholmes.dev>
2024-08-21 15:56:41 -0400

    fix: move deprecated notice to type def

--- packages/astro/types/actions.d.ts
+++ packages/astro/types/actions.d.ts
@@ -1,3 +1,7 @@
 declare module 'astro:actions' {
+	export {
+		/** @deprecated Import `z` from `astro:schema` instead. */
+		z,
+	} from 'astro/zod';
 	export * from 'astro/actions/runtime/virtual/server.js';
 }
