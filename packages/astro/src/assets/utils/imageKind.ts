98fbc4115848347eac77bad32ec8f6bf02f047e9
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-08-23 14:01:29 +0200

    fix: improve error for inferSize and Image component

--- packages/astro/src/assets/utils/imageKind.ts
+++ packages/astro/src/assets/utils/imageKind.ts
@@ -1,11 +1,12 @@
 import type { ImageMetadata, UnresolvedImageTransform } from '../types.js';
+import { isRemotePath } from '@astrojs/internal-helpers/path';
 
 export function isESMImportedImage(src: ImageMetadata | string): src is ImageMetadata {
 	return typeof src === 'object';
 }
 
 export function isRemoteImage(src: ImageMetadata | string): src is string {
-	return typeof src === 'string';
+	return typeof src === 'string' && isRemotePath(src);
 }
 
 export async function resolveSrc(src: UnresolvedImageTransform['src']) {
