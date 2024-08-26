3c76860eeec30a6acfe03f00897fc975f1e7db63
bholmesdev <bholmesdev@gmail.com>
2024-08-26 09:17:26 -0400

    feat: remove getActionProps()

--- packages/astro/src/actions/runtime/virtual/shared.ts
+++ packages/astro/src/actions/runtime/virtual/shared.ts
@@ -181,26 +181,6 @@ export function getActionQueryString(name: string) {
 	return `?${searchParams.toString()}`;
 }
 
-/**
- * @deprecated You can now pass action functions
- * directly to the `action` attribute on a form.
- *
- * Example: `<form action={actions.like} />`
- */
-export function getActionProps<T extends (args: FormData) => MaybePromise<unknown>>(action: T) {
-	const params = new URLSearchParams(action.toString());
-	const actionName = params.get('_astroAction');
-	if (!actionName) {
-		// No need for AstroError. `getActionProps()` will be removed for stable.
-		throw new Error('Invalid actions function was passed to getActionProps()');
-	}
-	return {
-		type: 'hidden',
-		name: '_astroAction',
-		value: actionName,
-	} as const;
-}
-
 export type SerializedActionResult =
 	| {
 			type: 'data';
