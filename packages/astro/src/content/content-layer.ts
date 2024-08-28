0e35afe44f5a3c9f87b41dc89d5128b02e448895
Matt Kane <m@mk.gg>
2024-08-28 17:05:56 +0100

    fix: pass config instead of settings to content layer loaders (#11862)
    * fix: pass config instead of settings to content layer loaders
    
    * lint
    
    * changes to changeset from review

--- packages/astro/src/content/content-layer.ts
+++ packages/astro/src/content/content-layer.ts
@@ -3,7 +3,7 @@ import { isAbsolute } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import type { FSWatcher } from 'vite';
 import xxhash from 'xxhash-wasm';
-import type { AstroSettings } from '../@types/astro.js';
+import type { AstroSettings, ContentEntryType } from '../@types/astro.js';
 import { AstroUserError } from '../core/errors/errors.js';
 import type { Logger } from '../core/logger/core.js';
 import {
@@ -14,7 +14,12 @@ import {
 } from './consts.js';
 import type { LoaderContext } from './loaders/types.js';
 import type { MutableDataStore } from './mutable-data-store.js';
-import { getEntryDataAndImages, globalContentConfigObserver, posixRelative } from './utils.js';
+import {
+	getEntryConfigByExtMap,
+	getEntryDataAndImages,
+	globalContentConfigObserver,
+	posixRelative,
+} from './utils.js';
 
 export interface ContentLayerOptions {
 	store: MutableDataStore;
@@ -118,10 +123,14 @@ export class ContentLayer {
 			store: this.#store.scopedStore(collectionName),
 			meta: this.#store.metaStore(collectionName),
 			logger: this.#logger.forkIntegrationLogger(loaderName),
-			settings: this.#settings,
+			config: this.#settings.config,
 			parseData,
 			generateDigest: await this.#getGenerateDigest(),
 			watcher: this.#watcher,
+			entryTypes: getEntryConfigByExtMap([
+				...this.#settings.contentEntryTypes,
+				...this.#settings.dataEntryTypes,
+			] as Array<ContentEntryType>),
 		};
 	}
 
