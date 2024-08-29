dc9e610035d678d273c8f3716f807dbf0da0142f
Alexander Niebuhr <alexander@nbhr.io>
2024-08-29 08:47:15 +0200

    chore: fix testutils

--- packages/integrations/node/test/test-utils.js
+++ packages/integrations/node/test/test-utils.js
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'node:events';
+import { loadFixture as baseLoadFixture } from '@astrojs/test-utils';
 import httpMocks from 'node-mocks-http';
-import { loadFixture as baseLoadFixture } from '../../../astro/test/test-utils.js';
 
 process.env.ASTRO_NODE_AUTOSTART = 'disabled';
 process.env.ASTRO_NODE_LOGGING = 'disabled';
