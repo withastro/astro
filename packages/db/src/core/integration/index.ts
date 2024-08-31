d2574ad8932039f3eea3bd6d9368bec377a0c334
Luiz Ferraz <luiz@lferraz.com>
2024-08-31 13:29:58 -0300

    Fix linter issues

--- packages/db/src/core/integration/index.ts
+++ packages/db/src/core/integration/index.ts
@@ -2,7 +2,7 @@ import { existsSync } from 'node:fs';
 import { mkdir, writeFile } from 'node:fs/promises';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { type ManagedAppToken, getManagedAppTokenOrExit } from '@astrojs/studio';
+import type { ManagedAppToken } from '@astrojs/studio';
 import { LibsqlError } from '@libsql/client';
 import type { AstroConfig, AstroIntegration } from 'astro';
 import { blue, yellow } from 'kleur/colors';
