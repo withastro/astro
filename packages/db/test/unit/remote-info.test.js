e130103c756c39b12e0ae508caa59adc7f71340f
Luiz Ferraz <luiz@lferraz.com>
2024-08-31 13:04:42 -0300

    Test env combinations

--- packages/db/test/unit/remote-info.test.js
+++ packages/db/test/unit/remote-info.test.js
@@ -51,68 +51,67 @@ describe('RemoteDatabaseInfo', () => {
 
 describe('RemoteManagedToken', () => {
 	// Avoid conflicts with other tests
-	beforeEach(() => { clearEnvironment(); });
+	beforeEach(() => {
+		clearEnvironment();
+		process.env.ASTRO_STUDIO_APP_TOKEN = 'studio token'
+		process.env.ASTRO_DB_APP_TOKEN = 'db token'
+	});
 	after(() => { clearEnvironment(); });
 
 	test('given token for default remote', async () => {
-		const { token } = await getManagedRemoteToken('some token');
-		assert.equal(token, 'some token');
+		const { token } = await getManagedRemoteToken('given token');
+		assert.equal(token, 'given token');
 	});
 
 	test('token for default remote', async () => {
-		process.env.ASTRO_STUDIO_APP_TOKEN = 'some token'
 		const { token } = await getManagedRemoteToken();
 
-		assert.equal(token, 'some token');
+		assert.equal(token, 'studio token');
 	});
 
 	test('given token for configured Astro Studio remote', async () => {
 		process.env.ASTRO_STUDIO_REMOTE_DB_URL = 'https://studio.astro.build';
-		const { token } = await getManagedRemoteToken('some token');
-		assert.equal(token, 'some token');
+		const { token } = await getManagedRemoteToken('given token');
+		assert.equal(token, 'given token');
 	});
 
 	test('token for configured Astro Studio remote', async () => {
 		process.env.ASTRO_STUDIO_REMOTE_DB_URL = 'https://studio.astro.build';
-		process.env.ASTRO_STUDIO_APP_TOKEN = 'some token'
 		const { token } = await getManagedRemoteToken();
 
-		assert.equal(token, 'some token');
+		assert.equal(token, 'studio token');
 	});
 
 	test('given token for configured libSQL remote', async () => {
 		process.env.ASTRO_DB_REMOTE_URL = 'libsql://libsql.self.hosted';
-		const { token } = await getManagedRemoteToken('some token');
-		assert.equal(token, 'some token');
+		const { token } = await getManagedRemoteToken('given token');
+		assert.equal(token, 'given token');
 	});
 
 	test('token for configured libSQL remote', async () => {
 		process.env.ASTRO_DB_REMOTE_URL = 'libsql://libsql.self.hosted';
-		process.env.ASTRO_DB_APP_TOKEN = 'some token'
 		const { token } = await getManagedRemoteToken();
 
-		assert.equal(token, 'some token');
+		assert.equal(token, 'db token');
 	});
 
 	test('token for given Astro Studio remote', async () => {
 		process.env.ASTRO_DB_REMOTE_URL = 'libsql://libsql.self.hosted';
-		process.env.ASTRO_STUDIO_APP_TOKEN = 'some token'
 		const { token } = await getManagedRemoteToken(undefined, {
 			type: 'studio',
 			url: 'https://studio.astro.build',
 		});
 
-		assert.equal(token, 'some token');
+		assert.equal(token, 'studio token');
 	});
 
 	test('token for given libSQL remote', async () => {
 		process.env.ASTRO_STUDIO_REMOTE_URL = 'libsql://libsql.self.hosted';
-		process.env.ASTRO_DB_APP_TOKEN = 'some token'
 		const { token } = await getManagedRemoteToken(undefined, {
 			type: 'libsql',
 			url: 'libsql://libsql.self.hosted',
 		});
 
-		assert.equal(token, 'some token');
+		assert.equal(token, 'db token');
 	});
 });
