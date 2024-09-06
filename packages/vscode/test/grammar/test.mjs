7e48b9f2e6d19890e28e4b9a35d316f6b0c6ddb7
Alex Recuenco <safale93@gmail.com>
2024-09-06 11:16:36 +0200

    [bugfix] Executing tests on folder with a space on the name doesn't work (#893)
    On windows it requires escaping,
    
    Based on https://www.robvanderwoude.com/escapechars.php#:~:text=In%20batch%20files%2C%20the%20percent,instead%20of%20being%20further%20interpreted.
    
    Before merging this requires checking that it is correct

--- packages/vscode/test/grammar/test.mjs
+++ packages/vscode/test/grammar/test.mjs
@@ -16,7 +16,39 @@ const grammars = readdirSync(grammarDir)
 const allGrammars = [...grammars, ...dummyGrammars];
 
 /**
+ * Escapes command line arguments for Windows
+ * @param {string} arg
+ * @returns {string}
+ *
+ * Escaping sequences obtained trusting https://www.robvanderwoude.com/escapechars.php#:~:text=In%20batch%20files%2C%20the%20percent,instead%20of%20being%20further%20interpreted.
+ */
+function escapeWindowsArg(arg) {
+	// If the argument doesn't contain spaces or special characters, return it as is
+	const single_charac_escape = /(['`,;=()<>|&^])/g;
+	const double_carat_escape = /(!)/g;
+	const percentage_escape = /(%)/g;
+	const backlash_escape = /([\\"[\].*?])/g;
+	if (
+		![single_charac_escape, double_carat_escape, percentage_escape, backlash_escape].some((regex) =>
+			regex.test(arg),
+		)
+	) {
+		return arg;
+	}
+
+	// This one must go first, since it would otherwise conflict with the ! rule
+	arg = arg.replace(single_charac_escape, '^$1');
+	arg = arg.replace(double_carat_escape, '^^$1');
+	arg = arg.replace(percentage_escape, '%$1');
+	arg = arg.replace(backlash_escape, '\\$1');
+
+	// Wrap the entire argument in double quotes
+	return `"${arg}"`;
+}
+
+/**
  * @param  {Parameters<typeof spawn>} arg
+ * @returns {Promise<number | null>}
  */
 function promisifySpawn(...arg) {
 	const childProcess = spawn(...arg);
@@ -34,6 +66,8 @@ function promisifySpawn(...arg) {
 
 async function snapShotTest() {
 	const extraArgs = process.argv.slice(2);
+	const isWindows = process.platform === 'win32';
+
 	const args = [
 		'vscode-tmgrammar-snap',
 		'-s',
@@ -41,14 +75,19 @@ async function snapShotTest() {
 		'./test/grammar/fixtures/**/*.astro',
 		...allGrammars.reduce((/** @type string[] */ previous, path) => [...previous, '-g', path], []),
 		...extraArgs,
-	];
-
-	const code = await promisifySpawn(process.platform === 'win32' ? 'pnpm.cmd' : 'pnpm', args, {
-		stdio: 'inherit',
-		shell: true,
+	].map((arg) => {
+		if (isWindows) return escapeWindowsArg(arg);
+		return arg;
 	});
 
-	if (code > 0) {
+	const code = await promisifySpawn(isWindows ? 'pnpm.cmd' : 'pnpm', args, {
+		stdio: 'inherit',
+		// windows shell escaping on nodejs doesn't work
+		// See https://github.com/nodejs/node/issues/52554 and https://github.com/withastro/language-tools/pull/893
+		shell: isWindows,
+	});
+
+	if (code && code > 0) {
 		process.exit(code);
 	}
 }
