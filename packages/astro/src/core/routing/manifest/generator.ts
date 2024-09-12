f40fede0dde3ab6944f9349d085822fe5d998479
Uwe Jäger <uwe.jaeger@valiton.com>
2024-09-12 23:13:31 +0200

    Remove dependency on path-to-regexp

--- packages/astro/src/core/routing/manifest/generator.ts
+++ packages/astro/src/core/routing/manifest/generator.ts
@@ -1,7 +1,5 @@
 import type { AstroConfig, RoutePart } from '../../../@types/astro.js';
 
-import { compile } from 'path-to-regexp';
-
 /**
  * Sanitizes the parameters object by normalizing string values and replacing certain characters with their URL-encoded equivalents.
  * @param {Record<string, string | number | undefined>} params - The parameters object to be sanitized.
@@ -24,45 +22,39 @@ export function getRouteGenerator(
 	segments: RoutePart[][],
 	addTrailingSlash: AstroConfig['trailingSlash'],
 ) {
-	const template = segments
-		.map((segment) => {
-			return (
-				'/' +
-				segment
-					.map((part) => {
-						if (part.spread) {
-							return `:${part.content.slice(3)}(.*)?`;
-						} else if (part.dynamic) {
-							return `:${part.content}`;
-						} else {
-							return part.content
-								.normalize()
-								.replace(/\?/g, '%3F')
-								.replace(/#/g, '%23')
-								.replace(/%5B/g, '[')
-								.replace(/%5D/g, ']')
-								.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-						}
-					})
-					.join('')
-			);
-		})
-		.join('');
-
-	// Unless trailingSlash config is set to 'always', don't automatically append it.
-	let trailing: '/' | '' = '';
-	if (addTrailingSlash === 'always' && segments.length) {
-		trailing = '/';
-	}
-	const toPath = compile(template + trailing);
 	return (params: Record<string, string | number | undefined>): string => {
 		const sanitizedParams = sanitizeParams(params);
-		const path = toPath(sanitizedParams);
 
-		// When generating an index from a rest parameter route, `path-to-regexp` will return an
-		// empty string instead "/". This causes an inconsistency with static indexes that may result
-		// in the incorrect routes being rendered.
-		// To fix this, we return "/" when the path is empty.
+		// Unless trailingSlash config is set to 'always', don't automatically append it.
+		let trailing: '/' | '' = '';
+		if (addTrailingSlash === 'always' && segments.length) {
+			trailing = '/';
+		}
+
+		const path = segments
+			.map((segment) => {
+				return (
+					'/' +
+					segment
+						.map((part) => {
+							if (part.spread) {
+								return `${sanitizedParams[part.content.slice(3)] || ''}`;
+							} else if (part.dynamic) {
+								return `${sanitizedParams[part.content] || ''}`;
+							} else {
+								return part.content
+									.normalize()
+									.replace(/\?/g, '%3F')
+									.replace(/#/g, '%23')
+									.replace(/%5B/g, '[')
+									.replace(/%5D/g, ']');
+							}
+						})
+						.join('')
+				);
+			})
+			.join('') + trailing;
+
 		return path || '/';
 	};
 }
