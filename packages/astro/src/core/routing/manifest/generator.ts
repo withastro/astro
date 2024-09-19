9be3e1bba789af96d8b21d9c8eca8542cfb4ff77
Uwe Jäger <uwej711@googlemail.com>
2024-09-19 11:11:48 +0200

    Remove dependency on path-to-regexp (#12001)
    Co-authored-by: Erika <3019731+Princesseuh@users.noreply.github.com>

--- packages/astro/src/core/routing/manifest/generator.ts
+++ packages/astro/src/core/routing/manifest/generator.ts
@@ -1,15 +1,11 @@
 import type { AstroConfig, RoutePart } from '../../../@types/astro.js';
 
-import { compile } from 'path-to-regexp';
-
 /**
  * Sanitizes the parameters object by normalizing string values and replacing certain characters with their URL-encoded equivalents.
- * @param {Record<string, string | number | undefined>} params - The parameters object to be sanitized.
- * @returns {Record<string, string | number | undefined>} The sanitized parameters object.
+ * @param {Record<string, string | number>} params - The parameters object to be sanitized.
+ * @returns {Record<string, string | number>} The sanitized parameters object.
  */
-function sanitizeParams(
-	params: Record<string, string | number | undefined>,
-): Record<string, string | number | undefined> {
+function sanitizeParams(params: Record<string, string | number>): Record<string, string | number> {
 	return Object.fromEntries(
 		Object.entries(params).map(([key, value]) => {
 			if (typeof value === 'string') {
@@ -20,49 +16,49 @@ function sanitizeParams(
 	);
 }
 
+function getParameter(part: RoutePart, params: Record<string, string | number>): string | number {
+	if (part.spread) {
+		return params[part.content.slice(3)] || '';
+	}
+
+	if (part.dynamic) {
+		if (!params[part.content]) {
+			throw new TypeError(`Missing parameter: ${part.content}`);
+		}
+
+		return params[part.content];
+	}
+
+	return part.content
+		.normalize()
+		.replace(/\?/g, '%3F')
+		.replace(/#/g, '%23')
+		.replace(/%5B/g, '[')
+		.replace(/%5D/g, ']');
+}
+
+function getSegment(segment: RoutePart[], params: Record<string, string | number>): string {
+	const segmentPath = segment.map((part) => getParameter(part, params)).join('');
+
+	return segmentPath ? '/' + segmentPath : '';
+}
+
 export function getRouteGenerator(
 	segments: RoutePart[][],
 	addTrailingSlash: AstroConfig['trailingSlash'],
 ) {
-	const template = segments
-		.map((segment) => {
-			return (
-				'/' +
-				segment
-					.map((part) => {
-						if (part.spread) {
-							return `:${part.content.slice(3)}(.*)?`;
-						} else if (part.dynamic) {
-							return `:${part.content}`;
-						} else {
-							return part.content
-								.normalize()
-								.replace(/\?/g, '%3F')
-								.replace(/#/g, '%23')
-								.replace(/%5B/g, '[')
-								.replace(/%5D/g, ']')
-								.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-						}
-					})
-					.join('')
-			);
-		})
-		.join('');
-
-	// Unless trailingSlash config is set to 'always', don't automatically append it.
-	let trailing: '/' | '' = '';
-	if (addTrailingSlash === 'always' && segments.length) {
-		trailing = '/';
-	}
-	const toPath = compile(template + trailing);
-	return (params: Record<string, string | number | undefined>): string => {
+	return (params: Record<string, string | number>): string => {
 		const sanitizedParams = sanitizeParams(params);
-		const path = toPath(sanitizedParams);
 
-		// When generating an index from a rest parameter route, `path-to-regexp` will return an
-		// empty string instead "/". This causes an inconsistency with static indexes that may result
-		// in the incorrect routes being rendered.
-		// To fix this, we return "/" when the path is empty.
+		// Unless trailingSlash config is set to 'always', don't automatically append it.
+		let trailing: '/' | '' = '';
+		if (addTrailingSlash === 'always' && segments.length) {
+			trailing = '/';
+		}
+
+		const path =
+			segments.map((segment) => getSegment(segment, sanitizedParams)).join('') + trailing;
+
 		return path || '/';
 	};
 }
