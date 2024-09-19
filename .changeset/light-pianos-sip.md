f4aa42ffe859fd887eaa99dd5f189d9f5a8094c3
bluwy <bjornlu.dev@gmail.com>
2024-09-19 19:48:51 +0800

    Check route collision after getStaticPaths

---
+++ .changeset/light-pianos-sip.md
@@ -0,0 +1,5 @@
+---
+'astro': patch
+---
+
+Handles route collision detection only if it matches `getStaticPaths`
--- packages/astro/src/core/render/params-and-props.ts
+++ packages/astro/src/core/render/params-and-props.ts
@@ -33,12 +33,6 @@ export async function getProps(opts: GetParamsAndPropsOptions): Promise<Props> {
 		return {};
 	}
 
-	// This is a dynamic route, start getting the params
-	const params = getParams(route, pathname);
-	if (mod) {
-		validatePrerenderEndpointCollision(route, mod, params);
-	}
-
 	// During build, the route cache should already be populated.
 	// During development, the route cache is filled on-demand and may be empty.
 	const staticPaths = await callGetStaticPaths({
@@ -49,6 +43,7 @@ export async function getProps(opts: GetParamsAndPropsOptions): Promise<Props> {
 		ssr: serverLike,
 	});
 
+	const params = getParams(route, pathname);
 	const matchedStaticPath = findPathItemByKey(staticPaths, params, route, logger);
 	if (!matchedStaticPath && (serverLike ? route.prerender : true)) {
 		throw new AstroError({
@@ -56,6 +51,10 @@ export async function getProps(opts: GetParamsAndPropsOptions): Promise<Props> {
 			message: AstroErrorData.NoMatchingStaticPathFound.message(pathname),
 			hint: AstroErrorData.NoMatchingStaticPathFound.hint([route.component]),
 		});
+	}
+
+	if (mod) {
+		validatePrerenderEndpointCollision(route, mod, params);
 	}
 
 	const props: Props = matchedStaticPath?.props ? { ...matchedStaticPath.props } : {};
