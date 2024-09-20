daa98ff50bf5d6bf2d5773e125089072a5df80e3
Rohan Godha <git@rohangodha.com>
2024-09-20 15:55:36 -0400

    nits

--- packages/astro/src/content/loaders/file.ts
+++ packages/astro/src/content/loaders/file.ts
@@ -14,9 +14,8 @@ export interface FileOptions {
 
 /**
  * Loads entries from a JSON file. The file must contain an array of objects that contain unique `id` fields, or an object with string keys.
- * @todo Add support for other file types, such as YAML, CSV etc.
  * @param fileName The path to the JSON file to load, relative to the content directory.
- * @param options Additional options for
+ * @param options Additional options for the file loader
  */
 export function file(fileName: string, options?: FileOptions): Loader {
 	if (fileName.includes('*')) {
