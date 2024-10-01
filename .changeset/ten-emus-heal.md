b2e1aa050aca99e480905eaf8b03715426a88c9b
bluwy <bjornlu.dev@gmail.com>
2024-10-01 15:19:40 +0800

    Update changeset

--- .changeset/ten-emus-heal.md
+++ .changeset/ten-emus-heal.md
@@ -1,5 +1,22 @@
 ---
-'astro': patch
+'astro': minor
 ---
 
-add support parallel builds
+Adds the `build.concurreny` to specify the number of pages to build in parallel. In most cases, you should stick with the default value of `1`,
+and batch or cache long running tasks like fetch calls or data access to improve the overall rendering time.
+
+Use this option only if the refactors are not possible. If the number is set too high, the page rendering
+may slow down due to insufficient memory resources and because JS is single-threaded.
+
+In the future, Astro may reuse this option to render pages concurrently with mutliple threads.
+
+```js
+// astro.config.mjs
+import { defineConfig } from 'astro';
+
+export default defineConfig({
+  build: {
+    concurrency: 2,
+  },
+});
+```
--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1100,9 +1100,11 @@ export interface AstroUserConfig {
 		 * @type {boolean}
 		 * @default `1`
 		 * @description
-		 * The number of pages to build in parallel. While JS is single-threaded, this option may be helpful
-		 * if the pages take a sizable time to render but aren't performing resource-intensive tasks, for example,
-		 * due to network fetch calls or accessing external data.
+		 * The number of pages to build in parallel. In most cases, you should stick with the default value of `1`,
+		 * and batch or cache long running tasks like fetch calls or data access to improve the overall rendering time.
+		 *
+		 * Use this option only if the refactors are not possible. If the number is set too high, the page rendering
+		 * may slow down due to insufficient memory resources and because JS is single-threaded.
 		 *
 		 * In the future, Astro may reuse this option to render pages concurrently with mutliple threads.
 		 *
