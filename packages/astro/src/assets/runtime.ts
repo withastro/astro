72c561d134a5129aae73add0fff942081a8c458a
Michael Stramel <m.stramel89@gmail.com>
2024-10-01 23:39:39 -0500

    add comment explaining the code for backwards compat

--- packages/astro/src/assets/runtime.ts
+++ packages/astro/src/assets/runtime.ts
@@ -44,10 +44,12 @@ export function createSvgComponent({ meta, attributes, children }: SvgComponentP
 	makeNonEnumerable(Component);
 
 	if (import.meta.env.DEV) {
+		// Maintaining the current `console.log` output for SVG imports
 		Object.defineProperty(Component, Symbol.for('nodejs.util.inspect.custom'), {
 			value: (_: any, opts: any, inspect: any) => inspect(meta, opts),
 		});
 	}
 
+	// Attaching the metadata to the component to maintain current functionality
 	return Object.assign(Component, meta);
 }
