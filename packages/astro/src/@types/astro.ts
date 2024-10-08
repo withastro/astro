2a88d1d2ac2ae83014dd470e0eacc269a178a224
Emanuele Stoppa <my.burning@gmail.com>
2024-10-08 14:58:31 +0100

    Apply suggestions from code review
    Co-authored-by: Sarah Rainsberger <sarah@rainsberger.ca>

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1097,14 +1097,15 @@ export interface AstroUserConfig {
 		/**
 		 * @docs
 		 * @name build.concurrency
-		 * @type {boolean}
+		 * @type { number }
 		 * @default `1`
 		 * @description
-		 * The number of pages to build in parallel. In most cases, you should stick with the default value of `1`,
-		 * and batch or cache long running tasks like fetch calls or data access to improve the overall rendering time.
-		 *
-		 * Use this option only if the refactors are not possible. If the number is set too high, the page rendering
-		 * may slow down due to insufficient memory resources and because JS is single-threaded.
+		 * The number of pages to build in parallel.
+		 * 
+		 * **In most cases, you should not change the default value of `1`.**
+		 * 
+		 * Use this option only when other attempts to reduce the overall rendering time (e.g. batch or cache long running tasks like fetch calls or data access) are not possible or are insufficient.
+		 * If the number is set too high, page rendering may slow down due to insufficient memory resources and because JS is single-threaded.
 		 *
 		 * In the future, Astro may reuse this option to render pages concurrently with multiple threads.
 		 *
