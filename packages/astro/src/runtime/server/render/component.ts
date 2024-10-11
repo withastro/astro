1cf5bab08a2aabf46b60403be787fd7b8e02a3a8
Vladislav Lipatov <lipatov.xex@gmail.com>
2024-10-11 09:50:25 +0700

    Remove renderer check call if there's only one renderer

--- packages/astro/src/runtime/server/render/component.ts
+++ packages/astro/src/runtime/server/render/component.ts
@@ -131,9 +131,6 @@ async function renderFrameworkComponent(
 			// we can skip the `check` calls and use that renderer
 			if (renderers.length === 1) {
 				renderer = renderers[0];
-
-				// Still call `check` because some renderers store state during the check phase that they expect when `renderToStaticMarkup` is called.
-				await renderer.ssr.check.call({ result }, Component, props, children)
 			} else {
 				let error;
 
