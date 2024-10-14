07290bd66521c95a66eac7358c5b9f0d8bb9506d
bluwy <bjornlu.dev@gmail.com>
2024-10-14 15:37:49 +0800

    ahhhhhhhh

--- packages/astro/test/units/dev/restart.test.js
+++ packages/astro/test/units/dev/restart.test.js
@@ -11,14 +11,6 @@ import { createFixture, createRequestAndResponse } from '../test-utils.js';
 /** @type {import('astro').AstroInlineConfig} */
 const defaultInlineConfig = {
 	logLevel: 'silent',
-	vite: {
-		server: {
-			watch: {
-				useFsEvents: true,
-				usePolling: true,
-			},
-		},
-	},
 };
 
 function isStarted(container) {
@@ -61,6 +53,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 			// Create an error
 			let restartComplete = restart.restarted();
 			await fixture.writeFile('/astro.config.mjs', 'const foo = bar');
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/astro.config.mjs').replace(/\\/g, '/'),
+			);
 
 			// Wait for the restart to finish
 			let hmrError = await restartComplete;
@@ -70,6 +67,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 
 			restartComplete = restart.restarted();
 			await fixture.writeFile('/astro.config.mjs', 'const foo = bar2');
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/astro.config.mjs').replace(/\\/g, '/'),
+			);
 
 			hmrError = await restartComplete;
 			assert.ok(hmrError instanceof Error);
@@ -104,6 +106,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 			// Trigger a change
 			let restartComplete = restart.restarted();
 			await fixture.writeFile('/astro.config.mjs', '');
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/astro.config.mjs').replace(/\\/g, '/'),
+			);
 			await restartComplete;
 
 			assert.equal(isStarted(restart.container), true);
@@ -131,6 +138,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 			// Trigger a change
 			let restartComplete = restart.restarted();
 			await fixture.writeFile('/astro.config.ts', '');
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/astro.config.mjs').replace(/\\/g, '/'),
+			);
 			await restartComplete;
 
 			assert.equal(isStarted(restart.container), true);
@@ -156,6 +168,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 		try {
 			let restartComplete = restart.restarted();
 			await fixture.writeFile('/package.json', `{}`);
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/package.json').replace(/\\/g, '/'),
+			);
 			await restartComplete;
 		} finally {
 			await restart.container.close();
@@ -203,6 +220,11 @@ describe('dev container restarts', { timeout: 20000 }, () => {
 		try {
 			let restartComplete = restart.restarted();
 			await fixture.writeFile('/.astro/settings.json', `{ }`);
+			// TODO: fix this hack
+			restart.container.viteServer.watcher.emit(
+				'change',
+				fixture.getPath('/.astro/settings.json').replace(/\\/g, '/'),
+			);
 			await restartComplete;
 		} finally {
 			await restart.container.close();
