cdf0afa08c2ecb910e8c4657df36bee2c184b9e5
Emanuele Stoppa <my.burning@gmail.com>
2024-10-15 11:04:01 +0100

    Update packages/astro/test/fixtures/middleware-no-user-middleware/integration-middleware-url.js
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/astro/test/fixtures/middleware-no-user-middleware/integration-middleware-url.js
+++ packages/astro/test/fixtures/middleware-no-user-middleware/integration-middleware-url.js
@@ -2,11 +2,7 @@ import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
 	if (context.url.pathname === '/url') {
-		return new Response(JSON.stringify({ post: 'works' }), {
-			headers: {
-				'content-type': 'application/json',
-			},
-		});
+		return Response.json({ post: 'works' });
 	}
 
 	return next();
