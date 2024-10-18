c6c98ffc5a50bb54738926dec540ff5c430784c3
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-10-18 15:20:21 +0200

    chore: improve types tests

--- packages/astro/test/types/define-config.ts
+++ packages/astro/test/types/define-config.ts
@@ -5,35 +5,37 @@ import { expectTypeOf } from 'expect-type';
 
 describe('defineConfig()', () => {
 	it('Infers generics correctly', () => {
-		expectTypeOf(defineConfig({})).toEqualTypeOf<AstroUserDefineConfig<never>>();
+		const config_0 = defineConfig({});
+		expectTypeOf(config_0).toEqualTypeOf<AstroUserDefineConfig<never>>();
+		expectTypeOf(config_0.i18n!.defaultLocale).toEqualTypeOf<string>();
 
-		expectTypeOf(
-			defineConfig({
-				i18n: {
-					locales: ['en'],
-					defaultLocale: 'en',
-				},
-			}),
-		).toEqualTypeOf<AstroUserDefineConfig<['en']>>();
+		const config_1 = defineConfig({
+			i18n: {
+				locales: ['en'],
+				defaultLocale: 'en',
+			},
+		});
+		expectTypeOf(config_1).toEqualTypeOf<AstroUserDefineConfig<['en']>>();
+		expectTypeOf(config_1.i18n!.defaultLocale).toEqualTypeOf<'en'>();
 
-		expectTypeOf(
-			defineConfig({
-				i18n: {
-					locales: ['en', 'fr'],
-					defaultLocale: 'fr',
-				},
-			}),
-		).toEqualTypeOf<AstroUserDefineConfig<['en', 'fr']>>();
+		const config_2 = defineConfig({
+			i18n: {
+				locales: ['en', 'fr'],
+				defaultLocale: 'fr',
+			},
+		});
+		expectTypeOf(config_2).toEqualTypeOf<AstroUserDefineConfig<['en', 'fr']>>();
+		expectTypeOf(config_2.i18n!.defaultLocale).toEqualTypeOf<'en' | 'fr'>();
 
-		expectTypeOf(
-			defineConfig({
-				i18n: {
-					locales: ['en', { path: 'french', codes: ['fr', 'fr-FR'] }],
-					defaultLocale: 'en',
-				},
-			}),
-		).toEqualTypeOf<
+		const config_3 = defineConfig({
+			i18n: {
+				locales: ['en', { path: 'french', codes: ['fr', 'fr-FR'] }],
+				defaultLocale: 'en',
+			},
+		});
+		expectTypeOf(config_3).toEqualTypeOf<
 			AstroUserDefineConfig<['en', { readonly path: 'french'; readonly codes: ['fr', 'fr-FR'] }]>
 		>();
+		expectTypeOf(config_3.i18n!.defaultLocale).toEqualTypeOf<'en' | 'fr' | 'fr-FR'>();
 	});
 });
