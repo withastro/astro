0457437d6f60f82b17547b071a5e307e50b851ef
bholmesdev <hey@bholmes.dev>
2024-11-05 13:32:56 -0500

    test: remove middleware handler

--- packages/astro/e2e/fixtures/actions-blog/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware((ctx, next) => {
-	if (ctx.request.method === "GET" && ctx.url.pathname === "/sum") {
-		return next("/rewritten")
-	}
-	
-	return next()
-})
