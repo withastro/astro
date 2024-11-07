ca1cba3d9ff42319531d39bbf60bc086c4933a64
apatel369 <ap.patel1893@gmail.com>
2024-11-06 20:08:30 -0600

    fix issue with scripts in `injectRoute` shared entrypoint

--- packages/astro/src/core/build/plugins/plugin-hoisted-scripts.ts
+++ packages/astro/src/core/build/plugins/plugin-hoisted-scripts.ts
@@ -1,8 +1,7 @@
 import type { BuildOptions, Rollup, Plugin as VitePlugin } from 'vite';
 import type { AstroSettings } from '../../../@types/astro.js';
-import { viteID } from '../../util.js';
 import type { BuildInternals } from '../internal.js';
-import { getPageDataByViteID } from '../internal.js';
+import { getPageDatasByHoistedScriptId } from '../internal.js';
 import type { AstroBuildPlugin } from '../plugin.js';
 import type { StaticBuildOptions } from '../types.js';
 import { shouldInlineAsset } from './util.js';
@@ -73,23 +72,18 @@ export function vitePluginHoistedScripts(
 					shouldInlineAsset(output.code, output.fileName, assetsInlineLimit);
 				let removeFromBundle = false;
 				const facadeId = output.facadeModuleId!;
-				const pages = internals.hoistedScriptIdToPagesMap.get(facadeId)!;
-				for (const pathname of pages) {
-					const vid = viteID(new URL('.' + pathname, settings.config.root));
-					const pageInfo = getPageDataByViteID(internals, vid);
-					if (pageInfo) {
-						if (canBeInlined) {
-							pageInfo.hoistedScript = {
-								type: 'inline',
-								value: output.code,
-							};
-							removeFromBundle = true;
-						} else {
-							pageInfo.hoistedScript = {
-								type: 'external',
-								value: id,
-							};
-						}
+				for (const pageData of getPageDatasByHoistedScriptId(internals, facadeId)) {
+					if (canBeInlined) {
+						pageData.hoistedScript = {
+							type: 'inline',
+							value: output.code,
+						};
+						removeFromBundle = true;
+					} else {
+						pageData.hoistedScript = {
+							type: 'external',
+							value: id,
+						};
 					}
 				}
 
