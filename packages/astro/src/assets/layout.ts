fcfb64ce3ebcdb7829251cc680a9e964c401a030
Matt Kane <m@mk.gg>
2024-11-08 14:52:30 +0000

    Correct order

--- packages/astro/src/assets/layout.ts
+++ packages/astro/src/assets/layout.ts
@@ -2,33 +2,33 @@ import type { ImageLayout } from '../types/public/index.js';
 
 // Common screen widths. These will be filtered according to the image size and layout
 export const DEFAULT_RESOLUTIONS = [
-	6016, // 6K
-	5120, // 5K
-	4480, // 4.5K
-	3840, // 4K
-	3200, // QHD+
-	2560, // WQXGA
-	2048, // QXGA
-	1920, // 1080p
-	1668, // Various iPads
-	1280, // 720p
-	1080, // iPhone 6-8 Plus
-	960, // older horizontal phones
-	828, // iPhone XR/11
-	750, // iPhone 6-8
 	640, // older and lower-end phones
+	750, // iPhone 6-8
+	828, // iPhone XR/11
+	960, // older horizontal phones
+	1080, // iPhone 6-8 Plus
+	1280, // 720p
+	1668, // Various iPads
+	1920, // 1080p
+	2048, // QXGA
+	2560, // WQXGA
+	3200, // QHD+
+	3840, // 4K
+	4480, // 4.5K
+	5120, // 5K
+	6016, // 6K
 ];
 
 // A more limited set of screen widths, for statically generated images
 export const LIMITED_RESOLUTIONS = [
-	2560, // WQXGA
-	2048, // QXGA
-	1668, // Various iPads
-	1280, // 720p
-	1080, // iPhone 6-8 Plus
-	828, // iPhone XR/11
-	750, // iPhone 6-8
 	640, // older and lower-end phones
+	750, // iPhone 6-8
+	828, // iPhone XR/11
+	1080, // iPhone 6-8 Plus
+	1280, // 720p
+	1668, // Various iPads
+	2048, // QXGA
+	2560, // WQXGA
 ];
 
 /**
