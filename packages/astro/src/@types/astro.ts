471e7127a3a92f3008a71aa25a093e5f9b309176
Sarah Rainsberger <sarah11918@users.noreply.github.com>
2024-11-11 13:50:33 +0000

    [ci] format

--- packages/astro/src/@types/astro.ts
+++ packages/astro/src/@types/astro.ts
@@ -1439,47 +1439,47 @@ export interface AstroUserConfig {
 		 * @typeraw {Partial<ShikiConfig>}
 		 * @description
 		 * Shiki is our default syntax highlighter. You can configure all options via the `markdown.shikiConfig` object:
-	   * 
+		 *
 		 * ```js title="astro.config.mjs"
-	   * import { defineConfig } from 'astro/config';
-		 * 
-	   * export default defineConfig({
+		 * import { defineConfig } from 'astro/config';
+		 *
+		 * export default defineConfig({
 		 *   markdown: {
-	   *     shikiConfig: {
+		 *     shikiConfig: {
 		 *       // Choose from Shiki's built-in themes (or add your own)
-     *       // https://shiki.style/themes
-     *       theme: 'dracula',
-     *       // Alternatively, provide multiple themes
-     *       // See note below for using dual light/dark themes
-     *       themes: {
-     *         light: 'github-light',
-     *         dark: 'github-dark',
-     *       },
-     *       // Disable the default colors
-     *       // https://shiki.style/guide/dual-themes#without-default-color
-     *       // (Added in v4.12.0)
-     *       defaultColor: false,
-     *       // Add custom languages
-     *       // Note: Shiki has countless langs built-in, including .astro!
-     *       // https://shiki.style/languages
-     *       langs: [],
-     *       // Add custom aliases for languages
-     *       // Map an alias to a Shiki language ID: https://shiki.style/languages#bundled-languages
-     *       // https://shiki.style/guide/load-lang#custom-language-aliases
-     *       langAlias: {
-     *         cjs: "javascript"
-     *       },
-     *       // Enable word wrap to prevent horizontal scrolling
-     *       wrap: true,
-     *       // Add custom transformers: https://shiki.style/guide/transformers
-     *       // Find common transformers: https://shiki.style/packages/transformers
-     *       transformers: [],
-     *     },
-     *   },
-     * });
-     * ```
-     *
-	   * See the [code syntax highlighting guide](/en/guides/syntax-highlighting/) for usage and examples.
+		 *       // https://shiki.style/themes
+		 *       theme: 'dracula',
+		 *       // Alternatively, provide multiple themes
+		 *       // See note below for using dual light/dark themes
+		 *       themes: {
+		 *         light: 'github-light',
+		 *         dark: 'github-dark',
+		 *       },
+		 *       // Disable the default colors
+		 *       // https://shiki.style/guide/dual-themes#without-default-color
+		 *       // (Added in v4.12.0)
+		 *       defaultColor: false,
+		 *       // Add custom languages
+		 *       // Note: Shiki has countless langs built-in, including .astro!
+		 *       // https://shiki.style/languages
+		 *       langs: [],
+		 *       // Add custom aliases for languages
+		 *       // Map an alias to a Shiki language ID: https://shiki.style/languages#bundled-languages
+		 *       // https://shiki.style/guide/load-lang#custom-language-aliases
+		 *       langAlias: {
+		 *         cjs: "javascript"
+		 *       },
+		 *       // Enable word wrap to prevent horizontal scrolling
+		 *       wrap: true,
+		 *       // Add custom transformers: https://shiki.style/guide/transformers
+		 *       // Find common transformers: https://shiki.style/packages/transformers
+		 *       transformers: [],
+		 *     },
+		 *   },
+		 * });
+		 * ```
+		 *
+		 * See the [code syntax highlighting guide](/en/guides/syntax-highlighting/) for usage and examples.
 		 */
 		shikiConfig?: Partial<ShikiConfig>;
 
