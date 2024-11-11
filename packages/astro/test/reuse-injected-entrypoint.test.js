1a576a66e645b39e3f2e1bb2e1a9100054b298b1
apatel369 <ap.patel1893@gmail.com>
2024-11-11 17:12:54 -0600

    fix tests

--- packages/astro/test/reuse-injected-entrypoint.test.js
+++ packages/astro/test/reuse-injected-entrypoint.test.js
@@ -8,47 +8,42 @@ const routes = [
 		description: 'matches / to index.astro',
 		url: '/',
 		h1: 'index.astro',
-		hasScript: false,
 	},
 	{
 		description: 'matches /injected-a to to-inject.astro',
 		url: '/injected-a',
 		h1: 'to-inject.astro',
-		hasScript: true,
+		scriptContent: 'console.log("to-inject.astro");',
 	},
 	{
 		description: 'matches /injected-b to to-inject.astro',
 		url: '/injected-b',
 		h1: 'to-inject.astro',
-		hasScript: true,
+	  scriptContent: 'console.log("to-inject.astro");',
 	},
 	{
 		description: 'matches /dynamic-a/id-1 to [id].astro',
 		url: '/dynamic-a/id-1',
 		h1: '[id].astro',
 		p: 'id-1',
-		hasScript: false,
 	},
 	{
 		description: 'matches /dynamic-a/id-2 to [id].astro',
 		url: '/dynamic-a/id-2',
 		h1: '[id].astro',
 		p: 'id-2',
-		hasScript: false,
 	},
 	{
 		description: 'matches /dynamic-b/id-1 to [id].astro',
 		url: '/dynamic-b/id-1',
 		h1: '[id].astro',
 		p: 'id-1',
-		hasScript: false,
 	},
 	{
 		description: 'matches /dynamic-b/id-2 to [id].astro',
 		url: '/dynamic-b/id-2',
 		h1: '[id].astro',
 		p: 'id-2',
-		hasScript: false,
 	},
 ];
 
@@ -67,37 +62,44 @@ describe('Reuse injected entrypoint', () => {
 			await fixture.build();
 		});
 
-		routes.forEach(({ description, url, fourOhFour, h1, p, htmlMatch, hasScript  }) => {
-			const isEndpoint = htmlMatch && !h1 && !p;
+		routes.forEach(
+			({ description, url, fourOhFour, h1, p, htmlMatch, scriptContent }) => {
+				const isEndpoint = htmlMatch && !h1 && !p;
 
-			it(description, async () => {
-				const htmlFile = isEndpoint ? url : `${appendForwardSlash(url)}index.html`;
+				it(description, async () => {
+					const htmlFile = isEndpoint ? url : `${appendForwardSlash(url)}index.html`;
 
-				if (fourOhFour) {
-					assert.equal(fixture.pathExists(htmlFile), false);
-					return;
-				}
+					if (fourOhFour) {
+						assert.equal(fixture.pathExists(htmlFile), false);
+						return;
+					}
 
-				const html = await fixture.readFile(htmlFile);
-				const $ = cheerioLoad(html);
+					const html = await fixture.readFile(htmlFile);
+					const $ = cheerioLoad(html);
 
-				if (h1) {
-					assert.equal($('h1').text(), h1);
-				}
+					if (h1) {
+						assert.equal($('h1').text(), h1);
+					}
 
-				if (p) {
-					assert.equal($('p').text(), p);
-				}
+					if (p) {
+						assert.equal($('p').text(), p);
+					}
 
-				if (htmlMatch) {
-					assert.equal(html, htmlMatch);
-				}
+					if (htmlMatch) {
+						assert.equal(html, htmlMatch);
+					}
 
-				if (hasScript) {
-					assert.equal($('script[type="module"]').length, 1, 'should have client-side script');
-				}
-			});
-		});
+					if (scriptContent) {
+						const scriptTags = $('script[type="module"]').toArray();
+						const scriptFound = scriptTags.some((script) => {
+							const scriptText = $(script).text();
+							return scriptText.includes(scriptContent.trim());
+						});
+						assert(scriptFound, `Expected script content to be injected in SSG ${url}`);
+					}
+				});
+			},
+		);
 	});
 
 	describe('dev', () => {
@@ -116,33 +118,40 @@ describe('Reuse injected entrypoint', () => {
 			await devServer.stop();
 		});
 
-		routes.forEach(({ description, url, fourOhFour, h1, p, htmlMatch }) => {
-			// checks URLs as written above
-			it(description, async () => {
-				const html = await fixture.fetch(url).then((res) => res.text());
-				const $ = cheerioLoad(html);
+		routes.forEach(
+			({ description, url, fourOhFour, h1, p, htmlMatch, scriptContent }) => {
+				// checks URLs as written above
+				it(description, async () => {
+					const html = await fixture.fetch(url).then((res) => res.text());
+					const $ = cheerioLoad(html);
 
-				if (fourOhFour) {
-					assert.equal($('title').text(), '404: Not Found');
-					return;
-				}
+					if (fourOhFour) {
+						assert.equal($('title').text(), '404: Not Found');
+						return;
+					}
 
-				if (h1) {
-					assert.equal($('h1').text(), h1);
-				}
+					if (h1) {
+						assert.equal($('h1').text(), h1);
+					}
 
-				if (p) {
-					assert.equal($('p').text(), p);
-				}
+					if (p) {
+						assert.equal($('p').text(), p);
+					}
 
-				if (htmlMatch) {
-					assert.equal(html, htmlMatch);
-				}
+					if (htmlMatch) {
+						assert.equal(html, htmlMatch);
+					}
 
-				if (hasScript) {
-					assert.equal($('script[type="module"]').length, 1, 'should have client-side script');
-				}
-			});
-		});
+					if (scriptContent) {
+						const scriptTags = $('script[type="module"]').toArray();
+						const scriptFound = scriptTags.some((script) => {
+							const scriptSrc = $(script).attr('src');
+							return scriptSrc && scriptSrc.includes('/to-inject.astro?astro&type=script&index=0&lang.ts');
+						});
+						assert(scriptFound, `Expected script content to be injected in dev ${url}`);
+					}
+				});
+			},
+		);
 	});
 });
