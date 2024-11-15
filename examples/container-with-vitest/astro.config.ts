f155f384427eda397ef46c1d6be68d4ecb20ba81
Matt Kane <m@mk.gg>
2024-11-15 16:17:34 +0000

    Remove example

--- examples/container-with-vitest/astro.config.ts
+++ examples/container-with-vitest/astro.config.ts
@@ -3,8 +3,5 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	session: {
-		driver: "lruCache",
-	}
+	integrations: [react()]
 });
