c17d691da97d6599203caf0da766e289e4ba6c08
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-11-18 15:59:25 +0100

    Discard changes to packages/astro/src/core/build/plugins/plugin-pages.ts

--- packages/astro/src/core/build/plugins/plugin-pages.ts
+++ packages/astro/src/core/build/plugins/plugin-pages.ts
@@ -50,7 +50,7 @@ function vitePluginPages(opts: StaticBuildOptions, internals: BuildInternals): V
 						imports.push(`import { renderers } from "${RENDERERS_MODULE_ID}";`);
 						exports.push(`export { renderers };`);
 
-						return `/* patch secret here */  ${imports.join('\n')}${exports.join('\n')}`;
+						return `${imports.join('\n')}${exports.join('\n')}`;
 					}
 				}
 			}
