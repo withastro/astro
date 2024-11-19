f5569e8556987eb27a8bf032fe3717f878fd0c55
Aron Homberg <info@aron-homberg.de>
2024-11-19 04:24:24 +0100

    chore: removed the example and updated the changeset accordingly

--- examples/framework-custom/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// in a real-world scenario, this would be provided a separate package
-import customFrameworkRenderer from './src/custom-renderer';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [customFrameworkRenderer()]
-});
