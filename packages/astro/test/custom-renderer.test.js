77674506d0ce0c00723502088c46e5c9f1c2aac8
Aron Homberg <info@aron-homberg.de>
2024-11-19 05:30:01 +0100

    test: added fixture and test to prove the new behaviour

---
+++ packages/astro/test/custom-renderer.test.js
@@ -0,0 +1,69 @@
+import assert from 'node:assert/strict';
+import { after, before, describe, it } from 'node:test';
+import * as cheerio from 'cheerio';
+import { loadFixture } from './test-utils.js';
+
+describe('Custom Renderer - SSR', () => {
+	let fixture;
+	let warnLogsWritten = '';
+
+	// store the original console.warn function
+	const originalConsoleWarnFn = console.warn;
+
+	before(async () => {
+		fixture = await loadFixture({
+			root: './fixtures/custom-renderer/',
+		});
+
+		// temporarily override console.warn to capture the warning message
+		console.warn = (message) => {
+			warnLogsWritten += message;
+			originalConsoleWarnFn(message);
+		};
+	});
+
+	after(() => {
+		console.warn = originalConsoleWarnFn;
+	});
+
+	describe('dev', () => {
+		let devServer;
+		let $;
+
+		before(async () => {
+			devServer = await fixture.startDevServer();
+		});
+
+		after(async () => {
+			await devServer.stop();
+		});
+
+		it('renders /', async () => {
+			const html = await fixture.fetch('/').then((res) => res.text());
+			$ = cheerio.load(html);
+			assert.equal($('h1').text(), 'Client Directives');
+		});
+
+		it('renders SSR custom renderer functional components as expected', async () => {
+			const res = await fixture.fetch('/');
+			assert.equal(res.status, 200);
+
+			const html = await res.text();
+			$ = cheerio.load(html);
+
+			assert.equal($('p').length, 5);
+		});
+
+		it('does not log a warning message when using a custom renderer', async () => {
+			const res = await fixture.fetch('/');
+			assert.equal(res.status, 200);
+
+			assert.equal(
+				warnLogsWritten.indexOf(
+					'If you intended to use a different renderer, please provide a valid client:only directive.',
+				),
+				-1,
+			);
+		});
+	});
+});
