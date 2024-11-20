9fbe27b4901500aacd22832b55ec1df3ff78da5f
Matt Kane <m@mk.gg>
2024-11-20 10:45:46 +0000

    wip: implement sessions (#12478)
    * feat: implement sessions
    
    * Add middleware
    
    * Action middleware test

--- packages/astro/src/vite-plugin-astro-server/plugin.ts
+++ packages/astro/src/vite-plugin-astro-server/plugin.ts
@@ -174,5 +174,6 @@ export function createDevelopmentManifest(settings: AstroSettings): SSRManifest 
 				onRequest: NOOP_MIDDLEWARE_FN,
 			};
 		},
+		sessionConfig: settings.config.experimental.session,
 	};
 }
---
+++ packages/astro/test/fixtures/sessions/src/actions/index.ts
@@ -0,0 +1,28 @@
+// src/actions/addToCart.ts
+import { defineAction } from 'astro:actions';
+import { z } from 'astro:schema';
+
+export const server = {
+	addToCart: defineAction({
+		accept: 'form',
+		input: z.object({ productId: z.string() }),
+		handler: async (input, context) => {
+			const cart: Array<string> = (await context.session.get('cart')) || [];
+			cart.push(input.productId);
+			await context.session.set('cart', cart);
+			return { cart, message: 'Product added to cart at ' + new Date().toTimeString() };
+		},
+	}),
+	getCart: defineAction({
+		handler: async (input, context) => {
+			return await context.session.get('cart');
+		},
+	}),
+	clearCart: defineAction({
+		accept: 'json',
+		handler: async (input, context) => {
+			await context.session.set('cart', []);
+			return {cart: [], message: 'Cart cleared at ' + new Date().toTimeString() };
+		},
+	}),
+};
