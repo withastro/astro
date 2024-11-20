315cd063379e8659e7d4b4254a2e7fc7530a3c54
Matt Kane <m@mk.gg>
2024-11-20 10:44:29 +0000

    Action middleware test

--- packages/astro/test/fixtures/sessions/src/actions/index.ts
+++ packages/astro/test/fixtures/sessions/src/actions/index.ts
@@ -1,30 +1,28 @@
 // src/actions/addToCart.ts
-import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
+import { defineAction } from 'astro:actions';
+import { z } from 'astro:schema';
 
 export const server = {
-  addToCart: defineAction({
+	addToCart: defineAction({
 		accept: 'form',
-    input: z.object({ productId: z.string() }),
-    handler: async (input, context) => {
-			console.log('input', input);
-      const cart = await context.session.get("cart") || [];
-			console.log('cart', cart);
-      cart.push(input.productId);
-      await context.session.set("cart", cart);
-      return cart;
-    },
-  }),
+		input: z.object({ productId: z.string() }),
+		handler: async (input, context) => {
+			const cart: Array<string> = (await context.session.get('cart')) || [];
+			cart.push(input.productId);
+			await context.session.set('cart', cart);
+			return { cart, message: 'Product added to cart at ' + new Date().toTimeString() };
+		},
+	}),
 	getCart: defineAction({
 		handler: async (input, context) => {
-			return await context.session.get("cart");
+			return await context.session.get('cart');
 		},
 	}),
 	clearCart: defineAction({
 		accept: 'json',
 		handler: async (input, context) => {
-			await context.session.set("cart", []);
-			return [];
+			await context.session.set('cart', []);
+			return {cart: [], message: 'Cart cleared at ' + new Date().toTimeString() };
 		},
 	}),
 };
