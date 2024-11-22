9ee71940cad1cd1d9d320243bb90ae9e97f1d9dc
Emanuele Stoppa <my.burning@gmail.com>
2024-11-22 09:46:22 +0000

    fix(routing): don't access Request headers

--- packages/astro/src/core/middleware/noop-middleware.ts
+++ packages/astro/src/core/middleware/noop-middleware.ts
@@ -1,7 +1,8 @@
 import type { MiddlewareHandler } from '../../@types/astro.js';
 import { NOOP_MIDDLEWARE_HEADER } from '../constants.js';
 
-export const NOOP_MIDDLEWARE_FN: MiddlewareHandler = (ctx, next) => {
-	ctx.request.headers.set(NOOP_MIDDLEWARE_HEADER, 'true');
-	return next();
+export const NOOP_MIDDLEWARE_FN: MiddlewareHandler = async (_ctx, next) => {
+	const response = await next();
+	response.headers.set(NOOP_MIDDLEWARE_HEADER, 'true');
+	return response;
 };
