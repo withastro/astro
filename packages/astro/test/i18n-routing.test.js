10fdf3d23be757432f971738f9174f77f3c8e16b
mtwilliams <ematipico@users.noreply.github.com>
2024-12-05 06:24:19 +0000

    [ci] format

--- packages/astro/test/i18n-routing.test.js
+++ packages/astro/test/i18n-routing.test.js
@@ -1,6 +1,6 @@
-import * as cheerio from 'cheerio';
 import * as assert from 'node:assert/strict';
 import { after, afterEach, before, describe, it } from 'node:test';
+import * as cheerio from 'cheerio';
 import testAdapter from './test-adapter.js';
 import { loadFixture } from './test-utils.js';
 
