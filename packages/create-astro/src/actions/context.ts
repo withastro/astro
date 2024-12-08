8a6bcbde23e5c31c5fb1320f07aa02c024e71dae
apatel369 <ap.patel1893@gmail.com>
2024-12-08 14:26:54 -0600

    format

--- packages/create-astro/src/actions/context.ts
+++ packages/create-astro/src/actions/context.ts
@@ -34,9 +34,12 @@ export interface Context {
 }
 
 export async function getContext(argv: string[]): Promise<Context> {
-	const packageSpecifier = argv.find((argItem) => /^(astro|create-astro)@/.exec(argItem))?.split('@')[1];
+	const packageSpecifier = argv
+		.find((argItem) => /^(astro|create-astro)@/.exec(argItem))
+		?.split('@')[1];
 	// Fallback to 'latest' if it's a version number
-	const packageTag = packageSpecifier && /^v?\d[^a-zA-Z]*$/.test(packageSpecifier) ? 'latest' : packageSpecifier;
+	const packageTag =
+		packageSpecifier && /^v?\d[^a-zA-Z]*$/.test(packageSpecifier) ? 'latest' : packageSpecifier;
 
 	const flags = arg(
 		{
