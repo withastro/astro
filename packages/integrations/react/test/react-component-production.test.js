8ca95817212b8e580539b62662f911a2fb1814f6
jeroenpg <jeroenpg@gmail.com>
2024-12-14 18:11:00 +0100

    Added isWindows import

--- packages/integrations/react/test/react-component-production.test.js
+++ packages/integrations/react/test/react-component-production.test.js
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict';
 import { before, describe, it } from 'node:test';
 import { load as cheerioLoad } from 'cheerio';
-import { loadFixture } from '../../../astro/test/test-utils.js';
+import { isWindows, loadFixture } from '../../../astro/test/test-utils.js';
 
 let fixture;
 
