b3072038815d75da3c77804d181641eef8aaf357
Colin Bate <colin@colinbate.com>
2024-12-29 01:17:50 -0400

    Add support for TOML frontmatter in Markdown files.

---
+++ .changeset/lazy-pandas-love.md
@@ -0,0 +1,7 @@
+---
+'@astrojs/markdown-remark': minor
+---
+
+Add support for TOML frontmatter in .md and .mdx files
+
+Uses the standard +++ as a delimiter
--- packages/markdown/remark/test/frontmatter.test.js
+++ packages/markdown/remark/test/frontmatter.test.js
@@ -5,7 +5,7 @@ import { extractFrontmatter, parseFrontmatter } from '../dist/index.js';
 const bom = '\uFEFF';
 
 describe('extractFrontmatter', () => {
-	it('works', () => {
+	it('handles YAML', () => {
 		const yaml = `\nfoo: bar\n`;
 		assert.equal(extractFrontmatter(`---${yaml}---`), yaml);
 		assert.equal(extractFrontmatter(`${bom}---${yaml}---`), yaml);
@@ -19,10 +19,25 @@ describe('extractFrontmatter', () => {
 		assert.equal(extractFrontmatter(`---${yaml} ---`), undefined);
 		assert.equal(extractFrontmatter(`text\n---${yaml}---\n\ncontent`), undefined);
 	});
+
+	it('handles TOML', () => {
+		const toml = `\nfoo = "bar"\n`;
+		assert.equal(extractFrontmatter(`+++${toml}+++`), toml);
+		assert.equal(extractFrontmatter(`${bom}+++${toml}+++`), toml);
+		assert.equal(extractFrontmatter(`\n+++${toml}+++`), toml);
+		assert.equal(extractFrontmatter(`\n  \n+++${toml}+++`), toml);
+		assert.equal(extractFrontmatter(`+++${toml}+++\ncontent`), toml);
+		assert.equal(extractFrontmatter(`${bom}+++${toml}+++\ncontent`), toml);
+		assert.equal(extractFrontmatter(`\n\n+++${toml}+++\n\ncontent`), toml);
+		assert.equal(extractFrontmatter(`\n  \n+++${toml}+++\n\ncontent`), toml);
+		assert.equal(extractFrontmatter(` +++${toml}+++`), undefined);
+		assert.equal(extractFrontmatter(`+++${toml} +++`), undefined);
+		assert.equal(extractFrontmatter(`text\n+++${toml}+++\n\ncontent`), undefined);
+	});
 });
 
 describe('parseFrontmatter', () => {
-	it('works', () => {
+	it('works for YAML', () => {
 		const yaml = `\nfoo: bar\n`;
 		assert.deepEqual(parseFrontmatter(`---${yaml}---`), {
 			frontmatter: { foo: 'bar' },
@@ -81,7 +96,66 @@ describe('parseFrontmatter', () => {
 		});
 	});
 
-	it('frontmatter style', () => {
+	it('works for TOML', () => {
+		const toml = `\nfoo = "bar"\n`;
+		assert.deepEqual(parseFrontmatter(`+++${toml}+++`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '',
+		});
+		assert.deepEqual(parseFrontmatter(`${bom}+++${toml}+++`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: bom,
+		});
+		assert.deepEqual(parseFrontmatter(`\n+++${toml}+++`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '\n',
+		});
+		assert.deepEqual(parseFrontmatter(`\n  \n+++${toml}+++`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '\n  \n',
+		});
+		assert.deepEqual(parseFrontmatter(`+++${toml}+++\ncontent`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '\ncontent',
+		});
+		assert.deepEqual(parseFrontmatter(`${bom}+++${toml}+++\ncontent`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: `${bom}\ncontent`,
+		});
+		assert.deepEqual(parseFrontmatter(`\n\n+++${toml}+++\n\ncontent`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '\n\n\n\ncontent',
+		});
+		assert.deepEqual(parseFrontmatter(`\n  \n+++${toml}+++\n\ncontent`), {
+			frontmatter: { foo: 'bar' },
+			rawFrontmatter: toml,
+			content: '\n  \n\n\ncontent',
+		});
+		assert.deepEqual(parseFrontmatter(` +++${toml}+++`), {
+			frontmatter: {},
+			rawFrontmatter: '',
+			content: ` +++${toml}+++`,
+		});
+		assert.deepEqual(parseFrontmatter(`+++${toml} +++`), {
+			frontmatter: {},
+			rawFrontmatter: '',
+			content: `+++${toml} +++`,
+		});
+		assert.deepEqual(parseFrontmatter(`text\n+++${toml}+++\n\ncontent`), {
+			frontmatter: {},
+			rawFrontmatter: '',
+			content: `text\n+++${toml}+++\n\ncontent`,
+		});
+	});
+
+	it('frontmatter style for YAML', () => {
 		const yaml = `\nfoo: bar\n`;
 		const parse1 = (style) => parseFrontmatter(`---${yaml}---`, { frontmatter: style }).content;
 		assert.deepEqual(parse1('preserve'), `---${yaml}---`);
@@ -94,6 +168,22 @@ describe('parseFrontmatter', () => {
 		assert.deepEqual(parse2('preserve'), `\n  \n---${yaml}---\n\ncontent`);
 		assert.deepEqual(parse2('remove'), '\n  \n\n\ncontent');
 		assert.deepEqual(parse2('empty-with-spaces'), `\n  \n   \n        \n   \n\ncontent`);
+		assert.deepEqual(parse2('empty-with-lines'), `\n  \n\n\n\n\ncontent`);
+	});
+
+	it('frontmatter style for TOML', () => {
+		const toml = `\nfoo = "bar"\n`;
+		const parse1 = (style) => parseFrontmatter(`+++${toml}+++`, { frontmatter: style }).content;
+		assert.deepEqual(parse1('preserve'), `+++${toml}+++`);
+		assert.deepEqual(parse1('remove'), '');
+		assert.deepEqual(parse1('empty-with-spaces'), `   \n           \n   `);
+		assert.deepEqual(parse1('empty-with-lines'), `\n\n`);
+
+		const parse2 = (style) =>
+			parseFrontmatter(`\n  \n+++${toml}+++\n\ncontent`, { frontmatter: style }).content;
+		assert.deepEqual(parse2('preserve'), `\n  \n+++${toml}+++\n\ncontent`);
+		assert.deepEqual(parse2('remove'), '\n  \n\n\ncontent');
+		assert.deepEqual(parse2('empty-with-spaces'), `\n  \n   \n           \n   \n\ncontent`);
 		assert.deepEqual(parse2('empty-with-lines'), `\n  \n\n\n\n\ncontent`);
 	});
 });
