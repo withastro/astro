8b482f66435644465d931b2ae207bbcba2f2cdab
Louis Escher <louisescher@proton.me>
2025-01-05 09:29:22 +0100

    Add passthrough to zod schema to still access other properties after validation

--- packages/astro/src/content/utils.ts
+++ packages/astro/src/content/utils.ts
@@ -43,11 +43,18 @@ const entryTypeSchema = z
 				invalid_type_error: 'Content entry `id` must be a string',
 				// Default to empty string so we can validate properly in the loader
 			}),
-	});
+	}).passthrough();
 
 export const loaderReturnSchema = z.union([
 	z.array(entryTypeSchema),
-	z.record(z.string(), z.object({ id: z.string({ invalid_type_error: 'Content entry `id` must be a string' }).optional() })),
+	z.record(
+		z.string(), 
+		z.object({ 
+			id: z.string({ 
+				invalid_type_error: 'Content entry `id` must be a string' 
+			}).optional() 
+		}).passthrough()
+	),
 ]);
 
 const collectionConfigParser = z.union([
