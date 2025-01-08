6be0664b1372ff50f2a3cb85f51a401ccd9abcb8
Matt Kane <m@mk.gg>
2025-01-08 16:10:58 +0000

    fix: invalidate data store module when dev server starts

--- packages/astro/src/content/vite-plugin-content-virtual-mod.ts
+++ packages/astro/src/content/vite-plugin-content-virtual-mod.ts
@@ -46,6 +46,17 @@ interface AstroContentVirtualModPluginParams {
 	fs: typeof nodeFs;
 }
 
+function invalidateDataStore(server: ViteDevServer) {
+	const module = server.moduleGraph.getModuleById(RESOLVED_DATA_STORE_VIRTUAL_ID);
+	if (module) {
+		server.moduleGraph.invalidateModule(module);
+	}
+	server.ws.send({
+		type: 'full-reload',
+		path: '*',
+	});
+}
+
 export function astroContentVirtualModPlugin({
 	settings,
 	fs,
@@ -60,7 +71,9 @@ export function astroContentVirtualModPlugin({
 		},
 		buildStart() {
 			// We defer adding the data store file to the watcher until the server is ready
-			devServer?.watcher.add(fileURLToPath(dataStoreFile));
+			devServer.watcher.add(fileURLToPath(dataStoreFile));
+			// Manually invalidate the data store to avoid a race condition in file watching
+			invalidateDataStore(devServer);
 		},
 		async resolveId(id) {
 			if (id === VIRTUAL_MODULE_ID) {
@@ -164,29 +177,17 @@ export function astroContentVirtualModPlugin({
 		configureServer(server) {
 			devServer = server;
 			const dataStorePath = fileURLToPath(dataStoreFile);
-
-			function invalidateDataStore() {
-				const module = server.moduleGraph.getModuleById(RESOLVED_DATA_STORE_VIRTUAL_ID);
-				if (module) {
-					server.moduleGraph.invalidateModule(module);
-				}
-				server.ws.send({
-					type: 'full-reload',
-					path: '*',
-				});
-			}
-
 			// If the datastore file changes, invalidate the virtual module
 
 			server.watcher.on('add', (addedPath) => {
 				if (addedPath === dataStorePath) {
-					invalidateDataStore();
+					invalidateDataStore(server);
 				}
 			});
 
 			server.watcher.on('change', (changedPath) => {
 				if (changedPath === dataStorePath) {
-					invalidateDataStore();
+					invalidateDataStore(server);
 				}
 			});
 		},
