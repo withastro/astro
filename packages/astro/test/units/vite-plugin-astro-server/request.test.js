8e9218b24efeec97d2b8a5a575410f97d7bb3c25
Emanuele Stoppa <my.burning@gmail.com>
2025-01-24 11:16:54 +0000

    Update packages/astro/test/units/vite-plugin-astro-server/request.test.js
    Co-authored-by: Matt Kane <m@mk.gg>

--- packages/astro/test/units/vite-plugin-astro-server/request.test.js
+++ packages/astro/test/units/vite-plugin-astro-server/request.test.js
@@ -42,7 +42,6 @@ describe('vite-plugin-astro-server', () => {
 				{
 					loader: createLoader({
 						import(id) {
-							console.log(id);
 							if (id === '\0astro-internal:middleware') {
 								return { onRequest: (_, next) => next() };
 							}
