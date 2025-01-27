5e1886629ebd666d95661ce91ed41b5e62c3f032
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-01-27 13:53:00 +0100

    Discard changes to packages/integrations/tailwind/test/fixtures/basic/astro.config.js

--- packages/integrations/tailwind/test/fixtures/basic/astro.config.js
+++ packages/integrations/tailwind/test/fixtures/basic/astro.config.js
@@ -1,11 +1,12 @@
-// @ts-check
+import { fileURLToPath } from 'node:url';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [
     tailwind({
-      configFile: "./tailwind.config.js"
+      configFile: fileURLToPath(new URL('./tailwind.config.js', import.meta.url)),
+      nesting: true
     }),
   ]
 });
