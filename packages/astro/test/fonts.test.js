5546c8dc5c7a7e5c7a2f0e6168bff409ead77382
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-02-04 16:19:23 +0100

    feat: improve tests

--- packages/astro/test/fonts.test.js
+++ packages/astro/test/fonts.test.js
@@ -9,37 +9,64 @@ describe('astro:fonts', () => {
 	/** @type {import('./test-utils.js').DevServer} */
 	let devServer;
 
-	before(async () => {
-		fixture = await loadFixture({
-			root: './fixtures/fonts/',
-			// experimental: {
-			// 	fonts: {
-			// 		families: [
-			// 			{
-			// 				name: 'Roboto',
-			// 				provider: 'google',
-			// 			},
-			// 		],
-			// 	},
-			// },
-		});
-		devServer = await fixture.startDevServer();
-	});
-
-	after(async () => {
-		await devServer.stop();
-	});
-
 	describe('<Fonts /> component', () => {
 		// TODO: remove once fonts are stabilized
-		it('Throws an error if fonts are not enabled', async () => {
-			const res = await fixture.fetch('/');
-			const body = await res.text();
-			assert.equal(
-				body.includes('<script type="module" src="/@vite/client">'),
-				true,
-				'Body does not include Vite error overlay script',
-			);
+		describe('Fonts are not enabled', () => {
+			before(async () => {
+				fixture = await loadFixture({
+					root: './fixtures/fonts/',
+				});
+				devServer = await fixture.startDevServer();
+			});
+
+			after(async () => {
+				await devServer.stop();
+			});
+
+			it('Throws an error if fonts are not enabled', async () => {
+				const res = await fixture.fetch('/');
+				const body = await res.text();
+				assert.equal(
+					body.includes('<script type="module" src="/@vite/client">'),
+					true,
+					'Body does not include Vite error overlay script',
+				);
+			});
+		});
+
+		describe('Fonts are enabled', () => {
+			before(async () => {
+				fixture = await loadFixture({
+					root: './fixtures/fonts/',
+					experimental: {
+						fonts: {
+							families: [
+								{
+									name: 'Roboto',
+									provider: 'google',
+								},
+							],
+						},
+					},
+				});
+				devServer = await fixture.startDevServer();
+			});
+
+			after(async () => {
+				await devServer.stop();
+			});
+
+			it('Includes styles', async () => {
+				const res = await fixture.fetch('/');
+				const body = await res.text();
+				assert.equal(body.includes('<style>'), true);
+			});
+
+			it('Includes links when preloading', async () => {
+				const res = await fixture.fetch('/preload');
+				const body = await res.text();
+				assert.equal(body.includes('<link rel="preload"'), true);
+			});
 		});
 	});
 });
