d72dba73c666d5a0a68ed83681bb821b306ed441
Matthew Lymer <mlymer@verticalscope.com>
2025-02-06 16:40:50 -0500

    Make the tests good, even

--- packages/astro/test/units/render/rendering.test.js
+++ packages/astro/test/units/render/rendering.test.js
@@ -2,6 +2,7 @@ import * as assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import { isPromise } from 'node:util/types';
 import { renderTemplate, createComponent, renderComponent } from '../../../dist/runtime/server/index.js';
+import * as cheerio from 'cheerio';
 
 function renderToString(item) {
 	if (isPromise(item)) {
@@ -25,41 +26,55 @@ function renderToString(item) {
 	return result;
 }
 
+function getRenderingOrder(html) {
+	return cheerio
+		.load(html, null, false)("*")
+		.map((_, node) => node.attribs["id"])
+		.toArray();
+}
+
 describe('rendering', () => {
-	describe('expressions', () => {
-		it('components are evaluated depth first', async () => {
-			const evaluated = [];
+	it('components are evaluated depth first', async () => {
+		const evaluated = [];
 
-			const Root = createComponent((_, props) => {
-				evaluated.push(props.id);
-				return renderTemplate`<root>
-					${renderComponent({}, "", Scalar, {id: `${props.id}/scalarA`})}
-					${renderComponent({}, "", Nested, {id: `${props.id}/nested`})}
-					${renderComponent({}, "", Scalar, {id: `${props.id}/scalarB`})}
-				</root>`;
-			});
-
-			const Scalar = createComponent((_, props) => {
-				evaluated.push(props.id);
-				return renderTemplate`<scalar id="${props.id}" />`;
-			});
-
-			const Nested = createComponent((_, props) => {
-				evaluated.push(props.id);
-				return renderTemplate`<nested id="${props.id}">
-					${renderComponent({}, "", Scalar, {id: `${props.id}/scalar`})}
-				</nested>`;
-			});
-
-			const result = await renderToString(Root({}, {id: "root"}, {}));
-			
-			assert.deepEqual(evaluated, [
-				"root",
-				"root/scalarA",
-				"root/nested",
-				"root/scalarB",
-				"root/nested/scalar"
-			]);
+		const Root = createComponent((_, props) => {
+			evaluated.push(props.id);
+			return renderTemplate`<poon id="${props.id}">
+				${renderComponent({}, "", Scalar, {id: `${props.id}/scalarA`})}
+				${renderComponent({}, "", Nested, {id: `${props.id}/nested`})}
+				${renderComponent({}, "", Scalar, {id: `${props.id}/scalarB`})}
+			</poon>`;
 		});
+
+		const Scalar = createComponent((_, props) => {
+			evaluated.push(props.id);
+			return renderTemplate`<scalar id="${props.id}"></scalar>`;
+		});
+
+		const Nested = createComponent((_, props) => {
+			evaluated.push(props.id);
+			return renderTemplate`<nested id="${props.id}">
+				${renderComponent({}, "", Scalar, {id: `${props.id}/scalar`})}
+			</nested>`;
+		});
+
+		const result = await renderToString(Root({}, {id: "root"}, {}));
+		const rendered = getRenderingOrder(result);
+
+		assert.deepEqual(rendered, [
+			"root",
+			"root/scalarA",
+			"root/nested",
+			"root/nested/scalar",			
+			"root/scalarB",
+		]);
+
+		assert.deepEqual(evaluated, [
+			"root",
+			"root/scalarA",
+			"root/nested",
+			"root/nested/scalar",			
+			"root/scalarB",
+		]);
 	});
 });
