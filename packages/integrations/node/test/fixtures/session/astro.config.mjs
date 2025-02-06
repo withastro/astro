4c3ded859c017f7358db9d42853be1815699f3c3
Matt Kane <m@mk.gg>
2025-02-06 15:09:50 +0000

    Add node test fixture

---
+++ packages/integrations/node/test/fixtures/session/astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+
+export default defineConfig({
+    output: 'server',
+    adapter: node({ mode: 'standalone' }),
+		experimental: {
+			session: true
+		}
+});
