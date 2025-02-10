385b447aa1d25eb74e40a2eb58c6e522ed67b083
Slawek Kolodziej <hfrntt@gmail.com>
2025-02-10 20:41:01 +0100

    feat(vercel): allow defining ISR exclusions with regular expressions

--- packages/integrations/vercel/test/fixtures/isr/astro.config.mjs
+++ packages/integrations/vercel/test/fixtures/isr/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 		isr: {
 			bypassToken: "1c9e601d-9943-4e7c-9575-005556d774a8",
 			expiration: 120,
-			exclude: ["/two", "/excluded/[dynamic]", "/excluded/[...rest]"]
+			exclude: ["/two", "/excluded/[dynamic]", "/excluded/[...rest]", /^\/api/]
 		}
 	})
 });
