b6227eac05fb7370a1d894e857039c98d47af45a
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-02-12 14:58:15 +0100

    Discard changes to examples/basics/astro.config.mjs

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -2,21 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    experimental: {
-        fonts: {
-            families: [
-                "Roboto",
-                {
-                    name: "Test",
-                    provider: "local",
-                    src: [
-                        {
-                            paths: ["./src/test.woff"]
-                        }
-                    ]
-                }
-            ]
-        }
-    }
-});
+export default defineConfig({});
