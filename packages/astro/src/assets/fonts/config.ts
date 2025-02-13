0a45984ef7ed5a6a9090e07f8d99bd028fb987b4
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-02-13 14:58:54 +0100

    feat: dedupe

--- packages/astro/src/assets/fonts/config.ts
+++ packages/astro/src/assets/fonts/config.ts
@@ -1,8 +1,25 @@
 import { z } from 'zod';
 
+function dedupe<T>(arr: Array<T>): Array<T> {
+	return [...new Set(arr)];
+}
+
 export const resolveFontOptionsSchema = z.object({
-	weights: z.array(z.string()).nonempty(),
-	styles: z.array(z.enum(['normal', 'italic', 'oblique'])).nonempty(),
-	subsets: z.array(z.string()).nonempty(),
-	fallbacks: z.array(z.string()).nonempty().optional(),
+	weights: z
+		.array(z.string())
+		.nonempty()
+		.transform((arr) => dedupe(arr)),
+	styles: z
+		.array(z.enum(['normal', 'italic', 'oblique']))
+		.nonempty()
+		.transform((arr) => dedupe(arr)),
+	subsets: z
+		.array(z.string())
+		.nonempty()
+		.transform((arr) => dedupe(arr)),
+	fallbacks: z
+		.array(z.string())
+		.nonempty()
+		.transform((arr) => dedupe(arr))
+		.optional(),
 });
