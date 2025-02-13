4da2b113d605261607eb8b75d97967d73197bbd0
Matt Kane <m@mk.gg>
2025-02-13 12:19:48 +0000

    Fix test

--- packages/astro/test/api-routes.test.js
+++ packages/astro/test/api-routes.test.js
@@ -30,27 +30,27 @@ describe('API routes', () => {
 			assert.equal(response.status, 403);
 			assert.equal(text, '');
 		});
-	});
 
-	it('should return a 403 status code with the correct body for GET', async () => {
-		const response = await fixture.fetch('/custom-status');
-		const text = await response.text();
-		assert.equal(response.status, 403);
-		assert.equal(text, 'hello world');
-	});
-	
-	it('should return the correct headers for GET', async () => {
-		const response = await fixture.fetch('/custom-status');
-		const headers = response.headers.get("x-hello");
-		assert.equal(headers, 'world');
-	});
+		it('should return a 403 status code with the correct body for GET', async () => {
+			const response = await fixture.fetch('/custom-status');
+			const text = await response.text();
+			assert.equal(response.status, 403);
+			assert.equal(text, 'hello world');
+		});
 
-	it('should return the correct headers for HEAD', async () => {
-		const response = await fixture.fetch('/custom-status', { method: 'HEAD' });
-		const headers = response.headers.get("x-hello");
-		assert.equal(headers, 'world');
+		it('should return the correct headers for GET', async () => {
+			const response = await fixture.fetch('/custom-status');
+			const headers = response.headers.get('x-hello');
+			assert.equal(headers, 'world');
+		});
+
+		it('should return the correct headers for HEAD', async () => {
+			const response = await fixture.fetch('/custom-status', { method: 'HEAD' });
+			const headers = response.headers.get('x-hello');
+			assert.equal(headers, 'world');
+		});
+		after(async () => {
+			await previewServer.stop();
+		});
 	});
-	after(async () => {
-		await previewServer.stop();
-	});	
 });
