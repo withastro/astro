868e2ab9edd363992534728138333161a3900c3f
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-02-20 12:27:51 +0100

    feat(fonts): dev headers (#13275)

--- packages/astro/test/fonts.test.js
+++ packages/astro/test/fonts.test.js
@@ -2,6 +2,7 @@
 import { after, before, describe, it } from 'node:test';
 import { loadFixture } from './test-utils.js';
 import assert from 'node:assert/strict';
+import * as cheerio from 'cheerio';
 
 describe('astro:fonts', () => {
 	/** @type {import('./test-utils.js').Fixture} */
@@ -58,14 +59,35 @@ describe('astro:fonts', () => {
 
 			it('Includes styles', async () => {
 				const res = await fixture.fetch('/');
-				const body = await res.text();
-				assert.equal(body.includes('<style>'), true);
+				const html = await res.text();
+				assert.equal(html.includes('<style>'), true);
 			});
 
 			it('Includes links when preloading', async () => {
 				const res = await fixture.fetch('/preload');
-				const body = await res.text();
-				assert.equal(body.includes('<link rel="preload"'), true);
+				const html = await res.text();
+				assert.equal(html.includes('<link rel="preload"'), true);
+			});
+
+			it('Has correct headers in dev', async () => {
+				let res = await fixture.fetch('/preload');
+				const html = await res.text();
+				const $ = cheerio.load(html);
+				const href = $('link[rel=preload][type^=font/woff2]').attr('href');
+
+				if (!href) {
+					assert.fail();
+				}
+
+				const headers = await fixture.fetch(href).then((r) => r.headers);
+				assert.equal(headers.has('Content-Length'), true);
+				assert.equal(headers.get('Content-Type'), 'font/woff2');
+				assert.equal(
+					headers.get('Cache-Control'),
+					'no-store, no-cache, must-revalidate, max-age=0',
+				);
+				assert.equal(headers.get('Pragma'), 'no-cache');
+				assert.equal(headers.get('Expires'), '0');
 			});
 		});
 	});
