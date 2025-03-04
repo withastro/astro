cf9a990d53aa155c0354de1ead09b38f234da13b
Chris Chua <chris.sirhc@gmail.com>
2025-03-04 20:41:06 +0800

    remove old incorrect doc change

--- packages/astro/src/types/public/config.ts
+++ packages/astro/src/types/public/config.ts
@@ -1255,8 +1255,6 @@ export interface ViteUserConfig extends OriginalViteUserConfig {
 		 *       // Add custom transformers: https://shiki.style/guide/transformers
 		 *       // Find common transformers: https://shiki.style/packages/transformers
 		 *       transformers: [],
-		 *       // Excluded languages from syntax highlighting
-		 *       excludeLangs: [],
 		 *     },
 		 *   },
 		 * });
