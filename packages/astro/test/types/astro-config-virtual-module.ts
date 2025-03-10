b91064354c3622c4c288b86d925f3424a67c3c8d
Emanuele Stoppa <ematipico@users.noreply.github.com>
2025-03-10 15:41:52 +0000

    [ci] format

--- packages/astro/test/types/astro-config-virtual-module.ts
+++ packages/astro/test/types/astro-config-virtual-module.ts
@@ -1,15 +1,18 @@
-import { expectTypeOf } from "expect-type";
-import { describe, it } from "node:test";
-import type { ServerDeserializedManifest, ClientDeserializedManifest } from "../../dist/types/public";
+import { describe, it } from 'node:test';
+import { expectTypeOf } from 'expect-type';
+import type {
+	ClientDeserializedManifest,
+	ServerDeserializedManifest,
+} from '../../dist/types/public';
 
-const server = null as typeof import("astro:config/server")
-const client = null as typeof import("astro:config/client")
+const server = null as typeof import('astro:config/server');
+const client = null as typeof import('astro:config/client');
 
 describe('astro:config', () => {
-    it('astro:config/server', () => {
-        expectTypeOf(server).toEqualTypeOf<ServerDeserializedManifest>();
-    })
-    it('astro:config/client', () => {
-        expectTypeOf(client).toEqualTypeOf<ClientDeserializedManifest>();
-    })
-})
+	it('astro:config/server', () => {
+		expectTypeOf(server).toEqualTypeOf<ServerDeserializedManifest>();
+	});
+	it('astro:config/client', () => {
+		expectTypeOf(client).toEqualTypeOf<ClientDeserializedManifest>();
+	});
+});
