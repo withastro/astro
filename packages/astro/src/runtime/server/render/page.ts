b645ffaaa2cadfeed5c087481c5f164442290d68
Matt Kane <m@mk.gg>
2025-03-18 15:15:30 +0000

    fix: set correct statusText for custom error pages

--- packages/astro/src/runtime/server/render/page.ts
+++ packages/astro/src/runtime/server/render/page.ts
@@ -81,14 +81,18 @@ export async function renderPage(
 		headers.set('Content-Length', body.byteLength.toString());
 	}
 	let status = init.status;
+	let statusText = init.statusText;
 	// Custom 404.astro and 500.astro are particular routes that must return a fixed status code
 	if (route?.route === '/404') {
 		status = 404;
+		statusText = 'Not Found';
 	} else if (route?.route === '/500') {
 		status = 500;
-	}
+		statusText = 'Internal Server Error';		
+	} 
+
 	if (status) {
-		return new Response(body, { ...init, headers, status });
+		return new Response(body, { ...init, headers, status, statusText });
 	} else {
 		return new Response(body, { ...init, headers });
 	}
