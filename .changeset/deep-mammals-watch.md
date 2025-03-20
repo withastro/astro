ecadb6b02e942feccf584547fe9c14d3d1e21ba6
Matt Kane <m@mk.gg>
2025-03-20 11:43:04 +0000

    fix(preact,svelte): empty target container before rendering `client:only` island (#13470)
    * fix(preact,svelte): empty target container before rendering `client:only` island
    
    * Remove log

---
+++ .changeset/deep-mammals-watch.md
@@ -0,0 +1,6 @@
+---
+'@astrojs/preact': patch
+'@astrojs/svelte': patch
+---
+
+Hides fallback content when rendering `client:only` island
--- packages/astro/e2e/client-only.test.js
+++ packages/astro/e2e/client-only.test.js
@@ -20,6 +20,9 @@ test.describe('Client only', () => {
 		const counter = await page.locator('#react-counter');
 		await expect(counter, 'component is visible').toBeVisible();
 
+		const fallback = await page.locator('[data-fallback=react]');
+		await expect(fallback, 'fallback content is hidden').not.toBeVisible();
+
 		const count = await counter.locator('pre');
 		await expect(count, 'initial count is 0').toHaveText('0');
 
@@ -37,6 +40,10 @@ test.describe('Client only', () => {
 
 		const counter = await page.locator('#preact-counter');
 		await expect(counter, 'component is visible').toBeVisible();
+
+		
+		const fallback = await page.locator('[data-fallback=preact]');
+		await expect(fallback, 'fallback content is hidden').not.toBeVisible();
 
 		const count = await counter.locator('pre');
 		await expect(count, 'initial count is 0').toHaveText('0');
@@ -56,6 +63,9 @@ test.describe('Client only', () => {
 		const counter = await page.locator('#solid-counter');
 		await expect(counter, 'component is visible').toBeVisible();
 
+		const fallback = await page.locator('[data-fallback=solid]');
+		await expect(fallback, 'fallback content is hidden').not.toBeVisible();
+
 		const count = await counter.locator('pre');
 		await expect(count, 'initial count is 0').toHaveText('0');
 
@@ -74,6 +84,9 @@ test.describe('Client only', () => {
 		const counter = await page.locator('#vue-counter');
 		await expect(counter, 'component is visible').toBeVisible();
 
+		const fallback = await page.locator('[data-fallback=vue]');
+		await expect(fallback, 'fallback content is hidden').not.toBeVisible();
+
 		const count = await counter.locator('pre');
 		await expect(count, 'initial count is 0').toHaveText('0');
 
@@ -91,6 +104,9 @@ test.describe('Client only', () => {
 
 		const counter = await page.locator('#svelte-counter');
 		await expect(counter, 'component is visible').toBeVisible();
+
+		const fallback = await page.locator('[data-fallback=svelte]');
+		await expect(fallback, 'fallback content is hidden').not.toBeVisible();
 
 		const count = await counter.locator('pre');
 		await expect(count, 'initial count is 0').toHaveText('0');
