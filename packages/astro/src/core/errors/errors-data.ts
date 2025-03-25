2665ebcd44dd84ba21e488c6339afcbad48fbd27
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-03-25 10:46:20 +0100

    Update packages/astro/src/core/errors/errors-data.ts
    Co-authored-by: Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1372,7 +1372,7 @@ export const FontFamilyNotFound = {
 	name: 'FontFamilyNotFound',
 	title: 'Font family not found',
 	message: (family: string) => `No data was found for the "${family}" family.`,
-	hint: 'This is often caused by a typo in the "family" property of the Font component. Review your component usage.',
+	hint: 'This is often caused by a typo in the \`family\` property of the Font component. Check that your component is using a font family you specified in the config.',
 } satisfies ErrorData;
 
 /**
