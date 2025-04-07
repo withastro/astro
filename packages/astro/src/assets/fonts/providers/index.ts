f18239a6f1160d28191acd3b95c6f942daa502c3
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-04-07 11:49:20 +0200

    Update index.ts

--- packages/astro/src/assets/fonts/providers/index.ts
+++ packages/astro/src/assets/fonts/providers/index.ts
@@ -26,7 +26,7 @@ function fontsource() {
 	});
 }
 
-// TODO: https://github.com/unjs/unifont/issues/108
+// TODO: https://github.com/unjs/unifont/issues/108. Once resolved, remove the unifont patch
 // This provider downloads too many files when there's a variable font
 // available. This is bad because it doesn't align with our default font settings
 function google() {
