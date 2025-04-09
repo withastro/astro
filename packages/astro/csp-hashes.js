30ad15596d68ee8189e26372b3e124175909bf2a
Emanuele Stoppa <my.burning@gmail.com>
2025-04-09 09:53:57 +0100

    chore: build hashes of scripts

---
+++ packages/astro/csp-hashes.js
@@ -0,0 +1,17 @@
+// This file is code-generated, please don't change it manually
+export default [
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/client/only.prebuilt.ts
+ "ZR0ZAU8UNTzLmo/ApeWH0y1mVLT+XtFkvZ5nw32W8jI=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/client/media.prebuilt.ts
+ "HDWxd14AUw8OvjrhhRRyyZFHCGnzxXGDrg59Qi8ayhc=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/client/load.prebuilt.ts
+ "XN6a2Vn8uvpBr/WhdYPdK0jVeCzlcOD2XYaP10veV4Y=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/client/visible.prebuilt.ts
+ "cSNmhdbFlyTDRozeu9HPjo+B2S4QAeMp0RO41PqgAcA=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/client/idle.prebuilt.ts
+ "GI/D8grziRZwfj/Mqmn+dcgU/i8sylHSR/IfobqcUT4=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/server/astro-island.prebuilt.ts
+ "mH3H4wSoDVWMXJKrmeBKYJQMdAZQ3dArB2N66JomkzI=",
+	// /Users/ema/www/withastro/astro/packages/astro/src/runtime/server/astro-island.prebuilt-dev.ts
+ "mH3H4wSoDVWMXJKrmeBKYJQMdAZQ3dArB2N66JomkzI="
+];
