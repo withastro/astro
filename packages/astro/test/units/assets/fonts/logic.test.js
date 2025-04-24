9cf802584b339bcbd4d5f3a0088dc77ead8dfa71
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-04-24 17:35:27 +0200

    feat: work on test

--- packages/astro/test/units/assets/fonts/logic.test.js
+++ packages/astro/test/units/assets/fonts/logic.test.js
@@ -6,9 +6,6 @@ import { fakeHasher } from './utils.js';
 
 describe('astro fonts logic', () => {
 	describe('resolveFamily()', () => {
-		// TODO: check local provider
-		// TODO: check deduping
-		// TODO: check remote provider
 		it('removes quotes correctly', async () => {
 			const hasher = { ...fakeHasher, hashObject: () => 'xxx' };
 			let family = await resolveFamily({
@@ -30,9 +27,7 @@ describe('astro fonts logic', () => {
 				},
 				remoteFontProviderResolver: {
 					// @ts-expect-error
-					resolve: async () => ({
-						provider: () => {},
-					}),
+					resolve: async () => ({}),
 				},
 			});
 			assert.equal(family.name, 'Test');
@@ -57,13 +52,130 @@ describe('astro fonts logic', () => {
 				},
 				remoteFontProviderResolver: {
 					// @ts-expect-error
-					resolve: async () => ({
-						provider: () => {},
-					}),
+					resolve: async () => ({}),
 				},
 			});
 			assert.equal(family.name, 'Foo bar');
 			assert.equal(family.nameWithHash, 'Foo bar-xxx');
+		});
+
+		it('resolves local variant correctly', async () => {
+			const family = await resolveFamily({
+				family: {
+					provider: 'local',
+					name: 'Test',
+					cssVariable: '--test',
+					variants: [
+						{
+							weight: '400',
+							style: 'normal',
+							src: ['/'],
+						},
+					],
+				},
+				hasher: fakeHasher,
+				localProviderUrlResolver: {
+					resolve: (url) => url + url,
+				},
+				remoteFontProviderResolver: {
+					// @ts-expect-error
+					resolve: async () => ({}),
+				},
+			});
+			if (family.provider === 'local') {
+				assert.deepStrictEqual(
+					family.variants.map((variant) => variant.src),
+					[[{ url: '//', tech: undefined }]],
+				);
+			} else {
+				assert.fail('Should be a local provider');
+			}
+		});
+
+		it('resolves remote providers', async () => {
+			const provider = () => {};
+			const family = await resolveFamily({
+				family: {
+					provider: {
+						entrypoint: '',
+					},
+					name: 'Test',
+					cssVariable: '--test',
+				},
+				hasher: fakeHasher,
+				localProviderUrlResolver: {
+					resolve: (url) => url,
+				},
+				remoteFontProviderResolver: {
+					// @ts-expect-error
+					resolve: async () => ({
+						provider,
+					}),
+				},
+			});
+			if (family.provider === 'local') {
+				assert.fail('Should be a remote provider');
+			} else {
+				assert.deepStrictEqual(family.provider, { provider });
+			}
+		});
+
+		it('dedupes properly', async () => {
+			let family = await resolveFamily({
+				family: {
+					provider: 'local',
+					name: '"Foo bar"',
+					cssVariable: '--test',
+					variants: [
+						{
+							weight: '400',
+							style: 'normal',
+							src: ['/'],
+						},
+					],
+					fallbacks: ['foo', 'bar', 'foo'],
+				},
+				hasher: fakeHasher,
+				localProviderUrlResolver: {
+					resolve: (url) => url,
+				},
+				remoteFontProviderResolver: {
+					// @ts-expect-error
+					resolve: async () => ({}),
+				},
+			});
+			assert.deepStrictEqual(family.fallbacks, ['foo', 'bar']);
+
+			family = await resolveFamily({
+				family: {
+					provider: { entrypoint: '' },
+					name: '"Foo bar"',
+					cssVariable: '--test',
+					weights: [400, '400', '500', 'bold'],
+					styles: ['normal', 'normal', 'italic'],
+					subsets: ['latin', 'latin'],
+					fallbacks: ['foo', 'bar', 'foo'],
+					unicodeRange: ['abc', 'def', 'abc'],
+				},
+				hasher: fakeHasher,
+				localProviderUrlResolver: {
+					resolve: (url) => url,
+				},
+				remoteFontProviderResolver: {
+					// @ts-expect-error
+					resolve: async () => ({}),
+				},
+			});
+
+			if (family.provider === 'local') {
+				assert.fail('Should be a remote provider');
+			} else {
+				assert.deepStrictEqual(family.weights, ['400', '500', 'bold']);
+				assert.deepStrictEqual(family.styles, ['normal', 'italic']);
+				assert.deepStrictEqual(family.subsets, ['latin']);
+				assert.deepStrictEqual(family.fallbacks, ['foo', 'bar']);
+				assert.deepStrictEqual(family.unicodeRange, ['abc', 'def']);
+			}
 		});
 	});
 });
