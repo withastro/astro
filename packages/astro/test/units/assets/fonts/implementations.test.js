bd10d34fdcaf05b88c685a9c5e7fb8d089eaab18
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-04-24 11:10:19 +0200

    feat: work on test

--- packages/astro/test/units/assets/fonts/implementations.test.js
+++ packages/astro/test/units/assets/fonts/implementations.test.js
@@ -7,6 +7,7 @@ import {
 	renderFontFace,
 	withFamily,
 } from '../../../../dist/assets/fonts/implementations/css-renderer.js';
+import { createDataCollector } from '../../../../dist/assets/fonts/implementations/data-collector.js';
 
 describe('astro fonts implementations', () => {
 	describe('createMinifiableCssRenderer()', () => {
@@ -45,6 +46,51 @@ describe('astro fonts implementations', () => {
 			assert.equal(handleValueWithSpaces('foo'), 'foo');
 			assert.equal(handleValueWithSpaces('x y'), '"x y"');
 		});
+	});
+
+	it('createDataCollector()', () => {
+		/** @type {Map<string, string>} */
+		const map = new Map();
+		/** @type {Array<import('../../../../dist/assets/fonts/types.js').PreloadData>} */
+		const preloadData = [];
+		/** @type {Array<import('../../../../dist/assets/fonts/logic/optimize-fallbacks.js').CollectedFontForMetrics>} */
+		const collectedFonts = [];
+
+		const dataCollector = createDataCollector({
+			hasUrl: (hash) => map.has(hash),
+			saveUrl: (hash, url) => {
+				map.set(hash, url);
+			},
+			savePreload: (preload) => {
+				preloadData.push(preload);
+			},
+			saveFontData: (collected) => {
+				collectedFonts.push(collected);
+			},
+		});
+
+		dataCollector.collect({ hash: 'xxx', originalUrl: 'abc', preload: null, data: {} });
+		dataCollector.collect({
+			hash: 'yyy',
+			originalUrl: 'def',
+			preload: { type: 'woff2', url: 'def' },
+			data: {},
+		});
+		dataCollector.collect({ hash: 'xxx', originalUrl: 'abc', preload: null, data: {} });
+
+		assert.deepStrictEqual(
+			[...map.entries()],
+			[
+				['xxx', 'abc'],
+				['yyy', 'def'],
+			],
+		);
+		assert.deepStrictEqual(preloadData, [{ type: 'woff2', url: 'def' }]);
+		assert.deepStrictEqual(collectedFonts, [
+			{ hash: 'xxx', url: 'abc', data: {} },
+			{ hash: 'yyy', url: 'def', data: {} },
+			{ hash: 'xxx', url: 'abc', data: {} },
+		]);
 	});
 
 	// TODO: cover more implementations
