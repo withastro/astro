bcbfb7740348ffe4f09256c4bb078ee4e9b7acda
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-04-28 11:15:43 +0200

    fix: test

--- packages/astro/test/fixtures/fonts/astro.config.mjs
+++ packages/astro/test/fixtures/fonts/astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    					experimental: {
+						fonts: [
+							{
+								name: 'Roboto',
+								cssVariable: '--font-roboto',
+								provider: fontProviders.fontsource(),
+							},
+						],
+					},
+});
