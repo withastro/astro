d8a6663b541bfe3758c3a54f6ea31ff7e2089161
Emanuele Stoppa <my.burning@gmail.com>
2025-05-15 09:16:10 +0100

    feedback

--- packages/astro/src/core/config/schemas/index.ts
+++ packages/astro/src/core/config/schemas/index.ts
@@ -2,7 +2,6 @@ export {
 	AstroConfigSchema,
 	ASTRO_CONFIG_DEFAULTS,
 	type AstroConfigType,
-	type CspAlgorithm,
 } from './base.js';
 export { createRelativeSchema } from './relative.js';
 export { AstroConfigRefinedSchema } from './refined.js';
