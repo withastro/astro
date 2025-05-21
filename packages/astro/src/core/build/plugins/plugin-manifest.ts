5a6d2aede4b397227be5acecfa9bfefb9a1af0f8
Matt Kane <m@mk.gg>
2025-05-21 16:53:58 +0100

    fix: ponyfill import.meta.resolve (#13833)
    * fix: polyfill import.meta.resolve
    
    * Don't resolve at runtime
    
    * Linit

--- packages/astro/src/core/build/plugins/plugin-manifest.ts
+++ packages/astro/src/core/build/plugins/plugin-manifest.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'node:url';
+import { resolve as importMetaResolve } from 'import-meta-resolve';
 import type { OutputChunk } from 'rollup';
 import { glob } from 'tinyglobby';
+import { builtinDrivers, type BuiltinDriverName } from 'unstorage';
 import type { Plugin as VitePlugin } from 'vite';
 import { getAssetsPrefix } from '../../../assets/utils/getAssetsPrefix.js';
 import { normalizeTheLocale } from '../../../i18n/index.js';
@@ -16,7 +18,6 @@ import { encodeKey } from '../../encryption.js';
 import { fileExtension, joinPaths, prependForwardSlash } from '../../path.js';
 import { DEFAULT_COMPONENTS } from '../../routing/default.js';
 import { serializeRouteData } from '../../routing/index.js';
-import { resolveSessionDriver } from '../../session.js';
 import { addRollupInput } from '../add-rollup-input.js';
 import { getOutFile, getOutFolder } from '../common.js';
 import { type BuildInternals, cssOrder, mergeInlineCss } from '../internal.js';
@@ -29,6 +30,27 @@ const replaceExp = new RegExp(`['"]${manifestReplace}['"]`, 'g');
 
 export const SSR_MANIFEST_VIRTUAL_MODULE_ID = '@astrojs-manifest';
 export const RESOLVED_SSR_MANIFEST_VIRTUAL_MODULE_ID = '\0' + SSR_MANIFEST_VIRTUAL_MODULE_ID;
+
+function resolveSessionDriver(driver: string | undefined): string | null {
+	if (!driver) {
+		return null;
+	}
+	try {
+		if (driver === 'fs') {
+			return importMetaResolve(builtinDrivers.fsLite, import.meta.url);
+		}
+		if (driver in builtinDrivers) {
+			return importMetaResolve(
+				builtinDrivers[driver as BuiltinDriverName],
+				import.meta.url,
+			);
+		}
+	} catch {
+		return null;
+	}
+
+	return driver;
+}
 
 function vitePluginManifest(options: StaticBuildOptions, internals: BuildInternals): VitePlugin {
 	return {
