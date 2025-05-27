f5f33f3db51c5c8e66b0edb267353326d7f59e00
Arpan Patel <florian-lefebvre@users.noreply.github.com>
2025-05-27 08:33:33 +0000

    [ci] format

--- packages/create-astro/src/messages.ts
+++ packages/create-astro/src/messages.ts
@@ -51,7 +51,12 @@ export const getName = () =>
 		});
 	});
 
-export const getVersion = (packageManager: string, packageName: string, packageTag = 'latest', fallback = '') =>
+export const getVersion = (
+	packageManager: string,
+	packageName: string,
+	packageTag = 'latest',
+	fallback = '',
+) =>
 	new Promise<string>(async (resolve) => {
 		let registry = await getRegistry(packageManager);
 		const { version } = await fetch(`${registry}/${packageName}/${packageTag}`, {
