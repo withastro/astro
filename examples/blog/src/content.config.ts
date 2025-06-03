2f6dfd7ebf584a92cec770abf947a636b2698228
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-06-03 10:48:53 +0200

    feat: move and make image optional

--- examples/blog/src/content.config.ts
+++ examples/blog/src/content.config.ts
@@ -11,7 +11,7 @@ const blog = defineCollection({
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: image(),
+		heroImage: image().optional(),
 	}),
 });
 
