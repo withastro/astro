4f73538600ee9984aa6b5931d84f2d8fbab9821c
aaronblue106 <aaronblue106@gmail.com>
2025-06-15 08:01:38 +0000

    feature: config cicd

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+  output: 'server',
+  adapter: cloudflare(),
+  vite: {
     plugins: [tailwindcss()],
   },
 });
