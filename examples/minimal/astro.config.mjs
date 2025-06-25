82f98198306f9f08460d3b4324dbb7e4dda3149a
vivek lokhande <vivek2811999@gmail.com>
2025-06-25 20:56:17 +0530

    fix: config file for minimal example

--- examples/minimal/astro.config.mjs
+++ examples/minimal/astro.config.mjs
@@ -1,35 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'static',
-  adapter: node({
-    mode: 'standalone',
-  }),
- 
-  integrations: [
-    {
-      name: 'config',
-      hooks: {
-        'astro:config:setup': ({ updateConfig }) => {
-          updateConfig({
-            base: '/blog',
-            site: 'https://example.com',
-            trailingSlash: 'always',
-            i18n: {
-              defaultLocale: 'en',
-              locales: ['en', 'fr'],
-              routing: {
-                prefixDefaultLocale: false,
-                redirectToDefaultLocale: true,
-                fallbackType: 'rewrite',
-              },
-            },
-          });
-        },
-      },
-    },
-  ],
-});
+export default defineConfig({});
