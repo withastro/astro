3d488909960c553a7f6cc6467fc74acec7e84e45
Dawid Gaweł <daw.gawel@gmail.com>
2025-06-26 23:49:55 +0200

    remove type union duplication (lint)

--- packages/astro/src/core/routing/rewrite.ts
+++ packages/astro/src/core/routing/rewrite.ts
@@ -22,7 +22,7 @@ type FindRouteToRewrite = {
 	trailingSlash: AstroConfig['trailingSlash'];
 	buildFormat: AstroConfig['build']['format'];
 	base: AstroConfig['base'];
-	outDir: AstroConfig['outDir'] | AstroConfig['build']['client'] | string;
+	outDir: URL | string;
 };
 
 interface FindRouteToRewriteResult {
