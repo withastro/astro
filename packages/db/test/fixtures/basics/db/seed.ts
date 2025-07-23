f99c94d3582db40f5ae578a5c66c2d9e7070afb6
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-07-23 10:18:47 +0200

    Discard changes to packages/db/test/fixtures/basics/db/seed.ts

--- packages/db/test/fixtures/basics/db/seed.ts
+++ packages/db/test/fixtures/basics/db/seed.ts
@@ -7,7 +7,7 @@ export default async function () {
 	await db.batch([
 		db
 			.insert(Themes)
-			.values([{ name: 'dracula' }, { name: 'monokai' }])
+			.values([{ name: 'dracula' }, { name: 'monokai', added: new Date() }])
 			.returning({ name: Themes.name }),
 		db
 			.insert(Author)
