2b27a2fbcff55ea167b39ebe58039d5faaf2e6ac
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-08-01 09:34:25 +0200

    feat: experimental static import.meta.env

--- packages/astro/src/env/env-loader.ts
+++ packages/astro/src/env/env-loader.ts
@@ -6,12 +6,15 @@ import type { AstroConfig } from '../types/public/index.js';
 // except that the first character cannot be a number.
 const isValidIdentifierRe = /^[_$a-zA-Z][\w$]*$/;
 
-function getPrivateEnv(
-	fullEnv: Record<string, string>,
-	astroConfig: AstroConfig,
-	useRawValues: boolean,
-): Record<string, string> {
-	const viteConfig = astroConfig.vite;
+function getPrivateEnv({
+	fullEnv,
+	viteConfig,
+	useStatic,
+}: {
+	fullEnv: Record<string, string>;
+	viteConfig: AstroConfig['vite'];
+	useStatic: boolean;
+}): Record<string, string> {
 	let envPrefixes: string[] = ['PUBLIC_'];
 	if (viteConfig.envPrefix) {
 		envPrefixes = Array.isArray(viteConfig.envPrefix)
@@ -22,45 +25,51 @@ function getPrivateEnv(
 	const privateEnv: Record<string, string> = {};
 	for (const key in fullEnv) {
 		// Ignore public env var
-		if (isValidIdentifierRe.test(key) && envPrefixes.every((prefix) => !key.startsWith(prefix))) {
-			if (typeof process.env[key] !== 'undefined') {
-				let value = process.env[key];
-				// Replacements are always strings, so try to convert to strings here first
-				if (typeof value !== 'string') {
-					value = `${value}`;
-				}
-				// Boolean values should be inlined to support `export const prerender`
-				// We already know that these are NOT sensitive values, so inlining is safe
-				if (
-					!useRawValues &&
-					(value === '0' || value === '1' || value === 'true' || value === 'false')
-				) {
-					privateEnv[key] = value;
-				} else {
-					privateEnv[key] = `process.env.${key}`;
-				}
-			} else {
-				privateEnv[key] = JSON.stringify(fullEnv[key]);
+		if (!isValidIdentifierRe.test(key) || envPrefixes.some((prefix) => key.startsWith(prefix))) {
+			continue;
+		}
+		// Only replace with process.env if not static
+		// TODO: make static the default and only way to do this in Astro 6
+		if (!useStatic && typeof process.env[key] !== 'undefined') {
+			let value = process.env[key];
+			// Replacements are always strings, so try to convert to strings here first
+			if (typeof value !== 'string') {
+				value = `${value}`;
 			}
+			// Boolean values should be inlined to support `export const prerender`
+			// We already know that these are NOT sensitive values, so inlining is safe
+			if (value === '0' || value === '1' || value === 'true' || value === 'false') {
+				privateEnv[key] = value;
+			} else {
+				privateEnv[key] = `process.env.${key}`;
+			}
+		} else {
+			privateEnv[key] = JSON.stringify(fullEnv[key]);
 		}
 	}
 	return privateEnv;
 }
 
-function getEnv(mode: string, config: AstroConfig, useRawValues: boolean) {
+interface EnvLoaderOptions {
+	mode: string;
+	config: AstroConfig;
+	useStatic: boolean;
+}
+
+function getEnv({ mode, config, useStatic }: EnvLoaderOptions) {
 	const loaded = loadEnv(mode, config.vite.envDir ?? fileURLToPath(config.root), '');
-	const privateEnv = getPrivateEnv(loaded, config, useRawValues);
+	const privateEnv = getPrivateEnv({ fullEnv: loaded, viteConfig: config.vite, useStatic });
 
 	return { loaded, privateEnv };
 }
 
-export const createEnvLoader = (mode: string, config: AstroConfig, useRawValues: boolean) => {
-	let { loaded, privateEnv } = getEnv(mode, config, useRawValues);
+export const createEnvLoader = (options: EnvLoaderOptions) => {
+	let { loaded, privateEnv } = getEnv(options);
 	return {
 		get: () => {
 			// We refresh the env we have in case process.env has been updated since creating
 			// the env loader. That can happen in eg. integrations
-			({ loaded, privateEnv } = getEnv(mode, config, useRawValues));
+			({ loaded, privateEnv } = getEnv(options));
 			return loaded;
 		},
 		getPrivateEnv: () => privateEnv,
