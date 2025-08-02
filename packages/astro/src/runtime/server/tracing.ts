2e1caaac66ab9ebb9b9dd8e75f41617bed67d239
Luiz Ferraz <luiz@lferraz.com>
2025-08-01 22:58:31 -0300

    Add hook for async operation completion

--- packages/astro/src/runtime/server/tracing.ts
+++ packages/astro/src/runtime/server/tracing.ts
@@ -4,12 +4,15 @@ type EventArgs = {
 	[K in keyof TraceEvents]: [event: K, payload: TraceEvents[K]];
 }[keyof TraceEvents];
 
-const eventLifecycle: Record<'before' | 'after', TraceListener[]> = {
+type OperationLifecycle = 'before' | 'onComplete' | 'after';
+
+const eventLifecycle: Record<OperationLifecycle, TraceListener[]> = {
 	before: [],
+	onComplete: [],
 	after: [],
 };
 
-function onTrace(lifecycle: 'before' | 'after', listener: TraceListener, signal?: AbortSignal) {
+function onTrace(lifecycle: OperationLifecycle, listener: TraceListener, signal?: AbortSignal) {
 	const wrapped: TraceListener = (...args) => {
 		try {
 			const res: unknown = listener(...args);
@@ -21,12 +24,21 @@ function onTrace(lifecycle: 'before' | 'after', listener: TraceListener, signal?
 	};
 
 	const listeners = eventLifecycle[lifecycle];
-	listeners.push(wrapped);
+
 	if (signal) {
+		if (signal.aborted) {
+			// The signal is already aborted, the listener should never be called.
+			// Returning early avoids both possible scenarios:
+			// - The `abort` event is being processed and the listener would be removed depending on a race condition.
+			// - The `abort` signal was already processed and the listener will never be removed, triggering after the signal is aborted.
+			return;
+		}
 		signal.addEventListener('abort', () => {
 			listeners.splice(listeners.indexOf(wrapped), 1);
 		});
 	}
+
+	listeners.push(wrapped);
 }
 
 /**
@@ -34,6 +46,13 @@ function onTrace(lifecycle: 'before' | 'after', listener: TraceListener, signal?
  */
 export function onBeforeTrace(listener: TraceListener, signal?: AbortSignal) {
 	onTrace('before', listener, signal);
+}
+
+/**
+ * @experimental
+ */
+export function onCompleteTrace(listener: TraceListener, signal?: AbortSignal) {
+	onTrace('onComplete', listener, signal);
 }
 
 /**
@@ -49,7 +68,11 @@ export function wrapWithTracing<This, Args extends any[], Return, Event extends 
 	payload: TraceEvents[Event] | ((this: This, ...args: Args) => TraceEvents[Event]),
 ): (this: This, ...args: Args) => Return {
 	return function (this: This, ...args: Args): Return {
-		if (eventLifecycle.before.length === 0 && eventLifecycle.after.length === 0) {
+		if (
+			eventLifecycle.before.length === 0 &&
+			eventLifecycle.onComplete.length === 0 &&
+			eventLifecycle.after.length === 0
+		) {
 			// Avoid constructing payloads and emitting events if no listeners are attached
 			return fn.apply(this, args);
 		}
@@ -63,9 +86,27 @@ export function wrapWithTracing<This, Args extends any[], Return, Event extends 
 			listener(...eventArgs);
 		}
 
-		const result = fn.apply(this, args);
+		let result = fn.apply(this, args);
 
-		for (const listener of eventLifecycle.before) {
+		if (result instanceof Promise) {
+			if (eventLifecycle.onComplete.length > 0) {
+				// Only attach a `finally` handler if there are onComplete listeners.
+				// This avoids unnecessary entries on the event loop when tracing implementations don't use the `onComplete` hook.
+				result = result.finally(() => {
+					// Call hook after the async operation completes
+					for (const listener of eventLifecycle.onComplete) {
+						listener(...eventArgs);
+					}
+				}) as /* Safe to cast because Promise.finally doesn't change the resolved or thrown value. */ Return;
+			}
+		} else {
+			// Operation was synchronous, call onComplete listeners immediately
+			for (const listener of eventLifecycle.onComplete) {
+				listener(...eventArgs);
+			}
+		}
+
+		for (const listener of eventLifecycle.after) {
 			listener(...eventArgs);
 		}
 
