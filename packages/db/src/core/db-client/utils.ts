89153ece33f2c8d6409027ac9857067f46167ca7
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-09 06:41:38 -0700

    refactor(db): enhance boolean parsing logic in LibSQL config handling

--- packages/db/src/core/db-client/utils.ts
+++ packages/db/src/core/db-client/utils.ts
@@ -6,11 +6,22 @@ const rawLibSQLOptions = z.record(z.string());
 const parseNumber = (value: string) => z.coerce.number().parse(value);
 const parseBoolean = (value: string) => z.coerce.boolean().parse(value);
 
+const booleanValues = ['true', 'false'];
+
+// parse a value that should be a boolean, but could be a valueless variable)
+// e.g. 'file://local-copy.db?readYourWrites' & 'file://local-copy.db?readYourWrites=true' should be parsed as true
+const parseOptionalBoolean = (value: string) => {
+	if (booleanValues.includes(value)) {
+		return parseBoolean(value);
+	}
+	return true; // If the value is not explicitly 'true' or 'false', assume it's true (valueless variable)
+};
+
 const libSQLConfigTransformed = rawLibSQLOptions.transform((raw) => {
-    // Ensure the URL is always present
+	// Ensure the URL is always present
 	const parsed: Partial<LibSQLConfig> = {};
 
-    // Optional fields
+	// Optional fields
 	for (const [key, value] of Object.entries(raw)) {
 		switch (key) {
 			case 'syncInterval':
@@ -20,27 +31,27 @@ const libSQLConfigTransformed = rawLibSQLOptions.transform((raw) => {
 			case 'readYourWrites':
 			case 'offline':
 			case 'tls':
-				parsed[key] = parseBoolean(value);
+				parsed[key] = parseOptionalBoolean(value);
 				break;
-            case 'authToken':
-            case 'encryptionKey':
-            case 'syncUrl':
+			case 'authToken':
+			case 'encryptionKey':
+			case 'syncUrl':
 				parsed[key] = value;
-                break;
+				break;
 		}
 	}
 
-    // Return the parsed config
-    return parsed;
+	// Return the parsed config
+	return parsed;
 });
 
 export const parseLibSQLConfig = (config: Record<string, string>): Partial<LibSQLConfig> => {
-    try {
-        return libSQLConfigTransformed.parse(config);
-    } catch (error) {
-        if (error instanceof z.ZodError) {
-            throw new Error(`Invalid LibSQL config: ${error.errors.map(e => e.message).join(', ')}`);
-        }
-        throw error;
-    }
-}
+	try {
+		return libSQLConfigTransformed.parse(config);
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			throw new Error(`Invalid LibSQL config: ${error.errors.map((e) => e.message).join(', ')}`);
+		}
+		throw error;
+	}
+};
