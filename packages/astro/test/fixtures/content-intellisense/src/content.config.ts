c5002bbf7bd2e133acbd7b689f93c9b8987d4e94
Chris Swithinbank <swithinbank@gmail.com>
2025-08-14 11:25:00 +0200

    Add collection that misuses `$schema`

--- packages/astro/test/fixtures/content-intellisense/src/content.config.ts
+++ packages/astro/test/fixtures/content-intellisense/src/content.config.ts
@@ -21,10 +21,15 @@ const blogCL = defineCollection({
 const dataSchema = z.object({ name: z.string(), color: z.string() });
 const dataYML = defineCollection({ loader: file('src/data-cl.yml'), schema: dataSchema });
 const dataJSON = defineCollection({ loader: file('src/data-cl.json'), schema: dataSchema });
+const dataWithSchemaMisuse = defineCollection({
+	loader: file('src/$schema-misuse.json'),
+	schema: z.object({ value: z.number() }),
+});
 
 export const collections = {
 	"blog-cc": blogCC,
 	"blog-cl": blogCL,
 	"data-cl": dataYML,
 	"data-cl-json": dataJSON,
+	"data-schema-misuse": dataWithSchemaMisuse,
 };
