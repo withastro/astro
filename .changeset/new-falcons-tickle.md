6e6635a31e4fd61fe0aad67f7180e14bec153db0
Matt Kane <m@mk.gg>
2025-08-29 11:19:45 +0100

    fix(vercel): ensure srcset widths are always from vercel allowlist

---
+++ .changeset/new-falcons-tickle.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/vercel': patch
+---
+
+Fixes a bug that caused invalid image sizes to be generated when the requested widths were larger than the source image
--- packages/integrations/vercel/src/image/build-service.ts
+++ packages/integrations/vercel/src/image/build-service.ts
@@ -62,6 +62,80 @@ const service: ExternalImageService = {
 
 		return '/_vercel/image?' + searchParams;
 	},
+	// Adapted from the base service's getSrcSet, but always returning widths that are valid for Vercel
+	getSrcSet(options) {
+		const { inputtedWidth, densities, widths, ...props } = options;
+
+		// If `validateOptions` returned a different width than the one of the image, use it for attributes
+		if (inputtedWidth) {
+			props.width = inputtedWidth;
+		}
+
+		let targetWidth = props.width;
+		let targetHeight = props.height;
+		if (isESMImportedImage(props.src)) {
+			const aspectRatio = props.src.width / props.src.height;
+			if (targetHeight && !targetWidth) {
+				// If we have a height but no width, use height to calculate the width
+				targetWidth = Math.round(targetHeight * aspectRatio);
+			} else if (targetWidth && !targetHeight) {
+				// If we have a width but no height, use width to calculate the height
+				targetHeight = Math.round(targetWidth / aspectRatio);
+			} else if (!targetWidth && !targetHeight) {
+				// If we have neither width or height, use the original image's dimensions
+				targetWidth = props.src.width;
+				targetHeight = props.src.height;
+			}
+		}
+
+		// Since `widths` and `densities` ultimately control the width and height of the image,
+		// we don't want the dimensions the user specified, we'll create those ourselves.
+		const {
+			width: transformWidth,
+			height: transformHeight,
+			...transformWithoutDimensions
+		} = options;
+
+		// Collect widths to generate from specified densities or widths
+		let allWidths: Array<{
+			width: number;
+			descriptor: `${number}x` | `${number}w`;
+		}> = [];
+
+		if (densities) {
+			// Densities can either be specified as numbers, or descriptors (ex: '1x'), we'll convert them all to numbers
+			const densityValues = densities.map((density) => {
+				if (typeof density === 'number') {
+					return density;
+				} else {
+					return parseFloat(density);
+				}
+			});
+
+			// Calculate the widths for each density, rounding to avoid floats.
+			const densityWidths = densityValues
+				.sort((a, b) => a - b)
+				.map((density) => Math.round(targetWidth! * density));
+
+			allWidths = densityWidths.map((width, index) => ({
+				width,
+				descriptor: `${densityValues[index]}x`,
+			}));
+		} else if (widths?.length) {
+			allWidths = widths.map((width) => ({
+				width,
+				descriptor: `${width}w`,
+			}));
+		}
+
+		return allWidths.map(({ width, descriptor }) => {
+			const transform = { ...transformWithoutDimensions, width };
+			return {
+				transform,
+				descriptor,
+			};
+		});
+	},
 };
 
 function removeLeadingForwardSlash(path: string) {
