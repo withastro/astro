aa355a050229ed5789fa0231612c18ec7a103589
Matt Kane <m@mk.gg>
2025-09-17 14:02:32 +0100

    Fix unit tests for environment API (#14394)
    * Fix unit tests for environment API
    
    * Format

--- packages/astro/test/units/vite-plugin-astro-server/request.test.js
+++ packages/astro/test/units/vite-plugin-astro-server/request.test.js
@@ -5,7 +5,8 @@ import { createContainer } from '../../../dist/core/dev/container.js';
 import { createLoader } from '../../../dist/core/module-loader/index.js';
 import { createRoutesList } from '../../../dist/core/routing/index.js';
 import { createComponent, render } from '../../../dist/runtime/server/index.js';
-import { createController, DevApp } from '../../../dist/vite-plugin-astro-server/index.js';
+import { AstroServerApp } from '../../../dist/vite-plugin-app/app.js';
+import { createController } from '../../../dist/vite-plugin-astro-server/index.js';
 import { createDevelopmentManifest } from '../../../dist/vite-plugin-astro-server/plugin.js';
 import testAdapter from '../../test-adapter.js';
 import {
@@ -18,7 +19,7 @@ import {
 
 async function createDevApp(overrides = {}, root) {
 	const settings = overrides.settings ?? (await createBasicSettings({ root }));
-	const loader = overrides.loader ?? createLoader();
+	const loader = overrides.loader ?? createLoader({});
 	const manifest = createDevelopmentManifest(settings);
 	const routesList = await createRoutesList(
 		{
@@ -43,7 +44,7 @@ async function createDevApp(overrides = {}, root) {
 
 	// TODO: temporarily inject route list inside manifest
 
-	return new DevApp(manifest, true, settings, defaultLogger, loader, routesList);
+	return new AstroServerApp(manifest, true, defaultLogger, routesList, loader, settings);
 }
 
 describe('vite-plugin-astro-server', () => {
