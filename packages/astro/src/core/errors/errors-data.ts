920008ddf6f2ad7ecbf59c15e4e799c7019f75e0
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-09-23 13:52:11 +0200

    Apply suggestions from code review
    Co-authored-by: Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>

--- packages/astro/src/core/errors/errors-data.ts
+++ packages/astro/src/core/errors/errors-data.ts
@@ -1370,14 +1370,14 @@ export const ExperimentalFontsNotEnabled = {
  * @description
  * Font family not found
  * @message
- * No data was found for the cssVariable passed to the Font component or getFontData function.
+ * No data was found for the `cssVariable` passed to the `<Font />` component or to the `getFontData()` function.
  */
 export const FontFamilyNotFound = {
 	name: 'FontFamilyNotFound',
 	title: 'Font family not found',
 	message: (family: string) =>
 		`No data was found for the \`"${family}"\` family passed to the \`<Font>\` component.`,
-	hint: 'This is often caused by a typo. Check that the Font component and getFontData function are using a `cssVariable` specified in your config.',
+	hint: 'This is often caused by a typo. Check that the `<Font />` component or `getFontData()` function are using a `cssVariable` specified in your config.',
 } satisfies ErrorData;
 
 /**
