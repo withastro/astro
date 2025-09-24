656bbde1b85956d8e6380ae7e4a54e026d4eb3f7
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-09-24 18:08:22 +0200

    fix: build

--- packages/markdown/remark/src/import-plugin-default.ts
+++ packages/markdown/remark/src/import-plugin-default.ts
@@ -1,9 +1,12 @@
 // This file should be imported as `#import-plugin`
+
+import { createRequire } from 'node:module';
 import path from 'node:path';
 import { pathToFileURL } from 'node:url';
 import type * as unified from 'unified';
 
 let cwdUrlStr: string | undefined;
+const require = createRequire(import.meta.url);
 
 // In non-browser environments, we can try to resolve from the filesystem too
 export async function importPlugin(p: string): Promise<unified.Plugin> {
@@ -15,7 +18,7 @@ export async function importPlugin(p: string): Promise<unified.Plugin> {
 
 	// Try import from user project
 	cwdUrlStr ??= pathToFileURL(path.join(process.cwd(), 'package.json')).toString();
-	const resolved = import.meta.resolve(p, cwdUrlStr);
+	const resolved = require.resolve(p, { paths: [cwdUrlStr] });
 	const importResult = await import(/* @vite-ignore */ resolved);
 	return importResult.default;
 }
