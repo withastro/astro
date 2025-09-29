7ca2d2919b538a001687ce24ca31f44081581ff8
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-09-29 10:19:34 +0200

    fix: do not use proxy

--- packages/astro/src/runtime/server/astro-global.ts
+++ packages/astro/src/runtime/server/astro-global.ts
@@ -2,31 +2,98 @@ import { ASTRO_GENERATOR } from '../../core/constants.js';
 import { AstroError, AstroErrorData } from '../../core/errors/index.js';
 import type { AstroGlobal } from '../../types/public/context.js';
 
+function createError(name: string) {
+	return new AstroError({
+		...AstroErrorData.UnavailableAstroGlobalProperty,
+		message: AstroErrorData.UnavailableAstroGlobalProperty.message(name),
+	});
+}
+
 // This is used to create the top-level Astro global; the one that you can use
 // inside of getStaticPaths. See the `astroGlobalArgs` option for parameter type.
 export function createAstro(site: string | undefined): AstroGlobal {
-	return new Proxy({} as AstroGlobal, {
-		get(_, prop: keyof AstroGlobal) {
-			if (prop === 'site') {
-				// This is created inside of the runtime so we don't have access to the Astro logger.
-				console.warn(
-					`Astro.site inside getStaticPaths is deprecated and will be removed in a future major version of Astro. Use import.meta.env.SITE instead`,
-				);
-				return site ? new URL(site) : undefined;
-			}
-
-			if (prop === 'generator') {
-				// This is created inside of the runtime so we don't have access to the Astro logger.
-				console.warn(
-					`Astro.generator inside getStaticPaths is deprecated and will be removed in a future major version of Astro.`,
-				);
-				return ASTRO_GENERATOR;
-			}
-
-			throw new AstroError({
-				...AstroErrorData.UnavailableAstroGlobalProperty,
-				message: AstroErrorData.UnavailableAstroGlobalProperty.message(prop),
-			});
+	return {
+		// TODO: throw in Astro 7
+		get site() {
+			// This is created inside of the runtime so we don't have access to the Astro logger.
+			console.warn(
+				`Astro.site inside getStaticPaths is deprecated and will be removed in a future major version of Astro. Use import.meta.env.SITE instead`,
+			);
+			return site ? new URL(site) : undefined;
 		},
-	});
+		// TODO: throw in Astro 7
+		get generator() {
+			// This is created inside of the runtime so we don't have access to the Astro logger.
+			console.warn(
+				`Astro.generator inside getStaticPaths is deprecated and will be removed in a future major version of Astro.`,
+			);
+			return ASTRO_GENERATOR;
+		},
+		get callAction(): any {
+			throw createAstro('callAction');
+		},
+		get clientAddress(): any {
+			throw createAstro('clientAddress');
+		},
+		get cookies(): any {
+			throw createAstro('cookies');
+		},
+		get csp(): any {
+			throw createAstro('csp');
+		},
+		get currentLocale(): any {
+			throw createError('currentLocale');
+		},
+		get getActionResult(): any {
+			throw createAstro('getActionResult');
+		},
+		get isPrerendered(): any {
+			throw createError('isPrerendered');
+		},
+		get locals(): any {
+			throw createError('locals');
+		},
+		get originPathname(): any {
+			throw createError('originPathname');
+		},
+		get params(): any {
+			throw createError('params');
+		},
+		get preferredLocale(): any {
+			throw createError('preferredLocale');
+		},
+		get preferredLocaleList(): any {
+			throw createError('preferredLocaleList');
+		},
+		get props(): any {
+			throw createError('props');
+		},
+		get redirect(): any {
+			throw createError('redirect');
+		},
+		get request(): any {
+			throw createError('request');
+		},
+		get response(): any {
+			throw createError('response');
+		},
+		get rewrite(): any {
+			throw createError('rewrite');
+		},
+		get routePattern(): any {
+			throw createError('routePattern');
+		},
+		get self(): any {
+			throw createError('self');
+		},
+		get slots(): any {
+			throw createError('slots');
+		},
+		get url(): any {
+			throw createError('url');
+		},
+		get session(): any {
+			throw createError('session');
+		},
+	};
 }
