d6caec833b4ce53a01b647d4331a31429a0ffaef
Emanuele Stoppa <my.burning@gmail.com>
2025-10-02 16:59:32 +0200

    feat: support for astro actions (#14478)

--- packages/integrations/cloudflare/test/fixtures/vite-plugin/src/dev.ts
+++ packages/integrations/cloudflare/test/fixtures/vite-plugin/src/dev.ts
@@ -3,6 +3,11 @@ import { renderers } from "astro:renderers";
 import { routes } from "astro:routes"
 import { createExports } from "@astrojs/cloudflare/entrypoints/server.js";
 
-const manifest = Object.assign(serializedManifest, { renderers });
+const actions = async () => {
+	const actions = await import("astro-internal:actions");
+	return actions
+};
+const manifest = Object.assign(serializedManifest, { renderers, actions });
+
 
 export default createExports(manifest, routes).default
