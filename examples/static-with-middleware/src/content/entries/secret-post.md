871d8ce91495413f41cd52aaca090b9de572ee48
Lieke <lieke.beunders@decathlon.com>
2025-10-13 17:48:12 +0200

    chore: use formatter

--- examples/static-with-middleware/src/content/entries/secret-post.md
+++ examples/static-with-middleware/src/content/entries/secret-post.md
@@ -2,4 +2,5 @@
 title: 'Secret post'
 pubDate: 'Jul 22 2022'
 ---
+
 This post is only visible for users that are logged in.
--- examples/static-with-middleware/src/middleware.ts
+++ examples/static-with-middleware/src/middleware.ts
@@ -1,43 +1,43 @@
-import { defineMiddleware } from "astro:middleware";
+import { defineMiddleware } from 'astro:middleware';
 
-const originalRequestCookieName = "VP-Original-Request";
+const originalRequestCookieName = 'VP-Original-Request';
 
 export const onRequest = defineMiddleware(async (context, next) => {
-          console.log("Middleware running on:", context.originPathname, context.cookies.get(originalRequestCookieName)?.value);
+	console.log(
+		'Middleware running on:',
+		context.originPathname,
+		context.cookies.get(originalRequestCookieName)?.value,
+	);
 
-  if ( !context.originPathname.includes("secret") 
-  ) {
-    return next();
-  }
+	if (!context.originPathname.includes('secret')) {
+		return next();
+	}
 
-  // todo, check cookie
-  const isAuthed = false;
+	// todo, check cookie
+	const isAuthed = false;
 
+	console.log('Middleware running on:', context.originPathname, { isAuthed });
 
-      console.log("Middleware running on:", context.originPathname, {isAuthed});
+	if (!isAuthed) {
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: '/login',
+				'Set-Cookie': `${originalRequestCookieName}=${context.originPathname}; Path=/; HttpOnly`,
+			},
+		});
+	}
 
+	if (context.originPathname.startsWith('/api/finish-login')) {
+		const redirectTo = context.cookies.get(originalRequestCookieName)?.value || '/';
+		return new Response(null, {
+			status: 302,
+			headers: {
+				Location: redirectTo,
+				'Set-Cookie': `${originalRequestCookieName}=/; Path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+			},
+		});
+	}
 
-  if (!isAuthed) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: "/login",
-          "Set-Cookie": `${originalRequestCookieName}=${context.originPathname}; Path=/; HttpOnly`,
-        },
-      });
-    } 
-  
-  if (context.originPathname.startsWith("/api/finish-login")) {
-    const redirectTo =
-      context.cookies.get(originalRequestCookieName)?.value || "/";
-    return new Response(null, {
-      status: 302,
-      headers: {
-        Location: redirectTo,
-        "Set-Cookie": `${originalRequestCookieName}=/; Path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
-      },
-    });
-  }
-
-  return next();
+	return next();
 });
