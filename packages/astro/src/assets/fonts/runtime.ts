ac543928a671f403201bdc24313db56644af2700
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-10-16 15:08:16 +0200

    feat: smarter variable weight

--- packages/astro/src/assets/fonts/runtime.ts
+++ packages/astro/src/assets/fonts/runtime.ts
@@ -3,6 +3,7 @@ import type { ConsumableMap, PreloadData, PreloadFilter } from './types.js';
 
 export function createGetFontData({ consumableMap }: { consumableMap?: ConsumableMap }) {
 	return function getFontData(cssVariable: string) {
+		// TODO: remove once fonts are stabilized
 		if (!consumableMap) {
 			throw new AstroError(AstroErrorData.ExperimentalFontsNotEnabled);
 		}
@@ -33,11 +34,27 @@ export function filterPreloads(
 		preload.some(
 			(p) =>
 				// Always check the weight
-				(!p.weight || p.weight.toString() === weight) &&
+				(p.weight === undefined ||
+					weight === undefined ||
+					checkWeight(p.weight.toString(), weight)) &&
 				// Only check the style if specified
-				(!p.style || p.style === style) &&
+				(p.style === undefined || p.style === style) &&
 				// Only check the subset if specified
-				(p!.subset || p.subset === subset),
+				(p.subset === undefined || p.subset === subset),
 		),
 	);
+}
+
+function checkWeight(input: string, target: string): boolean {
+	// If the input looks like "100 900", we check it as is
+	if (input.includes(' ')) {
+		return input === target;
+	}
+	// If the target looks like "100 900", we check if the input is between the values
+	if (target.includes(' ')) {
+		const [a, b] = target.split(' ');
+		const parsedInput = Number.parseInt(input);
+		return parsedInput >= Number.parseInt(a) && parsedInput <= Number.parseInt(b);
+	}
+	return input === target;
 }
