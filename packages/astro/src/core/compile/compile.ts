cb8c31a1937c1d46f7c9e3a29bfe7994e4f93681
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-10-20 08:45:08 +0200

    Update packages/astro/src/core/compile/compile.ts

--- packages/astro/src/core/compile/compile.ts
+++ packages/astro/src/core/compile/compile.ts
@@ -47,6 +47,7 @@ export async function compile({
 			normalizedFilename: normalizeFilename(filename, astroConfig.root),
 			sourcemap: 'both',
 			internalURL: 'astro/compiler-runtime',
+			// TODO: remove in Astro v7
 			astroGlobalArgs: JSON.stringify(astroConfig.site),
 			scopedStyleStrategy: astroConfig.scopedStyleStrategy,
 			resultScopedSlot: true,
