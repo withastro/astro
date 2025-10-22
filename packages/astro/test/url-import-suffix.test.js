f3905f3c52187165da980facd0987c58e4b8efa9
Simen Sagholen Førrisdal <simen.forrisdal@adresseavisen.no>
2025-10-22 15:28:13 +0200

    test: add failing case for url imports

---
+++ packages/astro/test/url-import-suffix.test.js
@@ -0,0 +1,27 @@
+import * as assert from 'node:assert/strict';
+import { before, describe, it } from 'node:test';
+import * as cheerio from 'cheerio';
+import { loadFixture } from './test-utils.js';
+
+describe('imports using ?url suffix', () => {
+  let fixture;
+  const chunkName = 'index.DqQksVyv.css'
+
+  before(async () => {
+		fixture = await loadFixture({ root: './fixtures/url-import-suffix/' });
+		await fixture.build();
+	});
+
+  it('includes the built asset in the output', async () => {
+    const assets = await fixture.readdir('/_astro');
+    assert.ok(assets.some((f) => f === chunkName));
+  });
+
+  it('links the asset in the html', async () => {
+    const html = await fixture.readFile('/index.html');
+    const $ = cheerio.load(html);
+
+    const linkHref = $('link[rel="stylesheet"]').attr('href');
+    assert.ok(linkHref, `/_astro/${chunkName}`);
+  });
+})
