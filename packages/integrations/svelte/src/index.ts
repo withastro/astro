3d55c5d0fb520d470b33d391e5b68861f5b51271
Matt Kane <m@mk.gg>
2025-11-05 07:35:28 +0000

    feat: return `clientEntrypoint` from `getContainerRenderer` (#14715)
    * feat: return `clientEntrypoint` from `getContainerRenderer`
    
    * :scissors:

--- packages/integrations/svelte/src/index.ts
+++ packages/integrations/svelte/src/index.ts
@@ -1,6 +1,6 @@
 import type { Options } from '@sveltejs/vite-plugin-svelte';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import type { AstroIntegration, AstroRenderer, ContainerRenderer } from 'astro';
+import type { AstroIntegration, AstroRenderer } from 'astro';
 
 function getRenderer(): AstroRenderer {
 	return {
@@ -10,12 +10,7 @@ function getRenderer(): AstroRenderer {
 	};
 }
 
-export function getContainerRenderer(): ContainerRenderer {
-	return {
-		name: '@astrojs/svelte',
-		serverEntrypoint: '@astrojs/svelte/server.js',
-	};
-}
+export { getRenderer as getContainerRenderer };
 
 export default function svelteIntegration(options?: Options): AstroIntegration {
 	return {
