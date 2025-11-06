3cb1ba75f7091b945ede98dea736fe94df150484
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2025-11-06 17:08:57 +0100

    fix: strange

--- packages/language-tools/language-server/test/setup.js
+++ packages/language-tools/language-server/test/setup.js
@@ -1,6 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 // @ts-expect-error
 import { cli } from '../../../astro/test/test-utils.js';
-import { fixtureDir } from './utils.ts';
+
+// Copied from utils.ts so we don't have to import TS code for Node 20
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fixtureDir = path.join(__dirname, './fixture');
 
 export default async function setup() {
 	// We only run the tests that require sync on Node.js versions other than 20 because the language server supports
