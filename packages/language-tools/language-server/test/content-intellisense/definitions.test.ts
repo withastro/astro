7cfef16bb0b988c09ebdab9dee82ca3306b7bee0
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-11-06 14:18:57 +0100

    wip

--- packages/language-tools/language-server/test/content-intellisense/definitions.test.ts
+++ packages/language-tools/language-server/test/content-intellisense/definitions.test.ts
@@ -3,46 +3,55 @@ import path from 'node:path';
 import { before, describe, it } from 'node:test';
 import type { LocationLink } from '@volar/language-server';
 import { Position } from '@volar/language-server';
+// @ts-ignore
+import { cli } from '../../../../astro/test/test-utils.js';
 import { getLanguageServer, type LanguageServer } from '../server.ts';
 import { fixtureDir } from '../utils.ts';
 
-describe('Content Intellisense - Go To Everywhere', async () => {
-	let languageServer: LanguageServer;
+describe(
+	'Content Intellisense - Go To Everywhere',
+	{ skip: parseInt(process.versions.node) === 20 },
+	async () => {
+		let languageServer: LanguageServer;
 
-	before(async () => (languageServer = await getLanguageServer()));
-
-	it('Provide definitions for keys', async () => {
-		const document = await languageServer.handle.openTextDocument(
-			path.join(fixtureDir, 'src', 'content', 'blog', 'definitions.md'),
-			'markdown',
-		);
-
-		const definitions = (await languageServer.handle.sendDefinitionRequest(
-			document.uri,
-			Position.create(1, 2),
-		)) as LocationLink[];
-
-		const targetUris = definitions?.map((definition) => definition.targetUri);
-		assert.strictEqual(
-			targetUris.every((uri) => uri.endsWith('config.ts')),
-			true,
-		);
-
-		const { targetRange, targetSelectionRange, originSelectionRange } = definitions[0];
-
-		assert.deepStrictEqual(targetRange, {
-			start: { line: 5, character: 2 },
-			end: { line: 5, character: 65 },
+		before(async () => {
+			await cli('sync', '--root', fixtureDir).getResult();
+			languageServer = await getLanguageServer();
 		});
 
-		assert.deepStrictEqual(targetSelectionRange, {
-			start: { line: 5, character: 2 },
-			end: { line: 5, character: 7 },
-		});
+		it('Provide definitions for keys', async () => {
+			const document = await languageServer.handle.openTextDocument(
+				path.join(fixtureDir, 'src', 'content', 'blog', 'definitions.md'),
+				'markdown',
+			);
 
-		assert.deepStrictEqual(originSelectionRange, {
-			start: { line: 1, character: 0 },
-			end: { line: 1, character: 5 },
+			const definitions = (await languageServer.handle.sendDefinitionRequest(
+				document.uri,
+				Position.create(1, 2),
+			)) as LocationLink[];
+
+			const targetUris = definitions?.map((definition) => definition.targetUri);
+			assert.strictEqual(
+				targetUris.every((uri) => uri.endsWith('config.ts')),
+				true,
+			);
+
+			const { targetRange, targetSelectionRange, originSelectionRange } = definitions[0];
+
+			assert.deepStrictEqual(targetRange, {
+				start: { line: 5, character: 2 },
+				end: { line: 5, character: 65 },
+			});
+
+			assert.deepStrictEqual(targetSelectionRange, {
+				start: { line: 5, character: 2 },
+				end: { line: 5, character: 7 },
+			});
+
+			assert.deepStrictEqual(originSelectionRange, {
+				start: { line: 1, character: 0 },
+				end: { line: 1, character: 5 },
+			});
 		});
-	});
-});
+	},
+);
--- packages/language-tools/language-server/test/content-intellisense/hover.test.ts
+++ packages/language-tools/language-server/test/content-intellisense/hover.test.ts
@@ -2,25 +2,37 @@ import assert from 'node:assert';
 import path from 'node:path';
 import { before, describe, it } from 'node:test';
 import { Position } from '@volar/language-server';
+// @ts-ignore
+import { cli } from '../../../../astro/test/test-utils.js';
 import { getLanguageServer, type LanguageServer } from '../server.ts';
 import { fixtureDir } from '../utils.ts';
 
-describe('Content Intellisense - Hover', async () => {
-	let languageServer: LanguageServer;
+describe(
+	'Content Intellisense - Hover',
+	{ skip: parseInt(process.versions.node) === 20 },
+	async () => {
+		let languageServer: LanguageServer;
 
-	before(async () => (languageServer = await getLanguageServer()));
-
-	it('Provide hover information for collection properties', async () => {
-		const document = await languageServer.handle.openTextDocument(
-			path.join(fixtureDir, 'src', 'content', 'blog', 'hover.md'),
-			'markdown',
-		);
-
-		const hover = await languageServer.handle.sendHoverRequest(document.uri, Position.create(1, 1));
-
-		assert.deepStrictEqual(hover?.contents, {
-			kind: 'markdown',
-			value: "The blog post's title.",
+		before(async () => {
+			await cli('sync', '--root', fixtureDir).getResult();
+			languageServer = await getLanguageServer();
 		});
-	});
-});
+
+		it('Provide hover information for collection properties', async () => {
+			const document = await languageServer.handle.openTextDocument(
+				path.join(fixtureDir, 'src', 'content', 'blog', 'hover.md'),
+				'markdown',
+			);
+
+			const hover = await languageServer.handle.sendHoverRequest(
+				document.uri,
+				Position.create(1, 1),
+			);
+
+			assert.deepStrictEqual(hover?.contents, {
+				kind: 'markdown',
+				value: "The blog post's title.",
+			});
+		});
+	},
+);
