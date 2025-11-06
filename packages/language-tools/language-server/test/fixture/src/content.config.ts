3e13785b1487378cc2fb7eb64e421514181e195f
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-11-06 14:35:27 +0100

    fix: pattern

--- packages/language-tools/language-server/test/fixture/src/content.config.ts
+++ packages/language-tools/language-server/test/fixture/src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
+	loader: glob({ base: './src/content/blog', pattern: '**/[^_]*.md' }),
 	schema: z.object({
 		title: z.string().describe("The blog post's title.").optional(),
 		description: z.string().optional(),
@@ -12,7 +12,7 @@ const blog = defineCollection({
 });
 
 const caching = defineCollection({
-	loader: glob({ base: './src/content/caching', pattern: '**/*.md' }),
+	loader: glob({ base: './src/content/caching', pattern: '**/[^_]*.md' }),
 	schema: z.object({
 		title: z.string().describe('I will be changed'),
 	}),
