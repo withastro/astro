8eab21af62b758120343e401ac27a9086c22130d
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-11-10 18:46:17 +0100

    refactor: remove getActionPath()

--- packages/astro/types/actions.d.ts
+++ packages/astro/types/actions.d.ts
@@ -1,7 +1,3 @@
 declare module 'astro:actions' {
 	export * from 'astro/actions/runtime/server.js';
-
-	export function getActionPath(
-		action: import('astro/actions/runtime/server.js').ActionClient<any, any, any>,
-	): string;
 }
