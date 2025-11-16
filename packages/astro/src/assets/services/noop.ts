1445fad485db4b60cc971e428615c3888966da35
ktym4a <shoma@ktym4a.me>
2025-11-17 00:19:44 +0900

    fix: Add validateOptions to noopService to handle transform images

--- packages/astro/src/assets/services/noop.ts
+++ packages/astro/src/assets/services/noop.ts
@@ -1,9 +1,19 @@
+import { isESMImportedImage } from '../utils/imageKind.js';
 import { baseService, type LocalImageService } from './service.js';
 
 // Empty service used for platforms that don't support Sharp / users who don't want transformations.
 const noopService: LocalImageService = {
 	...baseService,
 	propertiesToHash: ['src'],
+	validateOptions(options, imageConfig) {
+		if (isESMImportedImage(options.src)) {
+			return {
+				src: options.src,
+				format: options.src.format,
+			};
+		}
+		return baseService.validateOptions?.(options, imageConfig) ?? options;
+	},
 	async transform(inputBuffer, transformOptions) {
 		return {
 			data: inputBuffer,
