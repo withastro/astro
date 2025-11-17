39c2469e9517e4afdfd00f33dae06c6068b08b60
ktym4a <shoma@ktym4a.me>
2025-11-18 01:11:26 +0900

    fix: Simplify noop service by removing ESM image check and format field

--- packages/astro/src/assets/services/noop.ts
+++ packages/astro/src/assets/services/noop.ts
@@ -1,18 +1,14 @@
-import { isESMImportedImage } from '../utils/imageKind.js';
 import { baseService, type LocalImageService } from './service.js';
 
 // Empty service used for platforms that don't support Sharp / users who don't want transformations.
 const noopService: LocalImageService = {
 	...baseService,
 	propertiesToHash: ['src'],
-	validateOptions(options, imageConfig) {
-		if (isESMImportedImage(options.src)) {
-			return {
-				src: options.src,
-				format: options.src.format,
-			};
-		}
-		return baseService.validateOptions?.(options, imageConfig) ?? options;
+	async validateOptions(options, imageConfig) {
+		const newOptions = await (baseService.validateOptions?.(options, imageConfig) ?? options);
+		delete newOptions.format;
+
+		return newOptions;
 	},
 	async transform(inputBuffer, transformOptions) {
 		return {
