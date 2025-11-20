00c1322bcb647c422c02e5380d481377a23b9fd0
Erika <3019731+Princesseuh@users.noreply.github.com>
2025-11-20 15:12:17 +0100

    fix: node leak into content collections (#14820)
    Co-authored-by: Emanuele Stoppa <my.burning@gmail.com>

--- packages/astro/src/core/app/index.ts
+++ packages/astro/src/core/app/index.ts
@@ -1,13 +1,13 @@
 export type { RoutesList } from '../../types/astro.js';
 export { App } from './app.js';
 export { BaseApp, type RenderErrorOptions, type RenderOptions } from './base.js';
-export { deserializeManifest, fromRoutingStrategy, toRoutingStrategy } from './common.js';
-export { DevApp } from './dev/app.js';
+export { fromRoutingStrategy, toRoutingStrategy } from './common.js';
 export { createConsoleLogger } from './logging.js';
 export {
 	deserializeRouteData,
 	deserializeRouteInfo,
 	serializeRouteData,
 	serializeRouteInfo,
+	deserializeManifest
 } from './manifest.js';
 export { AppPipeline } from './pipeline.js';
