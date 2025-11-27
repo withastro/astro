abb67735017442f8512b7c125691349193f27d91
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-11-27 18:54:55 +0100

    chore(fonts): classes

--- packages/astro/src/assets/fonts/infra/fontace-font-file-reader.ts
+++ packages/astro/src/assets/fonts/infra/fontace-font-file-reader.ts
@@ -4,24 +4,22 @@ import { AstroError, AstroErrorData } from '../../../core/errors/index.js';
 import type { FontFileReader } from '../definitions.js';
 import type { Style } from '../types.js';
 
-export function createFontaceFontFileReader(): FontFileReader {
-	return {
-		extract({ family, url }) {
-			try {
-				const data = fontace(readFileSync(url));
-				return {
-					weight: data.weight,
-					style: data.style as Style,
-				};
-			} catch (cause) {
-				throw new AstroError(
-					{
-						...AstroErrorData.CannotDetermineWeightAndStyleFromFontFile,
-						message: AstroErrorData.CannotDetermineWeightAndStyleFromFontFile.message(family, url),
-					},
-					{ cause },
-				);
-			}
-		},
-	};
+export class FontaceFontFileReader implements FontFileReader {
+	extract({ family, url }: { family: string; url: string }): { weight: string; style: Style } {
+		try {
+			const data = fontace(readFileSync(url));
+			return {
+				weight: data.weight,
+				style: data.style as Style,
+			};
+		} catch (cause) {
+			throw new AstroError(
+				{
+					...AstroErrorData.CannotDetermineWeightAndStyleFromFontFile,
+					message: AstroErrorData.CannotDetermineWeightAndStyleFromFontFile.message(family, url),
+				},
+				{ cause },
+			);
+		}
+	}
 }
