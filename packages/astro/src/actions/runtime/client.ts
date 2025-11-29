a3afba95929d83226e6f49098fa2d939f6ce7805
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-11-29 12:50:21 +0100

    refactor: move defineAction and getActionContext exports to /entrypoints

--- packages/astro/src/actions/runtime/client.ts
+++
@@ -1,7 +0,0 @@
-export function defineAction() {
-	throw new Error('[astro:action] `defineAction()` unexpectedly used on the client.');
-}
-
-export function getActionContext() {
-	throw new Error('[astro:action] `getActionContext()` unexpectedly used on the client.');
-}
--- packages/astro/src/actions/runtime/entrypoints/client.ts
+++ packages/astro/src/actions/runtime/entrypoints/client.ts
@@ -2,3 +2,11 @@ export { actions, getActionPath } from "../virtual.js";
 export { isInputError, isActionError, ActionError } from "../shared.js";
 
 export type { ActionAPIContext, ActionErrorCode } from "../shared.js";
+
+export function defineAction() {
+	throw new Error('[astro:action] `defineAction()` unexpectedly used on the client.');
+}
+
+export function getActionContext() {
+	throw new Error('[astro:action] `getActionContext()` unexpectedly used on the client.');
+}
