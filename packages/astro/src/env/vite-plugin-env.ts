3feb29ff4a7c7aa5025845c7eb2f688eb395db10
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-12-10 15:07:38 +0100

    wip

--- packages/astro/src/env/vite-plugin-env.ts
+++ packages/astro/src/env/vite-plugin-env.ts
@@ -49,53 +49,67 @@ export function astroEnv({ settings, sync, envLoader }: AstroEnvPluginParams): P
 
 			populated = true;
 		},
-		resolveId(id) {
-			if (id === CLIENT_VIRTUAL_MODULE_ID) {
-				return RESOLVED_CLIENT_VIRTUAL_MODULE_ID;
-			}
-			if (id === SERVER_VIRTUAL_MODULE_ID) {
-				return RESOLVED_SERVER_VIRTUAL_MODULE_ID;
-			}
-			if (id === INTERNAL_VIRTUAL_MODULE_ID) {
-				return RESOLVED_INTERNAL_VIRTUAL_MODULE_ID;
-			}
+		resolveId: {
+			filter: {
+				id: new RegExp(
+					`^(${CLIENT_VIRTUAL_MODULE_ID}|${SERVER_VIRTUAL_MODULE_ID}|${INTERNAL_VIRTUAL_MODULE_ID})$`,
+				),
+			},
+			handler(id) {
+				if (id === CLIENT_VIRTUAL_MODULE_ID) {
+					return RESOLVED_CLIENT_VIRTUAL_MODULE_ID;
+				}
+				if (id === SERVER_VIRTUAL_MODULE_ID) {
+					return RESOLVED_SERVER_VIRTUAL_MODULE_ID;
+				}
+				if (id === INTERNAL_VIRTUAL_MODULE_ID) {
+					return RESOLVED_INTERNAL_VIRTUAL_MODULE_ID;
+				}
+			},
 		},
-		load(id, options) {
-			if (id === RESOLVED_INTERNAL_VIRTUAL_MODULE_ID) {
-				return { code: `export const schema = ${JSON.stringify(schema)};` };
-			}
-
-			if (id === RESOLVED_SERVER_VIRTUAL_MODULE_ID && !options?.ssr) {
-				throw new AstroError({
-					...AstroErrorData.ServerOnlyModule,
-					message: AstroErrorData.ServerOnlyModule.message(SERVER_VIRTUAL_MODULE_ID),
-				});
-			}
-
-			if (id === RESOLVED_CLIENT_VIRTUAL_MODULE_ID || id === RESOLVED_SERVER_VIRTUAL_MODULE_ID) {
-				const loadedEnv = envLoader.get();
-
-				const validatedVariables = validatePublicVariables({
-					schema,
-					loadedEnv,
-					validateSecrets,
-					sync,
-				});
-				const { client, server } = getTemplates({
-					schema,
-					validatedVariables,
-					// In dev, we inline process.env to avoid freezing it
-					loadedEnv: isBuild ? null : loadedEnv,
-				});
-
-				if (id === RESOLVED_CLIENT_VIRTUAL_MODULE_ID) {
-					return { code: client };
+		load: {
+			filter: {
+				id: new RegExp(
+					`^(${RESOLVED_CLIENT_VIRTUAL_MODULE_ID}|${RESOLVED_SERVER_VIRTUAL_MODULE_ID}|${RESOLVED_INTERNAL_VIRTUAL_MODULE_ID})$`,
+				),
+			},
+			handler(id, options) {
+				if (id === RESOLVED_INTERNAL_VIRTUAL_MODULE_ID) {
+					return { code: `export const schema = ${JSON.stringify(schema)};` };
 				}
 
-				if (id === RESOLVED_SERVER_VIRTUAL_MODULE_ID) {
-					return { code: server };
+				if (id === RESOLVED_SERVER_VIRTUAL_MODULE_ID && !options?.ssr) {
+					throw new AstroError({
+						...AstroErrorData.ServerOnlyModule,
+						message: AstroErrorData.ServerOnlyModule.message(SERVER_VIRTUAL_MODULE_ID),
+					});
 				}
-			}
+
+				if (id === RESOLVED_CLIENT_VIRTUAL_MODULE_ID || id === RESOLVED_SERVER_VIRTUAL_MODULE_ID) {
+					const loadedEnv = envLoader.get();
+
+					const validatedVariables = validatePublicVariables({
+						schema,
+						loadedEnv,
+						validateSecrets,
+						sync,
+					});
+					const { client, server } = getTemplates({
+						schema,
+						validatedVariables,
+						// In dev, we inline process.env to avoid freezing it
+						loadedEnv: isBuild ? null : loadedEnv,
+					});
+
+					if (id === RESOLVED_CLIENT_VIRTUAL_MODULE_ID) {
+						return { code: client };
+					}
+
+					if (id === RESOLVED_SERVER_VIRTUAL_MODULE_ID) {
+						return { code: server };
+					}
+				}
+			},
 		},
 	};
 }
