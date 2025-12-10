93c8cb23963b950b14635e9514105a1654bf75cb
Florian Lefebvre <contact@florian-lefebvre.dev>
2025-12-10 15:45:16 +0100

    wip

--- packages/astro/src/vite-plugin-scripts/index.ts
+++ packages/astro/src/vite-plugin-scripts/index.ts
@@ -17,39 +17,44 @@ export default function astroScriptsPlugin({ settings }: { settings: AstroSettin
 	return {
 		name: 'astro:scripts',
 
-		async resolveId(id) {
-			if (id.startsWith(SCRIPT_ID_PREFIX)) {
-				return id;
-			}
-			return undefined;
-		},
+		// TODO: check if actually useful
+		// async resolveId(id) {
+		// 	if (id.startsWith(SCRIPT_ID_PREFIX)) {
+		// 		return id;
+		// 	}
+		// 	return undefined;
+		// },
 
-		async load(id) {
-			if (id === BEFORE_HYDRATION_SCRIPT_ID) {
-				return {
-					code: settings.scripts
-						.filter((s) => s.stage === 'before-hydration')
-						.map((s) => s.content)
-						.join('\n'),
-				};
-			}
-			if (id === PAGE_SCRIPT_ID) {
-				return {
-					code: settings.scripts
-						.filter((s) => s.stage === 'page')
-						.map((s) => s.content)
-						.join('\n'),
-				};
-			}
-			if (id === PAGE_SSR_SCRIPT_ID) {
-				return {
-					code: settings.scripts
-						.filter((s) => s.stage === 'page-ssr')
-						.map((s) => s.content)
-						.join('\n'),
-				};
-			}
-			return null;
+		load: {
+			filter: {
+				id: new RegExp(`^(${BEFORE_HYDRATION_SCRIPT_ID}|${PAGE_SCRIPT_ID}|${PAGE_SSR_SCRIPT_ID})$`),
+			},
+			handler(id) {
+				if (id === BEFORE_HYDRATION_SCRIPT_ID) {
+					return {
+						code: settings.scripts
+							.filter((s) => s.stage === 'before-hydration')
+							.map((s) => s.content)
+							.join('\n'),
+					};
+				}
+				if (id === PAGE_SCRIPT_ID) {
+					return {
+						code: settings.scripts
+							.filter((s) => s.stage === 'page')
+							.map((s) => s.content)
+							.join('\n'),
+					};
+				}
+				if (id === PAGE_SSR_SCRIPT_ID) {
+					return {
+						code: settings.scripts
+							.filter((s) => s.stage === 'page-ssr')
+							.map((s) => s.content)
+							.join('\n'),
+					};
+				}
+			},
 		},
 		buildStart() {
 			const hasHydrationScripts = settings.scripts.some((s) => s.stage === 'before-hydration');
