52ed875056457afb73dd7a653dbfcca749b436bc
Alexander Niebuhr <alexander@nbhr.io>
2025-12-29 19:03:48 +0100

    chore: add manual pages support test fixture

---
+++ packages/integrations/cloudflare/test/fixtures/manual-pages-support/astro.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+import cloudflare from '@astrojs/cloudflare';
+import { defineConfig, envField, fontProviders } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import { fileURLToPath } from 'node:url';
+import react from '@astrojs/react';
+import vue from "@astrojs/vue"
+
+export default defineConfig({
+	adapter: cloudflare({
+		imageService: 'passthrough',
+	}),
+	build: {
+		inlineStylesheets: 'never',
+		client: './',
+		server: './_worker.js',
+	},
+	vite: {
+		resolve: {
+			alias: {
+				'@images': fileURLToPath(new URL('./images', import.meta.url)),
+			},
+		},
+	},
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "fr"],
+		routing: {
+			prefixDefaultLocale: false,
+			redirectToDefaultLocale: false
+		},
+		fallback: {
+			"fr": "en"
+		}
+	},
+	integrations: [mdx(), react(), vue()],
+	env: {
+		schema: {
+			FOO: envField.string({ context: 'server', access: 'public' }),
+			BAR: envField.string({ context: 'client', access: 'public' }),
+			SECRET: envField.string({ context: 'server', access: 'secret' }),
+		}
+	},
+	prefetch: true,
+	experimental: {
+		fonts: [{
+			provider: fontProviders.google(),
+			name: "Roboto",
+			cssVariable: "--font-roboto"
+		}],
+		chromeDevtoolsWorkspace: true
+	},
+});
