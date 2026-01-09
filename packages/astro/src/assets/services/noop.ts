4d4d9631d16b982d5be84f86d3fe9cd9e35e9b80
Julien Cayzac <julien.cayzac@rakuten.com>
2026-01-09 20:27:19 +0900

    revert change to noop service

--- packages/astro/src/assets/services/noop.ts
+++ packages/astro/src/assets/services/noop.ts
@@ -1,3 +1,4 @@
+import { isESMImportedImage } from '../utils/imageKind.js';
 import { baseService, type LocalImageService, verifyOptions } from './service.js';
 
 // Empty service used for platforms that don't support Sharp / users who don't want transformations.
@@ -5,7 +6,12 @@ const noopService: LocalImageService = {
 	...baseService,
 	propertiesToHash: ['src'],
 	async validateOptions(options) {
-		delete options.format;
+		if (isESMImportedImage(options.src) && options.src.format === 'svg') {
+			options.format = 'svg';
+		} else {
+			delete options.format;
+		}
+
 		verifyOptions(options);
 
 		return options;
