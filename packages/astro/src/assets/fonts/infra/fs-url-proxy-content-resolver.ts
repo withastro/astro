214eafb442ce57eb4553e6c0eccb1fa73689248c
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-01-15 12:18:24 +0100

    unify remote and local content resolver handling

--- packages/astro/src/assets/fonts/infra/fs-url-proxy-content-resolver.ts
+++ packages/astro/src/assets/fonts/infra/fs-url-proxy-content-resolver.ts
@@ -1,10 +1,14 @@
 import { readFileSync } from 'node:fs';
+import { isAbsolute } from 'node:path';
 import { AstroError, AstroErrorData } from '../../../core/errors/index.js';
 import type { UrlProxyContentResolver } from '../definitions.js';
 
-// TODO: use based on if url is absolute
-export class LocalUrlProxyContentResolver implements UrlProxyContentResolver {
+export class FsUrlProxyContentResolver implements UrlProxyContentResolver {
 	resolve(url: string): string {
+		if (!isAbsolute(url)) {
+			// HTTP URLs are enough
+			return url;
+		}
 		try {
 			// We use the url and the file content for the hash generation because:
 			// - The URL is not hashed unlike remote providers
