344e5cdb0565d9e4ac77522d2f70a059c5bc5e47
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-01-20 09:52:28 +0100

    move

--- packages/astro/src/assets/fonts/runtime.ts
+++ packages/astro/src/assets/fonts/runtime.ts
@@ -1,40 +1,7 @@
 import * as fontsMod from 'virtual:astro:assets/fonts/internal';
-import { AstroError, AstroErrorData } from '../../core/errors/index.js';
+import { createGetFontBuffer } from './core/create-get-font-buffer.js';
 import { createGetFontData } from './core/create-get-font-data.js';
-import type { BufferImports } from './types.js';
 
 export const getFontData = createGetFontData(fontsMod);
 
-export function createGetFontBuffer({ bufferImports }: { bufferImports?: BufferImports }) {
-	return async function getFontBuffer(url: string) {
-		// TODO: remove once fonts are stabilized
-		if (!bufferImports) {
-			throw new AstroError(AstroErrorData.ExperimentalFontsNotEnabled);
-		}
-		// Should always be able to split but we default to a hash that will always fail
-		const hash = url.split('/').pop() ?? '';
-		const fn = bufferImports[hash];
-		if (!fn) {
-			throw new AstroError({
-				...AstroErrorData.FontBufferNotFound,
-				message: AstroErrorData.FontBufferNotFound.message(url),
-			});
-		}
-		let mod;
-		try {
-			mod = await fn();
-		} catch {
-			throw new AstroError({
-				...AstroErrorData.FontBufferNotFound,
-				message: AstroErrorData.FontBufferNotFound.message(url),
-			});
-		}
-		if (!mod?.default) {
-			throw new AstroError({
-				...AstroErrorData.FontBufferNotFound,
-				message: AstroErrorData.FontBufferNotFound.message(url),
-			});
-		}
-		return mod.default;
-	};
-}
+export const getFontBuffer = createGetFontBuffer(fontsMod);
