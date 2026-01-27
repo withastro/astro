e2915b679b66e7d1c9d6f1c73994ae045df7be60
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-01-27 10:41:50 +0100

    misc

--- packages/integrations/cloudflare/src/entrypoints/server.ts
+++ packages/integrations/cloudflare/src/entrypoints/server.ts
@@ -1,8 +1,5 @@
 import { handle } from '../utils/handler.js';
 
 export default {
-	async fetch(request, env, ctx) {
-		const response = await handle(request, env, ctx)
-		return response
-	}
+	fetch: handle,
 } satisfies ExportedHandler<Env>;
