eeb18701b6024ffa83157eeedd6f090dfbb3fb09
Matt Kane <mkane@cloudflare.com>
2026-01-29 12:31:41 +0000

    lock

--- packages/integrations/cloudflare/test/fixtures/linked-package-client/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import cloudflare from '@astrojs/cloudflare';
-import react from '@astrojs/react';
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-	integrations: [react()],
-	adapter: cloudflare(),
-	output: 'server',
-});
