8c4c10dca82dca12c8901cf1d1ac08bcd03dfb5b
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-05 15:56:23 +0100

    wip

---
+++ packages/integrations/node/src/entrypoints/middleware.ts
@@ -0,0 +1,40 @@
+import type { RequestHandler } from '../types.js';
+import { NodeApp } from 'astro/app/node';
+import { manifest } from 'astro:ssr-manifest';
+import { setGetEnv } from 'astro/env/setup';
+import * as options from 'virtual:astro-node:config';
+import { createAppHandler } from '../serve-app.js';
+
+setGetEnv((key) => process.env[key]);
+
+// using spread args because express trips up if the function's
+// stringified body includes req, res, next, locals directly
+export function astroMiddleware(): RequestHandler {
+	const app = new NodeApp(manifest, !options.experimentalDisableStreaming);
+	const handler = createAppHandler(app, options);
+	const logger = app.getAdapterLogger();
+
+	return async function (...args) {
+		// assume normal invocation at first
+		const [req, res, next, locals] = args;
+		// short circuit if it is an error invocation
+		if (req instanceof Error) {
+			const error = req;
+			if (next) {
+				return next(error);
+			} else {
+				throw error;
+			}
+		}
+		try {
+			await handler(req, res, next, locals);
+		} catch (err) {
+			logger.error(`Could not render ${req.url}`);
+			console.error(err);
+			if (!res.headersSent) {
+				res.writeHead(500, `Server error`);
+				res.end();
+			}
+		}
+	};
+}
