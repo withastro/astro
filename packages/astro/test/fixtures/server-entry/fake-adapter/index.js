ee7e53f9de2338517e149895efd26fca44ad80b6
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-11 09:03:23 +0100

    feat: support serverEntrypoint for self adapters (#15460)
    Co-authored-by: Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>

--- packages/astro/test/fixtures/server-entry/fake-adapter/index.js
+++ packages/astro/test/fixtures/server-entry/fake-adapter/index.js
@@ -1,32 +1,37 @@
 // @ts-check
 
+const ENTRYPOINT = '@test/server-entry-fake-adapter/server.js'
+
 /**
- * @param {{ shape: 'string' | 'object' | 'array' }} param0
+ * @param {{ type: 'rollupInput'; shape: 'string' | 'object' | 'array' } | { type: 'serverEntrypoint' }} options
  * @returns {import('astro').AstroIntegration}
  */
-export default function fakeAdapter({ shape}) {
+export default function fakeAdapter(options) {
     return {
         name: '@test/server-entry-fake-adapter',
         hooks: {
             'astro:config:setup': (params) => {
+                if (options.type === 'rollupInput') {
                 params.updateConfig({
                     vite: {
                         build: {
                             rollupOptions: {
                                 input: {
-                                    string: '@test/server-entry-fake-adapter/server.js',
-                                    object: { foo: '@test/server-entry-fake-adapter/server.js' },
-                                    array: ['@test/server-entry-fake-adapter/server.js']
-                                }[shape]
+                                    string: ENTRYPOINT,
+                                    object: { foo: ENTRYPOINT },
+                                    array: [ENTRYPOINT]
+                                }[options.shape]
                             }
                         }
                     }
                 })
+            }
             },
             'astro:config:done': (params) => {
                 params.setAdapter({
                     name:'@test/server-entry-fake-adapter',
                     entryType: 'self',
+                    serverEntrypoint: options.type === 'serverEntrypoint' ? ENTRYPOINT : undefined,
                     supportedAstroFeatures: {
                         serverOutput: 'stable'
                     }
