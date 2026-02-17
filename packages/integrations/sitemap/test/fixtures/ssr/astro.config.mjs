307bbe4f079f08e037701c9f7eb7cacb534d600d
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-17 09:44:44 +0100

    improve test

--- packages/integrations/sitemap/test/fixtures/ssr/astro.config.mjs
+++ packages/integrations/sitemap/test/fixtures/ssr/astro.config.mjs
@@ -1,4 +1,4 @@
-import nodeServer from '@astrojs/node'
+import node from '@astrojs/node'
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
@@ -6,7 +6,5 @@ export default defineConfig({
   integrations: [sitemap()],
 	site: 'http://example.com',
 	output: 'server',
-	adapter: nodeServer({
-		mode: "standalone"
-	})
+	adapter: node()
 })
