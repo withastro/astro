68acdf5748afb73dd98720e7e6628a9f17ef275a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-19 19:43:44 -0500

    spelling: out-of-date
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/upgrade/upgrade.mjs
+++ packages/upgrade/upgrade.mjs
@@ -9,7 +9,7 @@ const IS_STACKBLITZ = !!process.versions.webcontainer;
 const minimumMajorVersion = IS_STACKBLITZ ? 20 : 22;
 
 if (requiredMajorVersion < minimumMajorVersion) {
-	console.error(`Node.js v${currentVersion} is out of date and unsupported!`);
+	console.error(`Node.js v${currentVersion} is out-of-date and unsupported!`);
 	console.error(`Please use Node.js v${minimumMajorVersion} or higher.`);
 	process.exit(1);
 }
