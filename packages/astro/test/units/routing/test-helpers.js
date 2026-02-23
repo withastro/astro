a131239e83878e5dab672402de61e0ea142df6da
Matthew Phillips <matthew@matthewphillips.info>
2026-02-23 17:30:03 -0500

    Add JSDoc to routing test helpers

--- packages/astro/test/units/routing/test-helpers.js
+++ packages/astro/test/units/routing/test-helpers.js
@@ -1,9 +1,25 @@
 import { getPattern } from '../../../dist/core/routing/pattern.js';
 
+/** @param {string} content */
 const staticPart = (content) => ({ content, dynamic: false, spread: false });
+/** @param {string} content */
 const dynamicPart = (content) => ({ content, dynamic: true, spread: false });
+/** @param {string} content */
 const spreadPart = (content) => ({ content, dynamic: true, spread: true });
 
+/**
+ * @param {object} options
+ * @param {import('../../../dist/types/public/internal.js').RoutePart[][]} options.segments
+ * @param {'always' | 'never' | 'ignore'} options.trailingSlash
+ * @param {string} options.route
+ * @param {string | undefined} options.pathname
+ * @param {'page' | 'endpoint' | 'redirect' | 'fallback'} [options.type]
+ * @param {string | undefined} [options.component]
+ * @param {boolean} [options.isIndex]
+ * @param {boolean} [options.prerender]
+ * @param {'project' | 'internal'} [options.origin]
+ * @param {string[] | undefined} [options.params]
+ */
 const makeRoute = ({
 	segments,
 	trailingSlash,
