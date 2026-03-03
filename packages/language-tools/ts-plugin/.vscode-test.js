647e47ef3b0e0ba6c660a2f93bce9cdb5892846d
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-03-03 10:28:45 +0100

    wip

--- packages/language-tools/ts-plugin/.vscode-test.js
+++ packages/language-tools/ts-plugin/.vscode-test.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vscode/test-cli');
 module.exports = defineConfig([
 	{
 		label: 'unitTests',
-		files: 'test/**/*.test.ts',
+		files: 'test/**/*.test.js',
 		extensionDevelopmentPath: '../vscode',
 		version: 'stable',
 		mocha: {
