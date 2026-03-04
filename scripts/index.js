8382d0f29022b75b308b66e6b1ab8dc0c846f8c7
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-03-04 10:15:55 +0100

    revert

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-import { spawnSync } from 'node:child_process';
-import * as semver from 'semver';
-
-// Re-exec with --experimental-strip-types on Node < 22.18 (where it's not enabled by default)
-if (
-	!semver.satisfies(process.versions.node, '>=22.20.0') &&
-	!process.execArgv.includes('--experimental-strip-types')
-) {
-	const result = spawnSync(
-		process.execPath,
-		['--experimental-strip-types', ...process.execArgv, process.argv[1], ...process.argv.slice(2)],
-		{ stdio: 'inherit' },
-	);
-	process.exit(result.status ?? 1);
-}
 
 export default async function run() {
 	const [cmd, ...args] = process.argv.slice(2);
