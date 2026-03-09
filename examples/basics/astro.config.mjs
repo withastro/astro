1cf8b19eae2df6f84174db3fb9bcf8744848ddf3
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2026-03-09 15:44:36 +0100

    chore: sneaky

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		rustCompiler: true
+	}
+});
