13ded0a558860d37b55753889ce263f09fcb37ac
Jack Lukic <jacklukic@gmail.com>
2026-03-13 10:54:21 -0400

    fix(build): emit before-hydration chunk for client environment

--- packages/astro/src/vite-plugin-scripts/index.ts
+++ packages/astro/src/vite-plugin-scripts/index.ts
@@ -61,7 +61,8 @@ export default function astroScriptsPlugin({ settings }: { settings: AstroSettin
 			const hasHydrationScripts = settings.scripts.some((s) => s.stage === 'before-hydration');
 			if (
 				hasHydrationScripts &&
-				(this.environment.name === ASTRO_VITE_ENVIRONMENT_NAMES.prerender ||
+				(this.environment.name === ASTRO_VITE_ENVIRONMENT_NAMES.client ||
+					this.environment.name === ASTRO_VITE_ENVIRONMENT_NAMES.prerender ||
 					this.environment.name === ASTRO_VITE_ENVIRONMENT_NAMES.ssr)
 			) {
 				this.emitFile({
