dfe51eb07630f0736d3a19e227be04f7f9d298d8
fkatsuhiro <nakayoshiryutokatsu@gmail.com>
2026-03-22 22:20:42 +0900

    fix: astro.config.mjs file

--- packages/integrations/netlify/test/static/fixtures/image-missing-dimension/astro.config.mjs
+++ packages/integrations/netlify/test/static/fixtures/image-missing-dimension/astro.config.mjs
@@ -2,16 +2,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  // 1. アダプターの設定
   adapter: netlify(),
-  
-  // 2. 出力モードの設定
   output: 'static',
-  
-  // 3. (オプション) 画像処理の挙動を固定する
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp' // またはデフォルト
+      entrypoint: 'astro/assets/services/sharp'
     }
   }
 });
