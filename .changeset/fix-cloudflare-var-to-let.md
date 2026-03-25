4c7a04ef6276f8e73e2a0a2261d7902753e59473
Nandan Acharya <samarthachar18@gmail.com>
2026-03-25 09:30:54 +0530

    chore(cloudflare): use let instead of var in virtual.d.ts

---
+++ .changeset/fix-cloudflare-var-to-let.md
@@ -0,0 +1,5 @@
+---
+'@astrojs/cloudflare': patch
+---
+
+Updates `var` to `let` for the ambient declaration of `astroCloudflareOptions`
--- packages/integrations/cloudflare/virtual.d.ts
+++ packages/integrations/cloudflare/virtual.d.ts
@@ -20,4 +20,4 @@ interface Env extends Cloudflare.Env {}
 type ImagesBinding = import('@cloudflare/workers-types').ImagesBinding;
 type Fetcher = import('@cloudflare/workers-types').Fetcher;
 
-declare var astroCloudflareOptions: import('@cloudflare/vite-plugin').PluginConfig;
+declare let astroCloudflareOptions: import('@cloudflare/vite-plugin').PluginConfig;
