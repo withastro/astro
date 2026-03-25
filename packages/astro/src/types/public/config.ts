d906ad85e3602801c361c2fb3af93f5960786a8f
Matthew Phillips <matthew@matthewphillips.info>
2026-03-25 10:09:25 -0400

    Update packages/astro/src/types/public/config.ts
    Co-authored-by: Armand Philippot <git@armand.philippot.eu>

--- packages/astro/src/types/public/config.ts
+++ packages/astro/src/types/public/config.ts
@@ -1743,7 +1743,7 @@ export interface AstroUserConfig<
 		 * @kind h4
 		 * @type {Record<string, any> | undefined}
 		 * @default `undefined`
-		 * @version 6.0.0
+		 * @version 6.1.0
 		 * @description
 		 *
 		 * The default encoder options passed to `sharp().jpeg()` when using Astro's built-in Sharp image service.
