b6560319c6d40fbec4c465d29c9cb8e2cc7ad23b
Carolina Rodríguez <carol.rodriguez.design@gmail.com>
2026-03-31 13:15:50 +0000

    Instalación tailwind y react

--- examples/basics/astro.config.mjs
+++ examples/basics/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
+});
