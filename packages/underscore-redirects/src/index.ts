402193ed5a08a8f65bafc004dc869cbd179039ae
Alexander Niebuhr <alexanderniebuhr@users.noreply.github.com>
2026-04-01 18:24:31 +0000

    [ci] format

--- packages/underscore-redirects/src/index.ts
+++ packages/underscore-redirects/src/index.ts
@@ -1,7 +1,7 @@
 export {
 	createHostedRouteDefinition,
 	createRedirectsFromAstroRoutes,
-	getTrailingSlashPaths
+	getTrailingSlashPaths,
 } from './astro.js';
 export { HostRoutes } from './host-route.js';
 export { printAsRedirects } from './print.js';
