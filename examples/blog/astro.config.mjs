39a4c434ff9d22878f35c11d2b52e611750290b2
Alex Dombroski <florian-lefebvre@users.noreply.github.com>
2026-04-08 06:16:08 +0000

    [ci] format

--- examples/blog/astro.config.mjs
+++ examples/blog/astro.config.mjs
@@ -8,26 +8,28 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-	fonts: [{
-		provider: fontProviders.local(),
-		name: "Atkinson",
-		cssVariable: "--font-atkinson",
-		fallbacks: ["sans-serif"],
-		options: {
-			variants: [
-				{
-					src: ['./src/assets/fonts/atkinson-regular.woff'],
-					weight: 400,
-					style: 'normal',
-					display: 'swap'
-				},
-				{
-					src: ['./src/assets/fonts/atkinson-bold.woff'],
-					weight: 700,
-					style: 'normal',
-					display: 'swap'
-				}
-			]
-		}
-	}]
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
 });
