affd49ed2790062e55c834d000bbb8ef6194ec3b
Roman <dev@rman.dev>
2026-04-08 10:59:32 +0100

    fix: use `for...of`

--- packages/astro/src/preferences/dlv.ts
+++ packages/astro/src/preferences/dlv.ts
@@ -1,8 +1,7 @@
 export default function dlv(obj: Record<string, unknown>, key: string): any {
-  const k = key.split('.');
-  for (let p = 0; p < k.length; p++) {
-    // @ts-expect-error: Type 'unknown' is not assignable to type 'Record<string, unknown>'
-    obj = obj ? obj[k[p]] : undefined;
+  for (const k of key.split('.')) {
+    // @ts-expect-error: Type 'unknown' is not assignable to type 'Record<string, unknown>'.
+    obj = obj?.[k];
   }
   return obj;
 }
