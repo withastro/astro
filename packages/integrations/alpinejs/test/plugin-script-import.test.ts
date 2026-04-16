6b0cd3dd05a73674a3fabec39837e6d77a2142d8
ocavue <ocavue@gmail.com>
2026-04-16 21:39:14 +1000

    fix extend

--- packages/integrations/alpinejs/test/plugin-script-import.test.ts
+++ packages/integrations/alpinejs/test/plugin-script-import.test.ts
@@ -4,7 +4,6 @@ import { prepareTestFactory } from './test-utils.js';
 const { test } = prepareTestFactory({ root: './fixtures/plugin-script-import/' });
 
 test.describe('Plugin Script Import', () => {
-	// @ts-expect-error astro fixture is defined in untyped test-utils.js
 	test('Extending Alpine using a script import should work', async ({ page, astro }) => {
 		await page.goto(astro.resolveUrl('/'));
 
