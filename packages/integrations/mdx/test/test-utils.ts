1f9444edfa9c1e185af652d1c868a6b17153e78f
ocavue <ocavue@gmail.com>
2026-04-18 11:36:02 +1000

    refactor(mdx): add SpyIntegrationLogger for test

--- packages/integrations/mdx/test/test-utils.ts
+++ packages/integrations/mdx/test/test-utils.ts
@@ -2,6 +2,10 @@ import type * as estree from 'estree';
 import type * as hast from 'hast';
 import type * as mdast from 'mdast';
 import type * as unified from 'unified';
+import {
+	AstroIntegrationLogger,
+	type AstroLogMessage,
+} from '../../../astro/dist/core/logger/core.js';
 
 export type RemarkPlugin<PluginParameters extends any[] = any[]> = unified.Plugin<
 	PluginParameters,
@@ -17,3 +21,24 @@ export type RecmaPlugin<PluginParameters extends any[] = any[]> = unified.Plugin
 	PluginParameters,
 	estree.Program
 >;
+
+export class SpyIntegrationLogger extends AstroIntegrationLogger {
+	readonly messages: AstroLogMessage[];
+
+	constructor() {
+		const messages: AstroLogMessage[] = [];
+		super(
+			{
+				destination: {
+					write(chunk): boolean {
+						messages.push(chunk);
+						return true;
+					},
+				},
+				level: 'warn',
+			},
+			'test-spy',
+		);
+		this.messages = messages;
+	}
+}
