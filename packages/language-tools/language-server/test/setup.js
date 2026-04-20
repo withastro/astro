44ec6f4f58dac011aa43871334c6049afca2f171
Matthew Phillips <matthew@matthewphillips.info>
2026-04-20 18:00:26 -0400

    fix: avoid loading Astro CLI test helper on Node 20

--- packages/language-tools/language-server/test/setup.js
+++ packages/language-tools/language-server/test/setup.js
@@ -1,7 +1,5 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-// @ts-expect-error
-import { cli } from '../../../astro/test/test-utils.js';
 
 // Copied from utils.ts so we don't have to import TS code for Node 20
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +11,8 @@ export default async function setup() {
 	// We only run the tests that require sync on Node.js versions other than 20 because the language server supports
 	// a lower minimum version than Astro itself due to our lowest supported VS Code version, which mean we can't run Astro
 	if (Number.parseInt(process.versions.node) !== 20) {
+		// @ts-expect-error
+		const { cli } = await import('../../../astro/test/test-utils.js');
 		const res = await cli('sync', '--root', fixtureDir).getResult();
 		if (res.exitCode !== 0) {
 			throw new Error(res.stderr);
