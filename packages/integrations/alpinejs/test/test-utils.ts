7a7d8a18cdbd54813b38c2322cef84e34e451ee7
rururux <rururux@gmail.com>
2026-04-25 19:49:48 +0900

    Remove redundant code

--- packages/integrations/alpinejs/test/test-utils.ts
+++ packages/integrations/alpinejs/test/test-utils.ts
@@ -1,5 +1,3 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { test as testBase } from '@playwright/test';
 import {
@@ -9,16 +7,6 @@ import {
 	type DevServer,
 } from '../../../astro/test/test-utils.js';
 
-// Get all test files in directory, assign unique port for each of them so they don't conflict
-const testFiles = await fs.readdir(new URL('.', import.meta.url));
-const testFileToPort = new Map();
-for (let i = 0; i < testFiles.length; i++) {
-	const file = testFiles[i];
-	if (file.endsWith('.test.js')) {
-		testFileToPort.set(file.slice(0, -8), 4000 + i);
-	}
-}
-
 function loadFixture(inlineConfig: AstroInlineConfig) {
 	if (!inlineConfig?.root) throw new Error("Must provide { root: './fixtures/...' }");
 
@@ -27,9 +15,6 @@ function loadFixture(inlineConfig: AstroInlineConfig) {
 	return baseLoadFixture({
 		...inlineConfig,
 		root: fileURLToPath(new URL(inlineConfig.root, import.meta.url)),
-		server: {
-			port: testFileToPort.get(path.basename(String(inlineConfig.root))),
-		},
 	});
 }
 
