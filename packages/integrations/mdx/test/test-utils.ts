91d5e11ad39bfd9ca62ffbd76801defb2a07a8f9
ematipico <estoppa@cloudflare.com>
2026-04-27 14:04:06 +0100

    fix tests

--- packages/integrations/mdx/test/test-utils.ts
+++ packages/integrations/mdx/test/test-utils.ts
@@ -3,7 +3,7 @@ import type * as hast from 'hast';
 import type * as mdast from 'mdast';
 import type * as unified from 'unified';
 
-export { SpyLogger } from '../../../astro/test/units/test-utils.js';
+export { SpyLogger } from '../../../astro/test/units/test-utils.ts';
 export {
 	loadFixture,
 	type AstroInlineConfig,
