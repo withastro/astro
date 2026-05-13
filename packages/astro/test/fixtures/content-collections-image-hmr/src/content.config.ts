2280feb249019065865ef8d968d07fa4f155924e
fkatsuhiro <nakayoshiryutokatsu@gmail.com>
2026-05-14 02:12:43 +0900

    test: edit the image url which is in content collections error

---
+++ packages/astro/test/fixtures/content-collections-image-hmr/src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			image: image(),
+			secondImage: image(),
+		}),
+});
+
+export const collections = { blog };
