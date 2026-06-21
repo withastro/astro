2f511e47d65a9e6cc08ab1014eab7116f591d17c
swaggysnippets <126371507+swaggysnippets@users.noreply.github.com>
2026-06-21 12:01:49 -0500

    Add Cloudflare Actions build regression test

--- packages/integrations/cloudflare/test/test-utils.ts
+++ packages/integrations/cloudflare/test/test-utils.ts
@@ -18,25 +18,5 @@ export async function loadFixture(inlineConfig: AstroInlineConfig): Promise<Fixt
 		root: new URL(inlineConfig.root as string, import.meta.url).toString(),
 	});
 
-	// For unknown reasons, the error below could raise during testing. We add a retry mechanism to handle it.
-	// Some further investigation is needed to understand the root cause.
-	//
-	// Unable to build fixture for the attempt 1: Error: There is a new version of the pre-bundle for "/astro/packages/integrations/cloudflare/test/fixtures/with-svelte/node_modules/.vite/deps_ssr/svelte_server.js?v=9924cddf", a page reload is going to ask for it.
-	const buildWithRetry: Fixture['build'] = async (...args) => {
-		let err: unknown;
-		for (let attempt = 1; attempt <= 3; attempt++) {
-			try {
-				return await fixture.build(...args);
-			} catch (error) {
-				console.error(`Unable to build fixture for the attempt ${attempt}:`, error);
-				err = error;
-			}
-		}
-
-		if (err) {
-			throw err;
-		}
-	};
-
-	return { ...fixture, build: buildWithRetry };
+	return fixture;
 }
