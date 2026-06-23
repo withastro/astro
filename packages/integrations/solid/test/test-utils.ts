d2d80a8b2a3873a82353b664c92030420224e06c
Matthew Phillips <matthew@matthewphillips.info>
2026-06-23 09:31:06 -0400

    Remove unused exports from test-utils

--- packages/integrations/solid/test/test-utils.ts
+++ packages/integrations/solid/test/test-utils.ts
@@ -1,6 +1,1 @@
-export {
-	isWindows,
-	loadFixture,
-	type DevServer,
-	type Fixture,
-} from 'astro/_internal/test/test-utils';
+export { loadFixture, type Fixture } from 'astro/_internal/test/test-utils';
