1f010ac6d7f80565295feb706bac8811d8268bb0
ocavue <ocavue@gmail.com>
2026-06-23 23:01:00 +1000

    Remove `@__PURE__` comment notation

--- packages/astro/test/units/build/server-islands.test.ts
+++ packages/astro/test/units/build/server-islands.test.ts
@@ -125,9 +125,12 @@ describe('Build: Server islands in prerendered pages', () => {
 		const serverOutputDir = fileURLToPath(settings.config.build.server);
 		const outputFiles = await readFilesRecursive(serverOutputDir);
 		const manifestFilePath = outputFiles.find((file) => file.includes('server-island-manifest'));
-		const manifestContent = manifestFilePath ? await fs.readFile(manifestFilePath, 'utf-8') : null;
+		let manifestContent = manifestFilePath ? await fs.readFile(manifestFilePath, 'utf-8') : null;
 
 		assert.ok(manifestContent, 'Server island manifest chunk should be emitted');
+
+		// Remove the @__PURE__ comment notation
+		manifestContent = manifestContent.replaceAll('/* @__PURE__ */', '');
 
 		assert.ok(
 			/["']Island["']/.test(manifestContent),
