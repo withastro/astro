3b1490ca25570476ed7fd3881d50403d87fc1ab5
ocavue <ocavue@gmail.com>
2026-09-17 13:25:25 +1000

    update

--- packages/language-tools/language-server/bin/nodeServer.js
+++ packages/language-tools/language-server/bin/nodeServer.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { createRequire } from 'node:module';
 
-if (process.argv.includes('--version')) {
-	const { version } = createRequire(import.meta.url)('../package.json');
-	console.info(`${version}`);
-} else {
-	import('../dist/nodeServer.js').catch((error) => {
-		console.error(error);
-		process.exitCode = 1;
-	});
+async function main() {
+	if (process.argv.includes('--version')) {
+		const json = await import('../package.json', { with: { type: 'json' } });
+		console.log(json.default.version)
+	} else { 
+		await import('../dist/nodeServer.js')
+	}
 }
+
+void main()
